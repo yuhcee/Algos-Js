4b9ac0e587bc69bdf6a6b4df0e54d50318b43ca4
yuhcee <egbouchenna001@gmail.com>
2022-03-03 14:55:34 +0100

    add buildGraph func

--- Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/courseSchedule2.js
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/courseSchedule2.js
@@ -12,37 +12,15 @@
  */
 
 // Topolgical sort must work on a directed Graph and Acyclic graph
-/* const findOrder = (numCourses, prerequisites) => {
-    const visited = new Set();
-    const topSort = [];
+const findOrder = (numCourses, prerequisites) => {
     const graph = buildGraph(numCourses, prerequisites);
-
-    for (let node in graph) {
-        if (hasPath(graph, node, visited, topSort) === true) {
-            return [];
-        } else {
-            topSort.push(node);
-        }
-    }
-
-    return topSort;
-};
-
-const hasPath = (graph, node, visited) => {
-    if (!visited.has(String(node))) return false;
-    visited.add(String(node));
-
-    for (let neighbor of graph[node]) {
-        hasPath(graph, neighbor, visited);
-    }
-
-    return true;
+    
 };
 
 const buildGraph = (n, edges) => {
     const graph = {};
 
-    for (let i = 0; i < n; i++) {
+    for (let i = 0; i < n; i += 1) {
         graph[i] = [];
     }
 
@@ -51,63 +29,64 @@ const buildGraph = (n, edges) => {
 
         graph[a].push(b);
     }
+
     return graph;
-}; */
-
-const findOrder = (numCourses, prerequisites) => {
-    const adjList = buildAdjList(numCourses, prerequisites);
-    const visited = {};
-    const arrive = new Array(numCourses).fill(0);
-    const depart = new Array(numCourses).fill(0);
-    const topSort = [];
-    
-    for (let node = 0; node < adjList.length; node++) {
-        if (!visited[node]) {
-            if (dfs(node, adjList, visited, arrive, depart, topSort)) return [];
-        }
-    }
-    
-    return topSort;
-};
-
-const dfs = (node, adjList, visited, arrive, depart, topSort) => {
-    arrive[node]++;
-    visited[node] = true;
-
-    for (let neighbor of adjList[node]) {
-        if (!visited[neighbor]) {
-            visited[neighbor] = true;
-            if (dfs(neighbor, adjList, visited, arrive, depart, topSort)) return true;
-        } else {
-            if (depart[neighbor] === 0) return true;
-        }
-    }
-
-    depart[node]++;
-    topSort.push(node);
-    return false;
-};
-
-const buildAdjList = (n, edges) => {
-    const adjList = Array.from({ length: n }, () => []);
-
-    for (let edge of edges) {
-        let [a, b] = edge;
-        adjList[a].push(b);
-    }
-    return adjList;
 };
 
 const numCourses = 4,
-    prerequesities = [
+    prerequisites = [
         [1, 0],
         [2, 0],
         [3, 1],
         [3, 2],
     ];
-// console.log(findOrder(numCourses, prerequesities));
+// console.log(findOrder(numCourses, prerequisites));
 
 const numCourses2 = 2,
     prerequisites2 = [[0, 1]]; // Output: [0,1]
-// console.log(findOrder(numCourses, prerequesities));
+// console.log(findOrder(numCourses, prerequisites));
 console.log(findOrder(numCourses2, prerequisites2));
+
+/* const visited = new Set();
+const depart = new Array(numCourses).fill(0);
+const topSort = [];
+const graph = buildGraph(numCourses, prerequisites);
+
+for (let node in graph) {
+    if (!visited.has(String(node))) {
+      if (hasPath(graph, node, visited, depart, topSort)) return [];
+    }
+}
+
+return topSort;
+// };
+
+const hasPath = (graph, node, visited, depart, topSort) => {
+visited.add(String(node));
+
+for (let neighbor of graph[node]) {
+    if (!visited.has(String(neighbor))) {
+        visited.add(String(neighbor));
+        if(hasPath(graph, neighbor, visited, depart, topSort)) return true;
+    }
+    if(depart[neighbor] === 0) return true;
+}
+
+depart[node]++;
+topSort.push(node);
+return false;
+};
+
+const buildGraph = (n, edges) => {
+const graph = {};
+
+for (let i = 0; i < n; i++) {
+    graph[i] = [];
+}
+
+for (let edge of edges) {
+    const [a, b] = edge;
+    graph[a].push(b);
+}
+return graph;
+}; */
