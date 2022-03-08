79e2feaaecb0006435d899a45deaf79d74b9e4b7
yuhcee <egbouchenna001@gmail.com>
2022-03-08 23:06:43 +0100

    add trie description

---
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/trie.js
@@ -0,0 +1,19 @@
+/** 
+ * A trie (pronounced as "try") or prefix tree is a tree data structure used to efficiently store and retrieve keys in a dataset
+ * of strings. There are various applications of this data structure, such as autocomplete and spellchecker.
+ * 
+ * Implement the Trie class:
+ * 
+ * Trie() Initializes the trie object.
+ * void insert(String word) Inserts the string word into the trie.
+ * boolean search(String word) Returns true if the string word is in the trie (i.e., was inserted before), and false otherwise
+ * boolean startsWith(String prefix) Returns true if there is a previously inserted string word that has the prefix prefix, and
+ * false otherwise.
+ * 
+ * 
+ * Your Trie object will be instantiated and called as such:
+ * var obj = new Trie()
+ * obj.insert(word)
+ * var param_2 = obj.search(word)
+ * var param_3 = obj.startsWith(prefix)
+ */
