28c9765164aa5903c4aed8c8d2debcccec530ca9
yuhcee <egbouchenna001@gmail.com>
2022-03-09 22:01:46 +0100

    add buildGraph function

---
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/connectedComponents.js
@@ -0,0 +1,48 @@
+/**
+ * **323. Number of Connected Components in an Undirected Graph**
+ *
+ * You have a graph of `n` nodes. You are given an integer `n` and an array edges where
+ * `edges[i] = [ai, bi]` indicates that there is an edge between `ai` and `bi` in the graph.
+ *
+ * Return the *number of connected components in the graph*.
+ * @param {number} n
+ * @param {number[][]} edges
+ * @return {number}
+ */
+
+const countComponents = (n, edges) => {
+    const graph = buildGraph(n, edges);
+};
+
+const buildGraph = (n, edges) => {
+    const graph = {};
+
+    for (let i = 0; i < n; i++) {
+        graph[i] = [];
+    }
+
+    for (let edge of edges) {
+        const [a, b] = edge;
+        graph[a].pus(b);
+        graph[b].push(a);
+    }
+
+    return graph;
+};
+
+const n = 5,
+    edges = [
+        [0, 1],
+        [1, 2],
+        [3, 4],
+    ]; // Output: 2
+const n1 = 5,
+    edges1 = [
+        [0, 1],
+        [1, 2],
+        [2, 3],
+        [3, 4],
+    ]; // Output: 1
+
+console.log(countComponents(n, edges));
+console.log(countComponents(n1, edges1));
