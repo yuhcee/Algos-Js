2167bd8f85996f0aba61dc0aa3946f9ae49e6501
yuhcee <egbouchenna001@gmail.com>
2022-03-11 23:59:04 +0100

    retooling validAnagram

--- Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/validAnagram.js
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/validAnagram.js
@@ -1,78 +1,26 @@
-/* function validAnagram(string1, string2) {
-  if (string1.length !== string2.length) {
-    return false;
-  }
-  let letterCount = {};
+function frequencyCounter(arrOfStrings) {
+    const extractUniqueLetters = arrOfStrings.map((str) => [...new Set(str)].join(''));
+    const uniqueLetters = [...new Set(extractUniqueLetters.join(''))];
+    const result = [];
 
-  for (let letter of string1) {
-    !(letter in letterCount)
-      ? (letterCount[letter] = 1)
-      : letterCount[letter]++;
-  }
-
-  for (let alphabet of string2) {
-    if (!letterCount[alphabet]) {
-      return false;
-    } else {
-      letterCount[alphabet] -= 1;
+    for (let str of arrOfStrings) {
+        let letterCount = countOccurence(str);
+        for (let alphabet of uniqueLetters) {
+            if (letterCount[alphabet] === 2) {
+                result.push(str);
+            }
+        }
     }
-  }
-
-  return true;
-}
-
-console.log(validAnagram('a', 'a')); */
-
-function frequencyCounter(arrOrStrings) {
-  const extractUniqueLetters = arrOrStrings.map((str) => Array.from(new Set(str)).join(''));
-  const uniqueLetters = Array.from(new Set(extractUniqueLetters.join('')));
-  const result = [];
-  
-  for(let str of arrOrStrings ) {
-    let letterCount = countOccurence(str)
-    for (let alphabet of uniqueLetters) {
-      if (letterCount[alphabet] === 2) {
-        result.push(str);
-      }
-    }
-  }
 }
 
 function countOccurence(stringValue) {
-  let letterCount = {};
+    let letterCount = {};
 
-  for (let letter of stringValue) {
-    !(letter in letterCount)
-      ? (letterCount[letter] = 1)
-      : letterCount[letter]++;
-  }
-  return letterCount;
+    for (let letter of stringValue) {
+        letterCount[letter] = letterCount[letter]++ || 1;
+    }
+    console.log(letterCount);
+    return letterCount;
 }
 
-console.log(
-  frequencyCounter([
-    'asdf',
-    'fdas',
-    'asds',
-    'dfm',
-    'dfaa',
-    'aaaa',
-    'aabb',
-    'aaabb',
-  ])
-);
-
-
-class Sum {
-  constructor() {
-    this.value = 0;
-  }
-  sum(value) {
-    this.value = this.value + value;
-    return this;
-  }
-}
-
-const sum = new Sum()
-
-console.log(sum.sum(1).sum(2).sum(3))
+console.log(frequencyCounter(['asdf', 'fdas', 'asds', 'dfm', 'dfaa', 'aaaa', 'aabb', 'aaabb']));
