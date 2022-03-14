a9295f94a536d663440e2d49eedaa513096719f9
yuhcee <egbouchenna001@gmail.com>
2022-03-14 23:45:13 +0100

    numIsland review

--- Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/numIsland.js
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/numIsland.js
@@ -7,39 +7,7 @@
  * @returns {number} number
  */
 
-const numIslands = (grid) => {
-    const visited = new Set();
-    let count = 0;
 
-    for (let r = 0; r < grid.length; r += 1) {
-        for (let c = 0; c < grid[0].length; c += 1) {
-            if (explore(grid, r, c, visited)) count += 1;
-        }
-    }
-
-    return count;
-};
-
-const explore = (grid, r, c, visited) => {
-    const rowInbounds = 0 <= r && r < grid.length;
-    const colInbounds = 0 <= c && c < grid[0].length;
-
-    if (!rowInbounds || !colInbounds) return 0;
-
-    if (grid[r][c] == '0') return 0;
-
-    const pos = `${r},${c}`;
-
-    if (visited.has(pos)) return 0;
-    visited.add(pos);
-
-    explore(grid, r + 1, c, visited);
-    explore(grid, r - 1, c, visited);
-    explore(grid, r, c + 1, visited);
-    explore(grid, r, c - 1, visited);
-
-    return 1;
-};
 const grid = [
     ["1","1","1","1","0"],
     ["1","1","0","1","0"],
