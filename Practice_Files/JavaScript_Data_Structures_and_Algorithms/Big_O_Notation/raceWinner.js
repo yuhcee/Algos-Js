fee39714ed527b73a2b8472797303ae777d3ecc7
yuhcee <egbouchenna001@gmail.com>
2022-03-14 23:51:49 +0100

    raceWinner: review

--- Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/raceWinner.js
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/raceWinner.js
@@ -28,22 +28,7 @@ function getDistance(graph, nodeA) {
   }
 }
 
-function buildGraph(edges) {
-  const graph = {};
 
-  for (let edge of edges) {
-    const [a, b] = edge;
-    // if (!(a in graph)) graph[a] = [];
-    // if (!(b in graph)) graph[b] = [];
-    graph[a] = graph[a] || [];
-    graph[b] = graph[b] || [];
-
-    graph[a].push(b);
-    graph[b].push(a);
-  }
-
-  return graph;
-}
 
 const roads = [
   ['R', 'L'],
