fd0ccf405fac7635983184743ba2cada4813c717
yuhcee <egbouchenna001@gmail.com>
2022-03-15 17:59:45 +0100

    uniquePaths: add problem description

---
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/uniquePaths.js
@@ -0,0 +1,17 @@
+/**
+ * **980. Unique Paths III**
+ * 
+ * You are given an `m x n` integer array grid where `grid[i][j]` could be:
+ * - 1 representing the starting square. There is exactly one starting square.
+ * - 2 representing the ending square. There is exactly one ending square.
+ * - 0 representing empty squares we can walk over.
+ * - -1 representing obstacles that we cannot walk over.
+ * 
+ * Return the number of *4-directional walks from the starting square to the ending square, that walk over every non-obstacle square exactly once*.
+ * 
+ * @param {number[][]} grid
+ * @return {number}
+ */
+const uniquePathsIII = (grid) => {
+    
+};
