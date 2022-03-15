778f0cee9ef8788fe3dcda86b4f0126b98eca52e
yuhcee <egbouchenna001@gmail.com>
2022-03-15 18:13:14 +0100

    initialize function

--- Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/uniquePaths.js
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/uniquePaths.js
@@ -12,7 +12,26 @@
  * @param {number[][]} grid
  * @return {number}
  */
-const uniquePathsIII = (grid) => {};
+const uniquePathsIII = (grid) => {
+    let result = 0,
+        traversable = 1,
+        startRow,
+        startCol,
+        rows = grid.length,
+        cols = grid[0].length;
+
+    for (let r = 0; r < rows; r += 1) {
+        for (let c = 0; c < cols; c += 1) {
+            let cell = grid[r][c];
+            if (cell === 0) traversable++;
+            if (cell === 1) (startRow = r), (startCol = c);
+        }
+    }
+    dfs(grid, startRow, startCol, result, traversable);
+    return result;
+};
+
+
 
 const grid = [
         [1, 0, 0, 0],
