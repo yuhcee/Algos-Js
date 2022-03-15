804b729e9988e0c9a2318ade203f2bca34beb558
yuhcee <egbouchenna001@gmail.com>
2022-03-15 18:04:01 +0100

    uniquePaths: add sample tests

--- Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/uniquePaths.js
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/uniquePaths.js
@@ -1,17 +1,30 @@
 /**
  * **980. Unique Paths III**
- * 
+ *
  * You are given an `m x n` integer array grid where `grid[i][j]` could be:
  * - 1 representing the starting square. There is exactly one starting square.
  * - 2 representing the ending square. There is exactly one ending square.
  * - 0 representing empty squares we can walk over.
  * - -1 representing obstacles that we cannot walk over.
- * 
+ *
  * Return the number of *4-directional walks from the starting square to the ending square, that walk over every non-obstacle square exactly once*.
- * 
+ *
  * @param {number[][]} grid
  * @return {number}
  */
-const uniquePathsIII = (grid) => {
-    
-};
+const uniquePathsIII = (grid) => {};
+
+const grid = [
+        [1, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 2, -1],
+    ], // Output: 2
+    grid1 = [
+        [1, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 2],
+    ], // Output: 4
+    grid2 = [
+        [0, 1],
+        [2, 0],
+    ]; //Output: 0
