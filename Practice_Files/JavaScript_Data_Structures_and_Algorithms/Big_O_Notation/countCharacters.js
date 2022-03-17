a07778a124abcc0adda19071bca4f8458d4ea3d0
yuhcee <egbouchenna001@gmail.com>
2022-03-17 17:04:24 +0100

    countCharacters: add problem statement.

---
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/countCharacters.js
@@ -0,0 +1,13 @@
+/**
+ * **1160. Find Words That Can Be Formed by Characters**
+ *
+ * You are given an array of strings `words` and a string `chars`.
+ * A string is **good** if it can be formed by characters from chars (each character can only be used once)
+ *
+ * *Return the sum of lengths of all good strings in words.*
+ *
+ * @param {string[]} words
+ * @param {string} chars
+ * @return {number}
+ */
+const countCharacters = (words, chars) => {};
