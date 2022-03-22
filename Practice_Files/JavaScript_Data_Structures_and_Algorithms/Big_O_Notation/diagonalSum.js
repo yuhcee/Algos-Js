b53b13616f107c5f5f1685a877ba35b0ab46546a
yuhcee <egbouchenna001@gmail.com>
2022-03-22 16:32:22 +0100

    diagonalSum: implement function.

--- Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/diagonalSum.js
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/diagonalSum.js
@@ -1,14 +1,27 @@
 /**
  *  **1572. Matrix Diagonal Sum**
- * 
+ *
  * Given a square matrix mat, return the sum of the matrix diagonals.
- * 
+ *
  * Only include the sum of all the elements on the primary diagonal and all the elements on the secondary
  * diagonal that are not part of the primary diagonal.
- * 
+ *
  * @param {number[][]} mat
  * @return {number}
  */
- const diagonalSum = (mat) => {
+const diagonalSum = (mat) => {
+    let sum = 0;
+    size = mat.length;
 
- }
+    for (let i = 0; i < size; i++) {
+        sum += mat[i][i];
+        sum += mat[i][mat.length - 1 - i];
+    }
+
+    if (size % 2 === 0) return sum;
+    else {
+        let newSize = Math.floor(size / 2);
+        return sum - mat[newSize][newSize];
+    }
+};
+
