3ed71421203af85e9d40d1af1ef045a06f98d6c1
yuhcee <egbouchenna001@gmail.com>
2022-03-28 00:14:02 +0100

    getBiggestThree: add tests

--- Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/getBiggestThree.js
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/getBiggestThree.js
@@ -62,3 +62,21 @@ var getBiggestThree = function (grid) {
 
     return best.sort((a, b) => b - a);
 };
+const grid = [
+    [3, 4, 5, 1, 3],
+    [3, 3, 4, 2, 3],
+    [20, 30, 200, 40, 10],
+    [1, 5, 5, 4, 1],
+    [4, 3, 2, 2, 5],
+]; // Output: [228,216,211]
+
+const grid1 = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+]; // Output: [20,9,8]
+const grid2 = [[7, 7, 7]]; // Output: [7]
+
+console.log(getBiggestThree(grid));
+console.log(getBiggestThree(grid1));
+console.log(getBiggestThree(grid2));
