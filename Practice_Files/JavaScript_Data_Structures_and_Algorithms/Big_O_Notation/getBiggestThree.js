322356b5045976f6d807bcf3a3170596a2d39a3f
yuhcee <egbouchenna001@gmail.com>
2022-03-27 23:36:44 +0100

    getBiggestThree: add problem description.

---
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/getBiggestThree.js
@@ -0,0 +1,22 @@
+/**
+ * **1878. Get Biggest Three Rhombus Sums in a Grid**
+ * 
+ * You are given an `m x n` integer matrix `grid​​​`.
+ * 
+ * A *rhombus sum* is the sum of the elements that form the *border* of a regular rhombus shape in
+ * `grid​​​`. The rhombus must have the shape of a square rotated 45 degrees with each of the corners
+ * centered in a grid cell. Below is an image of four valid rhombus shapes with the corresponding colored
+ * cells that should be included in each *rhombus sum*:
+ * 
+ * Note that the rhombus can have an area of 0, which is depicted by the purple rhombus in the bottom
+ * right corner.
+ * 
+ * Return the biggest three distinct rhombus sums in the grid in descending order. If there are less than
+ * three distinct values, return all of them.
+ * 
+ * @param {number[][]} grid
+ * @return {number[]}
+ */
+ var getBiggestThree = function(grid) {
+    
+};
