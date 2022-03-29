485afb11179e086e0929cf49ebeaf2fc46d0dcae
yuhcee <egbouchenna001@gmail.com>
2022-03-29 01:35:31 +0100

    getBigThree: Revision.

--- Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/getBiggestThree.js
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/getBiggestThree.js
@@ -11,56 +11,13 @@
  * Note that the rhombus can have an area of 0, which is depicted by the purple rhombus in the bottom
  * right corner.
  *
- * Return the biggest three distinct rhombus sums in the grid in descending order. If there are less than
- * three distinct values, return all of them.
+ * Return *the biggest three distinct rhombus sums in the `grid` in descending order. If there are less than three distinct values, return all of them*.
  *
  * @param {number[][]} grid
  * @return {number[]}
  */
 var getBiggestThree = function (grid) {
-    let small = Math.min(grid.length, grid[0].length),
-        best = [];
-
-    for (let row of grid) {
-        for (let el of row) {
-            if (!best.includes(el)) {
-                best.push(el);
-                if (best.length > 3) {
-                    best.sort((a, b) => b - a);
-                    best.pop();
-                }
-            }
-        }
-    }
-
-    for (let side = 1; side <= small / 2; side++) {
-        for (let row = 0; row < grid.length - side * 2; row++) {
-            for (let col = side; col < grid[0].length - side; col++) {
-                let area = calcAreaAt(row, col, side);
-
-                if (!best.includes(area)) {
-                    best.push(area);
-                    if (best.length > 3) {
-                        best.sort((a, b) => b - a);
-                        best.pop();
-                    }
-                }
-            }
-        }
-    }
-
-    function calcAreaAt(i, j, sideLength) {
-        let res = 0;
-
-        for (let g = 0; g < sideLength; g++) res += grid[i++][j++];
-        for (let g = 0; g < sideLength; g++) res += grid[i++][j--];
-        for (let g = 0; g < sideLength; g++) res += grid[i--][j--];
-        for (let g = 0; g < sideLength; g++) res += grid[i--][j++];
-
-        return res;
-    }
-
-    return best.sort((a, b) => b - a);
+    
 };
 const grid = [
     [3, 4, 5, 1, 3],
