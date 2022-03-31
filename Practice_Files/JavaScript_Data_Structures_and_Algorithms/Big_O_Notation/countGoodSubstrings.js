e3540a332a8b94cb6ca84e09af2b759867eb22c5
yuhcee <egbouchenna001@gmail.com>
2022-03-31 23:45:15 +0100

    countGoodSubStrings: add problem description

---
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/countGoodSubstrings.js
@@ -0,0 +1,19 @@
+/**
+ *  **1876. Substrings of Size Three with Distinct Characters**
+ * 
+ * A string is **good** if there are no repeated characters.
+ * 
+ * Given a string `s​​​​​`, return *the number of **good substrings** of length **three** in `s​​​​​​`*.
+ * 
+ * Note that if there are multiple occurrences of the same substring, every occurrence should be counted.
+ * 
+ * A **substring** is a contiguous sequence of characters in a string.
+
+
+ * 
+ * @param {string} s
+ * @return {number}
+ */
+const countGoodSubstrings = (s) => {
+    
+};
