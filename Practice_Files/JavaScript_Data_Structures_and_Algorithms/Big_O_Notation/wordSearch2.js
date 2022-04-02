060d1b68d88b8f7120495ce9ba4c9fa7d4a49135
yuhcee <egbouchenna001@gmail.com>
2022-04-02 23:48:43 +0100

    wordSearch2: revis - iterate words

--- Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/wordSearch2.js
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/wordSearch2.js
@@ -30,13 +30,7 @@ Trie.prototype.startsWith = function (prefix) {
  */
 
 const findWords = (board, words) => {
-    const trie = new Trie();
-    const result = [];
-    const visited = new Set();
-
-    for (let word of words) {
-        trie.insert(word);
-    }
+    
 
     for (let r = 0; r < board.length; r++) {
         for (let c = 0; c < board[0].length; c++) {
