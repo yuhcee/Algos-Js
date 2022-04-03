81337b51fbb625e280f72ad7419bd9a5a654f141
yuhcee <egbouchenna001@gmail.com>
2022-04-03 23:39:02 +0100

    validPalindrome: add problem description

--- Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/validPalindrome.js
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/validPalindrome.js
@@ -1,4 +1,10 @@
 /**
+ * 
+ * *680. Valid Palindrome II*
+ * 
+ * Given a string s, return true if the s can be palindrome after deleting at most one
+ * character from it. 
+ * 
  * @param {string} s
  * @return {boolean}
  */
