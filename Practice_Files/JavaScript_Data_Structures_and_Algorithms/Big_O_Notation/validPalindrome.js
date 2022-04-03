f7669154889f9ee4a6ad0b70234e428ad8653069
yuhcee <egbouchenna001@gmail.com>
2022-04-03 23:56:18 +0100

    validPalindrome: checkPalindrome function

--- Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/validPalindrome.js
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/validPalindrome.js
@@ -1,13 +1,20 @@
 /**
- * 
+ *
  * *680. Valid Palindrome II*
- * 
+ *
  * Given a string s, return true if the s can be palindrome after deleting at most one
- * character from it. 
- * 
+ * character from it.
+ *
  * @param {string} s
  * @return {boolean}
  */
 const validPalindrome = (s) => {
-    if(checkPalindrome(s)) return true;
+    if (checkPalindrome(s)) return true;
+};
+
+const checkPalindrome = (s) => {
+    for (let i = 0; i < s.length / 2; i++) {
+        if (s.charAt(i) != s.charAt(s.length - 1 - i)) return false;
+    }
+    return true;
 };
