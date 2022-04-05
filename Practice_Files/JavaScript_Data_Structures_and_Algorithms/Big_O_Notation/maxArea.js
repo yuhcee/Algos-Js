91612c221fb0ca33b144cee4d861db13367bb121
yuhcee <egbouchenna001@gmail.com>
2022-04-05 23:56:46 +0100

    maxArea: init functio vars

--- Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/maxArea.js
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/maxArea.js
@@ -1,18 +1,20 @@
 /**
  * *Container With Most Water*
- * 
+ *
  * You are given an integer array height of length n. There are n vertical lines drawn such that the
  * two endpoints of the ith line are (i, 0) and (i, height[i]).
- * 
+ *
  * Find two lines that together with the x-axis form a container, such that the container contains the
  * most water.
- * 
+ *
  * Return the maximum amount of water a container can store.
- * 
+ *
  * Notice that you may not slant the container.
  * @param {number[]} height
  * @return {number}
  */
- var maxArea = function(height) {
-
- }
+var maxArea = function (height) {
+    let left = 0,
+        right = height.length - 1,
+        maxArea = 0;
+};
