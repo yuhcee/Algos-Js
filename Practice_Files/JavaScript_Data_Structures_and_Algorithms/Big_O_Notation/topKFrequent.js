3d89c2fb318b9356d0970b3c404e5d9a76121c05
yuhcee <egbouchenna001@gmail.com>
2022-04-09 23:43:06 +0100

    topKFrequent: init map to count frequency

--- Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/topKFrequent.js
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/topKFrequent.js
@@ -1,12 +1,15 @@
 /**
  * **347. Top K Frequent Elements**
- * 
+ *
  * Given an integer array `nums` and an integer `k`, return the `k` most frequent elements. You may return
  * the answer in *any order*.
- * 
+ *
  * @param {number[]} nums
  * @param {number} k
  * @return {number[]}
  */
 
- const topKFrequent = (nums, k) => {}
+const topKFrequent = (nums, k) => {
+    //  ! init map to count frequency
+    const map = new Map();
+};
