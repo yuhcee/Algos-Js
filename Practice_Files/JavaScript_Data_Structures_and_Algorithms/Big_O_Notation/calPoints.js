c65b0a6da51754563dbee8eb24b2707951a29608
yuhcee <egbouchenna001@gmail.com>
2022-04-11 00:06:58 +0100

    calPoints: add problem description

---
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/calPoints.js
@@ -0,0 +1,23 @@
+/** 
+ * ** 682. Baseball Game**
+ * 
+ * You are keeping score for a baseball game with strange rules. The game consists of several rounds, where
+ * the scores of past rounds may affect future rounds' scores.
+ * 
+ * At the beginning of the game, you start with an empty record. You are given a list of strings `ops`, where
+ * `ops[i]` is the `ith` operation you must apply to the record and is one of the following:
+ * 
+ * An integer `x` - Record a new score of `x`.
+ * 1. `"+"` - Record a new score that is the sum of the previous two scores. It is guaranteed there will always be two previous scores.
+ * 2. `"D"` - Record a new score that is double the previous score. It is guaranteed there will always be a previous score.
+ * 3. `"C"` - Invalidate the previous score, removing it from the record. It is guaranteed there will always
+ * be a previous score.
+ * 
+ * *Return *the sum of all the scores on the record*.
+
+ * @param {string[]} ops
+ * @return {number}
+ */
+const calPoints = (ops) => {
+
+};
