1758afdee79242109735eae9498352185dc27555
yuhcee <egbouchenna001@gmail.com>
2022-04-12 12:27:22 +0100

    validAnagram: replace with empty space and reassign to str2

--- Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/validAnagram.js
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/validAnagram.js
@@ -13,10 +13,11 @@ function validAnagram() {
     let [str1, str2] = arguments;
 
     // iterate through str1
-    for (let char of str1){
+    for (let char of str1) {
         // check if char exists in str2
-        if(str2.includes(char)){
-            
+        if (str2.includes(char)) {
+            // replace with empty space and reassign to str2
+            str2 = str2.replace(char, '');
         }
     }
 }
