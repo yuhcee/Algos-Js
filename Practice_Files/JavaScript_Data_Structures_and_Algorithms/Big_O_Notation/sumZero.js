ef6c557a5be39e1ed9aca0955f1dcf525ea39b1e
yuhcee <egbouchenna001@gmail.com>
2022-04-13 09:52:15 +0100

    sumZero: add problem description

---
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/sumZero.js
@@ -0,0 +1,11 @@
+/**
+ * **Sum Zero**
+ * 
+ * Write a function which accepts a sorted array of integers. 
+ * The function should find the **first** pair where the sum is **zero**.
+ * 
+ * Return *an array that includes those values that sums to 0 or undefined if not found*.
+ * 
+ * @param {*} arr 
+ */
+const sumZero = (arr) => {};
