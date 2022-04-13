007219e7ba198100745cf8b57ce39fca825644b6
yuhcee <egbouchenna001@gmail.com>
2022-04-13 23:34:46 +0100

    recursiveBinarySearch: if value at midpoint is equal to target, return true

--- Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/recursiveBinarySearch.js
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/recursiveBinarySearch.js
@@ -13,4 +13,7 @@ const recursiveBinarySearch = (list, target) => {
     if (list.length === 0) return false;
     // get the middle point of the array
     let midPoint = Math.floor(list.length / 2);
+    // if value at midpoint is equal to target, return true
+    if (list[midPoint] === target) return true;
+
 };
