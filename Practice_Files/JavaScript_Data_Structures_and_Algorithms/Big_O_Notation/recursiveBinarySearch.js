5453b4480d7445c5f82b506e3aca3e39236fc93e
yuhcee <egbouchenna001@gmail.com>
2022-04-13 17:35:46 +0100

    recursiveBinarySearch: add problem description

---
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/recursiveBinarySearch.js
@@ -0,0 +1,11 @@
+/**
+ * **Recursive Binary Search**
+ *
+ * Write a function that takes a **sorted** `array` and a `target`, and verify if `target` exist in the
+ * `array`.
+ *
+ * Return *true if target exists, otherwise false*.
+ * @param {*} list
+ * @param {*} target
+ */
+const recursiveBinarySearch = (list, target) => {};
