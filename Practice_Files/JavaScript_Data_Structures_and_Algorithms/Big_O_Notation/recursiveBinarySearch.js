194d45e27ad8d6f54e5f2d1a10be4003b93b4b67
yuhcee <egbouchenna001@gmail.com>
2022-04-13 17:37:49 +0100

    recursiveBinarySearch: return false if list is empty

--- Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/recursiveBinarySearch.js
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/recursiveBinarySearch.js
@@ -1,11 +1,16 @@
 /**
  * **Recursive Binary Search**
- *
+ * 
  * Write a function that takes a **sorted** `array` and a `target`, and verify if `target` exist in the
  * `array`.
- *
+ * 
  * Return *true if target exists, otherwise false*.
  * @param {*} list
  * @param {*} target
  */
-const recursiveBinarySearch = (list, target) => {};
+const recursiveBinarySearch = (list, target) => {
+    // return false if arr is empty
+    if (list.length === 0) return false;
+
+    
+}
