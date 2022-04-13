8a8bb84ec7a56423495a59bf15789006e698b071
yuhcee <egbouchenna001@gmail.com>
2022-04-13 10:15:14 +0100

    sumZero: add tests

--- Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/sumZero.js
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/sumZero.js
@@ -27,3 +27,8 @@ const sumZero = (arr) => {
         }
     }
 };
+
+console.log(sumZero([-3, -2, -1, 0, 1, 2, 3]));
+console.log(sumZero([-2, 0, 1, 2, 3]));
+console.log(sumZero([-2, 0, 1, 3]));
+console.log(sumZero([1, 2, 3]));
