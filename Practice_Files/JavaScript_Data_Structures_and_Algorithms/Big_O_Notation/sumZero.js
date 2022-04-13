9ba2ddd47cd47f2226902d7ba6bc604fb67387e6
yuhcee <egbouchenna001@gmail.com>
2022-04-13 09:53:40 +0100

    sumZero: init multiple pointers

--- Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/sumZero.js
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/sumZero.js
@@ -1,11 +1,15 @@
 /**
  * **Sum Zero**
- * 
- * Write a function which accepts a sorted array of integers. 
+ *
+ * Write a function which accepts a sorted array of integers.
  * The function should find the **first** pair where the sum is **zero**.
- * 
+ *
  * Return *an array that includes those values that sums to 0 or undefined if not found*.
- * 
- * @param {*} arr 
+ *
+ * @param {*} arr
  */
-const sumZero = (arr) => {};
+const sumZero = (arr) => {
+    // init multiple pointers
+    let left = 0,
+        right = arr.length - 1;
+};
