24edb90f314fe153f079a7503b50d4cee12868c2
yuhcee <egbouchenna001@gmail.com>
2022-04-14 08:09:57 +0100

    maxSubArraySum: check if arr is not empty

--- Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/maxSubArraySum.js
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/maxSubArraySum.js
@@ -1,12 +1,17 @@
 /**
  * **Max Sub Array Sum**
- * 
+ *
  * Write a function that accepts an array of `integers`, and a number called `n`.
  * The function should calculate the maximum sum of `n` consecutive elements in the array.
- * 
+ *
  * Return *the maximum sum of `n`consecutive elements in the array, otherwise return null*.
- * 
- * @param {*} arr 
- * @param {*} num 
+ *
+ * @param {*} arr
+ * @param {*} num
  */
-const maxSubArraySum = (arr, n) => {};
+const maxSubArraySum = (arr, n) => {
+    // check if arr is not empty
+    if (arr.length === 0) return null;
+
+    
+};
