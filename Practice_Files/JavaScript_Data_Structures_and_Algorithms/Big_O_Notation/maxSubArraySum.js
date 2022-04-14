b39f71f04dc86e1d4235710f1d4903e349bda628
yuhcee <egbouchenna001@gmail.com>
2022-04-14 08:08:29 +0100

    maxSubArraySum: add problem description

---
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/maxSubArraySum.js
@@ -0,0 +1,12 @@
+/**
+ * **Max Sub Array Sum**
+ * 
+ * Write a function that accepts an array of `integers`, and a number called `n`.
+ * The function should calculate the maximum sum of `n` consecutive elements in the array.
+ * 
+ * Return *the maximum sum of `n`consecutive elements in the array, otherwise return null*.
+ * 
+ * @param {*} arr 
+ * @param {*} num 
+ */
+const maxSubArraySum = (arr, n) => {};
