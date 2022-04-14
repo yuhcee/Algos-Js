29327b3c43646288a407281b1fb6907803945966
yuhcee <egbouchenna001@gmail.com>
2022-04-14 07:48:00 +0100

    countUniqueValues : implement multiple pointers

--- Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/countUniqueValues.js
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/countUniqueValues.js
@@ -2,9 +2,23 @@
  * **Count Unique Values**
  *
  * Implement a function which accepts a sorted array, and counts the uniques values in the array.
- * There can be negetive numbers in the array but it will always be sorted.
+ * There can be negative numbers in the array but it will always be sorted.
  *
  * Return *the number of unique values in the array*.
  * @param {*} arr
  */
-const countUniqueValues = (arr) => {};
+const countUniqueValues = (arr) => {
+    let i = 0;
+    if (arr.length === 0) return i;
+
+    // implement multiple pointers
+    for (let j = 1; j < arr.length; j++) {
+        if (arr[i] !== arr[j]) {
+            i++;
+            arr[i] = arr[j];
+        }
+    }
+
+    return i + 1;
+};
+
