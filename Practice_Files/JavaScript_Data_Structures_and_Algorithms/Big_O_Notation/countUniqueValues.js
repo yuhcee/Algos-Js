f73f4a856b71351dbff9f8cae377ae9c661a5007
yuhcee <egbouchenna001@gmail.com>
2022-04-14 07:09:38 +0100

    countUniqueValues : add problem description

---
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/countUniqueValues.js
@@ -0,0 +1,10 @@
+/**
+ * **Count Unique Values**
+ *
+ * Implement a function which accepts a sorted array, and counts the uniques values in the array.
+ * There can be negetive numbers in the array but it will always be sorted.
+ *
+ * Return *the number of unique values in the array*.
+ * @param {*} arr
+ */
+const countUniqueValues = (arr) => {};
