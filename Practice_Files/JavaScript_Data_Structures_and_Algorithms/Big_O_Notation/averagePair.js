65ada4cd06c67915ee7a3b5cba2a313cc925d963
yuhcee <egbouchenna001@gmail.com>
2022-04-15 16:17:41 +0100

    averagePair: add tests

--- Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/averagePair.js
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/averagePair.js
@@ -34,3 +34,8 @@ function averagePair(arr, target) {
     // return false, if not matches found
     return false;
 }
+
+console.log(averagePair([1, 2, 3], 2.5));
+console.log(averagePair([1, 3, 3, 5, 6, 7, 10, 12, 19], 8));
+console.log(averagePair([-1, 0, 3, 4, 5, 6], 4.1));
+console.log(averagePair([], 4));
