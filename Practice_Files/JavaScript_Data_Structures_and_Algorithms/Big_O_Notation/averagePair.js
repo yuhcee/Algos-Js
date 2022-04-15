0f3ef5bc1f0d03e77981e2aace5713d91474e77c
yuhcee <egbouchenna001@gmail.com>
2022-04-15 16:06:42 +0100

    averagePair: add problem description

---
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/averagePair.js
@@ -0,0 +1,16 @@
+/**
+ * Given a sorted array of sorted integers and a target average, determine if there's a pair of values in
+ * the array where the avearge of the pair equals the target average. 
+ * There may be more than one pair that matches the target average.
+ *
+ * Return *true if a pair average matches the target average, otherwise false*
+ * 
+ * @param {*} arr
+ * @param {*} target
+ * @returns
+ */
+function averagePair(arr, target) {
+    
+}
+
+
