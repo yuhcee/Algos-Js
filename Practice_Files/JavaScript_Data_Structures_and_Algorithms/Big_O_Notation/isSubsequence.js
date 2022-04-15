f502a2cf0e3ec3a6f29d5917133a9487d7ef8899
yuhcee <egbouchenna001@gmail.com>
2022-04-16 00:15:46 +0100

    isSubsequence: init two pointers

--- Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/isSubsequence.js
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/isSubsequence.js
@@ -1,17 +1,19 @@
 /**
  * **IsSubSequence**
- * 
+ *
  * Write a function which takes two strings and checks whether the characters of the first string form a
  * subsequence of the characters in the second string.
- * 
+ *
  * In other words, the function should check whether the characters in the first string appear somewhere in
  * the second string, **without their order changing**.
- * 
+ *
  * Return *true if characters of the first string exist in their right ordering in the second string*.
  */
 function isSubsequence() {
     // destructure variables from params arguments
     const [first, second] = arguments;
 
-
+    // init two pointers
+    let i = 0,
+        j = 0;
 }
