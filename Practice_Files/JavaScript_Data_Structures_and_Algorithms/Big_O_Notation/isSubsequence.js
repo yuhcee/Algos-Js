e8277b46d4b393a75d500ce3a76c058dae538035
yuhcee <egbouchenna001@gmail.com>
2022-04-16 00:12:55 +0100

    isSubsequence: add problem description

---
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/isSubsequence.js
@@ -0,0 +1,12 @@
+/**
+ * **IsSubSequence**
+ * 
+ * Write a function which takes two strings and checks whether the characters of the first string form a
+ * subsequence of the characters in the second string.
+ * 
+ * In other words, the function should check whether the characters in the first string appear somewhere in
+ * the second string, **without their order changing**.
+ * 
+ * Return *true if characters of the first string exist in their right ordering in the second string*.
+ */
+function isSubsequence() {}
