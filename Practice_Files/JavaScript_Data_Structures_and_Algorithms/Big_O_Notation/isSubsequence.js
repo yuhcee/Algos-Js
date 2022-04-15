c6e2178ed15fa672597f16f3094514c4af4324f2
yuhcee <egbouchenna001@gmail.com>
2022-04-16 00:24:21 +0100

    isSubsequence: add tests

--- Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/isSubsequence.js
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/isSubsequence.js
@@ -27,3 +27,7 @@ function isSubsequence() {
     // return false
     return false;
 }
+console.log(isSubsequence('hello', 'hello world'));
+console.log(isSubsequence('sing', 'sting'));
+console.log(isSubsequence('abc', 'abracadabra'));
+console.log(isSubsequence('abc', 'acb'));
