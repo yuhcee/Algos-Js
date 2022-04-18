6dd538eaedff6a0dd229b8bb72b226acdc8479a7
yuhcee <egbouchenna001@gmail.com>
2022-04-18 17:53:25 +0100

    sameFrequency: destructure parameters from arguments

--- Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/sameFrequency.js
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/sameFrequency.js
@@ -4,5 +4,6 @@
  * Write a function called **sameFrequency**. Given two positive integers, find out if the two numbers have the same frequency of digits.
  */
 function sameFrequency() {
-    
+    // destructure parameters from arguments
+    const [first, second] = arguments;
 }
