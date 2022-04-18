501548ead772b23c1fb0d6fb00ec850b3e990168
yuhcee <egbouchenna001@gmail.com>
2022-04-18 17:51:08 +0100

    sameFrequency: add problem description

---
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/sameFrequency.js
@@ -0,0 +1,8 @@
+/**
+ * **sameFrequency - Frequency Counter**
+ * 
+ * Write a function called **sameFrequency**. Given two positive integers, find out if the two numbers have the same frequency of digits.
+ */
+function sameFrequency() {
+    
+}
