61715e238d85e8d3f0f969996085550ccc7c0988
yuhcee <egbouchenna001@gmail.com>
2022-04-18 23:26:16 +0100

    sameFrequency: init a counts object

--- Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/sameFrequency.js
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/sameFrequency.js
@@ -1,9 +1,12 @@
 /**
  * **sameFrequency - Frequency Counter**
- * 
+ *
  * Write a function called **sameFrequency**. Given two positive integers, find out if the two numbers have the same frequency of digits.
  */
 function sameFrequency() {
     // destructure parameters from arguments
     const [first, second] = arguments;
+
+    // init a counts object
+    const counts = {};
 }
