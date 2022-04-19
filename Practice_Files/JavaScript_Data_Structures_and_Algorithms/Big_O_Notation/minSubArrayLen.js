47779bc8194e63e2c4046a9c8b4f71c4a9f40758
yuhcee <egbouchenna001@gmail.com>
2022-04-19 15:10:34 +0100

    minSubArrayLen : init local vars

--- Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/minSubArrayLen.js
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/minSubArrayLen.js
@@ -1,13 +1,18 @@
 /**
  * Write a function called **minSubArrayLen** which accepts two parameters - an array of positive integers
  *`nums` and a positive integer `sum`.
- * 
+ *
  * The function should return *the minimum length of a **contigous** subarray of which the sum is greater
  * than or equal to the integer* passed to the function. If there isn't one, return 0 instead.
- * 
- * @param {*} nums 
- * @param {*} sum 
+ *
+ * @param {*} nums
+ * @param {*} sum
  */
 const minSubArrayLen = (nums, sum) => {
-
-}
+    // init variables
+    let start = 0,
+        end = 0,
+        total = 0,
+        minLen = Infinity;
+    const { min } = Math;
+};
