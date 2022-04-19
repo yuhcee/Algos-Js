75c83d71eac5affa2ccff1621290e93d0b00cce5
yuhcee <egbouchenna001@gmail.com>
2022-04-19 15:07:26 +0100

    minSubArrayLen : add problem description

---
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/minSubArrayLen.js
@@ -0,0 +1,13 @@
+/**
+ * Write a function called **minSubArrayLen** which accepts two parameters - an array of positive integers
+ *`nums` and a positive integer `sum`.
+ * 
+ * The function should return *the minimum length of a **contigous** subarray of which the sum is greater
+ * than or equal to the integer* passed to the function. If there isn't one, return 0 instead.
+ * 
+ * @param {*} nums 
+ * @param {*} sum 
+ */
+const minSubArrayLen = (nums, sum) => {
+
+}
