7cf8d567309eb4a12ae98a306856bc6e52436936
yuhcee <egbouchenna001@gmail.com>
2022-04-20 14:24:44 +0100

    undirectedPath: build graph on the edges

--- Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/undirectedPath.js
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/undirectedPath.js
@@ -1,14 +1,34 @@
 /**
  * **undirected path**
- * 
- * Write a function, *undirectedPath*, that takes in an array of edges for an undirected graph and two 
- * (nodeA, nodeB). 
- * 
+ *
+ * Write a function, *undirectedPath*, that takes in an array of edges for an undirected graph and two
+ * (nodeA, nodeB).
+ *
  * The function should return a *boolean indicating whether or not there exists a path
  * between `nodeA` and `nodeB`*.
- * 
- * @param {*} edges 
- * @param {*} nodeA 
- * @param {*} nodeB 
+ *
+ * @param {*} edges
+ * @param {*} nodeA
+ * @param {*} nodeB
  */
-const undirectedPath = (edges, nodeA, nodeB) => {}
+
+// Iterative Approach
+const undirectedPath = (edges, nodeA, nodeB) => {
+    const graph = buildGraph(edges);
+    
+};
+
+// build graph on the edges
+const buildGraph = (edges) => {
+    const graph = {};
+
+    for (const [a, b] of edges) {
+        if (!(a in graph)) graph[a] = [];
+        if (!(b in graph)) graph[b] = [];
+
+        graph[a].push(b);
+        graph[b].push(a);
+    }
+
+    return graph;
+};
