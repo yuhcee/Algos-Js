21f8a0f5dfdc0b2c6b9e9dbbd8cbea41a0d2fe9a
yuhcee <egbouchenna001@gmail.com>
2022-04-20 18:38:08 +0100

    findLongestSubstring: add problem description

---
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/findLongestSubstring.js
@@ -0,0 +1,9 @@
+/**
+ * **Sliding Window - Find Longest Substring**
+ * 
+ * Write a function called **findLongestSubString**, which accepts a string and returns the length of the
+ * longest substringwith all distinct characters.
+ * 
+ * @param {*} str 
+ */
+function findLongestSubstring(str){}
