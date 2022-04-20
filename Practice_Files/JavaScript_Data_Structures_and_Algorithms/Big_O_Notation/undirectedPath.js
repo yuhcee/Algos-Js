ac2f10dac3c640e2b7f31d1b7e9eb07a91ab997c
yuhcee <egbouchenna001@gmail.com>
2022-04-20 14:19:50 +0100

    undirectedPath: add problem description

---
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/undirectedPath.js
@@ -0,0 +1,14 @@
+/**
+ * **undirected path**
+ * 
+ * Write a function, *undirectedPath*, that takes in an array of edges for an undirected graph and two 
+ * (nodeA, nodeB). 
+ * 
+ * The function should return a *boolean indicating whether or not there exists a path
+ * between `nodeA` and `nodeB`*.
+ * 
+ * @param {*} edges 
+ * @param {*} nodeA 
+ * @param {*} nodeB 
+ */
+const undirectedPath = (edges, nodeA, nodeB) => {}
