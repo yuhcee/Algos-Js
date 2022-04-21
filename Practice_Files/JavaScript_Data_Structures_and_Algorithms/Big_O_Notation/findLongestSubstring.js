f0d32b1f99fb5f233e8c050778913bdc86c06bb8
yuhcee <egbouchenna001@gmail.com>
2022-04-21 23:32:23 +0100

    findLongestSubstring: index - store the index of the next char so as to not double count

--- Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/findLongestSubstring.js
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/findLongestSubstring.js
@@ -21,5 +21,8 @@ function findLongestSubstring(str) {
         }
         //index - beginning of substring + 1 (to include current in count)
         longest = max(longest, i - start + 1);
+
+        // store the index of the next char so as to not double count
+        seen[char] = i + 1;
     }
 }
