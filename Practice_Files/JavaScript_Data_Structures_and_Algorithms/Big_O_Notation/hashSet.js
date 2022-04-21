506ab6a67b8dad2d16e65fbe0c0c8df896099b99
yuhcee <egbouchenna001@gmail.com>
2022-04-21 23:48:25 +0100

    hashSet: add problem description

---
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/hashSet.js
@@ -0,0 +1,15 @@
+/**
+ * **705. Design HashSet**
+ * 
+ * Design a HashSet without using any built-in hash table libraries.
+ * 
+ * Implement MyHashSet class:
+ * 
+ * - void add(key) Inserts the value key into the HashSet.
+ * - bool contains(key) Returns whether the value key exists in the HashSet or not.
+ * - void remove(key) Removes the value key in the HashSet. If key does not exist in the HashSet, do nothing.
+ */
+
+const MyHashSet = function () {
+    this.hashSet = new Set();
+};
