d25d69ebb60f1d63e3063ac7f942039dfe2a0adb
yuhcee <egbouchenna001@gmail.com>
2022-04-23 00:19:09 +0100

    hashMap: add tests

--- Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/hashMap.js
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/hashMap.js
@@ -40,3 +40,13 @@
  MyHashMap.prototype.remove = function(key) {
     delete this.map[key] ;
 };
+
+const myHashMap = new MyHashMap();
+myHashMap.put(1, 1); // The map is now [[1,1]]
+myHashMap.put(2, 2); // The map is now [[1,1], [2,2]]
+console.log(myHashMap.get(1));    // return 1, The map is now [[1,1], [2,2]]
+myHashMap.get(3);    // return -1 (i.e., not found), The map is now [[1,1], [2,2]]
+myHashMap.put(2, 1); // The map is now [[1,1], [2,1]] (i.e., update the existing value)
+myHashMap.get(2);    // return 1, The map is now [[1,1], [2,1]]
+myHashMap.remove(2); // remove the mapping for 2, The map is now [[1,1]]
+myHashMap.get(2);    // return -1 (i.e., not found), The map is now [[1,1]]
