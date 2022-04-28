78a5d80d845e1ce79c24eebe03ce65eeb7392a9a
yuhcee <egbouchenna001@gmail.com>
2022-04-28 23:08:47 +0100

    minimumEffortPath: init local vars

--- Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/minimumEffortPath.js
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/minimumEffortPath.js
@@ -1,13 +1,13 @@
 /**
  * **1631. Path With Minimum Effort**
- * 
+ *
  * You are a hiker preparing for an upcoming hike. You are given `heights`, a 2D array of size `rows x
  * columns`, where `heights[row][col]` represents the height of cell `(row, col)`. You are situated in the
  * top-left cell, `(0, 0)`, and you hope to travel to the bottom-right cell, `(rows-1, columns-1)` (i.e., **0-indexed**). You can move **up**, **down**, **left**, or **right**, and you wish to find a route that
  * requires the minimum **effort**.
- * 
+ *
  * A route's **effort** is the **maximum absolute difference** in heights between two consecutive cells of the route.
- * 
+ *
  * Return *the minimum **effort** required to travel from the top-left cell to the bottom-right cell*.
  *
  * @param {number[][]} heights
@@ -15,5 +15,16 @@
  */
 
 var minimumEffortPath = function (h) {
-    
+    const DIR = [
+        [1, 0],
+        [0, 1],
+        [-1, 0],
+        [0, -1],
+    ];
+
+    const n = h.length,
+        m = h[0].length;
+    let v = [],
+        min = 0,
+        max = 0;
 };
