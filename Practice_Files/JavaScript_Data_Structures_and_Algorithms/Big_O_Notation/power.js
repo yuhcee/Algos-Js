2d576a56a57cdef4e740202749ee84dfdd8f0b26
yuhcee <egbouchenna001@gmail.com>
2022-04-29 08:08:54 +0100

    power: add problem description

---
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/power.js
@@ -0,0 +1,11 @@
+/**
+ * **Power**
+ * 
+ * Write a function called Power, that accepts a base and an exponent. The function should return the power of the base to the exponent. This function should mimic the functionality of Math.pow() - do not worry about
+ * negetive bases and exponents.
+ * 
+ */
+
+const power =(base, exponent)=> {
+
+}
