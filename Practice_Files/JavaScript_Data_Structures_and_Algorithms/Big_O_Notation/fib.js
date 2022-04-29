1bbe94ffd9607945e80d7c2ad4923c85cc9fdd15
yuhcee <egbouchenna001@gmail.com>
2022-04-29 09:45:31 +0100

    fib: add recursive and dynamic programming

--- Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/fib.js
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/fib.js
@@ -7,4 +7,17 @@
  * sum of the previous two numbers.
  */
 
-const fib = (num) => {};
+const fib = (num, memo = {}) => {
+    if (num <= 2) return 1;
+    const key = num;
+
+    if (key in memo) return memo[key];
+
+    memo[key] = fib(num - 1, memo) + fib(num - 2, memo);
+
+    return memo[key];
+};
+
+console.log(fib(4));
+console.log(fib(24));
+console.log(fib(1000));
