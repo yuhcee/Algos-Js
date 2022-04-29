687a70c9a50d9de1d958cab5aa21d855aa1767d0
yuhcee <egbouchenna001@gmail.com>
2022-04-29 08:29:18 +0100

    factorial: add tests

--- Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/factorial.js
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/factorial.js
@@ -9,3 +9,8 @@ const factorial = (num) => {
 
     return num * factorial(num - 1);
 };
+
+console.log(factorial(0));
+console.log(factorial(2));
+console.log(factorial(4));
+console.log(factorial(7));
