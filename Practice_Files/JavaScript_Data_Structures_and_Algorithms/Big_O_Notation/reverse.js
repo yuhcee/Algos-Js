92a62700fc1be095c2cf6be8d44ca935b72c9b38
yuhcee <egbouchenna001@gmail.com>
2022-05-01 08:07:29 +0100

    reverse: add in-built method solution

--- Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/reverse.js
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/reverse.js
@@ -1,10 +1,13 @@
 /**
  * **Reverse**
  *
- * Write a recursive function called **reverse** which accepts a string and returns a new string in 
+ * Write a recursive function called **reverse** which accepts a string and returns a new string in
  * reverse.
  */
 
-const reverse = ( str ) => {
-    
-}
+const reverse = (str) => {
+    return str.split('').reverse().join('');
+};
+
+console.log(reverse('awesome'));
+console.log(reverse('akankwe'));
