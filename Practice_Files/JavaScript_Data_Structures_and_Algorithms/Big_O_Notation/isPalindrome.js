f223a29c456213739886d4bbf06d1f23c4d21031
yuhcee <egbouchenna001@gmail.com>
2022-05-01 08:20:16 +0100

    isPalindrome: add two-pointers solution

--- Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/isPalindrome.js
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/isPalindrome.js
@@ -5,4 +5,21 @@
  * it is a `palindrome` (reads the same forward and backward). Otherwise it returns `false`.
  */
 
-const isPalindrome = (str) => {};
+const isPalindrome = (str) => {
+    let l = 0,
+        r = str.length - 1;
+
+    while (l < r) {
+        if (str[l] !== str[r]) return false;
+        l++;
+        r--;
+    }
+
+    return true;
+};
+
+console.log(isPalindrome('awesome')); // false
+console.log(isPalindrome('foobar')); // false
+console.log(isPalindrome('tacocat')); // true
+console.log(isPalindrome('amanaplanacanalpanama')); // true
+console.log(isPalindrome('amanaplanacanalpandemonium')); // false
