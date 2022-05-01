4870e7e33d439081c032da7fdce363a6da0a7076
yuhcee <egbouchenna001@gmail.com>
2022-05-01 08:25:51 +0100

    isPalindrome: add recursive solution

--- Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/isPalindrome.js
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/isPalindrome.js
@@ -23,3 +23,19 @@ console.log(isPalindrome('foobar')); // false
 console.log(isPalindrome('tacocat')); // true
 console.log(isPalindrome('amanaplanacanalpanama')); // true
 console.log(isPalindrome('amanaplanacanalpandemonium')); // false
+
+const isPalindromeRecursive = (str) => {
+    let l = 0,
+        r = str.length - 1;
+
+    if (str.length === 1) return true;
+    if (str.length === 2) return str[l] === str[r];
+
+    return str[0] === str[r] ? isPalindromeRecursive(str.slice(1, -1)) : false;
+};
+
+console.log(isPalindromeRecursive('awesome')); // false
+console.log(isPalindromeRecursive('foobar')); // false
+console.log(isPalindromeRecursive('tacocat')); // true
+console.log(isPalindromeRecursive('amanaplanacanalpanama')); // true
+console.log(isPalindromeRecursive('amanaplanacanalpandemonium')); // false
