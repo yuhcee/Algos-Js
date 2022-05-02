160224decf35149e82ab97c62e62edc70b84dd26
yuhcee <egbouchenna001@gmail.com>
2022-05-02 10:03:20 +0100

    backSpaceCompare: add problem description

---
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/backSpaceCompare.js
@@ -0,0 +1,14 @@
+/**
+ * **Back Space String Compare**
+ *
+ * Given two strings `s` and `t`, return *`true` if they are equal when both are typed into empty
+ * text editors*. `'#'` means a backspace character.
+ *
+ * Note that after backspacing an empty text, the text will continue empty.
+ *
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+const backspaceCompare = (s, t) => {
+};
