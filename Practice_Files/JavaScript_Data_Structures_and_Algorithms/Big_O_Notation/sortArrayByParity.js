576be518fdc880433d2ad21edfabb6787208b8c8
yuhcee <egbouchenna001@gmail.com>
2022-05-02 09:24:43 +0100

    sortArrayByParity: add sliding window solution

--- Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/sortArrayByParity.js
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/sortArrayByParity.js
@@ -1,12 +1,24 @@
 /**
  * **Sort Array By Parity**
- * 
- * Given an integer array `nums`, move all the even integers at the beginning of the array followed 
+ *
+ * Given an integer array `nums`, move all the even integers at the beginning of the array followed
  * by all the odd integers.
- * 
+ *
  * Return ***any array** that satisfies this condition*.
- * 
+ *
  * @param {number[]} nums
  * @return {number[]}
  */
-const sortArrayByParity = (nums) => {};
+const sortArrayByParity = (nums) => {
+    let oddIdx = 0;
+
+    for (let i = 0; i < nums.length; i++) {
+        let num = nums[i];
+        if (num % 2 === 0) {
+            [num, oddIdx] = [oddIdx, num];
+            oddIdx++;
+        }
+    }
+
+    return nums;
+};
