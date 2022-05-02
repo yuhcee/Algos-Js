73de69ab9fb364c186c05285763a8701e60bd80c
yuhcee <egbouchenna001@gmail.com>
2022-05-02 09:21:30 +0100

    sortArrayByParity: add problem description

---
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/sortArrayByParity.js
@@ -0,0 +1,12 @@
+/**
+ * **Sort Array By Parity**
+ * 
+ * Given an integer array `nums`, move all the even integers at the beginning of the array followed 
+ * by all the odd integers.
+ * 
+ * Return ***any array** that satisfies this condition*.
+ * 
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+const sortArrayByParity = (nums) => {};
