f29446e3e746b5b3cf158333b6ba1029955753e4
yuhcee <egbouchenna001@gmail.com>
2022-05-03 18:58:29 +0100

    findUnsortedSubarray: add task description

---
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/findUnsortedSubArray.js
@@ -0,0 +1,14 @@
+/**
+ * **581. Shortest Unsorted Continuous Subarray**
+ *
+ * Given an integer array `nums`, you need to find one **continuous subarray** that if you only sort this
+ * subarray in ascending order, then the whole array will be sorted in ascending order.
+ *
+ * Return *the shortest such subarray and output its length*.
+ *
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findUnsortedSubarray = function (nums) {
+    
+};
