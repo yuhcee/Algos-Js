3873fa4ebd0d4217328d1ed7b5add15ab4131f6d
yuhcee <egbouchenna001@gmail.com>
2022-05-03 09:57:42 +0100

    capitalizeWords: add task description

---
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/capitalizeWords.js
@@ -0,0 +1,6 @@
+/**
+ * Write a recursive function called **capitalizeWords**. Given an array of words, return a new array
+ * containing each word capitalized.
+ */
+
+const capitalizeWords = (arr) => {};
