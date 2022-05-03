3a51671ff2afbb34d8253817118da5502361d7f0
yuhcee <egbouchenna001@gmail.com>
2022-05-03 08:54:08 +0100

    capitalizeFirst: add problem description

---
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/capitalizeFirst.js
@@ -0,0 +1,10 @@
+/**
+ * **Capitalize First**
+ *
+ * Write a recursive function called **capitalizeFirst**. Given an array of strings, capitalize the
+ * first letter of each string in the array.
+ *
+ */
+const capitalizeFirst = (arr)=> {
+    
+}
