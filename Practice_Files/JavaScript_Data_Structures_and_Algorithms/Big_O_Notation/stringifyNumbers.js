465a47a924aa3dd6c8eacfed95994b9b6f46d584
yuhcee <egbouchenna001@gmail.com>
2022-05-03 11:15:57 +0100

    stringifyNumbers: add task description

---
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/stringifyNumbers.js
@@ -0,0 +1,7 @@
+/**
+ * **stringifyNumbers**
+ *
+ * Write a function called `stringifyNumbers` which takes in an object and finds all of the values which
+ * are numbers and converts them to strings. Recursion would be a great way to solve this!
+ */
+const stringifyNumbers = (obj) => {};
