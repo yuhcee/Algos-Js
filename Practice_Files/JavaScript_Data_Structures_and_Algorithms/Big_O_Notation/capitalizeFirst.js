a471112e49600f74690e5c7cf83bd70a21b7101d
yuhcee <egbouchenna001@gmail.com>
2022-05-03 09:00:56 +0100

    capitalizeFirst: add recursive solution

--- Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/capitalizeFirst.js
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/capitalizeFirst.js
@@ -5,6 +5,20 @@
  * first letter of each string in the array.
  *
  */
-const capitalizeFirst = (arr)=> {
-    
-}
+const capitalizeFirst = (arr) => {
+    if (arr.length === 1) {
+        return [arr[0].charAt(0).toUppercase() + arr[0].substring(1)];
+    }
+
+    let res = capitalizeFirst(arr.slice(0, -1));
+    res.push(
+        arr
+            .slice(arr.length - 1)[0]
+            .charAt(0)
+            .toUppercase() + arr.slice(arr.length - 1)[0].substring(1)
+    );
+
+    return res;
+};
+
+
