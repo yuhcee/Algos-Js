0a775c5997ca07b09dd6dcab5ba5deaf5c98a871
yuhcee <egbouchenna001@gmail.com>
2022-05-03 11:10:53 +0100

    capitalizeWords: add recursive solution

--- Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/capitalizeWords.js
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/capitalizeWords.js
@@ -3,4 +3,15 @@
  * containing each word capitalized.
  */
 
-const capitalizeWords = (arr) => {};
+const capitalizedWords = (arr) => {
+    if (arr.length === 1) {
+        return [arr[0].toUpperCase()];
+    }
+    let res = capitalizedWords(arr.slice(0, -1));
+    res.push(arr.slice(arr.length - 1)[0].toUpperCase());
+
+    return res;
+};
+
+let words = ['i', 'am', 'learning', 'recursion'];
+console.log(capitalizedWords(words)); // ['I', 'AM', 'LEARNING', 'RECURSION']
