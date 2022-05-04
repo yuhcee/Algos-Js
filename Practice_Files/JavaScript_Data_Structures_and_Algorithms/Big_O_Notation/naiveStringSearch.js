f157984ecd8ebebe39bd550a2c121c659132803b
yuhcee <egbouchenna001@gmail.com>
2022-05-04 14:55:02 +0100

    naiveSearchString: add task description

---
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/naiveStringSearch.js
@@ -0,0 +1,8 @@
+/**
+ * **Naive String Search**
+ *
+ * Write a function that accepts two parameters, `longStr` and `shortStr` and check if `shortStr` exists
+ * in the longStr`.
+ *
+ * Return *the number of times it existed*.
+ */
