65c2f515ba6214a662cd49b4ad59a86600270254
yuhcee <egbouchenna001@gmail.com>
2022-05-07 23:36:53 +0100

    find132Pattern: add stack solution

--- Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/find132Pattern.js
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/find132Pattern.js
@@ -7,4 +7,22 @@
  * @param {number[]} nums
  * @return {boolean}
  */
-var find132pattern = (nums) => {};
+var find132pattern = (nums) => {
+    let arr = [];
+
+    let negative = -Infinity;
+
+    for (let i = nums.length - 1; i >= 0; i--) {
+        if (nums[i] < negative) {
+            return true;
+        }
+
+        while (arr.length > 0 && nums[i] > arr[arr.length - 1]) {
+            negative = arr.pop();
+        }
+
+        arr.push(nums[i]);
+    }
+
+    return false;
+};
