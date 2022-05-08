1678c99cb68146c1f76a20db084a54d835f122a5
yuhcee <egbouchenna001@gmail.com>
2022-05-08 23:10:44 +0100

    selectionSort: if a smaller number is found, designate that number to be the new minimum

--- Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/selectionSort.js
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/selectionSort.js
@@ -3,10 +3,11 @@ const selectionSort = (arr) => {
     // Store the first number as the smallest you've seen so far.
     let smallest = arr[0];
 
-    for(let i = 1; i < arr.length - 1; i++){
+    for (let i = 1; i < arr.length - 1; i++) {
         // Compare this item to the next item in the arr until you find a smaller number
-        if(smallest > arr[i]){
-    
+        if (smallest > arr[i]) {
+            // if a smaller number is found, designate that number to be the new minimum, and continue till the end.
+            smallest = arr[i];
         }
     }
 };
