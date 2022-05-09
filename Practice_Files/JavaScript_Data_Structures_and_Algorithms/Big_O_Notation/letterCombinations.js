83710b4294d64b2416a03f6962618aa263e827c0
yuhcee <egbouchenna001@gmail.com>
2022-05-09 22:56:04 +0100

    letterCombinations: add task description

---
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/letterCombinations.js
@@ -0,0 +1,13 @@
+/**
+ * **17. Letter Combinations of a Phone Number**
+ * 
+ * Given a string containing digits from `2-9` inclusive, return all possible letter 
+ * combinations that the number could represent. Return the answer in **any order**.
+ * 
+ * A mapping of digit to letters (just like on the telephone buttons) is given below. Note that 
+ * 1 does not map to any letters.
+ * 
+ * @param {string} digits
+ * @return {string[]}
+ */
+var letterCombinations =  (digits) => {};
