4274e195402c9931a8cacd75c345aa648f3cb1b5
yuhcee <egbouchenna001@gmail.com>
2022-05-09 14:06:06 +0100

    selectionSort: add ES6 revised solution

--- Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/selectionSort.js
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/selectionSort.js
@@ -1,5 +1,5 @@
 /* Implement selection sort on an array input */
-const selectionSort = (arr) => {
+/* const selectionSort = (arr) => {
     for (let i = 0; i < arr.length; i++) {
         // Store the first number(index) as the smallest you've seen so far.
         let smallest = i;
@@ -18,6 +18,18 @@ const selectionSort = (arr) => {
         }
     }
     return arr;
+}; */
+
+const selectionSort = (arr) => {
+    for (let i = 0; i < arr.length; i++) {
+        let lowest = i;
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[lowest] > arr[j]) lowest = j;
+        }
+        if (lowest !== i) swap(arr, lowest, i);
+    }
+    return arr;
 };
 
+const swap = (arr, lowest, i) => ([arr[i], arr[lowest]] = [arr[lowest], arr[i]]);
 console.log(selectionSort([2, 0, 34, 22, 10, 19, 17]));
