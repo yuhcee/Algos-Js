36a9f0f40b53f5a769a4ed57bfa8dbcfcad8426c
yuhcee <egbouchenna001@gmail.com>
2022-05-11 11:20:12 +0100

    selectionSort: add revision

--- Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/selectionSort.js
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/selectionSort.js
@@ -1,26 +1,23 @@
 /* Implement selection sort on an array input */
-/* const selectionSort = (arr) => {
+const selectionSort = (arr) => {
     for (let i = 0; i < arr.length; i++) {
         // Store the first number(index) as the smallest you've seen so far.
-        let smallest = i;
+        let min = i;
         for (let j = i + 1; j < arr.length; j++) {
             // Compare this item to the next item in the arr until you find a smaller number
-            if (arr[smallest] > arr[j]) {
+            if (arr[j] < arr[min]) {
                 // if a smaller number is found, designate that number(index) to be the new minimum, and continue till the end.
-                smallest = j;
+                min = j;
             }
         }
         // if the minimun is not the value(index) you initially began with, swap the two values
-        if (smallest !== i) {
-            temp = arr[i];
-            arr[i] = arr[smallest];
-            arr[smallest] = temp;
-        }
+        if (i !== min) swap(arr, min, i);
     }
-    return arr;
-}; */
 
-const selectionSort = (arr) => {
+    return arr;
+};
+
+/* const selectionSort = (arr) => {
     for (let i = 0; i < arr.length; i++) {
         let lowest = i;
         for (let j = i + 1; j < arr.length; j++) {
@@ -29,7 +26,7 @@ const selectionSort = (arr) => {
         if (lowest !== i) swap(arr, lowest, i);
     }
     return arr;
-};
+}; */
 
 const swap = (arr, lowest, i) => ([arr[i], arr[lowest]] = [arr[lowest], arr[i]]);
 console.log(selectionSort([2, 0, 34, 22, 10, 19, 17]));
