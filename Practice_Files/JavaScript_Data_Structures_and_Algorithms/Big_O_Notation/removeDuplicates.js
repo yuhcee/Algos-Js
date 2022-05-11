7f1ea3793cda319312148bfae7208d424f3aec37
yuhcee <egbouchenna001@gmail.com>
2022-05-11 10:22:07 +0100

    removeDuplicates: add revision

--- Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/removeDuplicates.js
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/removeDuplicates.js
@@ -2,7 +2,7 @@
  * **Remove All Adjacent Duplicates in String II**
  *
  * You are given a string s and an integer `k`, a `k` **duplicate removal** consists of choosing
- * `k` adjacent and equal letters from s and removing them, causing the left and the right side
+ * `k` adjacent and equal letters from `s` and removing them, causing the left and the right side
  * of the deleted substring to concatenate together.
  *
  * We repeatedly make `k` **duplicate removals** on `s` until we no longer can.
@@ -23,12 +23,9 @@ const removeDuplicates = (s, k) => {
             stack.push({ char, count: 1 });
             continue;
         }
-
         stack.at(-1).count++;
-
         if (stack.at(-1).count === k) stack.pop();
     }
-
 
     return stack.map(({ char, count }) => char.repeat(count)).join('');
 };
