f245f4858784884831060226e86538fd0cc1404f
yuhcee <egbouchenna001@gmail.com>
2022-05-12 15:15:06 +0100

    permutations: add recursive backtracking solution

--- Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/permutations.js
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/permutations.js
@@ -7,4 +7,25 @@
  * @param {number[]} nums
  * @return {number[][]}
  */
-const permuteUnique = (nums) => {};
+const permuteUnique = (nums) => {
+    const sorted = nums.sort((a, b) => a - b),
+        permutataions = [];
+
+    const recurse = (arr, permutation) => {
+        if (!arr.length) return permutataions.push(permutation);
+
+        let previous = -Infinity;
+
+        for (let i = 0; i < arr.length; i++) {
+            if (previous === arr[i]) continue;
+
+            let newArr = arr.slice(0, i).concat(arr.slice(0 + 1));
+            recurse(newArr, [...permutation, arr[i]]);
+
+            previous = arr[i];
+        }
+    };
+    recurse(sorted, []);
+
+    return permutataions;
+};
