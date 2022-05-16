23550c4cf8a17fcb69becfc9bec59549c9fc8d7f
yuhcee <egbouchenna001@gmail.com>
2022-05-16 11:00:56 +0100

    networkDelayTime: review solution

--- Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/networkDelayTime.js
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/networkDelayTime.js
@@ -13,43 +13,71 @@
  * @param {number} k
  * @return {number}
  */
-const networkDelayTime = (times, n, k) => {
-    // Initially we assume the time-taken to reach each node is a large value. Math.min() --> without any args will be large val (Infinity).
-    let nodes = new Array(n).fill(Math.min());
+// const networkDelayTime = (times, n, k) => {
+//     // Initially we assume the time-taken to reach each node is a large value. Math.min() --> without any args will be large val (Infinity).
+//     let nodes = new Array(n).fill(Math.min());
 
-    // K is staring node where we get the signal initally.So we dont have to wait for it.
-    // since its array we assign k-1(index) as 0.
+//     // K is staring node where we get the signal initally.So we dont have to wait for it.
+//     // since its array we assign k-1(index) as 0.
+//     nodes[k - 1] = 0;
+
+//     // iterate until we have found actual time for the signal
+//     let shouldComputeTime = true;
+//     while (shouldComputeTime) {
+//         /* we change to false initially thinking that we can find all actual times in first iteration &
+// 		we dont have to recompute again(positive case) */
+//         shouldComputeTime = false;
+
+//         for (let [src, target, timeTaken] of times) {
+//             // decrementing because we need to access our actualTimeTaken array(index-based)
+//             src -= 1;
+//             target -= 1;
+
+//             // if source itself is large, it means we havent found a way to reach the source so just continue with next nodes
+//             if (nodes[src] === Math.min()) continue;
+
+//             // if not checking if time taken to reach target is lesser than what we assumed in our array
+//             /* If so, then change its time to correct time and now we need to iterate again from beginning in the next cycle
+// 			because we have changed a time and now we need to recompute according to this time. */
+//             if (nodes[target] > nodes[src] + timeTaken) {
+//                 nodes[target] = nodes[src] + timeTaken;
+//                 // we need to recompute again as we did changes to a particular node's time
+//                 shouldComputeTime = true;
+//             }
+//         }
+//     }
+//     // the max value will be time taken to reach since its the last node
+//     let maxTimeToLastNode = Math.max(...nodes);
+//     // sometimes we wouldnt have recomputed again In that case there is no chance the signal would reach the end node so return -1
+//     return maxTimeToLastNode === Infinity ? -1 : maxTimeToLastNode;
+// };
+
+const networkDelayTime = (times, n, k) => {
+    const { max, min } = Math,
+        nodes = new Array(n).fill(min());
+
     nodes[k - 1] = 0;
 
-    // iterate until we have found actual time for the signal
-    let shouldComputeTime = true;
-    while (shouldComputeTime) {
-        /* we change to false initially thinking that we can find all actual times in first iteration &
-		we dont have to recompute again(positive case) */
-        shouldComputeTime = false;
+    let computeTime = true;
+
+    while (computeTime) {
+        computeTime = false;
 
         for (let [src, target, timeTaken] of times) {
-            // decrementing because we need to access our actualTimeTaken array(index-based)
             src -= 1;
             target -= 1;
 
-            // if source itself is large, it means we havent found a way to reach the source so just continue with next nodes
-            if (nodes[src] === Math.min()) continue;
+            if (nodes[src] === min()) continue;
 
-            // if not checking if time taken to reach target is lesser than what we assumed in our array
-            /* If so, then change its time to correct time and now we need to iterate again from beginning in the next cycle
-			because we have changed a time and now we need to recompute according to this time. */
             if (nodes[target] > nodes[src] + timeTaken) {
                 nodes[target] = nodes[src] + timeTaken;
-                // we need to recompute again as we did changes to a particular node's time
-                shouldComputeTime = true;
+                computeTime = true;
             }
         }
     }
-    // the max value will be time taken to reach since its the last node
-    let maxTimeToLastNode = Math.max(...nodes);
-    // sometimes we wouldnt have recomputed again In that case there is no chance the signal would reach the end node so return -1
-    return maxTimeToLastNode === Infinity ? -1 : maxTimeToLastNode;
+
+    let maxTimeToLastNode = max(...nodes);
+    return maxTimeToLastNode === min() ? -1 : maxTimeToLastNode;
 };
 
 const times = [
