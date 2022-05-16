93afa45a45e185790d418a80e0277ad00ac9f7fa
yuhcee <egbouchenna001@gmail.com>
2022-05-16 23:24:35 +0100

    shortestPatBinaryMatrix: return -1 if there's no movements

--- Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/shortestPathBinaryMatrix.js
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/shortestPathBinaryMatrix.js
@@ -1,18 +1,23 @@
 /**
  * **1091. Shortest Path in Binary Matrix**
- * 
+ *
  * Given an `n x n` binary matrix `grid`, return *the length of the **shortest clear path** in the matrix*. If there is no clear path, return `-1`.
- * 
+ *
  * A **clear path** in a binary matrix is a path from the **top-left** cell (i.e., `(0, 0)`) to
  * the bottom-right cell (i.e., `(n - 1, n - 1)`) such that:
- * 
+ *
  * - All the visited cells of the path are `0`.
- * - All the adjacent cells of the path are **8-directionally** connected (i.e., they are 
+ * - All the adjacent cells of the path are **8-directionally** connected (i.e., they are
  * different and they share an edge or a corner).
- * 
+ *
  * The **length of a clear path** is the number of visited cells of this path.
- * 
+ *
  * @param {number[][]} grid
  * @return {number}
  */
-const shortestPathBinaryMatrix = (grid) => {};
+const shortestPathBinaryMatrix = (grid) => {
+    const n = grid.length - 1;
+
+    // return -1 if there's no movements
+    if (grid[0][0] || grid[n][n]) return -1;
+};
