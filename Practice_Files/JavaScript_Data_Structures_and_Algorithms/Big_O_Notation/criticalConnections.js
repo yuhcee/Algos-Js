133683ee824ee502be4d17210e95a1c23d50d908
yuhcee <egbouchenna001@gmail.com>
2022-05-18 23:46:24 +0100

    ciriticalConnections: add task description

---
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/criticalConnections.js
@@ -0,0 +1,17 @@
+/**
+ * **1192. Critical Connections in a Network**
+ *
+ * There are `n` servers numbered from `0` to `n - 1` connected by undirected server-to-server
+ * `connections` forming a network where `connections[i] = [ai, bi]` represents a connection
+ * between servers `ai` and `bi`. Any server can reach other servers directly or indirectly
+ * through the network.
+ *
+ * A **critical connection** is a connection that, if removed, will make some servers unable to reach some other server.
+ *
+ * Return all critical connections in the network in any order.
+ *
+ * @param {number} n
+ * @param {number[][]} connections
+ * @return {number[][]}
+ */
+const criticalConnections = (n, connections) => {};
