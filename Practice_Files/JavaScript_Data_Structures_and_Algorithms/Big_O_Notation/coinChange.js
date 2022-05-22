f5d0436c7b21911c92353fe7c594faa29153504f
yuhcee <egbouchenna001@gmail.com>
2022-05-22 01:29:57 +0100

    coinChange: add dp solution

--- Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/coinChange.js
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/coinChange.js
@@ -1,16 +1,29 @@
 /**
  * **322. Coin Change**
- * 
- * You are given an integer array `coins` representing coins of different denominations and an integer 
+ *
+ * You are given an integer array `coins` representing coins of different denominations and an integer
  * `amount` representing a total amount of money.
- * 
- * Return *the fewest number of coins that you need to make up that amount*. If that amount of money 
+ *
+ * Return *the fewest number of coins that you need to make up that amount*. If that amount of money
  * cannot be made up by any combination of the coins, return -1.
- * 
+ *
  * You may assume that you have an infinite number of each kind of coin.
- * 
+ *
  * @param {number[]} coins
  * @param {number} amount
  * @return {number}
  */
-var coinChange = (coins, amount) => {};
+var coinChange = (coins, amount) => {
+    const dp = Array(amount + 1).fill(Number.MAX_VALUE);
+
+    dp[0] = 0;
+
+    for (let coin of coins) {
+        for (let i = coin; i <= amount; i++) {
+            dp[i] = Math.min(dp[i], dp[i - coin] + 1);
+        }
+    }
+    const ans = dp[dp.length - 1];
+
+    return ans === Number.MAX_VALUE ? -1 : ans;
+};
