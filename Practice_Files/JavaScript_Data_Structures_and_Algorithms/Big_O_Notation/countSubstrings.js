e1d7c28e7a509b149376a8b22b5e09dccba37632
yuhcee <egbouchenna001@gmail.com>
2022-05-23 22:42:26 +0100

    countSubstrings: init local vars

--- Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/countSubstrings.js
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/countSubstrings.js
@@ -1,13 +1,17 @@
 /**
  * **647. Palindromic Substrings**
- * 
+ *
  * Given a string s, return the number of palindromic substrings in it.
- * 
+ *
  * A string is a palindrome when it reads the same backward as forward.
- * 
+ *
  * A substring is a contiguous sequence of characters within the string.
- * 
+ *
  * @param {string} s
  * @return {number}
  */
- const countSubstrings = (s) =>  {}
+const countSubstrings = (s) => {
+    const len = s.length;
+    const dp = [];
+    let count = 0;
+};
