ea4f147f03fe45bd746340541ed6e2942d1482e1
yuhcee <egbouchenna001@gmail.com>
2022-05-24 23:49:17 +0100

    findMaxForm : add tests

--- Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/findMaxForm.js
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/findMaxForm.js
@@ -24,3 +24,18 @@ const findMaxForm = (S, M, N) => {
     }
     return dp[M][N];
 };
+
+const strs = ['10', '0001', '111001', '1', '0'],
+    m = 5,
+    n = 3;
+// Output: 4
+/* Explanation: The largest subset with at most 5 0's and 3 1's is {"10", "0001", "1", "0"}, so the answer is 4.
+Other valid but smaller subsets include {"0001", "1"} and {"10", "1", "0"}.
+{"111001"} is an invalid subset because it contains 4 1's, greater than the maximum of 3. */
+console.log(findMaxForm(strs, m, n));
+const strs1 = ['10', '0', '1'],
+    m1 = 1,
+    n1 = 1;
+// Output: 2
+// Explanation: The largest subset is {"0", "1"}, so the answer is 2.
+console.log(findMaxForm(strs1, m1, n1));
