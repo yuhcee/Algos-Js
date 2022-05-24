3cfbf0a27dbd4c1c57458aa7c82f30e8532423c2
yuhcee <egbouchenna001@gmail.com>
2022-05-24 23:42:27 +0100

    findMaxForm : add dp logic

--- Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/findMaxForm.js
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/findMaxForm.js
@@ -19,5 +19,7 @@ const findMaxForm = (S, M, N) => {
         let str = S[i],
             zeros = 0,
             ones = 0;
+        for (let j = 0; j < str.length; j++) str.charAt(j) === '0' ? zeros++ : ones++;
+        for (let j = M; j >= zeros; j--) for (let k = N; k >= ones; k--) dp[j][k] = Math.max(dp[j][k], dp[j - zeros][k - ones] + 1);
     }
 };
