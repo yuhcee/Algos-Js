9bf532b00c9660f35f3fc156429e38245f6f9c58
yuhcee <egbouchenna001@gmail.com>
2022-05-24 23:43:19 +0100

    findMaxForm : return res

--- Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/findMaxForm.js
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/findMaxForm.js
@@ -22,4 +22,5 @@ const findMaxForm = (S, M, N) => {
         for (let j = 0; j < str.length; j++) str.charAt(j) === '0' ? zeros++ : ones++;
         for (let j = M; j >= zeros; j--) for (let k = N; k >= ones; k--) dp[j][k] = Math.max(dp[j][k], dp[j - zeros][k - ones] + 1);
     }
+    return dp[M][N];
 };
