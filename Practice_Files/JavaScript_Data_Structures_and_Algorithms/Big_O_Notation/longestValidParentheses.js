a01e833e37850855b2a1403b290e8ed6674b409a
yuhcee <egbouchenna001@gmail.com>
2022-05-25 09:15:08 +0100

    longestValidParentheses : add dp solution

--- Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/longestValidParentheses.js
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/longestValidParentheses.js
@@ -9,7 +9,7 @@
  */
 
 // Using Stack
-const longestValidParentheses = (s) => {
+/* const longestValidParentheses = (s) => {
     let max = 0,
         stack = [];
     stack.push(-1);
@@ -26,7 +26,33 @@ const longestValidParentheses = (s) => {
         }
     }
     return max;
-};
+}; */
+
+// Using Dynamic Programming
+function longestValidParentheses(s) {
+    // maxLength variable is used to monitor the maximum length of the valid substring
+    // every time we fall in condition a. or b.
+    // by using it we don't need to find the maximum value in the resulting dp array
+    let maxLength = 0;
+    let dp = new Array(s.length).fill(0);
+
+    for (let i = 1; i < s.length; i++) {
+        if (s[i - 1] === '(' && s[i] === ')') {
+            // - condition a.
+            dp[i] = 2 + (dp[i - 2] || 0); // - dp[i-2] can return undefined, if so we will add zero instead
+            maxLength = Math.max(maxLength, dp[i]);
+        }
+        if (
+            s[i - 1] === ')' &&
+            s[i] === ')' &&
+            s[i - dp[i - 1] - 1] === '(' // - condition b.
+        ) {
+            dp[i] = 2 + dp[i - 1] + (dp[i - dp[i - 1] - 2] || 0);
+            maxLength = Math.max(maxLength, dp[i]);
+        }
+    }
+    return maxLength;
+}
 
 const s = '(()';
 // Output: 2
