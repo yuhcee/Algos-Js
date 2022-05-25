4dade9ee01a6d0df12e6cd0d08552f0b8705ab14
yuhcee <egbouchenna001@gmail.com>
2022-05-25 09:08:38 +0100

    longestValidParentheses : add stack solution

--- Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/longestValidParentheses.js
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/longestValidParentheses.js
@@ -1,9 +1,29 @@
 /**
  * **32. Longest Valid Parentheses**
+ *
  * Given a string containing just the characters `'('` and `')'`, find the length of the
  * longest valid (well-formed) parentheses substring.
  *
  * @param {string} s
  * @return {number}
  */
-const longestValidParentheses = (s) => {};
+
+// Using Stack
+const longestValidParentheses = (s) => {
+    const max = 0,
+        stack = [];
+    stack.push(-1);
+
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] === '(') stack.push(i);
+        else {
+            stack.pop();
+            if (!stack.length) {
+                stack.push(i);
+            } else {
+                max = Math.max(max, i - stack.at(-1));
+            }
+        }
+    }
+    return max;
+};
