4bb62d040a30b013b6a674543f923603c3be97b5
yuhcee <egbouchenna001@gmail.com>
2022-05-25 10:33:13 +0100

    maxEnvelopes: add task description

---
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/maxEnvelopes.js
@@ -0,0 +1,33 @@
+/**
+ * **354. Russian Doll Envelopes**
+ * 
+ * You are given a 2D array of integers `envelopes` where `envelopes[i] = [wi, hi]` represents the width and the height of an 
+ * envelope.
+ * 
+ * One envelope can fit into another if and only if both the width and height of one envelope are greater than the other 
+ * envelope's width and height.
+ * 
+ * Return *the maximum number of envelopes you can Russian doll (i.e., put one inside the other)*.
+ * 
+ * **Note**: You cannot rotate an envelope.
+
+ * @param {number[][]} envelopes
+ * @return {number}
+ */
+const maxEnvelopes = (envelopes) => {};
+
+const envelopes = [
+    [5, 4],
+    [6, 4],
+    [6, 7],
+    [2, 3],
+];
+// Output: 3
+// Explanation: The maximum number of envelopes you can Russian doll is 3 ([2,3] => [5,4] => [6,7]).
+console.log(maxEnvelopes(envelopes));
+const envelopes1 = [
+    [1, 1],
+    [1, 1],
+    [1, 1],
+];
+// Output: 1
