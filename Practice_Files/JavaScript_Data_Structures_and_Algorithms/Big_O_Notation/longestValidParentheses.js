119ff2cc053c2439b35b60f6b7d83ccadab54e18
yuhcee <egbouchenna001@gmail.com>
2022-05-25 09:01:53 +0100

    longestValidParentheses : add task description

---
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/longestValidParentheses.js
@@ -0,0 +1,9 @@
+/**
+ * **32. Longest Valid Parentheses**
+ * Given a string containing just the characters `'('` and `')'`, find the length of the
+ * longest valid (well-formed) parentheses substring.
+ *
+ * @param {string} s
+ * @return {number}
+ */
+const longestValidParentheses = (s) => {};
