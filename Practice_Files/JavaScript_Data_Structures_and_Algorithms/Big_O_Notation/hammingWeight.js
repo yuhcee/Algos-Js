7977fb10610ab3fb889ac618dbdde601f178327e
yuhcee <egbouchenna001@gmail.com>
2022-05-26 21:43:00 +0100

    hammingWeight : add task description

---
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/hammingWeight.js
@@ -0,0 +1,18 @@
+/**
+ * **191. Number of 1 Bits**
+ *
+ * Write a function that takes an unsigned integer and returns the number of '1' bits it has (also known
+ * as the Hamming weight).
+ *
+ * **Note:**
+ *
+ * - Note that in some languages, such as Java, there is no unsigned integer type. In this case, the input
+ * will be given as a signed integer type. It should not affect your implementation, as the integer's
+ * internal binary representation is the same, whether it is signed or unsigned.
+ *
+ * - In Java, the compiler represents the signed integers using 2's complement notation. Therefore, in  **Example 3**, the input represents the signed integer. `-3`.
+ *
+ * @param {number} n - a positive integer
+ * @return {number}
+ */
+const hammingWeight = (n) => {};
