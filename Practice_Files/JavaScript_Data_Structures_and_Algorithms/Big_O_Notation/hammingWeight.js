d2b7f1365174b83140ea7016c4d41af354488404
yuhcee <egbouchenna001@gmail.com>
2022-05-26 21:54:28 +0100

    hammingWeight : add third test

--- Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/hammingWeight.js
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/hammingWeight.js
@@ -33,3 +33,7 @@ const n1 = 00000000000000000000000010000000;
 // Output: 1
 // Explanation: The input binary string 00000000000000000000000010000000 has a total of one '1' bit.
 console.log(hammingWeight(n1));
+const n2 = 11111111111111111111111111111101;
+// Output: 31;
+// Explanation: The input binary string 11111111111111111111111111111101 has a total of thirty one '1' bits.
+console.log(hammingWeight(n2));
