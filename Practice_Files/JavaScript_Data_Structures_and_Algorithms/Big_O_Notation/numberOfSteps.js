103367345a85c28d13869149d3f868d96ab9128c
yuhcee <egbouchenna001@gmail.com>
2022-05-27 23:01:25 +0100

    numberOfSteps: add iterative simulation sol

--- Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/numberOfSteps.js
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/numberOfSteps.js
@@ -1,12 +1,25 @@
 /**
  * **1342. Number of Steps to Reduce a Number to Zero**
- * 
+ *
  * Given an integer `num`, return *the number of steps to reduce it to zero*.
- * 
+ *
  * In one step, if the current number is even, you have to divide it by `2`
  * otherwise, you have to subtract `1` from it.
- * 
+ *
  * @param {number} num
  * @return {number}
  */
-const numberOfSteps = (num) => {};
+const numberOfSteps = (num) => {
+    let steps = 0;
+
+    while (num !== 0) {
+        if (num % 2 === 0) num = num / 2;
+        else {
+            num = num - 1;
+        }
+        steps++;
+    }
+
+    return steps;
+};
+
