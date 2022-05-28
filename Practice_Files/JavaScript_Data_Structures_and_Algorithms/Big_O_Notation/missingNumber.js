8af640b725513aad64aa53e3fc7b4b638c3b8d58
yuhcee <egbouchenna001@gmail.com>
2022-05-28 03:14:15 +0100

    missingNumber: add tests

--- Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/missingNumber.js
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/missingNumber.js
@@ -23,4 +23,18 @@ const missingNumber = (nums) => {
     return -1;
 };
 
+const nums = [3, 0, 1];
+// Output: 2;
+// Explanation: n = 3 since there are 3 numbers, so all numbers are in the range [0,3]. 2 is the missing number in the range since it does not appear in nums.
+console.log(missingNumber(nums));
+
+const nums1 = [0, 1];
+// Output: 2;
+// Explanation: n = 2 since there are 2 numbers, so all numbers are in the range [0,2]. 2 is the missing number in the range since it does not appear in nums.
+console.log(missingNumber(nums1));
+
+const nums2 = [9, 6, 4, 2, 3, 5, 7, 0, 1];
+// Output: 8;
+// Explanation: n = 9 since there are 9 numbers, so all numbers are in the range [0,9]. 8 is the missing number in the range since it does not appear in nums.
+console.log(missingNumber(nums2));
 
