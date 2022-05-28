b96fe5a66c77d3b1a6d3f067db55320f02ee93c9
yuhcee <egbouchenna001@gmail.com>
2022-05-28 03:13:08 +0100

    missingNumber: add solution

--- Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/missingNumber.js
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/missingNumber.js
@@ -1,13 +1,26 @@
 /**
- * 
+ *
  * **268. Missing Number**
- * 
- * Given an array nums containing `n` distinct numbers in the range `[0, n]`, 
+ *
+ * Given an array nums containing `n` distinct numbers in the range `[0, n]`,
  * return *the only number in the range that is missing from the array*.
- * 
+ *
  * @param {number[]} nums
  * @return {number}
  */
- const missingNumber = (nums) => {
-     
- }
+const missingNumber = (nums) => {
+    nums.sort((a, b) => a - b);
+
+    if (nums.at(-1) !== nums.length) return nums.length;
+
+    if (nums[0] !== 0) return 0;
+
+    for (let i = 1; i < nums.length; i++) {
+        let expectedNum = nums[i - 1] + 1;
+        if (nums[i] !== expectedNum) return expectedNum;
+    }
+
+    return -1;
+};
+
+
