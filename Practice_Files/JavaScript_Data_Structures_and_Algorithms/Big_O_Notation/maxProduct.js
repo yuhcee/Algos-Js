0c7daf706f4c1e02d7f44511be94591176698952
yuhcee <egbouchenna001@gmail.com>
2022-05-29 23:39:48 +0100

    maxProduct: add task description

---
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/maxProduct.js
@@ -0,0 +1,11 @@
+/**
+ * **318. Maximum Product of Word Lengths**
+ * 
+ * Given a string array `words`, return *the maximum value of `length(word[i]) * length(word[j])` where the two words
+ * do not share common letters*. If no such two words exist, return `0`.
+ * 
+ * @param {string[]} words
+ * @return {number}
+ */
+// Solution #1: Set/Array
+const maxProduct = (words) => {};
