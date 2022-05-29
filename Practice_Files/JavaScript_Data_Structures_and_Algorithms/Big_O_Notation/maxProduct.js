7823f4678b15825ab50c126cf230bc637697d6ae
yuhcee <egbouchenna001@gmail.com>
2022-05-29 23:54:25 +0100

    maxProduct: return res

--- Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/maxProduct.js
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/maxProduct.js
@@ -15,7 +15,12 @@ const maxProduct = (words) => {
     let res = 0;
     for (let i = 0; i < words.length - 1; i++) {
         for (let j = i + 1; j < words.length; j++) {
-            
+            // if some letter in letters[j](words[j]) is found in letters[i](words[i]), then two words share common letters.
+            if (!letters[i].some((item) => letters[j].includes(item))) {
+                let prod = words[i].length * words[j].length;
+                if (prod > res) res = prod;
+            }
         }
     }
+    return res;
 };
