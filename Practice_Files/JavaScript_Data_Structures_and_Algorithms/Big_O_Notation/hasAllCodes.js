5c1570071c2d71fe116349c809fd9e5319c0b57b
yuhcee <egbouchenna001@gmail.com>
2022-06-01 23:38:39 +0100

    hasAllCodes: add task description

---
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/hasAllCodes.js
@@ -0,0 +1,11 @@
+/**
+ * **1461. Check If a String Contains All Binary Codes of Size K**
+ *
+ * Given a binary string `s` and an integer `k`, return `true` *if every binary code of length `k` is a substring of
+ * `s`*. Otherwise, return `false`.
+ * 
+ * @param {string} s
+ * @param {number} k
+ * @return {boolean}
+ */
+const hasAllCodes = (s, k) => {};
