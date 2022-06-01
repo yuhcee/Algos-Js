f2d389c6ddc6ba711eea128316fa0b467a78815f
yuhcee <egbouchenna001@gmail.com>
2022-06-01 23:50:20 +0100

    hasAllCodes: add test 1

--- Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/hasAllCodes.js
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/hasAllCodes.js
@@ -23,3 +23,8 @@ const hasAllCodes = (s, k) => {
     }
     return false;
 };
+const s = '00110110',
+    k = 2;
+// Output: true;
+// Explanation: The binary codes of length 2 are "00", "01", "10" and "11". They can be all found as substrings at indices 0, 1, 3 and 2 respectively.
+console.log(hasAllCodes(s, k));
