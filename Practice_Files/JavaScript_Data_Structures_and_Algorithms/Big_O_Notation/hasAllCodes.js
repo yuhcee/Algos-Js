f2b522efc017ae6d703b156dab41582767d631ff
yuhcee <egbouchenna001@gmail.com>
2022-06-01 23:41:28 +0100

    hasAllCodes: loop through s length

--- Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/hasAllCodes.js
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/hasAllCodes.js
@@ -3,9 +3,16 @@
  *
  * Given a binary string `s` and an integer `k`, return `true` *if every binary code of length `k` is a substring of
  * `s`*. Otherwise, return `false`.
- * 
+ *
  * @param {string} s
  * @param {number} k
  * @return {boolean}
  */
-const hasAllCodes = (s, k) => {};
+const hasAllCodes = (s, k) => {
+    let need = 1 << k;
+    let got = new Set();
+
+    for(let i = 0; i < s.length; i++) {
+        
+    }
+};
