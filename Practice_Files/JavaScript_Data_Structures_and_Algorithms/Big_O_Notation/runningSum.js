e460615e940b43dd7badad0bb629b42f3be32995
yuhcee <egbouchenna001@gmail.com>
2022-06-02 23:35:48 +0100

    chore(runningSum): using input array for output

--- Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/runningSum.js
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/runningSum.js
@@ -1,13 +1,16 @@
 /**
  * **1480. Running Sum of 1d Array**
- * 
+ *
  * Given an array `nums`. We define a running sum of an array as `runningSum[i] = sum(nums[0]…nums[i])`.
- * 
+ *
  * Return the running sum of `nums`.
- * 
+ *
  * @param {number[]} nums
  * @return {number[]}
  */
 const runningSum = (nums) => {
-    
+    for (let i = 1; i < nums.length; i++) {
+        nums[i] += nums[i - 1];
+    }
+    return nums;
 };
