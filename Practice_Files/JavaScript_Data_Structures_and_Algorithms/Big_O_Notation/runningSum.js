ad83e629a3235378fbb0654aca4cb82266418aca
yuhcee <egbouchenna001@gmail.com>
2022-06-02 23:45:44 +0100

    test(runningSum): add first example

--- Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/runningSum.js
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/runningSum.js
@@ -14,3 +14,7 @@ const runningSum = (nums) => {
     }
     return nums;
 };
+const nums = [1, 2, 3, 4];
+// Output: [1,3,6,10]
+// Explanation: Running sum is obtained as follows: [1, 1+2, 1+2+3, 1+2+3+4].
+console.log(runningSum(nums));
