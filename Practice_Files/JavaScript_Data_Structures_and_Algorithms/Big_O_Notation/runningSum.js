8bcd0216967125ea756e79b33d8d6465513d4c3f
yuhcee <egbouchenna001@gmail.com>
2022-06-02 23:28:38 +0100

    feat(runningSum): add task description

---
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/runningSum.js
@@ -0,0 +1,13 @@
+/**
+ * **1480. Running Sum of 1d Array**
+ * 
+ * Given an array `nums`. We define a running sum of an array as `runningSum[i] = sum(nums[0]…nums[i])`.
+ * 
+ * Return the running sum of `nums`.
+ * 
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+const runningSum = (nums) => {
+    
+};
