04591c2c8379097ba938b2fd36d6a5cb9547b533
yuhcee <egbouchenna001@gmail.com>
2022-06-03 23:45:56 +0100

    chore(transpose):init output

--- Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/transpose.js
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/transpose.js
@@ -1,18 +1,19 @@
 /**
  * **867. Transpose Matrix**
- * 
+ *
  * Given a 2D integer array `matrix`, return *the **transpose** of `matrix`*.
- * 
- * The *transpose* of a matrix is the matrix flipped over its main diagonal, switching the matrix's row and column 
+ *
+ * The *transpose* of a matrix is the matrix flipped over its main diagonal, switching the matrix's row and column
  * indices.
- * 
+ *
  * @param {number[][]} matrix
  * @return {number[][]}
  */
 const transpose = (matrix) => {
     // init rows
     const rows = matrix.length,
-    // init cols
-          cols = matrix[0].length,
-
-}
+        // init cols
+        cols = matrix[0].length,
+        // init output
+        output = [];
+};
