67c1c9f9472045699e96bb4c14b9c2e31e5af038
yuhcee <egbouchenna001@gmail.com>
2022-06-03 23:35:33 +0100

    feat(transpose): add task description

---
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/transpose.js
@@ -0,0 +1,12 @@
+/**
+ * **867. Transpose Matrix**
+ * 
+ * Given a 2D integer array `matrix`, return *the **transpose** of `matrix`*.
+ * 
+ * The *transpose* of a matrix is the matrix flipped over its main diagonal, switching the matrix's row and column 
+ * indices.
+ * 
+ * @param {number[][]} matrix
+ * @return {number[][]}
+ */
+const transpose = (matrix) => {}
