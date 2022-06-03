8327d2ca5efb9843c3b088b503a267ec936e95c1
yuhcee <egbouchenna001@gmail.com>
2022-06-03 23:52:42 +0100

    test(transpose):add first test

--- Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/transpose.js
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/transpose.js
@@ -30,3 +30,12 @@ const transpose = (matrix) => {
     // return output arr
     return output;
 };
+
+const matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+];
+// Output: [[1,4,7],[2,5,8],[3,6,9]]
+console.log(transpose(matrix));
+
