58ee15ffa01fbd18dc4cba7909ede98a3dd86891
yuhcee <egbouchenna001@gmail.com>
2022-06-06 23:43:08 +0100

    docs(totalNQueens): add task description

---
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/totalNQueens.js
@@ -0,0 +1,12 @@
+/**
+ * **52. N-Queens II**
+ *
+ * The *n-queens* puzzle is the problem of placing `n` queens on an `n x n` chessboard such that no two queens attack
+ * each other.
+ *
+ * Given an integer `n`, return *the number of distinct solutions to the **n-queens puzzle***.
+ *
+ * @param {number} n
+ * @return {number}
+ */
+const totalNQueens = (n) => {};
