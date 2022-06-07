f7f61bd9f9aeac1ee301ad844814fc9df6955c0a
yuhcee <egbouchenna001@gmail.com>
2022-06-07 23:56:52 +0100

    chore(mergeSortedArr): add third solution

--- Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/mergeSortedArr.js
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/mergeSortedArr.js
@@ -60,3 +60,31 @@ var merge = function (nums1, m, nums2, n) {
         }
     }
 };
+
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+var merge = function (nums1, m, nums2, n) {
+    let pointer1 = m - 1,
+        pointer2 = n - 1;
+
+    for (let index = m + n - 1; index >= 0; index--) {
+        if (pointer2 < 0) {
+            break;
+        }
+        const numberOnTheLeft = nums1[pointer1];
+        const numberOnTheRight = nums2[pointer2];
+
+        if (numberOnTheLeft > numberOnTheRight) {
+            nums1[index] = numberOnTheLeft;
+            pointer1--;
+        } else {
+            nums1[index] = numberOnTheRight;
+            pointer2--;
+        }
+    }
+};
