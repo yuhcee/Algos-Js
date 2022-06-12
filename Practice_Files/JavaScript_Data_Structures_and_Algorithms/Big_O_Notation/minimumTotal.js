f0fac421563d370c002bf01cd1f054086248b0fb
yuhcee <egbouchenna001@gmail.com>
2022-06-12 23:46:38 +0100

    chore(minimumTotal):calculate minimum from bottom up

--- Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/minimumTotal.js
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/minimumTotal.js
@@ -12,8 +12,7 @@ const minimumTotal = (triangle) => {
     for (let i = triangle.length - 1; i > 0; i--) {
         const row = triangle[i];
         for (let j = 0; j < row.length - 1; j++) {
-            
+            triangle[i - 1][j] += Math.min(row[j], row[j + 1]);
         }
     }
 };
-
