3199558c83572708752122deaede530013b9bd09
yuhcee <egbouchenna001@gmail.com>
2022-06-13 23:34:05 +0100

    docs(minDistance): add task description

---
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/minDistance.js
@@ -0,0 +1,13 @@
+/**
+ * **583. Delete Operation for Two Strings**
+ *
+ * Given two strings `word1` and `word2`, return *the minimum number of **steps** required to make
+ * `word1` and `word2` the same.
+ *
+ * In one *step*, you can delete exactly one character in either string.
+ *
+ * @param {string} word1
+ * @param {string} word2
+ * @return {number}
+ */
+const minDistance = (w1, w2) => {};
