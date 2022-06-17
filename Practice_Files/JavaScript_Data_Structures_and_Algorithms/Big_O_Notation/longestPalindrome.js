56db7163cc6840b0bec8126dcb3e971bbd43be3c
yuhcee <egbouchenna001@gmail.com>
2022-06-17 20:20:16 +0100

    docs(longestPalindrome):add task description

---
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/longestPalindrome.js
@@ -0,0 +1,9 @@
+/**
+ * **5. Longest Palindromic Substring**
+ *
+ * Given a string `s`, return *the longest palindromic substring in `s`*.
+ *
+ * @param {string} s
+ * @return {string}
+ */
+const longestPalindrome = (s) => {};
