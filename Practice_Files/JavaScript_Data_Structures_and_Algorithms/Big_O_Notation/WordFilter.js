23a5df4d9ea74b4318ebb46d75eec83c45d094d1
yuhcee <egbouchenna001@gmail.com>
2022-06-18 21:12:18 +0100

    docs(WordFilter):add task description

---
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/WordFilter.js
@@ -0,0 +1,15 @@
+/**
+ * **745. Prefix and Suffix Search**
+ * Design a special dictionary with some words that searchs the words in it by a prefix and a
+ * suffix.
+ *
+ * Implement the WordFilter class:
+ *
+ * - `WordFilter(string[] words)` Initializes the object with the `words` in the dictionary.
+ * - `f(string prefix, string suffix)` Returns *the index of the word in the dictionary*, which
+ * has the prefix `prefix` and the suffix `suffix`. If there is more than one valid index,
+ * return **the largest** of them. If there is no such word in the dictionary, return `-1`.
+ *
+ * @param {string[]} words
+ */
+const WordFilter = (words) => {};
