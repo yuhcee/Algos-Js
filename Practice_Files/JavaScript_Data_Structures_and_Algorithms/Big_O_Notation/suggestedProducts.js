24161a4534667f9526e53cdf58ca2da4acb8fdf4
yuhcee <egbouchenna001@gmail.com>
2022-06-19 22:55:19 +0100

    docs(suggestedProducts):add task description

---
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/suggestedProducts.js
@@ -0,0 +1,18 @@
+/**
+ * **1268. Search Suggestions System**
+ *
+ * You are given an array of strings `products` and a string `searchWord`.
+ *
+ * Design a system that suggests at most three product names from `products` after each character
+ * of `searchWord` is typed. Suggested products should have common prefix with `searchWord`. If
+ * there are more than three products with a common prefix return the three lexicographically
+ * minimums products.
+ *
+ * Return *a list of lists of the suggested products after each character of `searchWord` is
+ * typed*.
+ *
+ * @param {string[]} products
+ * @param {string} searchWord
+ * @return {string[][]}
+ */
+const suggestedProducts = (products, searchWord) => {};
