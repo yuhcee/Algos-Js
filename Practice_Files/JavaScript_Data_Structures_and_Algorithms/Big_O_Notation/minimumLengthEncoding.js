d89ce718c56be3fc940979ad34111dbc40c4e741
yuhcee <egbouchenna001@gmail.com>
2022-06-20 23:48:07 +0100

    chore(minimumLengthEncoding):iterate through word and get piece

--- Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/minimumLengthEncoding.js
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/minimumLengthEncoding.js
@@ -1,24 +1,26 @@
 /**
  * **820. Short Encoding of Words**
- * 
- * A **valid encoding** of an array of `words` is any reference string `s` and array of indices 
+ *
+ * A **valid encoding** of an array of `words` is any reference string `s` and array of indices
  * `indices` such that:
- * 
+ *
  * - `words.length == indices.length`
  * - The reference string `s` ends with the `'#'` character.
- * - For each index `indices[i]`, the **substring** of `s` starting from `indices[i]` and up to 
+ * - For each index `indices[i]`, the **substring** of `s` starting from `indices[i]` and up to
  * (but not including) the next `'#'` character is equal to `words[i]`.
- * 
- * Given an array of `words`, return *the **length of the shortest reference string** `s` 
+ *
+ * Given an array of `words`, return *the **length of the shortest reference string** `s`
  * possible of any **valid encoding** of `words`.
- * 
+ *
  * @param {string[]} words
  * @return {number}
  */
 const minimumLengthEncoding = (words) => {
     const set = new Set(words);
 
-    for(const word of words) {
-
+    for (const word of words) {
+        for (let i = 1; i < word.length; i++) {
+            const piece = word.slice(k);
+        }
     }
 };
