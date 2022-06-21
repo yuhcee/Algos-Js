98f0b4bd818f2a783de64a44821971af17c58e0a
yuhcee <egbouchenna001@gmail.com>
2022-06-21 23:43:33 +0100

    test(furthestBuilding):add test 1

--- Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/furthestBuilding.js
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/furthestBuilding.js
@@ -54,3 +54,53 @@ const add = (queue, num) => {
     }
     queue.push(num);
 };
+
+const heights = [4, 2, 7, 6, 9, 14, 12],
+    bricks = 5,
+    ladders = 1;
+// Output: 4;
+/* Explanation: Starting at building 0, you can follow these steps:
+- Go to building 1 without using ladders nor bricks since 4 >= 2.
+- Go to building 2 using 5 bricks. You must use either bricks or ladders because 2 < 7.
+- Go to building 3 without using ladders nor bricks since 7 >= 6.
+- Go to building 4 using your only ladder. You must use either bricks or ladders because 6 < 9.
+It is impossible to go beyond building 4 because you do not have any more bricks or ladders. */
+
+console.log(furthestBuilding(heights, bricks, ladders));
+// console.log(furthestBuilding(heights1, bricks1, ladders1));
+// console.log(furthestBuilding(heights2, bricks2, ladders2));
+/* var furthestBuilding = function(H, B, L) {
+    let len = H.length - 1, heap = [,]
+    const heapify = val => {
+        let i = heap.length, par = i >> 1, temp
+        heap.push(val)
+        while (heap[par] > heap[i]) {
+            temp = heap[par], heap[par] = heap[i], heap[i] = temp
+            i = par, par = i >> 1
+        }
+    }
+    const extract = () => {
+        if (heap.length === 1) return null
+        let top = heap[1], left, right, temp,
+            i = 1, child = heap[3] < heap[2] ? 3 : 2
+        if (heap.length > 2) heap[1] = heap.pop()
+        else heap.pop()
+        while (heap[i] > heap[child]) {
+            temp = heap[child], heap[child] = heap[i], heap[i] = temp
+            i = child, left = i << 1, right = left + 1
+            child = heap[right] < heap[left] ? right : left
+        }
+        return top
+    }    
+    for (let i = 0; i < len; i++) {
+        let diff = H[i+1] - H[i]
+        if (diff > 0) {
+            if (L > 0) heapify(diff), L--
+            else if (heap.length > 1 && diff > heap[1])
+                heapify(diff), B -= extract()
+            else B -= diff
+            if (B < 0) return i
+        }
+    }
+    return len
+}; */
