96e9071ef2d9cf9ec5674ec956ee43e4dbbb660a
yuhcee <egbouchenna001@gmail.com>
2022-06-22 23:14:45 +0100

    chore(findKthLargest): sort the nums array

--- Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/findKthLargest.js
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/findKthLargest.js
@@ -1,12 +1,15 @@
 /**
  * **215. Kth Largest Element in an Array**
- * 
+ *
  * Given an integer array `nums` and an integer `k`, return *the `kth` largest element in the array*.
- * 
+ *
  * Note that it is the `kth` largest element in the sorted order, not the `kth` distinct element.
- * 
+ *
  * @param {number[]} nums
  * @param {number} k
  * @return {number}
  */
-const findKthLargest = (nums, k) => {};
+const findKthLargest = (nums, k) => {
+    nums.sort((a, b) => a - b);
+
+};
