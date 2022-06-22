0eaf463fa1d50642e56c638c98223fce63f17981
yuhcee <egbouchenna001@gmail.com>
2022-06-22 23:13:02 +0100

    docs(findKthLargest): add task description

---
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/findKthLargest.js
@@ -0,0 +1,12 @@
+/**
+ * **215. Kth Largest Element in an Array**
+ * 
+ * Given an integer array `nums` and an integer `k`, return *the `kth` largest element in the array*.
+ * 
+ * Note that it is the `kth` largest element in the sorted order, not the `kth` distinct element.
+ * 
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+const findKthLargest = (nums, k) => {};
