cbff6abab2d3f60c6a7a119e84cc89f0bbc4e011
yuhcee <egbouchenna001@gmail.com>
2022-06-24 23:59:04 +0100

    test(isPossible):add test 1

--- Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/isPossible.js
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/isPossible.js
@@ -43,3 +43,12 @@ const isPossible = (target) => {
 
     return isPossible(target);
 };
+
+const target = [9, 3, 5];
+// Output: true;
+/* Explanation: Start with arr = [1, 1, 1] 
+[1, 1, 1], sum = 3 choose index 1
+[1, 3, 1], sum = 5 choose index 2
+[1, 3, 5], sum = 9 choose index 0
+[9, 3, 5] Done */
+console.log(isPossible(target));
