9c76e444d7c54f39706c998b4bf964e57a351adc
yuhcee <egbouchenna001@gmail.com>
2022-06-24 23:28:42 +0100

    docs(isPossible): add task description

---
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/isPossible.js
@@ -0,0 +1,16 @@
+/**
+ * **1354. Construct Target Array With Multiple Sums**
+ *
+ * You are given an array `target` of n integers. From a starting array `arr` consisting of `n` 1's, you may perform the
+ * following procedure :
+ *
+ * - let `x` be the sum of all elements currently in your array.
+ * - choose index `i`, such that `0 <= i < n` and set the value of `arr` at index `i` to `x`.
+ * - You may repeat this procedure as many times as needed.
+ *
+ * Return *`true` if it is possible to construct the `target` array from `arr`, otherwise, return `false`*.
+ *
+ * @param {number[]} target
+ * @return {boolean}
+ */
+const isPossible = (target) => {};
