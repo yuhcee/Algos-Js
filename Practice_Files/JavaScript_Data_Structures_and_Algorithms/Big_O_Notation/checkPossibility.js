67b326cf94f3c67509041ad0da3ffb893267a2fe
yuhcee <egbouchenna001@gmail.com>
2022-06-25 23:38:20 +0100

    docs(checkPossibility):add task description

---
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/checkPossibility.js
@@ -0,0 +1,12 @@
+/**
+ * **665. Non-decreasing Array**
+ *
+ * Given an array `nums` with `n` integers, your task is to check if it could become non-decreasing by modifying **at most one
+ * element**.
+ *
+ * We define an array is non-decreasing if `nums[i] <= nums[i + 1]` holds for every `i` *(0-based)* such that `(0 <= i <= n - 2)`.
+ *
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+const checkPossibility = (nums) => {};
