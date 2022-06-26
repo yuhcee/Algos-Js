baa9ee495232c5dd60dfff0487ecee8fe9940f96
yuhcee <egbouchenna001@gmail.com>
2022-06-26 19:06:41 +0100

    chore:(searchingChallenge):add solution using object

--- Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/searchingChallengeStrings.js
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/searchingChallengeStrings.js
@@ -10,6 +10,8 @@
  * have a value of `0` after being summed up.
  */
 
+// >>>>>>>>>>>>>>>>> Using Map <<<<<<<<<<<<<<<<<<<
+
 const SearchingChallenge = (strArr) => {
     // code goes here
     const map = new Map();
@@ -35,4 +37,33 @@ console.log(SearchingChallenge(strArr));
 
 const strArr1 = ['Z:0', 'A:-1'];
 console.log(SearchingChallenge(strArr1));
+// Output: A:-1
+
+// <=============== Using Object ================>
+
+const SearchingChallenge1 = (strArr) => {
+    const map = {};
+
+    for (let element of strArr) {
+        const [key, value] = element.split(':');
+
+        map[key] = map[key] !== undefined ? (map[key] += Number(value)) : Number(value);
+    }
+    const data = Object.keys(map)
+        .sort()
+        .map((key) => {
+            if (map[key] !== 0) {
+                return `${key}:${map[key]}`;
+            }
+        });
+
+    return data.filter((elem) => elem !== undefined).join(',');
+};
+
+const strArr2 = ['X:-1', 'Y:1', 'X:-4', 'B:3', 'X:5'];
+// Output: B:3,Y:1
+console.log(SearchingChallenge1(strArr2));
+
+const strArr3 = ['Z:0', 'A:-1'];
+console.log(SearchingChallenge1(strArr3));
 // Output: A:-1
