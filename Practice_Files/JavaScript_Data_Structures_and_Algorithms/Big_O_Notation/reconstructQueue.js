bde01403f4d28f628e18e4212c7a10bf88b97f34
yuhcee <egbouchenna001@gmail.com>
2022-06-29 09:23:11 +0100

    test(reconstructQueue):add test 1

--- Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/reconstructQueue.js
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/reconstructQueue.js
@@ -25,3 +25,22 @@ const reconstructQueue = (people) => {
 
     return result;
 };
+
+const people = [
+    [7, 0],
+    [4, 4],
+    [7, 1],
+    [5, 0],
+    [6, 1],
+    [5, 2],
+];
+// Output: [[5,0],[7,0],[5,2],[6,1],[4,4],[7,1]]
+/* Explanation:
+Person 0 has height 5 with no other people taller or the same height in front.
+Person 1 has height 7 with no other people taller or the same height in front.
+Person 2 has height 5 with two persons taller or the same height in front, which is person 0 and 1.
+Person 3 has height 6 with one person taller or the same height in front, which is person 1.
+Person 4 has height 4 with four people taller or the same height in front, which are people 0, 1, 2, and 3.
+Person 5 has height 7 with one person taller or the same height in front, which is person 1.
+Hence [[5,0],[7,0],[5,2],[6,1],[4,4],[7,1]] is the reconstructed queue. */
+console.log(reconstructQueue(people));
