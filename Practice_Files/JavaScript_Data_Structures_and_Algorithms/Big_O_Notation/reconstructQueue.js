bbe0d3841fbec758e83493d930300b0b668e92f0
yuhcee <egbouchenna001@gmail.com>
2022-06-29 09:16:45 +0100

    chore(reconstructQueue):sort people array

--- Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/reconstructQueue.js
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/reconstructQueue.js
@@ -12,4 +12,8 @@
  * @param {number[][]} people
  * @return {number[][]}
  */
-const reconstructQueue = (people) => {};
+const reconstructQueue = (people) => {
+    const result = [];
+    // sort people by height in descending order, if heights are same -> sort k by ascending order
+    people.sort((a, b) => (a[0] === b[0] ? a[1] - b[1] : b[0] - a[0]));
+};
