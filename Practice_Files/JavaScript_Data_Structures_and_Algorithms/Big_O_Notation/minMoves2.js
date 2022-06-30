478cd0982b52e1c35c408d09a8e3cabb7e8023cc
yuhcee <egbouchenna001@gmail.com>
2022-06-30 17:41:34 +0100

    docs(minMoves2):add task description

---
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/minMoves2.js
@@ -0,0 +1,14 @@
+/**
+ * **462. Minimum Moves to Equal Array Elements II**
+ *
+ * Given an integer array `nums` of size `n`, return *the minimum number of moves required to make all
+ * array elements equal*.
+ *
+ * In one move, you can increment or decrement an element of the array by `1`.
+ *
+ * Test cases are designed so that the answer will fit in a **32-bit** integer.
+ *
+ * @param {number[]} nums
+ * @return {number}
+ */
+const minMoves2 = (nums) => {};
