d29641b3bd269851961e58867940848503ddefbe
yuhcee <egbouchenna001@gmail.com>
2022-06-30 17:48:36 +0100

    test(minMoves2):add test 1

--- Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/minMoves2.js
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/minMoves2.js
@@ -24,3 +24,9 @@ const minMoves2 = (nums) => {
 
     return operations;
 };
+const nums = [1, 2, 3];
+// Output: 2
+/* Explanation:
+Only two moves are needed (remember each move increments or decrements one element):
+[1,2,3]  =>  [2,2,3]  =>  [2,2,2] */
+console.log(minMoves2(nums));
