a41af7439a70d62c9263f69b8f6fe4320ca62e58
yuhcee <egbouchenna001@gmail.com>
2022-07-04 18:34:36 +0100

    test(wiggleMaxLength):add test 2

--- Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/wiggleMaxLength.js
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/wiggleMaxLength.js
@@ -42,3 +42,9 @@ const nums = [1, 7, 4, 9, 2, 5];
 // Output: 6
 // Explanation: The entire sequence is a wiggle sequence with differences (6, -3, 5, -7, 3).
 console.log(wiggleMaxLength(nums));
+
+const nums2 = [1, 17, 5, 10, 13, 15, 10, 5, 16, 8];
+// Output: 7
+/* Explanation: There are several subsequences that achieve this length.
+One is [1, 17, 10, 13, 10, 16, 8] with differences (16, -7, 3, -3, 6, -8). */
+console.log(wiggleMaxLength(nums2));
