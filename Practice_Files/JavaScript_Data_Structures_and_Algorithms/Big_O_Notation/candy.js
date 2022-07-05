60066e8cb8ca11159f92bc4c443ca66170f54c33
yuhcee <egbouchenna001@gmail.com>
2022-07-05 17:43:29 +0100

    docs(candy):add task description

---
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/candy.js
@@ -0,0 +1,16 @@
+/**
+ * **
+ * 135. Candy**
+ * There are `n` children standing in a line. Each child is assigned a rating value given in the integer array `ratings`.
+ *
+ * You are giving candies to these children subjected to the following requirements:
+ *
+ * - Each child must have at least one candy.
+ * - Children with a higher rating get more candies than their neighbors.
+ *
+ * Return *the minimum number of candies you need to have to distribute the candies to the children*.
+ *
+ * @param {number[]} ratings
+ * @return {number}
+ */
+const candy = (ratings) => {};
