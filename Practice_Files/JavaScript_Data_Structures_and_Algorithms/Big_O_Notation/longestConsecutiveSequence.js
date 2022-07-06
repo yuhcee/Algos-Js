a1bed872413cd45b960974663b5f798a7c1cc472
yuhcee <egbouchenna001@gmail.com>
2022-07-06 14:59:41 +0100

    chore(longestConsecutiveSequence):take care of empty array

--- Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/longestConsecutiveSequence.js
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/longestConsecutiveSequence.js
@@ -1,11 +1,14 @@
 /**
  * **128. Longest Consecutive Sequence**
  *
- * Given an unsorted array of integers nums, return the length of the longest consecutive elements sequence.
+ * Given an unsorted array of integers `nums`, return the length of the longest consecutive elements sequence.
  *
- * You must write an algorithm that runs in O(n) time.
+ * You must write an algorithm that runs in `O(n)` time.
  *
  * @param {number[]} nums
  * @return {number}
  */
-const longestConsecutive = (nums) => {};
+const longestConsecutive = (nums) => {
+    // take care of empty array
+    if (nums.length < 1) return nums.length;
+};
