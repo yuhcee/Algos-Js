6694648d36ba4b6a84a3b6248f9e87ef8007d468
yuhcee <egbouchenna001@gmail.com>
2022-07-06 15:14:56 +0100

    test(longestConsecutiveSequence):add test 2

--- Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/longestConsecutiveSequence.js
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/longestConsecutiveSequence.js
@@ -41,3 +41,7 @@ const nums = [100, 4, 200, 1, 3, 2];
 // Output: 4
 // Explanation: The longest consecutive elements sequence is [1, 2, 3, 4]. Therefore its length is 4.
 console.log(longestConsecutive(nums));
+
+const nums1 = [0, 3, 7, 2, 5, 8, 4, 6, 0, 1];
+// Output: 9
+console.log(longestConsecutive(nums1));
