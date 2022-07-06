25fdd0639b801962cb1c25aeb67aaf46132179d0
yuhcee <egbouchenna001@gmail.com>
2022-07-06 14:56:55 +0100

    docs(longestConsecutiveSequence):add task description

---
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/longestConsecutiveSequence.js
@@ -0,0 +1,11 @@
+/**
+ * **128. Longest Consecutive Sequence**
+ *
+ * Given an unsorted array of integers nums, return the length of the longest consecutive elements sequence.
+ *
+ * You must write an algorithm that runs in O(n) time.
+ *
+ * @param {number[]} nums
+ * @return {number}
+ */
+const longestConsecutive = (nums) => {};
