74a68dfeb42469e72d00ea1cc6a6cdc159982c2b
yuhcee <egbouchenna001@gmail.com>
2022-07-08 09:12:55 +0100

    feat(isInterleave):add dp recursive solution

--- Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/isInterleave.js
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/isInterleave.js
@@ -3,7 +3,7 @@
  *
  * Given strings `s1`, `s2`, and `s3`, find whether `s3` is formed by an **interleaving** of `s1` and `s2`.
  *
- * An **interleaving** of two strings `s` and `t` is a configuration where they are divided into 
+ * An **interleaving** of two strings `s` and `t` is a configuration where they are divided into
  * **non-empty** substrings such that:
  *
  * - s = s1 + s2 + ... + sn
@@ -20,5 +20,23 @@
  */
 
 const isInterleave = (s1, s2, s3) => {
+    const dp = new Map();
 
+    const solve = (a = 0, b = 0, c = 0) => {
+        if (c === s3.length) return a === s1.length && b === s2.length;
+
+        const key = `${a},${b},${c}`;
+
+        if (dp.has(key)) return dp.get(key);
+
+        let takeS1 = false,
+            takeS2 = false;
+        if (s1[a] === s3[c]) takeS1 = solve(a + 1, b, c + 1);
+        if (s2[b] === s3[c]) takeS1 = solve(a, b + 1, c + 1);
+
+        dp.set(key, takeS1 || takeS2);
+        return takeS1 || takeS2;
+    };
+
+    return solve();
 };
