ef85eb805e7d637f597a6251710a43d53dcf28f3
yuhcee <egbouchenna001@gmail.com>
2022-07-11 16:18:52 +0100

    test(minCostClimbingStairs): add test 2

--- Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/minCostClimbingStairs.js
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/minCostClimbingStairs.js
@@ -37,3 +37,16 @@ const cost = [10, 15, 20];
 The total cost is 15. */
 
 console.log(minCostClimbingStairs(cost));
+
+const cost1 = [1, 100, 1, 1, 1, 100, 1, 1, 100, 1];
+// Output: 6
+/* Explanation: You will start at index 0.
+- Pay 1 and climb two steps to reach index 2.
+- Pay 1 and climb two steps to reach index 4.
+- Pay 1 and climb two steps to reach index 6.
+- Pay 1 and climb one step to reach index 7.
+- Pay 1 and climb two steps to reach index 9.
+- Pay 1 and climb one step to reach the top.
+The total cost is 6.
+ */
+console.log(minCostClimbingStairs(cost1));
