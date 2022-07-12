c1587cfb05150aa5223d833bfb9c7520a2f5e24c
yuhcee <egbouchenna001@gmail.com>
2022-07-12 16:50:33 +0100

    docs(rightSideView):add task description

---
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/rightSideView.js
@@ -0,0 +1,24 @@
+/**
+ *
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val === undefined ? 0 : val )
+ *     this.left = (left === undefined ? null : left )
+ *     this.right = (right === undefined ? null : right )
+ * }
+ *
+ */
+
+/**
+ * **199. Binary Tree Right Side View**
+ *
+ * Given the `root` of a binary tree, imagine yourself standing on the **right side** of it, return
+ * *the values of the nodes you can see ordered from top to bottom*.
+ *
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+
+var rightSideView = (root) => {
+    
+};
