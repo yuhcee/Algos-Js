4840884f7eab38095bb58553046f8323a9003c42
yuhcee <egbouchenna001@gmail.com>
2022-07-13 18:25:30 +0100

    chore(makesquare):get perimeter

--- Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/makesquare.js
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/makesquare.js
@@ -1,14 +1,17 @@
 /**
  * **473. Matchsticks to Square**
- * 
- * You are given an integer array `matchsticks` where `matchsticks[i]` is the length of the `ith` 
- * matchstick. You want to use **all the matchsticks** to make one square. You **should not break** 
+ *
+ * You are given an integer array `matchsticks` where `matchsticks[i]` is the length of the `ith`
+ * matchstick. You want to use **all the matchsticks** to make one square. You **should not break**
  * any stick, but you can link them up, and each matchstick must be used **exactly one time**.
- * 
+ *
  * Return *`true` if you can make this square and `false` otherwise*.
- * 
+ *
  * @param {number[]} matchsticks
  * @return {boolean}
  */
 const makesquare = (matchsticks) => {
+    // get perimeter
+    const perimeter = matchsticks.reduce((a, b) => a + b);
+
 };
