8de0fbb5daa094e37ddf4475a60e1acc6b66230b
yuhcee <egbouchenna001@gmail.com>
2022-07-15 00:54:26 +0100

    docs(maxAreaOfIsland):add task description

---
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/maxAreaOfIsland.js
@@ -0,0 +1,15 @@
+/**
+ * **695. Max Area of Island**
+ *
+ * You are given an `m x n` binary matrix `grid`. An island is a group of `1`'s (representing land)
+ * connected `4-directionally` (horizontal or vertical.) You may assume all four edges of the grid are
+ * surrounded by water.
+ *
+ * The area of an island is the number of cells with a value 1 in the island.
+ *
+ * Return *the maximum **area** of an island in `grid`. If there is no island, return `0`*.
+ *
+ * @param {number[][]} grid
+ * @return {number}
+ */
+const maxAreaOfIsland = function (grid) {};
