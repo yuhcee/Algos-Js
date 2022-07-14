6576ccdb32a3b41a8a4fee49fad51404244035c1
yuhcee <egbouchenna001@gmail.com>
2022-07-14 09:54:33 +0100

    docs(levelOrder):add task description

---
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/levelOrder.js
@@ -0,0 +1,20 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * **102. Binary Tree Level Order Traversal**
+ * 
+ * Given the `root` of a binary tree, return *the level order traversal of its nodes' values*. (i.e., 
+ * from left to right, level by level).
+ * 
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+const levelOrder = (root) => {
+    
+};
