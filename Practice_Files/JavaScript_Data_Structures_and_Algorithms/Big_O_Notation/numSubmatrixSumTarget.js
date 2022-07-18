29402e4ca4410fffcb46b8d03fb457c71f2985ed
yuhcee <egbouchenna001@gmail.com>
2022-07-18 11:39:49 +0100

    feat(numSubmatrixSumTarget):add solution 2

--- Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/numSubmatrixSumTarget.js
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/numSubmatrixSumTarget.js
@@ -87,3 +87,30 @@ const matrix2 = [[904]],
     target2 = 0;
 // Output: 0
 console.log(numSubmatrixSumTarget(matrix2, target2));
+
+// ================== ANOTHER SOLUTION =======================
+var numSubmatrixSumTarget2 = function (matrix, target) {
+    let row = matrix.length,
+        col = matrix[0].length;
+
+    for (let i = 1; i < row; i++) {
+        for (let j = 0; j < col; j++) {
+            matrix[i][j] += matrix[i - 1][j];
+        }
+    }
+
+    let ans = 0;
+    for (let i = 0; i < row; i++) {
+        for (let j = i; j < row; j++) {
+            let map = new Map(),
+                sum = 0;
+            map.set(0, 1);
+            for (let k = 0; k < col; k++) {
+                sum += matrix[j][k] - (i != 0 ? matrix[i - 1][k] : 0);
+                ans += map.get(sum - target) || 0;
+                map.set(sum, (map.get(sum) || 0) + 1);
+            }
+        }
+    }
+    return ans;
+};
