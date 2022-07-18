88d506d4b9c2c1e2f88cb0e948947d6004780685
yuhcee <egbouchenna001@gmail.com>
2022-07-18 10:35:09 +0100

    chore(numSubmatrixSumTarget):Initialize the result: count

--- Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/numSubmatrixSumTarget.js
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/numSubmatrixSumTarget.js
@@ -18,4 +18,19 @@ const numSubmatrixSumTarget = function (matrix, target) {
     // compute number of rows and columns
     const r = matrix.length,
         c = matrix[0].length;
+
+    /* Compute 2D prefix sum ps. To simplify the code, we allocate one more row and one more column, reserving row 0 and column 0 for zero values. This way, we avoid computing the first row and the first column separately. */
+    const ps = Array(r + 1)
+        .fill()
+        .map((_) => Array(c + 1).fill(0));
+
+    for (let i = 1; i < r + 1; ++i) {
+        for (let j = 1; j < c + 1; ++j) {
+            ps[i][j] = ps[i - 1][j] + ps[i][j - 1] - ps[i - 1][j - 1] + matrix[i - 1][j - 1];
+        }
+    }
+
+    // Initialize the result: `count = 0`.
+    let count = 0,
+        currSum;
 };
