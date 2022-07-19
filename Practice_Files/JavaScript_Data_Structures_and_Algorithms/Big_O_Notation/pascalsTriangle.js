73d382b795c47186e13fc7ef94ec92a16a769e49
yuhcee <egbouchenna001@gmail.com>
2022-07-19 07:27:48 +0100

    chore(pascalsTriangle):initialize base case

--- Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/pascalsTriangle.js
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/pascalsTriangle.js
@@ -3,10 +3,13 @@
  *
  * Given an integer `numRows`, return the first numRows of **Pascal's triangle**.
  *
- * In **Pascal's triangle**, each number is the sum of the two numbers directly above 
+ * In **Pascal's triangle**, each number is the sum of the two numbers directly above
  * it as shown:
  *
  * @param {number} numRows
  * @return {number[][]}
  */
-const generate = (numRows) => {};
+const generate = (numRows) => {
+    // initialize triangle with base case, first row is always 1
+    const triangle = [[1]];
+};
