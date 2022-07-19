a0c99550c59a8c31146c933def3711b076c882cb
yuhcee <egbouchenna001@gmail.com>
2022-07-19 07:24:45 +0100

    docs(pascalsTriangle):add task description

---
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/pascalsTriangle.js
@@ -0,0 +1,12 @@
+/**
+ * **118. Pascal's Triangle**
+ *
+ * Given an integer `numRows`, return the first numRows of **Pascal's triangle**.
+ *
+ * In **Pascal's triangle**, each number is the sum of the two numbers directly above 
+ * it as shown:
+ *
+ * @param {number} numRows
+ * @return {number[][]}
+ */
+const generate = (numRows) => {};
