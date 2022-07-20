650370a1052d9983dd979005b300c6bdc13c94b2
yuhcee <egbouchenna001@gmail.com>
2022-07-20 13:58:31 +0100

    docs(numMatchingSubseq):add task description

---
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/numMatchingSubseq.js
@@ -0,0 +1,18 @@
+/**
+ * **792. Number of Matching Subsequences**
+ *
+ * Given a string `s` and an array of strings `words`, return *the number of `words[i]` that is a
+ * subsequence of `s`*.
+ *
+ * A **subsequence** of a string is a new string generated from the original string with some
+ * characters (can be none) deleted without changing the relative order of the remaining characters.
+ *
+ * - For example, "ace" is a subsequence of "abcde".
+ *
+ * @param {string} s
+ * @param {string[]} words
+ * @return {number}
+ */
+const numMatchingSubseq =  (s, words) =>{
+    
+};
