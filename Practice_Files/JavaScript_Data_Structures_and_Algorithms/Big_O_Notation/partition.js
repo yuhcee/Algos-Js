bb9e434d7a9a9ce9613a11ac5f49cc56467c6745
yuhcee <egbouchenna001@gmail.com>
2022-07-22 13:26:58 +0100

    docs(partition):add task description

---
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/partition.js
@@ -0,0 +1,21 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * **86. Partition List**
+ *
+ * Given the `head` of a linked list and a value `x`, partition it such that all
+ * nodes **less than** `x` come before nodes **greater than or equal** to `x`.
+ *
+ * You should **preserve** the original relative order of the nodes in each of the
+ * two partitions.
+ *
+ * @param {ListNode} head
+ * @param {number} x
+ * @return {ListNode}
+ */
+const partition = (head, x) => {};
