c686f20de5f3a43569100f425230e4087d819680
yuhcee <egbouchenna001@gmail.com>
2022-07-27 10:54:57 +0100

    docs(flatten):add task description

---
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/flatten.js
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * **114. Flatten Binary Tree to Linked List**
+ *
+ * Given the `root` of a binary tree, flatten the tree into a "linked list":
+ *
+ * - The "linked list" should use the same `TreeNode` class where the `right` child
+ * pointer points to the next node in the list and the `left` child pointer is always
+ * `null`.
+ *
+ * - The "linked list" should be in the same order as a **pre-order traversal** of
+ * the binary tree.
+ *
+ * @param {TreeNode} root
+ * @return {void} Do not return anything, modify root in-place instead.
+ */
+const flatten = (root) => {
+};
