422c116cc4c51981dca1d71c0e568b9668d61f5f
yuhcee <egbouchenna001@gmail.com>
2022-08-01 12:52:09 +0100

    chore(kthSmallest):add solution

--- Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/kthSmallest.js
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/kthSmallest.js
@@ -1,15 +1,17 @@
 /**
  * 378. Kth Smallest Element in a Sorted Matrix
- * 
- * Given an `n x n` matrix where each of the rows and columns is sorted in ascending order, return the `kth` 
- * smallest element in the matrix.
- * 
+ *
+ * Given an `n x n` matrix where each of the rows and columns is sorted in ascending order, return *the
+ * `kth` smallest element in the matrix*.
+ *
  * Note that it is the `kth` smallest element **in the sorted order**, not the `kth` **distinct** element
- * 
+ *
  * You must find a solution with a memory complexity better than `O(n2)`.
- * 
+ *
  * @param {number[][]} matrix
  * @param {number} k
  * @return {number}
  */
-const kthSmallest = function (matrix, k) {};
+const kthSmallest = function (matrix, k) {
+    return matrix.flat().sort((a, b) => a - b)[k - 1];
+};
