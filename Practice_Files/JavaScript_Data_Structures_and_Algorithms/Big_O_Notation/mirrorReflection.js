8e42beadd5d88e7016da721143dcf705b5d463b6
yuhcee <egbouchenna001@gmail.com>
2022-08-02 02:01:46 +0100

    chore(mirrorReflection):add utility function

--- Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/mirrorReflection.js
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/mirrorReflection.js
@@ -1,19 +1,21 @@
 /**
  * **858. Mirror Reflection**
- * 
- * There is a special square room with mirrors on each of the four walls. Except for the southwest corner, 
+ *
+ * There is a special square room with mirrors on each of the four walls. Except for the southwest corner,
  * there are receptors on each of the remaining corners, numbered `0`, `1`, and `2`.
- * 
- * The square room has walls of length `p` and a laser ray from the southwest corner first meets the east 
+ *
+ * The square room has walls of length `p` and a laser ray from the southwest corner first meets the east
  * wall at a distance `q` from the `0th` receptor.
- * 
+ *
  * Given the two integers `p` and `q`, return *the number of the receptor that the ray meets first*.
- * 
+ *
  * The test cases are guaranteed so that the ray will meet a receptor eventually.
- * 
+ *
  * @param {number} p
  * @param {number} q
  * @return {number}
  */
+
+const f = (a, b) => (b ? f(b, a % b) : a);
 
 var mirrorReflection = function (p, q) {};
