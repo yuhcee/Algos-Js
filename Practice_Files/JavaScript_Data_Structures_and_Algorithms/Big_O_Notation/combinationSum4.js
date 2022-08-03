64bf634b250d37ae04a8c58201f5852720a7a232
yuhcee <egbouchenna001@gmail.com>
2022-08-03 02:12:54 +0100

    test(combinationSum4):add test case 1

--- Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/combinationSum4.js
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/combinationSum4.js
@@ -30,3 +30,17 @@ var combinationSum4 = function (nums, target) {
     };
     return solve();
 };
+const nums = [1, 2, 3],
+    target = 4;
+/* Output: 7
+Explanation:
+The possible combination ways are:
+(1, 1, 1, 1)
+(1, 1, 2)
+(1, 2, 1)
+(1, 3)
+(2, 1, 1)
+(2, 2)
+(3, 1)
+Note that different sequences are counted as different combinations. */
+console.log(combinationSum4(nums, target));
