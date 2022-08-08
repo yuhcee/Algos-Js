f118d526a410071f167e316277261630f940949b
yuhcee <egbouchenna001@gmail.com>
2022-08-08 03:37:22 +0100

    docs(sortedArrayToBST):add task description

---
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/sortedArrayToBST.js
@@ -0,0 +1,21 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * **108. Convert Sorted Array to Binary Search Tree**
+ *
+ * Given an integer array `nums` where the elements are sorted in **ascending order**, convert it to a
+ * ***height-balanced** binary search tree*.
+ *
+ * A **height-balanced** binary tree is a binary tree in which the depth of the two subtrees of every node
+ * never differs by more than one.
+ *
+ * @param {number[]} nums
+ * @return {TreeNode}
+ */
+var sortedArrayToBST = function (nums) {};
