2bc4643b4826ddc6e4d1f5c4d794770983501bc8
yuhcee <egbouchenna001@gmail.com>
2022-08-08 03:40:01 +0100

    test(sortedArrayToBST):add test case 1

--- Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/sortedArrayToBST.js
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/sortedArrayToBST.js
@@ -36,3 +36,7 @@ var sortedArrayToBST = function (nums) {
 
     return create(0, nums.length - 1);
 };
+const nums = [-10, -3, 0, 5, 9];
+// Output: [0,-3,9,-10,null,5]
+// Explanation: [0,-10,5,null,-3,null,9] is also accepted:
+console.log(sortedArrayToBST(nums));
