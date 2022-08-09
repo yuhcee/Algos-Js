24067608c6d51f1c1d9573b9146c0995cc778315
yuhcee <egbouchenna001@gmail.com>
2022-08-09 03:46:02 +0100

    docs(isValidBST):add task description

---
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/isValidBST.js
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * **98. Validate Binary Search Tree**
+ *
+ * Given the `root` of a binary tree, *determine if it is a valid binary search tree (BST)*.
+ *
+ * A **valid BST** is defined as follows:
+ *
+ * - The left subtree of a node contains only nodes with keys **less than** the node's key.
+ * - The right subtree of a node contains only nodes with keys **greater than** the node's key.
+ * - Both the left and right subtrees must also be binary search trees.
+ *
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isValidBST = function (root) {};
