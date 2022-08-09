6f2e2b34932d653c00d1e5821067e4734ce5786d
yuhcee <egbouchenna001@gmail.com>
2022-08-09 03:49:50 +0100

    test(isValidBST):add test case 2

--- Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/isValidBST.js
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/isValidBST.js
@@ -37,3 +37,7 @@ const root = [5, 1, 4, null, null, 3, 6];
 // Output: false
 // Explanation: The root node's value is 5 but its right child's value is 4.
 console.log(isValidBST(root));
+
+const root1 = [2, 1, 3];
+// Output: true
+console.log(isValidBST(root1));
