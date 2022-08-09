e5e8beda8fbf9fce86e6a89931477f13255101f3
yuhcee <egbouchenna001@gmail.com>
2022-08-09 03:47:20 +0100

    chore(isValidBST):implement solution

--- Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/isValidBST.js
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/isValidBST.js
@@ -20,4 +20,15 @@
  * @param {TreeNode} root
  * @return {boolean}
  */
-var isValidBST = function (root) {};
+var isValidBST = function (root) {
+    const isValid = validate(root, -Infinity, Infinity);
+
+    return isValid;
+
+    function validate(node, low, high) {
+        if (node === null) return true;
+        if ((low !== null && node.val <= low) || (high !== null && node.val >= high)) return false;
+
+        return validate(node.left, low, node.val) && validate(node.right, node.val, high);
+    }
+};
