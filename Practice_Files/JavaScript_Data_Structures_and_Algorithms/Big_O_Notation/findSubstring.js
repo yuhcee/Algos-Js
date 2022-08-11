643226687148ca1c833f58e3f5d9c5801a2e531c
yuhcee <egbouchenna001@gmail.com>
2022-08-11 22:28:47 +0100

    test(findSubstring):add test case 3

--- Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/findSubstring.js
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/findSubstring.js
@@ -78,10 +78,20 @@ The substring starting at 9 is "foobar". It is the concatenation of ["foo","bar"
 The output order does not matter. Returning [9,0] is fine too. */
 console.log(findSubstring(s, words));
 
-const s1 = "wordgoodgoodgoodbestword", words1 = ["word","good","best","word"]
+const s1 = 'wordgoodgoodgoodbestword',
+    words1 = ['word', 'good', 'best', 'word'];
 // Output: []
 /* Explanation: Since words.length == 4 and words[i].length == 4, the concatenated substring has to be of length 16.
 There is no substring of length 16 is s that is equal to the concatenation of any permutation of words.
 We return an empty array. */
 
 console.log(findSubstring(s1, words1));
+
+const s2 = 'barfoofoobarthefoobarman',
+    words2 = ['bar', 'foo', 'the'];
+// Output: [6,9,12]
+/* Explanation: Since words.length == 3 and words[i].length == 3, the concatenated substring has to be of length 9.
+The substring starting at 6 is "foobarthe". It is the concatenation of ["foo","bar","the"] which is a permutation of words.
+The substring starting at 9 is "barthefoo". It is the concatenation of ["bar","the","foo"] which is a permutation of words.
+The substring starting at 12 is "thefoobar". It is the concatenation of ["the","foo","bar"] which is a permutation of words. */
+console.log(findSubstring(s2, words2));
