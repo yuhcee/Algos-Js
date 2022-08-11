a946ee6cc6e795b734f0cf65d859cb4070bbeebc
yuhcee <egbouchenna001@gmail.com>
2022-08-11 22:23:26 +0100

    chore(findSubstring):add search function logic

--- Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/findSubstring.js
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/findSubstring.js
@@ -19,8 +19,30 @@
  * @return {number[]}
  */
 
- var wordLength, totalCount;
+var wordLength, totalCount;
 
- let search = function(s, wordCounts, i, used, matched) {}
+let search = function (s, wordCounts, i, used, matched) {
+    if (matched == totalCount) return true;
+    if (i >= s.length) return false;
+
+    var j,
+        testStr = s.substr(i, wordLength);
+
+    if (wordCounts.has(testStr)) {
+        // we found the test...
+        if (!used.has(testStr)) {
+            // we haven't found it before
+            used.set(testStr, 1);
+        } else if (used.get(testStr) < wordCounts.get(testStr)) {
+            // we have found it before, but we still have matches left
+            used.set(testStr, used.get(testStr) + 1);
+        } else {
+            // we haven't any matches left for that word
+            return false;
+        }
+        return search(s, wordCounts, i + wordLength, used, matched + 1); // continue searching
+    }
+    return false;
+};
 
 const findSubstring = function (s, words) {};
