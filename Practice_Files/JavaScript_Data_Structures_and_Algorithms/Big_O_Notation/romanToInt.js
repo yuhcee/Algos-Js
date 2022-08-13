3b9a96557a79101af71da01f9ac1070bffdc7915
yuhcee <egbouchenna001@gmail.com>
2022-08-13 22:54:43 +0100

    chore(romanToInt):add intMap

--- Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/romanToInt.js
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/romanToInt.js
@@ -20,4 +20,14 @@
  * @param {string} s
  * @return {number}
  */
-var romanToInt = (s) => {};
+var romanToInt = (s) => {
+    const intMap = {
+        I: 1,
+        V: 5,
+        X: 10,
+        L: 50,
+        C: 100,
+        D: 500,
+        M: 1000,
+    };
+};
