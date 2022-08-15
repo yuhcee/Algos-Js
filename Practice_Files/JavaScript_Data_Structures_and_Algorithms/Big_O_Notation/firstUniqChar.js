a17d3bc37bea3c148d09d27cccb92c02b79b81e0
yuhcee <egbouchenna001@gmail.com>
2022-08-15 23:18:51 +0100

    chore(firstUniqChar):implement solution

--- Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/firstUniqChar.js
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/firstUniqChar.js
@@ -9,8 +9,11 @@
  */
 const firstUniqChar = function (s) {
     for (let i = 0, n = s.length; i < n; i++) {
-       
+        const char = s[i];
+        
+        if (s.indexOf(char) === s.lastIndexOf(char))
+            return i;
     }
     
-
+    return -1;
 };
