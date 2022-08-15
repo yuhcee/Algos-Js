cfe0b06821f8238373136e7e50747138b06bb166
yuhcee <egbouchenna001@gmail.com>
2022-08-15 23:19:38 +0100

    test(firstUniqChar):add testcase 1

--- Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/firstUniqChar.js
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/firstUniqChar.js
@@ -1,19 +1,22 @@
 /**
  * **387. First Unique Character in a String**
- * 
- * Given a string `s`, *find the first non-repeating character in it and return its index*. If it does not 
+ *
+ * Given a string `s`, *find the first non-repeating character in it and return its index*. If it does not
  * exist, return -1.
- * 
+ *
  * @param {string} s
  * @return {number}
  */
 const firstUniqChar = function (s) {
     for (let i = 0, n = s.length; i < n; i++) {
         const char = s[i];
-        
-        if (s.indexOf(char) === s.lastIndexOf(char))
-            return i;
+
+        if (s.indexOf(char) === s.lastIndexOf(char)) return i;
     }
-    
+
     return -1;
 };
+
+const s = 'leetcode';
+// Output: 0
+console.log(firstUniqChar(s));
