776581c520226dd425bfafc8145d3e3ff147f9f2
yuhcee <egbouchenna001@gmail.com>
2022-08-15 23:17:41 +0100

    docs(firstUniqChar):add task description

---
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/firstUniqChar.js
@@ -0,0 +1,12 @@
+/**
+ * **387. First Unique Character in a String**
+ * 
+ * Given a string `s`, *find the first non-repeating character in it and return its index*. If it does not 
+ * exist, return -1.
+ * 
+ * @param {string} s
+ * @return {number}
+ */
+const firstUniqChar = function (s) {
+    
+};
