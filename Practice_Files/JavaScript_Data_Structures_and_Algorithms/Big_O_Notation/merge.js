58abdb0ec53baf7e96b603fafdd3e6c3f6bb41f8
yuhcee <egbouchenna001@gmail.com>
2022-08-18 11:28:47 +0100

    refactor:restructure files on topics

--- Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/merge.js
+++
@@ -1,8 +0,0 @@
-function merge(root) {
-  for (var i = 1; i < arguments.length; i++)
-    for (var key in arguments[i]) root[key] = arguments[i][key];
-  return root;
-}
-
-var merged = merge({ name: 'John' }, { city: 'Boston' });
-console.log(merged);
