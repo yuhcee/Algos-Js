5e4ea77fe26cfa85fdbb697f2cc1b6d11edb36fd
yuhcee <egbouchenna001@gmail.com>
2022-08-22 10:36:58 +0100

    chore(isPowerOfFour):add iterative solution

--- Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/isPowerOfFour.js
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/isPowerOfFour.js
@@ -1,10 +1,14 @@
 /**
  * **342. Power of Four**
- * 
+ *
  * Given an integer `n`, return *`true` if it is a power of four. Otherwise, return false*.
  * An integer `n` is a power of four, if there exists an integer `x` such that `n == 4x`.
  *
  * @param {number} n
  * @return {boolean}
  */
-var isPowerOfFour = function (n) {};
+const isPowerOfFour = function (n) {
+    if (n === 0) return false;
+    while (n % 4 === 0) n /= 4;
+    return n === 1;
+};
