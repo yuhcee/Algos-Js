31b68c83c7fd96bfae070abba489fe431cbe9cac
yuhcee <egbouchenna001@gmail.com>
2022-08-23 11:44:41 +0100

    docs(isPalindromeLinkedList):add task description

---
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/isPalindromeLinkedList.js
@@ -0,0 +1,16 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * **234. Palindrome Linked List**
+ * 
+ * Given the `head` of a singly linked list, return *`true` if it is a palindrome*.
+ * 
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var isPalindromeLinkedList = function (head) {};
