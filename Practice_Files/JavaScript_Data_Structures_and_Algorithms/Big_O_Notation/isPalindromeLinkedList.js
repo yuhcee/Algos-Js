11b2a0d4b28af417d5b0699de6d2cbe97c7f1bf7
yuhcee <egbouchenna001@gmail.com>
2022-08-23 11:46:24 +0100

    chore(isPalindromeLinkedList):init an empty array to store values

--- Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/isPalindromeLinkedList.js
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/isPalindromeLinkedList.js
@@ -7,10 +7,14 @@
  */
 /**
  * **234. Palindrome Linked List**
- * 
+ *
  * Given the `head` of a singly linked list, return *`true` if it is a palindrome*.
- * 
+ *
  * @param {ListNode} head
  * @return {boolean}
  */
-var isPalindromeLinkedList = function (head) {};
+var isPalindromeLinkedList = function (head) {
+    // init an empty array to store values
+    const vals = [];
+    
+};
