3ad4d3c3b81e54e37ca9f04faa995de4d39c1811
yuhcee <egbouchenna001@gmail.com>
2022-08-24 06:17:18 +0100

    docs(isPowerOfThree):add task description

---
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/isPowerOfThree.js
@@ -0,0 +1,11 @@
+/**
+ * **326. Power of Three**
+ *
+ * Given an integer `n`, return *`true` if it is a power of three. Otherwise, return false*.
+ *
+ * An integer n is a power of three, if there exists an integer `x` such that `n == 3x`.
+ *
+ * @param {number} n
+ * @return {boolean}
+ */
+const isPowerOfThree = function (n) {};
