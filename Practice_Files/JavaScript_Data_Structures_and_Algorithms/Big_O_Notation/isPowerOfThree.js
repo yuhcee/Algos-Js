076ac703dd4a8c170a53ddb2f919fb1cc3ad1616
yuhcee <egbouchenna001@gmail.com>
2022-08-24 06:18:59 +0100

    chore(isPowerOfThree):add loop iteration solution

--- Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/isPowerOfThree.js
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/isPowerOfThree.js
@@ -8,4 +8,8 @@
  * @param {number} n
  * @return {boolean}
  */
-const isPowerOfThree = function (n) {};
+const isPowerOfThree = function (n) {
+    if (n === 0) return false;
+    while (n % 3 === 0) n /= 3;
+    return n === 1;
+};
