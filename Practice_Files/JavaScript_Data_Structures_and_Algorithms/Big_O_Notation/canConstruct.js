21f2f1ac51e243e5276399fa87f767e04a60e289
yuhcee <egbouchenna001@gmail.com>
2022-08-25 11:10:06 +0100

    docs(canConstruct):add task description

---
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/canConstruct.js
@@ -0,0 +1,13 @@
+/**
+ * **383. Ransom Note**
+ * 
+ * Given two strings `ransomNote` and `magazine`, return *`true` if `ransomNote` can be constructed by using 
+ * the letters from `magazine` and false otherwise*.
+ * 
+ * Each letter in `magazine` can only be used once in `ransomNote`.
+ * 
+ * @param {string} ransomNote
+ * @param {string} magazine
+ * @return {boolean}
+ */
+const canConstruct = function (ransomNote, magazine) {};
