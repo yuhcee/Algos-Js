913f0040ee66a8aec5eb73fd76de4f64a1002c5d
yuhcee <egbouchenna001@gmail.com>
2022-08-27 17:32:06 +0100

    docs(maxSumSubmatrix):add task description

---
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/maxSumSubmatrix.js
@@ -0,0 +1,13 @@
+/**
+ * **363. Max Sum of Rectangle No Larger Than K**
+ *
+ * Given an `m x n` matrix `matrix` and an integer `k`, return *the max sum of a rectangle in the matrix
+ * such that its sum is no larger than `k`*.
+ *
+ * It is **guaranteed** that there will be a rectangle with a sum no larger than k.
+ *
+ * @param {number[][]} matrix
+ * @param {number} k
+ * @return {number}
+ */
+const maxSumSubmatrix = function (matrix, k) {};
