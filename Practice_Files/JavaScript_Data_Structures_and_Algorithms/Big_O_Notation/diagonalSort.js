7e1c616547338b5cadaefbbf32e258835a70dfb6
yuhcee <egbouchenna001@gmail.com>
2022-08-30 00:14:58 +0100

    chore(diagonalSort):sort rows

--- Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/diagonalSort.js
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/diagonalSort.js
@@ -13,38 +13,9 @@
  * @return {number[][]}
  */
 const diagonalSort = (mat) => {
+    // sort rows
     for (let row = 1; row < mat.length; row++) sortDiagonals(mat, row, 0);
-    for (let col = 0; col < mat[0].length; col++) sortDiagonals(mat, 0, col);
-
-    return mat;
-
-    function sortDiagonals(mat, x, y) {
-        let diagonals = [];
-        let size = Math.min(mat.length - x, mat[0].length - y);
-
-        for (let i = 0; i < size; i++) diagonals.push(mat[i + x][i + y]);
-        diagonals.sort((a, b) => a - b);
-
-        for (let i = 0; i < size; i++) mat[i + x][i + y] = diagonals[i];
-
-        return diagonals;
-    }
+    
 };
 
-const mat = [
-    [3, 3, 1, 1],
-    [2, 2, 1, 2],
-    [1, 1, 1, 2],
-];
-// Output: [[1,1,1,1],[1,2,2,2],[1,2,3,3]]
-console.log(diagonalSort(mat));
 
-const mat1 = [
-    [11, 25, 66, 1, 69, 7],
-    [23, 55, 17, 45, 15, 52],
-    [75, 31, 36, 44, 58, 8],
-    [22, 27, 33, 25, 68, 4],
-    [84, 28, 14, 11, 5, 50],
-];
-// Output: [[5,17,4,1,52,7],[11,11,25,45,8,69],[14,23,25,44,58,15],[22,27,31,36,50,66],[84,28,75,33,55,68]]
-console.log(diagonalSort(mat1));
