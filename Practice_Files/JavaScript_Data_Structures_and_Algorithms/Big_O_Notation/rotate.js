579ffcdc50a7a8646f815a6f27aef8a94aa8851e
yuhcee <egbouchenna001@gmail.com>
2022-08-31 00:05:58 +0100

    test(rotate):add testcase 1

--- Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/rotate.js
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/rotate.js
@@ -34,4 +34,11 @@ const rotate = function (matrix) {
     return matrix;
 };
 
+const matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+];
+// Output: [[7,4,1],[8,5,2],[9,6,3]]
+console.log(rotate(matrix));
 
