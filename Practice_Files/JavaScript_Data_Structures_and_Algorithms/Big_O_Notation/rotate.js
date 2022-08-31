f5871578e0114f5e26988665ae8b97d1a2df17bf
yuhcee <egbouchenna001@gmail.com>
2022-08-31 12:11:53 +0100

    docs(rotateImage):add task description

---
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/rotate.js
@@ -0,0 +1,13 @@
+/**
+ * **48. Rotate Image**
+ *
+ * You are given an `n x n` 2D `matrix` representing an image, rotate the image by **90** degrees
+ * (clockwise).
+ *
+ * You have to rotate the image in-place, which means you have to modify the input 2D matrix directly. DO
+ * NOT allocate another 2D matrix and do the rotation.
+ *
+ * @param {number[][]} matrix
+ * @return {void} Do not return anything, modify matrix in-place instead.
+ */
+const rotate = function (matrix) {};
