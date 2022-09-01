691dc0a47c275ca6d20579c01ff1081672bbca0f
yuhcee <egbouchenna001@gmail.com>
2022-09-01 03:52:15 +0100

    test(pacificAtlantic):add testcase 1

--- Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/pacificAtlantic.js
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/pacificAtlantic.js
@@ -50,13 +50,13 @@ const pacificAtlantic = function (heights) {
         for (let [dr, dc] of DIRS) {
             dr += r;
             dc += c;
-            if (grid[dr] && grid[dr][dc] !== undefined && grid[dr][dc] !== '#' && arr[dr][dc] >= val) {
+            if (grid[dr] && grid[dr][dc] !== undefined && grid[dr][dc] !== '#' && grid[dr][dc] >= val) {
                 explore(dr, dc, grid);
             }
         }
     }
 
-    for (let c = 0; c < colLength; c = +1) {
+    for (let c = 0; c < colLength; c += 1) {
         explore(rowLength - 1, c, ATLANTIC); // bottom
         explore(0, c, PACIFIC); // top
     }
@@ -68,11 +68,38 @@ const pacificAtlantic = function (heights) {
 
     for (let row = 0; row < rowLength; row++) {
         for (let col = 0; col < colLength; col++) {
-            if (PACIFIC[r][c] === '#' && ATLANTIC[r][c] === '#') {
-                output.push([r, c]);
+            if (PACIFIC[row][col] === '#' && ATLANTIC[row][col] === '#') {
+                output.push([row, col]);
             }
         }
     }
 
     return output;
 };
+
+const heights = [
+    [1, 2, 2, 3, 5],
+    [3, 2, 3, 4, 4],
+    [2, 4, 5, 3, 1],
+    [6, 7, 1, 4, 5],
+    [5, 1, 1, 2, 4],
+];
+// Output: [[0,4],[1,3],[1,4],[2,2],[3,0],[3,1],[4,0]]
+/* Explanation: The following cells can flow to the Pacific and Atlantic oceans, as shown below:
+[0,4]: [0,4] -> Pacific Ocean 
+       [0,4] -> Atlantic Ocean
+[1,3]: [1,3] -> [0,3] -> Pacific Ocean 
+       [1,3] -> [1,4] -> Atlantic Ocean
+[1,4]: [1,4] -> [1,3] -> [0,3] -> Pacific Ocean 
+       [1,4] -> Atlantic Ocean
+[2,2]: [2,2] -> [1,2] -> [0,2] -> Pacific Ocean 
+       [2,2] -> [2,3] -> [2,4] -> Atlantic Ocean
+[3,0]: [3,0] -> Pacific Ocean 
+       [3,0] -> [4,0] -> Atlantic Ocean
+[3,1]: [3,1] -> [3,0] -> Pacific Ocean 
+       [3,1] -> [4,1] -> Atlantic Ocean
+[4,0]: [4,0] -> Pacific Ocean 
+       [4,0] -> Atlantic Ocean
+Note that there are other possible paths for these cells to flow to the Pacific and Atlantic oceans. */
+
+console.log(pacificAtlantic(heights));
