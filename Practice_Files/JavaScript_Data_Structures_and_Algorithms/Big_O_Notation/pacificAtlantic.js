7b44bde30dc046d353b76819ec7ff148592d7c29
yuhcee <egbouchenna001@gmail.com>
2022-09-01 03:42:45 +0100

    chore(pacificAtlantic):push cells into output

--- Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/pacificAtlantic.js
+++ Practice_Files/JavaScript_Data_Structures_and_Algorithms/Big_O_Notation/pacificAtlantic.js
@@ -13,8 +13,8 @@
  * south, east, and west if the neighboring cell's **height is less than or equal to** the current cell's
  * height. Water can flow from any cell adjacent to an ocean into the ocean.
  *
- * Return *a **2D list** of grid coordinates `result` where `result[i] = [ri, ci]` denotes that rain
- * water can flow from cell (`ri, ci`) to both the Pacific and Atlantic oceans*.
+ * Return *a **2D list** of grid coordinates `result` where `result[i] = [ri, ci]` denotes that
+ * rain water can flow from cell (`ri, ci`) to both the Pacific and Atlantic oceans*.
  *
  * @param {number[][]} heights
  * @return {number[][]}
@@ -68,7 +68,9 @@ const pacificAtlantic = function (heights) {
 
     for (let row = 0; row < rowLength; row++) {
         for (let col = 0; col < colLength; col++) {
-            
+            if (PACIFIC[r][c] === '#' && ATLANTIC[r][c] === '#') {
+                output.push([r, c]);
+            }
         }
     }
 
