9e7598a629d9c67373507d9d0452898810c1fa0e
yuhcee <egbouchenna001@gmail.com>
2022-09-10 14:16:21 +0100

    chore(numberOfWeakCharacters):add solution

--- Datastructures_&_Algorithms/Sorting/numberOfWeakCharacters.js
+++ Datastructures_&_Algorithms/Sorting/numberOfWeakCharacters.js
@@ -17,5 +17,11 @@
  * @return {number}
  */
 const numberOfWeakCharacters = (properties, count = 0, max_d = 0) => {
-    
+    properties
+        .sort((a, b) => b[0] - a[0] || a[1] - b[1])
+        .forEach((res) => {
+            if (res[1] < max_d) count++;
+            if (res[1] > max_d) max_d = res[1];
+        });
+    return count;
 };
