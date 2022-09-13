f2cb404bcf72836f9f7684f028b9579f4d6b8eeb
yuhcee <egbouchenna001@gmail.com>
2022-09-13 23:48:30 +0100

    test(bagOfTokensScore):add testcase 3

--- Datastructures_&_Algorithms/Greedy Algorithms/bagOfTokensScore.js
+++ Datastructures_&_Algorithms/Greedy Algorithms/bagOfTokensScore.js
@@ -58,3 +58,12 @@ const tokens1 = [100, 200],
 /* Explanation: Play the 0th token (100) face up, your power becomes 50 and score becomes 1.
 There is no need to play the 1st token since you cannot play it face up to add to your score. */
 console.log(bagOfTokensScore(tokens1, power1));
+
+const tokens2 = [100, 200, 300, 400],
+    power2 = 200;
+// Output: 2
+/* Explanation: Play the tokens in this order to get a score of 2:
+1. Play the 0th token (100) face up, your power becomes 100 and score becomes 1.
+2. Play the 3rd token (400) face down, your power becomes 500 and score becomes 0.
+3. Play the 1st token (200) face up, your power becomes 300 and score becomes 1.
+4. Play the 2nd token (300) face up, your power becomes 0 and score becomes 2. */
