7217634f9f116b3bf14e67b242ef03d34a961e92
yuhcee <egbouchenna001@gmail.com>
2022-09-14 22:25:04 +0100

    docs(pseudoPalindromicPaths):add task description

---
+++ Datastructures_&_Algorithms/Lists/pseudoPalindromicPaths.js
@@ -0,0 +1,18 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * Given a binary tree where node values are digits from 1 to 9. A path in the binary tree is said to be
+ * **pseudo-palindromic** if at least one permutation of the node values in the path is a palindrome.
+ *
+ * Return *the number of **pseudo-palindromic** paths going from the root node to leaf nodes*.
+ *
+ * @param {TreeNode} root
+ * @return {number}
+ */
+const pseudoPalindromicPaths = function (root) {};
