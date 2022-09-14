f7eb11d68b77123ed0e5c024d5baa829924e6f11
yuhcee <egbouchenna001@gmail.com>
2022-09-14 22:36:19 +0100

    chore(pseudoPalindromicPaths):add solution

--- Datastructures_&_Algorithms/Lists/pseudoPalindromicPaths.js
+++ Datastructures_&_Algorithms/Lists/pseudoPalindromicPaths.js
@@ -15,4 +15,29 @@
  * @param {TreeNode} root
  * @return {number}
  */
-const pseudoPalindromicPaths = function (root) {};
+const pseudoPalindromicPaths = function (root) {
+    let count = 0;
+
+    const digits = new Array(10).fill(true);
+
+    const f = (node) => {
+        if (!node) return;
+
+        digits[node.val] = !digits[node.val];
+
+        if (!node.left && !node.right) {
+            if (digits.filter((num) => num === false).length <= 1) count += 1;
+            digits[node.val] = !digits[node.val];
+            return;
+        }
+
+        f(node.left);
+        f(node.right);
+
+        digits[node.val] = !digits[node.val];
+    };
+
+    f(root);
+
+    return count;
+};
