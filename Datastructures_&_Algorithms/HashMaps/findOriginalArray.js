9189c57ec4bd70f85b2677724e572fc96a68252d
yuhcee <egbouchenna001@gmail.com>
2022-09-15 23:28:01 +0100

    :white_check_mark:(findOriginalArray):add testcase 1

--- Datastructures_&_Algorithms/HashMaps/findOriginalArray.js
+++ Datastructures_&_Algorithms/HashMaps/findOriginalArray.js
@@ -42,3 +42,13 @@ const findOriginalArray = function (changed) {
 
     return map.size > 0 ? [] : result;
 };
+
+const changed = [1, 3, 4, 2, 6, 8];
+// Output: [1,3,4]
+/* Explanation: One possible original array could be [1,3,4]:
+- Twice the value of 1 is 1 * 2 = 2.
+- Twice the value of 3 is 3 * 2 = 6.
+- Twice the value of 4 is 4 * 2 = 8.
+Other original arrays could be [4,3,1] or [3,1,4]. */
+
+console.log(findOriginalArray(changed));
