183aec84f69f619b55886a000f90be7649afa084
yuhcee <egbouchenna001@gmail.com>
2022-09-16 23:51:50 +0100

    :books: (maximumScore) add task description

---
+++ Datastructures_&_Algorithms/Dynamic Programming/maximumScore.js
@@ -0,0 +1,20 @@
+/**
+ * **1770. Maximum Score from Performing Multiplication Operations**
+ *
+ * You are given two integer arrays `nums` and `multipliers` of size `n` and `m` respectively, where `n >= m`. The
+ * arrays are **1-indexed**.
+ *
+ * You begin with a score of `0`. You want to perform **exactly** `m` operations. On the `ith` operation **
+ * (1-indexed)**, you will:
+ *
+ * - Choose one integer `x` from **either the start or the end** of the array `nums`.
+ * - Add `multipliers[i] * x` to your score.
+ * - Remove `x` from the array `nums`.
+ *
+ * Return *the **maximum** score after performing `m` operations*.
+ *
+ * @param {number[]} nums
+ * @param {number[]} multipliers
+ * @return {number}
+ */
+const maximumScore = function (nums, multipliers) {};
