107cb62bcb28647ae0209248e4f745131d5a8c96
yuhcee <egbouchenna001@gmail.com>
2022-09-17 18:42:40 +0100

    :books: (palindromePairs) add task description

---
+++ Datastructures_&_Algorithms/Trees/Trie/palindromePairs.js
@@ -0,0 +1,11 @@
+/**
+ * **336. Palindrome Pairs**
+ *
+ * Given a list of **unique** words, return all the pairs of the ***distinct***
+ * indices `(i, j)` in the given list, so that the concatenation of the two words
+ * `words[i] + words[j]` is a palindrome.
+ *
+ * @param {string[]} words
+ * @return {number[][]}
+ */
+const palindromePairs = (words) => {};
