37c4c84d04958eb901c14387cc47d31e30fa7208
yuhcee <egbouchenna001@gmail.com>
2022-09-18 23:55:34 +0100

    :white_check_mark: (trap) add testcase 2

--- Datastructures_&_Algorithms/Arrays/trap.js
+++ Datastructures_&_Algorithms/Arrays/trap.js
@@ -39,3 +39,7 @@ const height = [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1];
 // Output: 6
 /* Explanation: The above elevation map (black section) is represented by array [0,1,0,2,1,0,1,3,2,1,2,1]. In this case, 6 units of rain water (blue section) are being trapped. */
 console.log(trap(height));
+
+const height1 = [4, 2, 0, 3, 2, 5];
+// Output: 9
+console.log(trap(height1));
