56b380234569c3200aa2afb79c5af72128c94169
yuhcee <egbouchenna001@gmail.com>
2022-09-18 23:39:18 +0100

    :books: (trap) add task description

---
+++ Datastructures_&_Algorithms/Arrays/trap.js
@@ -0,0 +1,10 @@
+/**
+ * **42. Trapping Rain Water**
+ *
+ * Given `n` non-negative integers representing an elevation map where the width of each bar is `1`, compute how much
+ * water it can trap after raining.
+ *
+ * @param {number[]} height
+ * @return {number}
+ */
+const trap = function (height) {};
