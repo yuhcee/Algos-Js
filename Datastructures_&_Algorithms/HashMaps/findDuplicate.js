1d51f5eb78fd62a717c4a0f97f87c9868dd55a6b
yuhcee <egbouchenna001@gmail.com>
2022-09-19 11:01:16 +0100

    :white_check_mark: (findDuplicate) add testcase 1

--- Datastructures_&_Algorithms/HashMaps/findDuplicate.js
+++ Datastructures_&_Algorithms/HashMaps/findDuplicate.js
@@ -49,3 +49,8 @@ const findDuplicate = function (paths) {
 
     return result;
 };
+
+const paths = ['root/a 1.txt(abcd) 2.txt(efgh)', 'root/c 3.txt(abcd)', 'root/c/d 4.txt(efgh)', 'root 4.txt(efgh)'];
+// Output: [["root/a/2.txt","root/c/d/4.txt","root/4.txt"],["root/a/1.txt","root/c/3.txt"]]
+console.log(findDuplicate(paths));
+ 
