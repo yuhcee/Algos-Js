4aa0809a5c80bd37288df2432a14771677729f3e
yuhcee <egbouchenna001@gmail.com>
2022-09-20 09:18:11 +0100

    :sparkles: (findLength) initialize dp

--- Datastructures_&_Algorithms/Dynamic Programming/findLength.js
+++ Datastructures_&_Algorithms/Dynamic Programming/findLength.js
@@ -1,11 +1,17 @@
 /**
  * **718. Maximum Length of Repeated Subarray**
  *
- * Given two integer arrays `nums1` and `nums2`, return *the maximum length of a subarray that appears in **both** 
+ * Given two integer arrays `nums1` and `nums2`, return *the maximum length of a subarray that appears in **both**
  * arrays*.
- * 
+ *
  * @param {number[]} nums1
  * @param {number[]} nums2
  * @return {number}
  */
-const findLength = function (nums1, nums2) {};
+const findLength = function (nums1, nums2) {
+    const N = nums1.length + 1,
+        M = nums2.length + 1,
+        memo = Array(N)
+            .fill()
+            .map((_) => Array(M).fill(0));
+};
