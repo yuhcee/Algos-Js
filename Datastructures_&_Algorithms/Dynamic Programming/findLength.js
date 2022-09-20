a7354f3002b4d33e38f6fa6b04ff92f2b59a309c
yuhcee <egbouchenna001@gmail.com>
2022-09-20 09:15:15 +0100

    :books: (findLength) add task description

---
+++ Datastructures_&_Algorithms/Dynamic Programming/findLength.js
@@ -0,0 +1,11 @@
+/**
+ * **718. Maximum Length of Repeated Subarray**
+ *
+ * Given two integer arrays `nums1` and `nums2`, return *the maximum length of a subarray that appears in **both** 
+ * arrays*.
+ * 
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ */
+const findLength = function (nums1, nums2) {};
