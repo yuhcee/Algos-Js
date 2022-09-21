2468c96a54ffec2781a5ae64ac97072a0bc5989f
yuhcee <egbouchenna001@gmail.com>
2022-09-21 23:32:06 +0100

    :books: (sumEvenAfterQueries) add task description

---
+++ Datastructures_&_Algorithms/Arrays/sumEvenAfterQueries.js
@@ -0,0 +1,17 @@
+/**
+ * **985. Sum of Even Numbers After Queries**
+ *
+ * You are given an integer array `nums` and an array `queries` where `queries[i] = [vali, indexi]`.
+ *
+ * For each query `i`, first, apply `nums[indexi] = nums[indexi] + vali`, then print the sum of the even
+ * values of `nums`.
+ *
+ * Return *an integer array `answer` where `answer[i]` is the answer to the ith query*.
+ *
+ * @param {number[]} nums
+ * @param {number[][]} queries
+ * @return {number[]}
+ */
+const sumEvenAfterQueries = function (nums, queries) {
+    
+};
