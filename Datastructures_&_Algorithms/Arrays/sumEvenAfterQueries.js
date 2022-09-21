a32460b0fab43e22eecc6dcd2426370d04d38e10
yuhcee <egbouchenna001@gmail.com>
2022-09-21 23:37:16 +0100

    :sparkles: (sumEvenAfterQueries) add solution

--- Datastructures_&_Algorithms/Arrays/sumEvenAfterQueries.js
+++ Datastructures_&_Algorithms/Arrays/sumEvenAfterQueries.js
@@ -15,4 +15,25 @@
 const sumEvenAfterQueries = function (nums, queries) {
     // Sum of all the even numbers
     let evenSum = nums.reduce((acc, curr) => (curr % 2 === 0 ? curr + acc : acc), 0);
+
+    const res = [];
+
+    for (let [val, ind] of queries) {
+        // if the number at that index is even, subtract it because we are anyway going to
+        // add the query's value to that number and check if that is even/not
+        if (nums[ind] % 2 === 0) {
+            evenSum -= nums[ind];
+        }
+        // adding the query's value
+        nums[ind] += val;
+
+        // checking even/not after adding the query's value.
+        // If even, that would add up to our existing evenSum
+        if (nums[ind] % 2 === 0) {
+            evenSum += nums[ind];
+        }
+        res.push(evenSum);
+    }
+
+    return res;
 };
