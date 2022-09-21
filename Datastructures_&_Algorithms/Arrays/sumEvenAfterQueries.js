8616bfef93ad2a7e11ee78625a7f9d04bc78bdc2
yuhcee <egbouchenna001@gmail.com>
2022-09-21 23:43:37 +0100

    :white_check_mark:(sumEvenAfterQueries) add testcase 2

--- Datastructures_&_Algorithms/Arrays/sumEvenAfterQueries.js
+++ Datastructures_&_Algorithms/Arrays/sumEvenAfterQueries.js
@@ -51,3 +51,8 @@ After adding 1 to nums[0], the array is [2,2,3,4], and the sum of even values is
 After adding -3 to nums[1], the array is [2,-1,3,4], and the sum of even values is 2 + 4 = 6.
 After adding -4 to nums[0], the array is [-2,-1,3,4], and the sum of even values is -2 + 4 = 2.
 After adding 2 to nums[3], the array is [-2,-1,3,6], and the sum of even values is -2 + 6 = 4. */
+console.log(sumEvenAfterQueries(nums, queries));
+
+const nums1 = [1],
+    queries1 = [[4, 0]];
+// Output: [0]
