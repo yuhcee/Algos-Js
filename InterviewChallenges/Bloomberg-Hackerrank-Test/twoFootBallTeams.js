1f4bfe0c9eaf70839937ff10d77b1eb9c9c65870
yuhcee <egbouchenna001@gmail.com>
2022-09-22 19:01:23 +0100

    :books: (counts) add task description

---
+++ InterviewChallenges/Bloomberg-Hackerrank-Test/twoFootBallTeams.js
@@ -0,0 +1,18 @@
+/**
+ * **Two FootBall Teams**
+ *
+ * The number of goals achieved by two football teams in matches in a league is
+ * given in the form of **two** `lists`.
+ *
+ * For each match of `team B`, compute the **total number of matches** of `team
+ * A` where `team A` has scored **less than** or **equal to the number** of
+ * goals scored by `team B` in that match.
+ *
+ * @param {*} teamA number[]
+ * @param {*} teamB number[]
+ * @returns number[]
+ */
+const counts = (teamA, teamB) => {
+    
+};
+
