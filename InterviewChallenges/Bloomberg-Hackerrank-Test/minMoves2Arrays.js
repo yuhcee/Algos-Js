213f33d449c376e858535c4b65a02d75220d4850
yuhcee <egbouchenna001@gmail.com>
2022-09-22 18:35:04 +0100

    :books: (minimumMoves) add task description

---
+++ InterviewChallenges/Bloomberg-Hackerrank-Test/minMoves2Arrays.js
@@ -0,0 +1,31 @@
+/**
+ * **6. Minimum moves**
+ *
+ * There are two arrays of integers `arr1` and `arr2`. One move is defined as an **increment** or **decrement** of 
+ * an element in an array.
+ *
+ * Determine *the **minimum number of moves** to match `arr1` with `arr2`*.
+ *
+ * **No reordering of the digits is allowed**.
+ *
+ * **Example:** \
+ *    arr1 = [123, 543],\
+ *    arr2 = [321, 279]
+ *
+ * - Match arr1[0] = 123 with arr2[0] = 321
+ *      - Increment 1 twice to get 3(2 moves).
+ *      - Decrement 3 twice to get 1(2 moves).
+ *      - 4 moves are needed to match 123 with 321.
+ *
+ * - Match arr1[1] = 543 with arr2[1] = 279
+ *      - Decrement 5 three times to get 2(3 moves).
+ *      - Increment 4 three times to get 7(3 moves).
+ *      - Increment 3 six times to get 9(6 moves).
+ *      - 12 moves are needed to match 543 with 279.
+ * - 16 total moves are needed to match the arrays arr1 and arr2.
+ */
+
+const minMoves2 = (arr1, arr2) => {
+    
+};
+
