adac96b0c52febb16eb035e7e57b1b8baa220538
yuhcee <egbouchenna001@gmail.com>
2022-09-24 22:31:40 +0100

    :sparkles:(concatenatedBinary): add solution

--- Datastructures_&_Algorithms/Math/concatenatedBinary.js
+++ Datastructures_&_Algorithms/Math/concatenatedBinary.js
@@ -7,4 +7,14 @@
  * @param {number} n
  * @return {number}
  */
-const concatenatedBinary = function (n) {};
+const concatenatedBinary = function (n) {
+    let ans = 1,
+        len = 4;
+
+    for (let i = 2; i <= n; i++) {
+        if (i === len) len *= 2;
+        ans = (ans * len + i) % 1000000007;
+    }
+
+    return ans;
+};
