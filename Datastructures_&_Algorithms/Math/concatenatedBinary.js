f5dc430148be1b76517988440a83f1befa5ee20e
yuhcee <egbouchenna001@gmail.com>
2022-09-24 22:28:23 +0100

    :books:(concatenatedBinary): add task description

---
+++ Datastructures_&_Algorithms/Math/concatenatedBinary.js
@@ -0,0 +1,10 @@
+/**
+ * **1680. Concatenation of Consecutive Binary Numbers**
+ *
+ * Given an integer `n`, return *the **decimal value** of the binary string formed by concatenating the binary representations
+ * of `1` to `n` in order, **modulo** `109 + 7`*.
+ *
+ * @param {number} n
+ * @return {number}
+ */
+const concatenatedBinary = function (n) {};
