ef48c585d9504c148ca107674e9ae0ea08034311
yuhcee <egbouchenna001@gmail.com>
2022-09-25 22:52:52 +0100

    :white_check_mark:(MyCircularQueue): add testcase 1

--- Datastructures_&_Algorithms/Arrays/MyCircularQueue.js
+++ Datastructures_&_Algorithms/Arrays/MyCircularQueue.js
@@ -80,3 +80,30 @@ MyCircularQueue.prototype.isFull = function () {
 
     return false;
 };
+
+/**
+ * Your MyCircularQueue object will be instantiated and called as such:
+ * var obj = new MyCircularQueue(k)
+ * var param_1 = obj.enQueue(value)
+ * var param_2 = obj.deQueue()
+ * var param_3 = obj.Front()
+ * var param_4 = obj.Rear()
+ * var param_5 = obj.isEmpty()
+ * var param_6 = obj.isFull()
+ */
+
+// const Input = ['MyCircularQueue', 'enQueue', 'enQueue', 'enQueue', 'enQueue', 'Rear', 'isFull', 'deQueue', 'enQueue', 'Rear'][([3], [1], [2], [3], [4], [], [], [], [4], [])];
+/* Output
+[null, true, true, true, false, 3, true, true, true, 4] */
+
+/* Explanation
+MyCircularQueue myCircularQueue = new MyCircularQueue(3);
+myCircularQueue.enQueue(1); // return True
+myCircularQueue.enQueue(2); // return True
+myCircularQueue.enQueue(3); // return True
+myCircularQueue.enQueue(4); // return False
+myCircularQueue.Rear();     // return 3
+myCircularQueue.isFull();   // return True
+myCircularQueue.deQueue();  // return True
+myCircularQueue.enQueue(4); // return True
+myCircularQueue.Rear();     // return 4 */
