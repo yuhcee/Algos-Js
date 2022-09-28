3d7553d69f3534a40360096a2e376b9a80362700
yuhcee <egbouchenna001@gmail.com>
2022-09-28 23:38:26 +0100

    :books: (removeNthFromEnd): add task description

---
+++ Datastructures_&_Algorithms/Lists/removeNthFromEnd.js
@@ -0,0 +1,18 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * **19. Remove Nth Node From End of List**
+ *
+ * Given the `head` of a linked list, remove the `nth` node from the end of the list and
+ * return its head.
+ *
+ * @param {ListNode} head
+ * @param {number} n
+ * @return {ListNode}
+ */
+const removeNthFromEnd = function (head, n) {};
