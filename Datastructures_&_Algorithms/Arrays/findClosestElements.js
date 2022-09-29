33255cd509e402802ee900dc142a492bb586b6b9
yuhcee <egbouchenna001@gmail.com>
2022-09-29 16:35:39 +0100

    :zap:(findClosestElements) initialize binary search bounds

--- Datastructures_&_Algorithms/Arrays/findClosestElements.js
+++ Datastructures_&_Algorithms/Arrays/findClosestElements.js
@@ -1,17 +1,21 @@
 /**
  * **658. Find K Closest Elements**
- * 
- * Given a sorted integer array `arr`, two integers `k` and `x`, return the `k` closest integers to `x` in the array. The result 
+ *
+ * Given a sorted integer array `arr`, two integers `k` and `x`, return the `k` closest integers to `x` in the array. The result
  * should also be sorted in ascending order.
- * 
+ *
  * An integer `a` is closer to `x` than an integer `b` if:
- * 
+ *
  * - `|a - x| < |b - x|`, or
  * - `|a - x| == |b - x|` and `a < b`
- * 
+ *
  * @param {number[]} arr
  * @param {number} k
  * @param {number} x
  * @return {number[]}
  */
- const findClosestElements = function(arr, k, x) {}
+const findClosestElements = function (arr, k, x) {
+    // Initialize binary search bounds
+    let left = 0,
+        right = arr.length - k;
+};
