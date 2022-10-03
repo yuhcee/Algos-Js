3957f6c6eeda5678519dbb40a723ea923039bc2c
yuhcee <egbouchenna001@gmail.com>
2022-10-03 23:35:28 +0100

    :zap:(numRollsToTarget) calculate number of rolls

--- Datastructures_&_Algorithms/Dynamic Programming/numRollsToTarget.js
+++ Datastructures_&_Algorithms/Dynamic Programming/numRollsToTarget.js
@@ -15,4 +15,15 @@
 const numRollsToTarget = function (n, k, target, memo = {}) {
     if (n === 0 && target === 0) return 1;
     if (n === 0 || target <= 0) return 0;
+
+    let key = `${n},${target}`;
+
+    if (key in memo) return memo[key];
+
+    let count = 0,
+        M = Math.pow(10, 9) + 7;
+
+    for (let i = 1; i <= k; i++) {
+        count = (count + numRollsToTarget(n - 1, k, target - i, memo)) % M;
+    }
 };
