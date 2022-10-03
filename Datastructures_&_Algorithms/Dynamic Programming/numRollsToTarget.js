d6018ee714d1936fb8c7336a0b00f0f0ae4d9710
yuhcee <egbouchenna001@gmail.com>
2022-10-03 23:33:47 +0100

    :books:(numRollsToTarget) add task description

---
+++ Datastructures_&_Algorithms/Dynamic Programming/numRollsToTarget.js
@@ -0,0 +1,15 @@
+/**
+ * **1155. Number of Dice Rolls With Target Sum**
+ *
+ * You have `n` dice and each die has `k` faces numbered from `1` to `k`.
+ *
+ * Given three integers `n`, `k`, and `target`, return *the number of possible ways (out of the `kn` total ways) to roll
+ * the dice so the sum of the face-up numbers equals `target`*. Since the answer may be too large, return it **modulo**
+ * `109 + 7`.
+ *
+ * @param {number} n
+ * @param {number} k
+ * @param {number} target
+ * @return {number}
+ */
+const numRollsToTarget = function (n, k, target, memo = {}) {};
