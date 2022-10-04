63369e8a6ec716048c287aba9e43fa3434335fc9
yuhcee <egbouchenna001@gmail.com>
2022-10-04 08:18:12 +0100

    :books:(minCost) add task description

---
+++ Datastructures_&_Algorithms/Arrays/minCost.js
@@ -0,0 +1,18 @@
+/**
+ * **1578. Minimum Time to Make Rope Colorful**
+ *
+ * Alice has `n` balloons arranged on a rope. You are given a **0-indexed** string `colors` where `colors[i]` is the
+ * color of the `ith` balloon.
+ *
+ * Alice wants the rope to be **colorful**. She does not want two consecutive balloons to be of the same color, so she
+ * asks Bob for help. Bob can remove some balloons from the rope to make it colorful. You are given a **0-indexed**
+ * integer array `neededTime` where `neededTime[i]` is the time (in seconds) that Bob needs to remove the `ith` balloon
+ * from the rope.
+ *
+ * Return *the **minimum time** Bob needs to make the rope **colorful***.
+ *
+ * @param {string} colors
+ * @param {number[]} neededTime
+ * @return {number}
+ */
+const minCost = function (colors, neededTime) {};
