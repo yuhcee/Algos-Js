fc937475c2939eeb201ea1d1f81b0730ecbfa84a
yuhcee <egbouchenna001@gmail.com>
2022-10-05 09:00:58 +0100

    :books:(hasPathSum) add task description

---
+++ Datastructures_&_Algorithms/Trees/BinaryTree/hasPathSum.js
@@ -0,0 +1,21 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * **112. Path Sum**
+ *
+ * Given the `root` of a binary tree and an integer `targetSum`, return `true` if the tree has a **root-to-leaf** path such that
+ * adding up all the values along the path equals `targetSum`.
+ *
+ * A **leaf** is a node with no children.
+ *
+ * @param {TreeNode} root
+ * @param {number} targetSum
+ * @return {boolean}
+ */
+const hasPathSum = function (root, targetSum) {};
