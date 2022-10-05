488e3023a6c4120c28876c5e54657c89577e1e9b
yuhcee <egbouchenna001@gmail.com>
2022-10-05 09:09:26 +0100

    :white_check_mark:(hasPathSum) add testcase 2

--- Datastructures_&_Algorithms/Trees/BinaryTree/hasPathSum.js
+++ Datastructures_&_Algorithms/Trees/BinaryTree/hasPathSum.js
@@ -33,3 +33,12 @@ const root = [5, 4, 8, 11, null, 13, 4, 7, 2, null, null, null, 1],
 // Output: true
 // Explanation: The root-to-leaf path with the target sum is shown.
 console.log(hasPathSum(root, targetSum));
+
+const root1 = [1, 2, 3],
+    targetSum1 = 5;
+// Output: false
+/* Explanation: There two root-to-leaf paths in the tree:
+(1 --> 2): The sum is 3.
+(1 --> 3): The sum is 4.
+There is no root-to-leaf path with sum = 5. */
+console.log(hasPathSum(root1, targetSum1));
