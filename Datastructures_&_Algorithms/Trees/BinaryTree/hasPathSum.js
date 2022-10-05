b2bc01d3960dd59702dc2c668208fdcb47299f52
yuhcee <egbouchenna001@gmail.com>
2022-10-05 09:06:50 +0100

    :zap:(hasPathSum) add recursive solution

--- Datastructures_&_Algorithms/Trees/BinaryTree/hasPathSum.js
+++ Datastructures_&_Algorithms/Trees/BinaryTree/hasPathSum.js
@@ -18,4 +18,12 @@
  * @param {number} targetSum
  * @return {boolean}
  */
-const hasPathSum = function (root, targetSum) {};
+const hasPathSum = function (root, targetSum) {
+    if (root === null) return false;
+
+    targetSum -= root.val;
+
+    if (root.left === null && root.right === null) return targetSum === 0;
+
+    return hasPathSum(root.left, targetSum) || hasPathSum(root.right, targetSum);
+};
