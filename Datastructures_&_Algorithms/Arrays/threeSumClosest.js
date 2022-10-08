60807caf868b1a5b9f30fae6ac55a4ebd3202c4a
yuhcee <egbouchenna001@gmail.com>
2022-10-08 23:28:39 +0100

    :books: (threeSumClosest) add task description

---
+++ Datastructures_&_Algorithms/Arrays/threeSumClosest.js
@@ -0,0 +1,21 @@
+/**
+ * **16. 3Sum Closest**
+ *
+ * Given an integer array `nums` of length `n` and an integer `target`, find three integers in
+ * `nums` such that the sum is closest to `target`.
+ *
+ * Return *the sum of the three integers*.
+ *
+ * You may assume that each input would have exactly one solution.
+ *
+ * **Constraints:**
+ *
+ * - `3 <= nums.length <= 1000`
+ * - `-1000 <= nums[i] <= 1000`
+ * - `-104 <= target <= 104`
+ *
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+const threeSumClosest = function (nums, target) {};
