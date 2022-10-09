40ab9c93980f2db2ccf9e68087bbae0787b7e040
yuhcee <egbouchenna001@gmail.com>
2022-10-09 22:10:34 +0100

    :sparkles: (MyCalendarThree) use +1 start and -1 for end

--- Datastructures_&_Algorithms/Trees/BinaryTree/MyCalendarThree.js
+++ Datastructures_&_Algorithms/Trees/BinaryTree/MyCalendarThree.js
@@ -13,7 +13,7 @@ var MyCalendarThree = function () {
  *
  * Implement the `MyCalendarThree` class:
  *
- * -  `MyCalendarThree()` Initializes the object.
+ * - `MyCalendarThree()` Initializes the object.
  * - `int book(int start, int end)` Returns an integer `k` representing the largest integer such
  * that there exists a `k`-booking in the calendar.
  *
@@ -21,4 +21,14 @@ var MyCalendarThree = function () {
  * @param {number} end
  * @return {number}
  */
-MyCalendarThree.prototype.book = function (start, end) {};
+MyCalendarThree.prototype.book = function (start, end) {
+    //Use +1 for start and -1 for end, but we need to check only start and end points of the meeting as the possible duration of the meetins are high 10^9
+    this.data.push([start, 1]);
+    this.data.push([end, -1]);
+};
+
+/**
+ * Your MyCalendarThree object will be instantiated and called as such:
+ * var obj = new MyCalendarThree()
+ * var param_1 = obj.book(start,end)
+ */
