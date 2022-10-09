fdd04a207355b110676772a95c29ae1afd73736a
yuhcee <egbouchenna001@gmail.com>
2022-10-09 22:01:15 +0100

    :books: (MyCalendarThree) add task description

---
+++ Datastructures_&_Algorithms/Trees/BinaryTree/MyCalendarThree.js
@@ -0,0 +1,22 @@
+var MyCalendarThree = function () {};
+
+/**
+ * **732. My Calendar III**
+ *
+ * A `k`-booking happens when `k` events have some non-empty intersection (i.e., there is some time
+ * that is common to all `k` events.)
+ *
+ * You are given some events `[start, end]`, after each given event, return an integer `k`
+ * representing the maximum k-booking between all the previous events.
+ *
+ * Implement the `MyCalendarThree` class:
+ *
+ * -  `MyCalendarThree()` Initializes the object.
+ * - `int book(int start, int end)` Returns an integer `k` representing the largest integer such
+ * that there exists a `k`-booking in the calendar.
+ *
+ * @param {number} start
+ * @param {number} end
+ * @return {number}
+ */
+MyCalendarThree.prototype.book = function (start, end) {};
