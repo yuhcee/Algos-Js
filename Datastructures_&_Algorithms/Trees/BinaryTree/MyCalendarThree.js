411d1f73a52d64e5e7d7d183e18e5dd0c3bcee5e
yuhcee <egbouchenna001@gmail.com>
2022-10-09 22:20:12 +0100

    :white_check_mark: (MyCalendarThree) add testcase 1

--- Datastructures_&_Algorithms/Trees/BinaryTree/MyCalendarThree.js
+++ Datastructures_&_Algorithms/Trees/BinaryTree/MyCalendarThree.js
@@ -52,3 +52,16 @@ MyCalendarThree.prototype.book = function (start, end) {
  * var obj = new MyCalendarThree()
  * var param_1 = obj.book(start,end)
  */
+
+const input = ['MyCalendarThree', 'book', 'book', 'book', 'book', 'book', 'book'][([], [10, 20], [50, 60], [10, 40], [5, 15], [5, 10], [25, 55])];
+/* Output
+ [null, 1, 1, 2, 3, 3, 3] */
+
+/* Explanation
+ MyCalendarThree myCalendarThree = new MyCalendarThree();
+ myCalendarThree.book(10, 20); // return 1, The first event can be booked and is disjoint, so the maximum k-booking is a 1-booking.
+ myCalendarThree.book(50, 60); // return 1, The second event can be booked and is disjoint, so the maximum k-booking is a 1-booking.
+ myCalendarThree.book(10, 40); // return 2, The third event [10, 40) intersects the first event, and the maximum k-booking is a 2-booking.
+ myCalendarThree.book(5, 15); // return 3, The remaining events cause the maximum K-booking to be only a 3-booking.
+ myCalendarThree.book(5, 10); // return 3
+ myCalendarThree.book(25, 55); // return 3 */
