d48988ffe7c053cee892943caeeaadae8c88c86c
yuhcee <egbouchenna001@gmail.com>
2022-10-10 10:07:47 +0100

    :books: (findTarget) add task description

---
+++ Datastructures_&_Algorithms/Trees/BinaryTree/findTarget.js
@@ -0,0 +1,19 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * **653. Two Sum IV - Input is a BST**
+ *
+ * Given the `root` of a Binary Search Tree and a target number `k`, return *`true` if there exist
+ * two elements in the BST such that their sum is equal to the given target*.
+ *
+ * @param {TreeNode} root
+ * @param {number} k
+ * @return {boolean}
+ */
+const findTarget = function (root, k) {};
