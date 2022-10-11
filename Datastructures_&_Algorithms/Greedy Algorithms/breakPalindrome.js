8ab44b56b797c42c4849f08ea3dc9b328bec916f
yuhcee <egbouchenna001@gmail.com>
2022-10-11 10:50:03 +0100

    :sparkles: (breakPalindrome) add task solution

--- Datastructures_&_Algorithms/Greedy Algorithms/breakPalindrome.js
+++ Datastructures_&_Algorithms/Greedy Algorithms/breakPalindrome.js
@@ -17,4 +17,19 @@
  * @param {string} palindrome
  * @return {string}
  */
-const breakPalindrome = (palindrome) => {};
+const breakPalindrome = (palindrome) => {
+    if (palindrome.length > 1) {
+        const p = palindrome.split(``);
+
+        for (var i = 0, n = p.length; i < n; i++) {
+            if (p[i] !== `a` && i !== (n - 1) / 2) {
+                p[i] = `a`;
+                return p.join(``);
+            }
+        }
+
+        p[n - 1] = 'b';
+
+        return p.join(``);
+    } else return ``;
+};
