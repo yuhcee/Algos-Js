9d0819af55921f8a7e7eba049f5887cf3abd034f
yuhcee <egbouchenna001@gmail.com>
2022-10-13 20:31:36 +0100

    :books: (largestPerimeter) add task description

---
+++ Datastructures_&_Algorithms/Greedy Algorithms/largestPerimeter.js
@@ -0,0 +1,11 @@
+/**
+ * **976. Largest Perimeter Triangle**
+ *
+ * Given an integer array `nums`, return *the largest perimeter of a triangle with a non-zero area,
+ * formed from three of these lengths. If it is impossible to form any triangle of a non-zero area,
+ * return `0`*.
+ *
+ * @param {number[]} nums
+ * @return {number}
+ */
+const largestPerimeter = function (nums) {};
