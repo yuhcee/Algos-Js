08c730aed5879c13719ce714fea28e5b42ec8c95
yuhcee <egbouchenna001@gmail.com>
2022-10-14 11:55:17 +0100

    :sparkles: (deleteNode) add constraints

--- Datastructures_&_Algorithms/Trees/BinaryTree/deleteNode.js
+++ Datastructures_&_Algorithms/Trees/BinaryTree/deleteNode.js
@@ -31,6 +31,13 @@
  * - We will build the linked list and pass the node to your function.
  * - The output will be the entire list after calling your function.
  *
+ * **Constraints:**
+ *
+ * - The number of the nodes in the given list is in the range `[2, 1000]`.
+ * - `-1000 <= Node.val <= 1000`
+ * - The value of each node in the list is **unique**.
+ * - The `node` to be deleted is **in the list** and is not a **tail node**.
+ *
  * @param {ListNode} node
  * @return {void} Do not return anything, modify node in-place instead.
  */
