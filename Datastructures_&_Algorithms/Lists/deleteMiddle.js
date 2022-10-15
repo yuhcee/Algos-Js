3188ab597cae47c12c7c2bc5156860991c4ebed2
yuhcee <egbouchenna001@gmail.com>
2022-10-15 17:47:43 +0100

    :white_check_mark:(deleteMiddle) add testcase 1

--- Datastructures_&_Algorithms/Lists/deleteMiddle.js
+++ Datastructures_&_Algorithms/Lists/deleteMiddle.js
@@ -6,17 +6,6 @@
  * }
  */
 /**
- * @param {ListNode} head
- * @return {ListNode}
- */
-/**
- * Definition for singly-linked list.
- * function ListNode(val, next) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.next = (next===undefined ? null : next)
- * }
- */
-/**
  * **2095. Delete the Middle Node of a Linked List**
  *
  * You are given the `head` of a linked list. **Delete** the **middle node**, and return
@@ -32,7 +21,7 @@
  * **Constraints:**
  *
  * - The number of nodes in the list is in the range `[1, 105]`.
- * - 1 <= Node.val <= 105
+ * - `1 <= Node.val <= 105`
  *
  * @param {ListNode} head
  * @return {ListNode}
@@ -67,3 +56,13 @@ const findMiddle = (head) => {
     }
     return slow;
 };
+
+const head = [1, 3, 4, 7, 1, 2, 6];
+// Output: [1,3,4,1,2,6]
+/* Explanation:
+The above figure represents the given linked list. The indices of the nodes are written below.
+Since n = 7, node 3 with value 7 is the middle node, which is marked in red.
+We return the new list after removing this node.  */
+console.log(deleteMiddle(head));
+
+
