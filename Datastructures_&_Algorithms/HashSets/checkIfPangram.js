835c662bc1443dba030f5dc9d714095595cc909a
yuhcee <egbouchenna001@gmail.com>
2022-10-17 19:51:20 +0100

    :books: (checkIfPangram) add task description

---
+++ Datastructures_&_Algorithms/HashSets/checkIfPangram.js
@@ -0,0 +1,12 @@
+/**
+ * ****1832. Check if the Sentence Is Pangram
+ *
+ * A **pangram** is a sentence where every letter of the English alphabet appears at least once.
+ *
+ * Given a string `sentence` containing only lowercase English letters, return *`true` if sentence
+ * is a **pangram**, or `false` otherwise*.
+ *
+ * @param {string} sentence
+ * @return {boolean}
+ */
+const checkIfPangram = function (sentence) {};
