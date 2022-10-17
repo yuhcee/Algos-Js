b004c57d95ec8011df0683ea4e5ad60a395670e1
yuhcee <egbouchenna001@gmail.com>
2022-10-17 20:56:33 +0100

    :sparkles: (checkIfPangram) add task solution

--- Datastructures_&_Algorithms/HashSets/checkIfPangram.js
+++ Datastructures_&_Algorithms/HashSets/checkIfPangram.js
@@ -6,7 +6,18 @@
  * Given a string `sentence` containing only lowercase English letters, return *`true` if sentence
  * is a **pangram**, or `false` otherwise*.
  *
+ * Constraints:
+
+1 <= sentence.length <= 1000
+sentence consists of lowercase English letters.
  * @param {string} sentence
  * @return {boolean}
  */
-const checkIfPangram = function (sentence) {};
+const checkIfPangram = function (sentence) {
+    const seen = new Set();
+
+    for (let char of sentence) {
+        seen.add(char);
+    }
+    return seen.size() === 26;
+};
