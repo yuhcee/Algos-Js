b2443b58af1ab59c7f605a0faacfab252e8ef83f
yuhcee <egbouchenna001@gmail.com>
2022-10-20 00:00:27 +0100

    :fire: review topKfrequent Elements

--- Datastructures_&_Algorithms/Sorting/topKFrequent.js
+++ Datastructures_&_Algorithms/Sorting/topKFrequent.js
@@ -1,8 +1,8 @@
 /**
  * **347. Top K Frequent Elements**
  *
- * Given an integer array `nums` and an integer `k`, return the `k` most frequent elements. You may return
- * the answer in *any order*.
+ * Given an integer array `nums` and an integer `k`, return the `k` most frequent elements. You may
+ * return the answer in *any order*.
  *
  * @param {number[]} nums
  * @param {number} k
