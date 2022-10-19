7112854740efd019d87bda7381ce19916de4e791
yuhcee <egbouchenna001@gmail.com>
2022-10-20 00:19:12 +0100

    :white_check_mark: (topKFrequent) add testcase 1

--- Datastructures_&_Algorithms/Strings/topKFrequentStrings.js
+++ Datastructures_&_Algorithms/Strings/topKFrequentStrings.js
@@ -18,3 +18,10 @@
  * @return {string[]}
  */
 const topKFrequent = function (words, k) {};
+
+const words = ['i', 'love', 'leetcode', 'i', 'love', 'coding'],
+    k = 2;
+// Output: ["i","love"]
+/* Explanation: "i" and "love" are the two most frequent words.
+Note that "i" comes before "love" due to a lower alphabetical order. */
+console.log(topKFrequent(words, k));
