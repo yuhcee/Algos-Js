fddeba031431f397f10a1dad8fcd024a99c0a887
yuhcee <egbouchenna001@gmail.com>
2022-10-22 17:52:07 +0100

    :books: (containsNearbyDuplicate) add task description

---
+++ Datastructures_&_Algorithms/HashSets/containsNearbyDuplicate.js
@@ -0,0 +1,17 @@
+/**
+ * **219. Contains Duplicate II**
+ *
+ * Given an integer array `nums` and an integer `k`, return `true` if there are two **distinct
+ * indices** `i` and `j` in the array such that `nums[i] == nums[j]` and `abs(i - j) <= k`.
+ *
+ * **Constraints:**
+ *
+ * -  `1 <= nums.length <= 105`
+ * - `-109 <= nums[i] <= 109`
+ * - `0 <= k <= 105`
+ *
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {boolean}
+ */
+const containsNearbyDuplicate = function (nums, k) {};
