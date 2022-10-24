70e8580c161126ab771aea8d541ad986075a0acf
yuhcee <egbouchenna001@gmail.com>
2022-10-24 15:28:34 +0100

    :sparkles: (findErrorNums) add task solution

--- Datastructures_&_Algorithms/Arrays/findErrorNums.js
+++ Datastructures_&_Algorithms/Arrays/findErrorNums.js
@@ -18,7 +18,23 @@
  * @param {number[]} nums
  * @return {number[]}
  */
-const findErrorNums = function (nums) {};
+const findErrorNums = function (nums) {
+    // Ex: [1,2,2,4]
+    const n = nums.length;
+
+    const except = (n * (n + 1)) / 2; // 1 + 2 + 3 + 4
+
+    const set = new Set(nums); // {1, 2, 4}
+    // let setSum = 0;
+    // set.forEach((num) => (setSum += num));
+    const setSum = Array.from(set).reduce((a, c) => a + c); // 1 + 2 + 4
+    const numSum = nums.reduce((acc, curr) => acc + curr); // 1 + 2 + 2 + 4
+
+    // numSum - setSum = [1 + 2 + 2 + 4] - [1 + 2 + 4] = 2
+    // except - setSum = [1 + 2 + 3 + 4] - [1 + 2 + 4] = 3
+
+    return [numSum - setSum, except - setSum];
+};
 
 const nums = [1, 2, 2, 4];
 // Output: [2,3]
