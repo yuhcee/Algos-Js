11b3e20e15dab4e4f2624f8e5d1f110805fc9a5c
yuhcee <egbouchenna001@gmail.com>
2022-10-24 22:40:11 +0100

    :white_check_mark: (maxLength) add testcase 2

--- Datastructures_&_Algorithms/Graphs/maxLength.js
+++ Datastructures_&_Algorithms/Graphs/maxLength.js
@@ -29,3 +29,9 @@ const arr = ['un', 'iq', 'ue'];
 - "ique" ("iq" + "ue")
 Maximum length is 4. */
 console.log(maxLength(arr));
+
+const arr1 = ['cha', 'r', 'act', 'ers'];
+// Output: 6
+/* Explanation: Possible longest valid concatenations are "chaers" ("cha" + "ers") and "acters" ("act" + "ers"). */
+console.log(maxLength(arr1));
+
