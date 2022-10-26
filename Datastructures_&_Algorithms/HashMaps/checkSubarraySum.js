ac3385890e153ceab985f983a00c3dd0fede9e71
yuhcee <egbouchenna001@gmail.com>
2022-10-26 23:36:52 +0100

    :books: (checkSubarraySum) add task description

---
+++ Datastructures_&_Algorithms/HashMaps/checkSubarraySum.js
@@ -0,0 +1,21 @@
+/**
+ * **523. Continuous Subarray Sum**
+ *
+ * Given an integer array `nums` and an integer `k`, return *`true` if `nums` has a continuous subarray
+ * of size **at least two** whose elements sum up to a multiple of `k`, or `false` otherwise.
+ *
+ * An integer `x` is a multiple of `k` if there exists an integer n such that `x = n * k`. 0 is always
+ * a multiple of `k`.
+ *
+ * **Constraints:**
+ *
+ * - `1 <= nums.length <= 105`
+ * - `0 <= nums[i] <= 109`
+ * - `0 <= sum(nums[i]) <= 231 - 1`
+ * - `1 <= k <= 231 - 1`
+ *
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {boolean}
+ */
+const checkSubarraySum = function (nums, k) {};
