12a13fdd41deaab7eff19b6c2460d7e40828de35
yuhcee <egbouchenna001@gmail.com>
2022-10-28 22:34:57 +0100

    :sparkles: (groupAnagrams) add task solution

--- Datastructures_&_Algorithms/HashMaps/groupAnagrams.js
+++ Datastructures_&_Algorithms/HashMaps/groupAnagrams.js
@@ -1,14 +1,27 @@
 /**
  * **49. Group Anagrams**
- * 
- * Given an array of strings `strs`, group the **anagrams together**. You can return 
+ *
+ * Given an array of strings `strs`, group the **anagrams together**. You can return
  * the answer in **any order**.
- * 
- * An **Anagram** is a word or phrase formed by rearranging the letters of a 
+ *
+ * An **Anagram** is a word or phrase formed by rearranging the letters of a
  * different word or phrase, typically using all the original letters exactly once.
- * 
+ *
  * @param {string[]} strs
  * @return {string[][]}
  */
 const groupAnagrams = function (strs) {
+    const groups = {};
+
+    for (let str of strs) {
+        let charkey = str.split('').sort().join('');
+
+        if (group[charkey]) {
+            group[charkey].push(str);
+        } else {
+            groupAnagrams[charkey] = [str];
+        }
+    }
+
+    return Object.values(groups);
 };
