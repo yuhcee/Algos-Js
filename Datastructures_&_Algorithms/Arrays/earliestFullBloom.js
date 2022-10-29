7fade1dec7292216b0ed54d852233019843ed14a
yuhcee <egbouchenna001@gmail.com>
2022-10-29 23:49:37 +0100

    :books: (earliestFullBloom) add task description

---
+++ Datastructures_&_Algorithms/Arrays/earliestFullBloom.js
@@ -0,0 +1,24 @@
+/**
+ * **2136. Earliest Possible Day of Full Bloom**
+ *
+ * You have `n` flower seeds. Every seed must be planted first before it can begin to grow, then bloom.
+ * Planting a seed takes time and so does the growth of a seed. You are given two **0-indexed** integer
+ * arrays `plantTime` and `growTime`, of length `n` each:
+ *
+ * - `plantTime[i]` is the number of **full days** it takes you to **plant** the `ith` seed. Every day,
+ * you can work on planting exactly one seed. You **do not** have to work on planting the same seed on
+ * consecutive days, but the planting of a seed is not complete until you have worked `plantTime[i]`
+ * days on planting it in total.
+ *
+ * - `growTime[i]` is the number of **full days** it takes the `ith` seed to grow after being completely
+ * planted. **After** the last day of its growth, the flower **blooms** and stays bloomed forever.
+ *
+ * From the beginning of day `0`, you can plant the seeds in **any** order.
+ *
+ * Return *the **earliest** possible day where **all** seeds are blooming*.
+ *
+ * @param {number[]} plantTime
+ * @param {number[]} growTime
+ * @return {number}
+ */
+const earliestFullBloom = function (plantTime, growTime) {};
