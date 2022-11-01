2d12014b664eacae646d2e30f0500ee5e65231ba
yuhcee <egbouchenna001@gmail.com>
2022-11-01 23:21:46 +0100

    :sparkles:(isToeplitzMatrix) add array manipulation solution

--- Datastructures_&_Algorithms/Arrays/isToeplitzMatrix.js
+++ Datastructures_&_Algorithms/Arrays/isToeplitzMatrix.js
@@ -33,6 +33,18 @@ const isToeplitzMatrix = (matrix) => {
     return true;
 };
 
+// =============== ARRAY MANIPULATION ===================
+const isToeplitzMatrixII = (matrix) => {
+    for (let i = 0; i < matrix.length - 1; i++) {
+        for (let j = 0; j < matrix[0].length - 1; j++) {
+            if (matrix[i][j] !== matrix[i + 1][j + 1]) {
+                return false;
+            }
+        }
+    }
+    return true;
+};
+
 const matrix = [
     [1, 2, 3, 4],
     [5, 1, 2, 3],
