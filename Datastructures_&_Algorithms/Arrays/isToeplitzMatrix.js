0f2779b9989863bb8e5680d1b0ce5fd6b9bcc20d
yuhcee <egbouchenna001@gmail.com>
2022-11-01 23:06:38 +0100

    :books:(isToeplitzMatrix) add task description

---
+++ Datastructures_&_Algorithms/Arrays/isToeplitzMatrix.js
@@ -0,0 +1,20 @@
+/**
+ * **766. Toeplitz Matrix**
+ *
+ * Given an `m x n` `matrix`, return *`true` if the
+ * matrix is Toeplitz. Otherwise, return `false`.
+ *
+ * A matrix is **Toeplitz** if every diagonal from
+ * top-left to bottom-right has the same elements.
+ *
+ * **Constraints:**
+ *
+ * - `m == matrix.length`
+ * - `n == matrix[i].length`
+ * - `1 <= m, n <= 20`
+ * - `0 <= matrix[i][j] <= 99`
+ *
+ * @param {number[][]} matrix
+ * @return {boolean}
+ */
+const isToeplitzMatrix = (matrix) => {};
