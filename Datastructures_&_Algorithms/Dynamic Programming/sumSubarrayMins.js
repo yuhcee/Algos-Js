b99822aef6fde1af91ec33a1111605fcd7526cb6
yuhcee <egbouchenna001@gmail.com>
2022-11-06 12:55:34 +0100

    :white_check_mark: (sumSubarrayMins) add tests

--- Datastructures_&_Algorithms/Dynamic Programming/sumSubarrayMins.js
+++ Datastructures_&_Algorithms/Dynamic Programming/sumSubarrayMins.js
@@ -6,10 +6,10 @@
  * Since the answer may be large, return the answer **modulo** `109 + 7`.
  *
  * **Constraints:**
- * 
+ *
  * `1 <= arr.length <= 3 * 104`
  * `1 <= arr[i] <= 3 * 104`
- * 
+ *
  * @param {number[]} arr
  * @return {number}
  */
@@ -41,3 +41,15 @@ var sumSubarrayMins = function (arr) {
 
     return ans % MOD;
 };
+
+const arr = [3, 1, 2, 4];
+// Output: 17
+/* Explanation: 
+Subarrays are [3], [1], [2], [4], [3,1], [1,2], [2,4], [3,1,2], [1,2,4], [3,1,2,4]. 
+Minimums are 3, 1, 2, 4, 1, 1, 2, 1, 1, 1.
+Sum is 17. */
+console.log(sumSubarrayMins(arr));
+
+const arr1 = [11, 81, 94, 43, 3];
+// Output: 444
+console.log(sumSubarrayMins(arr1));
