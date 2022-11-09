90a28e4cb9dd68bbe1b72991dea12cfdc09e41f3
yuhcee <egbouchenna001@gmail.com>
2022-11-09 23:55:20 +0100

    :sparkles: (makeGood) add task solution

--- Datastructures_&_Algorithms/Strings/makeGood.js
+++ Datastructures_&_Algorithms/Strings/makeGood.js
@@ -22,4 +22,22 @@
  * @param {string} s
  * @return {string}
  */
-const makeGood = function (s) {};
+const makeGood = function (s) {
+    let str = '';
+    for (let i = 0; i < s.length; i += 1) {
+        if (!str) {
+            str = s[i];
+            continue;
+        }
+        let char = str[str.length - 1];
+        
+        if (char === char.toUpperCase()) {
+            if (s[i] === char.toLowerCase()) str = str.slice(0, str.length - 1);
+            else str += s[i];
+        } else {
+            if (s[i] === char.toUpperCase()) str = str.slice(0, str.length - 1);
+            else str += s[i];
+        }
+    }
+    return str;
+};
