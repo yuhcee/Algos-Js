17aa5c28365392085a012b5197f3cb3aa2b74b60
yuhcee <egbouchenna001@gmail.com>
2022-11-11 20:37:10 +0100

    :white_check_mark: (removeDuplicates) add testcase 2

--- Datastructures_&_Algorithms/Arrays/removeDuplicates.js
+++ Datastructures_&_Algorithms/Arrays/removeDuplicates.js
@@ -50,3 +50,9 @@ const nums = [1, 1, 2];
 /* Explanation: Your function should return k = 2, with the first two elements of nums being 1 and 2 respectively.
 It does not matter what you leave beyond the returned k (hence they are underscores). */
 console.log(removeDuplicates(nums));
+
+const nums1 = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4];
+// Output: 5, nums = [0,1,2,3,4,_,_,_,_,_]
+/* Explanation: Your function should return k = 5, with the first five elements of nums being 0, 1, 2, 3, and 4 respectively.
+It does not matter what you leave beyond the returned k (hence they are underscores). */
+console.log(removeDuplicates(nums1));
