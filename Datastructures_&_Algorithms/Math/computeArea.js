aa67dbe29a5bf73c957af8b008b148fd74a3b3fe
yuhcee <egbouchenna001@gmail.com>
2022-11-17 22:58:20 +0100

    :books: (computeArea) add task description

---
+++ Datastructures_&_Algorithms/Math/computeArea.js
@@ -0,0 +1,30 @@
+/**
+ * **223. Rectangle Area**
+ *
+ * Given the coordinates of two **rectilinear** rectangles in a 2D plane, return
+ * *the total area covered by the two rectangles*.
+ *
+ * The first rectangle is defined by its **bottom-left** corner `(ax1, ay1)` and its
+ * **top-right** corner `(ax2, ay2)`.
+ *
+ * The second rectangle is defined by its **bottom-left** corner `(bx1, by1)` and
+ * its **top-right** corner `(bx2, by2)`.
+ *
+ * **Constraints:**
+ *
+ * - `-104 <= ax1 <= ax2 <= 104`
+ * - `-104 <= ay1 <= ay2 <= 104`
+ * - `-104 <= bx1 <= bx2 <= 104`
+ * - `-104 <= by1 <= by2 <= 104`
+ *
+ * @param {number} ax1
+ * @param {number} ay1
+ * @param {number} ax2
+ * @param {number} ay2
+ * @param {number} bx1
+ * @param {number} by1
+ * @param {number} bx2
+ * @param {number} by2
+ * @return {number}
+ */
+const computeArea = function (ax1, ay1, ax2, ay2, bx1, by1, bx2, by2) {};
