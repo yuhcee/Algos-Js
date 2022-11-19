761b9e14765effe4c2d4bdc603fea1e67ae7b770
yuhcee <egbouchenna001@gmail.com>
2022-11-19 22:54:18 +0100

    :books: (isUgly) add task description

---
+++ Datastructures_&_Algorithms/Math/isUgly.js
@@ -0,0 +1,12 @@
+/**
+ * **263. Ugly Number**
+ * 
+ * An **ugly number** is a positive integer whose prime factors are limited to `2`, 
+ * `3`, and `5`.
+ * 
+ * Given an integer `n`, return *`true` if `n` is an **ugly number***.
+ * 
+ * @param {number} n
+ * @return {boolean}
+ */
+const isUgly = function (n) {};
