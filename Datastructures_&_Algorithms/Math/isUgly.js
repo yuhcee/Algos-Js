4a3b410030bb11359d5ef97fedb89c7dac30296f
yuhcee <egbouchenna001@gmail.com>
2022-11-19 22:59:04 +0100

    :white_check_mark: (isUgly) add testcase 1

--- Datastructures_&_Algorithms/Math/isUgly.js
+++ Datastructures_&_Algorithms/Math/isUgly.js
@@ -1,12 +1,16 @@
 /**
  * **263. Ugly Number**
- * 
- * An **ugly number** is a positive integer whose prime factors are limited to `2`, 
+ *
+ * An **ugly number** is a positive integer whose prime factors are limited to `2`,
  * `3`, and `5`.
- * 
+ *
  * Given an integer `n`, return *`true` if `n` is an **ugly number***.
- * 
+ *
  * @param {number} n
  * @return {boolean}
  */
 const isUgly = function (n) {};
+
+const n = 6;
+// Output: true;
+// Explanation: 6 = 2 × 3
