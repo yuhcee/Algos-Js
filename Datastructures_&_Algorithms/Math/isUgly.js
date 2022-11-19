8bff4e598f62454d33d725204258f4c30e83e2ba
yuhcee <egbouchenna001@gmail.com>
2022-11-19 23:00:59 +0100

    :white_check_mark: (isUgly) add testcase 2

--- Datastructures_&_Algorithms/Math/isUgly.js
+++ Datastructures_&_Algorithms/Math/isUgly.js
@@ -9,8 +9,16 @@
  * @param {number} n
  * @return {boolean}
  */
-const isUgly = function (n) {};
+const isUgly = function (n) {
+    return true;
+};
 
 const n = 6;
 // Output: true;
 // Explanation: 6 = 2 × 3
+console.log(isUgly(n));
+
+const n1 = 1;
+// Output: true
+// Explanation: 1 has no prime factors, therefore all of its prime factors are limited to 2, 3, and 5.
+console.log(isUgly(n1));
