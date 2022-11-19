ad9b86af457f25a5aab94e10e266561309a42f40
yuhcee <egbouchenna001@gmail.com>
2022-11-19 23:07:14 +0100

    :sparkles: (isUgly) add task solution

--- Datastructures_&_Algorithms/Math/isUgly.js
+++ Datastructures_&_Algorithms/Math/isUgly.js
@@ -10,6 +10,14 @@
  * @return {boolean}
  */
 const isUgly = function (n) {
+    if (n < 1) return false;
+
+    while (n > 1) {
+        if (n % 2 === 0) n = n / 2;
+        else if (n % 3 === 0) n = n / 3;
+        else if (n % 5 === 0) n = n / 5;
+        else return false;
+    }
     return true;
 };
 
