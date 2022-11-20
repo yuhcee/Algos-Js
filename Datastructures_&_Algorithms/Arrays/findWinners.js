3382e4e81afe4af83c1d5a359e7392170d7336ee
yuhcee <egbouchenna001@gmail.com>
2022-11-20 14:40:21 +0100

    :books: (findWinners) add task description

--- Datastructures_&_Algorithms/Arrays/findWinners.js
+++ Datastructures_&_Algorithms/Arrays/findWinners.js
@@ -1,4 +1,21 @@
 /**
+ * **2225. Find Players With Zero or One Losses**
+ *
+ * You are given an integer array `matches` where `matches[i] = [winneri, loseri]` indicates that the player `winneri`
+ * defeated player `loseri` in a match.
+ *
+ * Return *a list `answer` of size `2` where*:
+ *
+ * - `answer[0]` is a list of all players that have **not** lost any matches.
+ * - `answer[1]` is a list of all players that have lost exactly **one** match.
+ *
+ * The values in the two lists should be returned in **increasing** order.
+ *
+ * **Note:**
+ *
+ * You should only consider the players that have played **at least one** match.
+ * The testcases will be generated such that no two matches will have the **same** outcome.
+ *
  * @param {number[][]} matches
  * @return {number[][]}
  */
