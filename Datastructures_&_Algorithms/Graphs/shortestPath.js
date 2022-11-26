38f5369850cd21f243b5bb7982b5326f0d02478f
yuhcee <egbouchenna001@gmail.com>
2022-11-26 14:53:37 +0100

    :books: (shortestPath) add task description

---
+++ Datastructures_&_Algorithms/Graphs/shortestPath.js
@@ -0,0 +1,15 @@
+/**
+ * **1293. Shortest Path in a Grid with Obstacles Elimination**
+ * 
+ * You are given an `m x n` integer matrix `grid` where each cell is either `0` (empty) or `1` (obstacle). You can 
+ * move up, down, left, or right from and to an empty cell in **one step**.
+ * 
+ * Return *the minimum number of **steps** to walk from the upper left corner `(0, 0)` to the lower right corner `(m 
+ * - 1, n - 1)` given that you can eliminate **at most** `k` obstacles*. If it is not possible to find such walk 
+ * return `-1`.
+ * 
+ * @param {number[][]} grid
+ * @param {number} k
+ * @return {number}
+ */
+var shortestPath = function (grid, k) {};
