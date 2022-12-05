7d0feb34230e7fa0a554d275137df35ea38e8ab2
yuhcee <egbouchenna001@gmail.com>
2022-12-05 15:55:39 +0100

    :books: (orderlyQueue) add task description

---
+++ Datastructures_&_Algorithms/Sorting/orderlyQueue.js
@@ -0,0 +1,14 @@
+/**
+ * **899. Orderly Queue**
+ *
+ * You are given a string `s` and an integer `k`. You can choose one of the first `k` letters of `s` and append it
+ * at the end of the string..
+ *
+ * Return the *lexicographically smallest string you could have after applying the mentioned step any number of
+ * moves*.
+ *
+ * @param {string} s
+ * @param {number} k
+ * @return {string}
+ */
+var orderlyQueue = function (s, k) {};
