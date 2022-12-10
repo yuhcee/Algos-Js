63331697bed84f107cf4386731f630cb02088d84
yuhcee <egbouchenna001@gmail.com>
2022-12-10 16:19:46 +0100

    :books: (reverseVowels) add task description

---
+++ Datastructures_&_Algorithms/Arrays/reverseVowels.js
@@ -0,0 +1,12 @@
+/**
+ * **345. Reverse Vowels of a String**
+ *
+ * Given a string `s`, reverse only all the vowels in the string and return it.
+ *
+ * The vowels are `'a'`, `'e'`, `'i'`, `'o'`, and `'u'`, and they can appear in both lower and upper cases, more
+ * than once.
+ *
+ * @param {string} s
+ * @return {string}
+ */
+var reverseVowels = function (s) {};
