dd6ab0ea8b974e8c9af3297c9506eb704b0b3b7c
yuhcee <egbouchenna001@gmail.com>
2022-12-10 16:21:45 +0100

    :heavy_plus_sign: (reverseVowels) add constraints

--- Datastructures_&_Algorithms/Arrays/reverseVowels.js
+++ Datastructures_&_Algorithms/Arrays/reverseVowels.js
@@ -6,7 +6,12 @@
  * The vowels are `'a'`, `'e'`, `'i'`, `'o'`, and `'u'`, and they can appear in both lower and upper cases, more
  * than once.
  *
+ * **Constraints:**
+ *
+ * - `1 <= s.length <= 3 * 105`
+ * - `s` consist of **printable ASCII** characters.
+ *
  * @param {string} s
  * @return {string}
  */
-var reverseVowels = function (s) {};
+const reverseVowels = function (s) {};
