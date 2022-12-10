e6c8e4a3883f016dcdc41e517c3893e62c6e6d19
yuhcee <egbouchenna001@gmail.com>
2022-12-10 16:25:45 +0100

    :sparkles: (reverseVowels) add task solution

--- Datastructures_&_Algorithms/Arrays/reverseVowels.js
+++ Datastructures_&_Algorithms/Arrays/reverseVowels.js
@@ -14,4 +14,31 @@
  * @param {string} s
  * @return {string}
  */
-const reverseVowels = function (s) {};
+const reverseVowels = function (s) {
+    // Set to keep track of all possible vowels
+    const vowels = new Set(['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']);
+    // right pointer starting at string length - 1
+    // left pointer starting at 0
+    // result is an array of characters
+    let right = s.length - 1,
+        left = 0,
+        result = s.split('');
+
+    while (left < right) {
+        const leftChar = result[left],
+            rightChar = result[right];
+        // increment the left pointer by 1 if left index is not a vowel
+        if (!vowels.has(leftChar)) left++;
+        // decrement the right pointer by 1 if right index is not a vowel
+        if (!vowels.has(rightChar)) right--;
+        // if both left and right pointers are vowels swap the values
+        // increment left and decrment right
+        if (vowels.has(leftChar) && vowels.has(rightChar)) {
+            [result[right], result[left]] = [result[left], result[right]];
+            left++;
+            right--;
+        }
+    }
+    // join the result array
+    return result.join('');
+};
