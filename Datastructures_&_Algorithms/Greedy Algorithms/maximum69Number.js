5d1b4c237aa596d6ce8a5eff65b836f0f0e82d92
yuhcee <egbouchenna001@gmail.com>
2022-12-11 21:47:28 +0100

    :heavy_plus_sign: (maximum69Number) add constraints

--- Datastructures_&_Algorithms/Greedy Algorithms/maximum69Number.js
+++ Datastructures_&_Algorithms/Greedy Algorithms/maximum69Number.js
@@ -5,6 +5,10 @@
  * 
  * Return *the maximum number you can get by changing **at most** one digit (`6` becomes `9`, and `9` becomes `6`)*.
  * 
+ * **Constraints:**
+ * - `1 <= num <= 104`
+ * - `num` consists of only `6` and `9` digits.
+ * 
  * @param {number} num
  * @return {number}
  */
