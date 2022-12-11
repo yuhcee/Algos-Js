07d61b9bcd294d81bc60f5a6d0a0627684c59d69
yuhcee <egbouchenna001@gmail.com>
2022-12-11 21:50:58 +0100

    :white_check_mark: (maximum69Number) add test 2

--- Datastructures_&_Algorithms/Greedy Algorithms/maximum69Number.js
+++ Datastructures_&_Algorithms/Greedy Algorithms/maximum69Number.js
@@ -1,17 +1,27 @@
 /**
  * **1323. Maximum 69 Number**
- * 
+ *
  * You are given a positive integer `num` consisting only of digits `6` and `9`.
- * 
+ *
  * Return *the maximum number you can get by changing **at most** one digit (`6` becomes `9`, and `9` becomes `6`)*.
- * 
+ *
  * **Constraints:**
  * - `1 <= num <= 104`
  * - `num` consists of only `6` and `9` digits.
- * 
+ *
  * @param {number} num
  * @return {number}
  */
-var maximum69Number = function (num) {
+const maximum69Number = function (num) {
     return Number(num.toString().replace(/6/, '9'));
 };
+
+const num = 9669;
+// Output: 9969
+/* Explanation: 
+Changing the first digit results in 6669.
+Changing the second digit results in 9969.
+Changing the third digit results in 9699.
+Changing the fourth digit results in 9666.
+The maximum number is 9969. */
+console.log(maximum69Number(num));
