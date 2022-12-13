bceca17772fc0c409d61766af438c7eeabe4bfca
yuhcee <egbouchenna001@gmail.com>
2022-12-13 22:28:57 +0100

    :sparkles: (StockSpanner) add task solution

--- Datastructures_&_Algorithms/Stacks/StockSpanner.js
+++ Datastructures_&_Algorithms/Stacks/StockSpanner.js
@@ -31,3 +31,23 @@
 const StockSpanner = function () {
     this.stack = { val: 0, span: 0, next: null };
 };
+StockSpanner.prototype.next = function(price) {
+    let chain = 1,
+		cur = this.stack;
+    if (price < this.stack.val) {
+        this.stack = { val: price, span: 1, next: this.stack };
+    } else {
+        while (cur !== null && price >= cur.val) {
+            chain += cur.span;
+            cur = cur.next
+        };
+        this.stack = { val: price, span: chain, next: cur };
+    };
+    return chain;
+};
+
+/** 
+ * Your StockSpanner object will be instantiated and called as such:
+ * var obj = new StockSpanner()
+ * var param_1 = obj.next(price)
+ */
