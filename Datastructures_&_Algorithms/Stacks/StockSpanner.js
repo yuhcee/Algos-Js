9943da8712c32fab3341629e78386f62d56acb41
yuhcee <egbouchenna001@gmail.com>
2022-12-13 22:28:03 +0100

    :heavy_plus_sign: (StockSpanner) add constraints

--- Datastructures_&_Algorithms/Stacks/StockSpanner.js
+++ Datastructures_&_Algorithms/Stacks/StockSpanner.js
@@ -4,7 +4,7 @@
  * Design an algorithm that collects daily price quotes for some stock and returns **the span** of that stock's
  * price for the current day.
  *
- * The **span** of the stock's price in one day is the maximum number of consecutive days (starting from that day 
+ * The **span** of the stock's price in one day is the maximum number of consecutive days (starting from that day
  * and going backward) for which the stock price was less than or equal to the price of that day.
  *
  * - For example, if the prices of the stock in the last four days is `[7,2,1,2]` and the price of the stock today
@@ -20,7 +20,14 @@
  * `StockSpanner()` Initializes the object of the class.
  * `int next(int price)` Returns the *span* of the stock's price given that today's price is `price`.
  *
+ * **Constraints:**
+ *
+ * 1 <= price <= 105
+ * At most `104` calls will be made to `next`.
+ *
  * @param {number} price
  * @return {number}
  */
-const StockSpanner = function () {};
+const StockSpanner = function () {
+    this.stack = { val: 0, span: 0, next: null };
+};
