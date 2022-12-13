5ed5f15726b4f19675f00b29910c2c3e33045c3e
yuhcee <egbouchenna001@gmail.com>
2022-12-13 22:41:33 +0100

    :white_check_mark: (removeDuplicates) add test 2

--- Datastructures_&_Algorithms/Stacks/removeDuplicates.js
+++ Datastructures_&_Algorithms/Stacks/removeDuplicates.js
@@ -27,3 +27,7 @@ const s = 'abbaca';
 /* Explanation: 
 For example, in "abbaca" we could remove "bb" since the letters are adjacent and equal, and this is the only possible move.  The result of this move is that the string is "aaca", of which only "aa" is possible, so the final string is "ca". */
 console.log(removeDuplicates(s));
+
+const s1 = 'azxxzy';
+// Output: "ay"
+console.log(removeDuplicates(s1));
