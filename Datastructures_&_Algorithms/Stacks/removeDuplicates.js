692c0af55f9c85379f14d5a71877c07ff755e3c8
yuhcee <egbouchenna001@gmail.com>
2022-12-13 22:40:55 +0100

    :white_check_mark: (removeDuplicates) add test 1

--- Datastructures_&_Algorithms/Stacks/removeDuplicates.js
+++ Datastructures_&_Algorithms/Stacks/removeDuplicates.js
@@ -21,3 +21,9 @@ const removeDuplicates = (s, result = []) => {
 
     return result.join('');
 };
+
+const s = 'abbaca';
+// Output: "ca"
+/* Explanation: 
+For example, in "abbaca" we could remove "bb" since the letters are adjacent and equal, and this is the only possible move.  The result of this move is that the string is "aaca", of which only "aa" is possible, so the final string is "ca". */
+console.log(removeDuplicates(s));
