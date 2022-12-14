3976834dcaae9e61b989cc0ff1ac15e4e2e66512
yuhcee <egbouchenna001@gmail.com>
2022-12-14 22:56:17 +0100

    :sparkles: (removeDuplicates) add task solution

--- Datastructures_&_Algorithms/Arrays/removeDuplicatesFromSortedArray.js
+++ Datastructures_&_Algorithms/Arrays/removeDuplicatesFromSortedArray.js
@@ -37,4 +37,17 @@
  * @param {number[]} nums
  * @return {number}
  */
-const removeDuplicates = function (nums) {};
+const removeDuplicates = function (nums) {
+    const len = nums.length;
+
+    for (i = 0; i < len - 1; i++) {
+        if (i > nums.length - 1) {
+            break;
+        }
+        if (nums[i] === nums[i + 1]) {
+            nums.splice(i, 1);
+            i--;
+        }
+    }
+    return nums.length;
+};
