6ea4e7e3bbd52d5b08be5171d747fa0ccdb58808
yuhcee <egbouchenna001@gmail.com>
2022-12-14 22:57:16 +0100

    :white_check_mark: (removeDuplicates) add test 1

--- Datastructures_&_Algorithms/Arrays/removeDuplicatesFromSortedArray.js
+++ Datastructures_&_Algorithms/Arrays/removeDuplicatesFromSortedArray.js
@@ -51,3 +51,9 @@ const removeDuplicates = function (nums) {
     }
     return nums.length;
 };
+
+const nums = [1, 1, 2];
+// Output: 2, nums = [1,2,_]
+/* Explanation: Your function should return k = 2, with the first two elements of nums being 1 and 2 respectively.
+It does not matter what you leave beyond the returned k (hence they are underscores). */
+console.log(removeDuplicates(nums));
