559732861b9606042b119e363a253c4c99551241
yuhcee <egbouchenna001@gmail.com>
2022-12-15 23:09:06 +0100

    :sparkles: (removeStones) add task solution

--- Datastructures_&_Algorithms/Graphs/removeStones.js
+++ Datastructures_&_Algorithms/Graphs/removeStones.js
@@ -11,7 +11,7 @@
  * return *the largest possible number of stones that can be removed*.
  *
  * **Constraints:**
- * 
+ *
  * - `1 <= stones.length <= 1000`
  * - `0 <= xi, yi <= 104`
  * - No two stones are at the same coordinate point.
@@ -19,4 +19,34 @@
  * @param {number[][]} stones
  * @return {number}
  */
-const removeStones = (stones) => {};
+const removeStones = (stones) => {
+    // find the number of valid stones
+    // if valid, traverse stones and look for same row and col then remove those stones
+    const visited = new Set();
+    let valid = 0;
+
+    // find and remove stones that have same row and col
+    const traverse = (row, col) => {
+        const key = `${row}-${col}`;
+
+        if (visited.has(key)) return;
+
+        visited.add(key); // mark visited
+        for (const [x, y] of stones) {
+            // if row or col is same, should be removed
+            if (row === x || col === y) traverse(x, y);
+        }
+    };
+
+    for (const [x, y] of stones) {
+        const key = `${x}-${y}`;
+
+        if (visited.has(key)) continue;
+
+        traverse(x, y);
+        valid++;
+    }
+
+    // subtract number of valid nodes from total number of stones
+    return stones.length - valid;
+};
