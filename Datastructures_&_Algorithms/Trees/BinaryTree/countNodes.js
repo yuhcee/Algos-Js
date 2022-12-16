cf165b244772f7bcb8a588e7d367d9ab2f339021
yuhcee <egbouchenna001@gmail.com>
2022-12-16 23:20:51 +0100

    :white_check_mark: (countNodes) add test 1

--- Datastructures_&_Algorithms/Trees/BinaryTree/countNodes.js
+++ Datastructures_&_Algorithms/Trees/BinaryTree/countNodes.js
@@ -22,3 +22,15 @@ const countNodes = function (root) {
     if (!root) return 0;
     return countNodes(root.left) + countNodes(root.right) + 1;
 };
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+
+const root = [1, 2, 3, 4, 5, 6];
+// Output: 6
