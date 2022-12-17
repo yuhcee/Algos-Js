a599e573ca867ad61d325f001008428e9478f556
yuhcee <egbouchenna001@gmail.com>
2022-12-17 07:50:39 +0100

    :heavy_plus_sign: (calculate) add constraints

--- Datastructures_&_Algorithms/Stacks/calculate.js
+++ Datastructures_&_Algorithms/Stacks/calculate.js
@@ -7,6 +7,16 @@
  * *Note*: You are not allowed to use any built-in function which evaluates strings as mathematical expressions,
  * such as `eval()`.
  *
+ * **Constraints:**
+ *
+ * - `1 <= s.length <= 3 * 105`
+ * - `s` consists of digits, `'+'`, `'-'`, `'('`, `')'`, and `' '`.
+ * - `s` represents a valid expression.
+ * - `'+'` is not used as a unary operation (i.e., `"+1"` and `"+(2 + 3)"` is invalid).
+ * - `'-'` could be used as a unary operation (i.e., `"-1"` and `"-(2 + 3)"` is valid).
+ * - There will be no two consecutive operators in the input.
+ * - Every number and running calculation will fit in a signed 32-bit integer.
+ *
  * @param {string} s
  * @return {number}
  */
