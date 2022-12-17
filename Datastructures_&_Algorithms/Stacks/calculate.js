2cb58e13c761ae5e3ca9a181d18654abe83cfa04
yuhcee <egbouchenna001@gmail.com>
2022-12-17 07:47:26 +0100

    :books: (calculate) add task description

---
+++ Datastructures_&_Algorithms/Stacks/calculate.js
@@ -0,0 +1,13 @@
+/**
+ * **224. Basic Calculator**
+ *
+ * Given a string `s` representing a valid expression, implement a basic calculator to evaluate it, and return *
+ * the result of the evaluation*.
+ *
+ * *Note*: You are not allowed to use any built-in function which evaluates strings as mathematical expressions,
+ * such as `eval()`.
+ *
+ * @param {string} s
+ * @return {number}
+ */
+const calculate = function (s) {};
