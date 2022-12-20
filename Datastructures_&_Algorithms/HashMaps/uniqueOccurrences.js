1b1c4bb91607f900d417e50b64bb2a9f3e94be1a
yuhcee <egbouchenna001@gmail.com>
2022-12-20 08:44:26 +0100

    :heavy_plus_sign: (uniqueOccurrences) add constraints

--- Datastructures_&_Algorithms/HashMaps/uniqueOccurrences.js
+++ Datastructures_&_Algorithms/HashMaps/uniqueOccurrences.js
@@ -4,6 +4,11 @@
  * Given an array of integers `arr`, return `true` if the number of occurrences of each value in the array is
  * **unique** or `false` otherwise.
  *
+ * **Constraints:**
+ *
+ * - `1 <= arr.length <= 1000`
+ * - `-1000 <= arr[i] <= 1000`
+ *
  * @param {number[]} arr
  * @return {boolean}
  */
