ee4077db079cadbfbfed0b5d03ca0e02ad60ef6b
yuhcee <egbouchenna001@gmail.com>
2022-12-20 08:59:17 +0100

    :white_check_mark: (uniqueOccurrences) add testcase 3

--- Datastructures_&_Algorithms/HashMaps/uniqueOccurrences.js
+++ Datastructures_&_Algorithms/HashMaps/uniqueOccurrences.js
@@ -35,3 +35,7 @@ console.log(uniqueOccurrences(arr));
 const arr1 = [1, 2];
 // Output: false
 console.log(uniqueOccurrences(arr1));
+
+const arr2 = [-3, 0, 1, -3, 1, 1, 1, -3, 10, 0];
+// Output: true
+console.log(uniqueOccurrences(arr2));
