f915bc0afb38f870f86ecbed91cc134633c8689e
yuhcee <egbouchenna001@gmail.com>
2022-12-20 08:56:47 +0100

    :white_check_mark: (uniqueOccurrences) add testcase 1

--- Datastructures_&_Algorithms/HashMaps/uniqueOccurrences.js
+++ Datastructures_&_Algorithms/HashMaps/uniqueOccurrences.js
@@ -26,3 +26,8 @@ const uniqueOccurrences = function (arr) {
 
     return resultToReturn ? false : true;
 };
+
+const arr = [1, 2, 2, 1, 1, 3];
+// Output: true
+// Explanation: The value 1 has 3 occurrences, 2 has 2 and 3 has 1. No two values have the same number of occurrences.
+console.log(uniqueOccurrences(arr));
