7f5977fa95e3943ba03844cb4c13beeb084fabe0
yuhcee <egbouchenna001@gmail.com>
2022-12-20 08:43:18 +0100

    :books: (uniqueOccurrences) add task description

---
+++ Datastructures_&_Algorithms/HashMaps/uniqueOccurrences.js
@@ -0,0 +1,10 @@
+/**
+ * **1207. Unique Number of Occurrences**
+ *
+ * Given an array of integers `arr`, return `true` if the number of occurrences of each value in the array is
+ * **unique** or `false` otherwise.
+ *
+ * @param {number[]} arr
+ * @return {boolean}
+ */
+const uniqueOccurrences = function (arr) {};
