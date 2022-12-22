0d0f6a105be38efa9f04516f7b88dfa074151c62
yuhcee <egbouchenna001@gmail.com>
2022-12-22 10:54:58 +0100

    :sparkles: (halvesAreAlike) add task solution

--- Datastructures_&_Algorithms/Strings/halvesAreAlike.js
+++ Datastructures_&_Algorithms/Strings/halvesAreAlike.js
@@ -21,7 +21,9 @@
 const halvesAreAlike = (s) => {
     let sum = 0;
     const vowels = new Set(['A', 'E', 'I', 'O', 'U', 'a', 'e', 'i', 'o', 'u']);
-    for (let i = 0; i < s.length / 2; i++) sum += vowels.has(s[i]) - vowels.has(s[s.length - 1 - i]);
+    for (let i = 0; i < s.length / 2; i++) {
+        sum += vowels.has(s[i]) - vowels.has(s[s.length - 1 - i]);
+    }
     return !sum;
 };
 
