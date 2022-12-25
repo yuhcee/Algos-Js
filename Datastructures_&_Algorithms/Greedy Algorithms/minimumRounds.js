c838e2d8ffb7daa6e50dcaf70740995ce864be6e
yuhcee <egbouchenna001@gmail.com>
2022-12-25 07:55:12 +0100

    :books: (minimumRounds) add task description

---
+++ Datastructures_&_Algorithms/Greedy Algorithms/minimumRounds.js
@@ -0,0 +1,13 @@
+/**
+ * **2244. Minimum Rounds to Complete All Tasks**
+ *
+ * You are given a **0-indexed** integer array `tasks`, where `tasks[i]` represents the difficulty level of a task. In
+ * each round, you can complete either 2 or 3 tasks of the **same difficulty level**.
+ *
+ * Return *the **minimum** rounds required to complete all the tasks, or `-1` if it is not possible to complete all the
+ * tasks.
+ *
+ * @param {number[]} tasks
+ * @return {number}
+ */
+const minimumRounds = function (tasks) {};
