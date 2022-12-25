03921b8207d529da062cd1e4055bd9435dd4cbb6
yuhcee <egbouchenna001@gmail.com>
2022-12-25 07:58:45 +0100

    :white_check_mark: (minimumRounds) add testcase 2

--- Datastructures_&_Algorithms/Greedy Algorithms/minimumRounds.js
+++ Datastructures_&_Algorithms/Greedy Algorithms/minimumRounds.js
@@ -40,3 +40,8 @@ const tasks = [2, 2, 3, 3, 2, 4, 4, 4, 4, 4];
 - In the fourth round, you complete 2 tasks of difficulty level 4.  
 It can be shown that all the tasks cannot be completed in fewer than 4 rounds, so the answer is 4. */
 console.log(minimumRounds(tasks));
+
+const tasks1 = [2, 3, 3];
+// Output: -1
+/* Explanation: There is only 1 task of difficulty level 2, but in each round, you can only complete either 2 or 3 tasks of the same difficulty level. Hence, you cannot complete all the tasks, and the answer is -1. */
+console.log(minimumRounds(tasks1));
