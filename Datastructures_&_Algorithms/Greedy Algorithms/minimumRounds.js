2f534d4c7067bf0edd4a521c445ac901982a03dc
yuhcee <egbouchenna001@gmail.com>
2022-12-25 07:56:58 +0100

    :sparkles: (minimumRounds) add task solution

--- Datastructures_&_Algorithms/Greedy Algorithms/minimumRounds.js
+++ Datastructures_&_Algorithms/Greedy Algorithms/minimumRounds.js
@@ -15,4 +15,18 @@
  * @param {number[]} tasks
  * @return {number}
  */
-const minimumRounds = function (tasks) {};
+const minimumRounds = function (tasks) {
+    const hash = {};
+    let minRounds = 0;
+
+    for (const task of tasks) {
+        hash[task] = hash[task] + 1 || 1;
+    }
+
+    for (const count of Object.values(hash)) {
+        if (count < 2) return -1;
+        minRounds += Math.ceil(count / 3);
+    }
+
+    return minRounds;
+};
