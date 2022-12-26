fa0f3b21179c7aae0bfe5048319685089387484e
yuhcee <egbouchenna001@gmail.com>
2022-12-26 10:13:19 +0100

    :white_check_mark: (removeStars) add testcase 1

--- Datastructures_&_Algorithms/Stacks/removeStars.js
+++ Datastructures_&_Algorithms/Stacks/removeStars.js
@@ -37,3 +37,12 @@ const removeStars = function (s) {
     }
     return stack.join(''); // Return the string formed by joining the characters in the stack
 };
+
+const s = 'leet**cod*e';
+// Output: "lecoe"
+/* Explanation: Performing the removals from left to right:
+- The closest character to the 1st star is 't' in "leet**cod*e". s becomes "lee*cod*e".
+- The closest character to the 2nd star is 'e' in "lee*cod*e". s becomes "lecod*e".
+- The closest character to the 3rd star is 'd' in "lecod*e". s becomes "lecoe".
+There are no more stars, so we return "lecoe". */
+console.log(removeStars(s));
