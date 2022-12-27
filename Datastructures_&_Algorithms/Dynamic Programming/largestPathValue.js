0da906eacede10c146d8b8d59842085fa2fe7f40
yuhcee <egbouchenna001@gmail.com>
2022-12-27 14:46:15 +0100

    :white_check_mark: (removeStars) add testcase 1

--- Datastructures_&_Algorithms/Dynamic Programming/largestPathValue.js
+++ Datastructures_&_Algorithms/Dynamic Programming/largestPathValue.js
@@ -83,3 +83,14 @@ const largestPathValue = function (colors, edges) {
 
     return result;
 };
+
+const colors = 'abaca',
+    edges = [
+        [0, 1],
+        [0, 2],
+        [2, 3],
+        [3, 4],
+    ];
+// Output: 3
+// Explanation: The path 0 -> 2 -> 3 -> 4 contains 3 nodes that are colored "a" (red in the above image).
+console.log(largestPathValue(colors, edges));
