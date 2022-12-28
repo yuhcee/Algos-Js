9c6b4bb81ef37644ae5e1c5f578f73a7cdae5f61
yuhcee <egbouchenna001@gmail.com>
2022-12-28 23:16:45 +0100

    :heavy_plus_sign: (isValid) add constraints

--- Datastructures_&_Algorithms/Stacks/isValid.js
+++ Datastructures_&_Algorithms/Stacks/isValid.js
@@ -10,6 +10,11 @@
  * Open brackets must be closed in the correct order.
  * Every close bracket has a corresponding open bracket of the same type.
  *
+ * **Constraints:**
+ * 
+ * - `1 <= s.length <= 104`
+ * - `s` consists of parentheses only `'()[]{}'`.
+ * 
  * @param {string} s
  * @return {boolean}
  */
