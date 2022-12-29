3f1a841c5bd2e3e728afe1b8af3dc0ac55b0f9e2
yuhcee <egbouchenna001@gmail.com>
2022-12-29 12:27:16 +0100

    :zap: (nearestExit) add tests

--- Datastructures_&_Algorithms/Graphs/nearestExit.js
+++ Datastructures_&_Algorithms/Graphs/nearestExit.js
@@ -55,3 +55,40 @@ const nearestExit = (maze, [y0, x0]) => {
     }
     return -1;
 };
+
+const maze = [
+        ['+', '+', '+'],
+        ['.', '.', '.'],
+        ['+', '+', '+'],
+    ],
+    entrance = [1, 0];
+// Output: 2
+/* Explanation: There is 1 exit in this maze at [1,2].
+[1,0] does not count as an exit since it is the entrance cell.
+Initially, you are at the entrance cell [1,0].
+- You can reach [1,2] by moving 2 steps right.
+Thus, the nearest exit is [1,2], which is 2 steps away. */
+
+console.log(nearestExit(maze, entrance));
+
+const maze1 = [
+        ['+', '+', '.', '+'],
+        ['.', '.', '.', '+'],
+        ['+', '+', '+', '.'],
+    ],
+    entrance1 = [1, 2];
+// Output: 1
+/* Explanation: There are 3 exits in this maze at [1,0], [0,2], and [2,3].
+Initially, you are at the entrance cell [1,2].
+- You can reach [1,0] by moving 2 steps left.
+- You can reach [0,2] by moving 1 step up.
+It is impossible to reach [2,3] from the entrance.
+Thus, the nearest exit is [0,2], which is 1 step away. */
+
+console.log(nearestExit(maze1, entrance1));
+
+const maze2 = [['.', '+']],
+    entrance2 = [0, 0];
+// Output: -1
+// Explanation: There are no exits in this maze.
+console.log(nearestExit(maze2, entrance2));
