a6e7e3d084d89961232d9c9b81b07a40b4128086
yuhcee <egbouchenna001@gmail.com>
2022-12-31 03:01:19 +0100

    :tada: (RunLength) add task solution

---
+++ Datastructures_&_Algorithms/Strings/runLength.ts
@@ -0,0 +1,24 @@
+/**
+ * **RunLength**
+ * 
+ * Have the function `RunLength(str)` take the `str` parameter being passed and return a 
+ * compressed version of the string using the `Run-length encoding algorithm`. This algorithm 
+ * works by taking the occurrence of each repeating character and outputting that number along 
+ * with a single character of the repeating sequence. 
+ * 
+ * For example: `"wwwggopp"` would return `3w2g1o2p`. The string will not contain any numbers, 
+ * punctuation, or symbols.
+ * 
+ * Once your function is working, take the final output string and intersperse it 
+ * character-by-character with your ChallengeToken.
+ * 
+ * Your ChallengeToken: enpohq47kd68
+ * 
+ * 
+ * @param {string} str
+ * @returns {string}
+ * 
+ */
+const RunLength = (str: string): string => {
+    
+}
