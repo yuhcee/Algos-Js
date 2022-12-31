8815308b6d3ad54a1a8943a5ba67076599e7980f
yuhcee <egbouchenna001@gmail.com>
2022-12-31 03:03:51 +0100

    :tada: (RunLength) add task solution

--- Datastructures_&_Algorithms/Strings/runLength.ts
+++ Datastructures_&_Algorithms/Strings/runLength.ts
@@ -1,24 +1,43 @@
 /**
  * **RunLength**
- * 
- * Have the function `RunLength(str)` take the `str` parameter being passed and return a 
- * compressed version of the string using the `Run-length encoding algorithm`. This algorithm 
- * works by taking the occurrence of each repeating character and outputting that number along 
- * with a single character of the repeating sequence. 
- * 
- * For example: `"wwwggopp"` would return `3w2g1o2p`. The string will not contain any numbers, 
+ *
+ * Have the function `RunLength(str)` take the `str` parameter being passed and return a
+ * compressed version of the string using the `Run-length encoding algorithm`. This algorithm
+ * works by taking the occurrence of each repeating character and outputting that number along
+ * with a single character of the repeating sequence.
+ *
+ * For example: `"wwwggopp"` would return `3w2g1o2p`. The string will not contain any numbers,
  * punctuation, or symbols.
- * 
- * Once your function is working, take the final output string and intersperse it 
+ *
+ * Once your function is working, take the final output string and intersperse it
  * character-by-character with your ChallengeToken.
- * 
+ *
  * Your ChallengeToken: enpohq47kd68
- * 
- * 
+ *
+ *
  * @param {string} str
  * @returns {string}
- * 
+ *
  */
 const RunLength = (str: string): string => {
-    
-}
+    let result: string = '';
+    let count: number = 1;
+    const CHALLENGETOKEN: string = 'enpohq47kd68';
+
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] === str[i + 1]) {
+            count++;
+        } else {
+            result += count + str[i];
+            count = 1;
+        }
+    }
+
+    let output: string = '';
+
+    for (let i = 0; i < result.length; i++) {
+        output += result[i] + CHALLENGETOKEN[i % CHALLENGETOKEN.length];
+    }
+
+    return output;
+};
