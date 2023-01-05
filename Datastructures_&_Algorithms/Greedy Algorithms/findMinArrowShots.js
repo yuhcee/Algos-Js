c2dd449f0bf554bbe97a95caa462471938da014a
yuhcee <egbouchenna001@gmail.com>
2023-01-05 10:23:02 +0100

    :white_check_mark: (findMinArrowShots) add testcase 1

--- Datastructures_&_Algorithms/Greedy Algorithms/findMinArrowShots.js
+++ Datastructures_&_Algorithms/Greedy Algorithms/findMinArrowShots.js
@@ -45,3 +45,15 @@ const findMinArrowShots = function (points) {
 
     return arrowCount;
 };
+
+const points = [
+    [10, 16],
+    [2, 8],
+    [1, 6],
+    [7, 12],
+];
+// Output: 2
+/* Explanation: The balloons can be burst by 2 arrows:
+- Shoot an arrow at x = 6, bursting the balloons [2,8] and [1,6].
+- Shoot an arrow at x = 11, bursting the balloons [10,16] and [7,12]. */
+console.log(findMinArrowShots(points));
