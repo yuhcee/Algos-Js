b9e85a98774bf0f504b1f42182b675f974fca335
yuhcee <egbouchenna001@gmail.com>
2023-01-06 16:11:07 +0100

    :white_check_mark: (maxIceCream) add testcase 2

--- Datastructures_&_Algorithms/Greedy Algorithms/maxIceCream.js
+++ Datastructures_&_Algorithms/Greedy Algorithms/maxIceCream.js
@@ -43,3 +43,9 @@ const costs = [1, 3, 2, 4, 1],
 // Output: 4
 // Explanation: The boy can buy ice cream bars at indices 0,1,2,4 for a total price of 1 + 3 + 2 + 1 = 7.
 console.log(maxIceCream(costs, coins));
+
+const costs1 = [10, 6, 8, 7, 7, 8],
+    coins1 = 5;
+// Output: 0
+// Explanation: The boy cannot afford any of the ice cream bars.
+console.log(maxIceCream(costs1, coins1));
