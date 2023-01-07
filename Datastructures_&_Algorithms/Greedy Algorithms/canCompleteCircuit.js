6139b82da82c3eb12b8d51a97284298c7b27967d
yuhcee <egbouchenna001@gmail.com>
2023-01-07 12:28:44 +0100

    :white_check_mark: (canCompleteCircuit) add testcase 2

--- Datastructures_&_Algorithms/Greedy Algorithms/canCompleteCircuit.js
+++ Datastructures_&_Algorithms/Greedy Algorithms/canCompleteCircuit.js
@@ -53,3 +53,14 @@ Travel to station 3. The cost is 5. Your gas is just enough to travel back to st
 Therefore, return 3 as the starting index. */
 console.log(canCompleteCircuit(gas, cost));
 
+const gas1 = [2, 3, 4],
+    cost1 = [3, 4, 3];
+// Output: -1
+/* Explanation:
+You can't start at station 0 or 1, as there is not enough gas to travel to the next station.
+Let's start at station 2 and fill up with 4 unit of gas. Your tank = 0 + 4 = 4
+Travel to station 0. Your tank = 4 - 3 + 2 = 3
+Travel to station 1. Your tank = 3 - 3 + 3 = 3
+You cannot travel back to station 2, as it requires 4 unit of gas but you only have 3.
+Therefore, you can't travel around the circuit once no matter where you start. */
+console.log(canCompleteCircuit(gas1, cost1));
