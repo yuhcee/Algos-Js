fbdb887e5dedc716ec09d14065c2d224f1371de7
yuhcee <egbouchenna001@gmail.com>
2023-01-08 23:48:51 +0100

    :books: (maxPoints) add task description

---
+++ Datastructures_&_Algorithms/HashMaps/maxPoints.js
@@ -0,0 +1,10 @@
+/**
+ * **149. Max Points on a Line**
+ *
+ * Given an array of `points` where `points[i] = [xi, yi]` represents a point on the **X-Y** plane, return *the maximum
+ * number of points that lie on the same straight line*.
+ *
+ * @param {number[][]} points
+ * @return {number}
+ */
+const maxPoints = (points) => {};
