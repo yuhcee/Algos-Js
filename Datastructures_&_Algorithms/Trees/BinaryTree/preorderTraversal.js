09ae6f201bde2839348e8d985b664f8aa7856613
yuhcee <egbouchenna001@gmail.com>
2023-01-09 08:17:56 +0100

    :books: (preorderTraversal) add task description

---
+++ Datastructures_&_Algorithms/Trees/BinaryTree/preorderTraversal.js
@@ -0,0 +1,17 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * **144. Binary Tree Preorder Traversal**
+ *
+ * Given the root of a binary tree, return the *preorder traversal of its nodes' values*.
+ *
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+const preorderTraversal = function (root) {};
