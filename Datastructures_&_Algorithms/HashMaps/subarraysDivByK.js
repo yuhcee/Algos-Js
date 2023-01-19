7120490ea88ce590c85858812ddd28e75e30f5c9
yuhcee <egbouchenna001@gmail.com>
2023-01-19 07:18:00 +0100

    :heavy_plus_sign: (subarraysDivByK) add constraints

--- Datastructures_&_Algorithms/HashMaps/subarraysDivByK.js
+++ Datastructures_&_Algorithms/HashMaps/subarraysDivByK.js
@@ -6,10 +6,14 @@
  *
  * A **subarray** is a **contiguous** part of an array.
  *
+ * **Constraints:**
+ *
+ * - `1 <= nums.length <= 3 * 104`
+ * - `-104 <= nums[i] <= 104`
+ * - `2 <= k <= 104`
+ *
  * @param {number[]} nums
  * @param {number} k
  * @return {number}
  */
-const subarraysDivByK = (nums, k) => { 
-    
-};
+const subarraysDivByK = (nums, k) => {};
