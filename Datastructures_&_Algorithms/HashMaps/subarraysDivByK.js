4a9092a70580a2d69b2d464b3d26dd8dfae98104
yuhcee <egbouchenna001@gmail.com>
2023-01-19 07:20:03 +0100

    :white_check_mark: (subarraysDivByK) add testcase 1

--- Datastructures_&_Algorithms/HashMaps/subarraysDivByK.js
+++ Datastructures_&_Algorithms/HashMaps/subarraysDivByK.js
@@ -36,3 +36,10 @@ const subarraysDivByK = (nums, k) => {
     }
     return s + (map[0] || 0);
 };
+
+const nums = [4, 5, 0, -2, -3, 1],
+    k = 5;
+// Output: 7
+/* Explanation: There are 7 subarrays with a sum divisible by k = 5:
+[4, 5, 0, -2, -3, 1], [5], [5, 0], [5, 0, -2, -3], [0], [0, -2, -3], [-2, -3] */
+console.log(subarraysDivByK(nums, k));
