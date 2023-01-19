017bcfe7530b268c63b699d99ed2f4bcdeea1bfd
yuhcee <egbouchenna001@gmail.com>
2023-01-19 07:16:25 +0100

    :books: (subarraysDivByK) add task description

---
+++ Datastructures_&_Algorithms/HashMaps/subarraysDivByK.js
@@ -0,0 +1,15 @@
+/**
+ * **974. Subarray Sums Divisible by K**
+ *
+ * Given an integer array `nums` and an integer `k`, return *the number of non-empty **subarrays**
+ * that have a sum divisible by `k`.
+ *
+ * A **subarray** is a **contiguous** part of an array.
+ *
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+const subarraysDivByK = (nums, k) => { 
+    
+};
