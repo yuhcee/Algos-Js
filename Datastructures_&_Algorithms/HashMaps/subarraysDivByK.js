3d4259e7131d5df09bf17692ea76f30faae06af7
yuhcee <egbouchenna001@gmail.com>
2023-01-19 07:20:46 +0100

    :white_check_mark: (subarraysDivByK) add testcase 2

--- Datastructures_&_Algorithms/HashMaps/subarraysDivByK.js
+++ Datastructures_&_Algorithms/HashMaps/subarraysDivByK.js
@@ -43,3 +43,8 @@ const nums = [4, 5, 0, -2, -3, 1],
 /* Explanation: There are 7 subarrays with a sum divisible by k = 5:
 [4, 5, 0, -2, -3, 1], [5], [5, 0], [5, 0, -2, -3], [0], [0, -2, -3], [-2, -3] */
 console.log(subarraysDivByK(nums, k));
+
+const nums1 = [5],
+    k1 = 9;
+// Output: 0
+console.log(subarraysDivByK(nums1, k1));
