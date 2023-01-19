8b2a400c762b1bf294184b2ea95df78ab0606c1c
yuhcee <egbouchenna001@gmail.com>
2023-01-19 07:18:53 +0100

    :sparkles: (subarraysDivByK) add task solution

--- Datastructures_&_Algorithms/HashMaps/subarraysDivByK.js
+++ Datastructures_&_Algorithms/HashMaps/subarraysDivByK.js
@@ -16,4 +16,23 @@
  * @param {number} k
  * @return {number}
  */
-const subarraysDivByK = (nums, k) => {};
+const subarraysDivByK = (nums, k) => {
+    let map = {};
+    let sum = 0; // cumulative sum
+
+    for (let i = 0; i < nums.length; i++) {
+        sum += nums[i];
+        // mod twice for negative numbers
+        const key = ((sum % k) + k) % k;
+        // if map[key] is undefined set 1, else increase 1
+        map[key] = map[key] + 1 || 1;
+    }
+
+    let s = 0;
+    for (let i = 0; i < k; i++) {
+        if (map[i] > 1) {
+            s += (map[i] * (map[i] - 1)) / 2; // sum of 1 to map[i] - 1;
+        }
+    }
+    return s + (map[0] || 0);
+};
