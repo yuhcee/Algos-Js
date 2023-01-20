29586988a398de1a83303b60f1e4be1379f84306
yuhcee <egbouchenna001@gmail.com>
2023-01-20 08:35:10 +0100

    :sparkles: (findSubsequences) add task solution

--- Datastructures_&_Algorithms/Arrays/findSubsequences.js
+++ Datastructures_&_Algorithms/Arrays/findSubsequences.js
@@ -12,4 +12,33 @@
  * @param {number[]} nums
  * @return {number[][]}
  */
-const findSubsequences = (nums) => {};
+const findSubsequences = (nums) => {
+    let res = []; // array to store the final result of non-decreasing subsequences
+
+    // backtrack function
+    function backtrack(start, curr) {
+        // if the current subsequence has at least 2 elements
+        if (curr.length >= 2) {
+            res.push(curr.slice()); // add it to the final result
+        }
+
+        const seen = new Set(); // set to keep track of previously seen elements
+
+        for (let i = start; i < nums.length; i++) {
+            // if the element has been seen before, skip it
+            if (seen.has(nums[i])) {
+                continue;
+            }
+            // if the current subsequence is empty or the element is greater than or equal to the last element in the subsequence
+            if (curr.length === 0 || nums[i] >= curr[curr.length - 1]) {
+                seen.add(nums[i]);
+                curr.push(nums[i]); // add the element to the current subsequence
+                backtrack(i + 1, curr); // call the backtracking function recursively
+                curr.pop(); // remove the element from the current subsequence
+            }
+        }
+    }
+
+    backtrack(0, []);
+    return res;
+};
