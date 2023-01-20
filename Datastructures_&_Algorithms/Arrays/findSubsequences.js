95252589cc304e92b47ce7930f33eaeed23d6ede
yuhcee <egbouchenna001@gmail.com>
2023-01-20 08:24:36 +0100

    :books: (findSubsequences) add task description

---
+++ Datastructures_&_Algorithms/Arrays/findSubsequences.js
@@ -0,0 +1,10 @@
+/**
+ * **491. Non-decreasing Subsequences**
+ *
+ * Given an integer array `nums`, return *all the different possible non-decreasing subsequences of
+ * the given array with at least two elements*. You may return the answer in **any order**.
+ *
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+const findSubsequences = (nums) => {};
