a879919073e3dafb7f26237ffae7d7f248a60993
yuhcee <egbouchenna001@gmail.com>
2023-01-21 23:11:57 +0100

    :heavy_plus_sign: (restoreIpAddresses) add constraints

--- Datastructures_&_Algorithms/Graphs/restoreIpAddresses.js
+++ Datastructures_&_Algorithms/Graphs/restoreIpAddresses.js
@@ -11,6 +11,12 @@
  * formed by inserting dots into `s`. You are **not** allowed to reorder or remove any digits in `s`. 
  * You may return the valid IP addresses in **any** order.
  * 
+ * 
+ * **Constraints:**
+ * 
+ * - `1 <= s.length <= 20`
+ * - `s` consists of digits only.
+ * 
  * @param {string} s
  * @return {string[]}
  */
