f85ec41c0ab2ca44494b0041c4f74f9108b6c0e2
yuhcee <egbouchenna001@gmail.com>
2023-01-22 23:04:55 +0100

    :heavy_plus_sign: (partition) add constraints

--- Datastructures_&_Algorithms/Dynamic Programming/partition.js
+++ Datastructures_&_Algorithms/Dynamic Programming/partition.js
@@ -4,6 +4,11 @@
  * Given a string `s`, partition `s` such that every `substring` of the partition is a **palindrome.**
  * Return *all possible palindrome partitioning of `s`*.
  *
+ * **Constraints:**
+ * 
+ * - `1 <= s.length <= 16`
+ * - `s` contains only lowercase English letters.
+ * 
  * @param {string} s
  * @return {string[][]}
  */
