e7c623f052a9801185172c058e442557fd43d0f7
yuhcee <egbouchenna001@gmail.com>
2023-01-22 23:06:19 +0100

    :heavy_plus_sign: (partition) add helper function to backtrack

--- Datastructures_&_Algorithms/Dynamic Programming/partition.js
+++ Datastructures_&_Algorithms/Dynamic Programming/partition.js
@@ -5,11 +5,29 @@
  * Return *all possible palindrome partitioning of `s`*.
  *
  * **Constraints:**
- * 
+ *
  * - `1 <= s.length <= 16`
  * - `s` contains only lowercase English letters.
- * 
+ *
  * @param {string} s
  * @return {string[][]}
  */
 const partition = function (s) {};
+
+function backtrack(s, temp, result) {
+    // base case: if the current string is empty, add the current partition to the result
+    if (s.length === 0) {
+        result.push(temp.slice());
+        return;
+    }
+    // try partitioning the current string into a palindrome substring
+    for (let i = 1; i <= s.length; i++) {
+        let sub = s.substring(0, i);
+        if (isPalindrome(sub)) {
+            // if the substring is a palindrome, add it to the current partition and continue the backtracking process
+            temp.push(sub);
+            backtrack(s.substring(i), temp, result);
+            temp.pop();
+        }
+    }
+}
