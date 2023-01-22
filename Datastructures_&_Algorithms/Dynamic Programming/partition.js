339fbae4f701dbcf40ad22e87170ce7a0a048c51
yuhcee <egbouchenna001@gmail.com>
2023-01-22 23:03:37 +0100

    :books: (partition) add task description

---
+++ Datastructures_&_Algorithms/Dynamic Programming/partition.js
@@ -0,0 +1,10 @@
+/**
+ * **131. Palindrome Partitioning**
+ *
+ * Given a string `s`, partition `s` such that every `substring` of the partition is a **palindrome.**
+ * Return *all possible palindrome partitioning of `s`*.
+ *
+ * @param {string} s
+ * @return {string[][]}
+ */
+const partition = function (s) {};
