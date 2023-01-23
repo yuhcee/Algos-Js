3f6fb60083f9e89713fb324bbc71f52a929a5deb
yuhcee <egbouchenna001@gmail.com>
2023-01-23 23:59:42 +0100

    :sparkles: (findJudge) add task solution

--- Datastructures_&_Algorithms/Graphs/findJudge.js
+++ Datastructures_&_Algorithms/Graphs/findJudge.js
@@ -31,4 +31,30 @@
  * @param {number[][]} trust
  * @return {number}
  */
-const findJudge = function (n, trust) {};
+const findJudge = function (n, trust) {
+    // if there's only 1 person and no trust relationship return 1
+    if (N == 1 && trust.length == 0) return 1;
+    // if there's not enough trust relationship to have a judge return -1
+    if (trust.length < N - 1) return -1;
+
+    // array to keep track of the number of people each person trusts
+    let trustCount = new Array(N + 1).fill(0);
+    // array to keep track of the number of people who trust each person
+    let trustedByCount = new Array(N + 1).fill(0);
+
+    // iterate through trust relationships array
+    for (let i = 0; i < trust.length; i++) {
+        let a = trust[i][0], b = trust[i][1];
+        trustCount[a]++;
+        trustedByCount[b]++;
+    }
+
+    // iterate through trustCount and trustedByCount arrays
+    for (let i = 1; i <= N; i++) {
+        // check if there is exactly one person who trusts no one and is trusted by everyone else
+        if (trustCount[i] == 0 && trustedByCount[i] == N - 1) return i;
+    }
+
+    // no judge found
+    return -1;
+};
