a9102787b8b2fb854ae3945b419fbe9a5a9c9e6d
yuhcee <egbouchenna001@gmail.com>
2023-01-26 08:25:31 +0100

    :white_check_mark: (findCheapestPrice) add testcase 1

--- Datastructures_&_Algorithms/Dynamic Programming/findCheapestPrice.js
+++ Datastructures_&_Algorithms/Dynamic Programming/findCheapestPrice.js
@@ -72,3 +72,21 @@ const findCheapestPrice = (n, flights, src, dst, k) => {
     // Otherwise, return the last element of the array
     return dp[dst][k + 1] === Number.MAX_SAFE_INTEGER ? -1 : dp[dst][k + 1];
 };
+
+const n = 4,
+    flights = [
+        [0, 1, 100],
+        [1, 2, 100],
+        [2, 0, 100],
+        [1, 3, 600],
+        [2, 3, 200],
+    ],
+    src = 0,
+    dst = 3,
+    k = 1;
+// Output: 700
+/* Explanation:
+The graph is shown above.
+The optimal path with at most 1 stop from city 0 to 3 is marked in red and has cost 100 + 600 = 700.
+Note that the path through cities [0,1,2,3] is cheaper but is invalid because it uses 2 stops. */
+console.log(findCheapestPrice(n, flights, src, dst, k));
