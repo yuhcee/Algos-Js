12e1dc6228b222ade9cab110a0d313983d4523d3
yuhcee <egbouchenna001@gmail.com>
2023-01-26 08:16:49 +0100

    :heavy_plus_sign: (findCheapestPrice) add constraints

--- Datastructures_&_Algorithms/Dynamic Programming/findCheapestPrice.js
+++ Datastructures_&_Algorithms/Dynamic Programming/findCheapestPrice.js
@@ -8,6 +8,18 @@
  * You are also given three integers `src`, `dst`, and `k`, return *the **cheapest price** from `src` 
  * to `dst` with at most `k` stops. If there is no such route, return `-1`.
  * 
+ * **Constraints**:
+ * 
+ * - `1 <= n <= 100`
+ * - `0 <= flights.length <= (n * (n - 1) / 2)`
+ * - `flights[i].length == 3`
+ * - `0 <= fromi, toi < n`
+ * - `fromi != toi`
+ * - `1 <= pricei <= 104`
+ * - There will not be any multiple flights between two cities.
+ * - `0 <= src, dst, k < n`
+ * - `src != dst`
+ * 
  * @param {number} n
  * @param {number[][]} flights
  * @param {number} src
