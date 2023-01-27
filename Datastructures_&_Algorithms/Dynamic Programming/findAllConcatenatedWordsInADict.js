c6b5be162b1558269a692f0cad5e909eeedc7e18
yuhcee <egbouchenna001@gmail.com>
2023-01-27 08:43:40 +0100

    :books: (findAllConcatenatedWordsInADict) add task description

---
+++ Datastructures_&_Algorithms/Dynamic Programming/findAllConcatenatedWordsInADict.js
@@ -0,0 +1,13 @@
+/**
+ * **472. Concatenated Words**
+ *
+ * Given an array of strings `words` **(without duplicates)**, return *all the **concatenated words**
+ * in the given list of `words`.
+ *
+ * A **concatenated word** is defined as a string that is comprised entirely of at least two shorter
+ * words in the given array.
+ *
+ * @param {string[]} words
+ * @return {string[]}
+ */
+const findAllConcatenatedWordsInADict = function (words) {};
