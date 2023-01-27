e4051657c2abcf488a7ebbbb4a58b7a95012b6e2
yuhcee <egbouchenna001@gmail.com>
2023-01-27 08:46:43 +0100

    :heavy_plus_sign: (findAllConcatenatedWordsInADict) add constraints

--- Datastructures_&_Algorithms/Dynamic Programming/findAllConcatenatedWordsInADict.js
+++ Datastructures_&_Algorithms/Dynamic Programming/findAllConcatenatedWordsInADict.js
@@ -7,6 +7,14 @@
  * A **concatenated word** is defined as a string that is comprised entirely of at least two shorter
  * words in the given array.
  *
+ * **Constraints:**
+ * 
+ * - `1 <= words.length <= 104`
+ * - `1 <= words[i].length <= 30`
+ * - `words[i]` consists of only lowercase English letters.
+ * - All the strings of words are unique.
+ * - `1 <= sum(words[i].length) <= 105`
+ * 
  * @param {string[]} words
  * @return {string[]}
  */
