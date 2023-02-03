d8d19209ee599a7d6ca675a234f5fbb8bb90f994
yuhcee <egbouchenna001@gmail.com>
2023-02-03 12:02:37 +0100

    :heavy_plus_sign: (isAlienSorted) add constraints

--- Datastructures_&_Algorithms/HashMaps/isAlienSorted.js
+++ Datastructures_&_Algorithms/HashMaps/isAlienSorted.js
@@ -1,12 +1,19 @@
 /**
  * **953. Verifying an Alien Dictionary**
- * 
- * In an alien language, surprisingly, they also use English lowercase letters, but possibly in a 
+ *
+ * In an alien language, surprisingly, they also use English lowercase letters, but possibly in a
  * different `order`. The `order` of the alphabet is some permutation of lowercase letters.
- * 
- * Given a sequence of `words` written in the alien language, and the `order` of the alphabet, return 
+ *
+ * Given a sequence of `words` written in the alien language, and the `order` of the alphabet, return
  * `true` if and only if the given `words` are sorted lexicographically in this alien language.
- * 
+ *
+ * **Constraints:**
+ *
+ * - `1 <= words.length <= 100`
+ * - `1 <= words[i].length <= 20`
+ * - `order.length == 26`
+ * - All characters in `words[i]` and `order` are English lowercase letters.
+ *
  * @param {string[]} words
  * @param {string} order
  * @return {boolean}
