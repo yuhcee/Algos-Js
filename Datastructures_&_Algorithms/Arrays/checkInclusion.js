5441b5cb1f9c344c278c1afb4e8e55add51ad4b6
yuhcee <egbouchenna001@gmail.com>
2023-02-04 11:32:22 +0100

    :sparkles: (checkInclusion) add task description

---
+++ Datastructures_&_Algorithms/Arrays/checkInclusion.js
@@ -0,0 +1,13 @@
+/**
+ * **567. Permutation in String**
+ * 
+ * Given two strings `s1` and `s2`, return *`true` if `s2` contains a permutation of s1, or `false` 
+ * otherwise*.
+ * 
+ * In other words, return `true` if one of `s1`'s permutations is the substring of `s2`.
+ * 
+ * @param {string} s1
+ * @param {string} s2
+ * @return {boolean}
+ */
+const checkInclusion = (s1, s2) => {};
