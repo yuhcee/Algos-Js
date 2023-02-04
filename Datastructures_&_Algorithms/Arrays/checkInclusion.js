fc73516b988553f501d87df7f0b875104d9c37b2
yuhcee <egbouchenna001@gmail.com>
2023-02-04 11:39:52 +0100

    :sparkles: (checkInclusion) add task solution

--- Datastructures_&_Algorithms/Arrays/checkInclusion.js
+++ Datastructures_&_Algorithms/Arrays/checkInclusion.js
@@ -15,7 +15,31 @@
  * @param {string} s2
  * @return {boolean}
  */
-const checkInclusion = (s1, s2) => {};
+const checkInclusion = (s1, s2) => {
+    // Get the length of s1 and s2
+    let s1Len = s1.length;
+    let s2Len = s2.length;
+
+    // Create an array to store the frequency of characters in s1
+    let s1Freq = new Array(26).fill(0);
+
+    // Loop through s1 and increase the frequency of each character in s1Freq
+    for (let i = 0; i < s1Len; i++) {
+        s1Freq[s1.charCodeAt(i) - 97]++;
+    }
+
+    // Create a window of size s1Len in s2 and check if the frequency of characters in the window matches the frequency in s1Freq
+    for (let i = 0; i <= s2Len - s1Len; i++) {
+        let s2Freq = new Array(26).fill(0);
+        for (let j = 0; j < s1Len; j++) {
+            s2Freq[s2.charCodeAt(i + j) - 97]++;
+        }
+        if (matches(s1Freq, s2Freq)) {
+            return true;
+        }
+    }
+    return false;
+};
 
 // Helper function to check if the frequency of characters in two arrays match
 function matches(s1Freq, s2Freq) {
