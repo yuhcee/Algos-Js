58f2eca8bbaf474844f544f7d5e5476e6416167c
yuhcee <egbouchenna001@gmail.com>
2023-02-07 03:10:22 +0100

    :sparkles:  (Vite + React) complete solution

--- InterviewChallenges/React-Practice/sample-exercise/src/App.jsx
+++ InterviewChallenges/React-Practice/sample-exercise/src/App.jsx
@@ -1,18 +1,62 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 
 function App() {
+    const [answer, setAnswer] = useState('');
+    const [error, setError] = useState(null);
+    const [status, setStatus] = useState('typing');
 
-  return (
-    <div className="App">
-      {/* <div className='success'> That's right! 🎉</div> */}
-      {/* <div className='failed'> Not Quite, Try again! 😔</div> */}
+    const handleInputChange = (e) => {
+        setAnswer(e.target.value);
+    };
 
-      <label htmlFor='word'>Enter word: </label>
-      <input id='word'/>
-      <button className='submit-btn'>Submit</button>
-    </div>
-  )
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        setStatus('submitting');
+        try {
+            await submitForm(answer);
+            setStatus('success');
+            setError(null);
+        } catch (err) {
+            setStatus('typing');
+            setError(err);
+        }
+    };
+
+    function submitForm(answer) {
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                let shouldError = answer !== checkPalindrome(answer);
+                if (shouldError) {
+                    reject(new Error('Not Quite, Try again! 😔'));
+                } else {
+                    resolve();
+                }
+            }, 1000);
+        });
+    }
+
+    function checkPalindrome(value) {
+        return value.split('').reverse().join('');
+    }
+
+    return (
+        <div className="App">
+            <h2 className="header">Palindrome Checker</h2>
+
+            {status === 'success' && <p className="success"> That's right! 🎉</p>}
+
+            {error !== null && <p className="failed">{error.message}</p>}
+
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="word">Enter word: </label>
+                <input id="word" onChange={handleInputChange} disabled={status === 'submitting'} />
+                <button className="submit-btn" disabled={answer.length === 0 || status === 'submitting'}>
+                    Submit
+                </button>
+            </form>
+        </div>
+    );
 }
 
-export default App
+export default App;
