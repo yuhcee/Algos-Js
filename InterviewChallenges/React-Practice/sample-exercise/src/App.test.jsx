8d1fd0517482377ecce27f8074fb4ca0af2ac35e
yuhcee <egbouchenna001@gmail.com>
2023-02-07 09:01:15 +0100

    :white_check_mark: (Vite + React) add testcase

--- InterviewChallenges/React-Practice/sample-exercise/src/App.test.jsx
+++ InterviewChallenges/React-Practice/sample-exercise/src/App.test.jsx
@@ -1,12 +1,12 @@
 import { describe, it, expect } from 'vitest';
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render, screen, waitFor, fireEvent } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 
 import App from './App';
-
 describe('App', async () => {
     it('renders App component', () => {
         render(<App />);
-        screen.debug();
+        // screen.debug();
     });
 
     it('Header to be in the Document', () => {
@@ -28,7 +28,38 @@ describe('App', async () => {
 
         // ASSERT
         expect(screen.getByRole('heading')).toHaveTextContent('Palindrome Checker');
+    });
+
+    it('Button is disabled', async () => {
+        render(<App />);
+
+        expect(screen.queryByText(/That's right!/)).toBeNull();
+        fireEvent.click(screen.getByRole('button'));
+
+        await waitFor(() => screen.getByText('Submit'));
         expect(screen.getByRole('button')).toBeDisabled();
+    });
+
+    it('Changes input value', async () => {
+        const user = userEvent.setup();
+        render(<App />);
+        screen.debug();
+        expect(screen.queryByText(/That's right!/)).toBeNull();
+
+        fireEvent.change(screen.getByLabelText(/Enter word/), {
+            target: { value: 'mom' },
+        });
+        await user.type(screen.getByRole('textbox'), 'mom');
+
+        expect(screen.getByRole('button')).toBeTruthy();
+    });
+
+    it('type', async () => {
+        const user = userEvent.setup();
+        render(<App />);
+        await user.type(screen.getByRole('textbox'), 'mom');
+
+        expect(screen.getByRole('textbox')).toHaveValue('mom');
     });
 });
 
