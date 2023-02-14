95abcf4fc0d6d9bd2326bcda40d2f91f696dadaf
yuhcee <egbouchenna001@gmail.com>
2023-02-14 09:44:57 +0100

    :book: (addBinary) add task description

---
+++ Datastructures_&_Algorithms/Strings/addBinary.js
@@ -0,0 +1,10 @@
+/**
+ * **67. Add Binary**
+ *
+ * Given two binary strings a and b, return their sum as a binary string.
+ * 
+ * @param {string} a
+ * @param {string} b
+ * @return {string}
+ */
+const addBinary = (a, b) => {};
