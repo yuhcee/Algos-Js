21b2a6ef257f08045552f69e65f23b6bafdd3002
yuhcee <egbouchenna001@gmail.com>
2023-02-15 07:38:21 +0100

    :book: (addToArrayForm) add task description

---
+++ Datastructures_&_Algorithms/Arrays/addToArrayForm.js
@@ -0,0 +1,16 @@
+/**
+ * **989. Add to Array-Form of Integer**
+ * 
+ * The **array-form** of an integer `num` is an array representing its 
+ * digits in left to right order.
+ * 
+ * For example, for `num = 1321`, the array form is `[1,3,2,1]`.
+ * 
+ * Given `num`, the **array-form** of an integer, and an integer `k`, 
+ * return *the **array-form** of the integer `num + k`*.
+ * 
+ * @param {number[]} num
+ * @param {number} k
+ * @return {number[]}
+ */
+const addToArrayForm = function (num, k) {};
