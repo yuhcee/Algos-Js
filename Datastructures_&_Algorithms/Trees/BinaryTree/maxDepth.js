a64497ea4a8801320d6479bdf8f941c570c21873
yuhcee <egbouchenna001@gmail.com>
2023-02-16 07:40:13 +0100

    :heavy_plus_sign: (maxDepth) add constraints

--- Datastructures_&_Algorithms/Trees/BinaryTree/maxDepth.js
+++ Datastructures_&_Algorithms/Trees/BinaryTree/maxDepth.js
@@ -13,6 +13,11 @@ function TreeNode(val, left, right) {
  * A binary tree's **maximum depth** is the number of nodes along the longest path from the root node down to the
  * farthest leaf node.
  *
+ * **Constraints:**
+ *
+ * - The number of nodes in the tree is in the range `[0, 104]`.
+ * - `-100 <= Node.val <= 100`
+ *
  * @param {TreeNode} root
  * @return {number}
  */
