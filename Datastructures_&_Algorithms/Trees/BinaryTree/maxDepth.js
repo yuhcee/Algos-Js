3ec295cc0205caf9749e165e4f88fecea089be41
yuhcee <egbouchenna001@gmail.com>
2023-02-16 07:37:43 +0100

    :book: (maxDepth) add task description

---
+++ Datastructures_&_Algorithms/Trees/BinaryTree/maxDepth.js
@@ -0,0 +1,21 @@
+// Definition for a binary tree node.
+function TreeNode(val, left, right) {
+    this.val = val === undefined ? 0 : val;
+    this.left = left === undefined ? null : left;
+    this.right = right === undefined ? null : right;
+}
+
+/**
+ * **104. Maximum Depth of Binary Tree**
+ *
+ * Given the root of a binary tree, return *its maximum depth*.
+ *
+ * A binary tree's **maximum depth** is the number of nodes along the longest path from the root node down to the
+ * farthest leaf node.
+ *
+ * @param {TreeNode} root
+ * @return {number}
+ */
+const maxDepth = function (root) {
+    
+};
