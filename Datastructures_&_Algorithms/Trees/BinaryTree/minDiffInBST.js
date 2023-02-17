08fdbcbb9661f0627de183967185b14fa4849a21
yuhcee <egbouchenna001@gmail.com>
2023-02-17 11:10:02 +0100

    :heavy_plus_sign: (minDiffInBST) add constraints

--- Datastructures_&_Algorithms/Trees/BinaryTree/minDiffInBST.js
+++ Datastructures_&_Algorithms/Trees/BinaryTree/minDiffInBST.js
@@ -12,6 +12,11 @@
  * Given the `root` of a Binary Search Tree (BST), return *the minimum difference between the values 
  * of any two different nodes in the tree*.
  * 
+ * **Constraints:**
+ * 
+ * - The number of nodes in the tree is in the range `[2, 100]`.
+ * - `0 <= Node.val <= 105`
+ * 
  * @param {TreeNode} root
  * @return {number}
  */
