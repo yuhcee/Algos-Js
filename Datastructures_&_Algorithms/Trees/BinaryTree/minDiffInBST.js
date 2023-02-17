749b4a31d889627aca050ebdd87a25e8b95ba95d
yuhcee <egbouchenna001@gmail.com>
2023-02-17 10:51:38 +0100

    :book: (minDiffInBST) add task description

---
+++ Datastructures_&_Algorithms/Trees/BinaryTree/minDiffInBST.js
@@ -0,0 +1,18 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * **783. Minimum Distance Between BST Nodes**
+ * 
+ * Given the `root` of a Binary Search Tree (BST), return *the minimum difference between the values 
+ * of any two different nodes in the tree*.
+ * 
+ * @param {TreeNode} root
+ * @return {number}
+ */
+const minDiffInBST = function (root) {};
