dd50ef90f137fa2a96c5b5b52f95bfb949606c0a
yuhcee <egbouchenna001@gmail.com>
2023-02-18 23:40:34 +0100

    :tada: (invertTree) add task solution

--- Datastructures_&_Algorithms/Trees/BinaryTree/invertTree.js
+++ Datastructures_&_Algorithms/Trees/BinaryTree/invertTree.js
@@ -12,13 +12,23 @@
  * Given the root of a binary tree, invert the tree, and return *its root*.
  *
  * **Constraints:**
- * 
+ *
  * - The number of nodes in the tree is in the range `[0, 100]`.
  * - `-100 <= Node.val <= 100`
- * 
+ *
  * @param {TreeNode} root
  * @return {TreeNode}
  */
 const invertTree = function (root) {
+    if (root === null) {
+        return null;
+    }
 
+    const left = invertTree(root.left);
+    const right = invertTree(root.right);
+
+    root.left = right;
+    root.right = left;
+
+    return root;
 };
