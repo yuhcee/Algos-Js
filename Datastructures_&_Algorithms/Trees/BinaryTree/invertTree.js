2264e498bb4a7f1499c180c7a5c9d82f5ceebb5e
yuhcee <egbouchenna001@gmail.com>
2023-02-18 23:33:16 +0100

    :book: (invertTree) add task description

---
+++ Datastructures_&_Algorithms/Trees/BinaryTree/invertTree.js
@@ -0,0 +1,19 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * **226. Invert Binary Tree**
+ *
+ * Given the root of a binary tree, invert the tree, and return *its root*.
+ *
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+const invertTree = function (root) {
+    
+};
