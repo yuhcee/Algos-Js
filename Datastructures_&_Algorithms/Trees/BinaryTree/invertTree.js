26f39b4a5609a8cfd9d4cdf5d846fac36880efc1
yuhcee <egbouchenna001@gmail.com>
2023-02-18 23:43:06 +0100

    :white_check_mark: (invertTree) add testcase 2

--- Datastructures_&_Algorithms/Trees/BinaryTree/invertTree.js
+++ Datastructures_&_Algorithms/Trees/BinaryTree/invertTree.js
@@ -40,3 +40,7 @@ const invertTree = function (root) {
 const root = [4, 2, 7, 1, 3, 6, 9];
 // Output: [4,7,2,9,6,3,1]
 console.log(invertTree(root));
+
+const root1 = [2, 1, 3];
+// Output: [2,3,1]
+console.log(invertTree(root1));
