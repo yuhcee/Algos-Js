bf8ff03c3d135f51a905a54034d0fe1f04b86031
yuhcee <egbouchenna001@gmail.com>
2023-02-18 23:37:21 +0100

    :heavy_plus_sign: (invertTree) add constraints

--- Datastructures_&_Algorithms/Trees/BinaryTree/invertTree.js
+++ Datastructures_&_Algorithms/Trees/BinaryTree/invertTree.js
@@ -11,9 +11,14 @@
  *
  * Given the root of a binary tree, invert the tree, and return *its root*.
  *
+ * **Constraints:**
+ * 
+ * - The number of nodes in the tree is in the range `[0, 100]`.
+ * - `-100 <= Node.val <= 100`
+ * 
  * @param {TreeNode} root
  * @return {TreeNode}
  */
 const invertTree = function (root) {
-    
+
 };
