3911b274ac108a750edf5c57c3bd10e8af06e69c
yuhcee <egbouchenna001@gmail.com>
2023-02-19 07:11:10 +0100

    :heavy_plus_sign: (zigzagLevelOrder) add constraints

--- Datastructures_&_Algorithms/Trees/BinaryTree/zigzagLevelOrder.js
+++ Datastructures_&_Algorithms/Trees/BinaryTree/zigzagLevelOrder.js
@@ -12,7 +12,15 @@
  * Given the `root` of a binary tree, return *the zigzag level order traversal of its nodes' values*.
  * (i.e., from left to right, then right to left for the next level and alternate between).
  *
+ * **Constraints:**
+ *
+ * - The number of nodes in the tree is in the range `[0, 2000]`.
+ * - `-100 <= Node.val <= 100`
+ *
  * @param {TreeNode} root
  * @return {number[][]}
  */
-const zigzagLevelOrder = function (root) {};
+const zigzagLevelOrder = function (root) {
+    // Initialize a result array to store the level order traversal
+    const result = [];
+};
