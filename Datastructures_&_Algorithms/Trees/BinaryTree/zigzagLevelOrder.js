c3ed4776dfc9a88a6e95bfa3416f14a13812ab9a
yuhcee <egbouchenna001@gmail.com>
2023-02-19 07:17:24 +0100

    :white_check_mark: (zigzagLevelOrder) add testcase 1

--- Datastructures_&_Algorithms/Trees/BinaryTree/zigzagLevelOrder.js
+++ Datastructures_&_Algorithms/Trees/BinaryTree/zigzagLevelOrder.js
@@ -77,3 +77,7 @@ const zigzagLevelOrder = function (root) {
     // Return the result array
     return result;
 };
+
+const root = [3, 9, 20, null, null, 15, 7];
+// Output: [[3],[20,9],[15,7]]
+console.log(zigzagLevelOrder(root));
