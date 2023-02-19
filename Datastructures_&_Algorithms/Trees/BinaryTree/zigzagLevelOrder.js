30f4e62cededd8019d84dea579f2c3628a68223b
yuhcee <egbouchenna001@gmail.com>
2023-02-19 07:09:14 +0100

    :book: (zigzagLevelOrder) add task description

---
+++ Datastructures_&_Algorithms/Trees/BinaryTree/zigzagLevelOrder.js
@@ -0,0 +1,18 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * **103. Binary Tree Zigzag Level Order Traversal**
+ *
+ * Given the `root` of a binary tree, return *the zigzag level order traversal of its nodes' values*.
+ * (i.e., from left to right, then right to left for the next level and alternate between).
+ *
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+const zigzagLevelOrder = function (root) {};
