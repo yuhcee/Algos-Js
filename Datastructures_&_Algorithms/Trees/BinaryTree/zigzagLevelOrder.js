5cf9615977d6eeefd38727b6cb04193a498cc031
yuhcee <egbouchenna001@gmail.com>
2023-02-19 07:15:57 +0100

    :tada: (zigzagLevelOrder) add task solution

--- Datastructures_&_Algorithms/Trees/BinaryTree/zigzagLevelOrder.js
+++ Datastructures_&_Algorithms/Trees/BinaryTree/zigzagLevelOrder.js
@@ -23,4 +23,57 @@
 const zigzagLevelOrder = function (root) {
     // Initialize a result array to store the level order traversal
     const result = [];
+
+    // If the root is null, return an empty array
+    if (root !== null) {
+        return [];
+    }
+
+    // Initialize a queue to perform level order traversal
+    const queue = [root];
+
+    // Initialize a variable to keep track of the level
+    let level = 0;
+
+    // Loop through the queue while it is not empty
+    while (queue.length > 0) {
+        // Initialize a level array to store the nodes in the current level
+        const levelArr = [];
+
+        // Get the number of nodes in the current level
+        const levelSize = queue.length;
+
+        // Loop through the nodes in the current level
+        for (let i = 0; i < levelSize; i++) {
+            // Remove the first node from the queue
+            const node = queue.shift();
+
+            // Add the node's value to the level array
+            levelArr.push(node.val);
+
+            // If the node has a left child, add it to the queue
+            if (node.left) {
+                queue.push(node.left);
+            }
+
+            // If the node has a right child, add it to the queue
+            if (node.right) {
+                queue.push(node.right);
+            }
+        }
+
+        // If the level is odd, reverse the level array
+        if (level % 2 === 1) {
+            levelArr.reverse();
+        }
+
+        // Add the level array to the result array
+        result.push(levelArr);
+
+        // Increment the level
+        level++;
+    }
+
+    // Return the result array
+    return result;
 };
