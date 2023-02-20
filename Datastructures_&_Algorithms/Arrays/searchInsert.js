cf69aa0f12f654373b6a140b1fad73dbd311b2e0
yuhcee <egbouchenna001@gmail.com>
2023-02-20 06:47:50 +0100

    :book: (searchInsert) add task description

---
+++ Datastructures_&_Algorithms/Arrays/searchInsert.js
@@ -0,0 +1,13 @@
+/**
+ * **35. Search Insert Position**
+ *
+ * Given a sorted array of distinct integers and a target value, return the index if the target is
+ * found. If not, return the index where it would be if it were inserted in order.
+ *
+ * You must write an algorithm with `O(log n)` runtime complexity.
+ *
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+const searchInsert = function (nums, target) {};
