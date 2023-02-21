c438459b2a28d0ce3a18d08357042df1e672570f
yuhcee <egbouchenna001@gmail.com>
2023-02-21 08:22:52 +0100

    :book: (singleNonDuplicate) add task description

---
+++ Datastructures_&_Algorithms/Arrays/singleNonDuplicate.js
@@ -0,0 +1,14 @@
+/**
+ * **540. Single Element in a Sorted Array**
+ *
+ * You are given a sorted array consisting of only integers where every element appears exactly
+ * twice, except for one element which appears exactly once.
+ *
+ * Return *the single element that appears only once.*
+ *
+ * Your solution must run in `O(log n)` time and `O(1)` space.
+ *
+ * @param {number[]} nums
+ * @return {number}
+ */
+const singleNonDuplicate = function (nums) {};
