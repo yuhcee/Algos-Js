27600d55a7adde9a3089cddf5952264e89f9f19b
yuhcee <egbouchenna001@gmail.com>
2023-02-23 23:52:15 +0100

    :tada: (findMaximizedCapital) add task solution

--- Datastructures_&_Algorithms/Greedy Algorithms/findMaximizedCapital.js
+++ Datastructures_&_Algorithms/Greedy Algorithms/findMaximizedCapital.js
@@ -1,6 +1,6 @@
 /**
  * **502. IPO**
- * 
+ *
  * Suppose LeetCode will start its **IPO** soon. In order to sell a good price of its shares to
  * Venture Capital, LeetCode would like to work on some projects to increase its capital before the
  * **IPO**. Since it has limited resources, it can only finish at most `k` distinct projects before
@@ -30,4 +30,29 @@
  * @returns {number} - The maximum amount of capital that can be raised.
  *
  */
-const findMaximizedCapital = function (k, initialCapital, profits, capital) {};
+const findMaximizedCapital = function (k, initialCapital, profits, capital) {
+    // Create a queue to store the capital required for each project in ascending order
+    const capitalAscQueue = new MinPriorityQueue();
+    // Create a queue to store the profits to be earned from each project in descending order
+    const profitsDescQueue = new MaxPriorityQueue();
+    // Iterate through each project and enqueue the capital required and profit to be earned in the capitalAscQueue
+    for (let i = 0; i < capital.length; i++) {
+        capitalAscQueue.enqueue([capital[i], profits[i]], capital[i]);
+    }
+    // Perform at most k projects
+    for (let i = 0; i < k; i++) {
+        // Dequeue all projects with capital required less than or equal to the available capital
+        while (!capitalAscQueue.isEmpty() && capitalAscQueue.front().element[0] <= initialCapital) {
+            const project = capitalAscQueue.dequeue().element;
+            profitsDescQueue.enqueue(project, project[1]);
+        }
+        // If there are no projects available to invest in, return the current capital
+        if (profitsDescQueue.isEmpty()) {
+            return initialCapital;
+        }
+        // Invest in the project with the highest profit and add the earned profit to the current capital
+        initialCapital += profitsDescQueue.dequeue().element[1];
+    }
+    // Return the final amount of capital
+    return initialCapital;
+};
