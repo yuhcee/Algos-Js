541c6f72a299308f61b9ecc271ed4789f9444d1a
yuhcee <egbouchenna001@gmail.com>
2023-02-23 23:46:58 +0100

    :book: (findMaximizedCapital) add task description

---
+++ Datastructures_&_Algorithms/Greedy Algorithms/findMaximizedCapital.js
@@ -0,0 +1,33 @@
+/**
+ * **502. IPO**
+ * 
+ * Suppose LeetCode will start its **IPO** soon. In order to sell a good price of its shares to
+ * Venture Capital, LeetCode would like to work on some projects to increase its capital before the
+ * **IPO**. Since it has limited resources, it can only finish at most `k` distinct projects before
+ * the **IPO**. Help LeetCode design the best way to maximize its total capital after finishing at
+ * most `k` distinct projects.
+ *
+ * You are given `n` projects where the `ith` project has a pure profit `profits[i]` and a minimum
+ * capital of `capital[i]` is needed to start it.
+ *
+ * Initially, you have `w` capital. When you finish a project, you will obtain its pure profit and
+ * the profit will be added to your total capital.
+ *
+ * Pick a list of at most `k` distinct projects from given projects to maximize your final capital,
+ * and return *the final maximized capital*.
+ *
+ * The answer is guaranteed to fit in a 32-bit signed integer.
+ *
+ * This function finds the maximum amount of capital that can be raised by performing at most k
+ * projects.
+ *
+ * The function takes in the following parameters:
+ *
+ * @param {number} k - The maximum number of projects that can be performed.
+ * @param {number} initialCapital - The initial capital available to invest.
+ * @param {number[]} profits - An array of profits to be earned from each project.
+ * @param {number[]} capital - An array of capital required for each project.
+ * @returns {number} - The maximum amount of capital that can be raised.
+ *
+ */
+const findMaximizedCapital = function (k, initialCapital, profits, capital) {};
