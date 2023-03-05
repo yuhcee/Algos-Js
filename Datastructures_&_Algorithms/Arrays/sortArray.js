240f7cdb117aa8fb0fac6f535849a6f02e8d8bcd
yuhcee <egbouchenna001@gmail.com>
2023-03-05 09:46:33 +0100

    :heavy_plus_sign: (sortArray) add constraints

--- Datastructures_&_Algorithms/Arrays/sortArray.js
+++ Datastructures_&_Algorithms/Arrays/sortArray.js
@@ -6,9 +6,12 @@
  * You must solve the problem **without using any built-in** functions in `O(nlog(n))` time
  * complexity and with the smallest space complexity possible.
  *
+ * **Constraints:**
+ *
+ * - `1 <= nums.length <= 5 * 104`
+ * - `-5 * 104 <= nums[i] <= 5 * 104`
+ *
  * @param {number[]} nums
  * @return {number[]}
  */
-const sortArray = function (nums) {
-    
-};
+const sortArray = function (nums) {};
