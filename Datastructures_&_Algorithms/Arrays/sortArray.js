63ea079a38c140fdf41285a8f67101a305cdae99
yuhcee <egbouchenna001@gmail.com>
2023-03-05 09:48:18 +0100

    :white_check_mark: (sortArray) add testcase 1

--- Datastructures_&_Algorithms/Arrays/sortArray.js
+++ Datastructures_&_Algorithms/Arrays/sortArray.js
@@ -43,3 +43,8 @@ const sortArray = function (nums) {
 
     return nums;
 };
+
+const nums = [5, 2, 3, 1];
+// Output: [1,2,3,5]
+/* Explanation: After sorting the array, the positions of some numbers are not changed (for example, 2 and 3), while the positions of other numbers are changed (for example, 1 and 5). */
+console.log(sortArray(nums));
