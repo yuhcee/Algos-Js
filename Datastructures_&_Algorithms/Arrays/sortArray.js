5148a4c0d054325621541c44bc60f24e9d5d2e26
yuhcee <egbouchenna001@gmail.com>
2023-03-05 09:45:34 +0100

    :books: (sortArray) add task description

---
+++ Datastructures_&_Algorithms/Arrays/sortArray.js
@@ -0,0 +1,14 @@
+/**
+ * **912. Sort an Array**
+ *
+ * Given an array of integers `nums`, sort the array in ascending order and return it.
+ *
+ * You must solve the problem **without using any built-in** functions in `O(nlog(n))` time
+ * complexity and with the smallest space complexity possible.
+ *
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+const sortArray = function (nums) {
+    
+};
