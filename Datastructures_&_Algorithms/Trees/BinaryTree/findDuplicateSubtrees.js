7ef67aa9af317f5580d474ea064e3ab339a5f21a
yuhcee <egbouchenna001@gmail.com>
2023-03-05 10:00:16 +0100

    :books: (findDuplicateSubtrees) add task description

---
+++ Datastructures_&_Algorithms/Trees/BinaryTree/findDuplicateSubtrees.js
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * **652. Find Duplicate Subtrees**
+ *
+ * Given the `root` of a binary tree, return all **duplicate subtrees**.
+ *
+ * For each kind of duplicate subtrees, you only need to return the root node of any **one**
+ * of them.
+ *
+ * Two trees are **duplicate** if they have the **same structure** with the **same node
+ * values**.
+ *
+ * @param {TreeNode} root
+ * @return {TreeNode[]}
+ */
+const findDuplicateSubtrees = function (root) {
+    
+};
