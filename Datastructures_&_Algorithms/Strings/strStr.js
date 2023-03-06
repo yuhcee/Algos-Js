7caf1ae9538b327f20d162e2e4ffa54424a35c06
yuhcee <egbouchenna001@gmail.com>
2023-03-06 09:07:58 +0100

    :books: (strStr) add task description

---
+++ Datastructures_&_Algorithms/Strings/strStr.js
@@ -0,0 +1,13 @@
+/**
+ * **28. Find the Index of the First Occurrence in a String**
+ *
+ * Given two strings `needle` and `haystack`, return the index of the first occurrence of 
+ * `needle` in `haystack`, or `-1` if `needle` is not part of `haystack`.
+ *
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+const strStr = function (haystack, needle) {
+
+};
