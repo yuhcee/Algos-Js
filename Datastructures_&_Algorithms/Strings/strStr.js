75fc6bd82a5821180a1d45fce5348c4c58fd614e
yuhcee <egbouchenna001@gmail.com>
2023-03-06 09:11:50 +0100

    :heavy_plus_sign: (strStr) add constraints

--- Datastructures_&_Algorithms/Strings/strStr.js
+++ Datastructures_&_Algorithms/Strings/strStr.js
@@ -1,13 +1,16 @@
 /**
  * **28. Find the Index of the First Occurrence in a String**
  *
- * Given two strings `needle` and `haystack`, return the index of the first occurrence of 
+ * Given two strings `needle` and `haystack`, return the index of the first occurrence of
  * `needle` in `haystack`, or `-1` if `needle` is not part of `haystack`.
+ *
+ * **Constraints:**
+ *
+ * - `1 <= haystack.length, needle.length <= 104`
+ * - `haystack` and `needle` consist of only lowercase English characters.
  *
  * @param {string} haystack
  * @param {string} needle
  * @return {number}
  */
-const strStr = function (haystack, needle) {
-
-};
+const strStr = function (haystack, needle) {};
