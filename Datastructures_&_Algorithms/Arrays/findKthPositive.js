b87c78139b70ef58f5bbf65759924685942846fe
yuhcee <egbouchenna001@gmail.com>
2023-03-06 12:15:19 +0100

    :books: (findKthPositive) add task description

---
+++ Datastructures_&_Algorithms/Arrays/findKthPositive.js
@@ -0,0 +1,13 @@
+/**
+ * **1539. Kth Missing Positive Number**
+ * 
+ * Given an array `arr` of positive integers sorted in a **strictly increasing order**, and an 
+ * integer `k`.
+ * 
+ * Return *the `kth` **positive** integer that is **missing** from this array*.
+ * 
+ * @param {number[]} arr
+ * @param {number} k
+ * @return {number}
+ */
+const findKthPositive = function (arr, k) {};
