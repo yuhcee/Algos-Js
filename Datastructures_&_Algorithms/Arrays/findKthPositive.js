a4df4f08039a7173a4d48d9dcbc6ef0ed57b56b1
yuhcee <egbouchenna001@gmail.com>
2023-03-06 12:16:37 +0100

    :heavy_plus_sign: (findKthPositive) add constraints

--- Datastructures_&_Algorithms/Arrays/findKthPositive.js
+++ Datastructures_&_Algorithms/Arrays/findKthPositive.js
@@ -1,11 +1,18 @@
 /**
  * **1539. Kth Missing Positive Number**
- * 
- * Given an array `arr` of positive integers sorted in a **strictly increasing order**, and an 
+ *
+ * Given an array `arr` of positive integers sorted in a **strictly increasing order**, and an
  * integer `k`.
- * 
+ *
  * Return *the `kth` **positive** integer that is **missing** from this array*.
- * 
+ *
+ * **Constraints:**
+ *
+ * - `1 <= arr.length <= 1000`
+ * - `1 <= arr[i] <= 1000`
+ * - `1 <= k <= 1000`
+ * - `arr[i] < arr[j] for 1 <= i < j <= arr.length`
+ *
  * @param {number[]} arr
  * @param {number} k
  * @return {number}
