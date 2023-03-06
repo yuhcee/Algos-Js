72545bdde9dfba9017725e378905563d15870b1a
yuhcee <egbouchenna001@gmail.com>
2023-03-06 09:30:55 +0100

    :tada: (compress) add task solution

--- Datastructures_&_Algorithms/Strings/compress.js
+++ Datastructures_&_Algorithms/Strings/compress.js
@@ -25,4 +25,29 @@
  * @param {character[]} chars
  * @return {number}
  */
-const compress = function (chars) {};
+const compress = function (chars) {
+    if (chars.length > 1) {
+        let s = '';
+        let groupSize = 1;
+
+        for (let i = 0; i < chars.length; i++) {
+            if (chars[i] === chars[i + 1]) {
+                groupSize++;
+            } else {
+                s += chars[i];
+                if (groupSize > 1) {
+                    s += groupSize;
+                    groupSize = 1;
+                }
+            }
+        }
+
+        chars.splice(0, chars.length);
+
+        for (let j = 0; j < s.length; j++) {
+            chars[j] = s[j];
+        }
+    }
+
+    return chars.length;
+};
