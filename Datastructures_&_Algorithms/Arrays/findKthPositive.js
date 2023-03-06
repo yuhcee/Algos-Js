fb5c43278909d998da938554c2d8041250e5b007
yuhcee <egbouchenna001@gmail.com>
2023-03-06 12:35:48 +0100

    :white_check_mark: (findKthPositive) add testcase 1

--- Datastructures_&_Algorithms/Arrays/findKthPositive.js
+++ Datastructures_&_Algorithms/Arrays/findKthPositive.js
@@ -40,3 +40,9 @@ const findKthPositive = function (arr, k) {
     // if we get here, it means the kth missing integer is greater than arr[n-1]
     return arr[n - 1] + k - missingCount;
 };
+
+const arr = [2, 3, 4, 7, 11],
+    k = 5;
+// Output: 9
+/* Explanation: The missing positive integers are [1,5,6,8,9,10,12,13,...]. The 5th missing positive integer is 9. */
+console.log(findKthPositive(arr, k));
