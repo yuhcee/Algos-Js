2e27079f6cb1f914792f43b512022b388cdf89f9
yuhcee <egbouchenna001@gmail.com>
2023-03-06 09:37:49 +0100

    :white_check_mark: (compress) add testcase 3

--- Datastructures_&_Algorithms/Strings/compress.js
+++ Datastructures_&_Algorithms/Strings/compress.js
@@ -61,3 +61,8 @@ const chars1 = ['a'];
 // Output: Return 1, and the first character of the input array should be: ["a"]
 // Explanation: The only group is "a", which remains uncompressed since it's a single character.
 console.log(compress(chars1));
+
+const chars2 = ['a', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b'];
+// Output: Return 4, and the first 4 characters of the input array should be: ["a","b","1","2"].
+// Explanation: The groups are "a" and "bbbbbbbbbbbb". This compresses to "ab12".
+console.log(compress(chars2));
