bdaf582d9515d377d8eebd3021b522f2d97014fe
yuhcee <egbouchenna001@gmail.com>
2023-03-06 09:13:50 +0100

    :white_check_mark: (strStr) add testcase 1

--- Datastructures_&_Algorithms/Strings/strStr.js
+++ Datastructures_&_Algorithms/Strings/strStr.js
@@ -39,3 +39,10 @@ const strStr = function (haystack, needle) {
 
     return -1;
 };
+
+const haystack = 'sadbutsad',
+    needle = 'sad';
+// Output: 0
+/* Explanation: "sad" occurs at index 0 and 6.
+The first occurrence is at index 0, so we return 0. */
+console.log(strStr(haystack, needle));
