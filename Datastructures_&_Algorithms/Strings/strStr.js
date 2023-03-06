13e8581ad154034d1a735849e7bf12ba5669437c
yuhcee <egbouchenna001@gmail.com>
2023-03-06 09:12:37 +0100

    :tada: (strStr) add task solution

--- Datastructures_&_Algorithms/Strings/strStr.js
+++ Datastructures_&_Algorithms/Strings/strStr.js
@@ -13,4 +13,29 @@
  * @param {string} needle
  * @return {number}
  */
-const strStr = function (haystack, needle) {};
+const strStr = function (haystack, needle) {
+    if (!needle) {
+        return 0;
+    }
+
+    let haystackArr = haystack.split('');
+    const first = needle[0];
+    let buffer = 0;
+
+    while (haystackArr.indexOf(first) !== -1) {
+        if (haystackArr.length < needle.length) {
+            break;
+        }
+        const index = haystackArr.indexOf(first);
+        const str = haystack.substring(index, index + needle.length);
+        if (str === needle) {
+            return index + buffer;
+        } else {
+            haystackArr = haystackArr.splice(index + 1);
+            haystack = haystack.substring(index + 1);
+            buffer += index + 1;
+        }
+    }
+
+    return -1;
+};
