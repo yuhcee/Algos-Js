7c488b9e62600136c838a61438670a1550296495
yuhcee <egbouchenna001@gmail.com>
2023-03-07 08:48:32 +0100

    :books: (countSubarrays) add task description

---
+++ Datastructures_&_Algorithms/Arrays/countSubarrays.js
@@ -0,0 +1,20 @@
+/**
+ * **2444. Count Subarrays With Fixed Bounds**
+ *
+ * You are given an integer array nums and two integers minK and maxK.
+ *
+ * A **fixed-bound** subarray of `nums` is a subarray that satisfies the following conditions:
+ *
+ * - The **minimum** value in the subarray is equal to `minK`.
+ * - The **maximum** value in the subarray is equal to `maxK`.
+ *
+ * Return *the number of fixed-bound subarrays*.
+ *
+ * A **subarray** is a **contiguous** part of an array.
+ *
+ * @param {number[]} nums
+ * @param {number} minK
+ * @param {number} maxK
+ * @return {number}
+ */
+const countSubarrays = function (nums, minK, maxK) {};
