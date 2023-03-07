5ed11ebab1e3b47ac65792759bad7a4c4a6249b9
yuhcee <egbouchenna001@gmail.com>
2023-03-07 08:49:53 +0100

    :tada: (countSubarrays) add task solution

--- Datastructures_&_Algorithms/Arrays/countSubarrays.js
+++ Datastructures_&_Algorithms/Arrays/countSubarrays.js
@@ -22,4 +22,29 @@
  * @param {number} maxK
  * @return {number}
  */
-const countSubarrays = function (nums, minK, maxK) {};
+const countSubarrays = function (nums, minK, maxK) {
+    // left : Index of value out of range
+    // min : Index of minimum value in range
+    // max : Index of maximum value in range
+    // count : Number of valid subarrays
+    let left = -1,
+        min = -1,
+        max = -1,
+        count = 0;
+
+    for (let i = 0; i < nums.length; i++) {
+        // If value is out of given range
+        if (nums[i] < minK || nums[i] > maxK) left = i;
+
+        // Store index of minK
+        if (nums[i] === minK) min = i;
+
+        // Store index of maxK
+        if (nums[i] === maxK) max = i;
+
+        // Math.min(min, max) gives minimum index that should be considered
+        let c = Math.min(min, max) - left;
+        if (c > 0) count += c;
+    }
+    return count;
+};
