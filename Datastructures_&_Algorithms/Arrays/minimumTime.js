90231f95b12faaa909e6c860b8133a98da973442
yuhcee <egbouchenna001@gmail.com>
2023-03-08 07:54:34 +0100

    :tada: (minimumTime) add task solution

--- Datastructures_&_Algorithms/Arrays/minimumTime.js
+++ Datastructures_&_Algorithms/Arrays/minimumTime.js
@@ -21,4 +21,22 @@
  * @param {number} totalTrips
  * @return {number}
  */
-const minimumTime = function (time, totalTrips) {};
+const minimumTime = function (time, totalTrips) {
+    function getOutcome(t) {
+        return time.reduce((acc, cur) => acc + Math.floor(t / cur), 0);
+    }
+
+    const minTime = Math.min(...time);
+    let low = 1,
+        high = totalTrips * minTime;
+    // find earliest high such that
+    // getOutcome(high) >= totalTrips
+    while (low < high) {
+        const mid = Math.floor((low + high) / 2);
+        const outcome = getOutcome(mid);
+        if (outcome >= totalTrips) high = mid;
+        else low = mid + 1;
+    }
+
+    return high;
+};
