0df6b3512759428db9e74e18538bc23989d21fd4
yuhcee <egbouchenna001@gmail.com>
2023-03-08 08:22:05 +0100

    :white_check_mark: (minJumps) add testcase 1

--- Datastructures_&_Algorithms/Arrays/minJumps.js
+++ Datastructures_&_Algorithms/Arrays/minJumps.js
@@ -73,3 +73,8 @@ const minJumps = function (arr) {
         res++;
     }
 };
+
+const arr = [100, -23, -23, 404, 100, 23, 23, 23, 3, 404];
+// Output: 3
+/* Explanation: You need three jumps from index 0 --> 4 --> 3 --> 9. Note that index 9 is the last index of the array. */
+console.log(minJumps(arr));
