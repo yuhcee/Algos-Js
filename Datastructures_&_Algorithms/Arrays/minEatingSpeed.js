e85c4cd21dfdb3d881c84bd565779f6e150b5bbf
yuhcee <egbouchenna001@gmail.com>
2023-03-09 07:26:33 +0100

    :tada: (minEatingSpeed) add task solution

--- Datastructures_&_Algorithms/Arrays/minEatingSpeed.js
+++ Datastructures_&_Algorithms/Arrays/minEatingSpeed.js
@@ -22,4 +22,27 @@
  * @param {number} h
  * @return {number}
  */
-const minEatingSpeed = function (piles, h) {};
+const minEatingSpeed = function (piles, h) {
+    let left = 1;
+    let right = 1e9;
+
+    while (left < right) {
+        const mid = left + Math.floor((right - left) / 2);
+        const hoursNeeded = calcHours(mid);
+
+        if (h < hoursNeeded) left = mid + 1;
+        else right = mid;
+    }
+
+    return left;
+
+    function calcHours(k) {
+        let count = 0;
+
+        for (const pile of piles) {
+            count += Math.ceil(pile / k);
+        }
+
+        return count;
+    }
+};
