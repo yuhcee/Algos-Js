fae5e3fea3cf18c9a8e1e62945fe9ad77c0be8aa
yuhcee <egbouchenna001@gmail.com>
2023-03-11 14:07:33 +0100

    :books: (sortedListToBST) add task description

---
+++ Datastructures_&_Algorithms/Lists/sortedListToBST.js
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * **109. Convert Sorted List to Binary Search Tree**
+ *
+ * Given the `head` of a singly linked list where elements are sorted in **ascending order**,
+ * *convert it to a height-balanced binary search tree*.
+ *
+ * @param {ListNode} head
+ * @return {TreeNode}
+ */
+const sortedListToBST = function (head) {};
