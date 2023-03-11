b2a4af3861bed23b4ba54da7e17eb61eed019492
yuhcee <egbouchenna001@gmail.com>
2023-03-11 14:10:14 +0100

    :white_check_mark: (sortedListToBST) add testcase 2

--- Datastructures_&_Algorithms/Lists/sortedListToBST.js
+++ Datastructures_&_Algorithms/Lists/sortedListToBST.js
@@ -48,3 +48,6 @@ const sortedListToBST = function (head) {
 const head = [-10, -3, 0, 5, 9];
 // Output: [0,-3,9,-10,null,5]
 /* Explanation: One possible answer is [0,-3,9,-10,null,5], which represents the shown height balanced BST. */
+
+const head1 = [];
+// Output: [];
