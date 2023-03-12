edddfecad2fe11aa8e58bdbae2f3b1ecc4f524dc
yuhcee <egbouchenna001@gmail.com>
2023-03-12 21:59:45 +0100

    :books: (mergeKLists) add task description

---
+++ Datastructures_&_Algorithms/Lists/mergeKLists.js
@@ -0,0 +1,18 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * **23. Merge k Sorted Lists**
+ *
+ * You are given an array of `k` linked-lists `lists`, each linked-list is sorted in ascending order
+ *
+ * *Merge all the linked-lists into one sorted linked-list and return it*.
+ *
+ * @param {ListNode[]} lists
+ * @return {ListNode}
+ */
+var mergeKLists = function (lists) {};
