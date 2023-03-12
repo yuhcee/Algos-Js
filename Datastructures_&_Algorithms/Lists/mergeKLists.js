12129123bc36e73e36f5d745b8019bd67f960628
yuhcee <egbouchenna001@gmail.com>
2023-03-12 22:01:59 +0100

    :heavy_plus_sign: (mergeKLists) add constraints

--- Datastructures_&_Algorithms/Lists/mergeKLists.js
+++ Datastructures_&_Algorithms/Lists/mergeKLists.js
@@ -12,6 +12,15 @@
  *
  * *Merge all the linked-lists into one sorted linked-list and return it*.
  *
+ * **Constraints:**
+ *
+ * - `k == lists.length`
+ * - `0 <= k <= 104`
+ * - `0 <= lists[i].length <= 500`
+ * - `-104 <= lists[i][j] <= 104`
+ * - `lists[i]` is sorted in **ascending order**.
+ * - The sum of `lists[i].length` will not exceed `104`.
+ *
  * @param {ListNode[]} lists
  * @return {ListNode}
  */
