8e471f41d228eff196e67c232b5006e6231721bd
yuhcee <egbouchenna001@gmail.com>
2023-03-12 22:03:16 +0100

    :white_check_mark: (mergeKLists) add testcase 3

--- Datastructures_&_Algorithms/Lists/mergeKLists.js
+++ Datastructures_&_Algorithms/Lists/mergeKLists.js
@@ -41,3 +41,18 @@ var mergeKLists = function (lists) {
     });
     return resultNode;
 };
+
+const lists = [
+    [1, 4, 5],
+    [1, 3, 4],
+    [2, 6],
+];
+// Output: [1,1,2,3,4,4,5,6]
+/* Explanation: The linked-lists are:
+[
+  1->4->5,
+  1->3->4,
+  2->6
+]
+merging them into one sorted list:
+1->1->2->3->4->4->5->6 */
