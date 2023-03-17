7056c995f5b0c0499a13261551e8afbc6dd52342
yuhcee <egbouchenna001@gmail.com>
2023-03-17 07:26:39 +0100

    :tada: (isSymmetric) add task solution

--- Datastructures_&_Algorithms/Trees/BinaryTree/isSymmetric.js
+++ Datastructures_&_Algorithms/Trees/BinaryTree/isSymmetric.js
@@ -15,4 +15,17 @@
  * @param {TreeNode} root
  * @return {boolean}
  */
-const isSymmetric = function (root) {};
+const isSymmetric = function (root) {
+    if (!root) return true;
+    return dfs(root.left, root.right);
+
+    function dfs(leftNode, rightNode) {
+        if (!leftNode && !rightNode) {
+            return true;
+        }
+        if ((leftNode && !rightNode) || (!leftNode && rightNode) || leftNode.val !== rightNode.val) {
+            return false;
+        }
+        return dfs(leftNode.right, rightNode.left) && dfs(leftNode.left, rightNode.right);
+    }
+};
