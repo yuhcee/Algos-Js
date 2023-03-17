80b08983b317a07b5071c08273377e055bc05c99
yuhcee <egbouchenna001@gmail.com>
2023-03-17 07:23:19 +0100

    :books: (isSymmetric) add task description

---
+++ Datastructures_&_Algorithms/Trees/BinaryTree/isSymmetric.js
@@ -0,0 +1,18 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * **101. Symmetric Tree**
+ *
+ * Given the `root` of a binary tree, *check whether it is a mirror of itself* (i.e., symmetric
+ * around its center).
+ *
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+const isSymmetric = function (root) {};
