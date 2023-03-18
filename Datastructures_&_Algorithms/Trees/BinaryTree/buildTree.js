3cba38bdc59913296be0963b932a01c900086183
yuhcee <egbouchenna001@gmail.com>
2023-03-18 07:18:30 +0100

    :tada: (buildTree) add task solution

--- Datastructures_&_Algorithms/Trees/BinaryTree/buildTree.js
+++ Datastructures_&_Algorithms/Trees/BinaryTree/buildTree.js
@@ -27,4 +27,26 @@
  * @param {number[]} postorder
  * @return {TreeNode}
  */
-const buildTree = function (inorder, postorder) {};
+const buildTree = function (inorder, postorder) {
+    const n = inorder.length;
+    const map = new Map();
+    inorder.forEach((num, index) => map.set(num, index));
+
+    let index_post = n - 1;
+
+    return treeBuilder(0, n - 1);
+
+    function treeBuilder(left, right) {
+        if (index_post < 0 || left > right) return null;
+
+        const curr_val = postorder[index_post--];
+        const curr_node = new TreeNode(curr_val);
+
+        const curr_index = map.get(curr_val);
+
+        curr_node.right = treeBuilder(curr_index + 1, right);
+        curr_node.left = treeBuilder(left, curr_index - 1);
+
+        return curr_node;
+    }
+};
