eb9d4c7a1fea3aaae243bfcb9ecf0f5899f10994
yuhcee <egbouchenna001@gmail.com>
2023-03-18 07:05:47 +0100

    :books: (buildTree) add task description

---
+++ Datastructures_&_Algorithms/Trees/BinaryTree/buildTree.js
@@ -0,0 +1,20 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * **106. Construct Binary Tree from Inorder and Postorder Traversal**
+ *
+ * Given two integer arrays `inorder` and `postorder` where `inorder` is the inorder traversal
+ * of a binary tree and `postorder` is the postorder traversal of the same tree, construct and
+ * return *the binary tree*.
+ *
+ * @param {number[]} inorder
+ * @param {number[]} postorder
+ * @return {TreeNode}
+ */
+const buildTree = function (inorder, postorder) {};
