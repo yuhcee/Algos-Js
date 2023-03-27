b581ca954f58dbd412645a13e5f098e399a200e9
yuhcee <egbouchenna001@gmail.com>
2023-03-27 08:24:33 +0100

    :white_check_mark: (bulbSwitch) add testcase 1

--- Datastructures_&_Algorithms/Math/bulbSwitch.js
+++ Datastructures_&_Algorithms/Math/bulbSwitch.js
@@ -20,3 +20,12 @@
 const bulbSwitch = function (n) {
     return Math.floor(Math.sqrt(n));
 };
+
+const n = 3;
+// Output: 1
+/* Explanation: At first, the three bulbs are [off, off, off].
+After the first round, the three bulbs are [on, on, on].
+After the second round, the three bulbs are [on, off, on].
+After the third round, the three bulbs are [on, off, off]. 
+So you should return 1 because there is only one bulb is on. */
+console.log(bulbSwitch(n));
