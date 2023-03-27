db5a7038dbad69c6746b96fff9628e841a796a8b
yuhcee <egbouchenna001@gmail.com>
2023-03-27 08:21:38 +0100

    :books: (bulbSwitch) add task description

---
+++ Datastructures_&_Algorithms/Math/bulbSwitch.js
@@ -0,0 +1,18 @@
+/**
+ * **319. Bulb Switcher**
+ * 
+ * There are `n` bulbs that are initially off. You first turn on all the bulbs, then 
+ * you turn off every second bulb.
+ * 
+ * On the third round, you toggle every third bulb (turning on if it's off or turning
+ * off if it's on). For the `i`th round, you toggle every `i` bulb. For the `n`th
+ * round, you only toggle the last bulb.
+ * 
+ * Return *the number of bulbs that are on after `n` rounds*.
+ * 
+ * 
+ * 
+ * @param {number} n
+ * @return {number}
+ */
+const bulbSwitch = function(n) {}
