70b4721353107b25c27205a30a87e84e605c8fb0
yuhcee <egbouchenna001@gmail.com>
2023-04-01 00:42:02 +0100

    :heavy_plus_sign: (minPathSum) add constraints

--- Datastructures_&_Algorithms/Dynamic Programming/minPathSum.js
+++ Datastructures_&_Algorithms/Dynamic Programming/minPathSum.js
@@ -6,6 +6,14 @@
  *
  * **Note:** You can only move either down or right at any point in time.
  *
+ * **Constraints:**
+ * 
+ * - `m == grid.length`
+ * - `n == grid[i].length`
+ * - `1 <= m, n <= 200`
+ * - `0 <= grid[i][j] <= 100`
+ * 
+ * 
  * @param {number[][]} grid
  * @return {number}
  */
