d20e93b0c309d8f6fe7b2c532de63e4e4a562ce1
yuhcee <egbouchenna001@gmail.com>
2023-04-01 00:43:00 +0100

    :tada: (minPathSum) add task solution

--- Datastructures_&_Algorithms/Dynamic Programming/minPathSum.js
+++ Datastructures_&_Algorithms/Dynamic Programming/minPathSum.js
@@ -7,14 +7,36 @@
  * **Note:** You can only move either down or right at any point in time.
  *
  * **Constraints:**
- * 
+ *
  * - `m == grid.length`
  * - `n == grid[i].length`
  * - `1 <= m, n <= 200`
  * - `0 <= grid[i][j] <= 100`
- * 
- * 
+ *
+ *
  * @param {number[][]} grid
  * @return {number}
  */
-const minPathSum = function (grid) {};
+const minPathSum = function (grid) {
+    const m = grid.length;
+    const n = grid[0].length;
+    const dp = Array(m)
+        .fill()
+        .map(() => Array(n).fill(0));
+    dp[0][0] = grid[0][0];
+    for (let i = 1; i < m; i++) {
+        dp[i][0] = grid[i][0] + dp[i - 1][0];
+    }
+
+    for (let j = 1; j < n; j++) {
+        dp[0][j] = grid[0][j] + dp[0][j - 1];
+    }
+
+    for (let i = 1; i < m; i++) {
+        for (let j = 1; j < n; j++) {
+            dp[i][j] = grid[i][j] + Math.min(dp[i - 1][j], dp[i][j - 1]);
+        }
+    }
+
+    return dp[m - 1][n - 1];
+};
