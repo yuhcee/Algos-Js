8c8a7c3136c5b3452d51d69669867d1a9b3195f7
yuhcee <egbouchenna001@gmail.com>
2023-04-03 00:28:40 +0100

    :white_check_mark: (mincostTickets) add testcase 2

--- Datastructures_&_Algorithms/Dynamic Programming/mincostTickets.js
+++ Datastructures_&_Algorithms/Dynamic Programming/mincostTickets.js
@@ -56,3 +56,12 @@ const days = [1, 4, 6, 7, 8, 20],
 // On day 20, you bought a 1-day pass for costs[0] = $2, which covered day 20.
 // In total, you spent $11 and covered all the days of your travel.
 console.log(mincostTickets(days, costs));
+
+const days1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 30, 31],
+    costs1 = [2, 7, 15];
+// Output: 17
+// Explanation: For example, here is one way to buy passes that lets you travel your travel plan:
+// On day 1, you bought a 30-day pass for costs[2] = $15 which covered days 1, 2, ..., 30.
+// On day 31, you bought a 1-day pass for costs[0] = $2 which covered day 31.
+// In total, you spent $17 and covered all the days of your travel.
+console.log(mincostTickets(days1, costs1));
