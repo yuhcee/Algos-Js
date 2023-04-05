52a8b5a3ac09414c8756b72c190a60b340aebf38
yuhcee <egbouchenna001@gmail.com>
2023-04-05 09:37:05 +0100

    :white_check_mark: (successfulPairs) add testcase 1

--- Datastructures_&_Algorithms/Arrays/successfulPairs.js
+++ Datastructures_&_Algorithms/Arrays/successfulPairs.js
@@ -60,3 +60,14 @@ function search(potions, target) {
 
     return res;
 }
+
+const spells = [5, 1, 3],
+    potions = [1, 2, 3, 4, 5],
+    success = 7;
+// Output: [4,0,3]
+/* Explanation:
+- 0th spell: 5 * [1,2,3,4,5] = [5,10,15,20,25]. 4 pairs are successful.
+- 1st spell: 1 * [1,2,3,4,5] = [1,2,3,4,5]. 0 pairs are successful.
+- 2nd spell: 3 * [1,2,3,4,5] = [3,6,9,12,15]. 3 pairs are successful.
+Thus, [4,0,3] is returned. */
+console.log(successfulPairs(spells, potions, success));
