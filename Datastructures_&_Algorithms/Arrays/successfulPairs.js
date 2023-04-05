731a4bf74eb953056862012a6d1f3e8ea1b7713b
yuhcee <egbouchenna001@gmail.com>
2023-04-05 09:36:23 +0100

    :tada: (successfulPairs) add task solution

--- Datastructures_&_Algorithms/Arrays/successfulPairs.js
+++ Datastructures_&_Algorithms/Arrays/successfulPairs.js
@@ -12,18 +12,51 @@
  * will form a successful pair with the `ith` spell*.
  *
  * **Constraints:
- * 
+ *
  * - `n == spells.length`
  * - `m == potions.length`
  * - `1 <= n, m <= 105`
  * - `1 <= spells[i], potions[i] <= 105`
  * - `1 <= success <= 1010`
- * 
+ *
  * @param {number[]} spells
  * @param {number[]} potions
  * @param {number} success
  * @return {number[]}
  */
 const successfulPairs = function (spells, potions, success) {
-        
+    let res = [];
+    potions.sort((a, b) => b - a);
+    let map = new Map();
+
+    for (let i = 0; i < spells.length; i++) {
+        if (!map.has(spells[i])) {
+            let s = success / spells[i];
+            let len = search(potions, s);
+            res.push(len);
+            map.set(spells[i], len);
+        } else {
+            let len = map.get(spells[i]);
+            res.push(len);
+        }
+    }
+
+    return res;
 };
+
+function search(potions, target) {
+    let res = 0;
+    let left = 0;
+    let right = potions.length - 1;
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2);
+        if (potions[mid] < target) {
+            right = mid - 1;
+        } else {
+            left = mid + 1;
+            res = mid + 1;
+        }
+    }
+
+    return res;
+}
