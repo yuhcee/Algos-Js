5dc99c73f62b4556a8ac2758ad975463f12ac412
yuhcee <egbouchenna001@gmail.com>
2023-04-05 18:20:29 +0100

    :books: (numRescueBoats) add task description

---
+++ Datastructures_&_Algorithms/Sorting/numRescueBoats.js
@@ -0,0 +1,15 @@
+/**
+ * **881. Boats to Save People**
+ *
+ * You are given an array `people` where `people[i]` is the weight of the `ith` person, and an
+ * **infinite number of boats** where each boat can carry a maximum weight of `limit`. Each boat
+ * carries at most two people at the same time, provided the sum of the weight of those people is at
+ * most `limit`.
+ *
+ * Return *the minimum number of boats to carry every given person*.
+ *
+ * @param {number[]} people
+ * @param {number} limit
+ * @return {number}
+ */
+const numRescueBoats = function (people, limit) {};
