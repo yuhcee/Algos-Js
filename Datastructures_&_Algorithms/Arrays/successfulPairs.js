f644e5a25d2afa357e7685fda16ba346d9578367
yuhcee <egbouchenna001@gmail.com>
2023-04-05 09:20:40 +0100

    :books: (successfulPairs) add task description

---
+++ Datastructures_&_Algorithms/Arrays/successfulPairs.js
@@ -0,0 +1,19 @@
+/**
+ * **2300. Successful Pairs of Spells and Potions**
+ *
+ * You are given two positive integer arrays `spells` and `potions`, of length `n` and `m`
+ * respectively, where `spells[i]` represents the strength of the `ith` spell and `potions[j]`
+ * represents the strength of the `jth` potion.
+ *
+ * You are also given an integer `success`. A spell and potion pair is considered **successful** if
+ * the **product** of their strengths is **at least** `success`.
+ *
+ * Return *an integer array `pairs` of length `n` where `pairs[i]` is the number of **potions** that
+ * will form a successful pair with the `ith` spell*.
+ *
+ * @param {number[]} spells
+ * @param {number[]} potions
+ * @param {number} success
+ * @return {number[]}
+ */
+const successfulPairs = function (spells, potions, success) {};
