2ab3ae6ccc6a6a7445314bed2bdaec75cc03e6ae
yuhcee <egbouchenna001@gmail.com>
2023-04-06 18:15:55 +0100

    :tada: (partitionString) add task solution

--- Datastructures_&_Algorithms/Strings/partitionString.js
+++ Datastructures_&_Algorithms/Strings/partitionString.js
@@ -17,4 +17,17 @@
  * @param {string} s
  * @return {number}
  */
-const partitionString = function (s) {};
+const partitionString = function (s) {
+    let set = new Set();
+    let count = 1;
+
+    for (const c of s) {
+        if (!set.has(c)) set.add(c);
+        else {
+            set = new Set([c]);
+            count++;
+        }
+    }
+
+    return count;
+};
