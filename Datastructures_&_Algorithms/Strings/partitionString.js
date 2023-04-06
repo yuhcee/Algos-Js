68c7f45b373fbcc363900840e61324a4c2ab7df1
yuhcee <egbouchenna001@gmail.com>
2023-04-06 18:14:22 +0100

    :books: (partitionString) add task description

---
+++ Datastructures_&_Algorithms/Strings/partitionString.js
@@ -0,0 +1,15 @@
+/**
+ * **2405. Optimal Partition of String**
+ *
+ * Given a string `s`, partition the string into one or more substrings such that the characters in
+ * each substring are **unique**. That is, no letter appears in a single substring more than
+ * **once**.
+ *
+ * Return *the **minimum** number of substrings in such a partition*.
+ *
+ * Note that each character should belong to exactly one substring in a partition.
+ *
+ * @param {string} s
+ * @return {number}
+ */
+const partitionString = function (s) {};
