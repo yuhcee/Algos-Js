e74eb5058427e3694cb3a381610d620e14f360ae
yuhcee <egbouchenna001@gmail.com>
2023-04-06 18:17:03 +0100

    :white_check_mark: (partitionString) add testcase 1

--- Datastructures_&_Algorithms/Strings/partitionString.js
+++ Datastructures_&_Algorithms/Strings/partitionString.js
@@ -31,3 +31,10 @@ const partitionString = function (s) {
 
     return count;
 };
+
+const s = 'abacaba';
+// Output: 4
+/* Explanation:
+Two possible partitions are ("a","ba","cab","a") and ("ab","a","ca","ba").
+It can be shown that 4 is the minimum number of substrings needed. */
+console.log(partitionString(s));
