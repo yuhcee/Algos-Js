82098d97c0a9f9466783f7586dc7be60ef4e667e
yuhcee <egbouchenna001@gmail.com>
2023-04-07 18:10:29 +0100

    :white_check_mark: (minimizeArrayValue) add testcase 1

--- Datastructures_&_Algorithms/Dynamic Programming/minimizeArrayValue.js
+++ Datastructures_&_Algorithms/Dynamic Programming/minimizeArrayValue.js
@@ -29,3 +29,14 @@ const minimizeArrayValue = function (nums) {
     });
     return maxNum;
 };
+
+const nums = [3, 7, 1, 6];
+// Output: 5
+/* Explanation:
+One set of optimal operations is as follows:
+1. Choose i = 1, and nums becomes [4,6,1,6].
+2. Choose i = 3, and nums becomes [4,6,2,5].
+3. Choose i = 1, and nums becomes [5,5,2,5].
+The maximum integer of nums is 5. It can be shown that the maximum number cannot be less than 5.
+Therefore, we return 5. */
+console.log(minimizeArrayValue(nums));
