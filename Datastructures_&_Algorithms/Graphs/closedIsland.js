67c9a108de7269a3573ecf43a9bfd68f871eaec0
yuhcee <egbouchenna001@gmail.com>
2023-04-08 17:53:49 +0100

    :tada: (closedIsland) add task solution

--- Datastructures_&_Algorithms/Graphs/closedIsland.js
+++ Datastructures_&_Algorithms/Graphs/closedIsland.js
@@ -15,4 +15,36 @@
  * @param {number[][]} grid
  * @return {number}
  */
-const closedIsland = function (grid) {};
+const closedIsland = function (grid) {
+    let res = 0;
+
+    for (let r = 0; r < grid.length; r++) {
+        for (let c = 0; c < grid[r].length; c++) {
+            if (grid[r][c] === 0 && (r === 0 || c === 0 || r === grid.length - 1 || c === grid[r].length - 1)) {
+                dfs(r, c);
+            }
+        }
+    }
+
+    for (let r = 0; r < grid.length; r++) {
+        for (let c = 0; c < grid[r].length; c++) {
+            if (grid[r][c] === 0) {
+                dfs(r, c);
+                res++;
+            }
+        }
+    }
+
+    return res;
+
+    function dfs(r, c) {
+        if (r < 0 || c < 0 || r >= grid.length || c >= grid[r].length || grid[r][c] === 1) return;
+
+        grid[r][c] = 1;
+
+        dfs(r + 1, c);
+        dfs(r - 1, c);
+        dfs(r, c + 1);
+        dfs(r, c - 1);
+    }
+};
