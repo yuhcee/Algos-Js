fb686989dff68ac7229a4c61355bb02b5c30781a
yuhcee <egbouchenna001@gmail.com>
2023-04-09 17:48:33 +0100

    :white_check_mark: (numEnclaves) add testcase 1

--- Datastructures_&_Algorithms/Graphs/numEnclaves.js
+++ Datastructures_&_Algorithms/Graphs/numEnclaves.js
@@ -51,3 +51,13 @@ const numEnclaves = function (A) {
         return area;
     }
 };
+
+const grid = [
+    [0, 0, 0, 0],
+    [1, 0, 1, 0],
+    [0, 1, 1, 0],
+    [0, 0, 0, 0],
+];
+// Output: 3
+// Explanation: There are three 1s that are enclosed by 0s, and one 1 that is not enclosed because its on the boundary.
+console.log(numEnclaves(grid));
