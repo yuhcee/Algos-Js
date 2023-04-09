e7d7dda2463dad6c2ec1a4e673d74e7b1fb0be61
yuhcee <egbouchenna001@gmail.com>
2023-04-09 17:47:15 +0100

    :tada: (numEnclaves) add task solution

--- Datastructures_&_Algorithms/Graphs/numEnclaves.js
+++ Datastructures_&_Algorithms/Graphs/numEnclaves.js
@@ -22,5 +22,32 @@
  * @return {number}
  */
 const numEnclaves = function (A) {
-    
+    let result = 0;
+    for (let row = 0; row < A.length; row++) {
+        for (let col = 0; col < A[row].length; col++) {
+            if (A[row][col] == 1) {
+                let localResult = dfs(row, col, 0);
+                if (localResult != -Infinity) result += localResult;
+            }
+        }
+    }
+
+    return result;
+
+    function dfs(row, col, area) {
+        if (row < 0 || col < 0 || row >= A.length || col >= A[0].length || A[row][col] == 0) return area;
+
+        if ((row == 0 || col == 0 || row == A.length - 1 || col == A[0].length - 1) && A[row][col] == 1) {
+            area = -Infinity;
+        }
+
+        area++;
+        A[row][col] = 0;
+        area = dfs(row - 1, col, area); // top
+        area = dfs(row + 1, col, area); // bottom
+        area = dfs(row, col - 1, area); // left
+        area = dfs(row, col + 1, area); // right
+
+        return area;
+    }
 };
