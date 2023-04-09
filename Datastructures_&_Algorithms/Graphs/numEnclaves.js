ab34afdb4d546dc9ce9f2e3e5dd9b250b99ca6c6
yuhcee <egbouchenna001@gmail.com>
2023-04-09 17:46:57 +0100

    :heavy_plus_sign: (numEnclaves) add constraints

--- Datastructures_&_Algorithms/Graphs/numEnclaves.js
+++ Datastructures_&_Algorithms/Graphs/numEnclaves.js
@@ -10,7 +10,17 @@
  * Return *the number of land cells in `grid` for which we cannot walk off the boundary of the grid
  * in any number of **moves**.
  *
+ *
+ * **Constraints:**
+ *
+ * - `m == grid.length`
+ * - `n == grid[i].length`
+ * - `1 <= m, n <= 500`
+ * - `grid[i][j]` is either `0` or `1`.
+ *
  * @param {number[][]} grid
  * @return {number}
  */
-const numEnclaves = function (A) {};
+const numEnclaves = function (A) {
+    
+};
