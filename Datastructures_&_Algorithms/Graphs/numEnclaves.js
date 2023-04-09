878d3a075ac4fb459629a14a35bcfe45f9aec0da
yuhcee <egbouchenna001@gmail.com>
2023-04-09 17:43:38 +0100

    :books: (numEnclaves) add task description

---
+++ Datastructures_&_Algorithms/Graphs/numEnclaves.js
@@ -0,0 +1,16 @@
+/**
+ * **1020. Number of Enclaves**
+ *
+ * You are given an `m x n` binary matrix `grid`, where `0` represents a sea cell and `1` represents
+ * a land cell.
+ *
+ * A **move** consists of walking from one land cell to another adjacent (**4-directionally**) land
+ * cell or walking off the boundary of the `grid`.
+ *
+ * Return *the number of land cells in `grid` for which we cannot walk off the boundary of the grid
+ * in any number of **moves**.
+ *
+ * @param {number[][]} grid
+ * @return {number}
+ */
+const numEnclaves = function (A) {};
