24b1ee939eb2ed743f470e1ad8c2928dfc9bfa9c
yuhcee <egbouchenna001@gmail.com>
2023-04-13 23:39:24 +0100

    :tada: (validateStackSequences) add task solution

--- Datastructures_&_Algorithms/Stacks/validateStackSequences.js
+++ Datastructures_&_Algorithms/Stacks/validateStackSequences.js
@@ -17,4 +17,25 @@
  * @param {number[]} popped
  * @return {boolean}
  */
-const validateStackSequences = function (pushed, popped) {};
+// Define a function called validateStackSequences that takes in two arrays as arguments
+const validateStackSequences = function (pushed, popped) {
+    // Create an empty array called stack to represent the stack
+    const stack = [];
+    // Initialize a variable i to 0 to represent the current index of the popped array
+    let i = 0;
+    // Loop through the pushed array using a for...of loop, using num as the iterator variable
+    for (const num of pushed) {
+        // Push the current number onto the stack
+        stack.push(num);
+        // While the stack is not empty and the top element of the stack equals the current element of the popped array
+        while (stack.length && stack[stack.length - 1] === popped[i]) {
+            // Pop the top element off the stack
+            stack.pop();
+            // Increment i to move to the next element of the popped array
+            i++;
+        }
+    }
+    // If the stack is empty, then all elements were successfully popped off and the sequences are valid, so return true
+    // Otherwise, return false
+    return stack.length === 0;
+};
