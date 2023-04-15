848c50d3e6668f318ae0a26d9810f291774a74f9
yuhcee <egbouchenna001@gmail.com>
2023-04-15 21:32:35 +0100

    :heavy_plus_sign: (maxValueOfCoins) add constraints

--- Datastructures_&_Algorithms/Dynamic Programming/maxValueOfCoins.js
+++ Datastructures_&_Algorithms/Dynamic Programming/maxValueOfCoins.js
@@ -1,18 +1,23 @@
 /**
  * **2218. Maximum Value of K Coins From Piles**
- * 
- * There are `n` **piles* of coins on a table. Each pile consists of a **positive number** of 
+ *
+ * There are `n` **piles* of coins on a table. Each pile consists of a **positive number** of
  * coins of assorted denominations.
- * 
- * In one move, you can choose any coin on **top** of any pile, remove it, and add it to your 
+ *
+ * In one move, you can choose any coin on **top** of any pile, remove it, and add it to your
  * wallet.
- * 
- * Given a list `piles`, where `piles[i]` is a list of integers denoting the composition of the 
- * `ith` pile from **top to bottom**, and a positive integer `k`, return *the **maximum total 
+ *
+ * Given a list `piles`, where `piles[i]` is a list of integers denoting the composition of the
+ * `ith` pile from **top to bottom**, and a positive integer `k`, return *the **maximum total
  * value** of coins you can have in your wallet if you choose **exactly** `k` coins optimally.
- * 
- * 
- * 
+ *
+ * **Constraints:**
+ *
+ * - `n == piles.length`
+ * - `1 <= n <= 1000`
+ * - `1 <= piles[i][j] <= 105`
+ * - `1 <= k <= sum(piles[i].length) <= 2000`
+ *
  * @param {number[][]} piles
  * @param {number} k
  * @return {number}
