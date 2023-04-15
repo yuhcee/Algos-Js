1590fe52bd9ce17db8f7c9bd967c2879d0e8b565
yuhcee <egbouchenna001@gmail.com>
2023-04-15 22:00:14 +0100

    :tada: (maxValueOfCoins) add task solution

--- Datastructures_&_Algorithms/Dynamic Programming/maxValueOfCoins.js
+++ Datastructures_&_Algorithms/Dynamic Programming/maxValueOfCoins.js
@@ -22,4 +22,44 @@
  * @param {number} k
  * @return {number}
  */
-const maxValueOfCoins = function (piles, k) {};
+
+const maxValueOfCoins = function (piles, k) {
+    const n = piles.length; // Number of piles
+    const cache = {}; // Cache to store already computed values
+
+    /**
+     * Recursive helper function that computes the maximum total value of coins
+     * you can have in your wallet if you choose exactly `k` coins optimally
+     * from the `i`th pile and onwards.
+     *
+     * @param {number} i - The index of the current pile.
+     * @param {number} k - The number of coins to choose.
+     * @return {number} The maximum total value of coins you can have in your wallet.
+     */
+    function getMaxValue(i, k) {
+        // Base case: no more piles or no more coins to choose
+        if (i >= n || k <= 0) {
+            return 0;
+        }
+
+        const key = i + ',' + k; // Create a unique key for the cache
+        if (cache[key]) {
+            return cache[key]; // Return the cached value if available
+        }
+
+        let result = getMaxValue(i + 1, k); // Skip the current pile
+        let sum = 0; // Sum of coins from the current pile
+        for (let j = 0; j < piles[i].length && j < k; j++) {
+            // Iterate through the coins in the current pile
+            sum += piles[i][j]; // Add the coin to the sum
+            result = Math.max(result, sum + getMaxValue(i + 1, k - j - 1));
+            // Recursively compute the maximum total value of coins for the remaining piles
+            // and add the current sum to it. Update the result if necessary.
+        }
+
+        cache[key] = result; // Store the result in the cache
+        return result;
+    }
+
+    return getMaxValue(0, k); // Start the recursive computation from the first pile
+};
