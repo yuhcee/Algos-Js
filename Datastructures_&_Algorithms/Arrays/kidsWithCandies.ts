be3a21fab91167e39a5af9b13be4bfb85c79026f
yuhcee <egbouchenna001@gmail.com>
2023-04-17 03:32:06 +0100

    :tada: (kidsWithCandies) add task solution

--- Datastructures_&_Algorithms/Arrays/kidsWithCandies.ts
+++ Datastructures_&_Algorithms/Arrays/kidsWithCandies.ts
@@ -15,4 +15,17 @@
  * @param {number} extraCandies
  * @return {boolean[]}
  */
-const kidsWithCandies = function (candies: number[], extraCandies: number): boolean[] {};
+const kidsWithCandies = function (candies: number[], extraCandies: number): boolean[] {
+    const max: number = Math.max(...candies);
+    const result: boolean[] = [];
+
+    for (let i = 0; i < candies.length; i++) {
+        if (candies[i] + extraCandies >= max) {
+            result.push(true);
+        } else {
+            result.push(false);
+        }
+    }
+
+    return result;
+};
