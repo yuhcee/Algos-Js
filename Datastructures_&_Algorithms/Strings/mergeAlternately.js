f3edccca6e0b2e38b96327c2338cd96239e87397
yuhcee <egbouchenna001@gmail.com>
2023-04-18 17:31:58 +0100

    :books: (mergeAlternately) add task description

---
+++ Datastructures_&_Algorithms/Strings/mergeAlternately.js
@@ -0,0 +1,14 @@
+/**
+ * **1768. Merge Strings Alternately**
+ *
+ * You are given two strings `word1` and `word2`. Merge the strings by adding letters in alternating
+ * order, starting with `word1`. If a string is longer than the other, append the additional letters
+ * onto the end of the merged string.
+ *
+ * Return *the merged string*.
+ *
+ * @param {string} word1
+ * @param {string} word2
+ * @return {string}
+ */
+const mergeAlternately = function (word1, word2) {};
