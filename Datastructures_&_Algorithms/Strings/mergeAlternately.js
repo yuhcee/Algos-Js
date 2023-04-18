43086e0f1f3b5711e732a0c25f6eabb1d6e4fe54
yuhcee <egbouchenna001@gmail.com>
2023-04-18 17:35:21 +0100

    :tada: (mergeAlternately) add task solution

--- Datastructures_&_Algorithms/Strings/mergeAlternately.js
+++ Datastructures_&_Algorithms/Strings/mergeAlternately.js
@@ -16,4 +16,20 @@
  * @param {string} word2
  * @return {string}
  */
-const mergeAlternately = function (word1, word2) {};
+const mergeAlternately = function (word1, word2) {
+    let merged = '';
+    let i = 0;
+    let j = 0;
+
+    while (i < word1.length || j < word2.length) {
+        if (i < word1.length) {
+            merged += word1[i];
+            i++;
+        }
+        if (j < word2.length) {
+            merged += word2[j];
+            j++;
+        }
+    }
+    return merged;
+};
