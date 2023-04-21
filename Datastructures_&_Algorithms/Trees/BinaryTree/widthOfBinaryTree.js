d55f371b7a79140e89054aadb0daa327bd604cb3
yuhcee <egbouchenna001@gmail.com>
2023-04-21 23:36:41 +0100

    :tada: (widthOfBinaryTree) add task solution

--- Datastructures_&_Algorithms/Trees/BinaryTree/widthOfBinaryTree.js
+++ Datastructures_&_Algorithms/Trees/BinaryTree/widthOfBinaryTree.js
@@ -30,4 +30,55 @@
  * @param {TreeNode} root
  * @return {number}
  */
-const widthOfBinaryTree = function (root) {};
+const widthOfBinaryTree = function (root) {
+    // Check if the root node exists
+    if (!root) {
+      // If not, return 0 because there's no tree to traverse
+      return 0;
+    }
+  
+    // Initialize the maximum width to 0
+    let maxWidth = 0;
+    // Create a queue to perform the BFS traversal
+    const queue = [{ node: root, index: 1 }];
+  
+    // While there are nodes in the queue
+    while (queue.length) {
+      // Get the size of the current level
+      const levelSize = queue.length;
+      // Initialize the index of the leftmost non-null node to null
+      let leftmostIndex = null;
+  
+      // Loop through each node in the current level
+      for (let i = 0; i < levelSize; i++) {
+        // Get the next node from the front of the queue
+        const { node, index } = queue.shift();
+  
+        // If this is the first node in the level, set the leftmostIndex to its index
+        if (i === 0) {
+          leftmostIndex = index;
+        }
+  
+        // Add the left child to the queue if it exists
+        if (node.left) {
+          // The left child's index is double the parent's index
+          queue.push({ node: node.left, index: index * 2 });
+        }
+  
+        // Add the right child to the queue if it exists
+        if (node.right) {
+          // The right child's index is double the parent's index plus 1
+          queue.push({ node: node.right, index: index * 2 + 1 });
+        }
+      }
+  
+      // Calculate the width of the current level
+      const width = queue.length ? queue[0].index - leftmostIndex + 1 : 0;
+      // Update the maximum width if necessary
+      maxWidth = Math.max(maxWidth, width);
+    }
+  
+    // Return the maximum width
+    return maxWidth;
+  };
+  
