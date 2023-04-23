1077edeeed80d8d874a74d8d4f22c12e873ee427
yuhcee <egbouchenna001@gmail.com>
2023-04-23 18:02:00 +0100

    :tada: (numberOfArrays) add task solution

--- Datastructures_&_Algorithms/Dynamic Programming/numberOfArrays.js
+++ Datastructures_&_Algorithms/Dynamic Programming/numberOfArrays.js
@@ -19,4 +19,60 @@
  * @param {number} k
  * @return {number}
  */
-const numberOfArrays = function (s, k) {};
+const numberOfArrays = function (s, k) {
+    // Initialize the number of arrays to 0
+    let numberOfArrays = 0;
+    // Create a memo to store the number of arrays for each substring
+    const memo = {};
+
+    // Create a helper function to recursively find the number of arrays
+    const findNumberOfArrays = (startIndex) => {
+        // If the startIndex is equal to the length of the string, return 1
+        if (startIndex === s.length) {
+            return 1;
+        }
+
+        // If the startIndex is greater than the length of the string, return 0
+        if (startIndex > s.length) {
+            return 0;
+        }
+
+        // If the number of arrays for the substring starting at startIndex is already in the memo,
+        // return it
+        if (memo[startIndex] !== undefined) {
+            return memo[startIndex];
+        }
+
+        // Initialize the number of arrays for the substring starting at startIndex to 0
+        let numberOfArrays = 0;
+
+        // Loop through the possible lengths of the substring
+        for (let length = 1; length <= s.length - startIndex; length++) {
+            // Get the substring starting at startIndex with the current length
+            const substring = s.substring(startIndex, startIndex + length);
+
+            // If the substring is greater than k, break out of the loop
+            if (substring > k) {
+                break;
+            }
+
+            // If the substring starts with 0, continue to the next iteration
+            if (substring.startsWith('0')) {
+                continue;
+            }
+
+            // Add the number of arrays for the substring starting at startIndex + length to the
+            // number of arrays for the substring starting at startIndex
+            numberOfArrays += findNumberOfArrays(startIndex + length);
+        }
+
+        // Store the number of arrays for the substring starting at startIndex in the memo
+        memo[startIndex] = numberOfArrays;
+
+        // Return the number of arrays for the substring starting at startIndex
+        return numberOfArrays;
+    };
+
+    // Return the number of arrays for the substring starting at 0
+    return findNumberOfArrays(0);
+};
