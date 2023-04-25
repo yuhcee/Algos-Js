25dcf3dcbe59f89d83d9173e02200af261b5433f
yuhcee <egbouchenna001@gmail.com>
2023-04-25 09:51:57 +0100

    :heavy_plus_sign: (search) add constraints

--- Datastructures_&_Algorithms/Arrays/search.js
+++ Datastructures_&_Algorithms/Arrays/search.js
@@ -7,6 +7,13 @@
  *
  * You must write an algorithm with `O(log n)` runtime complexity.
  *
+ * **Constraints:**
+ *
+ * - `1 <= nums.length <= 104`
+ * - `-104 < nums[i], target < 104`
+ * - All the integers in `nums` are **unique**.
+ * - `nums` is sorted in ascending order.
+ *
  * @param {number[]} nums
  * @param {number} target
  * @return {number}
