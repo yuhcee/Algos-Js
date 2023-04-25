4a2bbe132125cd7f131733cec982298a62f0660c
yuhcee <egbouchenna001@gmail.com>
2023-04-25 09:58:08 +0100

    :tada: (search) add task solution

--- Datastructures_&_Algorithms/Arrays/search.js
+++ Datastructures_&_Algorithms/Arrays/search.js
@@ -18,4 +18,19 @@
  * @param {number} target
  * @return {number}
  */
-const search = function (nums, target) {};
+const search = function (nums, target) {
+    let left = 0;
+    let right = nums.length - 1;
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2);
+        if (target === nums[mid]) {
+            return mid;
+        } else if (target < nums[mid]) {
+            right = mid - 1;
+        } else {
+            left = mid + 1;
+        }
+    }
+
+    return -1;
+};
