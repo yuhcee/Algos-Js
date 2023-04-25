58685b7c3a9598e42111f3cf1240ee4c2ac058e1
yuhcee <egbouchenna001@gmail.com>
2023-04-25 09:59:59 +0100

    :white_check_mark: (search) add testcase 2

--- Datastructures_&_Algorithms/Arrays/search.js
+++ Datastructures_&_Algorithms/Arrays/search.js
@@ -40,3 +40,9 @@ const nums = [-1, 0, 3, 5, 9, 12],
 // Output: 4
 // Explanation: 9 exists in nums and its index is 4
 console.log(search(nums, target));
+
+const nums1 = [-1, 0, 3, 5, 9, 12],
+    target1 = 2;
+// Output: -1
+// Explanation: 2 does not exist in nums so return -1
+console.log(search(nums1, target1));
