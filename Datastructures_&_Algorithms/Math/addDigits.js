9efea1994f0cfdca8d78432917cd4abd73f08293
yuhcee <egbouchenna001@gmail.com>
2023-04-26 15:11:31 +0100

    :books: (addDigits) add task description

---
+++ Datastructures_&_Algorithms/Math/addDigits.js
@@ -0,0 +1,9 @@
+/**
+ * **258. Add Digits**
+ * 
+ * Given an integer `num`, repeatedly add all its digits until the result has only one digit, and return it.
+ * 
+ * @param {number} num
+ * @return {number}
+ */
+const addDigits = function (num) {};
