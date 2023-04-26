cb2545bce4fc62042d44e5dd8aab647c579d0d8b
yuhcee <egbouchenna001@gmail.com>
2023-04-26 15:15:40 +0100

    :heavy_plus_sign: (addDigits) add constraints

--- Datastructures_&_Algorithms/Math/addDigits.js
+++ Datastructures_&_Algorithms/Math/addDigits.js
@@ -1,8 +1,12 @@
 /**
  * **258. Add Digits**
- * 
+ *
  * Given an integer `num`, repeatedly add all its digits until the result has only one digit, and return it.
- * 
+ *
+ * **Constraints:**
+ *
+ * - `0 <= num <= 231 - 1`
+ *
  * @param {number} num
  * @return {number}
  */
