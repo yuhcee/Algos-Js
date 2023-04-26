9c7fbb293309483d5be48e7d306f486342373fa2
yuhcee <egbouchenna001@gmail.com>
2023-04-26 15:17:07 +0100

    :white_check_mark: (addDigits) add testcase 1

--- Datastructures_&_Algorithms/Math/addDigits.js
+++ Datastructures_&_Algorithms/Math/addDigits.js
@@ -19,3 +19,11 @@ const addDigits = function (num) {
 
     return sum < 10 ? sum : addDigits(sum);
 };
+
+const num = 38;
+// Output: 2
+// Explanation: The process is
+// 38 --> 3 + 8 --> 11
+// 11 --> 1 + 1 --> 2
+// Since 2 has only one digit, return it
+console.log(addDigits(num));
