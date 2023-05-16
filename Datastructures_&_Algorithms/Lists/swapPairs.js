a860aa82b66e714c74da39abdbc56d7fc555dbde
yuhcee <egbouchenna001@gmail.com>
2023-05-16 08:58:05 +0100

    :sparkles: (swapPairs) add task solution

--- Datastructures_&_Algorithms/Lists/swapPairs.js
+++ Datastructures_&_Algorithms/Lists/swapPairs.js
@@ -1,15 +1,15 @@
 /**
  * **24. Swap Nodes in Pairs**
- * 
+ *
  * Given a linked list, swap every two adjacent nodes and return its head.
- * You must solve the problem without modifying the values in the list's nodes (i.e., 
+ * You must solve the problem without modifying the values in the list's nodes (i.e.,
  * only nodes themselves may be changed.)
- * 
+ *
  * **Constraints:**
- * 
+ *
  * - The number of nodes in the list is in the range `[0, 100]`.
  * - `0 <= Node.val <= 100`
- * 
+ *
  * Definition for singly-linked list.
  * function ListNode(val, next) {
  *     this.val = (val === undefined ? 0 : val)
@@ -22,4 +22,32 @@
  * @param {ListNode} head - The head of the linked list.
  * @returns {ListNode} - The head of the modified linked list.
  */
-const swapPairs = function (head) {};
+const swapPairs = function (head) {
+    // Create a dummy node as the previous node.
+    const dummy = new ListNode(0);
+    dummy.next = head;
+
+    // Set the previous node as the dummy node.
+    let prev = dummy;
+
+    while (head && head.next) {
+        // Get the first and second nodes to be swapped.
+        const node1 = head;
+        const node2 = head.next;
+
+        // Update the previous node's next pointer.
+        prev.next = node2;
+
+        // Perform the swap operation.
+        const next = node2.next;
+        node2.next = node1;
+        node1.next = next;
+
+        // Update the previous node and move to the next pair.
+        prev = node1;
+        head = next;
+    }
+
+    // Return the head of the modified linked list.
+    return dummy.next;
+};
