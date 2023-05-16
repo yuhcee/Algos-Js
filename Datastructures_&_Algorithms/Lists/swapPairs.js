42148daf4f7a86f67808c40f3ad81d6ce0423a73
yuhcee <egbouchenna001@gmail.com>
2023-05-16 08:55:03 +0100

    :books: (swapPairs) add task description

---
+++ Datastructures_&_Algorithms/Lists/swapPairs.js
@@ -0,0 +1,20 @@
+/**
+ * **24. Swap Nodes in Pairs**
+ * 
+ * Given a linked list, swap every two adjacent nodes and return its head.
+ * You must solve the problem without modifying the values in the list's nodes (i.e., 
+ * only nodes themselves may be changed.)
+ * 
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val === undefined ? 0 : val)
+ *     this.next = (next === undefined ? null : next)
+ * }
+ */
+
+/**
+ * Swaps every two adjacent nodes in the given linked list.
+ * @param {ListNode} head - The head of the linked list.
+ * @returns {ListNode} - The head of the modified linked list.
+ */
+const swapPairs = function (head) {};
