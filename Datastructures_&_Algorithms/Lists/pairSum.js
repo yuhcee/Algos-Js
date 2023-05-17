82402ac7db2429d593cc89edc3d1988f56fd8641
yuhcee <egbouchenna001@gmail.com>
2023-05-17 12:45:07 +0100

    :white_check_mark (pairSum) add testcase 4

--- Datastructures_&_Algorithms/Lists/pairSum.js
+++ Datastructures_&_Algorithms/Lists/pairSum.js
@@ -66,10 +66,15 @@ Nodes 0 and 1 are the twins of nodes 3 and 2, respectively. All have twin sum = 
 There are no other nodes with twins in the linked list.
 Thus, the maximum twin sum of the linked list is 6. */
 
-const head2 = [4,2,2,3]
+const head2 = [4, 2, 2, 3];
 // Output: 7
 /* Explanation:
 The nodes with twins present in this linked list are:
 - Node 0 is the twin of node 3 having a twin sum of 4 + 3 = 7.
 - Node 1 is the twin of node 2 having a twin sum of 2 + 2 = 4.
-Thus, the maximum twin sum of the linked list is max(7, 4) = 7. */ 
+Thus, the maximum twin sum of the linked list is max(7, 4) = 7. */
+
+const head3 = [1, 100000];
+// Output: 100001
+/* Explanation:
+There is only one node with a twin in the linked list having twin sum of 1 + 100000 = 100001. */
