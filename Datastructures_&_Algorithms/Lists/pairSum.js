7cfca0668779552b47969d938fd806f418fb8d9c
yuhcee <egbouchenna001@gmail.com>
2023-05-17 12:23:47 +0100

    :books: (pairSum) add task description

---
+++ Datastructures_&_Algorithms/Lists/pairSum.js
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val === undefined ? 0 : val)
+ *     this.next = (next === undefined ? null : next)
+ * }
+ */
+
+/**
+ * **2130. Maximum Twin Sum of a Linked List**
+ *
+ * In a linked list of size `n`, where `n` is even, the `ith` node
+ * (0-indexed) of the linked list is known as the twin of the `(n-1-i)th`
+ * node, if `0 <= i <= (n / 2) - 1`.
+ *
+ * - For example, if `n = 4`, then node `0` is the twin of node `3`, and
+ * node `1` is the twin of node `2`. These are the only nodes with twins
+ * for `n = 4`.
+ * The **twin sum** is defined as the sum of a node and its twin.
+ *
+ * Given the `head` of a linked list with even length, return the
+ * ***maximum twin sum** of the linked list*.
+ * 
+ * 
+ *
+ * Returns the maximum twin sum of the linked list.
+ * @param {ListNode} head - The head of the linked list.
+ * @returns {number} - The maximum twin sum.
+ */
+const pairSum = function (head) {};
