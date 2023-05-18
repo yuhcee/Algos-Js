dc71fcaaacb30bbaa11ec7f744c7be604798865d
yuhcee <egbouchenna001@gmail.com>
2023-05-18 23:27:37 +0100

    :books: (findSmallestSetOfVertices) add task description

---
+++ Datastructures_&_Algorithms/Graphs/findSmallestSetOfVertices.js
@@ -0,0 +1,20 @@
+/**
+ * **1557. Minimum Number of Vertices to Reach All Nodes**
+ * 
+ * Given a **directed acyclic graph**, with `n` vertices numbered from `0` to 
+ * `n-1`, and an array `edges` where `edges[i] = [fromi, toi]` represents a 
+ * directed edge from node `fromi` to node `toi`.
+ * 
+ * *Find the smallest set of vertices from which all nodes in the graph are 
+ * reachable. It's guaranteed that a unique solution exists.*
+ * 
+ * Notice that you can return the vertices in any order.
+ * 
+ * @param {number} n - The number of vertices in the graph.
+ * @param {number[][]} edges - The array of directed edges.
+ * @return {number[]} - The smallest set of vertices from which all nodes 
+ * are reachable.
+ */
+const findSmallestSetOfVertices = function (n, edges) {
+   
+};
