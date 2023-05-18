4f42332e8b570b0b8629d65cb0aa69a25bf955b4
yuhcee <egbouchenna001@gmail.com>
2023-05-18 23:30:49 +0100

    :white_check_mark (findSmallestSetOfVertices) add testcase 1

--- Datastructures_&_Algorithms/Graphs/findSmallestSetOfVertices.js
+++ Datastructures_&_Algorithms/Graphs/findSmallestSetOfVertices.js
@@ -1,26 +1,26 @@
 /**
  * **1557. Minimum Number of Vertices to Reach All Nodes**
- * 
- * Given a **directed acyclic graph**, with `n` vertices numbered from `0` to 
- * `n-1`, and an array `edges` where `edges[i] = [fromi, toi]` represents a 
+ *
+ * Given a **directed acyclic graph**, with `n` vertices numbered from `0` to
+ * `n-1`, and an array `edges` where `edges[i] = [fromi, toi]` represents a
  * directed edge from node `fromi` to node `toi`.
- * 
- * *Find the smallest set of vertices from which all nodes in the graph are 
+ *
+ * *Find the smallest set of vertices from which all nodes in the graph are
  * reachable. It's guaranteed that a unique solution exists.*
- * 
+ *
  * Notice that you can return the vertices in any order.
- * 
+ *
  * **Constraints:**
- * 
+ *
  * - `2 <= n <= 10^5`
  * - `1 <= edges.length <= min(10^5, n * (n - 1) / 2)`
  * - `edges[i].length == 2`
  * - `0 <= fromi, toi < n`
  * - All pairs `(fromi, toi)` are distinct.
- * 
+ *
  * @param {number} n - The number of vertices in the graph.
  * @param {number[][]} edges - The array of directed edges.
- * @return {number[]} - The smallest set of vertices from which all nodes 
+ * @return {number[]} - The smallest set of vertices from which all nodes
  * are reachable.
  */
 const findSmallestSetOfVertices = function (n, edges) {
@@ -43,3 +43,16 @@ const findSmallestSetOfVertices = function (n, edges) {
 
     return smallestSet;
 };
+
+const n = 6;
+const edges = [
+    [0, 1],
+    [0, 2],
+    [2, 5],
+    [3, 4],
+    [4, 2],
+];
+// Output: [0, 3]
+/* Explanation: It's not possible to reach all the nodes from a single vertex.
+From 0 we can reach [0,1,2,5]. From 3 we can reach [3,4,2,5]. So we output [0,3]. */
+console.log(findSmallestSetOfVertices(n, edges));
