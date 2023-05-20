f85a6ffa8a9e14c1de0409a5fe20a01889425e8e
yuhcee <egbouchenna001@gmail.com>
2023-05-20 17:35:09 +0100

    :heavy_plus_sign: (calEquation) add constraints

--- Datastructures_&_Algorithms/HashMaps/calEquation.js
+++ Datastructures_&_Algorithms/HashMaps/calEquation.js
@@ -13,6 +13,18 @@
  * **Note**: The input is always valid. You may assume that evaluating the queries will not result
  * in division by zero and that there is no contradiction.
  *
+ *  **Constraints:**
+ * 
+ * - `1 <= equations.length <= 20`
+ * - `equations[i].length == 2`
+ * - `1 <= Ai.length, Bi.length <= 5`
+ * - `values.length == equations.length`
+ * - `0.0 < values[i] <= 20.0`
+ * - `1 <= queries.length <= 20`
+ * - `queries[i].length == 2`
+ * - `1 <= Cj.length, Dj.length <= 5`
+ * - `Ai, Bi, Cj, Dj` consist of lower case English letters and digits.
+ * 
  * @param {string[][]} equations - Array of variable pairs equations.
  * @param {number[]} values - Array of real numbers representing the equation values.
  * @param {string[][]} queries - Array of queries to evaluate.
@@ -21,5 +33,3 @@
 const calcEquation = (equations, values, queries) => {
     
 };
-
-
