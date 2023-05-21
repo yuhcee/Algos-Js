c1d5e275aa03d8721c0a9c594781fcc172a6adb0
yuhcee <egbouchenna001@gmail.com>
2023-05-21 23:48:13 +0100

    :sparkles: (shortestBridge) add task solution

--- Datastructures_&_Algorithms/Graphs/shortestBridge.js
+++ Datastructures_&_Algorithms/Graphs/shortestBridge.js
@@ -21,4 +21,89 @@
  * @param {number[][]} grid
  * @return {number}
  */
-const shortestBridge = function (grid) {};
+const shortestBridge = function (grid) {
+    const directions = [
+        [-1, 0],
+        [0, -1],
+        [1, 0],
+        [0, 1],
+    ]; // Possible directions: up, left, down, right
+    const m = grid.length; // Number of rows
+    const n = grid[0].length; // Number of columns
+
+    // Function to mark the first island using depth-first search (DFS)
+    const markFirstIsland = (i, j) => {
+        if (i < 0 || i >= m || j < 0 || j >= n || grid[i][j] !== 1) {
+            return;
+        }
+
+        // Mark the cell as visited
+        grid[i][j] = -1;
+
+        // Recursively mark the neighboring cells
+        for (const [dx, dy] of directions) {
+            markFirstIsland(i + dx, j + dy);
+        }
+    };
+
+    // Function to find the shortest path to the second island using breadth-first search (BFS)
+    const findShortestPath = (queue) => {
+        let steps = 0;
+
+        while (queue.length > 0) {
+            const size = queue.length;
+
+            for (let i = 0; i < size; i++) {
+                const [x, y] = queue.shift();
+
+                for (const [dx, dy] of directions) {
+                    const newX = x + dx;
+                    const newY = y + dy;
+
+                    if (newX < 0 || newX >= m || newY < 0 || newY >= n || grid[newX][newY] === -1) {
+                        continue; // Skip if out of bounds or already visited
+                    }
+
+                    if (grid[newX][newY] === 1) {
+                        return steps; // Found the second island
+                    }
+
+                    grid[newX][newY] = -1; // Mark as visited
+                    queue.push([newX, newY]); // Add to the queue for further exploration
+                }
+            }
+
+            steps++; // Increment the number of steps
+        }
+
+        return -1; // No path to the second island found
+    };
+
+    // Step 1: Mark the first island and identify its cells
+    let firstIslandFound = false;
+    for (let i = 0; i < m; i++) {
+        if (firstIslandFound) {
+            break; // Exit the loop once the first island is found
+        }
+
+        for (let j = 0; j < n; j++) {
+            if (grid[i][j] === 1) {
+                markFirstIsland(i, j);
+                firstIslandFound = true;
+                break;
+            }
+        }
+    }
+
+    // Step 2: Find the shortest path to the second island from the cells of the first island
+    const queue = [];
+    for (let i = 0; i < m; i++) {
+        for (let j = 0; j < n; j++) {
+            if (grid[i][j] === -1) {
+                queue.push([i, j]);
+            }
+        }
+    }
+
+    return findShortestPath(queue);
+};
