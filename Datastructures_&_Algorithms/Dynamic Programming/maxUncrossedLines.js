eb4cb3390c84147533c173b0d37febdb734556f6
yuhcee <egbouchenna001@gmail.com>
2023-05-22 23:06:24 +0100

    :books: (maxUncrossedLines) add task description

---
+++ Datastructures_&_Algorithms/Dynamic Programming/maxUncrossedLines.js
@@ -0,0 +1,22 @@
+/**
+ * **1035. Uncrossed Lines**
+ *
+ * You are given two integer arrays `nums1` and `nums2`. We write the integers of `nums1`
+ * and `nums2` (in the order they are given) on two separate horizontal lines.
+ *
+ * We may draw connecting lines: a straight line connecting two numbers `nums1[i]` and
+ * `nums2[j]` such that:
+ *
+ * - `nums1[i] == nums2[j]`, and
+ * - the line we draw does not intersect any other connecting (non-horizontal) line.
+ *
+ * Note that a connecting line cannot intersect even at the endpoints (i.e., each number
+ * can only belong to one connecting line).
+ *
+ * Return *the maximum number of connecting lines we can draw in this way*.
+ *
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ */
+const maxUncrossedLines = function (nums1, nums2) {};
