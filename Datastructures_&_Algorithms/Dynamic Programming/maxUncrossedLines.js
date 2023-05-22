e86f80a50b4f5ffb6093f936f215e5474833d2e9
yuhcee <egbouchenna001@gmail.com>
2023-05-22 23:08:08 +0100

    :sparkles: (maxUncrossedLines) add task solution

--- Datastructures_&_Algorithms/Dynamic Programming/maxUncrossedLines.js
+++ Datastructures_&_Algorithms/Dynamic Programming/maxUncrossedLines.js
@@ -24,4 +24,29 @@
  * @param {number[]} nums2
  * @return {number}
  */
-const maxUncrossedLines = function (nums1, nums2) {};
+const maxUncrossedLines = function (nums1, nums2) {
+    const m = nums1.length;
+    const n = nums2.length;
+
+    // Create a 2D array to store the maximum number of connecting lines
+    const dp = new Array(m + 1);
+    for (let i = 0; i <= m; i++) {
+        dp[i] = new Array(n + 1).fill(0);
+    }
+
+    // Iterate through the arrays and fill in the DP array
+    for (let i = 1; i <= m; i++) {
+        for (let j = 1; j <= n; j++) {
+            if (nums1[i - 1] === nums2[j - 1]) {
+                // If the numbers are equal, we can draw a connecting line
+                dp[i][j] = dp[i - 1][j - 1] + 1;
+            } else {
+                // If the numbers are not equal, we take the maximum of the previous lines
+                dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+    }
+
+    // The bottom-right element of the DP array contains the maximum number of connecting lines
+    return dp[m][n];
+};
