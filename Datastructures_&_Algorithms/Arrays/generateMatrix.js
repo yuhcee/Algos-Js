668ae39302ee2bf59fac69cf5f6a591ce01313c0
yuhcee <egbouchenna001@gmail.com>
2023-05-23 21:48:12 +0100

    :white_check_mark (generateMatrix) add testcase 3

--- Datastructures_&_Algorithms/Arrays/generateMatrix.js
+++ Datastructures_&_Algorithms/Arrays/generateMatrix.js
@@ -59,3 +59,7 @@ console.log(generateMatrix(n));
 const n1 = 1;
 // Output: [[1]]
 console.log(generateMatrix(n1));
+
+const n2 = 4;
+// Output: [[1,2,3,4],[12,13,14,5],[11,16,15,6],[10,9,8,7]]
+console.log(generateMatrix(n2));
