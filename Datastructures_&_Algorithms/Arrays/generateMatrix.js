136cf1c6483015a7996b53fb0557a5dbf9649e06
yuhcee <egbouchenna001@gmail.com>
2023-05-23 21:40:29 +0100

    :heavy_plus_sign: (generateMatrix) add constraints

--- Datastructures_&_Algorithms/Arrays/generateMatrix.js
+++ Datastructures_&_Algorithms/Arrays/generateMatrix.js
@@ -4,6 +4,10 @@
  * Given a positive integer `n`, generate an `n x n` `matrix` filled with elements from 
  * `1` to `n2` in spiral order.
  * 
+ * **Constraints:**
+ * 
+ * - `1 <= n <= 20`
+ * 
  * @param {number} n
  * @return {number[][]}
  */
