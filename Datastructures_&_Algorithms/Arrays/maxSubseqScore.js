44890b8680f673dead5ab6f29d801f2c2548b2da
yuhcee <egbouchenna001@gmail.com>
2023-05-24 17:22:45 +0100

    :heavy_plus_sign: (maxScore) add MinHeap class implementation

--- Datastructures_&_Algorithms/Arrays/maxSubseqScore.js
+++ Datastructures_&_Algorithms/Arrays/maxSubseqScore.js
@@ -18,15 +18,63 @@
  * {0, 1, ..., n-1}` by deleting some or no elements.
  *
  * **Constraints:**
- * 
+ *
  * - `n == nums1.length == nums2.length`
  * - `1 <= n <= 105`
  * - `0 <= nums1[i], nums2[j] <= 105`
  * - `1 <= k <= n`
- * 
+ *
  * @param {number[]} nums1
  * @param {number[]} nums2
  * @param {number} k
  * @return {number}
  */
 const maxScore = function (nums1, nums2, k) {};
+
+function swap(arr, i, j) {
+    const temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+}
+
+// MinHeap class implementation
+class MinHeap {
+    heap = [];
+
+    // Add an item to the heap
+    add(item) {
+        this.heap.push(item);
+        let index = this.heap.length - 1;
+        let parentIndex = Math.floor((index - 1) / 2);
+        // Perform heapify-up to maintain heap property
+        while (parentIndex >= 0 && this.heap[parentIndex] > this.heap[index]) {
+            swap(this.heap, parentIndex, index);
+            index = parentIndex;
+            parentIndex = Math.floor((index - 1) / 2);
+        }
+    }
+
+    // Remove the root item (smallest element) from the heap
+    remove() {
+        if (!this.heap.length) {
+            return null;
+        }
+        const item = this.heap[0];
+        this.heap[0] = this.heap[this.heap.length - 1];
+        this.heap.pop();
+        let index = 0;
+        // Perform heapify-down to maintain heap property
+        while (2 * index + 1 < this.heap.length) {
+            let smallerChildIndex = 2 * index + 1;
+            if (2 * index + 2 < this.heap.length && this.heap[2 * index + 2] < this.heap[2 * index + 1]) {
+                smallerChildIndex = 2 * index + 2;
+            }
+            if (this.heap[index] < this.heap[smallerChildIndex]) {
+                break;
+            }
+            swap(this.heap, index, smallerChildIndex);
+            index = smallerChildIndex;
+        }
+        return item;
+    }
+}
