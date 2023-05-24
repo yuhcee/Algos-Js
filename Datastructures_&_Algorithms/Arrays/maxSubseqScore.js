5834e1faf4000e1ea7f7635fed8a31847095db06
yuhcee <egbouchenna001@gmail.com>
2023-05-24 17:24:42 +0100

    :white_check_mark (maxScore) add testcase 1

--- Datastructures_&_Algorithms/Arrays/maxSubseqScore.js
+++ Datastructures_&_Algorithms/Arrays/maxSubseqScore.js
@@ -110,3 +110,16 @@ class MinHeap {
         return item;
     }
 }
+
+const nums1 = [1, 3, 3, 2],
+    nums2 = [2, 1, 3, 4],
+    k = 3;
+// Output: 12
+/* Explanation: 
+The four possible subsequence scores are:
+- We choose the indices 0, 1, and 2 with score = (1+3+3) * min(2,1,3) = 7.
+- We choose the indices 0, 1, and 3 with score = (1+3+2) * min(2,1,4) = 6. 
+- We choose the indices 0, 2, and 3 with score = (1+3+2) * min(2,3,4) = 12. 
+- We choose the indices 1, 2, and 3 with score = (3+3+2) * min(1,3,4) = 8.
+Therefore, we return the max score, which is 12. */
+console.log(maxScore(nums1, nums2, k));
