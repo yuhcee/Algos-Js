a089d3137e4e7558f2aba5cbcfe8dd45b5c29d1d
yuhcee <egbouchenna001@gmail.com>
2023-05-28 23:57:40 +0100

    :white_check_mark (minCostToCutStick) add testcase 2

--- Datastructures_&_Algorithms/Dynamic Programming/minCostToCutStick.js
+++ Datastructures_&_Algorithms/Dynamic Programming/minCostToCutStick.js
@@ -59,3 +59,10 @@ const n = 7,
 /* The first cut is done to a rod of length 7 so the cost is 7. The second cut is done to a rod of length 6 (i.e. the second part of the first cut), the third is done to a rod of length 4 and the last cut is to a rod of length 3. The total cost is 7 + 6 + 4 + 3 = 20.
 Rearranging the cuts to be [3, 5, 1, 4] for example will lead to a scenario with total cost = 16 (as shown in the example photo 7 + 4 + 3 + 2 = 16). */
 console.log(minCostToCutStick(n, cuts));
+
+const n1 = 9,
+    cuts1 = [5, 6, 1, 4, 2];
+// Output: 22
+/* Explanation: If you try the given cuts ordering the cost will be 25.
+There are much ordering with total cost <= 25, for example, the order [4, 6, 5, 2, 1] has total cost = 22 which is the minimum possible. */
+console.log(minCostToCutStick(n1, cuts1));
