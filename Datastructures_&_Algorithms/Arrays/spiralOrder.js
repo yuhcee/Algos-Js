905e3a108705d0fa1edb7aaae1ddfc9af8f76b0d
yuhcee <egbouchenna001@gmail.com>
2023-05-30 00:00:55 +0100

    :white_check_mark (spiralOrder) add testcase 3

--- Datastructures_&_Algorithms/Arrays/spiralOrder.js
+++ Datastructures_&_Algorithms/Arrays/spiralOrder.js
@@ -68,6 +68,20 @@ const matrix = [
 // Output: [1,2,3,6,9,8,7,4,5]
 console.log(spiralOrder(matrix));
 
-const matrix1 = [[1,2,3,4],[5,6,7,8],[9,10,11,12]]
+const matrix1 = [
+    [1, 2, 3, 4],
+    [5, 6, 7, 8],
+    [9, 10, 11, 12],
+];
 // Output: [1,2,3,4,8,12,11,10,9,5,6,7]
 console.log(spiralOrder(matrix1));
+
+const matrix2 = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [10, 11, 12],
+    [13, 14, 15],
+];
+// Output: [1,2,3,6,9,12,15,14,13,10,7,4,5,8,11]
+console.log(spiralOrder(matrix2));
