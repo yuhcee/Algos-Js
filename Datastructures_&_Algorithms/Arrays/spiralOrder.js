332b56a304dafd5b69aaef0c40a32af4ebd2553d
yuhcee <egbouchenna001@gmail.com>
2023-05-29 23:57:29 +0100

    :white_check_mark (spiralOrder) add testcase 1

--- Datastructures_&_Algorithms/Arrays/spiralOrder.js
+++ Datastructures_&_Algorithms/Arrays/spiralOrder.js
@@ -59,3 +59,11 @@ const spiralOrder = function (matrix) {
 
     return result;
 };
+
+const matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+];
+// Output: [1,2,3,6,9,8,7,4,5]
+console.log(spiralOrder(matrix));
