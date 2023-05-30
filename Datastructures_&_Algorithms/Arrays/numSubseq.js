f026d8e139080eb5e30778f3d1b1cd733f6a0f86
yuhcee <egbouchenna001@gmail.com>
2023-05-30 23:46:06 +0100

    :books: (numSubseq) add task description

---
+++ Datastructures_&_Algorithms/Arrays/numSubseq.js
@@ -0,0 +1,14 @@
+/**
+ * **1498. Number of Subsequences That Satisfy the Given Sum Condition**
+ *
+ * You are given an array of integers `nums` and an integer `target`.
+ *
+ * Return *the number of **non-empty** subsequences of `nums` such that the sum of the minimum
+ * and maximum element on it is less or equal to `target`. Since the answer may be too large,
+ * return it **modulo** `109 + 7`.
+ *
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+const numSubseq = function (nums, target) {};
