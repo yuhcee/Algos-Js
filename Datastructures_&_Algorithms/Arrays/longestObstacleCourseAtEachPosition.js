78beecc3e14eeae1d1738548aadb59ac3245a041
yuhcee <egbouchenna001@gmail.com>
2023-05-31 18:21:22 +0100

    :heavy_plus_sign: (longestObstacleCourseAtEachPosition) add binary search func

--- Datastructures_&_Algorithms/Arrays/longestObstacleCourseAtEachPosition.js
+++ Datastructures_&_Algorithms/Arrays/longestObstacleCourseAtEachPosition.js
@@ -28,3 +28,20 @@
  * @return {number[]}
  */
 const longestObstacleCourseAtEachPosition = function (obstacles) {};
+// Binary search to find the correct position for the obstacle height
+function binarySearch(arr, target) {
+    let left = 0;
+    let right = arr.length - 1;
+
+    while (left <= right) {
+        const mid = Math.floor((left + right) / 2);
+
+        if (arr[mid] <= target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+
+    return left;
+}
