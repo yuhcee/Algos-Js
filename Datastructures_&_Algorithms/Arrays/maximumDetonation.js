d4460d07b1faf663df4fdc4256e75233562b59b3
yuhcee <egbouchenna001@gmail.com>
2023-06-03 00:02:14 +0100

    :white_check_mark (maximumDetonation) add testcase 1

--- Datastructures_&_Algorithms/Arrays/maximumDetonation.js
+++ Datastructures_&_Algorithms/Arrays/maximumDetonation.js
@@ -103,3 +103,4 @@ The above figure shows the positions and ranges of the 2 bombs.
 If we detonate the left bomb, the right bomb will not be affected.
 But if we detonate the right bomb, both bombs will be detonated.
 So the maximum bombs that can be detonated is max(1, 2) = 2. */
+console.log(maximumDetonation(bombs));
