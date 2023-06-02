d6d8faa943a1c483921b52f22448136892f20cb2
yuhcee <egbouchenna001@gmail.com>
2023-06-02 23:12:06 +0100

    :books: (maxVowels) add task description

---
+++ Datastructures_&_Algorithms/Arrays/maxVowels.js
@@ -0,0 +1,13 @@
+/**
+ * **1456. Maximum Number of Vowels in a Substring of Given Length**
+ *
+ * Given a string `s` and an integer `k`, return *the maximum number of
+ * vowel letters in any substring of `s` with length `k`.
+ *
+ * Vowel letters in English are `'a'`, `'e'`, `'i'`, `'o'`, and `'u'`.
+ *
+ * @param {string} s
+ * @param {number} k
+ * @return {number}
+ */
+const maxVowels = function (s, k) {};
