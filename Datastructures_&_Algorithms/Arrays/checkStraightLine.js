12cc06e308d7435a327ad434d9596c9dc3808cc8
yuhcee <egbouchenna001@gmail.com>
2023-06-05 07:00:56 +0100

    :heavy_plus_sign: (checkStraightLine) add constraints

--- Datastructures_&_Algorithms/Arrays/checkStraightLine.js
+++ Datastructures_&_Algorithms/Arrays/checkStraightLine.js
@@ -5,6 +5,13 @@
  * represents the coordinate of a point. Check if these points make a straight line in the
  * XY plane.
  *
+ * **Constraints:**
+ *
+ * - `2 <= coordinates.length <= 1000`
+ * - `coordinates[i].length == 2`
+ * - `-10^4 <= coordinates[i][0], coordinates[i][1] <= 10^4`
+ * - `coordinates` contains no duplicate point.
+ *
  * @param {number[][]} coordinates
  * @return {boolean}
  */
