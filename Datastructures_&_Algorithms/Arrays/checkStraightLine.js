c71333bac9b2fa0813648473dc158674451147e5
yuhcee <egbouchenna001@gmail.com>
2023-06-05 06:59:35 +0100

    :books: (checkStraightLine) add task description

---
+++ Datastructures_&_Algorithms/Arrays/checkStraightLine.js
@@ -0,0 +1,11 @@
+/**
+ * **1232. Check If It Is a Straight Line**
+ *
+ * You are given an array `coordinates`, `coordinates[i] = [x, y]`, where `[x, y]`
+ * represents the coordinate of a point. Check if these points make a straight line in the
+ * XY plane.
+ *
+ * @param {number[][]} coordinates
+ * @return {boolean}
+ */
+const checkStraightLine = function (coordinates) {};
