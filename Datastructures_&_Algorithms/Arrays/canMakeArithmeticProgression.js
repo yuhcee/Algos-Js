070edfa2b9faef3228d844f8c8d45c320e763e84
yuhcee <egbouchenna001@gmail.com>
2023-06-06 14:13:38 +0100

    :books: (canMakeArithmeticProgression) add task description

---
+++ Datastructures_&_Algorithms/Arrays/canMakeArithmeticProgression.js
@@ -0,0 +1,13 @@
+/**
+ * **1502. Can Make Arithmetic Progression From Sequence**
+ *
+ * A sequence of numbers is called an **arithmetic progression** if the difference between
+ * any two consecutive elements is the same.
+ *
+ * Given an array of numbers `arr`, return *`true` if the array can be rearranged to form
+ * an **arithmetic progression**. Otherwise, return `false`*.
+ *
+ * @param {number[]} arr
+ * @return {boolean}
+ */
+const canMakeArithmeticProgression = function (arr) {};
