970384d8f8f1f556956760951fafbbfb59311984
yuhcee <egbouchenna001@gmail.com>
2023-06-06 14:20:44 +0100

    :sparkles: (canMakeArithmeticProgression) add task solution

--- Datastructures_&_Algorithms/Arrays/canMakeArithmeticProgression.js
+++ Datastructures_&_Algorithms/Arrays/canMakeArithmeticProgression.js
@@ -15,4 +15,19 @@
  * @param {number[]} arr
  * @return {boolean}
  */
-const canMakeArithmeticProgression = function (arr) {};
+const canMakeArithmeticProgression = function (arr) {
+    // Sort the array in ascending order
+    arr.sort((a, b) => a - b);
+
+    // Calculate the common difference between consecutive elements
+    const diff = arr[1] - arr[0];
+
+    // Check if the difference between any two consecutive elements is different
+    for (let i = 1; i < arr.length - 1; i++) {
+        if (arr[i + 1] - arr[i] !== diff) {
+            return false; // Difference is different, not an arithmetic progression
+        }
+    }
+
+    return true; // All differences are the same, it's an arithmetic progression
+};
