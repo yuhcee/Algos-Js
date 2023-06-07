f8ca29a0cf59067b4296e94426224f7c021ee655
yuhcee <egbouchenna001@gmail.com>
2023-06-07 23:38:53 +0100

    :sparkles: (minFlips) add task solution

--- Datastructures_&_Algorithms/Math/minFlips.js
+++ Datastructures_&_Algorithms/Math/minFlips.js
@@ -19,4 +19,26 @@
  * @param {number} c
  * @return {number}
  */
-const minFlips = function (a, b, c) {};
+const minFlips = function (a, b, c) {
+    let flips = 0;
+
+    for (let i = 0; i < 32; i++) {
+        const bitA = (a >> i) & 1; // Get i-th bit of a
+        const bitB = (b >> i) & 1; // Get i-th bit of b
+        const bitC = (c >> i) & 1; // Get i-th bit of c
+
+        if ((bitA | bitB) !== bitC) {
+            // If (bitA OR bitB) is not equal to bitC, a flip is needed
+
+            if (bitC === 1) {
+                // If bitC is 1, we can flip either bitA or bitB to 1
+                flips++;
+            } else {
+                // If bitC is 0, both bitA and bitB should be flipped to 0
+                flips += bitA + bitB;
+            }
+        }
+    }
+
+    return flips;
+};
