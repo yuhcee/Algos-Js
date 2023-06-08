d42b0774901458585f4b3b34c2af6ff9f0cda515
yuhcee <egbouchenna001@gmail.com>
2023-06-08 19:34:21 +0100

    :books: (countNegatives) add task description

---
+++ Datastructures_&_Algorithms/Arrays/countNegatives.js
@@ -0,0 +1,10 @@
+/**
+ * **1351. Count Negative Numbers in a Sorted Matrix**
+ *
+ * Given a `m x n` matrix `grid` which is sorted in non-increasing order both row-wise and
+ * column-wise, return *the number of **negative** numbers in grid*.
+ *
+ * @param {number[][]} grid
+ * @return {number}
+ */
+const countNegatives = function (grid) {};
