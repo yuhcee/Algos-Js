78f50a6f8e9486d6e926c78d67135207573feb53
yuhcee <egbouchenna001@gmail.com>
2023-06-09 12:30:47 +0100

    :books: (nextGreatestLetter) add task description

---
+++ Datastructures_&_Algorithms/Arrays/nextGreatestLetter.js
@@ -0,0 +1,14 @@
+/**
+ * **744. Find Smallest Letter Greater Than Target**
+ * 
+ * You are given an array of characters `letters` that is sorted in non-decreasing order, 
+ * and a character `target`. There are **at least two different** characters in `letters`.
+ * 
+ * Return the smallest character in `letters` *that is lexicographically greater than 
+ * `target`*. If such a character does not exist, return the first character in `letters`.
+ * 
+ * @param {character[]} letters
+ * @param {character} target
+ * @return {character}
+ */
+const nextGreatestLetter = function (letters, target) {};
