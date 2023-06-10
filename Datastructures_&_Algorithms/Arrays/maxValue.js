ca89b85d68ec4186de964d258f9edb35f5de34bc
yuhcee <egbouchenna001@gmail.com>
2023-06-10 23:53:24 +0100

    :sparkles: (maxValue) add task solution

--- Datastructures_&_Algorithms/Arrays/maxValue.js
+++ Datastructures_&_Algorithms/Arrays/maxValue.js
@@ -14,13 +14,49 @@
  * Note that `abs(x)` equals `x` if `x >= 0`, and `-x` otherwise.
  *
  * **Constraints:**
- * 
+ *
  * - `1 <= n <= maxSum <= 109`
  * - `0 <= index < n`
- * 
+ *
  * @param {number} n
  * @param {number} index
  * @param {number} maxSum
  * @return {number}
  */
-const maxValue = function (n, index, maxSum) {};
+const maxValue = function (n, index, maxSum) {
+    // Initialize the minimum and maximum possible values
+    let min = Math.floor(maxSum / n);
+    let max = maxSum;
+
+    // Helper function to calculate the sum given a number and length
+    const findSum = (num, len) => {
+        if (len < num) {
+            // If the length is smaller than the number, calculate the sum using arithmetic series
+            return (len * (num + num - len + 1)) / 2;
+        }
+        // Calculate the sum using arithmetic series for the first 'num' elements
+        // and add the remaining 'len - num' elements
+        return (num * (num + 1)) / 2 + (len - num);
+    };
+
+    // Helper function to check if a given number is within the constraints
+    const isWithin = (num) => {
+        // Calculate the left sum for the given number
+        const leftSum = findSum(num, index + 1);
+        // Calculate the right sum for the given number
+        const rightSum = findSum(num, n - index);
+        // Check if the total sum is within the maximum sum
+        return maxSum >= leftSum + rightSum - num;
+    };
+
+    // Perform binary search to find the maximum value that satisfies the constraints
+    while (min <= max) {
+        const mid = (min + max) >> 1;
+        // If the current mid value is within the constraints, update the minimum value
+        // Otherwise, update the maximum value
+        isWithin(mid) ? (min = mid + 1) : (max = mid - 1);
+    }
+
+    // Return the maximum value
+    return max;
+};
