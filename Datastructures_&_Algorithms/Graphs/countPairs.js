bec3d7268a961d08f60e46ea70a26e6ef6dc50ac
yuhcee <egbouchenna001@gmail.com>
2023-06-11 23:48:29 +0100

    :books: (countPairs) add task description

---
+++ Datastructures_&_Algorithms/Graphs/countPairs.js
@@ -0,0 +1,14 @@
+/**
+ * **2316. Count Unreachable Pairs of Nodes in an Undirected Graph**
+ *
+ * You are given an integer `n`. There is an **undirected** graph with `n` nodes, numbered from `0` to
+ * `n - 1`. You are given a 2D integer array `edges` where `edges[i] = [ai, bi]` denotes that there
+ * exists an **undirected** edge connecting nodes `ai` and `bi`.
+ *
+ * Return *the **number of pairs** of different nodes that are **unreachable** from each other.
+ *
+ * @param {number} n
+ * @param {number[][]} edges
+ * @return {number}
+ */
+const countPairs = function (n, edges) {};
