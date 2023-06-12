35e48a0f9ab1b3de2da4b06b4835d45c2e1e1a57
yuhcee <egbouchenna001@gmail.com>
2023-06-12 23:54:31 +0100

    :white_check_mark (summaryRanges) add testcase 2

--- Datastructures_&_Algorithms/Arrays/summaryRanges.js
+++ Datastructures_&_Algorithms/Arrays/summaryRanges.js
@@ -53,3 +53,12 @@ const nums = [0, 1, 2, 4, 5, 7];
 // [4,5] --> "4->5"
 // [7,7] --> "7"
 console.log(summaryRanges(nums));
+
+const nums1 = [0, 2, 3, 4, 6, 8, 9];
+// Output: ['0', '2->4', '6', '8->9'];
+/* Explanation: The ranges are:
+[0,0] --> "0"
+[2,4] --> "2->4"
+[6,6] --> "6"
+[8,9] --> "8->9" */
+console.log(summaryRanges(nums1));
