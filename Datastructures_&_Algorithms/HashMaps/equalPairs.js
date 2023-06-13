42dc9d8863ba3dc0881178ab3c1a283a79e7ef23
yuhcee <egbouchenna001@gmail.com>
2023-06-13 12:08:09 +0100

    :white_check_mark (equalPairs) add testcase 1

--- Datastructures_&_Algorithms/HashMaps/equalPairs.js
+++ Datastructures_&_Algorithms/HashMaps/equalPairs.js
@@ -50,3 +50,13 @@ const equalPairs = function (grid) {
 
     return count;
 };
+
+const grid = [
+    [3, 2, 1],
+    [1, 7, 6],
+    [2, 7, 7],
+];
+// Output: 1
+/* Explanation: There is 1 equal row and column pair:
+- (Row 2, Column 1): [2,7,7] */
+console.log(equalPairs(grid));
