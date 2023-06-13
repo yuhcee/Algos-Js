831bd9c435d26b152de7e9ed50c27f2de8e97927
yuhcee <egbouchenna001@gmail.com>
2023-06-13 12:05:14 +0100

    :books: (equalPairs) add task description

---
+++ Datastructures_&_Algorithms/HashMaps/equalPairs.js
@@ -0,0 +1,13 @@
+/**
+ * **2352. Equal Row and Column Pairs**
+ *
+ * Given a **0-indexed** `n x n` integer matrix `grid`, *return the number of pairs `(ri, cj)`
+ * such that row `ri` and column `cj` are equal*.
+ *
+ * A row and column pair is considered equal if they contain the same elements in the same
+ * order (i.e., an equal array).
+ *
+ * @param {number[][]} grid
+ * @return {number}
+ */
+const equalPairs = function (grid) {};
