7c1297440a7427352b231275c8accf4cf3cdb1fe
yuhcee <egbouchenna001@gmail.com>
2023-06-15 20:30:27 +0100

    :books: (findDifference) add task description

---
+++ Datastructures_&_Algorithms/HashMaps/findDifference.js
@@ -0,0 +1,17 @@
+/**
+ * **2215. Find the Difference of Two Arrays**
+ *
+ * Given two **0-indexed** integer arrays `nums1` and `nums2`, return *a list answer of size 2 where*:
+ *
+ * - `answer[0]`* is a list of all **distinct** integers in `nums1` which are **not** present in
+ * `nums2`*.
+ * - `answer[1]` is a list of all **distinct** integers in `nums2` which are **not** present in
+ * `nums1`.
+ *
+ * **Note** that the integers in the lists may be returned in **any** order.
+ *
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[][]}
+ */
+const findDifference = function (nums1, nums2) {};
