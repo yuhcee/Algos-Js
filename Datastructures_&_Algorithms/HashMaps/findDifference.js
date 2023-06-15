f7f9b221fee79b8231533a292da434a873a9ab6c
yuhcee <egbouchenna001@gmail.com>
2023-06-15 20:32:39 +0100

    :white_check_mark (findDifference) add testcase 1

--- Datastructures_&_Algorithms/HashMaps/findDifference.js
+++ Datastructures_&_Algorithms/HashMaps/findDifference.js
@@ -40,3 +40,11 @@ const findDifference = function (nums1, nums2) {
 
     return answer; // Return the answer array
 };
+
+const nums1 = [1, 2, 3],
+    nums2 = [2, 4, 6];
+// Output: [[1,3],[4,6]]
+/* Explanation:
+For nums1, nums1[1] = 2 is present at index 0 of nums2, whereas nums1[0] = 1 and nums1[2] = 3 are not present in nums2. Therefore, answer[0] = [1,3].
+For nums2, nums2[0] = 2 is present at index 1 of nums1, whereas nums2[1] = 4 and nums2[2] = 6 are not present in nums2. Therefore, answer[1] = [4,6]. */
+console.log(findDifference(nums1, nums2));
