3ae8d8745dbe9b4ac75439479e246425fa65138e
yuhcee <egbouchenna001@gmail.com>
2023-06-17 23:43:09 +0100

    :white_check_mark (average) add testcase 1

--- Datastructures_&_Algorithms/Arrays/average.js
+++ Datastructures_&_Algorithms/Arrays/average.js
@@ -32,3 +32,9 @@ const average = function (salary) {
 
     return avg;
 };
+
+const salary = [4000, 3000, 1000, 2000];
+// Output: 2500.00000
+/* Explanation: Minimum salary and maximum salary are 1000 and 4000 respectively.
+Average salary excluding minimum and maximum salary is (2000+3000) / 2 = 2500 */
+console.log(average(salary));
