fb3cade6a91774dc38dc61d6ff4b436c86458683
yuhcee <egbouchenna001@gmail.com>
2023-06-18 23:31:32 +0100

    :books: (countPaths) add task description

---
+++ Datastructures_&_Algorithms/Dynamic Programming/countPaths.js
@@ -0,0 +1,16 @@
+/**
+ * **2328. Number of Increasing Paths in a Grid**
+ *
+ * You are given an `m x n` integer matrix `grid`, where you can move from a cell to any adjacent cell
+ * in all `4` directions.
+ *
+ * Return *the number of **strictly increasing** paths in the grid such that you can start from
+ * **any** cell and end at **any** cell*. Since the answer may be very large, return it **modulo**
+ * `109 + 7`.
+ *
+ * Two paths are considered different if they do not have exactly the same sequence of visited cells.
+ *
+ * @param {number[][]} grid
+ * @return {number}
+ */
+const countPaths = (grid) => {};
