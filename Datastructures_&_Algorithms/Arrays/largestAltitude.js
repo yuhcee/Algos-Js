4927f0bcd01701e57ae4bbda9c79f693f13a08d0
yuhcee <egbouchenna001@gmail.com>
2023-06-19 11:33:37 +0100

    :white_check_mark (largestAltitude) add testcase 2

--- Datastructures_&_Algorithms/Arrays/largestAltitude.js
+++ Datastructures_&_Algorithms/Arrays/largestAltitude.js
@@ -38,3 +38,8 @@ const gain = [-5, 1, 5, 0, -7];
 // Output: 1
 // Explanation: The altitudes are [0,-5,-4,1,1,-6]. The highest is 1.
 console.log(largestAltitude(gain));
+
+const gain1 = [-4, -3, -2, -1, 4, 3, 2];
+// Output: 0
+// Explanation: The altitudes are [0,-4,-7,-9,-10,-6,-3,-1]. The highest is 0.
+console.log(largestAltitude(gain1));
