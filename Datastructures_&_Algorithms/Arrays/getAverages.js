0a1ffe42be4a6910f7fe55a38167b25b9107ea5e
yuhcee <egbouchenna001@gmail.com>
2023-06-20 23:55:48 +0100

    :white_check_mark (getAverages) add testcase 1

--- Datastructures_&_Algorithms/Arrays/getAverages.js
+++ Datastructures_&_Algorithms/Arrays/getAverages.js
@@ -54,3 +54,15 @@ const getAverages = function (nums, k) {
 
     return avgs;
 };
+
+const nums = [7, 4, 3, 9, 1, 8, 5, 2, 6],
+    k = 3;
+// Output: [-1,-1,-1,5,4,4,-1,-1,-1]
+/* Explanation:
+- avg[0], avg[1], and avg[2] are -1 because there are less than k elements before each index.
+- The sum of the subarray centered at index 3 with radius 3 is: 7 + 4 + 3 + 9 + 1 + 8 + 5 = 37.
+  Using integer division, avg[3] = 37 / 7 = 5.
+- For the subarray centered at index 4, avg[4] = (4 + 3 + 9 + 1 + 8 + 5 + 2) / 7 = 4.
+- For the subarray centered at index 5, avg[5] = (3 + 9 + 1 + 8 + 5 + 2 + 6) / 7 = 4.
+- avg[6], avg[7], and avg[8] are -1 because there are less than k elements after each index. */
+console.log(getAverages(nums, k));
