0be86ed6f979a185e89ccb39c857e3a723120347
yuhcee <egbouchenna001@gmail.com>
2023-06-22 23:56:14 +0100

    :white_check_mark (maxProfit) add testcase 1

--- Datastructures_&_Algorithms/Dynamic Programming/maxProfit.js
+++ Datastructures_&_Algorithms/Dynamic Programming/maxProfit.js
@@ -35,3 +35,14 @@ const maxProfit = function (prices, fee) {
 
     return cash; // return the maximum profit without owning any stock
 };
+
+const prices = [1, 3, 2, 8, 4, 9],
+    fee = 2;
+// Output: 8
+/* Explanation: The maximum profit can be achieved by:
+- Buying at prices[0] = 1
+- Selling at prices[3] = 8
+- Buying at prices[4] = 4
+- Selling at prices[5] = 9
+The total profit is ((8 - 1) - 2) + ((9 - 4) - 2) = 8. */
+console.log(maxProfit(prices, fee));
