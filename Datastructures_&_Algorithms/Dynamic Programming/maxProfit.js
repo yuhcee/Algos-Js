3a80c5c56b51d84b93ae3c6ac5aa5fdaecb4b312
yuhcee <egbouchenna001@gmail.com>
2023-06-22 23:58:11 +0100

    :books: (maxProfit) modify task description

--- Datastructures_&_Algorithms/Dynamic Programming/maxProfit.js
+++ Datastructures_&_Algorithms/Dynamic Programming/maxProfit.js
@@ -1,13 +1,13 @@
 /**
  * **714. Best Time to Buy and Sell Stock with Transaction Fee**
  *
- * You are given an array prices where prices[i] is the price of a given stock on the ith day,
- * and an integer fee representing a transaction fee.
+ * You are given an array `prices` where `prices[i]` is the price of a given stock on the `ith`
+ * day, and an integer fee representing a transaction fee.
  *
  * Find the maximum profit you can achieve. You may complete as many transactions as you like,
  * but you need to pay the transaction fee for each transaction.
  *
- * Note: You may not engage in multiple transactions simultaneously (i.e., you must sell the
+ * **Note**: You may not engage in multiple transactions simultaneously (i.e., you must sell the
  * stock before you buy again).
  *
  * **Constraints:**
