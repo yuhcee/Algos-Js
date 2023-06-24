88a1149b6b324da420c39a48be28be7825efcae4
yuhcee <egbouchenna001@gmail.com>
2023-06-24 23:16:22 +0100

    :books: (tallestBillboard) add task description

---
+++ Datastructures_&_Algorithms/Dynamic Programming/tallestBillboard.js
@@ -0,0 +1,17 @@
+/**
+ *
+ * **956. Tallest Billboard**
+ *
+ * You are installing a billboard and want it to have the largest height. The billboard will have two
+ * steel supports, one on each side. Each steel support must be an equal height.
+ *
+ * You are given a collection of `rods` that can be welded together. For example, if you have rods of
+ * lengths `1`, `2`, and `3`, you can weld them together to make a support of length `6`.
+ *
+ * Return *the largest possible height of your billboard installation. If you cannot support the
+ * billboard, return `0`*.
+ *
+ * @param {number[]} rods
+ * @return {number}
+ */
+const tallestBillboard = function (rods) {};
