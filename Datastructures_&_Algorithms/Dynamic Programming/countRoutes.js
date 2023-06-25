103e918c4b89da9602b7cffb737dacf6c603e175
yuhcee <egbouchenna001@gmail.com>
2023-06-25 07:11:41 +0100

    :books: (countRoutes) add task description

---
+++ Datastructures_&_Algorithms/Dynamic Programming/countRoutes.js
@@ -0,0 +1,25 @@
+/**
+ * **1575. Count All Possible Routes**
+ *
+ * You are given an array of **distinct** positive integers locations where `locations[i]` represents
+ * the position of city `i`. You are also given integers `start`, `finish` and `fuel` representing the
+ * starting city, ending city, and the initial amount of fuel you have, respectively.
+ *
+ * At each step, if you are at city `i`, you can pick any city `j` such that `j != i` and `0 <= j <
+ * locations.length` and move to city `j`. Moving from city `i` to city `j` reduces the amount of fuel
+ * you have by `|locations[i] - locations[j]|`. Please notice that `|x|` denotes the absolute value of
+ * `x`.
+ *
+ * Notice that `fuel` **cannot** become negative at any point in time, and that you are **allowed** to
+ * visit any city more than once (including `start` and `finish`).
+ *
+ * Return *the count of all possible routes from `start` to `finish`. Since the answer may be too
+ * large, return it **modulo** `109 + 7`.
+ *
+ * @param {number[]} locations
+ * @param {number} start
+ * @param {number} finish
+ * @param {number} fuel
+ * @return {number}
+ */
+var countRoutes = function (locations, start, finish, fuel) {};
