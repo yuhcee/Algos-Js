d6017d5b5acf40c6253e8134f04465734e71da61
yuhcee <egbouchenna001@gmail.com>
2023-06-28 23:49:54 +0100

    :books: (maxProbability) add task description

---
+++ Datastructures_&_Algorithms/Graphs/maxProbability.js
@@ -0,0 +1,21 @@
+/**
+ * **1514. Path with Maximum Probability**
+ * 
+ * You are given an undirected weighted graph of `n` nodes (0-indexed), represented by an edge list 
+ * where `edges[i] = [a, b]` is an undirected edge connecting the nodes `a` and `b` with a probability 
+ * of success of traversing that edge `succProb[i]`.
+ * 
+ * Given two nodes `start` and `end`, find the path with the maximum probability of success to go from 
+ * `start` to `end` and return its success probability.
+ * 
+ * If there is no path from `start` to `end`, **return 0**. Your answer will be accepted if it differs 
+ * from the correct answer by at most **1e-5**.
+ * 
+ * @param {number} n
+ * @param {number[][]} edges
+ * @param {number[]} succProb
+ * @param {number} start
+ * @param {number} end
+ * @return {number}
+ */
+const maxProbability = function (n, edges, succProb, start, end) {};
