12aba73d29d9d64145691364ffa27cd2da803762
yuhcee <egbouchenna001@gmail.com>
2023-06-30 00:10:04 +0100

    :books: (canPlaceFlowers) add task description

---
+++ Datastructures_&_Algorithms/Arrays/canPlaceFlowers.js
@@ -0,0 +1,15 @@
+/**
+ * **605. Can Place Flowers**
+ *
+ * You have a long flowerbed in which some of the plots are planted, and some are not. However,
+ * flowers cannot be planted in **adjacent** plots.
+ *
+ * Given an integer array `flowerbed` containing `0`'s and `1`'s, where `0` means empty and `1` means
+ * not empty, and an integer `n`, return *`true` if `n` new flowers can be planted in the `flowerbed`
+ * without violating the no-adjacent-flowers rule and `false` otherwise*.
+ *
+ * @param {number[]} flowerbed
+ * @param {number} n
+ * @return {boolean}
+ */
+const canPlaceFlowers = function (flowerbed, n) {};
