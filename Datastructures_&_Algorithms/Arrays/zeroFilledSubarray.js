a00d8e15158d2ee90a6446407a1670f2b23f2e8f
yuhcee <egbouchenna001@gmail.com>
2023-06-29 23:28:01 +0100

    :books: (zeroFilledSubarray) add task description

---
+++ Datastructures_&_Algorithms/Arrays/zeroFilledSubarray.js
@@ -0,0 +1,11 @@
+/**
+ * **2348. Number of Zero-Filled Subarrays**
+ *
+ * Given an integer array `nums`, return *the number of **subarrays** filled with `0`*.
+ *
+ * A **subarray** is a contiguous non-empty sequence of elements within an array.
+ *
+ * @param {number[]} nums
+ * @return {number}
+ */
+const zeroFilledSubarray = function (nums) {};
