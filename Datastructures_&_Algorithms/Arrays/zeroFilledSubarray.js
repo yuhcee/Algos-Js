ceb712c9f09d26ed6c1256451c071c30d4329008
yuhcee <egbouchenna001@gmail.com>
2023-06-29 23:29:27 +0100

    :sparkles: (zeroFilledSubarray) add task solution

--- Datastructures_&_Algorithms/Arrays/zeroFilledSubarray.js
+++ Datastructures_&_Algorithms/Arrays/zeroFilledSubarray.js
@@ -13,4 +13,34 @@
  * @param {number[]} nums
  * @return {number}
  */
-const zeroFilledSubarray = function (nums) {};
+const zeroFilledSubarray = function (nums) {
+    // Helper function to calculate the sum of numbers from 1 to n
+    function getOutcome(n) {
+        return (n * (n + 1)) / 2;
+    }
+
+    let result = 0;
+    let count = 0;
+
+    // Iterate through the elements of the array
+    for (const val of nums.values()) {
+        if (val === 0) {
+            // If the current element is 0, increment the count
+            count++;
+        } else {
+            // If the current element is not 0, calculate the outcome (number of subarrays filled with 0) for the previous count
+            const outcome = getOutcome(count);
+            result += outcome;
+            count = 0;
+        }
+    }
+
+    // Calculate the outcome for the remaining count (if any)
+    if (count) {
+        const outcome = getOutcome(count);
+        result += outcome;
+        count = 0;
+    }
+
+    return result;
+};
