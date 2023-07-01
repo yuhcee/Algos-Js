1fbc8982a521bb48f65bd7176b498a75f3dd92d1
yuhcee <egbouchenna001@gmail.com>
2023-07-01 22:31:55 +0100

    :books: (distributeCookies) add task description

---
+++ Datastructures_&_Algorithms/Dynamic Programming/distributeCookies.js
@@ -0,0 +1,18 @@
+/**
+ * **2305. Fair Distribution of Cookies**
+ *
+ * You are given an integer array `cookies`, where `cookies[i]` denotes the number of cookies in the
+ * `ith` bag. You are also given an integer `k` that denotes the number of children to distribute all
+ * the bags of cookies to. All the cookies in the same bag must go to the same child and cannot be
+ * split up.
+ *
+ * The **unfairness** of a distribution is defined as the **maximum total** cookies obtained by a
+ * single child in the distribution.
+ *
+ * Return *the **minimum** unfairness of all distributions*.
+ *
+ * @param {number[]} cookies
+ * @param {number} k
+ * @return {number}
+ */
+const distributeCookies = function (cookies, k) {};
