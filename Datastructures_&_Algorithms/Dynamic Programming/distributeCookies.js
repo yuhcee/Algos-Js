1919afe02d1d6838ecbb97dcb00e99042a5baff9
yuhcee <egbouchenna001@gmail.com>
2023-07-01 22:33:49 +0100

    :sparkles: (distributeCookies) add task solution

--- Datastructures_&_Algorithms/Dynamic Programming/distributeCookies.js
+++ Datastructures_&_Algorithms/Dynamic Programming/distributeCookies.js
@@ -21,4 +21,30 @@
  * @param {number} k
  * @return {number}
  */
-const distributeCookies = function (cookies, k) {};
+const distributeCookies = function (cookies, k) {
+    let ans = Infinity; // Initialize the minimum unfairness to infinity
+    let bag = Array(k).fill(0); // Create an array to represent the bags of cookies for each child
+
+    // Backtracking function to distribute cookies
+    function backtrack(i) {
+        if (i >= cookies.length) {
+            let max = -Infinity; // Track the maximum number of cookies received by a child
+            for (let b of bag) {
+                max = Math.max(max, b); // Update the maximum number of cookies
+            }
+            ans = Math.min(ans, max); // Update the minimum unfairness
+            return;
+        }
+
+        // Distribute the current cookie to each child and recurse
+        for (let j = 0; j < k; j++) {
+            bag[j] += cookies[i]; // Add the current cookie to the j-th bag
+            backtrack(i + 1); // Recurse to the next cookie
+            bag[j] -= cookies[i]; // Undo the distribution for backtracking
+        }
+    }
+
+    backtrack(0); // Start distributing cookies from the first one
+
+    return ans; // Return the minimum unfairness
+};
