208f531235212004deebb6b7dcce26ab17e8164c
yuhcee <egbouchenna001@gmail.com>
2023-07-02 23:58:43 +0100

    :sparkles: (maxProfit) add task solution

--- Datastructures_&_Algorithms/Dynamic Programming/maxProfit1.js
+++ Datastructures_&_Algorithms/Dynamic Programming/maxProfit1.js
@@ -10,11 +10,26 @@
  * profit, return `0`.
  *
  * **Constraints:**
- * 
+ *
  * - `1 <= prices.length <= 105`
  * - `0 <= prices[i] <= 104`
- * 
+ *
  * @param {number[]} prices
  * @return {number}
  */
-const maxProfit = function (prices) {};
+const maxProfit = function (prices) {
+    let minPrice = Infinity; // Initialize the minimum price to a very large value
+    let maxProfit = 0; // Initialize the maximum profit to 0
+
+    for (let i = 0; i < prices.length; i++) {
+        if (prices[i] < minPrice) {
+            // If the current price is lower than the minimum price seen so far, update the minimum price
+            minPrice = prices[i];
+        } else if (prices[i] - minPrice > maxProfit) {
+            // If the difference between the current price and the minimum price is greater than the maximum profit, update the maximum profit
+            maxProfit = prices[i] - minPrice;
+        }
+    }
+
+    return maxProfit;
+};
