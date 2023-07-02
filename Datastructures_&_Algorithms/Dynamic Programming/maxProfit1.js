cc32f40770e9388a106f872794c280778a3cde84
yuhcee <egbouchenna001@gmail.com>
2023-07-02 23:58:21 +0100

    :heavy_plus_sign: (maxProfit) add constraints

--- Datastructures_&_Algorithms/Dynamic Programming/maxProfit1.js
+++ Datastructures_&_Algorithms/Dynamic Programming/maxProfit1.js
@@ -9,6 +9,11 @@
  * Return *the maximum profit you can achieve from this transaction*. If you cannot achieve any
  * profit, return `0`.
  *
+ * **Constraints:**
+ * 
+ * - `1 <= prices.length <= 105`
+ * - `0 <= prices[i] <= 104`
+ * 
  * @param {number[]} prices
  * @return {number}
  */
