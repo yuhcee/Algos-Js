936a3509dd068a643c7d25cc6451b624717da37a
yuhcee <egbouchenna001@gmail.com>
2023-07-03 00:00:13 +0100

    :white_check_mark:(maxProfit) add testcase 2

--- Datastructures_&_Algorithms/Dynamic Programming/maxProfit1.js
+++ Datastructures_&_Algorithms/Dynamic Programming/maxProfit1.js
@@ -39,3 +39,8 @@ const prices = [7, 1, 5, 3, 6, 4];
 /* Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
 Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell. */
 console.log(maxProfit(prices)); // Output: 5
+
+const prices1 = [7, 6, 4, 3, 1];
+// Output: 0
+// Explanation: In this case, no transactions are done and the max profit = 0.
+console.log(maxProfit(prices1)); // Output: 5
