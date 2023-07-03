3b84b88a18c4103602ef43888afabc0c976d9fc6
yuhcee <egbouchenna001@gmail.com>
2023-07-03 23:22:29 +0100

    :books: (buddyStrings) add task description

---
+++ Datastructures_&_Algorithms/Strings/buddyStrings.js
@@ -0,0 +1,16 @@
+/**
+ * **859. Buddy Strings**
+ *
+ * Given two strings `s` and `goal`, return *`true` if you can swap two letters in `s` so the result
+ * is equal to `goal`, otherwise, return `false`*.
+ *
+ * Swapping letters is defined as taking two indices `i` and `j` (0-indexed) such that `i != j` and
+ * swapping the characters at `s[i]` and `s[j]`.
+ *
+ * For example, swapping at indices `0` and `2` in "`abcd"` results in `"cbad"`.
+ *
+ * @param {string} s
+ * @param {string} goal
+ * @return {boolean}
+ */
+const buddyStrings = function (s, goal) {};
