351a180d9700e15de75f3d58710d6d4c5ce26f42
yuhcee <egbouchenna001@gmail.com>
2023-07-04 10:28:40 +0100

    :books: (singleNumber) add task description

---
+++ Datastructures_&_Algorithms/Arrays/singleNumber.js
@@ -0,0 +1,13 @@
+/**
+ * **137. Single Number II**
+ *
+ * Given an integer array `nums` where every element appears **three times** except for one,
+ * which appears **exactly once**. *Find the single element and return it*.
+ *
+ * You must implement a solution with a linear runtime complexity and use only constant extra
+ * space.
+ *
+ * @param {number[]} nums
+ * @return {number}
+ */
+const singleNumber = function (nums) {};
