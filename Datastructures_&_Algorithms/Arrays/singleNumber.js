f02436ff79a19285766985006c079b1d51919c17
yuhcee <egbouchenna001@gmail.com>
2023-07-04 10:30:01 +0100

    :heavy_plus_sign: (singleNumber) add constraints

--- Datastructures_&_Algorithms/Arrays/singleNumber.js
+++ Datastructures_&_Algorithms/Arrays/singleNumber.js
@@ -7,6 +7,13 @@
  * You must implement a solution with a linear runtime complexity and use only constant extra
  * space.
  *
+ * **Constraints:**
+ *
+ * - `1 <= nums.length <= 3 * 104`
+ * - `-231 <= nums[i] <= 231 - 1`
+ * - Each element in `nums` appears exactly **three times** except for one element which
+ * appears **once**.
+ *
  * @param {number[]} nums
  * @return {number}
  */
