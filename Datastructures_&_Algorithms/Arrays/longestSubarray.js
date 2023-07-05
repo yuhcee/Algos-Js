0718c6d638db23d23d229585e13a0c90d64b3612
yuhcee <egbouchenna001@gmail.com>
2023-07-05 18:15:00 +0100

    :books: (longestSubarray) add task description

---
+++ Datastructures_&_Algorithms/Arrays/longestSubarray.js
@@ -0,0 +1,12 @@
+/**
+ * **1493. Longest Subarray of 1's After Deleting One Element**
+ *
+ * Given a binary array `nums`, you should delete one element from it.
+ *
+ * Return *the size of the longest non-empty subarray containing only `1`'s in the resulting
+ * array*. Return `0` if there is no such subarray.
+ *
+ * @param {number[]} nums
+ * @return {number}
+ */
+const longestSubarray = function (nums) {};
