a48739075403e9674f231029b04f96d062cb4134
yuhcee <egbouchenna001@gmail.com>
2023-07-05 18:15:58 +0100

    :heavy_plus_sign: (longestSubarray) add constraints

--- Datastructures_&_Algorithms/Arrays/longestSubarray.js
+++ Datastructures_&_Algorithms/Arrays/longestSubarray.js
@@ -6,6 +6,11 @@
  * Return *the size of the longest non-empty subarray containing only `1`'s in the resulting
  * array*. Return `0` if there is no such subarray.
  *
+ * **Constraints:**
+ *
+ * - `1 <= nums.length <= 105`
+ * - `nums[i]` is either `0` or `1`.
+ *
  * @param {number[]} nums
  * @return {number}
  */
