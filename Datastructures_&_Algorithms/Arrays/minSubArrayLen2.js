d8417a97a5e4c4e6fad105dc18fdbdf5b379f168
yuhcee <egbouchenna001@gmail.com>
2023-07-06 15:59:42 +0100

    :books: (minSubArrayLen) add task description

---
+++ Datastructures_&_Algorithms/Arrays/minSubArrayLen2.js
@@ -0,0 +1,13 @@
+/**
+ * **209. Minimum Size Subarray Sum**
+ *
+ * Given an array of positive integers `nums` and a positive integer `target`, return *the
+ * **minimal length** of a subarray whose sum is greater than or equal to `target`*.
+ *
+ * If there is no such subarray, return `0` instead.
+ *
+ * @param {number} target
+ * @param {number[]} nums
+ * @return {number}
+ */
+const minSubArrayLen = function (target, nums) {};
