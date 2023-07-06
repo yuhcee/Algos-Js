04923fc91f93ae225d7999d2b1c4b09e6e680725
yuhcee <egbouchenna001@gmail.com>
2023-07-06 16:01:03 +0100

    :sparkles: (minSubArrayLen) add task solution

--- Datastructures_&_Algorithms/Arrays/minSubArrayLen2.js
+++ Datastructures_&_Algorithms/Arrays/minSubArrayLen2.js
@@ -16,4 +16,23 @@
  * @param {number[]} nums
  * @return {number}
  */
-const minSubArrayLen = function (target, nums) {};
+const minSubArrayLen = function (target, nums) {
+    let minLength = Infinity; // Variable to store the minimum length of the subarray
+    let windowSum = 0; // Variable to track the sum of the current window
+    let windowStart = 0; // Variable to track the start index of the current window
+
+    // Iterate over the array
+    for (let i = 0; i < nums.length; i++) {
+        windowSum += nums[i]; // Add the current element to the window sum
+
+        // Check if the window sum is greater than or equal to the target
+        while (windowSum >= target) {
+            const windowLength = i - windowStart + 1; // Calculate the length of the current window
+            minLength = Math.min(minLength, windowLength); // Update the minLength if necessary
+            windowSum -= nums[windowStart]; // Remove the leftmost element from the window sum
+            windowStart++; // Move the window's start index to the right
+        }
+    }
+
+    return minLength === Infinity ? 0 : minLength; // Return the minLength, or 0 if no subarray is found
+};
