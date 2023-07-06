04959a7d514b3d8e8a17457c2f247ebfa2065476
yuhcee <egbouchenna001@gmail.com>
2023-07-06 16:00:32 +0100

    :heavy_plus_sign: (minSubArrayLen) add constraints

--- Datastructures_&_Algorithms/Arrays/minSubArrayLen2.js
+++ Datastructures_&_Algorithms/Arrays/minSubArrayLen2.js
@@ -6,6 +6,12 @@
  *
  * If there is no such subarray, return `0` instead.
  *
+ * **Constraints:**
+ *
+ * - `1 <= target <= 109`
+ * - `1 <= nums.length <= 105`
+ * - `1 <= nums[i] <= 104`
+ *
  * @param {number} target
  * @param {number[]} nums
  * @return {number}
