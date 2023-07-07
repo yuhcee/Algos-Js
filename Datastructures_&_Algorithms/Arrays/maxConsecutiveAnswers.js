5bc9d8b873a6bf683f185754b08063af9211c572
yuhcee <egbouchenna001@gmail.com>
2023-07-07 23:57:42 +0100

    :books: (maxConsecutiveAnswers) add task description

---
+++ Datastructures_&_Algorithms/Arrays/maxConsecutiveAnswers.js
@@ -0,0 +1,21 @@
+/**
+ * **2024. Maximize the Confusion of an Exam**
+ *
+ * A teacher is writing a test with `n` true/false questions, with `'T'` denoting true and `'F'` denoting
+ * false. He wants to confuse the students by **maximizing** the number of **consecutive** questions with
+ * the **same** answer (multiple trues or multiple falses in a row).
+ *
+ * You are given a string `answerKey`, where `answerKey[i]` is the original answer to the `ith` question.
+ * In addition, you are given an integer `k`, the maximum number of times you may perform the following
+ * operation:
+ *
+ * - Change the answer key for any question to `'T'` or `'F'` (i.e., set answerKey[i] to `'T'` or `'F'`).
+ *
+ * Return *the maximum number of consecutive `'T'`s or 'F's in the answer key after performing the
+ * operation at most `k` times*.
+ *
+ * @param {string} answerKey
+ * @param {number} k
+ * @return {number}
+ */
+function maxConsecutiveAnswers(answerKey, k) {}
