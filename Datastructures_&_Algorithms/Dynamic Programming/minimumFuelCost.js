9ef7e0a3523a6844237aaebbca11c33f6fb19979
yuhcee <egbouchenna001@gmail.com>
2023-07-13 23:45:49 +0100

    :books: (minimumFuelCost) add task description

---
+++ Datastructures_&_Algorithms/Dynamic Programming/minimumFuelCost.js
@@ -0,0 +1,23 @@
+/**
+ * **2477. Minimum Fuel Cost to Report to the Capital**
+ *
+ * There is a tree (i.e., a connected, undirected graph with no cycles) structure country network
+ * consisting of `n` cities numbered from `0` to `n - 1` and exactly `n - 1` roads. The capital
+ * city is city `0`. You are given a 2D integer array roads where `roads[i] = [ai, bi]` denotes
+ * that there exists a **bidirectional road** connecting cities `ai` and `bi`.
+ *
+ * There is a meeting for the representatives of each city. The meeting is in the capital city.
+ *
+ * There is a car in each city. You are given an integer `seats` that indicates the number of
+ * seats in each car.
+ *
+ * A representative can use the car in their city to travel or change the car and ride with
+ * another representative. The cost of traveling between two cities is one liter of fuel.
+ *
+ * Return *the minimum number of liters of fuel to reach the capital city*.
+ *
+ * @param {number[][]} roads - a 2D array that represents the roads in the network, each element of the array is a road represented as [ai, bi], indicating a bidirectional road between cities ai and bi
+ * @param {number} seats - an integer that represents the number of seats in each car
+ * @return {number} - returns the minimum number of liters of fuel required to reach the capital city
+ */
+const minimumFuelCost = function (roads, seats) {};
