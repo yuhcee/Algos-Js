928c2689ed5fcfb3cc2646baec255cc5f305b32f
yuhcee <egbouchenna001@gmail.com>
2023-07-13 23:31:45 +0100

    :heavy_plus_sign: (canFinish) add constraints

--- Datastructures_&_Algorithms/Graphs/courseSchedule1.js
+++ Datastructures_&_Algorithms/Graphs/courseSchedule1.js
@@ -6,47 +6,64 @@
  *
  * Return `true` if you can finish all courses. Otherwise, return `false`.
  *
- * @param {number} numCourses
- * @param {number[][]} prerequisites
- * @return {boolean}
+ * @param {number} numCourses - The total number of courses
+ * @param {number[][]} prerequisites - The prerequisites for the courses
+ * @return {boolean} - Whether it is possible to finish all courses
  */
-const canFinish = function (numCourses, prerequisites) {
+var canFinish = function (numCourses, prerequisites) {
+    // Build the graph
     const graph = buildGraph(numCourses, prerequisites);
-    const depart = new Array(numCourses).fill(0);
+    // Set to keep track of visited nodes
     const visited = new Set();
+    // Array to track departure time of each node during DFS traversal
+    const depart = new Array(numCourses).fill(0);
 
+    // Perform DFS on each node
     for (let node in graph) {
-        if (!visited.has(String(node))) {
-            visited.add(String(node));
+        // If the node has not been visited yet
+        if (!visited.has(node)) {
+            visited.add(node);
+            // Check if there is a path from this node to any other node
             if (hasPath(graph, node, visited, depart)) return false;
         }
     }
+
+    // All nodes have been visited and there is no cycle
     return true;
 };
 
+// DFS function to check if there is a path from the current node to any other node
 const hasPath = (graph, node, visited, depart) => {
+    // Iterate over each neighbor of the current node
     for (let neighbor of graph[node]) {
-        if (!visited.has(String(neighbor))) {
-            visited.add(String(neighbor));
+        // If the neighbor has not been visited yet
+        if (!visited.has(neighbor)) {
+            visited.add(neighbor);
+            // Check if there is a path from this neighbor to any other node
             if (hasPath(graph, neighbor, visited, depart)) return true;
         }
+        // If the neighbor has already been visited and its departure time is 0
         if (depart[neighbor] === 0) return true;
     }
+
+    // Update the departure time of the current node
     depart[node]++;
     return false;
 };
 
+// Function to build the graph
 const buildGraph = (n, edges) => {
     const graph = {};
 
+    // Initialize an empty array for each node in the graph
     for (let i = 0; i < n; i++) {
         graph[i] = [];
     }
 
+    // Add edges to the graph
     for (let edge of edges) {
-        const [src, dest] = edge;
-
-        graph[src].push(dest);
+        const [a, b] = edge;
+        graph[a].push(b);
     }
 
     return graph;
@@ -63,16 +80,15 @@ const numCourses = 20,
         [15, 1],
         [17, 4],
     ]; // false
+console.log(canFinish(numCourses, prerequisites));
 
 const numCourses1 = 2,
     prerequisites1 = [
         [1, 0],
         [0, 1],
     ]; // false
+console.log(canFinish(numCourses1, prerequisites1));
 
 const numCourses2 = 2,
     prerequisites2 = [[1, 0]]; // true
-
-console.log(canFinish(numCourses, prerequisites));
-console.log(canFinish(numCourses1, prerequisites1));
 console.log(canFinish(numCourses2, prerequisites2));
