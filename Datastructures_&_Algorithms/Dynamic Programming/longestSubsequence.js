81f5f3984bdc3fd47660dc44d1f524a39bb299ee
yuhcee <egbouchenna001@gmail.com>
2023-07-14 23:55:24 +0100

    :heavy_plus_sign: (longestSubsequence) add constraints

--- Datastructures_&_Algorithms/Dynamic Programming/longestSubsequence.js
+++ Datastructures_&_Algorithms/Dynamic Programming/longestSubsequence.js
@@ -1,15 +1,20 @@
 /**
  * **1218. Longest Arithmetic Subsequence of Given Difference**
- * 
- * Given an integer array `arr` and an integer `difference`, return the length of the longest 
- * subsequence in `arr` which is an arithmetic sequence such that the difference between adjacent 
+ *
+ * Given an integer array `arr` and an integer `difference`, return the length of the longest
+ * subsequence in `arr` which is an arithmetic sequence such that the difference between adjacent
  * elements in the subsequence equals `difference`.
- * 
- * A **subsequence** is a sequence that can be derived from `arr` by deleting some or no elements 
+ *
+ * A **subsequence** is a sequence that can be derived from `arr` by deleting some or no elements
  * without changing the order of the remaining elements.
- * 
+ *
+ * **Constraints:**
+ *
+ * - `1 <= arr.length <= 105`
+ * - `-104 <= arr[i], difference <= 104`
+ *
  * @param {number[]} arr
  * @param {number} difference
  * @return {number}
  */
-const longestSubsequence = function(arr, difference) {}
+const longestSubsequence = function (arr, difference) {};
