a1e7f2832cb5b30090ef866c9224c6bb104be117
yuhcee <egbouchenna001@gmail.com>
2023-07-14 23:54:30 +0100

    :books: (longestSubsequence) add task description

---
+++ Datastructures_&_Algorithms/Dynamic Programming/longestSubsequence.js
@@ -0,0 +1,15 @@
+/**
+ * **1218. Longest Arithmetic Subsequence of Given Difference**
+ * 
+ * Given an integer array `arr` and an integer `difference`, return the length of the longest 
+ * subsequence in `arr` which is an arithmetic sequence such that the difference between adjacent 
+ * elements in the subsequence equals `difference`.
+ * 
+ * A **subsequence** is a sequence that can be derived from `arr` by deleting some or no elements 
+ * without changing the order of the remaining elements.
+ * 
+ * @param {number[]} arr
+ * @param {number} difference
+ * @return {number}
+ */
+const longestSubsequence = function(arr, difference) {}
