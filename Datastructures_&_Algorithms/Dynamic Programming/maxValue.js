f895740942bbb2c0d2492879f0fd1bc95dd9fa29
yuhcee <egbouchenna001@gmail.com>
2023-07-15 23:56:15 +0100

    :sparkles: (maxValue) add task solution

--- Datastructures_&_Algorithms/Dynamic Programming/maxValue.js
+++ Datastructures_&_Algorithms/Dynamic Programming/maxValue.js
@@ -23,4 +23,38 @@
  * @param {number} k
  * @return {number}
  */
-const maxValue = function (events, k) {};
+const maxValue = function (events, k) {
+    events.sort((a, b) => a[1] - b[1]); // Sort events based on end days in ascending order
+    const n = events.length;
+    const dp = Array.from({ length: k + 1 }, () => Array(n + 1).fill(0));
+
+    for (let i = 1; i <= k; i++) {
+        for (let j = 1; j <= n; j++) {
+            const [start, end, value] = events[j - 1];
+            const prevEventIndex = binarySearch(events, start);
+            dp[i][j] = Math.max(
+                dp[i][j - 1], // Skip the current event
+                dp[i - 1][prevEventIndex] + value // Include the current event
+            );
+        }
+    }
+
+    return dp[k][n];
+};
+
+// Binary search to find the previous event index
+function binarySearch(events, target) {
+    let left = 0;
+    let right = events.length - 1;
+
+    while (left <= right) {
+        const mid = Math.floor((left + right) / 2);
+        if (events[mid][1] < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+
+    return left;
+}
