e34f90be73ee619906e3f57b8fad8413dd6f287b
yuhcee <egbouchenna001@gmail.com>
2023-07-16 23:27:00 +0100

    :books: (smallestSufficientTeam) add task description

---
+++ Datastructures_&_Algorithms/Dynamic Programming/smallestSufficientTeam.js
@@ -0,0 +1,23 @@
+/**
+ * **1125. Smallest Sufficient Team**
+ * 
+ * In a project, you have a list of required skills `req_skills`, and a list of people. The `ith` 
+ * person `people[i]` contains a list of skills that the person has.
+ * 
+ * Consider a sufficient team: a set of people such that for every required skill in `req_skills`, 
+ * there is at least one person in the team who has that skill. We can represent these teams by the 
+ * index of each person.
+ * 
+ * For example, `team = [0, 1, 3]` represents the people with skills `people[0]`, `people[1]`, and 
+ * `people[3]`.
+ * 
+ * Return *any sufficient team of the smallest possible size, represented by the index of each 
+ * person*. You may return the answer in **any order**.
+ * 
+ * It is **guaranteed** an answer exists.
+ * 
+ * @param {string[]} req_skills
+ * @param {string[][]} people
+ * @return {number[]}
+ */
+const smallestSufficientTeam = function (req_skills, people) {};
