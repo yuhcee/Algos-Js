3ee6292185272cda0d57963801f0cff63c1a1adc
yuhcee <egbouchenna001@gmail.com>
2023-07-17 23:59:12 +0100

    :sparkles: (maxDistance) add task solution

--- Datastructures_&_Algorithms/Dynamic Programming/maxDistance.js
+++ Datastructures_&_Algorithms/Dynamic Programming/maxDistance.js
@@ -17,4 +17,69 @@
  * @param {number[][]} grid
  * @return {number}
  */
-const maxDistance = function (grid) {};
+const maxDistance = function (grid) {
+    const n = grid.length;
+    const queue = [];
+    let hasWater = false;
+    let hasLand = false;
+
+    // Check if there is at least one land and one water cell
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < n; j++) {
+            if (grid[i][j] === 0) {
+                hasWater = true;
+            } else {
+                hasLand = true;
+            }
+        }
+    }
+
+    // If either land or water is missing, return -1
+    if (!hasLand || !hasWater) {
+        return -1;
+    }
+
+    // Add all land cells to the queue
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < n; j++) {
+            if (grid[i][j] === 1) {
+                queue.push([i, j]);
+            }
+        }
+    }
+
+    let distance = -1;
+
+    // Perform breadth-first search to find the maximum distance
+    while (queue.length > 0) {
+        const size = queue.length;
+
+        for (let i = 0; i < size; i++) {
+            const [x, y] = queue.shift();
+
+            // Explore the four adjacent cells
+            const directions = [
+                [1, 0], // Down
+                [-1, 0], // Up
+                [0, 1], // Right
+                [0, -1], // Left
+            ];
+
+            for (const [dx, dy] of directions) {
+                const newX = x + dx;
+                const newY = y + dy;
+
+                // Check if the cell is within the grid and is water
+                if (newX >= 0 && newX < n && newY >= 0 && newY < n && grid[newX][newY] === 0) {
+                    grid[newX][newY] = 1; // Mark the cell as visited land
+                    queue.push([newX, newY]);
+                }
+            }
+        }
+
+        // Increment the distance after exploring a level
+        distance++;
+    }
+
+    return distance;
+};
