fc9e9fe505001d797a356e3864eef4835e153a13
yuhcee <egbouchenna001@gmail.com>
2023-07-18 23:53:14 +0100

    :white_check_mark:(frequencySort) add testcase 1

--- Datastructures_&_Algorithms/HashSets/frequencySort.js
+++ Datastructures_&_Algorithms/HashSets/frequencySort.js
@@ -37,3 +37,9 @@ const frequencySort = function (s) {
 
     return sortedString;
 };
+
+const s = 'tree';
+// Output: "eert"
+/* Explanation: 'e' appears twice while 'r' and 't' both appear once.
+So 'e' must appear before both 'r' and 't'. Therefore "eetr" is also a valid answer. */
+console.log(frequencySort(s));
