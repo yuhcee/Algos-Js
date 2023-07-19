60d9951964c1a5d19ff30edfd3c8da9f510a4d40
yuhcee <egbouchenna001@gmail.com>
2023-07-19 23:53:01 +0100

    :heavy_plus_sign: (eraseOverlapIntervals) add constraints

--- Datastructures_&_Algorithms/Dynamic Programming/eraseOverlapIntervals.js
+++ Datastructures_&_Algorithms/Dynamic Programming/eraseOverlapIntervals.js
@@ -3,6 +3,12 @@
  *
  * Given an array of intervals `intervals` where `intervals[i] = [starti, endi]`, return *the minimum number of intervals you need to remove to make the rest of the intervals non-overlapping*.
  *
+ * **Constraints:**
+ *
+ * - `1 <= intervals.length <= 105`
+ * - `intervals[i].length == 2`
+ * - `-5 * 104 <= starti < endi <= 5 * 104`
+ *
  * @param {number[][]} intervals
  * @return {number}
  */
