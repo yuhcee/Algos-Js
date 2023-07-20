b694f9b3cd6c92d6ae4e452712222da1e486e4c4
yuhcee <egbouchenna001@gmail.com>
2023-07-20 23:22:16 +0100

    :sparkles: (asteroidCollision) add task solution

--- Datastructures_&_Algorithms/Stacks/asteroidCollision.js
+++ Datastructures_&_Algorithms/Stacks/asteroidCollision.js
@@ -20,4 +20,31 @@
  * @param {number[]} asteroids
  * @return {number[]}
  */
-const asteroidCollision = function (asteroids) {};
+const asteroidCollision = function (asteroids) {
+    // If the stack is empty or the current asteroid is moving to the right, push it onto the stack
+    if (stack.length === 0 || asteroid > 0) {
+        stack.push(asteroid);
+      } else {
+        // The current asteroid is moving to the left (negative)
+  
+        // Handle potential collisions with asteroids in the stack
+        while (stack.length > 0 && stack[stack.length - 1] > 0 && Math.abs(stack[stack.length - 1]) < Math.abs(asteroid)) {
+          // Pop asteroids from the stack as long as the top asteroid is moving to the right (positive)
+          // and its absolute value is less than the absolute value of the current asteroid
+          stack.pop();
+        }
+  
+        // After handling collisions, check if the current asteroid is destroyed or survives
+        if (stack.length === 0 || stack[stack.length - 1] < 0) {
+          // The current asteroid survives the collisions or there are no more asteroids in the stack
+          stack.push(asteroid);
+        } else if (stack[stack.length - 1] === Math.abs(asteroid)) {
+          // Both the current asteroid and the top asteroid in the stack have the same absolute value, so they both explode
+          stack.pop();
+        }
+        // Otherwise, the current asteroid is destroyed and we do not push it onto the stack
+      }
+    }
+  
+    return stack;
+};
