e306918fbf2c935f83afa82c3c74f6a05c3ce180
yuhcee <egbouchenna001@gmail.com>
2023-07-21 23:48:29 +0100

    :white_check_mark:(findNumberOfLIS) add testcase 1

--- Datastructures_&_Algorithms/Dynamic Programming/findNumberOfLIS.js
+++ Datastructures_&_Algorithms/Dynamic Programming/findNumberOfLIS.js
@@ -48,3 +48,8 @@ const findNumberOfLIS = function (nums) {
 
     return count; // Return the count of longest increasing subsequences
 };
+
+const nums = [1, 3, 5, 4, 7];
+// Output: 2
+// Explanation: The two longest increasing subsequences are [1, 3, 4, 7] and [1, 3, 5, 7].
+console.log(findNumberOfLIS(nums));
