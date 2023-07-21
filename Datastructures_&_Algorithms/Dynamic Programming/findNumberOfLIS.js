8dbb9195fe664e84c99d889c21479acebaa4c24c
yuhcee <egbouchenna001@gmail.com>
2023-07-21 23:46:09 +0100

    :books: (findNumberOfLIS) add task description

---
+++ Datastructures_&_Algorithms/Dynamic Programming/findNumberOfLIS.js
@@ -0,0 +1,11 @@
+/**
+ * **673. Number of Longest Increasing Subsequence**
+ * 
+ * Given an integer array `nums`, return *the number of longest increasing subsequences*.
+ * 
+ * **Notice** that the sequence has to be **strictly** increasing.
+ * 
+ * @param {number[]} nums
+ * @return {number}
+ */
+const findNumberOfLIS = function (nums) {};
