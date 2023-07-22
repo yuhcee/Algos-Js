e1d9980f24d0acf63161230f7520281310b25334
yuhcee <egbouchenna001@gmail.com>
2023-07-22 23:04:56 +0100

    :sparkles: (knightProbability) add task solution

--- Datastructures_&_Algorithms/Dynamic Programming/knightProbability.js
+++ Datastructures_&_Algorithms/Dynamic Programming/knightProbability.js
@@ -27,4 +27,58 @@
  * @param {number} column
  * @return {number}
  */
-const knightProbability = function (n, k, row, column) {};
+const knightProbability = function (n, k, row, column) {
+    // Create a 2D array to store the probability of being on each cell after k moves
+    const dp = Array.from({ length: n }, () => Array(n).fill(0));
+
+    // The 8 possible moves a knight can make (relative to its current position)
+    const moves = [
+        [1, 2],
+        [2, 1],
+        [2, -1],
+        [1, -2],
+        [-1, -2],
+        [-2, -1],
+        [-2, 1],
+        [-1, 2],
+    ];
+
+    // Mark the starting position with a probability of 1 since the knight is initially at that position
+    dp[row][column] = 1;
+
+    // Loop through k moves
+    for (let move = 0; move < k; move++) {
+        // Create a new DP array to store the updated probabilities after each move
+        const newDp = Array.from({ length: n }, () => Array(n).fill(0));
+
+        // Loop through each cell on the chessboard
+        for (let r = 0; r < n; r++) {
+            for (let c = 0; c < n; c++) {
+                // Check all possible moves for the knight from the current cell
+                for (const [dr, dc] of moves) {
+                    const newR = r + dr;
+                    const newC = c + dc;
+
+                    // Check if the new position is still within the chessboard
+                    if (newR >= 0 && newR < n && newC >= 0 && newC < n) {
+                        // Update the probability for the new position based on the probability of the current position
+                        newDp[newR][newC] += dp[r][c] / 8;
+                    }
+                }
+            }
+        }
+
+        // Update the dp array with the new probabilities after the current move
+        dp.splice(0, dp.length, ...newDp);
+    }
+
+    // Calculate the total probability of the knight being on the chessboard after k moves
+    let totalProbability = 0;
+    for (let r = 0; r < n; r++) {
+        for (let c = 0; c < n; c++) {
+            totalProbability += dp[r][c];
+        }
+    }
+
+    return totalProbability;
+};
