5b748bfc9a8bd6baf940f45d3f6efc1bce8d4779
yuhcee <egbouchenna001@gmail.com>
2023-07-25 00:35:09 +0100

    :books: (myPow) add task description

---
+++ Datastructures_&_Algorithms/Math/myPow.js
@@ -0,0 +1,12 @@
+/**
+ * **50. Pow(x, n)**
+ *
+ * Implement `pow(x, n)`, which calculates `x` raised to the power `n` (i.e., `xn`).
+ *
+ * Calculate x raised to the power n (x^n).
+ *
+ * @param {number} x - The base number.
+ * @param {number} n - The exponent.
+ * @return {number} - The result of x raised to the power n.
+ */
+const myPow = function (x, n) {};
