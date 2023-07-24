0e7a800f1a1f7c898a8bda025a24f926ea346fad
yuhcee <egbouchenna001@gmail.com>
2023-07-25 00:37:30 +0100

    :heavy_plus_sign: (myPow) add constraints

--- Datastructures_&_Algorithms/Math/myPow.js
+++ Datastructures_&_Algorithms/Math/myPow.js
@@ -5,6 +5,14 @@
  *
  * Calculate x raised to the power n (x^n).
  *
+ *  **Constraints:**
+ *
+ * - `-100.0 < x < 100.0`
+ * - `-231 <= n <= 231-1`
+ * - `n` is an integer.
+ * - Either `x` is not zero or `n > 0`.
+ * - `-104 <= xn <= 104`
+ *
  * @param {number} x - The base number.
  * @param {number} n - The exponent.
  * @return {number} - The result of x raised to the power n.
