798d297b3582f2dd7ab2fb01e16bc5134cf32d83
yuhcee <egbouchenna001@gmail.com>
2023-07-24 23:57:27 +0100

    :white_check_mark:(distinctNames) add testcase 1

--- Datastructures_&_Algorithms/HashSets/distinctNames.js
+++ Datastructures_&_Algorithms/HashSets/distinctNames.js
@@ -67,3 +67,20 @@ const distinctNames = function (ideas) {
 
     return result;
 };
+
+const ideas = ['coffee', 'donuts', 'time', 'toffee'];
+// Output: 6
+/* Explanation: The following selections are valid:
+- ("coffee", "donuts"): The company name created is "doffee conuts".
+- ("donuts", "coffee"): The company name created is "conuts doffee".
+- ("donuts", "time"): The company name created is "tonuts dime".
+- ("donuts", "toffee"): The company name created is "tonuts doffee".
+- ("time", "donuts"): The company name created is "dime tonuts".
+- ("toffee", "donuts"): The company name created is "doffee tonuts".
+Therefore, there are a total of 6 distinct company names.
+
+The following are some examples of invalid selections:
+- ("coffee", "time"): The name "toffee" formed after swapping already exists in the original array.
+- ("time", "toffee"): Both names are still the same after swapping and exist in the original array.
+- ("coffee", "toffee"): Both names formed after swapping already exist in the original array. */
+console.log(distinctNames(ideas));
