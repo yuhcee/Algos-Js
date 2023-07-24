d362cfc7180b2a99c6fb09ec6cd1e04150e61c08
yuhcee <egbouchenna001@gmail.com>
2023-07-24 23:54:23 +0100

    :books: (distinctNames) add task description

--- Datastructures_&_Algorithms/HashSets/distinctNames.js
+++ Datastructures_&_Algorithms/HashSets/distinctNames.js
@@ -1,15 +1,21 @@
 /**
  * **2306. Naming a Company**
- * 
-You are given an array of strings ideas that represents a list of names to be used in the process of naming a company. The process of naming a company is as follows:
-
-Choose 2 distinct names from ideas, call them ideaA and ideaB.
-Swap the first letters of ideaA and ideaB with each other.
-If both of the new names are not found in the original ideas, then the name ideaA ideaB (the concatenation of ideaA and ideaB, separated by a space) is a valid company name.
-Otherwise, it is not a valid name.
-Return the number of distinct valid names for the company.
-
+ *
+ *
+ * You are given an array of strings ideas that represents a list of names to be used in the process
+ * of naming a company. The process of naming a company is as follows:
+ *
+ * 1. Choose 2 **distinct** names from `ideas`, call them `ideaA` and `ideaB`.
+ *
+ * 2. Swap the first letters of `ideaA` and `ideaB` with each other.
+ *
+ * 3. If both of the new names are not found in the original ideas, then the name `ideaA` `ideaB`
+ * (the **concatenation** of `ideaA` and `ideaB`, separated by a space) is a valid company name.
+ *
+ * 4. Otherwise, it is not a valid name.
+ *
+ * Return the number of distinct valid names for the company.
  * @param {string[]} ideas
  * @return {number}
  */
-var distinctNames = function (ideas) {};
+const distinctNames = function (ideas) {};
