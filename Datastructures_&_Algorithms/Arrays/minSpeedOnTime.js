38084b6b482933d4a9ed5400897003ff564e220b
yuhcee <egbouchenna001@gmail.com>
2023-07-26 23:45:55 +0100

    :books: (minSpeedOnTime) add task description

---
+++ Datastructures_&_Algorithms/Arrays/minSpeedOnTime.js
@@ -0,0 +1,22 @@
+/**
+ * **1870. Minimum Speed to Arrive on Time**
+ *
+ * You are given a floating-point number `hour`, representing the amount of time you have to reach the office. To commute to the office, you
+ * must take `n` trains in sequential order. You are also given an integer array `dist` of length `n`, where `dist[i]` describes the
+ * distance (in kilometers) of the `ith` train ride.
+ *
+ * Each train can only depart at an integer `hour`, so you may need to wait in between each train ride.
+ *
+ * - For example, if the `1st` train ride takes `1.5` hours, you must wait for an additional `0.5` hours before you can depart on the `2nd`
+ * train ride at the 2 hour mark.
+ *
+ * Return *the **minimum positive integer** speed (**in kilometers per hour**) that all the trains must travel at for you to reach the
+ * office on time, or `-1` if it is impossible to be on time*.
+ *
+ * Tests are generated such that the answer will not exceed `107` and hour will have **at most two digits after the decimal point**.
+ *
+ * @param {number[]} dist - Array of distances (in kilometers) of each train ride.
+ * @param {number} hour - The time available to reach the office.
+ * @return {number} - The minimum positive integer speed (in kilometers per hour) needed to reach the office on time.
+ */
+const minSpeedOnTime = function (dist, hour) {};
