5abf1fb366ba06aa9fb97bf826c970a07c87fa15
yuhcee <egbouchenna001@gmail.com>
2023-07-27 23:33:52 +0100

    :white_check_mark:(maxRunTime) add testcase 1

--- Datastructures_&_Algorithms/Arrays/maxRunTime.js
+++ Datastructures_&_Algorithms/Arrays/maxRunTime.js
@@ -56,3 +56,14 @@ const maxRunTime = function (n, batteries) {
     // The answer will be the lower bound (left)
     return left;
 };
+
+const n = 2,
+    batteries = [3, 3, 3];
+// Output: 4
+/* Explanation: 
+Initially, insert battery 0 into the first computer and battery 1 into the second computer.
+After two minutes, remove battery 1 from the second computer and insert battery 2 instead. Note that battery 1 can still run for one minute.
+At the end of the third minute, battery 0 is drained, and you need to remove it from the first computer and insert battery 1 instead.
+By the end of the fourth minute, battery 1 is also drained, and the first computer is no longer running.
+We can run the two computers simultaneously for at most 4 minutes, so we return 4. */
+console.log(maxRunTime(n, batteries));
