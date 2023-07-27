d97aecd56114777081ddc6437df9101979b9d8d6
yuhcee <egbouchenna001@gmail.com>
2023-07-27 23:30:06 +0100

    :books: (maxRunTime) add task description

---
+++ Datastructures_&_Algorithms/Arrays/maxRunTime.js
@@ -0,0 +1,21 @@
+/**
+ * **2141. Maximum Running Time of N Computers**
+ *
+ * You have `n` computers. You are given the integer `n` and a **0-indexed** integer array
+ * `batteries` where the ith battery can run a computer for `batteries[i]` minutes. You are
+ * interested in running **all** `n` computers **simultaneously** using the given batteries.
+ *
+ * Initially, you can insert **at most one battery** into each computer. After that and at any
+ * integer time moment, you can remove a battery from a computer and insert another battery **any
+ * number of times**. The inserted battery can be a totally new battery or a battery from another
+ * computer. You may assume that the removing and inserting processes take no time.
+ *
+ * Note that the batteries cannot be recharged.
+ *
+ * Return *the **maximum** number of minutes you can run all the `n` computers simultaneously*.
+ *
+ * @param {number} n - The number of computers.
+ * @param {number[]} batteries - Array of battery times for each computer.
+ * @return {number} - The maximum number of minutes all n computers can run simultaneously.
+ */
+const maxRunTime = function (n, batteries) {};
