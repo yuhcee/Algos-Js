991ac4d7e1ad83f96bc56dbd540e696d764d7cea
yuhcee <egbouchenna001@gmail.com>
2023-07-31 22:08:56 +0100

    :white_check_mark:(combine) add testcase 1

--- Datastructures_&_Algorithms/Graphs/combine.js
+++ Datastructures_&_Algorithms/Graphs/combine.js
@@ -37,3 +37,9 @@ const combine = function (n, k) {
     backtrack([], 1); // Start the backtracking process with an empty current array and 'start' at 1
     return result;
 };
+const n = 4,
+    k = 2;
+// Output: [[1,2],[1,3],[1,4],[2,3],[2,4],[3,4]]
+/* Explanation: There are 4 choose 2 = 6 total combinations.
+Note that combinations are unordered, i.e., [1,2] and [2,1] are considered to be the same combination. */
+console.log(combine(n, k));
