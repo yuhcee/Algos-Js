372584b26d97a1482c294419b47d317bc4c98dca
yuhcee <egbouchenna001@gmail.com>
2023-07-31 22:07:09 +0100

    :books: (combine) add task description

---
+++ Datastructures_&_Algorithms/Graphs/combine.js
@@ -0,0 +1,13 @@
+/**
+ * **77. Combinations**
+ *
+ * Given two integers `n` and `k`, return *all possible combinations of `k` numbers chosen from the
+ * range `[1, n]`.
+ *
+ * You may return the answer in **any order**.
+ *
+ * @param {number} n
+ * @param {number} k
+ * @return {number[][]}
+ */
+const combine = function (n, k) {};
