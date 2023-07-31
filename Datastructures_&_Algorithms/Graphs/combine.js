cc48952d2077d481c9bc6aa45d99306fe6fafad9
yuhcee <egbouchenna001@gmail.com>
2023-07-31 22:08:05 +0100

    :sparkles: (combine) add task solution

--- Datastructures_&_Algorithms/Graphs/combine.js
+++ Datastructures_&_Algorithms/Graphs/combine.js
@@ -15,4 +15,25 @@
  * @param {number} k
  * @return {number[][]}
  */
-const combine = function (n, k) {};
+const combine = function (n, k) {
+    const result = []; // Array to store the final combinations
+
+    // Helper function to perform backtracking and generate combinations
+    const backtrack = (current, start) => {
+        // If the current combination has k elements, add it to the result array
+        if (current.length === k) {
+            result.push([...current]);
+            return;
+        }
+
+        // Loop through the remaining numbers from 'start' to 'n'
+        for (let i = start; i <= n; i++) {
+            current.push(i); // Include the current number in the combination
+            backtrack(current, i + 1); // Recursively generate combinations with the next numbers
+            current.pop(); // Backtrack: Remove the last number to explore other possibilities
+        }
+    };
+
+    backtrack([], 1); // Start the backtracking process with an empty current array and 'start' at 1
+    return result;
+};
