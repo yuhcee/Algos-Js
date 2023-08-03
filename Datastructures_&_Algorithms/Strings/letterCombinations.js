e1aab593872763fe1b6451ecf148536815ae3bd0
yuhcee <egbouchenna001@gmail.com>
2023-08-03 22:43:03 +0100

    :sparkles: (letterCombinations) add task solution

--- Datastructures_&_Algorithms/Strings/letterCombinations.js
+++ Datastructures_&_Algorithms/Strings/letterCombinations.js
@@ -10,23 +10,23 @@
  * @param {string} digits
  * @return {string[]}
  */
-const letterCombinations = (digits) => {
-    const letters = { 2: ['a', 'b', 'c'], 3: ['d', 'e', 'f'], 4: ['g', 'h', 'i'], 5: ['j', 'k', 'l'], 6: ['m', 'n', 'o'], 7: ['p', 'q', 'r', 's'], 8: ['t', 'u', 'v'], 9: ['w', 'x', 'y', 'z'] };
+// const letterCombinations = (digits) => {
+//     const letters = { 2: ['a', 'b', 'c'], 3: ['d', 'e', 'f'], 4: ['g', 'h', 'i'], 5: ['j', 'k', 'l'], 6: ['m', 'n', 'o'], 7: ['p', 'q', 'r', 's'], 8: ['t', 'u', 'v'], 9: ['w', 'x', 'y', 'z'] };
 
-    return digits.split('').reduce((prev, digit) => {
-        if (prev.length === 0) return letters[digit];
+//     return digits.split('').reduce((prev, digit) => {
+//         if (prev.length === 0) return letters[digit];
 
-        let res = [];
+//         let res = [];
 
-        for (let l0 of prev) {
-            for (let l1 of letters[digit]) {
-                res.push(`${l0}${l1}`);
-            }
-        }
+//         for (let l0 of prev) {
+//             for (let l1 of letters[digit]) {
+//                 res.push(`${l0}${l1}`);
+//             }
+//         }
 
-        return res;
-    }, []);
-};
+//         return res;
+//     }, []);
+// };
 
 /* const buildCharacters = () => {
     const graph = {};
@@ -40,6 +40,61 @@ const letterCombinations = (digits) => {
     }
     return graph;
 }; */
+
+/**
+ * Function to generate all possible letter combinations for a given string of digits.
+ * @param {string} digits - The input string containing digits from 2 to 9.
+ * @return {string[]} - An array containing all possible letter combinations.
+ */
+const letterCombinations = function (digits) {
+    // Check if the input is empty or null, return an empty array in that case.
+    if (!digits || digits.length === 0) {
+        return [];
+    }
+
+    // Mapping of digits to corresponding letters on the telephone buttons.
+    const digitToLetters = {
+        2: 'abc',
+        3: 'def',
+        4: 'ghi',
+        5: 'jkl',
+        6: 'mno',
+        7: 'pqrs',
+        8: 'tuv',
+        9: 'wxyz',
+    };
+
+    // Array to store all the generated combinations.
+    const combinations = [];
+
+    /**
+     * Helper function to perform backtracking and generate letter combinations.
+     * @param {string} current - The current combination being generated.
+     * @param {number} index - The index of the current digit in the input string.
+     */
+    const backtrack = (current, index) => {
+        // If the current combination has the same length as the input digits,
+        // it is a valid combination. Add it to the result array.
+        if (index === digits.length) {
+            combinations.push(current);
+            return;
+        }
+
+        // Get the possible letters for the current digit from the mapping.
+        const letters = digitToLetters[digits[index]];
+
+        // Recursively generate combinations for the next digit by appending each possible letter.
+        for (const letter of letters) {
+            backtrack(current + letter, index + 1);
+        }
+    };
+
+    // Start the backtracking process with an empty string and index 0.
+    backtrack('', 0);
+
+    // Return the array containing all the generated combinations.
+    return combinations;
+};
 
 const digits = '23'; // Output: ["ad","ae","af","bd","be","bf","cd","ce","cf"]
 const digits1 = ''; // Output: []
