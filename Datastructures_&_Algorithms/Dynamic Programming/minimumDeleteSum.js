eb2eacf84ae3969a65862b143e7751b1c1986776
yuhcee <egbouchenna001@gmail.com>
2023-08-03 22:47:29 +0100

    :heavy_plus_sign: (minimumDeleteSum) add constraints

--- Datastructures_&_Algorithms/Dynamic Programming/minimumDeleteSum.js
+++ Datastructures_&_Algorithms/Dynamic Programming/minimumDeleteSum.js
@@ -4,10 +4,13 @@
  * Given two strings `s1` and `s2`, return *the lowest **ASCII** sum of deleted characters to make two
  * strings equal*.
  *
+ * **Constraints:**
+ *
+ * - `1 <= s1.length, s2.length <= 1000`
+ * - `s1` and `s2` consist of lowercase English letters.
+ *
  * @param {string} s1
  * @param {string} s2
  * @return {number}
  */
-const minimumDeleteSum = function (s1, s2) {
-    
-};
+const minimumDeleteSum = function (s1, s2) {};
