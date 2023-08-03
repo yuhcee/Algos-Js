a3b4b0aeebfd33c0544571a454c42624da9afad3
yuhcee <egbouchenna001@gmail.com>
2023-08-03 22:46:29 +0100

    :books: (minimumDeleteSum) add task description

---
+++ Datastructures_&_Algorithms/Dynamic Programming/minimumDeleteSum.js
@@ -0,0 +1,13 @@
+/**
+ * **712. Minimum ASCII Delete Sum for Two Strings**
+ *
+ * Given two strings `s1` and `s2`, return *the lowest **ASCII** sum of deleted characters to make two
+ * strings equal*.
+ *
+ * @param {string} s1
+ * @param {string} s2
+ * @return {number}
+ */
+const minimumDeleteSum = function (s1, s2) {
+    
+};
