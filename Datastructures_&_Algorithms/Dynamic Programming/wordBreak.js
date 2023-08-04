4ff4e3739590ad35553b37ba774145da693b975a
yuhcee <egbouchenna001@gmail.com>
2023-08-04 23:57:19 +0100

    :books: (wordBreak) add task description

---
+++ Datastructures_&_Algorithms/Dynamic Programming/wordBreak.js
@@ -0,0 +1,14 @@
+/**
+ * **139. Word Break**
+ *
+ * Given a string s and a dictionary of strings wordDict, return true if s can be
+ * segmented into a space-separated sequence of one or more dictionary words.
+ *
+ * Note that the same word in the dictionary may be reused multiple times in the
+ * segmentation.
+ *
+ * @param {string} s
+ * @param {string[]} wordDict
+ * @return {boolean}
+ */
+var wordBreak = function (s, wordDict) {};
