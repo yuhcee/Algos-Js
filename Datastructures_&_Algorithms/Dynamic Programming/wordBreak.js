800d2d9512eb7e1b53d0daa3d86953b032b17070
yuhcee <egbouchenna001@gmail.com>
2023-08-04 23:58:28 +0100

    :sparkles: (wordBreak) add task solution

--- Datastructures_&_Algorithms/Dynamic Programming/wordBreak.js
+++ Datastructures_&_Algorithms/Dynamic Programming/wordBreak.js
@@ -19,4 +19,29 @@ All the strings of wordDict are unique.
  * @param {string[]} wordDict
  * @return {boolean}
  */
-var wordBreak = function (s, wordDict) {};
+var wordBreak = function (s, wordDict) {
+    // Create a set to store the words in the wordDict for faster lookup
+    const wordSet = new Set(wordDict);
+
+    // Create an array to store the DP results for each index in the string
+    // dp[i] will be true if s[0...i] can be segmented into words from wordDict
+    const dp = new Array(s.length + 1).fill(false);
+    dp[0] = true; // Empty string can always be segmented
+
+    // Iterate through the string characters
+    for (let i = 1; i <= s.length; i++) {
+        // For each index i, check if s[0...i] can be segmented into words
+        // by checking all possible substrings s[j...i] (where j goes from 0 to i-1)
+        for (let j = 0; j < i; j++) {
+            // Check if the substring s[j...i] can be segmented and s[j...i] is present in wordDict
+            if (dp[j] && wordSet.has(s.substring(j, i))) {
+                // If both conditions are met, set dp[i] to true and break the loop
+                dp[i] = true;
+                break;
+            }
+        }
+    }
+
+    // dp[s.length] will hold the result for the entire string s
+    return dp[s.length];
+};
