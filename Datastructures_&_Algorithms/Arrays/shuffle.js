4da48dc4f3f3fe611d05e1d28222921564b614f0
yuhcee <egbouchenna001@gmail.com>
2023-08-06 23:58:05 +0100

    :heavy_plus_sign: (shuffle) add constraints

--- Datastructures_&_Algorithms/Arrays/shuffle.js
+++ Datastructures_&_Algorithms/Arrays/shuffle.js
@@ -6,6 +6,12 @@
  *
  * Return *the array in the form `[x1,y1,x2,y2,...,xn,yn]`*.
  *
+ * **Constraints:**
+ *
+ * - `1 <= n <= 500`
+ * - `nums.length == 2n`
+ * - `1 <= nums[i] <= 10^3`
+ *
  * @param {number[]} nums
  * @param {number} n
  * @return {number[]}
