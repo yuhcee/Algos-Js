a6fe5bd93db695be5050f6b11c62bdca59ebcb79
yuhcee <egbouchenna001@gmail.com>
2023-08-06 23:59:38 +0100

    :white_check_mark:(shuffle) add testcase 1

--- Datastructures_&_Algorithms/Arrays/shuffle.js
+++ Datastructures_&_Algorithms/Arrays/shuffle.js
@@ -28,3 +28,9 @@ const shuffle = function (nums, n) {
 
     return res;
 };
+
+const nums = [2, 5, 1, 3, 4, 7],
+    n = 3;
+// Output: [2,3,5,4,1,7]
+// Explanation: Since x1=2, x2=5, x3=1, y1=3, y2=4, y3=7 then the answer is [2,3,5,4,1,7].
+console.log(shuffle(nums, n));
