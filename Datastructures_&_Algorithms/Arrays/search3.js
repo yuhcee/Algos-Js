83ecedb4a053a7caf938135b9b8c34b7bdeb781f
yuhcee <egbouchenna001@gmail.com>
2023-08-10 23:39:52 +0100

    :sparkles: (search) add task solution

--- Datastructures_&_Algorithms/Arrays/search3.js
+++ Datastructures_&_Algorithms/Arrays/search3.js
@@ -26,4 +26,38 @@
  * @param {number} target
  * @return {boolean}
  */
-const search = function (nums, target) {};
+const search = function (nums, target) {
+    let left = 0;
+    let right = nums.length - 1;
+
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2);
+
+        if (nums[mid] === target) {
+            return true;
+        }
+
+        // If the left half is sorted
+        if (nums[left] < nums[mid]) {
+            if (nums[left] <= target && target < nums[mid]) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        // If the right half is sorted
+        else if (nums[left] > nums[mid]) {
+            if (nums[mid] < target && target <= nums[right]) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        // Handle duplicates
+        else {
+            left++;
+        }
+    }
+
+    return false;
+};
