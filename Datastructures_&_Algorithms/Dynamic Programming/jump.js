2d53c666e0b7f43923b296f136cebdc86e6fe980
yuhcee <egbouchenna001@gmail.com>
2023-08-12 23:10:06 +0100

    :white_check_mark:(jump) add testcase 1

--- Datastructures_&_Algorithms/Dynamic Programming/jump.js
+++ Datastructures_&_Algorithms/Dynamic Programming/jump.js
@@ -57,3 +57,8 @@ const jump = function (nums) {
         }
     }
 };
+
+const nums = [2, 3, 1, 1, 4];
+// Output: 2
+// Explanation: The minimum number of jumps to reach the last index is 2. Jump 1 step from index 0 to 1, then 3 steps to the last index.
+console.log(jump(nums));
