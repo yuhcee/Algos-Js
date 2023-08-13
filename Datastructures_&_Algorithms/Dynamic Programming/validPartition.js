8092be452d94f52a79cbdab3b06154cddd32e85d
yuhcee <egbouchenna001@gmail.com>
2023-08-13 22:29:48 +0100

    :heavy_plus_sign: (validPartition) add comments

--- Datastructures_&_Algorithms/Dynamic Programming/validPartition.js
+++ Datastructures_&_Algorithms/Dynamic Programming/validPartition.js
@@ -27,27 +27,37 @@
  * @return {boolean}
  */
 const validPartition = function (nums) {
+    // Memoization object to store computed results for each index
     const memo = {};
+    // Total number of elements in the array
     const n = nums.length;
 
+    // Recursive function to check if valid partition is possible starting from index i
     const explore = (i) => {
+        // Base case: If we've reached the end of the array, a valid partition is achieved
         if (i === n) return true;
+        // If we've already computed the result for this index, return it
         if (memo[i] !== undefined) return memo[i];
 
+        // Case 1: Check for subarray of 2 equal elements
         if (i + 1 < n && nums[i] === nums[i + 1]) {
             if (explore(i + 2)) return true;
         }
 
+        // Case 2: Check for subarray of 3 equal elements
         if (i + 2 < n && nums[i] === nums[i + 1] && nums[i] === nums[i + 2]) {
             if (explore(i + 3)) return true;
         }
 
+        // Case 3: Check for subarray of 3 consecutive increasing elements
         if (i + 2 < n && 1 + nums[i] === nums[i + 1] && 1 + nums[i + 1] === nums[i + 2]) {
             if (explore(i + 3)) return true;
         }
-
+        // If none of the cases lead to valid partition, memoize false and return false
         return (memo[i] = false);
     };
+
+    // Start the recursive process from index 0
     return explore(0);
 };
 
