3e1a2b7dc7e4927edae75fa6a3a2dccbd5c86bd4
yuhcee <egbouchenna001@gmail.com>
2023-08-20 22:52:18 +0100

    :books: (sortItems) add task description

---
+++ Datastructures_&_Algorithms/Graphs/sortItems.js
@@ -0,0 +1,26 @@
+/**
+ * **1203. Sort Items by Groups Respecting Dependencies**
+ * 
+ * There are `n` items each belonging to zero or one of `m` groups where 
+ * `group[i]` is the group that the `i`-th item belongs to and it's equal to 
+ * `-1` if the `i`-th item belongs to no group. The items and the groups are 
+ * zero indexed. A group can have no item belonging to it.
+ * 
+ * Return a sorted list of the items such that:
+ * 
+ * - The items that belong to the same group are next to each other in the 
+ * sorted list.
+ * - There are some relations between these items where `beforeItems[i]` is a 
+ * list containing all the items that should come before the `i`-th item in 
+ * the sorted array (to the left of the `i`-th item).
+ * 
+ * Return any solution if there is more than one solution and return an 
+ * **empty list** if there is no solution.
+ * 
+ * @param {number} n
+ * @param {number} m
+ * @param {number[]} group
+ * @param {number[][]} beforeItems
+ * @return {number[]}
+ */
+const sortItems = function (n, m, group, beforeItems) {};
