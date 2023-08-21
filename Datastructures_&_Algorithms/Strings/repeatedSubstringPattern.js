eae93e604d8bd6099d7528422c73d3111935261b
yuhcee <egbouchenna001@gmail.com>
2023-08-22 00:02:02 +0100

    :books: (repeatedSubstringPattern) add task description

---
+++ Datastructures_&_Algorithms/Strings/repeatedSubstringPattern.js
@@ -0,0 +1,10 @@
+/**
+ * **459. Repeated Substring Pattern**
+ *
+ * Given a string `s`, check if it can be constructed by taking a substring
+ * of it and appending multiple copies of the substring together.
+ *
+ * @param {string} s
+ * @return {boolean}
+ */
+const repeatedSubstringPattern = function (s) {};
