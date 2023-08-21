9d1e242eb7ab304346682089eaaf6a67e591d228
yuhcee <egbouchenna001@gmail.com>
2023-08-22 00:02:48 +0100

    :heavy_plus_sign: (repeatedSubstringPattern) add constraints

--- Datastructures_&_Algorithms/Strings/repeatedSubstringPattern.js
+++ Datastructures_&_Algorithms/Strings/repeatedSubstringPattern.js
@@ -4,6 +4,11 @@
  * Given a string `s`, check if it can be constructed by taking a substring
  * of it and appending multiple copies of the substring together.
  *
+ * **Constraints:**
+ *
+ * - `1 <= s.length <= 104`
+ * - `s` consists of lowercase English letters.
+ *
  * @param {string} s
  * @return {boolean}
  */
