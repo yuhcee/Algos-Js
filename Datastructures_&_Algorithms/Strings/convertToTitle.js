aa454707890fefc3246df80bbf75e6e9459a2069
yuhcee <egbouchenna001@gmail.com>
2023-08-22 23:46:22 +0100

    :books: (convertToTitle) add task description

---
+++ Datastructures_&_Algorithms/Strings/convertToTitle.js
@@ -0,0 +1,23 @@
+/**
+ * **168. Excel Sheet Column Title**
+ * 
+ * Given an integer `columnNumber`, return *its corresponding column title as 
+ * it appears in an Excel sheet*.
+ * 
+ * For example:
+ * 
+ * ```
+ * A -> 1
+ * B -> 2
+ * C -> 3
+ * ...
+ * Z -> 26
+ * AA -> 27
+ * AB -> 28 
+ * ...
+ * ```
+ * 
+ * @param {number} columnNumber
+ * @return {string}
+ */
+const convertToTitle = function (columnNumber) {};
