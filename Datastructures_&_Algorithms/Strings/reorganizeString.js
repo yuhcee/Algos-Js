bf8d09cb8644a612fe5e6e87c90fc871a6632022
yuhcee <egbouchenna001@gmail.com>
2023-08-24 22:51:48 +0100

    :sparkles: (reorganizeString) add task solution

--- Datastructures_&_Algorithms/Strings/reorganizeString.js
+++ Datastructures_&_Algorithms/Strings/reorganizeString.js
@@ -14,4 +14,50 @@
  * @param {string} s
  * @return {string}
  */
-const reorganizeString = function (s) {};
+const reorganizeString = function (s) {
+    // Step 1: Count the frequency of each letter
+    const frequency = {};
+    for (const char of s) {
+        frequency[char] = (frequency[char] || 0) + 1;
+    }
+
+    // Step 2: Find the most frequent letter (the "king" letter)
+    let kingLetter = '';
+    let maxFrequency = 0;
+    for (const char in frequency) {
+        if (frequency[char] > maxFrequency) {
+            maxFrequency = frequency[char];
+            kingLetter = char;
+        }
+    }
+
+    // Check if it's possible to rearrange the string
+    if (maxFrequency > Math.floor((s.length + 1) / 2)) {
+        return '';
+    }
+
+    // Step 3: Rearrange the letters
+    let rearranged = new Array(s.length);
+    let idx = 0;
+
+    // Fill in the "king" letters
+    while (frequency[kingLetter] > 0) {
+        rearranged[idx] = kingLetter;
+        idx += 2; // Leave space for other letters
+        frequency[kingLetter]--;
+    }
+
+    // Fill in the other letters
+    for (const char in frequency) {
+        while (frequency[char] > 0) {
+            if (idx >= s.length) {
+                idx = 1; // Start at the second position
+            }
+            rearranged[idx] = char;
+            idx += 2; // Leave space for "king" letters
+            frequency[char]--;
+        }
+    }
+
+    return rearranged.join('');
+};
