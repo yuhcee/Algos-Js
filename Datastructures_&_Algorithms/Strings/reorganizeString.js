5012d07b911f65c82ef97ba820c37b9f0a29478e
yuhcee <egbouchenna001@gmail.com>
2023-08-24 22:48:20 +0100

    :books: (reorganizeString) add task description

---
+++ Datastructures_&_Algorithms/Strings/reorganizeString.js
@@ -0,0 +1,12 @@
+/**
+ * **767. Reorganize String**
+ *
+ * Given a string `s`, rearrange the characters of `s` so that any two
+ * adjacent characters are not the same.
+ *
+ * Return *any possible rearrangement of `s` or return `""` if not possible*.
+ *
+ * @param {string} s
+ * @return {string}
+ */
+const reorganizeString = function (s) {};
