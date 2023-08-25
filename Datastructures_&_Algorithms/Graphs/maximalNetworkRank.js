14a68a2c56108e8fde9fe29a17ad98a1576f7cb9
yuhcee <egbouchenna001@gmail.com>
2023-08-25 23:53:08 +0100

    :white_check_mark:(maximalNetworkRank) add testcase 1

--- Datastructures_&_Algorithms/Graphs/maximalNetworkRank.js
+++ Datastructures_&_Algorithms/Graphs/maximalNetworkRank.js
@@ -52,3 +52,14 @@ const maximalNetworkRank = function (n, roads) {
 
     return maximalRank;
 };
+
+const n = 4,
+    roads = [
+        [0, 1],
+        [0, 3],
+        [1, 2],
+        [1, 3],
+    ];
+// Output: 4
+// Explanation: The network rank of cities 0 and 1 is 4 as there are 4 roads that are connected to either 0 or 1. The road between 0 and 1 is only counted once.
+console.log(maximalNetworkRank(n, roads));
