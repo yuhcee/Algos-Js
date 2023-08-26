8454e02f72d9715a43d771b49a968ba33bbff814
yuhcee <egbouchenna001@gmail.com>
2023-08-26 22:58:58 +0100

    :books: (findLongestChain) add task description

---
+++ Datastructures_&_Algorithms/Dynamic Programming/longestChain.js
@@ -0,0 +1,35 @@
+/**
+ * **646. Maximum Length of Pair Chain**
+ *
+ * You are given an array of `n` pairs `pairs` where `pairs[i] =
+ * [lefti, righti]` and `lefti < righti`.
+ *
+ * A pair `p2 = [c, d]` **follows** a pair `p1 = [a, b]` if `b < c`. A
+ * **chain** of pairs can be formed in this fashion.
+ *
+ * Return *the length longest chain which can be formed*.
+ *
+ * You do not need to use up all the given intervals. You can select
+ * pairs in any order.
+ *
+ * @param {number[][]} pairs
+ * @return {number}
+ */
+const findLongestChain = function (pairs) {
+    pairs.sort((a, b) => a[1] - b[1]);
+    const n = pairs.length;
+    const dp = new Array(n).fill(1); // Initialize dp array with all elements set to 1
+
+    for (let i = 1; i < n; i++) {
+        for (let j = 0; j < i; j++) {
+            // Check if the first element of the current pair is greater than the second element of the previous pair
+            if (pairs[i][0] > pairs[j][1]) {
+                // Update the dp array by taking the maximum of the current value and the value at the previous pair plus 1
+                dp[i] = Math.max(dp[i], dp[j] + 1);
+            }
+        }
+    }
+
+    // Return the maximum value in the dp array, which represents the length of the longest chain that can be formed
+    return Math.max(...dp);
+};
