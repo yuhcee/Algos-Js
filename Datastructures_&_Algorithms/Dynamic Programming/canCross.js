4e8724e16de64d0dc656837222575357b939d113
yuhcee <egbouchenna001@gmail.com>
2023-08-28 09:05:30 +0100

    :white_check_mark: (canCross) add testcase 2

--- Datastructures_&_Algorithms/Dynamic Programming/canCross.js
+++ Datastructures_&_Algorithms/Dynamic Programming/canCross.js
@@ -52,3 +52,8 @@ const stones = [0, 1, 3, 5, 6, 8, 12, 17];
 // Output: true
 // Explanation: The frog can jump to the last stone by jumping 1 unit to the 2nd stone, then 2 units to the 3rd stone, then 2 units to the 4th stone, then 3 units to the 6th stone, 4 units to the 7th stone, and 5 units to the 8th stone.
 console.log(canCross(stones));
+
+const stones1 = [0, 1, 2, 3, 4, 8, 9, 11];
+// Output: false
+// Explanation: There is no way to jump to the last stone as the gap between the 5th and 6th stone is too large.
+console.log(canCross(stones1));
