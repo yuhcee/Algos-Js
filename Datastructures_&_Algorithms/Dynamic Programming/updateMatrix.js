7a7cc43886e8008502d09edafedb8002a3ceb73e
yuhcee <egbouchenna001@gmail.com>
2023-08-28 23:28:55 +0100

    :books: (updateMatrix) add task description

---
+++ Datastructures_&_Algorithms/Dynamic Programming/updateMatrix.js
@@ -0,0 +1,12 @@
+/**
+ * **542. 01 Matrix**
+ *
+ * Given an `m x n` binary matrix `mat`, return *the distance of the
+ * nearest `0` for each cell*.
+ *
+ * The distance between two adjacent cells is `1`.
+ *
+ * @param {number[][]} mat
+ * @return {number[][]}
+ */
+const updateMatrix = function (mat) {};
