987565c84a324f5c4a2c151b39e9b91ae8f64ce3
yuhcee <egbouchenna001@gmail.com>
2023-08-30 23:55:35 +0100

    :sparkles: (minimumReplacement) add tak solution

--- Datastructures_&_Algorithms/Greedy Algorithms/minimumReplacement.js
+++ Datastructures_&_Algorithms/Greedy Algorithms/minimumReplacement.js
@@ -15,4 +15,24 @@ Constraints:
  * @param {number[]} nums
  * @return {number}
  */
-const minimumReplacement = function (nums) {};
+const minimumReplacement = function (nums) {
+    const n = nums.length;
+    let last = nums[n - 1]; // Initialize 'last' with the last element
+    let ans = 0; // Initialize the total operations count
+
+    // Traverse the array in reverse order
+    for (let i = n - 2; i >= 0; --i) {
+        if (nums[i] > last) {
+            // If the current element needs replacement
+            let t = Math.floor(nums[i] / last); // Calculate how many times the element needs to be divided
+            if (nums[i] % last !== 0) {
+                t++; // If there's a remainder, increment 't'
+            }
+            last = Math.floor(nums[i] / t); // Update 'last' for the next comparison
+            ans += t - 1; // Add (t - 1) to 'ans' for the number of operations
+        } else {
+            last = nums[i]; // Update 'last' without replacement
+        }
+    }
+    return ans; // Return the total number of operations
+};
