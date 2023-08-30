3aff096352a5737181adcfffa3357d427cd55655
yuhcee <egbouchenna001@gmail.com>
2023-08-30 23:56:55 +0100

    :white_check_mark: (minimumReplacement) add testcase 1

--- Datastructures_&_Algorithms/Greedy Algorithms/minimumReplacement.js
+++ Datastructures_&_Algorithms/Greedy Algorithms/minimumReplacement.js
@@ -36,3 +36,11 @@ const minimumReplacement = function (nums) {
     }
     return ans; // Return the total number of operations
 };
+
+const nums = [3, 9, 3];
+// Output: 2
+// Explanation: Here are the steps to sort the array in non-decreasing order:
+// - From [3,9,3], replace the 9 with 3 and 6 so the array becomes [3,3,6,3]
+// - From [3,3,6,3], replace the 6 with 3 and 3 so the array becomes [3,3,3,3,3]
+// There are 2 steps to sort the array in non-decreasing order. Therefore, we return 2.
+console.log(minimumReplacement(nums));
