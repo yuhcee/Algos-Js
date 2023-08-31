87d829c8e5a089392a386b421b61f7d222918c69
yuhcee <egbouchenna001@gmail.com>
2023-08-31 23:47:30 +0100

    :white_check_mark: (minTaps) add testcase 1

--- Datastructures_&_Algorithms/Greedy Algorithms/minTaps.js
+++ Datastructures_&_Algorithms/Greedy Algorithms/minTaps.js
@@ -55,3 +55,16 @@ const minTaps = function (n, ranges) {
 
     return taps;
 };
+
+const n = 5,
+    ranges = [3, 4, 1, 1, 0, 0];
+// Output: 1
+/* Explanation: The tap at point 0 can cover the interval [-3,3]
+The tap at point 1 can cover the interval [-3,5]
+The tap at point 2 can cover the interval [1,3]
+The tap at point 3 can cover the interval [2,4]
+The tap at point 4 can cover the interval [4,4]
+The tap at point 5 can cover the interval [5,5]
+Opening Only the second tap will water the whole garden [0,5] */
+
+console.log(minTaps(n, ranges));
