ca15fff7926c294e34521a6fa979953c80520bc4
yuhcee <egbouchenna001@gmail.com>
2023-09-04 00:01:46 +0100

    :white_check_mark: (minExtraChar) add testcase 2

--- Datastructures_&_Algorithms/Dynamic Programming/minExtraChar.js
+++ Datastructures_&_Algorithms/Dynamic Programming/minExtraChar.js
@@ -61,3 +61,9 @@ const s = 'leetscode',
 // Output: 1
 /* Explanation: We can break s in two substrings: "leet" from index 0 to 3 and "code" from index 5 to 8. There is only 1 unused character (at index 4), so we return 1. */
 console.log(minExtraChar(s, dictionary));
+
+const s1 = 'sayhelloworld',
+    dictionary1 = ['hello', 'world'];
+// Output: 3
+/* Explanation: We can break s in two substrings: "hello" from index 3 to 7 and "world" from index 8 to 12. The characters at indices 0, 1, 2 are not used in any substring and thus are considered as extra characters. Hence, we return 3. */
+console.log(minExtraChar(s1, dictionary1));
