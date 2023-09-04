632668631dfdf6c8949cd8135ad0d88ee0211005
yuhcee <egbouchenna001@gmail.com>
2023-09-04 23:54:39 +0100

    :heavy_plus_sign: (findAnagrams) add constraints

--- Datastructures_&_Algorithms/HashMaps/findAnagrams.js
+++ Datastructures_&_Algorithms/HashMaps/findAnagrams.js
@@ -6,6 +6,11 @@
 
 An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
  * 
+Constraints:
+
+1 <= s.length, p.length <= 3 * 104
+s and p consist of lowercase English letters.
+
  * @param {string} s
  * @param {string} p
  * @return {number[]}
