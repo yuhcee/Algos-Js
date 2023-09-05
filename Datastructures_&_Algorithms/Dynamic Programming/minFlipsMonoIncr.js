dc58cd8504b8e0c926620a2141e2937cd7691eb1
yuhcee <egbouchenna001@gmail.com>
2023-09-05 23:56:38 +0100

    :white_check_mark: (minFlipsMonoIncr) add testcase 2

--- Datastructures_&_Algorithms/Dynamic Programming/minFlipsMonoIncr.js
+++ Datastructures_&_Algorithms/Dynamic Programming/minFlipsMonoIncr.js
@@ -47,3 +47,8 @@ const s = '00110';
 // Output: 1
 // Explanation: We flip the last digit to get 00111.
 console.log(minFlipsMonoIncr(s));
+
+const s1 = '010110';
+// Output: 2
+// Explanation: We flip to get 011111, or alternatively 000111.
+console.log(minFlipsMonoIncr(s1));
