95a7a164e5f737468786a9b00c7a0bf702cb797f
yuhcee <egbouchenna001@gmail.com>
2023-09-05 23:55:04 +0100

    :sparkles: (minFlipsMonoIncr) add tak solution

--- Datastructures_&_Algorithms/Dynamic Programming/minFlipsMonoIncr.js
+++ Datastructures_&_Algorithms/Dynamic Programming/minFlipsMonoIncr.js
@@ -16,4 +16,30 @@
  * @param {string} s
  * @return {number}
  */
-const minFlipsMonoIncr = function (s) {};
+const minFlipsMonoIncr = function (s) {
+    let n = s.length;
+    // Initialize prefix and suffix arrays with zeros
+    let prefix = new Array(n).fill(0);
+    let suffix = new Array(n).fill(0);
+
+    // Compute the prefix count of 1's
+    for (let i = 1; i < n; i++) {
+        prefix[i] = prefix[i - 1] + (s[i - 1] === '1' ? 1 : 0);
+    }
+
+    // Compute the suffix count of 0's
+    for (let i = n - 2; i >= 0; i--) {
+        suffix[i] = suffix[i + 1] + (s[i + 1] === '0' ? 1 : 0);
+    }
+
+    // Calculate the minimum flips for each position
+    let minFlips = Math.min(suffix[0], prefix[n - 1]); // edge cases: all 0s or all 1s
+    for (let i = 1; i < n; i++) {
+        minFlips = Math.min(minFlips, prefix[i] + suffix[i]);
+    }
+
+    // If string has only 1s or only 0s
+    if (s.indexOf('1') === -1 || s.indexOf('0') === -1) return 0;
+
+    return minFlips;
+};
