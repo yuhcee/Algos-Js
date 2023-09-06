206d222c8a9e09e0d4d05d9d5035cc5b1f975a7c
yuhcee <egbouchenna001@gmail.com>
2023-09-07 00:06:16 +0100

    :white_check_mark: (countSubTrees) add testcase 3

--- Datastructures_&_Algorithms/Graphs/countSubTrees.js
+++ Datastructures_&_Algorithms/Graphs/countSubTrees.js
@@ -1,25 +1,32 @@
 /**
  * **1519. Number of Nodes in the Sub-Tree With the Same Label**
- * 
- * You are given a tree (i.e. a connected, undirected graph that has no cycles) consisting of n nodes numbered from 0 to n - 1 and exactly n - 1 edges. The root of the tree is the node 0, and each node of the tree has a label which is a lower-case character given in the string labels (i.e. The node with the number i has the label labels[i]).
-
-The edges array is given on the form edges[i] = [ai, bi], which means there is an edge between nodes ai and bi in the tree.
-
-Return an array of size n where ans[i] is the number of nodes in the subtree of the ith node which have the same label as node i.
-
-A subtree of a tree T is the tree consisting of a node in T and all of its descendant nodes.
- * 
-
-Constraints:
-
-1 <= n <= 105
-edges.length == n - 1
-edges[i].length == 2
-0 <= ai, bi < n
-ai != bi
-labels.length == n
-labels is consisting of only of lowercase English letters.
-
+ *
+ * You are given a tree (i.e. a connected, undirected graph that has no
+ * cycles) consisting of n nodes numbered from `0` to `n - 1` and exactly
+ * `n - 1` edges. The root of the tree is the node `0`, and each node of
+ * the tree has a label which is a lower-case character given in the string
+ * labels (i.e. The node with the number `i` has the label `labels[i]`).
+ *
+ * The edges array is given on the form `edges[i] = [ai, bi]`, which means
+ * there is an edge between nodes ai and bi in the tree.
+ *
+ * Return *an array of size `n`* where `ans[i]` is the number of nodes in
+ * the subtree of the `ith` node which have the same label as node `i`.
+ *
+ * A subtree of a tree `T` is the tree consisting of a node in `T` and all
+ * of its descendant nodes.
+ *
+ *
+ * **Constraints:**
+ *
+ * - `1 <= n <= 105`
+ * - `edges.length == n - 1`
+ * - `edges[i].length == 2`
+ * - `0 <= ai, bi < n`
+ * - `ai != bi`
+ * - `labels.length == n`
+ * - `labels` is consisting of only of lowercase English letters.
+ *
  * @param {number} n
  * @param {number[][]} edges
  * @param {string} labels
@@ -90,3 +97,14 @@ The sub-tree of node 3 contains only node 3, so the answer is 1.
 The sub-tree of node 1 contains nodes 1 and 2, both have label 'b', thus the answer is 2.
 The sub-tree of node 0 contains nodes 0, 1, 2 and 3, all with label 'b', thus the answer is 4. */
 console.log(countSubTrees(n1, edges1, labels1));
+
+const n2 = 5,
+    edges2 = [
+        [0, 1],
+        [0, 2],
+        [1, 3],
+        [0, 4],
+    ],
+    labels2 = 'aabab';
+// Output: [3,2,1,1,1]
+console.log(countSubTrees(n2, edges2, labels2));
