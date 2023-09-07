96f9a957e1c1c120f87672bb2b528aca090eaa0a
yuhcee <egbouchenna001@gmail.com>
2023-09-07 15:21:01 +0100

    :sparkles: (minDeletionSize) add tak solution

--- Datastructures_&_Algorithms/Strings/minDeletionSize.js
+++ Datastructures_&_Algorithms/Strings/minDeletionSize.js
@@ -32,4 +32,20 @@
  * @param {string[]} strs
  * @return {number}
  */
-const minDeletionSize = function (strs) {};
+const minDeletionSize = function (strs) {
+    let count = 0;
+    let row = strs.length;
+    let col = strs[0].length;
+
+    for (let i = 0; i < col; i++) {
+        for (let j = 0; j < row - 1; j++) {
+            if (strs[j][i] > strs[j + 1][i]) {
+                count++;
+
+                break;
+            }
+        }
+    }
+
+    return count;
+};
