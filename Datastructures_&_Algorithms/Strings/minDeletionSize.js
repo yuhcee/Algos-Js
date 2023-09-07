2f915f2c4d30d60cbc6d48408beb56e454d84f15
yuhcee <egbouchenna001@gmail.com>
2023-09-07 15:22:04 +0100

    :white_check_mark: (minDeletionSize) add testcase 1

--- Datastructures_&_Algorithms/Strings/minDeletionSize.js
+++ Datastructures_&_Algorithms/Strings/minDeletionSize.js
@@ -49,3 +49,12 @@ const minDeletionSize = function (strs) {
 
     return count;
 };
+
+const strs = ['cba', 'daf', 'ghi'];
+// Output: 1;
+/* Explanation: The grid looks as follows:
+  cba
+  daf
+  ghi
+Columns 0 and 2 are sorted, but column 1 is not, so you only need to delete 1 column. */
+console.log(minDeletionSize(strs));
