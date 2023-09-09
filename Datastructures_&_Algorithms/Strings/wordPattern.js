ffc531e2da490b6b67a1c6e66f25c6d089f8f5d0
yuhcee <egbouchenna001@gmail.com>
2023-09-09 23:41:55 +0100

    :heavy_plus_sign: (wordPattern) add constraints

--- Datastructures_&_Algorithms/Strings/wordPattern.js
+++ Datastructures_&_Algorithms/Strings/wordPattern.js
@@ -7,6 +7,15 @@
  * Here **follow** means a full match, such that there is a bijection
  * between a letter in `pattern` and a **non-empty** word in `s`.
  *
+ * **Constraints:**
+ *
+ * - `1 <= pattern.length <= 300`
+ * - `pattern` contains only lower-case English letters.
+ * - `1 <= s.length <= 3000`
+ * - `s` contains only lowercase English letters and spaces `' '`.
+ * - `s` **does not contain** any leading or trailing spaces.
+ * - All the words in `s` are separated by a **single space**.
+ *
  * @param {string} pattern
  * @param {string} s
  * @return {boolean}
