7ff42d6c57b15721ea01cf1d9c932fd033bb0855
yuhcee <egbouchenna001@gmail.com>
2023-09-10 20:03:43 +0100

    :white_check_mark: (countOrders) add testcase 2

--- Datastructures_&_Algorithms/Math/countOrders.js
+++ Datastructures_&_Algorithms/Math/countOrders.js
@@ -30,3 +30,10 @@ const n = 1;
 // Output: 1
 // Explanation: Unique order (P1, D1), Delivery 1 always is after of Pickup 1.
 console.log(countOrders(n));
+
+const n1 = 2;
+// Output: 6
+/* Explanation: All possible orders: 
+(P1,P2,D1,D2), (P1,P2,D2,D1), (P1,D1,P2,D2), (P2,P1,D1,D2), (P2,P1,D2,D1) and (P2,D2,P1,D1).
+This is an invalid order (P1,D2,P2,D1) because Pickup 2 is after of Delivery 2. */
+console.log(countOrders(n1));
