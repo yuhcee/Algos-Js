e6b73895d6ac09ae0a93fed8e761d2659dc8654f
yuhcee <egbouchenna001@gmail.com>
2023-09-10 20:01:17 +0100

    :books: (countOrders) add task description

---
+++ Datastructures_&_Algorithms/Math/countOrders.js
@@ -0,0 +1,14 @@
+/**
+ * **359. Count All Valid Pickup and Delivery Options**
+ * 
+ * Given `n` orders, each order consist in pickup and delivery services. 
+ * 
+ * Count all valid pickup/delivery possible sequences such that delivery(i) is 
+ * always after of pickup(i). 
+ * 
+ * Since the answer may be too large, return it modulo 10^9 + 7.
+ * 
+ * @param {number} n
+ * @return {number}
+ */
+const countOrders = function (n) {};
