c7b3d34410693a2f8ed3b2bccd7f2372c915b7d9
yuhcee <egbouchenna001@gmail.com>
2023-09-11 23:47:06 +0100

    :sparkles: (groupThePeople) add tak solution

--- Datastructures_&_Algorithms/HashMaps/groupThePeople.js
+++ Datastructures_&_Algorithms/HashMaps/groupThePeople.js
@@ -51,3 +51,12 @@ const groupThePeople = function (groupSizes) {
 
     return result;
 };
+
+const groupSizes = [3, 3, 3, 3, 3, 1, 3];
+// Output: [[5],[0,1,2],[3,4,6]]
+/* Explanation: 
+The first group is [5]. The size is 1, and groupSizes[5] = 1.
+The second group is [0,1,2]. The size is 3, and groupSizes[0] = groupSizes[1] = groupSizes[2] = 3.
+The third group is [3,4,6]. The size is 3, and groupSizes[3] = groupSizes[4] = groupSizes[6] = 3.
+Other possible solutions are [[2,1,6],[5],[0,4,3]] and [[5],[0,6,2],[4,3,1]]. */
+console.log(groupThePeople(groupSizes));
