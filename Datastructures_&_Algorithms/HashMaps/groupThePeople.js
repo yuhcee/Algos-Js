dc6c8f47d010317e25efd91d7ed983e55423dc7d
yuhcee <egbouchenna001@gmail.com>
2023-09-11 23:45:39 +0100

    :heavy_plus_sign: (groupThePeople) add constraints

--- Datastructures_&_Algorithms/HashMaps/groupThePeople.js
+++ Datastructures_&_Algorithms/HashMaps/groupThePeople.js
@@ -1,22 +1,27 @@
 /**
  * **1282. Group the People Given the Group Size They Belong To**
- * 
- * There are `n` people that are split into some unknown number of groups. Each 
+ *
+ * There are `n` people that are split into some unknown number of groups. Each
  * person is labeled with a **unique ID** from `0` to `n - 1`.
- * 
- * You are given an integer array `groupSizes`, where `groupSizes[i]` is the size 
- * of the group that person `i` is in. For example, if `groupSizes[1] = 3`, then 
+ *
+ * You are given an integer array `groupSizes`, where `groupSizes[i]` is the size
+ * of the group that person `i` is in. For example, if `groupSizes[1] = 3`, then
  * person `1` must be in a group of size `3`.
- * 
- * Return *a list of groups such that each person `i` is in a group of size 
+ *
+ * Return *a list of groups such that each person `i` is in a group of size
  * `groupSizes[i]`*.
- * 
- * Each person should appear in **exactly one group**, and every person must be 
- * in a group. If there are multiple answers, **return any of them**. It is 
- * **guaranteed** that there will be **at least one** valid solution for the 
+ *
+ * Each person should appear in **exactly one group**, and every person must be
+ * in a group. If there are multiple answers, **return any of them**. It is
+ * **guaranteed** that there will be **at least one** valid solution for the
  * given input.
- * 
- * 
+ *
+ * **Constraints:**
+ *
+ * - `groupSizes.length == n`
+ * - `1 <= n <= 500`
+ * - `1 <= groupSizes[i] <= n`
+ *
  * @param {number[]} groupSizes
  * @return {number[][]}
  */
