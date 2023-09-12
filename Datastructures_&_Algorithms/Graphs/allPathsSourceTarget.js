a7e7df641fdd741011c4131d5d4c5cab9e071b22
yuhcee <egbouchenna001@gmail.com>
2023-09-12 23:59:03 +0100

    :heavy_plus_sign: (allPathsSourceTarget) add constraints

--- Datastructures_&_Algorithms/Graphs/allPathsSourceTarget.js
+++ Datastructures_&_Algorithms/Graphs/allPathsSourceTarget.js
@@ -9,6 +9,15 @@
  * can visit from node `i` (i.e., there is a directed edge from node
  * `i` to node `graph[i][j]`).
  *
+ * Constraints:
+
+n == graph.length
+2 <= n <= 15
+0 <= graph[i][j] < n
+graph[i][j] != i (i.e., there will be no self-loops).
+All the elements of graph[i] are unique.
+The input graph is guaranteed to be a DAG.
+ * 
  * @param {number[][]} graph
  * @return {number[][]}
  */
