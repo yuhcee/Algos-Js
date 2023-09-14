9d9286ae4406df9951b38d7318ccfc84a843bf95
yuhcee <egbouchenna001@gmail.com>
2023-09-14 23:41:28 +0100

    :white_check_mark: (dailyTemperatures) add testcase 2

--- Datastructures_&_Algorithms/Stacks/dailyTemperatures.js
+++ Datastructures_&_Algorithms/Stacks/dailyTemperatures.js
@@ -15,10 +15,10 @@
  * @param {number[]} temperatures
  * @return {number[]}
  */
-const dailyTemperatures = function(temperatures) {
+const dailyTemperatures = function (temperatures) {
     const stack = [];
     const answer = new Array(temperatures.length).fill(0);
-    
+
     for (let i = 0; i < temperatures.length; i++) {
         while (stack.length && temperatures[i] > temperatures[stack[stack.length - 1]]) {
             const j = stack.pop();
@@ -26,10 +26,14 @@ const dailyTemperatures = function(temperatures) {
         }
         stack.push(i);
     }
-    
+
     return answer;
 };
 
-const temperatures = [73,74,75,71,69,72,76,73]
+const temperatures = [73, 74, 75, 71, 69, 72, 76, 73];
 // Output: [1,1,4,2,1,1,0,0]
 console.log(dailyTemperatures(temperatures));
+
+const temperatures1 = [30, 40, 50, 60];
+// Output: [1,1,1,0]
+console.log(dailyTemperatures(temperatures1));
