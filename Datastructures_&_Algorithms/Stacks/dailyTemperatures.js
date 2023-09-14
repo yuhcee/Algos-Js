aa8a37658d4346f7ba9907592b8b9dd23ab76e30
yuhcee <egbouchenna001@gmail.com>
2023-09-14 23:40:53 +0100

    :white_check_mark: (dailyTemperatures) add testcase 1

--- Datastructures_&_Algorithms/Stacks/dailyTemperatures.js
+++ Datastructures_&_Algorithms/Stacks/dailyTemperatures.js
@@ -29,3 +29,7 @@ const dailyTemperatures = function(temperatures) {
     
     return answer;
 };
+
+const temperatures = [73,74,75,71,69,72,76,73]
+// Output: [1,1,4,2,1,1,0,0]
+console.log(dailyTemperatures(temperatures));
