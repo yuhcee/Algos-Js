ecac7281e7012b1c83b33b790de3bc7ff862962f
yuhcee <egbouchenna001@gmail.com>
2023-09-15 23:43:25 +0100

    :sparkles: (maxProfit) add task solution

--- Datastructures_&_Algorithms/Dynamic Programming/maxProfit2.js
+++ Datastructures_&_Algorithms/Dynamic Programming/maxProfit2.js
@@ -23,4 +23,18 @@
  * @param {number[]} prices
  * @return {number}
  */
-const maxProfit = function (prices) {};
+const maxProfit = function (prices) {
+    if (prices == null || prices.length === 0) return 0;
+
+    let sold = 0;
+    let hold = -Infinity;
+    let rest = 0;
+
+    for (let p of prices) {
+        hold = Math.max(hold, rest - p);
+        rest = Math.max(rest, sold);
+        sold = hold + p;
+    }
+
+    return Math.max(sold, rest);
+};
