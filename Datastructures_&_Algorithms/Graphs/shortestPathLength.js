c8337ecb1ef29b1e64d287c4995d5a73a06cbeda
yuhcee <egbouchenna001@gmail.com>
2023-09-17 22:13:25 +0100

    :books: (shortestPathLength) add task description

---
+++ Datastructures_&_Algorithms/Graphs/shortestPathLength.js
@@ -0,0 +1,16 @@
+/**
+ * **847. Shortest Path Visiting All Nodes**
+ *
+ * You have an undirected, connected graph of `n` nodes labeled
+ * from `0` to `n - 1`. You are given an array `graph` where
+ * `graph[i]` is a list of all the nodes connected with node `i`
+ * by an edge.
+ *
+ * Return *the length of the shortest path that visits every
+ * node*. You may start and stop at any node, you may revisit
+ * nodes multiple times, and you may reuse edges.
+ *
+ * @param {number[][]} graph
+ * @return {number}
+ */
+const shortestPathLength = function (graph) {};
