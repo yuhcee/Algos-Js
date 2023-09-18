6759cad5f5b0e9c4890b4ec3b63cab4e52242195
yuhcee <egbouchenna001@gmail.com>
2023-09-18 23:41:06 +0100

    :white_check_mark:(kWeakestRows) add testcase 1

--- Datastructures_&_Algorithms/Arrays/kWeakestRows.js
+++ Datastructures_&_Algorithms/Arrays/kWeakestRows.js
@@ -34,3 +34,22 @@ const kWeakestRows = function (mat, k) {
     // Extract the first k indices
     return rows.slice(0, k).map((row) => row[0]);
 };
+
+const mat = [
+        [1, 1, 0, 0, 0],
+        [1, 1, 1, 1, 0],
+        [1, 0, 0, 0, 0],
+        [1, 1, 0, 0, 0],
+        [1, 1, 1, 1, 1],
+    ],
+    k = 3;
+// Output: [2,0,3]
+/* Explanation: 
+The number of soldiers in each row is: 
+- Row 0: 2 
+- Row 1: 4 
+- Row 2: 1 
+- Row 3: 2 
+- Row 4: 5 
+The rows ordered from weakest to strongest are [2,0,3,1,4]. */
+console.log(kWeakestRows(mat, k));
