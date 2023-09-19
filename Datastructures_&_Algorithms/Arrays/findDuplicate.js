19ff44921c4c61aa0809db64b2d7eda1d9aa2d83
yuhcee <egbouchenna001@gmail.com>
2023-09-19 22:22:58 +0100

    :books: (findDuplicate) add task description

---
+++ Datastructures_&_Algorithms/Arrays/findDuplicate.js
@@ -0,0 +1,16 @@
+/**
+ * **287. Find the Duplicate Number**
+ *
+ * Given an array of integers `nums` containing `n + 1` integers where
+ * each integer is in the range `[1, n]` inclusive.
+ *
+ * There is only **one repeated number** in `nums`, return *this
+ * repeated number*.
+ *
+ * You must solve the problem **without** modifying the array nums and
+ * uses only constant extra space.
+ *
+ * @param {number[]} nums
+ * @return {number}
+ */
+const findDuplicate = function (nums) {};
