93d047bb01e86f981df794a19e04102c803680c6
yuhcee <egbouchenna001@gmail.com>
2023-09-21 23:55:30 +0100

    :books: (findMedianSortedArrays) add task description

---
+++ Datastructures_&_Algorithms/Arrays/findMedianSortedArrays.js
@@ -0,0 +1,13 @@
+/**
+ * **4. Median of Two Sorted Arrays**
+ *
+ * Given two sorted arrays `nums1` and `nums2` of size m and n
+ * respectively, return ***the median** of the two sorted arrays*.
+ *
+ * The overall run time complexity should be `O(log (m+n))`.
+ *
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ */
+const findMedianSortedArrays = function (nums1, nums2) {};
