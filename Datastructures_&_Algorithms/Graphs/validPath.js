9523f7b782f5a4c778998b488ec68b96a0b78de5
yuhcee <egbouchenna001@gmail.com>
2023-09-23 21:53:25 +0100

    :white_check_mark:(validPath) add testcase 2

--- Datastructures_&_Algorithms/Graphs/validPath.js
+++ Datastructures_&_Algorithms/Graphs/validPath.js
@@ -80,3 +80,17 @@ const n = 3,
 - 0 → 1 → 2
 - 0 → 2 */
 console.log(validPath(n, edges, source, destination));
+
+const n1 = 6,
+    edges1 = [
+        [0, 1],
+        [0, 2],
+        [3, 5],
+        [5, 4],
+        [4, 3],
+    ],
+    source1 = 0,
+    destination1 = 5;
+// Output: false
+// Explanation: There is no path from vertex 0 to vertex 5.
+console.log(validPath(n1, edges1, source1, destination1));
