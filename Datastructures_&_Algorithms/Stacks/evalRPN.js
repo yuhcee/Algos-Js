57cfab8da86f73f452f43b45ff307e1c60358549
yuhcee <egbouchenna001@gmail.com>
2023-09-24 23:24:59 +0100

    :sparkles: (evalRPN) add task solution

--- Datastructures_&_Algorithms/Stacks/evalRPN.js
+++ Datastructures_&_Algorithms/Stacks/evalRPN.js
@@ -27,4 +27,36 @@
  * @param {string[]} tokens
  * @return {number}
  */
-const evalRPN = function (tokens) {};
+let operators = {
+    '+': function add(a, b) {
+        return a + b;
+    },
+    '-': function sub(a, b) {
+        return a - b;
+    },
+    '*': function mul(a, b) {
+        return a * b;
+    },
+    '/': function div(a, b) {
+        let result = a / b;
+        if (result < 0) {
+            return Math.ceil(result);
+        } else return Math.floor(result);
+    },
+};
+const evalRPN = function (tokens) {
+    let result = 0;
+    let stack1 = [];
+    while (tokens.length > 0) {
+        let item = tokens.shift();
+        if (operators[item]) {
+            let b = parseInt(stack1.pop());
+            let a = parseInt(stack1.pop());
+            result = operators[item](a, b);
+            stack1.push(result);
+        } else {
+            stack1.push(item);
+        }
+    }
+    return stack1.pop();
+};
