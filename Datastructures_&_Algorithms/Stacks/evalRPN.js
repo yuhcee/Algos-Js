6306f4f4d5873a8a1688f31a513599657380c5e2
yuhcee <egbouchenna001@gmail.com>
2023-09-24 22:57:51 +0100

    :heavy_plus_sign: (evalRPN) add constraints

--- Datastructures_&_Algorithms/Stacks/evalRPN.js
+++ Datastructures_&_Algorithms/Stacks/evalRPN.js
@@ -1,23 +1,29 @@
 /**
  * **150. Evaluate Reverse Polish Notation**
- * 
- * You are given an array of strings `tokens` that represents an 
+ *
+ * You are given an array of strings `tokens` that represents an
  * arithmetic expression in a Reverse Polish Notation.
- * 
- * Evaluate the expression. Return *an integer that represents the value 
+ *
+ * Evaluate the expression. Return *an integer that represents the value
  * of the expression*.
- * 
+ *
  * **Note** that:
- * 
+ *
  * - The valid operators are `'+'`, `'-'`, `'*'`, and `'/'`.
  * - Each operand may be an integer or another expression.
  * - The division between two integers always **truncates toward zero**.
  * - There will not be any division by zero.
- * - The input represents a valid arithmetic expression in a reverse 
+ * - The input represents a valid arithmetic expression in a reverse
  * polish notation.
- * - The answer and all the intermediate calculations can be represented 
+ * - The answer and all the intermediate calculations can be represented
  * in a **32-bit** integer.
- * 
+ *
+ * **Constraints:**
+ *
+ * - `1 <= tokens.length <= 104`
+ * - `tokens[i]` is either an operator: `"+"`, `"-"`, `"*"`, or `"/"`, or
+ * an integer in the range `[-200, 200]`.
+ *
  * @param {string[]} tokens
  * @return {number}
  */
