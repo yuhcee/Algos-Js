fd46dd7f4271a163180edc7c314a025ae0275cb1
yuhcee <egbouchenna001@gmail.com>
2023-09-26 23:13:14 +0100

    :heavy_plus_sign: (removeDuplicateLetters) add constraints

--- Datastructures_&_Algorithms/Stacks/removeDuplicateLetters.js
+++ Datastructures_&_Algorithms/Stacks/removeDuplicateLetters.js
@@ -5,6 +5,11 @@
  * once and only once. You must make sure your result is the smallest in 
  * lexicographical order among all possible results.
  * 
+ * **Constraints:**
+ * 
+ * - `1 <= s.length <= 104`
+ * - `s` consists of lowercase English letters.
+ * 
  * @param {string} s
  * @return {string}
  */
