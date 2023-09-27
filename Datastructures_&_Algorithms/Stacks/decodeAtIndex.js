838243939bd771384b4aefd15f9fee4d6cdbf580
yuhcee <egbouchenna001@gmail.com>
2023-09-27 18:43:12 +0100

    :books: (decodeAtIndex) add task description

---
+++ Datastructures_&_Algorithms/Stacks/decodeAtIndex.js
@@ -0,0 +1,20 @@
+/**
+ * **880. Decoded String at Index**
+ *
+ * You are given an encoded string `s`. To decode the string to a tape,
+ * the encoded string is read one character at a time and the following
+ * steps are taken:
+ *
+ * - If the character read is a letter, that letter is written onto the
+ * tape.
+ * - If the character read is a digit `d`, the entire current tape is
+ * repeatedly written `d - 1` more times in total.
+ *
+ * Given an integer `k`, return *the `kth` letter (**1-indexed**) in the
+ * decoded string.
+ *
+ * @param {string} s
+ * @param {number} k
+ * @return {string}
+ */
+const decodeAtIndex = function (s, k) {};
