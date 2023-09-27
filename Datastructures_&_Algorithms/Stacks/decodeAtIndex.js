744008e22f42800ca1970142b816e957aeee40de
yuhcee <egbouchenna001@gmail.com>
2023-09-27 18:44:49 +0100

    :heavy_plus_sign: (decodeAtIndex) add constraints

--- Datastructures_&_Algorithms/Stacks/decodeAtIndex.js
+++ Datastructures_&_Algorithms/Stacks/decodeAtIndex.js
@@ -13,6 +13,16 @@
  * Given an integer `k`, return *the `kth` letter (**1-indexed**) in the
  * decoded string.
  *
+ * **Constraints:**
+ *
+ * - `2 <= s.length <= 100`
+ * - `s` consists of lowercase English letters and digits `2` through `9`.
+ * - `s` starts with a letter.
+ * - `1 <= k <= 109`
+ * - It is guaranteed that `k` is less than or equal to the length of the
+ * decoded string.
+ * - The decoded string is guaranteed to have less than `263` letters.
+ *
  * @param {string} s
  * @param {number} k
  * @return {string}
