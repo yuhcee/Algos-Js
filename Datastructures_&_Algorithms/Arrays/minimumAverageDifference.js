f2991d1f854d4563ec827eaf7c15a7dc6e9cf65d
yuhcee <egbouchenna001@gmail.com>
2023-09-30 23:24:28 +0100

    :sparkles: (minimumAverageDifference) add task solution

--- Datastructures_&_Algorithms/Arrays/minimumAverageDifference.js
+++ Datastructures_&_Algorithms/Arrays/minimumAverageDifference.js
@@ -22,8 +22,30 @@
  * **Constraints:**
  * - `1 <= nums.length <= 105`
  * - `0 <= nums[i] <= 105`
- * 
+ *
  * @param {number[]} nums
  * @return {number}
  */
-const minimumAverageDifference = function (nums) {};
+const minimumAverageDifference = function (nums) {
+    const len = nums.length;
+    let leftSum = 0,
+        min = Infinity,
+        result = 0;
+    let rightSum = nums.reduce((acc, val) => acc + val);
+
+    for (let i = 0; i < len; i++) {
+        leftSum += nums[i];
+        rightSum -= nums[i];
+
+        const leftAvg = Math.floor(leftSum / (i + 1));
+        const rightAvg = Math.floor(rightSum / (len - i - 1) || 0);
+
+        const diff = Math.abs(leftAvg - rightAvg);
+        if (diff < min) {
+            min = diff;
+            result = i;
+        }
+    }
+
+    return result;
+};
