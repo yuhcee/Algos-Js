180ea4b5723f67f9b35af9b030cbc46aa0c5b5ab
yuhcee <egbouchenna001@gmail.com>
2023-10-03 10:08:57 +0100

    :books: (numIdenticalPairs) add task description

---
+++ Datastructures_&_Algorithms/HashMaps/numIdenticalPairs.js
@@ -0,0 +1,13 @@
+/**
+ * **1512. Number of Good Pairs**
+ *
+ * Given an array of integers `nums`, return *the number of
+ * **good pairs***.
+ *
+ * A pair `(i, j)` is called good if `nums[i] == nums[j]` and
+ * `i` < `j`.
+ *
+ * @param {number[]} nums
+ * @return {number}
+ */
+const numIdenticalPairs = function (nums) {};
