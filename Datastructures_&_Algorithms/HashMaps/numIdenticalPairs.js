8ee2b39ef34392d7283075e4ebaef1ce4a442e46
yuhcee <egbouchenna001@gmail.com>
2023-10-03 10:16:25 +0100

    :sparkles: (numIdenticalPairs) add task solution

--- Datastructures_&_Algorithms/HashMaps/numIdenticalPairs.js
+++ Datastructures_&_Algorithms/HashMaps/numIdenticalPairs.js
@@ -15,4 +15,20 @@
  * @param {number[]} nums
  * @return {number}
  */
-const numIdenticalPairs = function (nums) {};
+const numIdenticalPairs = function (nums) {
+    const countPairs = {};
+
+    for (let num of nums) {
+        countPairs[num] = 1 || countPairs[num]++;
+    }
+
+    let goodPairs = 0;
+
+    for (let count of Object.values(countPairs)) {
+        if (count > 1) {
+            goodPairs += (count * (count - 1)) / 2;
+        }
+    }
+
+    return goodPairs;
+};
