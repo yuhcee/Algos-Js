bc2e9b8d4fc9dce65b85eae08037c5ef8fccd1e4
yuhcee <egbouchenna001@gmail.com>
2023-10-05 19:20:05 +0100

    :heavy_plus_sign: (majorityElement) add constraints

--- Datastructures_&_Algorithms/Arrays/majorityElement.js
+++ Datastructures_&_Algorithms/Arrays/majorityElement.js
@@ -4,6 +4,11 @@
  * Given an integer array of size `n`, find all elements that
  * appear more than `⌊ n/3 ⌋` times.
  *
+ * **Constraints:**
+ *
+ * - `1 <= nums.length <= 5 * 104`
+ * - `-109 <= nums[i] <= 109`
+ *
  * @param {number[]} nums
  * @return {number[]}
  */
