d10427d774b9218df77ed2e935fedb18ea9c7b7b
yuhcee <egbouchenna001@gmail.com>
2023-10-05 19:20:55 +0100

    :sparkles: (majorityElement) add task solution

--- Datastructures_&_Algorithms/Arrays/majorityElement.js
+++ Datastructures_&_Algorithms/Arrays/majorityElement.js
@@ -12,4 +12,40 @@
  * @param {number[]} nums
  * @return {number[]}
  */
-const majorityElement = function (nums) {};
+const majorityElement = function (nums) {
+    let count1 = 0,
+        count2 = 0;
+    let candidate1 = null,
+        candidate2 = null;
+
+    // Find candidates
+    for (let num of nums) {
+        if (candidate1 === num) {
+            count1++;
+        } else if (candidate2 === num) {
+            count2++;
+        } else if (count1 === 0) {
+            candidate1 = num;
+            count1 = 1;
+        } else if (count2 === 0) {
+            candidate2 = num;
+            count2 = 1;
+        } else {
+            count1--;
+            count2--;
+        }
+    }
+
+    // Verify candidates
+    count1 = 0;
+    count2 = 0;
+    for (let num of nums) {
+        if (num === candidate1) count1++;
+        else if (num === candidate2) count2++;
+    }
+
+    let result = [];
+    if (count1 > nums.length / 3) result.push(candidate1);
+    if (count2 > nums.length / 3) result.push(candidate2);
+    return result;
+};
