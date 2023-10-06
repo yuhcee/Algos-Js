9e19792a0136c4d2a0e4d809444d2b801cd98860
yuhcee <egbouchenna001@gmail.com>
2023-10-06 23:01:35 +0100

    :books: (integerBreak) add task description

---
+++ Datastructures_&_Algorithms/Dynamic Programming/integerBreak.js
@@ -0,0 +1,12 @@
+/**
+ * **343. Integer Break**
+ *
+ * Given an integer `n`, break it into the sum of `k` **positive integers**,
+ * where `k >= 2`, and maximize the product of those integers.
+ *
+ * Return *the maximum product you can get*.
+ *
+ * @param {number} n
+ * @return {number}
+ */
+const integerBreak = function (n) {};
