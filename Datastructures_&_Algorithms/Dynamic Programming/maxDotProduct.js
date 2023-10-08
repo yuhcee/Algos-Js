207090d20d7eb8b7b4b9d29d331be00c805656c7
yuhcee <egbouchenna001@gmail.com>
2023-10-08 22:03:30 +0100

    :books: (maxDotProduct) add task description

---
+++ Datastructures_&_Algorithms/Dynamic Programming/maxDotProduct.js
@@ -0,0 +1,18 @@
+/**
+ * **1458. Max Dot Product of Two Subsequences**
+ *
+ * Given two arrays `nums1` and `nums2`.
+ *
+ * Return the maximum dot product between **non-empty** subsequences of
+ * `nums1` and `nums2` with the same length.
+ *
+ * A subsequence of a array is a new array which is formed from the original
+ * array by deleting some (can be none) of the characters without disturbing
+ * the relative positions of the remaining characters. (ie, `[2,3,5]` is a
+ * subsequence of `[1,2,3,4,5]` while `[1,5,3]` is not).
+ *
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ */
+const maxDotProduct = function (nums1, nums2) {};
