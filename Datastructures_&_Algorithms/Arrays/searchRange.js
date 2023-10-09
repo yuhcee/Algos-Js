fbaea4642802bc376de7fa279700f6a8b3d89685
yuhcee <egbouchenna001@gmail.com>
2023-10-09 23:55:51 +0100

    :white_check_mark: (searchRange) add test case 1

--- Datastructures_&_Algorithms/Arrays/searchRange.js
+++ Datastructures_&_Algorithms/Arrays/searchRange.js
@@ -1,22 +1,27 @@
 /**
  * **34. Find First and Last Position of Element in Sorted Array**
- * 
- * Given an array of integers nums sorted in non-decreasing order, find the 
+ *
+ * Given an array of integers nums sorted in non-decreasing order, find the
  * starting and ending position of a given target value.
- * 
+ *
  * If target is not found in the array, return [-1, -1].
- * 
+ *
  * You must write an algorithm with O(log n) runtime complexity.
- * 
+ *
  * **Constraints:**
- * 
+ *
  * - `0 <= nums.length <= 105`
  * - `-109 <= nums[i] <= 109`
  * - `nums` is a non-decreasing array.
  * - `-109 <= target <= 109`
- * 
+ *
  * @param {number[]} nums
  * @param {number} target
  * @return {number[]}
  */
-const searchRange = function(nums, target) {}
+const searchRange = function (nums, target) {};
+
+const nums = [5, 7, 7, 8, 8, 10],
+    target = 8;
+// Output: [3,4]
+console.log(searchRange(nums, target));
