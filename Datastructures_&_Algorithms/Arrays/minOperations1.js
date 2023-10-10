9e5d7c2aadb2542bf2de41f38fab276ac2e9a035
yuhcee <egbouchenna001@gmail.com>
2023-10-11 00:07:56 +0100

    :sparkles: (minOperations) add task solution

--- Datastructures_&_Algorithms/Arrays/minOperations1.js
+++ Datastructures_&_Algorithms/Arrays/minOperations1.js
@@ -1,25 +1,26 @@
 /**
  * **2009. Minimum Number of Operations to Make Array Continuous**
  *
- * You are given an integer array nums. In one operation, you can
- * replace any element in nums with any integer.
+ * You are given an integer array `nums`. In one operation, you can
+ * replace **any** element in `nums` with **any** integer.
  *
- * nums is considered continuous if both of the following conditions are
- * fulfilled:
+ * `nums` is considered **continuous** if both of the following
+ * conditions are fulfilled:
  *
- * All elements in nums are unique.
- * The difference between the maximum element and the minimum element in
- * nums equals nums.length - 1.
- * For example, nums = [4, 2, 5, 3] is continuous, but nums = [1, 2, 3,
- * 5, 6] is not continuous.
+ * - All elements in `nums` are unique.
+ * - The difference between the **maximum** element and the **minimum**
+ * element in `nums` equals `nums.length - 1`.
+ * For example, `nums = [4, 2, 5, 3]` is continuous, but nums = `[1, 2,
+ * 3, 5, 6]` is not continuous.
  *
- * Return the minimum number of operations to make nums continuous.
- * 
+ * Return *the **minimum** number of operations to make `nums`
+ * **continuous***.
+ *
  * **Constraints:**
-
-1 <= nums.length <= 105
-1 <= nums[i] <= 109
- * 
+ *
+ * - `1 <= nums.length <= 105`
+ * - `1 <= nums[i] <= 109`
+ *
  * @param {number[]} nums
  * @return {number}
  */
@@ -52,7 +53,6 @@ const nums1 = [1, 2, 3, 5, 6];
 The resulting array is [1,2,3,5,4], which is continuous. */
 console.log(minOperations(nums1));
 
-
 const nums2 = [1, 10, 100, 1000];
 // Output: 3
 /* Explanation: One possible solution is to:
@@ -61,4 +61,3 @@ const nums2 = [1, 10, 100, 1000];
 - Change the fourth element to 4.
 The resulting array is [1,2,3,4], which is continuous. */
 console.log(minOperations(nums2));
-
