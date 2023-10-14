6fe46a041654a34697c72365e654ae065f7f3ae2
yuhcee <egbouchenna001@gmail.com>
2023-10-14 23:54:15 +0100

    :sparkles: (paintWalls) add task solution

--- Datastructures_&_Algorithms/Dynamic Programming/paintWalls.js
+++ Datastructures_&_Algorithms/Dynamic Programming/paintWalls.js
@@ -24,7 +24,18 @@
  * @param {number[]} time
  * @return {number}
  */
-const paintWalls = function (cost, time) {};
+const paintWalls = function (cost, time) {
+    const n = cost.length;
+    const dp = new Array(n + 1).fill(Number.MAX_SAFE_INTEGER);
+    dp[0] = 0;
+
+    for (let i = 0; i < n; ++i) {
+        for (let j = n; j > 0; --j) {
+            dp[j] = Math.min(dp[j], dp[Math.max(j - time[i] - 1, 0)] + cost[i]);
+        }
+    }
+    return dp[n];
+};
 
 const cost = [1, 2, 3, 2],
     time = [1, 2, 3, 2];
