848dfc47f2b0bc2e15e17a2b5eb7346a0cc288e2
yuhcee <egbouchenna001@gmail.com>
2023-10-14 23:46:57 +0100

    :books: (paintWalls) add task description

---
+++ Datastructures_&_Algorithms/Dynamic Programming/paintWalls.js
@@ -0,0 +1,20 @@
+/**
+ * **2742. Painting the Walls**
+ *
+ * You are given two **0-indexed** integer arrays, `cost` and `time`, of
+ * size `n` representing the costs and the time taken to paint `n`
+ * different walls respectively. There are two painters available:
+ *
+ * - **A paid painter** that paints the `ith` wall in `time[i]` units of
+ * time and takes `cost[i]` units of money.
+ * - A **free painter** that paints **any** wall in `1` unit of time at
+ * a cost of `0`. But the free painter can only be used if the paid
+ * painter is already **occupied**.
+ *
+ * Return *the minimum amount of money required to paint the `n` walls*.
+ *
+ * @param {number[]} cost
+ * @param {number[]} time
+ * @return {number}
+ */
+const paintWalls = function (cost, time) {};
