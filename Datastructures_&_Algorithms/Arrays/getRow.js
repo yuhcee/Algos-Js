fa760578524af71ba52aa3dc91133ed634553c88
yuhcee <egbouchenna001@gmail.com>
2023-10-16 23:28:49 +0100

    :books: (getRow) add task description

---
+++ Datastructures_&_Algorithms/Arrays/getRow.js
@@ -0,0 +1,13 @@
+/**
+ * **119. Pascal's Triangle II**
+ * 
+ * Given an integer `rowIndex`, return the `rowIndexth` (**0-indexed**) 
+ * row of the **Pascal's triangle**.
+ * 
+ * In **Pascal's triangle**, each number is the sum of the two numbers 
+ * directly above it as shown:
+ * 
+ * @param {number} rowIndex
+ * @return {number[]}
+ */
+const getRow = function (rowIndex) {};
