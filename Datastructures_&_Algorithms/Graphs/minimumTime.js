89325e8c27713ef09d88e6b374f382151bb3b75a
yuhcee <egbouchenna001@gmail.com>
2023-10-18 23:11:36 +0100

    :white_check_mark: (minimumTime) add test case 2

--- Datastructures_&_Algorithms/Graphs/minimumTime.js
+++ Datastructures_&_Algorithms/Graphs/minimumTime.js
@@ -49,3 +49,21 @@ We start course 1 and course 2 simultaneously at month 0.
 Course 1 takes 3 months and course 2 takes 2 months to complete respectively.
 Thus, the earliest time we can start course 3 is at month 3, and the total time required is 3 + 5 = 8 months. */
 console.log(minimumTime(n, relations, time));
+
+const n1 = 5,
+    relations1 = [
+        [1, 5],
+        [2, 5],
+        [3, 5],
+        [3, 4],
+        [4, 5],
+    ],
+    time1 = [1, 2, 3, 4, 5];
+// Output: 12
+/* Explanation: The figure above represents the given graph and the time required to complete each course.
+You can start courses 1, 2, and 3 at month 0.
+You can complete them after 1, 2, and 3 months respectively.
+Course 4 can be taken only after course 3 is completed, i.e., after 3 months. It is completed after 3 + 4 = 7 months.
+Course 5 can be taken only after courses 1, 2, 3, and 4 have been completed, i.e., after max(1,2,3,7) = 7 months.
+Thus, the minimum time needed to complete all the courses is 7 + 5 = 12 months. */
+console.log(minimumTime(n1, relations1, time1));
