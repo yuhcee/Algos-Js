40f922ba90a76ea32d988504c4768ce531722952
yuhcee <egbouchenna001@gmail.com>
2023-10-18 23:03:01 +0100

    :books: (minimumTime) add task description

---
+++ Datastructures_&_Algorithms/Graphs/minimumTime.js
@@ -0,0 +1,26 @@
+/**
+ * **2050. Parallel Courses III**
+ *
+ * You are given an integer `n`, which indicates that there are `n` courses labeled from `1` to `n`.
+ * You are also given a 2D integer array `relations` where `relations[j] = [prevCoursej, nextCoursej]
+ * ` denotes that course `prevCoursej` has to be completed **before** course `nextCoursej`
+ * (prerequisite relationship). Furthermore, you are given a **0-indexed** integer array `time`
+ * where `time[i]` denotes how many **months** it takes to complete the `(i+1)th` course.
+ *
+ * You must find the minimum number of months needed to complete all the courses following these
+ * rules:
+ *
+ * - You may start taking a course at **any time** if the prerequisites are met.
+ * - **Any number of courses** can be taken at the **same time**.
+ *
+ * Return *the **minimum** number of months needed to complete all the courses.
+ *
+ * **Note:** The test cases are generated such that it is possible to complete every course (i.e.,
+ * the graph is a directed acyclic graph).
+ *
+ * @param {number} n
+ * @param {number[][]} relations
+ * @param {number[]} time
+ * @return {number}
+ */
+const minimumTime = function (n, relations, time) {};
