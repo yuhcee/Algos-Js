83739d68eeb7c99425bf6d1221c64314354c5231
yuhcee <egbouchenna001@gmail.com>
2023-10-19 23:58:07 +0100

    :books: (const backspaceCompare = function (s, t) {}; ) add task description

---
+++ Datastructures_&_Algorithms/Arrays/backspaceCompare.js
@@ -0,0 +1,13 @@
+/**
+ * **844. Backspace String Compare**
+ *
+ * Given two strings s and t, return true if they are equal when both are typed into empty text
+ * editors. '#' means a backspace character.
+ *
+ * Note that after backspacing an empty text, the text will continue empty.
+ *
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+const backspaceCompare = function (s, t) {};
