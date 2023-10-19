3f5b1c0114a816b007380c889247e31ae2e24a2c
yuhcee <egbouchenna001@gmail.com>
2023-10-20 00:05:44 +0100

    :white_check_mark: (backspaceCompare) add test case 3

--- Datastructures_&_Algorithms/Arrays/backspaceCompare.js
+++ Datastructures_&_Algorithms/Arrays/backspaceCompare.js
@@ -1,15 +1,15 @@
 /**
  * **844. Backspace String Compare**
  *
- * Given two strings s and t, return true if they are equal when both are typed into empty text
- * editors. '#' means a backspace character.
+ * Given two strings `s` and `t`, return `true` *if they are equal when both are typed into empty 
+ * text editors.* `'#'` means a backspace character.
  *
  * Note that after backspacing an empty text, the text will continue empty.
  *
  * **Constraints:**
-
-1 <= s.length, t.length <= 200
-s and t only contain lowercase letters and '#' characters.
+ * 
+ * - `1 <= s.length, t.length <= 200`
+ * - `s` and `t` only contain lowercase letters and `'#'` characters.
  * 
  * @param {string} s
  * @param {string} t
@@ -54,11 +54,20 @@ function getNextValidChar(str, index) {
     return index;
 }
 
-const s = "ab#c", t = "ad#c"
+const s = 'ab#c',
+    t = 'ad#c';
 // Output: true
 // Explanation: Both s and t become "ac".
+console.log(backspaceCompare(s, t));
 
-
-const s = "ab##", t = "c#d#"
+const s1 = 'ab##',
+    t1 = 'c#d#';
 // Output: true
 // Explanation: Both s and t become "".
+console.log(backspaceCompare(s1, t1));
+
+const s2 = 'a#c',
+    t2 = 'b';
+// Output: false
+// Explanation: s becomes "c" while t becomes "b".
+console.log(backspaceCompare(s2, t2));
