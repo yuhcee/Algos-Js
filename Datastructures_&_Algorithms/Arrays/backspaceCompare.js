171d0d141115d9db3b4dd3821e67c6ac203c0d6e
yuhcee <egbouchenna001@gmail.com>
2023-10-19 23:59:37 +0100

    :sparkles: (backspaceCompare) add task solution

--- Datastructures_&_Algorithms/Arrays/backspaceCompare.js
+++ Datastructures_&_Algorithms/Arrays/backspaceCompare.js
@@ -15,4 +15,41 @@ s and t only contain lowercase letters and '#' characters.
  * @param {string} t
  * @return {boolean}
  */
-const backspaceCompare = function (s, t) {};
+const backspaceCompare = function (s, t) {
+    let i = s.length - 1,
+        j = t.length - 1;
+
+    while (i >= 0 || j >= 0) {
+        i = getNextValidChar(s, i);
+        j = getNextValidChar(t, j);
+
+        if (i >= 0 && j >= 0 && s[i] !== t[j]) {
+            return false;
+        }
+
+        if (i >= 0 !== j >= 0) {
+            // One string is finished but the other is not
+            return false;
+        }
+
+        i--;
+        j--;
+    }
+
+    return true;
+};
+
+function getNextValidChar(str, index) {
+    let backspaceCount = 0;
+    while (index >= 0) {
+        if (str[index] === '#') {
+            backspaceCount++;
+        } else if (backspaceCount > 0) {
+            backspaceCount--;
+        } else {
+            break;
+        }
+        index--;
+    }
+    return index;
+}
