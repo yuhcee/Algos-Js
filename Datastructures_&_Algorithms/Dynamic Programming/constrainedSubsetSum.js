8b265bf5eb00cc181442ac2f724e05e0fbabf4fa
yuhcee <egbouchenna001@gmail.com>
2023-10-21 22:58:52 +0100

    :books: ( constrainedSubsetSum) add task description

---
+++ Datastructures_&_Algorithms/Dynamic Programming/constrainedSubsetSum.js
@@ -0,0 +1,16 @@
+/**
+ * **1425. Constrained Subsequence Sum**
+ *
+ *
+ * Given an integer array `nums` and an integer `k`, return the maximum sum of a **
+ * non-empty** subsequence of that array such that for every two **consecutive** integers in the
+ * subsequence, `nums[i]` and `nums[j]`, where `i < j`, the condition `j - i <= k` is satisfied.
+ *
+ * A *subsequence* of an array is obtained by deleting some number of elements (can be zero) from
+ * the array, leaving the remaining elements in their original order.
+ *
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+const constrainedSubsetSum = function (nums, k) {};
