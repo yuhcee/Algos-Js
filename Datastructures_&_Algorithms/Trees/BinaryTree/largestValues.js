fadb19a05cf72cea40646d69d59b795ee826493c
yuhcee <egbouchenna001@gmail.com>
2023-10-25 00:04:04 +0100

    :books: (largestValues) add task description

---
+++ Datastructures_&_Algorithms/Trees/BinaryTree/largestValues.js
@@ -0,0 +1,18 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * **515. Find Largest Value in Each Tree Row**
+ *
+ * Given the `root` of a binary tree, return *an array of the largest
+ * value in each row of the tree (**0-indexed**)*.
+ *
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+const largestValues = function (root) {};
