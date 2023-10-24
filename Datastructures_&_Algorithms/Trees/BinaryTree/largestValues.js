17061fbf6e77fc4d82e5872a52ffefb2cc5c1da9
yuhcee <egbouchenna001@gmail.com>
2023-10-25 00:05:21 +0100

    :heavy_plus_sign: (largestValues) add constraints

--- Datastructures_&_Algorithms/Trees/BinaryTree/largestValues.js
+++ Datastructures_&_Algorithms/Trees/BinaryTree/largestValues.js
@@ -12,6 +12,11 @@
  * Given the `root` of a binary tree, return *an array of the largest
  * value in each row of the tree (**0-indexed**)*.
  *
+ * **Constraints:**
+ *
+ * - The number of nodes in the tree will be in the range `[0, 104]`.
+ * - `-231 <= Node.val <= 231 - 1`
+ *
  * @param {TreeNode} root
  * @return {number[]}
  */
