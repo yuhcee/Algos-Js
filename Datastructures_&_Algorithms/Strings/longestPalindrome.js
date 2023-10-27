e56efebf66e72996e1b4cb335a50555215e78e82
yuhcee <egbouchenna001@gmail.com>
2023-10-27 14:05:10 +0100

    :sparkles: (longestPalindrome) add task solution

--- Datastructures_&_Algorithms/Strings/longestPalindrome.js
+++ Datastructures_&_Algorithms/Strings/longestPalindrome.js
@@ -10,9 +10,22 @@ const longestPalindrome = (s) => {
     let longest = s[0];
 
     for (let i = 0; i < s.length; i++) {
-        checkPalindrome(i, i); // Check for odd-length palindrome centered at i
+        // checkPalindrome(i, i); // Check for odd-length palindrome centered at i
         checkPalindrome(i, i + 1); // Check for even-length palindrome centered between i and i + 1
     }
+
+    function checkPalindrome(left, right) {
+        while (left >= 0 && right < s.length && s[left] === s[right]) {
+            left--;
+            right++;
+        }
+
+        if (right - left - 1 > longest.length) {
+            longest = s.substring(left + 1, right);
+        }
+    }
+
+    return longest;
 };
 
 const s = 'babad';
