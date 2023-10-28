d426de6061732a07efc3f1238440530348025165
yuhcee <egbouchenna001@gmail.com>
2023-10-28 22:54:10 +0100

    :books: (countVowelPermutation) add task description

---
+++ Datastructures_&_Algorithms/Dynamic Programming/countVowelPermutation.js
@@ -0,0 +1,20 @@
+/**
+ * **1220. Count Vowels Permutation**
+ *
+ * Given an integer `n`, your task is to count how many strings of
+ * length `n` can be formed under the following rules:
+ *
+ * - Each character is a lower case vowel (`'a'`, `'e'`, `'i'`, `'o'`,
+ * `'u'`)
+ * - Each vowel `'a'` may only be followed by an `'e'`.
+ * - Each vowel `'e'` may only be followed by an `'a'` or an `'i'`.
+ * - Each vowel `'i'` may not be followed by another `'i'`.
+ * - Each vowel `'o'` may only be followed by an `'i'` or a `'u'`.
+ * - Each vowel `'u'` may only be followed by an `'a'`.
+ *
+ * Since the answer may be too large, return it modulo `10^9 + 7`.
+ *
+ * @param {number} n
+ * @return {number}
+ */
+const countVowelPermutation = function (n) {};
