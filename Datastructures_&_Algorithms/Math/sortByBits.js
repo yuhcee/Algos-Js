59e9316807f304c61a923ec181d7bebafd477742
yuhcee <egbouchenna001@gmail.com>
2023-10-30 23:53:15 +0100

    :books: (sortByBits) add task description

---
+++ Datastructures_&_Algorithms/Math/sortByBits.js
@@ -0,0 +1,14 @@
+/**
+ * **1356. Sort Integers by The Number of 1 Bits**
+ *
+ * You are given an integer array `arr`. Sort the integers in the
+ * array in ascending order by the number of `1`'s in their binary
+ * representation and in case of two or more integers have the same
+ * number of `1`'s you have to sort them in ascending order.
+ *
+ * Return *the array after sorting it*.
+ *
+ * @param {number[]} arr
+ * @return {number[]}
+ */
+const sortByBits = function (arr) {};
