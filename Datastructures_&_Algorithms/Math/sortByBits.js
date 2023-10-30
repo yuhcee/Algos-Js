1c313b66acaa36d6adc8c1022863615e61a6383d
yuhcee <egbouchenna001@gmail.com>
2023-10-30 23:58:28 +0100

    :white_check_mark: (sortByBits) add test case 2

--- Datastructures_&_Algorithms/Math/sortByBits.js
+++ Datastructures_&_Algorithms/Math/sortByBits.js
@@ -45,3 +45,8 @@ const arr = [0, 1, 2, 3, 4, 5, 6, 7, 8];
 [7] has 3 bits.
 The sorted array by bits is [0,1,2,4,8,3,5,6,7] */
 console.log(sortByBits(arr));
+
+const arr1 = [1024, 512, 256, 128, 64, 32, 16, 8, 4, 2, 1];
+// Output: [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024];
+/* Explantion: All integers have 1 bit in the binary representation, you should just sort them in ascending order. */
+console.log(sortByBits(arr1));
