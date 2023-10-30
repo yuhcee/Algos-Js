de20ba222f930baf5e6c3789180da4ab68ab12bd
yuhcee <egbouchenna001@gmail.com>
2023-10-30 23:57:36 +0100

    :white_check_mark: (sortByBits) add test case 1

--- Datastructures_&_Algorithms/Math/sortByBits.js
+++ Datastructures_&_Algorithms/Math/sortByBits.js
@@ -36,3 +36,12 @@ const sortByBits = function (arr) {
         return bitCountA - bitCountB; // Sort by bit count
     });
 };
+
+const arr = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+// Output: [0,1,2,4,8,3,5,6,7]
+/* Explantion: [0] is the only integer with 0 bits.
+[1,2,4,8] all have 1 bit.
+[3,5,6] have 2 bits.
+[7] has 3 bits.
+The sorted array by bits is [0,1,2,4,8,3,5,6,7] */
+console.log(sortByBits(arr));
