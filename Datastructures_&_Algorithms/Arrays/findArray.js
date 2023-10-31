9de33e94b210c63eaaf045a927f5c062794288a1
yuhcee <egbouchenna001@gmail.com>
2023-10-31 23:50:44 +0100

    :books: (findArray) add task description

---
+++ Datastructures_&_Algorithms/Arrays/findArray.js
@@ -0,0 +1,16 @@
+/**
+ * **2433. Find The Original Array of Prefix Xor**
+ *
+ * You are given an **integer** array `pref` of size `n`. Find and
+ * return *the array `arr` of size `n` that satisfies:*
+ *
+ * - `pref[i] = arr[0] ^ arr[1] ^ ... ^ arr[i]`.
+ *
+ * Note that `^` denotes the **bitwise-xor** operation.
+ *
+ * It can be proven that the answer is **unique**.
+ *
+ * @param {number[]} pref
+ * @return {number[]}
+ */
+const findArray = function (pref) {};
