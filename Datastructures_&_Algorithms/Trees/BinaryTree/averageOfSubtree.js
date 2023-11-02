525abe65ec94bd0e2dc29441019b2dff76ab6f2f
yuhcee <egbouchenna001@gmail.com>
2023-11-02 22:49:56 +0100

    :books: (averageOfSubtree) add task description

---
+++ Datastructures_&_Algorithms/Trees/BinaryTree/averageOfSubtree.js
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * **2265. Count Nodes Equal to Average of Subtree**
+ *
+ * Given the `root` of a binary tree, return *the number of nodes
+ * where the value of the node is equal to the **average** of the
+ * values in its **subtree**.
+ *
+ * **Note**:
+ *
+ * - The average of `n` elements is the **sum** of the `n` elements
+ * divided by `n` and **rounded down** to the nearest integer.
+ * - A **subtree** of `root` is a tree consisting of `root` and all of
+ * its descendants.
+ *
+ * @param {TreeNode} root
+ * @return {number}
+ */
+const averageOfSubtree = function (root) {};
