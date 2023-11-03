be37ff2f1c86e22fb70af6a4a67890d125a42eb5
yuhcee <egbouchenna001@gmail.com>
2023-11-03 23:43:11 +0100

    :white_check_mark: (buildArray) add test case 3

--- Datastructures_&_Algorithms/Arrays/buildArray.js
+++ Datastructures_&_Algorithms/Arrays/buildArray.js
@@ -70,3 +70,13 @@ Read 1 from the stream and push it to the stack. s = [1].
 Read 2 from the stream and push it to the stack. s = [1,2].
 Read 3 from the stream and push it to the stack. s = [1,2,3]. */
 console.log(buildArray(target1, n1));
+
+const target2 = [1, 2],
+    n2 = 4;
+// Output: ["Push","Push"]
+/* Explanation: Initially the stack s is empty. The last element is the top of the stack.
+Read 1 from the stream and push it to the stack. s = [1].
+Read 2 from the stream and push it to the stack. s = [1,2].
+Since the stack (from the bottom to the top) is equal to target, we stop the stack operations.
+The answers that read integer 3 from the stream are not accepted. */
+console.log(buildArray(target2, n2));
