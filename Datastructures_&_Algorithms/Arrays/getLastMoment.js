6cdee16276c6e372fbdd99a679f7ac85bb556834
yuhcee <egbouchenna001@gmail.com>
2023-11-04 23:42:22 +0100

    :books: (getLastMoment) add task description

---
+++ Datastructures_&_Algorithms/Arrays/getLastMoment.js
@@ -0,0 +1,25 @@
+/**
+ * **1503. Last Moment Before All Ants Fall Out of a Plank**
+ *
+ * We have a wooden plank of the length `n` **units**. Some ants are
+ * walking on the plank, each ant moves with a speed of **1 unit per
+ * second**. Some of the ants move to the **left**, the other move to
+ * the **right**.
+ *
+ * When two ants moving in two **different** directions meet at some
+ * point, they change their directions and continue moving again.
+ * Assume changing directions does not take any additional time.
+ *
+ * When an ant reaches **one end** of the plank at a time `t`, it
+ * falls out of the plank immediately.
+ *
+ * Given an integer `n` and two integer arrays `left` and `right`,
+ * the positions of the ants moving to the left and the right, return
+ * *the moment when the last ant(s) fall out of the plank*.
+ *
+ * @param {number} n
+ * @param {number[]} left
+ * @param {number[]} right
+ * @return {number}
+ */
+const getLastMoment = function (n, left, right) {};
