1a30931d35c42317000b90ea9bf4356694dd1b2b
yuhcee <egbouchenna001@gmail.com>
2023-11-05 23:52:14 +0100

    :heavy_plus_sign: (getWinner) add constraints

--- Datastructures_&_Algorithms/Arrays/getWinner.js
+++ Datastructures_&_Algorithms/Arrays/getWinner.js
@@ -1,20 +1,26 @@
 /**
  * **1535. Find the Winner of an Array Game**
- * 
- * Given an integer array `arr` of **distinct** integers and an 
+ *
+ * Given an integer array `arr` of **distinct** integers and an
  * integer `k`.
- * 
- * A game will be played between the first two elements of the array 
- * (i.e. `arr[0]` and `arr[1]`). In each round of the game, we compare 
- * `arr[0]` with `arr[1]`, the larger integer wins and remains at 
- * position `0`, and the smaller integer moves to the end of the 
+ *
+ * A game will be played between the first two elements of the array
+ * (i.e. `arr[0]` and `arr[1]`). In each round of the game, we compare
+ * `arr[0]` with `arr[1]`, the larger integer wins and remains at
+ * position `0`, and the smaller integer moves to the end of the
  * array. The game ends when an integer wins `k` consecutive rounds.
- * 
+ *
  * Return *the integer which will win the game*.
- * 
+ *
  * It is **guaranteed that** there will be a winner of the game.
- * 
- * 
+ *
+ * **Constraints:**
+ *
+ * - `2 <= arr.length <= 105`
+ * - `1 <= arr[i] <= 106`
+ * - `arr` contains distinct integers.
+ * - `1 <= k <= 109`
+ *
  * @param {number[]} arr
  * @param {number} k
  * @return {number}
