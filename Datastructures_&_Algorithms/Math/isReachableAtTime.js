a43cf6633a0a1c6bd0817b0a94b32b4f7884cf8d
yuhcee <egbouchenna001@gmail.com>
2023-11-08 23:27:44 +0100

    :books: (isReachableAtTime) add task description

---
+++ Datastructures_&_Algorithms/Math/isReachableAtTime.js
@@ -0,0 +1,23 @@
+/**
+ * **2849. Determine if a Cell Is Reachable at a Given Time**
+ *
+ * You are given four integers `sx`, `sy`, `fx`, `fy`, and a
+ * **non-negative** integer `t`.
+ *
+ * In an infinite 2D grid, you start at the cell `(sx, sy)`. Each
+ * second, you **must** move to any of its adjacent cells.
+ *
+ * Return *`true` if you can reach cell `(fx, fy)` after exactly `t`
+ * seconds, or `false` otherwise*.
+ *
+ * A cell's **adjacent** cells are the 8 cells around it that share at
+ * least one corner with it. You can visit the same cell several times.
+ *
+ * @param {number} sx
+ * @param {number} sy
+ * @param {number} fx
+ * @param {number} fy
+ * @param {number} t
+ * @return {boolean}
+ */
+const isReachableAtTime = function (sx, sy, fx, fy, t) {};
