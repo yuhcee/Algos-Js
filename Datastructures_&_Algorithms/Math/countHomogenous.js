d42916067d90a00a815a54a3f336e18e895997ac
yuhcee <egbouchenna001@gmail.com>
2023-11-09 22:34:48 +0100

    :books: (countHomogenous) add task description

---
+++ Datastructures_&_Algorithms/Math/countHomogenous.js
@@ -0,0 +1,17 @@
+/**
+ * **1759. Count Number of Homogenous Substrings**
+ *
+ * Given a string `s`, return *the number of **homogenous** substrings
+ * of `s`. Since the answer may be too large, return it **modulo**
+ * `109 + 7`.
+ *
+ * A string is **homogenous** if all the characters of the string are
+ * the same.
+ *
+ * A **substring** is a contiguous sequence of characters within a
+ * string.
+ *
+ * @param {string} s
+ * @return {number}
+ */
+const countHomogenous = function (s) {};
