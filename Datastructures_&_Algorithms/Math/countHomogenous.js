e63dc0b27ab731f9c1722ad20ac5e0f19c61db0c
yuhcee <egbouchenna001@gmail.com>
2023-11-09 22:41:24 +0100

    :white_check_mark: (countHomogenous) add test case 1

--- Datastructures_&_Algorithms/Math/countHomogenous.js
+++ Datastructures_&_Algorithms/Math/countHomogenous.js
@@ -41,3 +41,16 @@ const countHomogenous = function (s) {
 
     return count; // Return the total count of homogenous substrings
 };
+
+const s = 'abbcccaa';
+// Output: 13
+/* Explanation: The homogenous substrings are listed as below:
+"a"   appears 3 times.
+"aa"  appears 1 time.
+"b"   appears 2 times.
+"bb"  appears 1 time.
+"c"   appears 3 times.
+"cc"  appears 2 times.
+"ccc" appears 1 time.
+3 + 1 + 2 + 1 + 3 + 2 + 1 = 13. */
+console.log(countHomogenous(s));
