b50b6be8d3d4bf507d420623e2aa2916ee07c52d
yuhcee <egbouchenna001@gmail.com>
2023-11-11 23:38:24 +0100

    :books: (Graph) add task description

---
+++ Datastructures_&_Algorithms/Graphs/Graph.js
@@ -0,0 +1,27 @@
+/**
+ * **2642. Design Graph With Shortest Path Calculator**
+ *
+ * There is a **directed weighted** graph that consists of `n` nodes
+ * numbered from `0` to `n - 1`. The edges of the graph are initially
+ * represented by the given array `edges` where `edges[i] = [fromi,
+ * toi, edgeCosti]` meaning that there is an edge from `fromi` to
+ * `toi` with the cost `edgeCosti`.
+ *
+ * Implement the `Graph` class:
+ *
+ * - `Graph(int n, int[][] edges)` initializes the object with `n`
+ * nodes and the given edges.
+ *
+ * - `addEdge(int[] edge)` adds an edge to the list of edges where
+ * `edge = [from, to, edgeCost]`. It is guaranteed that there is no
+ * edge between the two nodes before adding this one.
+ *
+ * - `int shortestPath(int node1, int node2)` returns the **minimum**
+ * cost of a path from `node1` to `node2`. If no path exists, return
+ * `-1`. The cost of a path is the sum of the costs of the edges in
+ * the path.
+ *
+ * @param {number} n
+ * @param {number[][]} edges
+ */
+const Graph = function (n, edges) {};
