2cac9bd74729cd60eaa3a38b738808ee18b0ad2b
yuhcee <egbouchenna001@gmail.com>
2023-11-12 23:33:55 +0100

    :sparkles: (numBusesToDestination) add task solution

--- Datastructures_&_Algorithms/HashSets/numBusesToDestination.js
+++ Datastructures_&_Algorithms/HashSets/numBusesToDestination.js
@@ -29,4 +29,42 @@
  * @param {number} target
  * @return {number}
  */
-const numBusesToDestination = function (routes, source, target) {};
+const numBusesToDestination = function (routes, source, target) {
+    if (source === target) return 0;
+
+    // Map each stop to the list of buses (routes) that visit it
+    const stopToBuses = new Map();
+    for (let i = 0; i < routes.length; i++) {
+        for (const stop of routes[i]) {
+            if (!stopToBuses.has(stop)) {
+                stopToBuses.set(stop, []);
+            }
+            stopToBuses.get(stop).push(i);
+        }
+    }
+
+    // BFS
+    const visitedBuses = new Set(); // Buses we have already taken
+    const visitedStops = new Set(); // Stops we have already visited
+    const queue = [[source, 0]]; // [currentStop, busesTaken]
+    visitedStops.add(source);
+
+    while (queue.length > 0) {
+        const [currentStop, busesTaken] = queue.shift();
+        const buses = stopToBuses.get(currentStop) || [];
+
+        for (const bus of buses) {
+            if (visitedBuses.has(bus)) continue;
+            visitedBuses.add(bus);
+
+            for (const stop of routes[bus]) {
+                if (visitedStops.has(stop)) continue;
+                if (stop === target) return busesTaken + 1; // Found the target
+                visitedStops.add(stop);
+                queue.push([stop, busesTaken + 1]);
+            }
+        }
+    }
+
+    return -1; // If we're here, it's not possible to reach the target
+};
