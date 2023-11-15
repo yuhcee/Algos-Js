709bacd97e6741bacc7e74bd7dad5d27ee96b0a6
yuhcee <egbouchenna001@gmail.com>
2023-11-15 23:58:08 +0100

    :sparkles: (maximumElementAfterDecrementingAndRearranging) add task solution

--- Datastructures_&_Algorithms/Greedy Algorithms/maximumElementAfterDecrementingAndRearranging.js
+++ Datastructures_&_Algorithms/Greedy Algorithms/maximumElementAfterDecrementingAndRearranging.js
@@ -30,4 +30,15 @@
  * @param {number[]} arr
  * @return {number}
  */
-const maximumElementAfterDecrementingAndRearranging = function (arr) {};
+const maximumElementAfterDecrementingAndRearranging = function (arr) {
+    arr.sort((a, b) => a - b);
+    arr[0] = 1;
+
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] - arr[i - 1] > 1) {
+            arr[i] = arr[i - 1] + 1;
+        }
+    }
+
+    return arr[arr.length - 1];
+};
