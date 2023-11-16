5691259f9058de6b470e14d361739155cdb3d5e1
yuhcee <egbouchenna001@gmail.com>
2023-11-16 23:48:11 +0100

    :books: (findDifferentBinaryString) add task description

---
+++ Datastructures_&_Algorithms/Strings/findDifferentBinaryString.js
@@ -0,0 +1,12 @@
+/**
+ * **1980. Find Unique Binary String**
+ *
+ * Given an array of strings `nums` containing `n` **unique** binary 
+ * strings each of length n, return *a binary string of length n that 
+ * **does not appear** in `nums`. If there are multiple answers, you 
+ * may return **any** of them*.
+ *
+ * @param {string[]} nums
+ * @return {string}
+ */
+const findDifferentBinaryString = function (nums) {};
