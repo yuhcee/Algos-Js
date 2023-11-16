d9f87e71a717d24365961221106c177a23cf6705
yuhcee <egbouchenna001@gmail.com>
2023-11-16 23:49:50 +0100

    :sparkles: (findDifferentBinaryString) add task solution

--- Datastructures_&_Algorithms/Strings/findDifferentBinaryString.js
+++ Datastructures_&_Algorithms/Strings/findDifferentBinaryString.js
@@ -17,4 +17,13 @@
  * @param {string[]} nums
  * @return {string}
  */
-const findDifferentBinaryString = function (nums) {};
+const findDifferentBinaryString = function (nums) {
+    let result = '';
+
+    for (let i = 0; i < nums.length; i++) {
+        // Flip the ith character of the ith string (0 to 1 or 1 to 0)
+        result += nums[i][i] === '0' ? '1' : '0';
+    }
+
+    return result;
+};
