03dc05b185e1128bbe4528b5eb636012653e224e
yuhcee <egbouchenna001@gmail.com>
2023-11-17 23:31:22 +0100

    :white_check_mark: (minPairSum) add test case 2

--- Datastructures_&_Algorithms/Greedy Algorithms/minPairSum.js
+++ Datastructures_&_Algorithms/Greedy Algorithms/minPairSum.js
@@ -50,3 +50,9 @@ const nums = [3, 5, 2, 3];
 /* Explanation: The elements can be paired up into pairs (3,3) and (5,2).
 The maximum pair sum is max(3+3, 5+2) = max(6, 7) = 7. */
 console.log(minPairSum(nums));
+
+const nums1 = [3, 5, 4, 2, 4, 6];
+// Output: 8
+/* Explanation: The elements can be paired up into pairs (3,5), (4,4), and (6,2).
+The maximum pair sum is max(3+5, 4+4, 6+2) = max(8, 8, 8) = 8. */
+console.log(minPairSum(nums1));
