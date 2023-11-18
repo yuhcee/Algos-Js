e056f3e2428f8d110e42e2230491b87ef50cc495
yuhcee <egbouchenna001@gmail.com>
2023-11-18 23:57:02 +0100

    :white_check_mark: (maxFrequency) add test case 2

--- Datastructures_&_Algorithms/Greedy Algorithms/maxFrequency.js
+++ Datastructures_&_Algorithms/Greedy Algorithms/maxFrequency.js
@@ -50,3 +50,12 @@ const nums = [1, 2, 4],
 /* Explanation: Increment the first element three times and the second element two times to make nums = [4,4,4].
 4 has a frequency of 3. */
 console.log(maxFrequency(nums, k));
+
+const nums1 = [1, 4, 8, 13],
+    k1 = 5;
+// Output: 2
+/* Explanation: There are multiple optimal solutions:
+- Increment the first element three times to make nums = [4,4,8,13]. 4 has a frequency of 2.
+- Increment the second element four times to make nums = [1,8,8,13]. 8 has a frequency of 2.
+- Increment the third element five times to make nums = [1,4,13,13]. 13 has a frequency of 2. */
+console.log(maxFrequency(nums1, k1));
