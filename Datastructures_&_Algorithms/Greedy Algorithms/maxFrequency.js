25cf1a47df2ab4267bad8fa0848cf6c495007f1c
yuhcee <egbouchenna001@gmail.com>
2023-11-18 23:53:12 +0100

    :books: (maxFrequency) add task description

---
+++ Datastructures_&_Algorithms/Greedy Algorithms/maxFrequency.js
@@ -0,0 +1,19 @@
+/**
+ * **1838. Frequency of the Most Frequent Element**
+ *
+ *
+ * The **frequency** of an element is the number of times it occurs in
+ * an array.
+ *
+ * You are given an integer array `nums` and an integer `k`. In one
+ * operation, you can choose an index of `nums` and increment the
+ * element at that index by `1`.
+ *
+ * Return *the **maximum possible frequency** of an element after
+ * performing **at most** `k` operations.
+ *
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+const maxFrequency = function (nums, k) {};
