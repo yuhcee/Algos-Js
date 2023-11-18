bb8bf5e4a6fcdd2bd90032a8ab37dcdc99b72b1a
yuhcee <egbouchenna001@gmail.com>
2023-11-18 23:55:18 +0100

    :sparkles: (maxFrequency) add task solution

--- Datastructures_&_Algorithms/Greedy Algorithms/maxFrequency.js
+++ Datastructures_&_Algorithms/Greedy Algorithms/maxFrequency.js
@@ -22,4 +22,24 @@
  * @param {number} k
  * @return {number}
  */
-const maxFrequency = function (nums, k) {};
+const maxFrequency = function (nums, k) {
+    nums.sort((a, b) => a - b);
+
+    let left = 0;
+    let maxFreq = 0;
+    let sum = 0;
+
+    for (let right = 0; right < nums.length; right++) {
+        sum += nums[right];
+
+        // Check if the sum of differences in the current window is more than k
+        while (nums[right] * (right - left + 1) - sum > k) {
+            sum -= nums[left];
+            left++;
+        }
+
+        maxFreq = Math.max(maxFreq, right - left + 1);
+    }
+
+    return maxFreq;
+};
