64e5618815635256fd93c0cf65a0776d6bbb2111
yuhcee <egbouchenna001@gmail.com>
2023-11-21 00:02:26 +0100

    :white_check_mark: (garbageCollection) add test case 2

--- Datastructures_&_Algorithms/Arrays/garbageCollection.js
+++ Datastructures_&_Algorithms/Arrays/garbageCollection.js
@@ -69,3 +69,25 @@ The paper garbage truck takes 15 minutes to pick up all the paper garbage.
 The glass garbage truck takes 15 minutes to pick up all the glass garbage.
 It takes a total of 7 + 15 + 15 = 37 minutes to collect all the garbage. */
 console.log(garbageCollection(garbage, travel));
+
+const garbage1 = ['G', 'P', 'GP', 'GG'],
+    travel1 = [2, 4, 3];
+// Output: 21
+/* Explanation:
+The paper garbage truck:
+1. Travels from house 0 to house 1
+2. Collects the paper garbage at house 1
+3. Travels from house 1 to house 2
+4. Collects the paper garbage at house 2
+Altogether, it takes 8 minutes to pick up all the paper garbage.
+The glass garbage truck:
+1. Collects the glass garbage at house 0
+2. Travels from house 0 to house 1
+3. Travels from house 1 to house 2
+4. Collects the glass garbage at house 2
+5. Travels from house 2 to house 3
+6. Collects the glass garbage at house 3
+Altogether, it takes 13 minutes to pick up all the glass garbage.
+Since there is no metal garbage, we do not need to consider the metal garbage truck.
+Therefore, it takes a total of 8 + 13 = 21 minutes to collect all the garbage. */
+console.log(garbageCollection(garbage1, travel1));
