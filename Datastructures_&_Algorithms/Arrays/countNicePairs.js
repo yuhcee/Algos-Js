65bf661fb3474623e2690ba2f02a43d9edc2a9b7
yuhcee <egbouchenna001@gmail.com>
2023-11-21 23:47:39 +0100

    :books: (countNicePairs) add task description

---
+++ Datastructures_&_Algorithms/Arrays/countNicePairs.js
@@ -0,0 +1,19 @@
+/**
+ * **1814. Count Nice Pairs in an Array**
+ *
+ * You are given an array `nums` that consists of non-negative
+ * integers. Let us define `rev(x)` as the reverse of the non-negative
+ * integer `x`. For example, `rev(123) = 321`, and `rev(120) = 21`. A
+ * pair of indices `(i, j)` is nice if it satisfies all of the
+ * following conditions:
+ *
+ * - `0 <= i < j < nums.length`
+ * - `nums[i] + rev(nums[j]) == nums[j] + rev(nums[i])`
+ *
+ * Return *the number of nice pairs of indices*. Since that number can
+ * be too large, return it **modulo** `109 + 7`.
+ *
+ * @param {number[]} nums
+ * @return {number}
+ */
+const countNicePairs = function (nums) {};
