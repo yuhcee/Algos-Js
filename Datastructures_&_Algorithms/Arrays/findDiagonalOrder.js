eefb3b76a22639563ed3b8ffbc0bd270bf223711
yuhcee <egbouchenna001@gmail.com>
2023-11-22 17:38:41 +0100

    :heavy_plus_sign: (findDiagonalOrder) add constraints

--- Datastructures_&_Algorithms/Arrays/findDiagonalOrder.js
+++ Datastructures_&_Algorithms/Arrays/findDiagonalOrder.js
@@ -4,6 +4,13 @@
  * Given a 2D integer array `nums`, return *all elements of `nums` in
  * diagonal order as shown in the below images.
  *
+ * **Constraints:**
+ *
+ * - `1 <= nums.length <= 105`
+ * - `1 <= nums[i].length <= 105`
+ * - `1 <= sum(nums[i].length) <= 105`
+ * - `1 <= nums[i][j] <= 105`
+ *
  * @param {number[][]} nums
  * @return {number[]}
  */
