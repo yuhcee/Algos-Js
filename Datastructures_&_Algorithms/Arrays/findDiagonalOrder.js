96ce22a0cc346f2fb7abe3a19d592fab08875c25
yuhcee <egbouchenna001@gmail.com>
2023-11-22 17:40:37 +0100

    :white_check_mark: (findDiagonalOrder) add test case 2

--- Datastructures_&_Algorithms/Arrays/findDiagonalOrder.js
+++ Datastructures_&_Algorithms/Arrays/findDiagonalOrder.js
@@ -46,3 +46,7 @@ const nums = [
 ];
 // Output: [1,4,2,7,5,3,8,6,9]
 console.log(findDiagonalOrder(nums));
+
+const nums1 = [[1, 2, 3, 4, 5], [6, 7], [8], [9, 10, 11], [12, 13, 14, 15, 16]];
+// Output: [1,6,2,8,7,3,9,4,12,10,5,13,11,14,15,16]
+console.log(findDiagonalOrder(nums1));
