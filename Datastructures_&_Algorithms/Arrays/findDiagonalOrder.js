9970caa02ee5c9b9872ba4e0897a1703bb8a6e2f
yuhcee <egbouchenna001@gmail.com>
2023-11-22 17:31:31 +0100

    :books: (findDiagonalOrder) add task description

---
+++ Datastructures_&_Algorithms/Arrays/findDiagonalOrder.js
@@ -0,0 +1,10 @@
+/**
+ * **1424. Diagonal Traverse II**
+ *
+ * Given a 2D integer array `nums`, return *all elements of `nums` in
+ * diagonal order as shown in the below images.
+ *
+ * @param {number[][]} nums
+ * @return {number[]}
+ */
+const findDiagonalOrder = function (nums) {};
