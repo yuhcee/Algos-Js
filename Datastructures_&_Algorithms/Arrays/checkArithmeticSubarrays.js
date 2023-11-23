25ba878df01897fe31b2920a2392ba5c17179577
yuhcee <egbouchenna001@gmail.com>
2023-11-23 23:59:09 +0100

    :white_check_mark: (checkArithmeticSubarrays) add test case 2

--- Datastructures_&_Algorithms/Arrays/checkArithmeticSubarrays.js
+++ Datastructures_&_Algorithms/Arrays/checkArithmeticSubarrays.js
@@ -69,3 +69,9 @@ In the 0th query, the subarray is [4,6,5]. This can be rearranged as [6,5,4], wh
 In the 1st query, the subarray is [4,6,5,9]. This cannot be rearranged as an arithmetic sequence.
 In the 2nd query, the subarray is [5,9,3,7]. This can be rearranged as [3,5,7,9], which is an arithmetic sequence. */
 console.log(checkArithmeticSubarrays(nums, l, r));
+
+const nums1 = [-12, -9, -3, -12, -6, 15, 20, -25, -20, -15, -10],
+    l1 = [0, 1, 6, 4, 8, 7],
+    r1 = [4, 4, 9, 7, 9, 10];
+// Output: [false,true,false,false,true,true]
+console.log(checkArithmeticSubarrays(nums1, l1, r1));
