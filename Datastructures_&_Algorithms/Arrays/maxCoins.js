347a84c15391ca4e38ba529f886e7c5a7493fe56
yuhcee <egbouchenna001@gmail.com>
2023-11-24 23:53:09 +0100

    :white_check_mark: (maxCoins) add test case 1

--- Datastructures_&_Algorithms/Arrays/maxCoins.js
+++ Datastructures_&_Algorithms/Arrays/maxCoins.js
@@ -38,3 +38,11 @@ const maxCoins = function (piles) {
 
     return maxCoins;
 };
+
+const piles = [2, 4, 1, 2, 7, 8];
+// Output: 9
+/* Explanation: Choose the triplet (2, 7, 8), Alice Pick the pile with 8 coins, you the pile with 7 coins and Bob the last one.
+Choose the triplet (1, 2, 4), Alice Pick the pile with 4 coins, you the pile with 2 coins and Bob the last one.
+The maximum number of coins which you can have are: 7 + 2 = 9.
+On the other hand if we choose this arrangement (1, 2, 8), (2, 4, 7) you only get 2 + 4 = 6 coins which is not optimal. */
+console.log(maxCoins(piles));
