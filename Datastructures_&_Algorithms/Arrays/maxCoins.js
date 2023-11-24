0059f34658772eb567905dc41643182bc4dd2478
yuhcee <egbouchenna001@gmail.com>
2023-11-24 23:49:13 +0100

    :books: (maxCoins) add task description

---
+++ Datastructures_&_Algorithms/Arrays/maxCoins.js
@@ -0,0 +1,20 @@
+/**
+ * **1561. Maximum Number of Coins You Can Get**
+ *
+ * There are `3n` piles of coins of varying size, you and your friends will take piles of coins as
+ * follows:
+ *
+ * - In each step, you will choose **any** `3` piles of coins (not necessarily consecutive).
+ * - Of your choice, Alice will pick the pile with the maximum number of coins.
+ * - You will pick the next pile with the maximum number of coins.
+ * - Your friend Bob will pick the last pile.
+ * - Repeat until there are no more piles of coins.
+ *
+ * Given an array of integers `piles` where `piles[i]` is the number of coins in the `ith` pile.
+ *
+ * Return *the maximum number of coins that you can have*.
+ *
+ * @param {number[]} piles
+ * @return {number}
+ */
+const maxCoins = function (piles) {};
