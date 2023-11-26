42c4247da094fac68f5e032075d5d7fbee2467ec
yuhcee <egbouchenna001@gmail.com>
2023-11-26 23:21:37 +0100

    :books: (largestSubmatrix) add task description

---
+++ Datastructures_&_Algorithms/Arrays/largestSubmatrix.js
@@ -0,0 +1,14 @@
+/**
+ * **1727. Largest Submatrix With Rearrangements**
+ *
+ * You are given a binary matrix `matrix` of size `m x n`, and you are
+ * allowed to rearrange the columns of the `matrix` in any order.
+ *
+ * Return *the area of the largest submatrix within `matrix` where
+ * **every** element of the submatrix is `1` after reordering the columns
+ * optimally.
+ *
+ * @param {number[][]} matrix
+ * @return {number}
+ */
+const largestSubmatrix = function (matrix) {};
