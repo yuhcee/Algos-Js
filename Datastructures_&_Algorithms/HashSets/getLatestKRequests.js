91152ac287faac818c55f7f4eb530380aba22638
yuhcee <egbouchenna001@gmail.com>
2023-11-29 17:10:26 +0100

    :books: (getLatestKRequests) add task description

---
+++ Datastructures_&_Algorithms/HashSets/getLatestKRequests.js
@@ -0,0 +1,34 @@
+/**
+ * **Get Latest K Requests**
+ *
+ *
+ * Given `n` request ids as an array of strings, `requests`, and an
+ * integer `k`, after all requests are received, find the `k` most recent
+ * requests.
+ *
+ * Report the answer in order of most recent to least recent.
+ *
+ * Example:
+ *
+ * Suppose n = 5, requests - ['item 1', 'item2', 'item3', 'item 1',
+ * 'Item3'] and k = 3.
+ *
+ * Starting from the right and moving left, collecting distinct requests,
+ * there is `"item3"` followed by `"item1"`. Skip the second instance of
+ * "item3" and find "item2". The answer is ['item3*, "item1*, item27.
+ *
+ * **Function Description**
+ *
+ * Complete the function `getLatestKRequests` in the editor below.
+ *
+ * `getLatestKRequests` takes the following arguments:
+ * - str `requests(n)`: the request ids
+ * - int `k`: the number of requests to report
+ *
+ * **Returns**
+ * str[k]: the `k` most recent requests
+ *
+ * @param {arr} requests
+ * @param {num} K
+ */
+const getLatestKRequests = (requests, K) => {};
