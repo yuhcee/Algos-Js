12dba97ff9b5515cd4e669fbaa150bbec67b146f
yuhcee <egbouchenna001@gmail.com>
2023-11-29 17:20:44 +0100

    :white_check_mark: (getLatestKRequests) add test case 1

--- Datastructures_&_Algorithms/HashSets/getLatestKRequests.js
+++ Datastructures_&_Algorithms/HashSets/getLatestKRequests.js
@@ -11,11 +11,11 @@
  * Example:
  *
  * Suppose n = 5, requests - ['item 1', 'item2', 'item3', 'item 1',
- * 'Item3'] and k = 3.
+ * 'item3'] and k = 3.
  *
  * Starting from the right and moving left, collecting distinct requests,
  * there is `"item3"` followed by `"item1"`. Skip the second instance of
- * "item3" and find "item2". The answer is ['item3*, "item1*, item27.
+ * "item3" and find "item2". The answer is ['item3', "item1"", 'item2'].
  *
  * **Function Description**
  *
@@ -53,3 +53,7 @@ const getLatestKRequests = (requests, K) => {
 
     return latestKRequests;
 };
+
+const requests = ['item 1', 'item2', 'item3', 'item 1', 'item3'],
+    K = 3;
+console.log(getLatestKRequests(requests, K));
