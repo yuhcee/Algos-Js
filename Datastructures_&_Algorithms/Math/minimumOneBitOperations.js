20e2197c2049249ffa40fc7c6e5115d5ac8f2e4b
yuhcee <egbouchenna001@gmail.com>
2023-11-30 23:58:33 +0100

    :sparkles: (minimumOneBitOperations) add task solution

--- Datastructures_&_Algorithms/Math/minimumOneBitOperations.js
+++ Datastructures_&_Algorithms/Math/minimumOneBitOperations.js
@@ -18,4 +18,14 @@
  * @param {number} n
  * @return {number}
  */
-const minimumOneBitOperations = function (n) {};
+const minimumOneBitOperations = function (n) {
+    if (n === 0) return 0;
+
+    let bit = 0;
+    // Find the most significant bit
+    while (1 << bit <= n) {
+        bit++;
+    }
+
+    return (1 << bit) - 1 - minimumOneBitOperations(n ^ (1 << (bit - 1)));
+};
