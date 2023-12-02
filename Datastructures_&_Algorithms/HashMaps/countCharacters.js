f1f45a0dcf95e3a398582609afda52378a90b9b2
yuhcee <egbouchenna001@gmail.com>
2023-12-02 22:34:29 +0100

    :books: (countCharacters) add task description

--- Datastructures_&_Algorithms/HashMaps/countCharacters.js
+++ Datastructures_&_Algorithms/HashMaps/countCharacters.js
@@ -2,7 +2,8 @@
  * **1160. Find Words That Can Be Formed by Characters**
  *
  * You are given an array of strings `words` and a string `chars`.
- * A string is **good** if it can be formed by characters from chars (each character can only be used once)
+ * A string is **good** if it can be formed by characters from chars (each character can only be 
+ * used once)
  *
  * *Return the sum of lengths of all good strings in words.*
  *
