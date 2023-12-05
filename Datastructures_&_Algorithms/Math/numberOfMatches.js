d163a2638f1ee20e9b34a9cc662812a81eedb72e
yuhcee <egbouchenna001@gmail.com>
2023-12-05 23:53:30 +0100

    :white_check_mark: (numberOfMatches) add test case 2

--- Datastructures_&_Algorithms/Math/numberOfMatches.js
+++ Datastructures_&_Algorithms/Math/numberOfMatches.js
@@ -38,3 +38,13 @@ const n = 7;
 - 3rd Round: Teams = 2, Matches = 1, and 1 team is declared the winner.
 Total number of matches = 3 + 2 + 1 = 6. */
 console.log(numberOfMatches(n));
+
+const n1 = 14;
+// Output: 13
+/* Explanation: Details of the tournament:
+- 1st Round: Teams = 14, Matches = 7, and 7 teams advance.
+- 2nd Round: Teams = 7, Matches = 3, and 4 teams advance.
+- 3rd Round: Teams = 4, Matches = 2, and 2 teams advance.
+- 4th Round: Teams = 2, Matches = 1, and 1 team is declared the winner.
+Total number of matches = 7 + 3 + 2 + 1 = 13. */
+console.log(numberOfMatches(n1));
