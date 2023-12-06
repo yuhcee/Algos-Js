702e4f4706abaf54ef6fff5ff8bd5698583e9cb3
yuhcee <egbouchenna001@gmail.com>
2023-12-06 12:54:33 +0100

    :white_check_mark: (totalMoney) add test case 3

--- Datastructures_&_Algorithms/Math/totalMoney.js
+++ Datastructures_&_Algorithms/Math/totalMoney.js
@@ -39,9 +39,14 @@ const totalMoney = function (n) {
 const n = 4;
 // Output: 10
 // Explanation: After the 4th day, the total is 1 + 2 + 3 + 4 = 10.
-// console.log(totalMoney(n));
+console.log(totalMoney(n));
 
 const n1 = 10;
 // Output: 37
 /* Explanation: After the 10th day, the total is (1 + 2 + 3 + 4 + 5 + 6 + 7) + (2 + 3 + 4) = 37. Notice that on the 2nd Monday, Hercy only puts in $2. */
 console.log(totalMoney(n1));
+
+const n2 = 20;
+// Output: 96
+// Explanation: After the 20th day, the total is (1 + 2 + 3 + 4 + 5 + 6 + 7) + (2 + 3 + 4 + 5 + 6 + 7 + 8) + (3 + 4 + 5 + 6 + 7 + 8) = 96.
+console.log(totalMoney(n2));
