8df8a8fe66dbe8f4582847153f871efdc9f885c4
yuhcee <egbouchenna001@gmail.com>
2023-12-07 17:03:45 +0100

    :books: (largestOddNumber) add task description

---
+++ Datastructures_&_Algorithms/Strings/largestOddNumber.js
@@ -0,0 +1,13 @@
+/**
+ * **1903. Largest Odd Number in String**
+ * 
+ * You are given a string `num`, representing a large integer. Return *the 
+ * **largest-valued** odd integer (as a string) that is a **non-empty 
+ * substring** of `num`, or an empty string `""` if no odd integer exists.
+ * 
+ * A **substring** is a contiguous sequence of characters within a string.
+ * 
+ * @param {string} num
+ * @return {string}
+ */
+const largestOddNumber = function (num) {};
