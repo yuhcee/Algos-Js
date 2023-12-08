6bd4c833e1a8f3bdeb3cfa9aca183d49fd8f49c5
yuhcee <egbouchenna001@gmail.com>
2023-12-08 17:01:28 +0100

    :books: (tree2str) add task description

---
+++ Datastructures_&_Algorithms/Trees/BinaryTree/tree2str.js
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * **606. Construct String from Binary Tree**
+ *
+ * Given the `root` of a binary tree, construct a string consisting of
+ * parenthesis and integers from a binary tree with the preorder traversal
+ * way, and return it.
+ *
+ * Omit all the empty parenthesis pairs that do not affect the one-to-one
+ * mapping relationship between the string and the original binary tree.
+ *
+ * @param {TreeNode} root
+ * @return {string}
+ */
+const tree2str = function (root) {};
