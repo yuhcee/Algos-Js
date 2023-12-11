d8b1c0d50feaa990997d356bd892fa9aca430577
yuhcee <egbouchenna001@gmail.com>
2023-12-11 23:54:42 +0100

    :books: (findSpecialInteger) add task description

---
+++ Datastructures_&_Algorithms/Arrays/findSpecialInteger.js
@@ -0,0 +1,11 @@
+/**
+ * **1287. Element Appearing More Than 25% In Sorted Array**
+ *
+ * Given an integer array **sorted** in non-decreasing order, there is
+ * exactly one integer in the array that occurs more than 25% of the time,
+ * return that integer.
+ *
+ * @param {number[]} arr
+ * @return {number}
+ */
+const findSpecialInteger = function (arr) {};
