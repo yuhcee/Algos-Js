833ec521f3414dde4d1bf85908597a0bc3ba60d4
yuhcee <egbouchenna001@gmail.com>
2023-12-12 23:50:15 +0100

    :books: (maxProduct) add task description

---
+++ Datastructures_&_Algorithms/Arrays/maxProduct1.js
@@ -0,0 +1,11 @@
+/**
+ * **1464. Maximum Product of Two Elements in an Array**
+ * 
+ * Given the array of integers `nums`, you will choose two different 
+ * indices `i` and `j` of that array. Return *the maximum value of `(nums
+ * [i]-1)*(nums[j]-1)`.
+ * 
+ * @param {number[]} nums
+ * @return {number}
+ */
+const maxProduct = function (nums) {};
