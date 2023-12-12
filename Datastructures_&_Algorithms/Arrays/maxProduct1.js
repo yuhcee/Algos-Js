c5e68aebc8c80131e93870e08bb1554a11d9713a
yuhcee <egbouchenna001@gmail.com>
2023-12-12 23:54:55 +0100

    :white_check_mark: (maxProduct) add test case 1

--- Datastructures_&_Algorithms/Arrays/maxProduct1.js
+++ Datastructures_&_Algorithms/Arrays/maxProduct1.js
@@ -21,3 +21,8 @@ const maxProduct = function (nums) {
 
     return x * y;
 };
+
+const nums = [3, 4, 5, 2];
+// Output: 12
+// Explanation: If you choose the indices i=1 and j=2 (indexed from 0), you will get the maximum value, that is, (nums[1]-1)*(nums[2]-1) = (4-1)*(5-1) = 3*4 = 12.
+console.log(maxProduct(nums));
