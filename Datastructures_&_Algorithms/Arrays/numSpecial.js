8755745a0ae7d299d3989c8a658d3c23b26632ff
yuhcee <egbouchenna001@gmail.com>
2023-12-13 18:20:33 +0100

    :heavy_plus_sign: (numSpecial) add constraints

--- Datastructures_&_Algorithms/Arrays/numSpecial.js
+++ Datastructures_&_Algorithms/Arrays/numSpecial.js
@@ -1,13 +1,20 @@
 /**
  * **1582. Special Positions in a Binary Matrix**
- * 
- * Given an `m x n` binary matrix `mat`, return the number of special 
+ *
+ * Given an `m x n` binary matrix `mat`, return the number of special
  * positions in `mat`.
- * 
- * A position `(i, j)` is called special if `mat[i][j] == 1` and all other 
- * elements in row `i` and column `j` are `0` (rows and columns are 
+ *
+ * A position `(i, j)` is called special if `mat[i][j] == 1` and all other
+ * elements in row `i` and column `j` are `0` (rows and columns are
  * **0-indexed**).
- * 
+ *
+ * **Constraints:**
+ *
+ * - `m == mat.length`
+ * - `n == mat[i].length`
+ * - `1 <= m, n <= 100`
+ * - `mat[i][j]` is either `0` or `1`.
+ *
  * @param {number[][]} mat
  * @return {number}
  */
