528398de24c8e8264f3132ddd1197c00b5302cc1
yuhcee <egbouchenna001@gmail.com>
2023-12-13 18:17:18 +0100

    :books: (numSpecial) add task description

---
+++ Datastructures_&_Algorithms/Arrays/numSpecial.js
@@ -0,0 +1,14 @@
+/**
+ * **1582. Special Positions in a Binary Matrix**
+ * 
+ * Given an `m x n` binary matrix `mat`, return the number of special 
+ * positions in `mat`.
+ * 
+ * A position `(i, j)` is called special if `mat[i][j] == 1` and all other 
+ * elements in row `i` and column `j` are `0` (rows and columns are 
+ * **0-indexed**).
+ * 
+ * @param {number[][]} mat
+ * @return {number}
+ */
+const numSpecial = function (mat) {};
