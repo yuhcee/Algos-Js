c49e3f85b0abe05c3c97636f4d5ea2d939d4d87f
yuhcee <egbouchenna001@gmail.com>
2023-12-14 23:02:55 +0100

    :white_check_mark: (onesMinusZeros) add test case 1

--- Datastructures_&_Algorithms/Arrays/onesMinusZeros.js
+++ Datastructures_&_Algorithms/Arrays/onesMinusZeros.js
@@ -57,3 +57,21 @@ const onesMinusZeros = function (grid) {
 
     return diff;
 };
+
+const grid = [
+    [0, 1, 1],
+    [1, 0, 1],
+    [0, 0, 1],
+];
+// Output: [[0,0,4],[0,0,4],[-2,-2,2]]
+/* Explanation:
+- diff[0][0] = onesRow0 + onesCol0 - zerosRow0 - zerosCol0 = 2 + 1 - 1 - 2 = 0 
+- diff[0][1] = onesRow0 + onesCol1 - zerosRow0 - zerosCol1 = 2 + 1 - 1 - 2 = 0 
+- diff[0][2] = onesRow0 + onesCol2 - zerosRow0 - zerosCol2 = 2 + 3 - 1 - 0 = 4 
+- diff[1][0] = onesRow1 + onesCol0 - zerosRow1 - zerosCol0 = 2 + 1 - 1 - 2 = 0 
+- diff[1][1] = onesRow1 + onesCol1 - zerosRow1 - zerosCol1 = 2 + 1 - 1 - 2 = 0 
+- diff[1][2] = onesRow1 + onesCol2 - zerosRow1 - zerosCol2 = 2 + 3 - 1 - 0 = 4 
+- diff[2][0] = onesRow2 + onesCol0 - zerosRow2 - zerosCol0 = 1 + 1 - 2 - 2 = -2
+- diff[2][1] = onesRow2 + onesCol1 - zerosRow2 - zerosCol1 = 1 + 1 - 2 - 2 = -2
+- diff[2][2] = onesRow2 + onesCol2 - zerosRow2 - zerosCol2 = 1 + 3 - 2 - 0 = 2 */
+console.log(onesMinusZeros(grid));
