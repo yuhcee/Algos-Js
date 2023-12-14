b3e2a674dc03767c928fb631cc95f2f28361f71e
yuhcee <egbouchenna001@gmail.com>
2023-12-14 22:59:51 +0100

    :books: (onesMinusZeros) add task description

---
+++ Datastructures_&_Algorithms/Arrays/onesMinusZeros.js
@@ -0,0 +1,20 @@
+/**
+ * **2482. Difference Between Ones and Zeros in Row and Column**
+ *
+ * You are given a 0-indexed m x n binary matrix grid.
+ *
+ * A **0-indexed** `m x n` difference matrix `diff` is created with the
+ * following procedure:
+ *
+ * - Let the number of ones in the `ith` row be `onesRowi`.
+ * - Let the number of ones in the `jth` column be `onesColj`.
+ * - Let the number of zeros in the `ith` row be `zerosRowi`.
+ * - Let the number of zeros in the `jth` column be `zerosColj`.
+ * - `diff[i][j] = onesRowi + onesColj - zerosRowi - zerosColj`
+ *
+ * Return the difference matrix `diff`.
+ *
+ * @param {number[][]} grid
+ * @return {number[][]}
+ */
+const onesMinusZeros = function (grid) {};
