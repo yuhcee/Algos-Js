f983e795ea572e3a0fd553dbbcd9a687ca501ec4
yuhcee <egbouchenna001@gmail.com>
2023-12-16 23:33:58 +0100

    :books: (isAnagram) add task description

---
+++ Datastructures_&_Algorithms/HashMaps/isAnagram.js
@@ -0,0 +1,15 @@
+/**
+ * **242. Valid Anagram**
+ *
+ * Given two strings `s` and `t`, return `true` if `t` is an anagram of
+ * `s`, and `false` otherwise.
+ *
+ * An **Anagram** is a word or phrase formed by rearranging the letters of
+ * a different word or phrase, typically using all the original letters
+ * exactly once.
+ *
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+const isAnagram = function (s, t) {};
