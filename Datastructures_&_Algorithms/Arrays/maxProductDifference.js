0db5e5a1ba8e8adbd87b517793e71681146ffafc
yuhcee <egbouchenna001@gmail.com>
2023-12-18 23:57:20 +0100

    :white_check_mark: (maxProductDifference) add test case 2

--- Datastructures_&_Algorithms/Arrays/maxProductDifference.js
+++ Datastructures_&_Algorithms/Arrays/maxProductDifference.js
@@ -54,3 +54,9 @@ const nums = [5, 6, 2, 7, 4];
 /* Explanation: We can choose indices 1 and 3 for the first pair (6, 7) and indices 2 and 4 for the second pair (2, 4).
 The product difference is (6 * 7) - (2 * 4) = 34. */
 console.log(maxProductDifference(nums));
+
+const nums1 = [4, 2, 5, 9, 7, 4, 8];
+// Output: 64
+/* Explanation: We can choose indices 3 and 6 for the first pair (9, 8) and indices 1 and 5 for the second pair (2, 4).
+The product difference is (9 * 8) - (2 * 4) = 64. */
+console.log(maxProductDifference(nums1));
