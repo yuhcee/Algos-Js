659e6137436bb0dcc5a4e087349fd47e421252a1
yuhcee <egbouchenna001@gmail.com>
2023-12-18 23:56:44 +0100

    :white_check_mark: (maxProductDifference) add test case 1

--- Datastructures_&_Algorithms/Arrays/maxProductDifference.js
+++ Datastructures_&_Algorithms/Arrays/maxProductDifference.js
@@ -48,3 +48,9 @@ const maxProductDifference = function (nums) {
     // Calculate the product difference
     return max1 * max2 - min1 * min2;
 };
+
+const nums = [5, 6, 2, 7, 4];
+// Output: 34
+/* Explanation: We can choose indices 1 and 3 for the first pair (6, 7) and indices 2 and 4 for the second pair (2, 4).
+The product difference is (6 * 7) - (2 * 4) = 34. */
+console.log(maxProductDifference(nums));
