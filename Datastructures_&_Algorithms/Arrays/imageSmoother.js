31898010f54f6e314809701aff00aeaaf8961293
yuhcee <egbouchenna001@gmail.com>
2023-12-19 16:14:59 +0100

    :white_check_mark: (imageSmoother) add test case 1

--- Datastructures_&_Algorithms/Arrays/imageSmoother.js
+++ Datastructures_&_Algorithms/Arrays/imageSmoother.js
@@ -56,3 +56,15 @@ const imageSmoother = function (img) {
 
     return smoothedImage;
 };
+
+const img = [
+    [1, 1, 1],
+    [1, 0, 1],
+    [1, 1, 1],
+];
+// Output: [[0,0,0],[0,0,0],[0,0,0]]
+/* Explanation:
+For the points (0,0), (0,2), (2,0), (2,2): floor(3/4) = floor(0.75) = 0
+For the points (0,1), (1,0), (1,2), (2,1): floor(5/6) = floor(0.83333333) = 0
+For the point (1,1): floor(8/9) = floor(0.88888889) = 0 */
+console.log(imageSmoother(img));
