c00325d21d62d21ac16b30ba0142ac9715aea24b
yuhcee <egbouchenna001@gmail.com>
2023-12-19 16:15:42 +0100

    :white_check_mark: (imageSmoother) add test case 2

--- Datastructures_&_Algorithms/Arrays/imageSmoother.js
+++ Datastructures_&_Algorithms/Arrays/imageSmoother.js
@@ -68,3 +68,15 @@ For the points (0,0), (0,2), (2,0), (2,2): floor(3/4) = floor(0.75) = 0
 For the points (0,1), (1,0), (1,2), (2,1): floor(5/6) = floor(0.83333333) = 0
 For the point (1,1): floor(8/9) = floor(0.88888889) = 0 */
 console.log(imageSmoother(img));
+
+const img1 = [
+    [100, 200, 100],
+    [200, 50, 200],
+    [100, 200, 100],
+];
+// Output: [[137,141,137],[141,138,141],[137,141,137]]
+/* Explanation:
+For the points (0,0), (0,2), (2,0), (2,2): floor((100+200+200+50)/4) = floor(137.5) = 137
+For the points (0,1), (1,0), (1,2), (2,1): floor((200+200+50+200+100+100)/6) = floor(141.666667) = 141
+For the point (1,1): floor((50+200+200+200+200+100+100+100+100)/9) = floor(138.888889) = 138 */
+console.log(imageSmoother(img1));
