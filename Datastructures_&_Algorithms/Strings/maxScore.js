6e8d780017ff4a653a4089c6a1e170a2591c6637
yuhcee <egbouchenna001@gmail.com>
2023-12-22 23:38:20 +0100

    :white_check_mark: (maxScore) add test case 2

--- Datastructures_&_Algorithms/Strings/maxScore.js
+++ Datastructures_&_Algorithms/Strings/maxScore.js
@@ -47,3 +47,8 @@ left = "011" and right = "101", score = 1 + 2 = 3
 left = "0111" and right = "01", score = 1 + 1 = 2 
 left = "01110" and right = "1", score = 2 + 1 = 3 */
 console.log(maxScore(s));
+
+const s1 = '00111';
+// Output: 5
+// Explanation: When left = "00" and right = "111", we get the maximum score = 2 + 3 = 5
+console.log(maxScore(s1));
