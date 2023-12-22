ffbd3693c8caf64db3c9ac1b08dcdeaf655cf1cc
yuhcee <egbouchenna001@gmail.com>
2023-12-22 23:31:28 +0100

    :books: (maxScore) add task description

---
+++ Datastructures_&_Algorithms/Strings/maxScore.js
@@ -0,0 +1,15 @@
+/**
+ * **1422. Maximum Score After Splitting a String**
+ *
+ * Given a string `s` of zeros and ones, return *the maximum score after
+ * splitting the string into two **non-empty** substrings (i.e. **left**
+ * substring and **right** substring).
+ *
+ * The score after splitting a string is the number of **zeros** in the
+ * **left** substring plus the number of **ones** in the **right**
+ * substring.
+ *
+ * @param {string} s
+ * @return {number}
+ */
+const maxScore = function (s) {};
