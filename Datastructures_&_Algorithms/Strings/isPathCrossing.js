400822054491032c18274d0471aeb9eff411d763
yuhcee <egbouchenna001@gmail.com>
2023-12-23 23:39:24 +0100

    :books: (isPathCrossing) add task description

---
+++ Datastructures_&_Algorithms/Strings/isPathCrossing.js
@@ -0,0 +1,48 @@
+/**
+ * **1496. Path Crossing**
+ *
+ * Given a string `path`, where `path[i] = 'N'`, `'S'`, `'E'` or `'W'`,
+ * each representing moving one unit north, south, east, or west,
+ * respectively. You start at the origin `(0, 0)` on a 2D plane and walk
+ * on the path specified by `path`.
+ *
+ * Return *`true` if the path crosses itself at any point, that is, if
+ * at any time you are on a location you have previously visited*.
+ * Return `false` otherwise.
+ *
+ * @param {string} path
+ * @return {boolean}
+ */
+var isPathCrossing = function (path) {
+    const visited = new Set();
+    visited.add('0,0'); // Starting position
+
+    let x = 0,
+        y = 0;
+
+    for (let move of path) {
+        switch (move) {
+            case 'N':
+                y++;
+                break;
+            case 'S':
+                y--;
+                break;
+            case 'E':
+                x++;
+                break;
+            case 'W':
+                x--;
+                break;
+        }
+
+        const currentPosition = `${x},${y}`;
+        if (visited.has(currentPosition)) {
+            return true;
+        }
+
+        visited.add(currentPosition);
+    }
+
+    return false;
+};
