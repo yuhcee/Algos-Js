785e179a42aae44bf7eda11d0f2a70b314a7144a
yuhcee <egbouchenna001@gmail.com>
2023-12-28 23:59:45 +0100

    :white_check_mark: (getLengthOfOptimalCompression) add test case 2

--- Datastructures_&_Algorithms/Dynamic Programming/getLengthOfOptimalCompression.js
+++ Datastructures_&_Algorithms/Dynamic Programming/getLengthOfOptimalCompression.js
@@ -62,3 +62,9 @@ const s = 'aaabcccd',
 // Output: 4
 /* Explanation: Compressing s without deleting anything will give us "a3bc3d" of length 6. Deleting any of the characters 'a' or 'c' would at most decrease the length of the compressed string to 5, for instance delete 2 'a' then we will have s = "abcccd" which compressed is abc3d. Therefore, the optimal way is to delete 'b' and 'd', then the compressed version of s will be "a3c3" of length 4. */
 console.log(getLengthOfOptimalCompression(s, k));
+
+const s1 = 'aabbaa',
+    k1 = 2;
+// Output: 2
+// Explanation: If we delete both 'b' characters, the resulting compressed string would be "a4" of length 2.
+console.log(getLengthOfOptimalCompression(s1, k1));
