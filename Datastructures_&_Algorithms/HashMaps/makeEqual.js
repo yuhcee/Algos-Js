4120dcd595628d433223b5cdc2ca326109ed2cbc
yuhcee <egbouchenna001@gmail.com>
2023-12-30 09:35:53 +0100

    :books: (makeEqual) add task description

---
+++ Datastructures_&_Algorithms/HashMaps/makeEqual.js
@@ -0,0 +1,16 @@
+/**
+ * **1897. Redistribute Characters to Make All Strings Equal**
+ *
+ * You are given an array of strings `words` (**0-indexed**).
+ *
+ * In one operation, pick two **distinct** indices `i` and `j`, where
+ * `words[i]` is a non-empty string, and move **any** character from
+ * `words[i]` to **any** position in `words[j]`.
+ *
+ * Return `true` if you can make **every** string in words **equal**
+ * using **any** number of operations, and false otherwise.
+ *
+ * @param {string[]} words
+ * @return {boolean}
+ */
+const makeEqual = function (words) {};
