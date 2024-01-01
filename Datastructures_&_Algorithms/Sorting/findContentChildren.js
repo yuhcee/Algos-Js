03dc7ac8131f1f777cad487c60313d094dcd1b8c
yuhcee <egbouchenna001@gmail.com>
2024-01-01 23:34:07 +0100

    :sparkles: (findContentChildren) add task solution

--- Datastructures_&_Algorithms/Sorting/findContentChildren.js
+++ Datastructures_&_Algorithms/Sorting/findContentChildren.js
@@ -13,13 +13,37 @@
  * output the maximum number.
  *
  * **Constraints:**
- * 
+ *
  * - `1 <= g.length <= 3 * 104`
  * - `0 <= s.length <= 3 * 104`
  * - `1 <= g[i], s[j] <= 231 - 1`
- * 
+ *
  * @param {number[]} g
  * @param {number[]} s
  * @return {number}
  */
-const findContentChildren = function (g, s) {};
+const findContentChildren = function (g, s) {
+    g.sort((a, b) => a - b); // Sort the greed factors in ascending order
+    s.sort((a, b) => a - b); // Sort the cookie sizes in ascending order
+
+    let contentChildren = 0;
+    let cookieIndex = 0;
+
+    for (let i = 0; i < g.length; i++) {
+        while (cookieIndex < s.length && s[cookieIndex] < g[i]) {
+            // Find the smallest cookie that satisfies the current child's greed factor
+            cookieIndex++;
+        }
+
+        if (cookieIndex < s.length) {
+            // If a suitable cookie is found, assign it to the child
+            contentChildren++;
+            cookieIndex++;
+        } else {
+            // If no suitable cookie is found, break the loop
+            break;
+        }
+    }
+
+    return contentChildren;
+};
