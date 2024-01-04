a5ef5d46f8e145fdc2e1c31d9e83420cc27b7e47
yuhcee <egbouchenna001@gmail.com>
2024-01-04 23:10:35 +0100

    :books: (minOperations) add task description

---
+++ Datastructures_&_Algorithms/HashMaps/minOperations.js
@@ -0,0 +1,21 @@
+/**
+ * **2870. Minimum Number of Operations to Make Array Empty**
+ *
+ * You are given a **0-indexed** array `nums` consisting of positive
+ * integers.
+ *
+ * There are two types of operations that you can apply on the array
+ * **any** number of times:
+ *
+ * - Choose **two** elements with **equal** values and **delete** them
+ * from the array.
+ * - Choose **three** elements with **equal** values and **delete** them
+ * from the array.
+ *
+ * Return *the **minimum** number of operations required to make the
+ * array empty, or `-1` if it is not possible.
+ *
+ * @param {number[]} nums
+ * @return {number}
+ */
+const minOperations = function (nums) {};
