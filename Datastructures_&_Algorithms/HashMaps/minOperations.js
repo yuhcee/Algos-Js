c9c8df8da19fe3253ec392da63c949938281d766
yuhcee <egbouchenna001@gmail.com>
2024-01-04 23:12:13 +0100

    :white_check_mark: (minOperations) add test case 1

--- Datastructures_&_Algorithms/HashMaps/minOperations.js
+++ Datastructures_&_Algorithms/HashMaps/minOperations.js
@@ -54,3 +54,13 @@ const minOperations = function (nums) {
 
     return operations;
 };
+
+const nums = [2, 3, 3, 2, 2, 4, 2, 3, 4];
+// Output: 4
+/* Explanation: We can apply the following operations to make the array empty:
+- Apply the first operation on the elements at indices 0 and 3. The resulting array is nums = [3,3,2,4,2,3,4].
+- Apply the first operation on the elements at indices 2 and 4. The resulting array is nums = [3,3,4,3,4].
+- Apply the second operation on the elements at indices 0, 1, and 3. The resulting array is nums = [4,4].
+- Apply the first operation on the elements at indices 0 and 1. The resulting array is nums = [].
+It can be shown that we cannot make the array empty in less than 4 operations. */
+console.log(minOperations(nums));
