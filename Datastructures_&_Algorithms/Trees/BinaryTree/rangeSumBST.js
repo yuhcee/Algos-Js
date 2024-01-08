7590b2879a098f4f176030dcd9ef0f3661944584
yuhcee <egbouchenna001@gmail.com>
2024-01-08 23:44:23 +0100

    :books: (rangeSumBST) add task description

---
+++ Datastructures_&_Algorithms/Trees/BinaryTree/rangeSumBST.js
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val === undefined ? 0 : val);
+ *     this.left = (left === undefined ? null : left);
+ *     this.right = (right === undefined ? null : right);
+ * }
+ */
+
+/**
+ * **938. Range Sum of BST**
+ *
+ * Given the root node of a binary search tree and two integers `low`
+ * and `high`, return *the sum of values of all nodes with a value in
+ * the **inclusive** range `[low, high]`*.
+ *
+ * @param {TreeNode} root
+ * @param {number} low
+ * @param {number} high
+ * @return {number}
+ */
+let rangeSumBST = function (root, low, high) {};
