7f9e21ff9e20e2d75fc131dde75e18f8d868bbf0
yuhcee <egbouchenna001@gmail.com>
2024-01-08 23:46:00 +0100

    :sparkles: (rangeSumBST) add task solution

--- Datastructures_&_Algorithms/Trees/BinaryTree/rangeSumBST.js
+++ Datastructures_&_Algorithms/Trees/BinaryTree/rangeSumBST.js
@@ -26,4 +26,24 @@
  * @param {number} high
  * @return {number}
  */
-let rangeSumBST = function (root, low, high) {};
+let rangeSumBST = function (root, low, high) {
+    if (!root) {
+        return 0;
+    }
+
+    let sum = 0;
+
+    if (root.val >= low && root.val <= high) {
+        sum += root.val;
+    }
+
+    if (root.val > low) {
+        sum += rangeSumBST(root.left, low, high);
+    }
+
+    if (root.val < high) {
+        sum += rangeSumBST(root.right, low, high);
+    }
+
+    return sum;
+};
