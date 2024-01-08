d1971e689795cd0a92f149698a0ce23d826255fa
yuhcee <egbouchenna001@gmail.com>
2024-01-08 23:45:35 +0100

    :heavy_plus_sign: (rangeSumBST) add helper function

--- Datastructures_&_Algorithms/Trees/BinaryTree/rangeSumBST.js
+++ Datastructures_&_Algorithms/Trees/BinaryTree/rangeSumBST.js
@@ -14,6 +14,13 @@
  * and `high`, return *the sum of values of all nodes with a value in
  * the **inclusive** range `[low, high]`*.
  *
+ * **Constraints:**
+ *
+ * - The number of nodes in the tree is in the range `[1, 2 * 104]`.
+ * - `1 <= Node.val <= 105`
+ * - `1 <= low <= high <= 105`
+ * - All `Node.val` are unique.
+ *
  * @param {TreeNode} root
  * @param {number} low
  * @param {number} high
