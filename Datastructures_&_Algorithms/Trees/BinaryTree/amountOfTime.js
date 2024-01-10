505d1380c96967815fda8f7368de0b56112d7ff5
yuhcee <egbouchenna001@gmail.com>
2024-01-10 19:28:04 +0100

    :heavy_plus_sign: (amountOfTime) add helper function

--- Datastructures_&_Algorithms/Trees/BinaryTree/amountOfTime.js
+++ Datastructures_&_Algorithms/Trees/BinaryTree/amountOfTime.js
@@ -25,6 +25,14 @@
  * Calculate the amount of time for the entire binary tree to be infected
  * @param {TreeNode} root - The root node of the binary tree
  * @param {number} start - The value of the node where the infection starts
- * @return {number} - The amount of time needed for the entire tree to be infected
+ * @return {number} - The amount of time needed for the entire tree to
+ * be infected.
+ *
+ * **Constraints:**
+ *
+ * - The number of nodes in the tree is in the range `[1, 105]`.
+ * - `1 <= Node.val <= 105`
+ * - Each node has a **unique** value.
+ * - A node with a value of `start` exists in the tree.
  */
 const amountOfTime = (root, start) => {};
