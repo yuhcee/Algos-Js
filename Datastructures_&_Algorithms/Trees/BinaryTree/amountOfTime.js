2b8308f9289e27209ade030758ef575d78b03af4
yuhcee <egbouchenna001@gmail.com>
2024-01-10 19:26:40 +0100

    :books: (amountOfTime) add task description

---
+++ Datastructures_&_Algorithms/Trees/BinaryTree/amountOfTime.js
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val === undefined ? 0 : val);
+ *     this.left = (left === undefined ? null : left);
+ *     this.right = (right === undefined ? null : right);
+ * }
+ */
+
+/**
+ * **2385. Amount of Time for Binary Tree to Be Infected**
+ *
+ * You are given the `root` of a binary tree with **unique** values, and
+ * an integer `start`. At minute `0`, an **infection** starts from the
+ * node with value `start`.
+ *
+ * Each minute, a node becomes infected if:
+ *
+ * - The node is currently uninfected.
+ * - The node is adjacent to an infected node.
+ *
+ * Return *the number of minutes needed for the entire tree to be
+ * infected*.
+ *
+ * Calculate the amount of time for the entire binary tree to be infected
+ * @param {TreeNode} root - The root node of the binary tree
+ * @param {number} start - The value of the node where the infection starts
+ * @return {number} - The amount of time needed for the entire tree to be infected
+ */
+const amountOfTime = (root, start) => {};
