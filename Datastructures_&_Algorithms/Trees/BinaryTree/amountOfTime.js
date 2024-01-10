0c2e646870c3369fd3b64d8820e0d8f82a5e4f36
yuhcee <egbouchenna001@gmail.com>
2024-01-10 19:29:54 +0100

    :white_check_mark: (amountOfTime) add test case 1

--- Datastructures_&_Algorithms/Trees/BinaryTree/amountOfTime.js
+++ Datastructures_&_Algorithms/Trees/BinaryTree/amountOfTime.js
@@ -70,3 +70,15 @@ const amountOfTime = (root, start) => {
 
     return amount;
 };
+
+const root = [1, 5, 3, null, 4, 10, 6, 9, 2],
+    start = 3;
+// Output: 4;
+/* Explanation: The following nodes are infected during:
+- Minute 0: Node 3
+- Minute 1: Nodes 1, 10 and 6
+- Minute 2: Node 5
+- Minute 3: Node 4
+- Minute 4: Nodes 9 and 2
+It takes 4 minutes for the whole tree to be infected so we return 4. */
+console.log(amountOfTime(root, start));
