3dbeda1b03476caefe71d0537d4702fdd98637ca
yuhcee <egbouchenna001@gmail.com>
2024-01-11 18:05:23 +0100

    :heavy_plus_sign: (maxAncestorDiff) add helper function

--- Datastructures_&_Algorithms/Trees/BinaryTree/maxAncestorDiff.js
+++ Datastructures_&_Algorithms/Trees/BinaryTree/maxAncestorDiff.js
@@ -16,6 +16,11 @@
  * A node `a` is an ancestor of `b` if either: any child of `a` is equal
  * to `b` or any child of `a` is an ancestor of `b`.
  *
+ * **Constraints:**
+ *
+ * - The number of nodes in the tree is in the range `[2, 5000]`.
+ * - `0 <= Node.val <= 105`
+ *
  * @param {TreeNode} root
  * @return {number}
  */
