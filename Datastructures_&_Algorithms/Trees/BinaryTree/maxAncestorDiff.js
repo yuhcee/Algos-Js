9bb476dc73daa3e5dc011b8612ee1ce050679487
yuhcee <egbouchenna001@gmail.com>
2024-01-11 18:04:12 +0100

    :books: (maxAncestorDiff) add task description

---
+++ Datastructures_&_Algorithms/Trees/BinaryTree/maxAncestorDiff.js
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * **1026. Maximum Difference Between Node and Ancestor**
+ *
+ * Given the `root` of a binary tree, find the maximum value `v` for
+ * which there exist **different** nodes `a` and `b` where `v = |a.val -
+ * b.val|` and `a` is an ancestor of `b`.
+ *
+ * A node `a` is an ancestor of `b` if either: any child of `a` is equal
+ * to `b` or any child of `a` is an ancestor of `b`.
+ *
+ * @param {TreeNode} root
+ * @return {number}
+ */
+const maxAncestorDiff = function (root) {};
