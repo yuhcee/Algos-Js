f10d6a1166c96053db698108c8ff8e66cd7c9ce3
yuhcee <egbouchenna001@gmail.com>
2024-01-12 23:09:15 +0100

    :white_check_mark: (halvesAreAlike) add test case 1

--- Datastructures_&_Algorithms/Strings/halvesAreAlike.js
+++ Datastructures_&_Algorithms/Strings/halvesAreAlike.js
@@ -1,11 +1,14 @@
 /**
  * **1704. Determine if String Halves Are Alike**
  *
- * You are given a string `s` of even length. Split this string into two halves of equal lengths, and let `a` be
+ * You are given a string `s` of even length. Split this string into two 
+ * halves of equal lengths, and let `a` be
  * the first half and `b` be the second half.
  *
- * Two strings are **alike** if they have the same number of vowels (`'a'`, `'e'`, `'i'`, `'o'`, `'u'`, `'A'`,
- * `'E'`, `'I'`, `'O'`, `'U'`). Notice that `s` contains uppercase and lowercase letters.
+ * Two strings are **alike** if they have the same number of vowels 
+ * (`'a'`, `'e'`, `'i'`, `'o'`, `'u'`, `'A'`,
+ * `'E'`, `'I'`, `'O'`, `'U'`). Notice that `s` contains uppercase and 
+ * lowercase letters.
  *
  * Return `true` if `a` and `b` are alike. Otherwise, return `false`.
  *
