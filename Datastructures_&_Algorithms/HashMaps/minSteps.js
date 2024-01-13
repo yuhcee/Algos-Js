3046aa929a41198827337498df427a3fa7da435a
yuhcee <egbouchenna001@gmail.com>
2024-01-13 22:59:59 +0100

    :books: (minSteps) add task description

---
+++ Datastructures_&_Algorithms/HashMaps/minSteps.js
@@ -0,0 +1,16 @@
+/**
+ * **1347. Minimum Number of Steps to Make Two Strings Anagram**
+ *
+ * You are given two strings of the same length `s` and `t`. In one step you can choose **any
+ * character** of `t` and replace it with **another character**.
+ *
+ * Return *the minimum number of steps to make `t` an anagram of `s`*.
+ *
+ * An **Anagram** of a string is a string that contains the same characters with a different (or
+ * the same) ordering.
+ *
+ * @param {string} s
+ * @param {string} t
+ * @return {number}
+ */
+const minSteps = function (s, t) {};
