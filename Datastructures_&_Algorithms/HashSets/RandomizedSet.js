b7d655518e5dfcda55012abc590c1b05f4402890
yuhcee <egbouchenna001@gmail.com>
2024-01-16 23:12:13 +0100

    :white_check_mark: (RandomizedSet) add test case 2

--- Datastructures_&_Algorithms/HashSets/RandomizedSet.js
+++ Datastructures_&_Algorithms/HashSets/RandomizedSet.js
@@ -5,7 +5,8 @@
  *
  * - `RandomizedSet()` Initializes the `RandomizedSet` object.
  *
- * - `bool insert(int val)` Inserts an item `val` into the set if not present. Returns `true` if the item was not
+ * - `bool insert(int val)` Inserts an item `val` into the set if not
+ * present. Returns `true` if the item was not
  * present, `false` otherwise.
  *
  * - `bool remove(int val)` Removes an item val from the set if present. Returns `true` if the item was present,
