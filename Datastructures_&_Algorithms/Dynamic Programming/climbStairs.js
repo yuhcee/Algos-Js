1326eaf5bf09fe271b368c3f0eaef19879760ff0
yuhcee <egbouchenna001@gmail.com>
2024-01-18 23:51:17 +0100

    :books: (climbStairs) add task description

---
+++ Datastructures_&_Algorithms/Dynamic Programming/climbStairs.js
@@ -0,0 +1,12 @@
+/**
+ * **70. Climbing Stairs**
+ *
+ * You are climbing a staircase. It takes `n` steps to reach the top.
+ *
+ * Each time you can either climb `1` or `2` steps. In how many distinct
+ * ways can you climb to the top?
+ *
+ * @param {number} n
+ * @return {number}
+ */
+const climbStairs = function (n) {};
