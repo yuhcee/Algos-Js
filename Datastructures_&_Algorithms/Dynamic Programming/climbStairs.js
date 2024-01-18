7cf543f569543ad0de1f23cf55456e9cb3977990
yuhcee <egbouchenna001@gmail.com>
2024-01-18 23:52:17 +0100

    :sparkles: (climbStairs) add task solution

--- Datastructures_&_Algorithms/Dynamic Programming/climbStairs.js
+++ Datastructures_&_Algorithms/Dynamic Programming/climbStairs.js
@@ -13,4 +13,18 @@
  * @param {number} n
  * @return {number}
  */
-const climbStairs = function (n) {};
+const climbStairs = function (n) {
+    if (n === 1) {
+        return 1;
+    }
+
+    const dp = new Array(n + 1);
+    dp[1] = 1;
+    dp[2] = 2;
+
+    for (let i = 3; i <= n; i++) {
+        dp[i] = dp[i - 1] + dp[i - 2];
+    }
+
+    return dp[n];
+};
