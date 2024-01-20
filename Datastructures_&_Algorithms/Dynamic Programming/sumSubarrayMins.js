243f8f67a0b7ccd837c13bb67b77e43a67d94f7d
yuhcee <egbouchenna001@gmail.com>
2024-01-20 23:01:32 +0100

    :books: (sumSubarrayMins) add task description

--- Datastructures_&_Algorithms/Dynamic Programming/sumSubarrayMins.js
+++ Datastructures_&_Algorithms/Dynamic Programming/sumSubarrayMins.js
@@ -1,8 +1,9 @@
 /**
  * **907. Sum of Subarray Minimums**
  *
- * Given an array of integers arr, find the sum of `min(b)`, where `b` ranges over every (contiguous) subarray of
- * `arr`.
+ * Given an array of integers arr, find the sum of `min(b)`, where `b`
+ * ranges over every (contiguous) subarray of`arr`.
+ *
  * Since the answer may be large, return the answer **modulo** `109 + 7`.
  *
  * **Constraints:**
