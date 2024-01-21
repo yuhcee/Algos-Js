a2f4c2e0ba6ab07fa5ba220e1a230e1b599816c6
yuhcee <egbouchenna001@gmail.com>
2024-01-21 16:18:32 +0100

    :white_check_mark: (rob) add test case 2

--- Datastructures_&_Algorithms/Dynamic Programming/rob.js
+++ Datastructures_&_Algorithms/Dynamic Programming/rob.js
@@ -45,3 +45,9 @@ const nums = [1, 2, 3, 1];
 // Explanation: Rob house 1 (money = 1) and then rob house 3 (money = 3).
 // Total amount you can rob = 1 + 3 = 4.
 console.log(rob(nums));
+
+const nums1 = [2, 7, 9, 3, 1];
+// Output: 12
+// Explanation: Rob house 1 (money = 2), rob house 3 (money = 9) and rob house 5 (money = 1).
+// Total amount you can rob = 2 + 9 + 1 = 12.
+console.log(rob(nums1));
