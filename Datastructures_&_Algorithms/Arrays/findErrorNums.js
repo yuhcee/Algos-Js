ba3b2eb9d0c0422808cd5872d2afd7e19b5a4b7a
yuhcee <egbouchenna001@gmail.com>
2024-01-22 15:25:33 +0100

    :sparkles: (findErrorNums) add task solution

--- Datastructures_&_Algorithms/Arrays/findErrorNums.js
+++ Datastructures_&_Algorithms/Arrays/findErrorNums.js
@@ -1,16 +1,16 @@
 /**
  * **645. Set Mismatch**
  *
- * You have a set of integers `s`, which originally contains all the 
+ * You have a set of integers `s`, which originally contains all the
  * numbers from `1` to `n`.
- * Unfortunately, due to some error, one of the numbers in `s` got 
- * duplicated to another number in the set, which results in **repetition 
+ * Unfortunately, due to some error, one of the numbers in `s` got
+ * duplicated to another number in the set, which results in **repetition
  * of one** number and **loss of another** number.
  *
- * You are given an integer array `nums` representing the data status of 
+ * You are given an integer array `nums` representing the data status of
  * this set after the error.
  *
- * Find the number that occurs twice and the number that is missing and 
+ * Find the number that occurs twice and the number that is missing and
  * return *them in the form of an array.
  *
  * **Constraints:**
@@ -23,6 +23,7 @@
  */
 const findErrorNums = function (nums) {
     // Ex: [1,2,2,4]
+
     const n = nums.length;
 
     const except = (n * (n + 1)) / 2; // 1 + 2 + 3 + 4
