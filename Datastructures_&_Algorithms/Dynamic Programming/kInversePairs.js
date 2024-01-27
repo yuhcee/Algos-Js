f6dfb064ec82876ab2a12710d72755496a2f2c85
yuhcee <egbouchenna001@gmail.com>
2024-01-27 15:47:13 +0100

    :books: (kInversePairs) add task description

---
+++ Datastructures_&_Algorithms/Dynamic Programming/kInversePairs.js
@@ -0,0 +1,16 @@
+/**
+ * **629. K Inverse Pairs Array**
+ *
+ * For an integer array `nums`, an **inverse pair** is a pair of integers `
+ * [i, j]` where `0 <= i < j < nums.length` and `nums[i] > nums[j]`.
+ *
+ * Given two integers `n` and `k`, return the number of different arrays
+ * consist of numbers from `1` to `n` such that there are exactly `k`
+ * **inverse pairs**. Since the answer can be huge, return it **modulo** `109
+ * + 7`.
+ *
+ * @param {number} n
+ * @param {number} k
+ * @return {number}
+ */
+const kInversePairs = function (n, k) {};
