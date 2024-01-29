4e9a09c82b5a8bcde7a3b4a2ea07fa4a54176e80
yuhcee <egbouchenna001@gmail.com>
2024-01-29 04:40:10 +0100

    :heavy_plus_sign: (isMatch) add constraints

--- Datastructures_&_Algorithms/Dynamic Programming/isMatch.js
+++ Datastructures_&_Algorithms/Dynamic Programming/isMatch.js
@@ -1,18 +1,24 @@
 /**
  * **Regex Matcher**
  *
- * Given a string `text` and a `pattern`. Evaluate if the given string 
- * matches matches the full text of the pattern. Check if a given pattern 
+ * Given a string `text` and a `pattern`. Evaluate if the given string
+ * matches matches the full text of the pattern. Check if a given pattern
  * exists in a given string or not including wild cards * and .
  *
- * In this problem, we are given a string (text) and a pattern, which can 
- * contain wildcard characters '*' and '.', and we need to check if the 
+ * In this problem, we are given a string (text) and a pattern, which can
+ * contain wildcard characters '*' and '.', and we need to check if the
  * pattern matches the text or not.
  *
  * Matching criteria:
  * - `"."` represents any single character.
  * - `"*"` represents zero or more repeating times of the character just
  * before it.
+ *
+ * **Constraints**
+ *
+ * - `1 <= text.length <= 20`
+ * - `1 <= pattern.length <= 20`
+ * - `text` and `pattern` are lowercase English letters.
  *
  */
 function isMatch(text, pattern) {}
