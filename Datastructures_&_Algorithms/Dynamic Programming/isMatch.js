514f8cd4e84c1c85c0a04ce10fae83a0ff9a0f79
yuhcee <egbouchenna001@gmail.com>
2024-01-29 04:37:46 +0100

    :books: (isMatch) add task description

---
+++ Datastructures_&_Algorithms/Dynamic Programming/isMatch.js
@@ -0,0 +1,18 @@
+/**
+ * **Regex Matcher**
+ *
+ * Given a string `text` and a `pattern`. Evaluate if the given string 
+ * matches matches the full text of the pattern. Check if a given pattern 
+ * exists in a given string or not including wild cards * and .
+ *
+ * In this problem, we are given a string (text) and a pattern, which can 
+ * contain wildcard characters '*' and '.', and we need to check if the 
+ * pattern matches the text or not.
+ *
+ * Matching criteria:
+ * - `"."` represents any single character.
+ * - `"*"` represents zero or more repeating times of the character just
+ * before it.
+ *
+ */
+function isMatch(text, pattern) {}
