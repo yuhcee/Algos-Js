b17e7a0c9333af5978ea3ac5416063e89f5ce724
yuhcee <egbouchenna001@gmail.com>
2024-01-29 04:41:04 +0100

    :sparkles: (isMatch) add task solution

--- Datastructures_&_Algorithms/Dynamic Programming/isMatch.js
+++ Datastructures_&_Algorithms/Dynamic Programming/isMatch.js
@@ -21,4 +21,29 @@
  * - `text` and `pattern` are lowercase English letters.
  *
  */
-function isMatch(text, pattern) {}
+function isMatch(text, pattern) {
+    const dp = Array(text.length + 1)
+        .fill(false)
+        .map(() => Array(pattern.length + 1).fill(false));
+    dp[0][0] = true;
+
+    for (let j = 1; j <= pattern.length; j++) {
+        if (pattern[j - 1] === '*') {
+            dp[0][j] = dp[0][j - 1];
+        }
+    }
+
+    for (let i = 1; i <= text.length; i++) {
+        for (let j = 1; j <= pattern.length; j++) {
+            if (pattern[j - 1] === '.' || pattern[j - 1] === text[i - 1]) {
+                dp[i][j] = dp[i - 1][j - 1];
+            } else if (pattern[j - 1] === '*') {
+                dp[i][j] = dp[i][j - 2] || ((pattern[j - 2] === '.' || pattern[j - 2] === text[i - 1]) && dp[i - 1][j]);
+            } else {
+                dp[i][j] = false;
+            }
+        }
+    }
+
+    return dp[text.length][pattern.length];
+}
