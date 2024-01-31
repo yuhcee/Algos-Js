6bf4774c7781fadd397619fa7735641462c613f6
yuhcee <egbouchenna001@gmail.com>
2024-01-31 23:37:01 +0100

    :heavy_plus_sign:(dailyTemperatures) add constraints

--- Datastructures_&_Algorithms/Stacks/dailyTemperatures.js
+++ Datastructures_&_Algorithms/Stacks/dailyTemperatures.js
@@ -4,7 +4,7 @@
  * Given an array of integers `temperatures` represents the daily
  * temperatures, return *an array `answer` such that `answer[i]` is the number
  * of days you have to wait after the `ith` day to get a warmer temperature*.
- * 
+ *
  * If there is no future day for which this is possible, keep `answer[i] == 0`
  * instead.
  *
@@ -15,6 +15,7 @@
  *
  * @param {number[]} temperatures
  * @return {number[]}
+ *
  */
 const dailyTemperatures = function (temperatures) {
     const stack = [];
