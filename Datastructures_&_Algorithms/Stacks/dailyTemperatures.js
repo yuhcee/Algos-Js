2053b7e253c4ce8d7fdf3ef05fb718a7908f6b23
yuhcee <egbouchenna001@gmail.com>
2024-01-31 23:36:25 +0100

    :books: (dailyTemperatures) add task description

--- Datastructures_&_Algorithms/Stacks/dailyTemperatures.js
+++ Datastructures_&_Algorithms/Stacks/dailyTemperatures.js
@@ -4,6 +4,7 @@
  * Given an array of integers `temperatures` represents the daily
  * temperatures, return *an array `answer` such that `answer[i]` is the number
  * of days you have to wait after the `ith` day to get a warmer temperature*.
+ * 
  * If there is no future day for which this is possible, keep `answer[i] == 0`
  * instead.
  *
@@ -20,7 +21,7 @@ const dailyTemperatures = function (temperatures) {
     const answer = new Array(temperatures.length).fill(0);
 
     for (let i = 0; i < temperatures.length; i++) {
-        while (stack.length && temperatures[i] > temperatures[stack[stack.length - 1]]) {
+        while (stack.length && temperatures[i] > temperatures[stack.at(-1)]) {
             const j = stack.pop();
             answer[j] = i - j;
         }
