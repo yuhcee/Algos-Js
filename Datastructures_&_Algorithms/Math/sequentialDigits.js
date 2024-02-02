8a632a8fcc797f9c28e03690c86574f831b6561b
yuhcee <egbouchenna001@gmail.com>
2024-02-02 20:49:41 +0100

    :books: (sequentialDigits) add task description

---
+++ Datastructures_&_Algorithms/Math/sequentialDigits.js
@@ -0,0 +1,14 @@
+/**
+ * **1291. Sequential Digits**
+ *
+ * An integer has *sequential digits* if and only if each digit in the number is
+ * one more than the previous digit.
+ *
+ * Return a **sorted** list of all the integers in the range `[low, high]`
+ * inclusive that have sequential digits.
+ *
+ * @param {number} low
+ * @param {number} high
+ * @return {number[]}
+ */
+const sequentialDigits = (low, high) => {};
