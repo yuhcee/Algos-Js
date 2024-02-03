b8defc7209fa14df1d35b854d115ddd2d8494288
yuhcee <egbouchenna001@gmail.com>
2024-02-03 21:38:50 +0100

    :books: (maxSumAfterPartitioning) add task description

---
+++ Datastructures_&_Algorithms/Dynamic Programming/maxSumAfterPartitioning.js
@@ -0,0 +1,15 @@
+/**
+ * **1043. Partition Array for Maximum Sum**
+ *
+ * Given an integer array `arr`, partition the array into (contiguous) subarrays
+ * of length **at most** `k`. After partitioning, each subarray has their values
+ * changed to become the maximum value of that subarray.
+ *
+ * Return the *largest sum of the given array after partitioning. Test cases are
+ * generated so that the answer fits in a 32-bit integer*.
+ *
+ * @param {number[]} arr
+ * @param {number} k
+ * @return {number}
+ */
+const maxSumAfterPartitioning = (arr, k) => {};
