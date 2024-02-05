85895db87e8d5d0f0e314b37a20c9c4004652b1f
yuhcee <egbouchenna001@gmail.com>
2024-02-05 22:22:30 +0100

    :books: (firstUniqChar) add task description

--- Datastructures_&_Algorithms/Arrays/firstUniqChar.js
+++ Datastructures_&_Algorithms/Arrays/firstUniqChar.js
@@ -1,7 +1,8 @@
 /**
  * **387. First Unique Character in a String**
  *
- * Given a string `s`, *find the first non-repeating character in it and return its index*. If it does not
+ * Given a string `s`, *find the first non-repeating character in it and return 
+ * its index*. If it does not
  * exist, return -1.
  *
  * @param {string} s
