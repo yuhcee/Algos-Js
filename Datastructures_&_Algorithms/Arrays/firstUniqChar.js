dee3419d8366c9b9822f7eae8d1185e238160a65
yuhcee <egbouchenna001@gmail.com>
2024-02-05 22:23:35 +0100

    :heavy_plus_sign:(firstUniqChar) add constraints

--- Datastructures_&_Algorithms/Arrays/firstUniqChar.js
+++ Datastructures_&_Algorithms/Arrays/firstUniqChar.js
@@ -5,6 +5,11 @@
  * its index*. If it does not
  * exist, return -1.
  *
+ * **Constraints:**
+ * 
+ * - `1 <= s.length <= 105`
+ * `s` consists of only lowercase English letters.
+ * 
  * @param {string} s
  * @return {number}
  */
