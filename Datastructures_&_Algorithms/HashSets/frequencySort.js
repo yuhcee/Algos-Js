d6796d2eab91dcf28ba2b164133112deff299484
yuhcee <egbouchenna001@gmail.com>
2024-02-07 02:11:34 +0100

    :books: (frequencySort) add task description

--- Datastructures_&_Algorithms/HashSets/frequencySort.js
+++ Datastructures_&_Algorithms/HashSets/frequencySort.js
@@ -2,7 +2,8 @@
  * **451. Sort Characters By Frequency**
  *
  * Given a string `s`, sort it in **decreasing order** based on the **frequency** of the characters.
- * The **frequency** of a character is the number of times it appears in the string.
+ * The **frequency** of a character is the number of times it appears in the 
+ * string.
  *
  * Return *the sorted string*. If there are multiple answers, return *any of them.*
  *
