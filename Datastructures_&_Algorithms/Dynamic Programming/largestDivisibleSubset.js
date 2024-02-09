7f80f135b040ceb4055f124088b5073df840df03
yuhcee <egbouchenna001@gmail.com>
2024-02-09 23:48:47 +0100

    :sparkles: (largestDivisibleSubset) add task solution

---
+++ Datastructures_&_Algorithms/Dynamic Programming/largestDivisibleSubset.js
@@ -0,0 +1,17 @@
+/**
+ * **368. Largest Divisible Subset**
+ *
+ *
+ * Given a set of **distinct** positive integers `nums`, return the largest
+ * subset `answer` such that every pair `(answer[i], answer[j])` of elements
+ * in this subset satisfies:
+ *
+ * - `answer[i] % answer[j] == 0`, or
+ * - `answer[j] % answer[i] == 0`
+ *
+ * If there are multiple solutions, return any of them.
+ *
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+const largestDivisibleSubset = function (nums) {};
