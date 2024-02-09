efdd220baf304607dcf0ebfaa948143522710fb1
yuhcee <egbouchenna001@gmail.com>
2024-02-09 23:50:29 +0100

    :sparkles: (largestDivisibleSubset) add task solution

--- Datastructures_&_Algorithms/Dynamic Programming/largestDivisibleSubset.js
+++ Datastructures_&_Algorithms/Dynamic Programming/largestDivisibleSubset.js
@@ -20,4 +20,34 @@
  * @param {number[]} nums
  * @return {number[]}
  */
-const largestDivisibleSubset = function (nums) {};
+const largestDivisibleSubset = function (nums) {
+    nums.sort((a, b) => a - b);
+    const n = nums.length;
+    const dp = new Array(n).fill(1);
+    const prev = new Array(n).fill(-1);
+
+    for (let i = 0; i < n; i++) {
+        for (let j = i - 1; j >= 0; j--) {
+            if (nums[i] % nums[j] === 0 && dp[i] < dp[j] + 1) {
+                dp[i] = dp[j] + 1;
+                prev[i] = j;
+            }
+        }
+    }
+
+    let maxIndex = 0;
+    for (let i = 1; i < n; i++) {
+        if (dp[i] > dp[maxIndex]) {
+            maxIndex = i;
+        }
+    }
+
+    const result = [];
+    let currIndex = maxIndex;
+    while (currIndex !== -1) {
+        result.push(nums[currIndex]);
+        currIndex = prev[currIndex];
+    }
+
+    return result.reverse();
+};
