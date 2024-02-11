3815656fe53c003ad809968bc81b2d76d47400de
yuhcee <egbouchenna001@gmail.com>
2024-02-11 20:03:44 +0100

    :white_check_mark: (cherryPickup) add test case 3

--- Datastructures_&_Algorithms/Dynamic Programming/cherryPickup.js
+++ Datastructures_&_Algorithms/Dynamic Programming/cherryPickup.js
@@ -75,3 +75,17 @@ Cherries taken by Robot #1, (3 + 2 + 5 + 2) = 12.
 Cherries taken by Robot #2, (1 + 5 + 5 + 1) = 12.
 Total of cherries: 12 + 12 = 24. */
 console.log(cherryPickup(grid));
+
+const grid1 = [
+    [1, 0, 0, 0, 0, 0, 1],
+    [2, 0, 0, 0, 0, 3, 0],
+    [2, 0, 9, 0, 0, 0, 0],
+    [0, 3, 0, 5, 4, 0, 0],
+    [1, 0, 2, 3, 0, 0, 6],
+];
+// Output: 28
+/* Explanation: Path of robot #1 and #2 are described in color green and blue respectively.
+Cherries taken by Robot #1, (1 + 9 + 5 + 2) = 17.
+Cherries taken by Robot #2, (1 + 3 + 4 + 3) = 11.
+Total of cherries: 17 + 11 = 28. */
+console.log(cherryPickup(grid1));
