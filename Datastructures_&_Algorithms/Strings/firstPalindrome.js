4ebc709a018d07560d45db5c7cbde9dd5a3d8512
yuhcee <egbouchenna001@gmail.com>
2024-02-13 21:43:03 +0100

    :books: (firstPalindrome) add task description

---
+++ Datastructures_&_Algorithms/Strings/firstPalindrome.js
@@ -0,0 +1,13 @@
+/**
+ * **2108. Find First Palindromic String in the Array**
+ *
+ * Given an array of strings `words`, return *the first **palindromic**
+ * string in the array*. If there is no such string, return an **empty
+ * string** `""`.
+ *
+ * A string is palindromic if it reads the same forward and backward.
+ *
+ * @param {string[]} words
+ * @return {string}
+ */
+const firstPalindrome = (words) => {};
