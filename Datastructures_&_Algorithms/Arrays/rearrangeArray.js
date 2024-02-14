e08c721ed9e8d2dafa5f5803b93c3feb66248fd1
yuhcee <egbouchenna001@gmail.com>
2024-02-14 23:04:23 +0100

    :white_check_mark: (rearrangeArray) add test case 1

--- Datastructures_&_Algorithms/Arrays/rearrangeArray.js
+++ Datastructures_&_Algorithms/Arrays/rearrangeArray.js
@@ -53,3 +53,11 @@ const rearrangeArray = (nums) => {
 
     return result;
 };
+
+const nums = [3, 1, -2, -5, 2, -4];
+// Output: [3,-2,1,-5,2,-4]
+/* Explanation:
+The positive integers in nums are [3,1,2]. The negative integers are [-2,-5,-4].
+The only possible way to rearrange them such that they satisfy all conditions is [3,-2,1,-5,2,-4].
+Other ways such as [1,-2,2,-5,3,-4], [3,1,2,-2,-5,-4], [-2,3,-5,1,-4,2] are incorrect because they do not satisfy one or more conditions. */
+console.log(rearrangeArray(nums));
