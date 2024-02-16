06e19ea0b966c86abec8f41e2356bc3724fe72c4
yuhcee <egbouchenna001@gmail.com>
2024-02-16 23:57:06 +0100

    :sparkles: (findLeastNumOfUniqueInts) add task solution

--- Datastructures_&_Algorithms/HashMaps/findLeastNumOfUniqueInts.js
+++ Datastructures_&_Algorithms/HashMaps/findLeastNumOfUniqueInts.js
@@ -14,4 +14,28 @@
  * @param {number} k
  * @return {number}
  */
-const findLeastNumOfUniqueInts = function (arr, k) {};
+const findLeastNumOfUniqueInts = function (arr, k) {
+    const frequencyMap = new Map();
+
+    // Step 1: Count the frequency of each integer
+    for (const num of arr) {
+        frequencyMap.set(num, (frequencyMap.get(num) || 0) + 1);
+    }
+
+    // Step 2: Sort the unique integers by their frequency in ascending order
+    const sortedUniqueIntegers = Array.from(frequencyMap.keys()).sort((a, b) => frequencyMap.get(a) - frequencyMap.get(b));
+
+    // Step 3: Iterate through the sorted unique integers and remove elements greedily until k elements have been removed
+    let uniqueCount = sortedUniqueIntegers.length;
+    for (const num of sortedUniqueIntegers) {
+        if (k >= frequencyMap.get(num)) {
+            k -= frequencyMap.get(num);
+            uniqueCount--;
+        } else {
+            break;
+        }
+    }
+
+    // Step 4: Return the number of remaining unique integers
+    return uniqueCount;
+};
