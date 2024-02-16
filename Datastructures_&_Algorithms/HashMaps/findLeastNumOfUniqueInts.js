b4c42b9d443101a1b578107bc31c79c54e2dcbf5
yuhcee <egbouchenna001@gmail.com>
2024-02-16 23:56:43 +0100

    :heavy_plus_sign:(findLeastNumOfUniqueInts) add constraints

--- Datastructures_&_Algorithms/HashMaps/findLeastNumOfUniqueInts.js
+++ Datastructures_&_Algorithms/HashMaps/findLeastNumOfUniqueInts.js
@@ -1,9 +1,15 @@
 /**
  * **1481. Least Number of Unique Integers after K Removals**
- * 
- * Given an array of integers `arr` and an integer `k`. Find *the least 
+ *
+ * Given an array of integers `arr` and an integer `k`. Find *the least
  * number of unique integers after removing **exactly** `k` elements.
- * 
+ *
+ * **Constraints:**
+ *
+ * - `1 <= arr.length <= 10^5`
+ * - `1 <= arr[i] <= 10^9`
+ * - `0 <= k <= arr.length`
+ *
  * @param {number[]} arr
  * @param {number} k
  * @return {number}
