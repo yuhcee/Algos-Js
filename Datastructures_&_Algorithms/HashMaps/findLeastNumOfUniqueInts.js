4332118c650ef73e17ba7e9c107b6975aa826647
yuhcee <egbouchenna001@gmail.com>
2024-02-16 23:56:02 +0100

    :books: (findLeastNumOfUniqueInts) add task description

---
+++ Datastructures_&_Algorithms/HashMaps/findLeastNumOfUniqueInts.js
@@ -0,0 +1,11 @@
+/**
+ * **1481. Least Number of Unique Integers after K Removals**
+ * 
+ * Given an array of integers `arr` and an integer `k`. Find *the least 
+ * number of unique integers after removing **exactly** `k` elements.
+ * 
+ * @param {number[]} arr
+ * @param {number} k
+ * @return {number}
+ */
+const findLeastNumOfUniqueInts = function (arr, k) {};
