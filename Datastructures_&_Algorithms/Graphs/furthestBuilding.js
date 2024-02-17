80c6ab41c888a35569c317cf57d1a170e3e5e85d
yuhcee <egbouchenna001@gmail.com>
2024-02-17 23:31:24 +0100

    :heavy_plus_sign:(furthestBuilding) add constraints

--- Datastructures_&_Algorithms/Graphs/furthestBuilding.js
+++ Datastructures_&_Algorithms/Graphs/furthestBuilding.js
@@ -1,21 +1,28 @@
 /**
  * **1642. Furthest Building You Can Reach**
  *
- * You are given an integer array `heights` representing the heights of 
+ * You are given an integer array `heights` representing the heights of
  * buildings, some `bricks`, and some `ladders`.
  *
- * You start your journey from building `0` and move to the next building by 
+ * You start your journey from building `0` and move to the next building by
  * possibly using bricks or ladders.
  *
  * While moving from building `i` to building `i+1` **(0-indexed)**,
  *
- * - If the current building's height is **greater than or equal** to the 
+ * - If the current building's height is **greater than or equal** to the
  * next building's height,you do not need a ladder or bricks.
- * - If the current building's height is **less than** the next building's 
+ * - If the current building's height is **less than** the next building's
  * height, you can either use one ladder or `(h[i+1] - h[i])` **bricks**.
  *
- * Return *the furthest building index (0-indexed) you can reach if you use 
+ * Return *the furthest building index (0-indexed) you can reach if you use
  * the given ladders and bricks optimally*.
+ *
+ * **Constraints:**
+ *
+ * - `1 <= heights.length <= 105`
+ * - `1 <= heights[i] <= 106`
+ * - `0 <= bricks <= 109`
+ * - `0 <= ladders <= heights.length`
  *
  * @param {number[]} heights
  * @param {number} bricks
