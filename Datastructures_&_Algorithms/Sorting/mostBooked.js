180bbf4254510121fa2788b92bbd0bbab7408292
yuhcee <egbouchenna001@gmail.com>
2024-02-18 22:40:13 +0100

    :white_check_mark: (mostBooked) add test case 2

--- Datastructures_&_Algorithms/Sorting/mostBooked.js
+++ Datastructures_&_Algorithms/Sorting/mostBooked.js
@@ -106,3 +106,23 @@ const n = 2,
 - At time 10, the meetings in both rooms finish. The fourth meeting starts in room 0 for the time period [10,11).
 Both rooms 0 and 1 held 2 meetings, so we return 0. */
 console.log(mostBooked(n, meetings));
+
+const n1 = 3,
+    meetings1 = [
+        [1, 20],
+        [2, 10],
+        [3, 5],
+        [4, 9],
+        [6, 8],
+    ];
+// Output: 1
+/* Explanation:
+- At time 1, all three rooms are not being used. The first meeting starts in room 0.
+- At time 2, rooms 1 and 2 are not being used. The second meeting starts in room 1.
+- At time 3, only room 2 is not being used. The third meeting starts in room 2.
+- At time 4, all three rooms are being used. The fourth meeting is delayed.
+- At time 5, the meeting in room 2 finishes. The fourth meeting starts in room 2 for the time period [5,10).
+- At time 6, all three rooms are being used. The fifth meeting is delayed.
+- At time 10, the meetings in rooms 1 and 2 finish. The fifth meeting starts in room 1 for the time period [10,12).
+Room 0 held 1 meeting while rooms 1 and 2 each held 2 meetings, so we return 1. */
+console.log(mostBooked(n1, meetings1));
