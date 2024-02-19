33f044af9f38b185752ebdc9ce2726efc8fddbcc
yuhcee <egbouchenna001@gmail.com>
2024-02-19 23:43:43 +0100

    :sparkles: (isPowerOfTwo) add task solution

--- Datastructures_&_Algorithms/Math/isPowerOfTwo.js
+++ Datastructures_&_Algorithms/Math/isPowerOfTwo.js
@@ -14,4 +14,8 @@
  * @param {number} n
  * @return {boolean}
  */
-const isPowerOfTwo = function (n) {};
+const isPowerOfTwo = function (n) {
+    if (n <= 0) return false;
+
+    return (n & (n - 1)) === 0;
+};
