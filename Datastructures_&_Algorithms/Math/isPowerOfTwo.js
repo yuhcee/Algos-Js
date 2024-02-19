48e95edd5cd936e46053d7810383488f760dfbb0
yuhcee <egbouchenna001@gmail.com>
2024-02-19 23:42:50 +0100

    :heavy_plus_sign:(isPowerOfTwo) add constraints

--- Datastructures_&_Algorithms/Math/isPowerOfTwo.js
+++ Datastructures_&_Algorithms/Math/isPowerOfTwo.js
@@ -1,11 +1,15 @@
 /**
  * **231. Power of Two**
  *
- * Given an integer `n`, return **`true` if it is a power of two. Otherwise, 
+ * Given an integer `n`, return **`true` if it is a power of two. Otherwise,
  * return `false`**.
  *
  * An integer `n` is a power of two, if there exists an integer `x` such
  * that `n == 2x`.
+ *
+ * **Constraints:**
+ *
+ * - `-231 <= n <= 231 - 1`
  *
  * @param {number} n
  * @return {boolean}
