9896137be6666d771bc2e5843e7c8104bf542a32
yuhcee <egbouchenna001@gmail.com>
2024-02-20 18:03:30 +0100

    :books: (missingNumber) add task description

--- Datastructures_&_Algorithms/Arrays/missingNumber.js
+++ Datastructures_&_Algorithms/Arrays/missingNumber.js
@@ -1,5 +1,4 @@
 /**
- *
  * **268. Missing Number**
  *
  * Given an array nums containing `n` distinct numbers in the range `[0, n]`,
@@ -37,4 +36,3 @@ const nums2 = [9, 6, 4, 2, 3, 5, 7, 0, 1];
 // Output: 8;
 // Explanation: n = 9 since there are 9 numbers, so all numbers are in the range [0,9]. 8 is the missing number in the range since it does not appear in nums.
 console.log(missingNumber(nums2));
-
