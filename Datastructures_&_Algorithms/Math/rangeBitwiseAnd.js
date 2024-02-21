221f6e13446e8d03082e842fb0632323922cb98a
yuhcee <egbouchenna001@gmail.com>
2024-02-21 23:38:32 +0100

    :heavy_plus_sign:(rangeBitwiseAnd) add constraints

--- Datastructures_&_Algorithms/Math/rangeBitwiseAnd.js
+++ Datastructures_&_Algorithms/Math/rangeBitwiseAnd.js
@@ -4,6 +4,10 @@
  * Given two integers `left` and `right` that represent the range `[left,
  * right]`,return *the bitwise AND of all numbers in this range, inclusive.*
  *
+ * **Constraints:**
+ *
+ * - `0 <= left <= right <= 231 - 1`
+ *
  * @param {number} left
  * @param {number} right
  * @return {number}
