4668f036447b01ac958438e5137ef904556a5889
yuhcee <egbouchenna001@gmail.com>
2024-02-22 22:24:21 +0100

    :books: (findJudge) add task description

--- Datastructures_&_Algorithms/Graphs/findJudge.js
+++ Datastructures_&_Algorithms/Graphs/findJudge.js
@@ -1,8 +1,8 @@
 /**
  * **997. Find the Town Judge**
  *
- * In a town, there are `n` people labeled from `1` to `n`. There is a rumor that one of these people
- * is secretly the town judge.
+ * In a town, there are `n` people labeled from `1` to `n`. There is a rumor 
+ * that one of these people is secretly the town judge.
  *
  * If the town judge exists, then:
  *
@@ -11,11 +11,11 @@
  * Everybody (except for the town judge) trusts the town judge.
  * There is exactly one person that satisfies properties **1** and **2**.
  *
- * You are given an array `trust` where `trust[i] = [ai, bi]` representing that the person labeled
- * `ai` trusts the person labeled `bi`.
+ * You are given an array `trust` where `trust[i] = [ai, bi]` representing 
+ * that the person labeled `ai` trusts the person labeled `bi`.
  *
- * Return *the label of the town judge if the town judge exists and can be identified, or return `-1`
- * otherwise*.
+ * Return *the label of the town judge if the town judge exists and can be 
+ * identified, or return `-1` otherwise*.
  *
  *
  * **Constraints:**
