35c5edfb39cc9eb6f61d25f20576bd3a18f568f1
yuhcee <egbouchenna001@gmail.com>
2024-02-23 19:42:59 +0100

    :books: (findCheapestPrice) add task description

--- Datastructures_&_Algorithms/Dynamic Programming/findCheapestPrice.js
+++ Datastructures_&_Algorithms/Dynamic Programming/findCheapestPrice.js
@@ -1,8 +1,8 @@
 /**
  * **787. Cheapest Flights Within K Stops**
  *
- * There are `n` cities connected by some number of flights. You are given an array `flights` where
- * `flights[i] = [fromi, toi, pricei]` indicates that there is a flight from city `fromi` to city
+ * There are `n` cities connected by some number of flights. You are given 
+ * an array `flights` where `flights[i] = [fromi, toi, pricei]` indicates that there is a flight from city `fromi` to city
  * `toi` with cost `pricei`.
  *
  * You are also given three integers `src`, `dst`, and `k`, return *the **cheapest price** from `src`
