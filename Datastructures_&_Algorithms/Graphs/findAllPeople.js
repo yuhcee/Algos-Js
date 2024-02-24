83fa1aa56e90d55be3ba0c910682a4992a68beac
yuhcee <egbouchenna001@gmail.com>
2024-02-24 08:43:40 +0100

    :white_check_mark: (findAllPeople) add test case 1

--- Datastructures_&_Algorithms/Graphs/findAllPeople.js
+++ Datastructures_&_Algorithms/Graphs/findAllPeople.js
@@ -93,3 +93,19 @@ const findAllPeople = function (n, meetings, firstPerson) {
     // Convert the set of known people to an array and return
     return [...knownSet];
 };
+
+const n = 6,
+    meetings = [
+        [1, 2, 5],
+        [2, 3, 8],
+        [1, 5, 10],
+    ],
+    firstPerson = 1;
+// Output: [0,1,2,3,5]
+/* Explanation:
+At time 0, person 0 shares the secret with person 1.
+At time 5, person 1 shares the secret with person 2.
+At time 8, person 2 shares the secret with person 3.
+At time 10, person 1 shares the secret with person 5.​​​​
+Thus, people 0, 1, 2, 3, and 5 know the secret after all the meetings. */
+console.log(findAllPeople(n, meetings, firstPerson));
