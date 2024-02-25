6435e3f62a576cb16e0e7f9dd80ae4cd2aae12f5
yuhcee <egbouchenna001@gmail.com>
2024-02-25 16:14:26 +0100

    :white_check_mark: (canTraverseAllPairs) add test case 2

--- Datastructures_&_Algorithms/Math/canTraverseAllPairs.js
+++ Datastructures_&_Algorithms/Math/canTraverseAllPairs.js
@@ -85,3 +85,8 @@ const nums = [2, 3, 6];
 To go from index 0 to index 1, we can use the sequence of traversals 0 -> 2 -> 1, where we move from index 0 to index 2 because gcd(nums[0], nums[2]) = gcd(2, 6) = 2 > 1, and then move from index 2 to index 1 because gcd(nums[2], nums[1]) = gcd(6, 3) = 3 > 1.
 To go from index 0 to index 2, we can just go directly because gcd(nums[0], nums[2]) = gcd(2, 6) = 2 > 1. Likewise, to go from index 1 to index 2, we can just go directly because gcd(nums[1], nums[2]) = gcd(3, 6) = 3 > 1. */
 console.log(canTraverseAllPairs(nums));
+
+const nums1 = [3, 9, 5];
+// Output: false
+// Explanation: No sequence of traversals can take us from index 0 to index 2 in this example. So, we return false.
+console.log(canTraverseAllPairs(nums1));
