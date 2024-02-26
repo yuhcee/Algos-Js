828bd83641a53fe51ca49684686ea7359a924c8e
yuhcee <egbouchenna001@gmail.com>
2024-02-26 23:11:02 +0100

    :sparkles: (isSameTree) add task solution

--- Datastructures_&_Algorithms/Trees/BinaryTree/isSameTree.js
+++ Datastructures_&_Algorithms/Trees/BinaryTree/isSameTree.js
@@ -25,4 +25,12 @@
  * @return {boolean}
  */
 
-const isSameTree = (p, q) => {};
+const isSameTree = (p, q) => {
+    if (p === null && q === null) {
+        return true;
+    }
+    if (p === null || q === null || p.val !== q.val) {
+        return false;
+    }
+    return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+};
