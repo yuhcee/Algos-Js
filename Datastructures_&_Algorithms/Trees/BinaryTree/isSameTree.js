6731c4f18a84c2df9d1d610e1a8cceebdea897ca
yuhcee <egbouchenna001@gmail.com>
2024-02-26 23:09:43 +0100

    :books: (isSameTree) add task description

---
+++ Datastructures_&_Algorithms/Trees/BinaryTree/isSameTree.js
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * **100. Same Tree**
+ *
+ * Given the roots of two binary trees `p` and `q`, write a function to
+ * check if they are the same or not.
+ *
+ * Two binary trees are considered the same if they are structurally
+ * identical, and the nodes have the same value.
+ *
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {boolean}
+ */
+
+const isSameTree = (p, q) => {};
