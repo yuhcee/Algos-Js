e913ffde4c331775fa0665aeee3355e9be7713a1
yuhcee <egbouchenna001@gmail.com>
2024-02-26 23:10:31 +0100

    :heavy_plus_sign:(isSameTree) add constraints

--- Datastructures_&_Algorithms/Trees/BinaryTree/isSameTree.js
+++ Datastructures_&_Algorithms/Trees/BinaryTree/isSameTree.js
@@ -15,6 +15,11 @@
  * Two binary trees are considered the same if they are structurally
  * identical, and the nodes have the same value.
  *
+ * **Constraints:**
+ *
+ * - The number of nodes in both trees is in the range `[0, 100]`.
+ * `-104 <= Node.val <= 104`
+ *
  * @param {TreeNode} p
  * @param {TreeNode} q
  * @return {boolean}
