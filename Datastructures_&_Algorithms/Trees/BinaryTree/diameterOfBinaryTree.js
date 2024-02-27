1b75de771812a1357dff4a1a497a4fbc9ffc957e
yuhcee <egbouchenna001@gmail.com>
2024-02-27 23:07:17 +0100

    :white_check_mark: (diameterOfBinaryTree) add test case 1

--- Datastructures_&_Algorithms/Trees/BinaryTree/diameterOfBinaryTree.js
+++ Datastructures_&_Algorithms/Trees/BinaryTree/diameterOfBinaryTree.js
@@ -48,3 +48,8 @@ const diameterOfBinaryTree = (root) => {
 
     return diameter;
 };
+
+const root = [1, 2, 3, 4, 5];
+// Output: 3
+// Explanation: 3 is the length of the path [4,2,1,3] or [5,2,1,3].
+console.log(diameterOfBinaryTree(root));
