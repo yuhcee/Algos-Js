ca26d79dd74f854774917d087fa932abca43cb74
yuhcee <egbouchenna001@gmail.com>
2024-02-27 23:02:51 +0100

    :books: (diameterOfBinaryTree) add task description

---
+++ Datastructures_&_Algorithms/Trees/BinaryTree/diameterOfBinaryTree.js
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * **543. Diameter of Binary Tree**
+ *
+ * Given the `root` of a binary tree, return *the length of the **diameter
+ * ** of the tree.*
+ *
+ * The **diameter**  of a binary tree is the **length**  of the longest path
+ * between any two nodes in a tree. This path may or may not pass through
+ * the `root`.
+ *
+ * The **length** of a path between two nodes is represented by the number
+ * of edges between them.
+ *
+ * @param {TreeNode} root
+ * @return {number}
+ */
+const diameterOfBinaryTree = (root) => {};
