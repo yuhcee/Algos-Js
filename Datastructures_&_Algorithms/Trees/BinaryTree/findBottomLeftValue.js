f3fc75310d63ac5aea8f6695f81724192225bbf3
yuhcee <egbouchenna001@gmail.com>
2024-02-28 05:09:11 +0100

    :heavy_plus_sign:(findBottomLeftValue) add constraints

--- Datastructures_&_Algorithms/Trees/BinaryTree/findBottomLeftValue.js
+++ Datastructures_&_Algorithms/Trees/BinaryTree/findBottomLeftValue.js
@@ -12,6 +12,11 @@
  * Given the `root` of a binary tree, return the leftmost value in the last
  * row of the tree.
  *
+ * **Constraints:**
+ *
+ * - The number of nodes in the tree is in the range `[1, 104]`.
+ * `-231 <= Node.val <= 231 - 1`
+ *
  * @param {TreeNode} root
  * @return {number}
  */
