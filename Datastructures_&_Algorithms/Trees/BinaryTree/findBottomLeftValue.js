8862a9f785c49fdb218958738aacfb04c12f5891
yuhcee <egbouchenna001@gmail.com>
2024-02-28 05:08:22 +0100

    :books: (findBottomLeftValue) add task description

---
+++ Datastructures_&_Algorithms/Trees/BinaryTree/findBottomLeftValue.js
@@ -0,0 +1,18 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * **513. Find Bottom Left Tree Value**
+ *
+ * Given the `root` of a binary tree, return the leftmost value in the last
+ * row of the tree.
+ *
+ * @param {TreeNode} root
+ * @return {number}
+ */
+const findBottomLeftValue = (root) => {};
