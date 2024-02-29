d0f4e9eac49b22e1911263413ebfb552c0fd6deb
yuhcee <egbouchenna001@gmail.com>
2024-02-29 23:55:08 +0100

    :white_check_mark: (isEvenOddTree) add test case 1

--- Datastructures_&_Algorithms/Trees/BinaryTree/isEvenOddTree.js
+++ Datastructures_&_Algorithms/Trees/BinaryTree/isEvenOddTree.js
@@ -58,3 +58,13 @@ const isEvenOddTree = (root) => {
 
     return true;
 };
+
+const root = [1, 10, 4, 3, null, 7, 9, 12, 8, 6, null, null, 2];
+// Output: true
+/* Explanation: The node values on each level are:
+Level 0: [1]
+Level 1: [10,4]
+Level 2: [3,7,9]
+Level 3: [12,8,6,2]
+Since levels 0 and 2 are all odd and increasing and levels 1 and 3 are all even and decreasing, the tree is Even-Odd. */
+console.log(isEvenOddTree(root));
