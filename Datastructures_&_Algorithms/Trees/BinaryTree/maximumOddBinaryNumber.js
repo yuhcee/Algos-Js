be4a1e82fdd27379f3111d3d8067ae04701112e9
yuhcee <egbouchenna001@gmail.com>
2024-03-01 22:35:39 +0100

    :books: (maximumOddBinaryNumber) add task description

---
+++ Datastructures_&_Algorithms/Trees/BinaryTree/maximumOddBinaryNumber.js
@@ -0,0 +1,18 @@
+/**
+ * **2864. Maximum Odd Binary Number**
+ *
+ * You are given a **binary** string s that contains at least one '1'.
+ *
+ * You have to **rearrange** the bits in such a way that the resulting
+ * binary number is the **maximum odd binary number** that can be created
+ * from this combination.
+ *
+ * Return *a string representing the maximum odd binary number that can be
+ * created from the given combination.*
+ *
+ * **Note** that the resulting string can have leading zeros.
+ * 
+ * @param {string} s
+ * @return {string}
+ */
+const maximumOddBinaryNumber = function (s) {};
