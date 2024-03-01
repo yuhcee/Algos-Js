a6942c8ee584eda778ddd8dce596e7de87784acb
yuhcee <egbouchenna001@gmail.com>
2024-03-01 22:37:37 +0100

    :sparkles: (maximumOddBinaryNumber) add task solution

--- Datastructures_&_Algorithms/Trees/BinaryTree/maximumOddBinaryNumber.js
+++ Datastructures_&_Algorithms/Trees/BinaryTree/maximumOddBinaryNumber.js
@@ -21,4 +21,13 @@
  * @param {string} s
  * @return {string}
  */
-const maximumOddBinaryNumber = function (s) {};
+const maximumOddBinaryNumber = (s) => {
+    let chars = s.split('').sort().reverse();
+    for (let i = chars.length - 1; i >= 0; i--) {
+        if (chars[i] === '1') {
+            [chars[i], chars[chars.length - 1]] = [chars[chars.length - 1], chars[i]];
+            break;
+        }
+    }
+    return chars.join('');
+};
