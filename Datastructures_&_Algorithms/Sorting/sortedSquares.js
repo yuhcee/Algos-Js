11c7713d9c7fd0790d9429038fc809becebc1e30
yuhcee <egbouchenna001@gmail.com>
2024-03-02 09:01:44 +0100

    :sparkles: (sortedSquares) add task solution

--- Datastructures_&_Algorithms/Sorting/sortedSquares.js
+++ Datastructures_&_Algorithms/Sorting/sortedSquares.js
@@ -14,4 +14,10 @@
  * @param {number[]} nums
  * @return {number[]}
  */
-const sortedSquares = (nums) => {};
+const sortedSquares = (nums) => {
+    for (let i = 0; i < nums.length; i++) {
+        nums[i] = nums[i] * nums[i];
+    }
+
+    return nums.sort((a, b) => a - b);
+};
