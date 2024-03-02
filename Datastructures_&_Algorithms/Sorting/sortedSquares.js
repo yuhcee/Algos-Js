426569cfc1f94c17ce46a38901f3e1a5e308670f
yuhcee <egbouchenna001@gmail.com>
2024-03-02 09:03:03 +0100

    :white_check_mark: (sortedSquares) add test case 2

--- Datastructures_&_Algorithms/Sorting/sortedSquares.js
+++ Datastructures_&_Algorithms/Sorting/sortedSquares.js
@@ -27,3 +27,7 @@ const nums = [-4, -1, 0, 3, 10];
 /* Explanation: After squaring, the array becomes [16,1,0,9,100].
 After sorting, it becomes [0,1,9,16,100]. */
 console.log(sortedSquares(nums));
+
+const nums1 = [-7, -3, 2, 3, 11];
+// Output: [4,9,9,49,121]
+console.log(sortedSquares(nums1));
