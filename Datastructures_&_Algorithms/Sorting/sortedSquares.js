21b0614716d5f5e41bc108dc7d9ac9d01c0a819a
yuhcee <egbouchenna001@gmail.com>
2024-03-02 09:01:16 +0100

    :heavy_plus_sign:(sortedSquares) add constraints

--- Datastructures_&_Algorithms/Sorting/sortedSquares.js
+++ Datastructures_&_Algorithms/Sorting/sortedSquares.js
@@ -1,10 +1,16 @@
 /**
  * **977. Squares of a Sorted Array**
- * 
- * Given an integer array `nums` sorted in **non-decreasing** order, return 
- * *an array of **the squares of each number** sorted in non-decreasing 
+ *
+ * Given an integer array `nums` sorted in **non-decreasing** order, return
+ * *an array of **the squares of each number** sorted in non-decreasing
  * order.*
- * 
+ *
+ * **Constraints:**
+ *
+ * - `1 <= nums.length <= 104`
+ * - `-104 <= nums[i] <= 104`
+ * - `nums` is sorted in **non-decreasing** order
+ *
  * @param {number[]} nums
  * @return {number[]}
  */
