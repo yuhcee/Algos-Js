3de334abb337f2da5c100e7c2c079dbf51e44cb4
yuhcee <egbouchenna001@gmail.com>
2024-03-03 07:34:02 +0100

    :books: (removeNthFromEnd) add task description

--- Datastructures_&_Algorithms/Lists/removeNthFromEnd.js
+++ Datastructures_&_Algorithms/Lists/removeNthFromEnd.js
@@ -8,8 +8,8 @@
 /**
  * **19. Remove Nth Node From End of List**
  *
- * Given the `head` of a linked list, remove the `nth` node from the end of the list and
- * return its head.
+ * Given the `head` of a linked list, remove the `nth` node from the end of
+ * the list and return its head.
  *
  * @param {ListNode} head
  * @param {number} n
