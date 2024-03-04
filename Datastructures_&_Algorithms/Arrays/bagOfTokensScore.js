f08c42fd1039c856c1810a81e412f1f00f9a7089
yuhcee <egbouchenna001@gmail.com>
2024-03-04 06:38:17 +0100

    :white_check_mark: (bagOfTokensScore) add test case 3

--- Datastructures_&_Algorithms/Arrays/bagOfTokensScore.js
+++ Datastructures_&_Algorithms/Arrays/bagOfTokensScore.js
@@ -63,3 +63,14 @@ const tokens1 = [200, 100],
 /* Explanation: Play token1 (100) face-up, reducing your power to 50 and increasing your score to 1.
 There is no need to play token0, since you cannot play it face-up to add to your score. The maximum score achievable is 1. */
 console.log(bagOfTokensScore(tokens1, power1));
+
+const tokens2 = [100, 200, 300, 400],
+    power2 = 200;
+// Output: 2
+/* Explanation: Play the tokens in this order to get a score of 2:
+Play token0 (100) face-up, reducing power to 100 and increasing score to 1.
+Play token3 (400) face-down, increasing power to 500 and reducing score to 0.
+Play token1 (200) face-up, reducing power to 300 and increasing score to 1.
+Play token2 (300) face-up, reducing power to 0 and increasing score to 2.
+The maximum score achievable is 2. */
+console.log(bagOfTokensScore(tokens2, power2));
