28c599deb9f70490480dfa1360af3343d4f1c9ab
yuhcee <egbouchenna001@gmail.com>
2024-03-05 19:10:40 +0100

    :books: (minimumLength) add task description

---
+++ Datastructures_&_Algorithms/Arrays/minimumLength.js
@@ -0,0 +1,24 @@
+/**
+ * **1750. Minimum Length of String After Deleting Similar Ends**
+ *
+ * Given a string `s` consisting only of characters `'a'`, `'b'`, and `'c'`.
+ * You are asked to apply the following algorithm on the string any number
+ * of times:
+ *
+ * 1. Pick a non-empty prefix from the string `s` where all the characters
+ * in the prefix are equal.
+ * 2. Pick a non-empty suffix from the string `s` where all the characters in
+ * this suffix are equal.
+ * 3. The prefix and the suffix should not intersect at any index.
+ * 4. The characters from the prefix and suffix must be the same.
+ * 5. Delete both the prefix and the suffix.
+ *
+ * Return the **minimum length** of s after performing the above operation
+ * any number of times (possibly zero times).
+ *
+ * Returns the minimum length of string after deleting similar ends.
+ * @param {string} s The input string consisting only of characters 'a',
+ * 'b', and 'c'.
+ * @return {number} The minimum length of string after deletion.
+ */
+const minimumLength = (s) => {};
