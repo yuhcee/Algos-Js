6a3aa1d3c21cf74949c9a5b7710918201517bd21
yuhcee <egbouchenna001@gmail.com>
2024-03-07 11:16:41 +0100

    :books: (middleNode) add task description

---
+++ Datastructures_&_Algorithms/Lists/middleNode.js
@@ -0,0 +1,19 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val === undefined ? 0 : val)
+ *     this.next = (next === undefined ? null : next)
+ * }
+ */
+/**
+ * **876. Middle of the Linked List**
+ *
+ * Given the head of a singly linked list, return the middle node of the
+ * linked list.
+ *
+ * If there are two middle nodes, return the second middle node.
+ *
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+const middleNode = (head) => {};
