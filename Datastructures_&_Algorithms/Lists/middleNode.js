cc551d6ec5cf46cc3078a560db2addda255353e6
yuhcee <egbouchenna001@gmail.com>
2024-03-07 11:17:29 +0100

    :heavy_plus_sign:(middleNode) add constraints

--- Datastructures_&_Algorithms/Lists/middleNode.js
+++ Datastructures_&_Algorithms/Lists/middleNode.js
@@ -13,6 +13,11 @@
  *
  * If there are two middle nodes, return the second middle node.
  *
+ * **Constraints:**
+ *
+ * - The number of nodes in the list is in the range `[1, 100]`.
+ * - `1 <= Node.val <= 100`
+ *
  * @param {ListNode} head
  * @return {ListNode}
  */
