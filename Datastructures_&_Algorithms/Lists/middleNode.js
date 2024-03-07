d2d70cd7d51ef9e41ed55a2b805df988c858ea17
yuhcee <egbouchenna001@gmail.com>
2024-03-07 11:19:56 +0100

    :white_check_mark: (middleNode) add test case 2

--- Datastructures_&_Algorithms/Lists/middleNode.js
+++ Datastructures_&_Algorithms/Lists/middleNode.js
@@ -40,3 +40,8 @@ const head = [1, 2, 3, 4, 5];
 // Output: [3,4,5]
 // Explanation: The middle node of the list is node 3.
 console.log(middleNode(head));
+
+const head1 = [1, 2, 3, 4, 5, 6];
+// Output: [4,5,6]
+// Explanation: Since the list has two middle nodes with values 3 and 4, we return the second one.
+console.log(middleNode(head1));
