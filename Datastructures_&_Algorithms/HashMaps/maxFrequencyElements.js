6e15e640cd329a0b3f957379e80136839edf3ca4
yuhcee <egbouchenna001@gmail.com>
2024-03-08 23:17:49 +0100

    :white_check_mark: (maxFrequencyElements) add test case 1

--- Datastructures_&_Algorithms/HashMaps/maxFrequencyElements.js
+++ Datastructures_&_Algorithms/HashMaps/maxFrequencyElements.js
@@ -40,3 +40,9 @@ const maxFrequencyElements = (nums) => {
 
     return count;
 };
+
+const nums = [1, 2, 2, 3, 1, 4];
+// Output: 4
+/* Explanation: The elements 1 and 2 have a frequency of 2 which is the maximum frequency in the array.
+So the number of elements in the array with maximum frequency is 4. */
+console.log(maxFrequencyElements(nums));
