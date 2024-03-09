6863ce2b9ae4994858c97d89dfa93d8bd8e6339c
yuhcee <egbouchenna001@gmail.com>
2024-03-09 02:52:13 +0100

    :books: (getCommon) add task description

---
+++ Datastructures_&_Algorithms/Arrays/getCommon.js
@@ -0,0 +1,15 @@
+/**
+ * **2540. Minimum Common Value**
+ *
+ * Given two integer arrays `nums1` and `nums2`, sorted in non-decreasing
+ * order, return the **minimum integer common** to both arrays. If there is
+ * no common integer amongst `nums1` and `nums2`, return -1.
+ *
+ * Note that an integer is said to be **common** to `nums1` and `nums2` if
+ * both arrays have **at least one** occurrence of that integer.
+ *
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ */
+const getCommon = (nums1, nums2) => {};
