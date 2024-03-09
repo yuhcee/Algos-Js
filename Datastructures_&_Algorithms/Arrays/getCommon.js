9ad3b5e972939d3e62426908a8402b291ce8dcd7
yuhcee <egbouchenna001@gmail.com>
2024-03-09 02:53:22 +0100

    :heavy_plus_sign:(getCommon) add constraints

--- Datastructures_&_Algorithms/Arrays/getCommon.js
+++ Datastructures_&_Algorithms/Arrays/getCommon.js
@@ -8,6 +8,12 @@
  * Note that an integer is said to be **common** to `nums1` and `nums2` if
  * both arrays have **at least one** occurrence of that integer.
  *
+ * **Constraints:**
+ * 
+ * - `1 <= nums1.length, nums2.length <= 105`
+ * - `1 <= nums1[i], nums2[j] <= 109`
+ * - `Both `nums1` and `nums2` are sorted in **non-decreasing** order.
+
  * @param {number[]} nums1
  * @param {number[]} nums2
  * @return {number}
