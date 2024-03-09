ee1bb3d89cc3dfff31ebf0035741133811b6f9fc
yuhcee <egbouchenna001@gmail.com>
2024-03-09 02:54:13 +0100

    :sparkles: (getCommon) add task solution

--- Datastructures_&_Algorithms/Arrays/getCommon.js
+++ Datastructures_&_Algorithms/Arrays/getCommon.js
@@ -18,4 +18,18 @@
  * @param {number[]} nums2
  * @return {number}
  */
-const getCommon = (nums1, nums2) => {};
+const getCommon = (nums1, nums2) => {
+    let i = 0;
+    let j = 0;
+
+    while (i < nums1.length && j < nums2.length) {
+        if (nums1[i] === nums2[j]) {
+            return nums1[i];
+        } else if (nums1[i] < nums2[j]) {
+            i++;
+        } else {
+            j++;
+        }
+    }
+    return -1;
+};
