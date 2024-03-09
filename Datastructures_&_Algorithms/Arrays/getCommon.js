4a6fd2cb36df300d2aebcee3756bc455cc4e96e0
yuhcee <egbouchenna001@gmail.com>
2024-03-09 02:55:03 +0100

    :white_check_mark: (getCommon) add test case 1

--- Datastructures_&_Algorithms/Arrays/getCommon.js
+++ Datastructures_&_Algorithms/Arrays/getCommon.js
@@ -33,3 +33,9 @@ const getCommon = (nums1, nums2) => {
     }
     return -1;
 };
+
+const nums1 = [1, 2, 3],
+    nums2 = [2, 4];
+// Output: 2
+// Explanation: The smallest element common to both arrays is 2, so we return 2.
+console.log(getCommon(nums1,nums 2));
