b6db61cb3eda2c6041c06e37afd4c4eecb10a9e9
yuhcee <egbouchenna001@gmail.com>
2024-03-10 11:43:32 +0100

    :books: (intersection) add task description

---
+++ Datastructures_&_Algorithms/HashSets/intersection.js
@@ -0,0 +1,12 @@
+/**
+ * **349. Intersection of Two Arrays**
+ *
+ * Given two integer arrays `nums1` and `nums2`, return *an array of their
+ * intersection*. Each element in the result must be **unique** and you may
+ * return the result in **any order**.
+ *
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+const intersection = (nums1, nums2) => {};
