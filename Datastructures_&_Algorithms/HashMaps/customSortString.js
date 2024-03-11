11c4396a09150652e912ff6cecd2e37bb3d97534
yuhcee <egbouchenna001@gmail.com>
2024-03-11 21:43:16 +0100

    :white_check_mark: (customSortString) add test case 2

--- Datastructures_&_Algorithms/HashMaps/customSortString.js
+++ Datastructures_&_Algorithms/HashMaps/customSortString.js
@@ -51,3 +51,10 @@ const order = 'cba',
 /* Explanation: "a", "b", "c" appear in order, so the order of "a", "b", "c" should be "c", "b", and "a".
 Since "d" does not appear in order, it can be at any position in the returned string. "dcba", "cdba", "cbda" are also valid outputs. */
 console.log(customSortString(order, s));
+
+const order1 = 'bcafg',
+    s1 = 'abcd';
+// Output:  "bcad"
+/* Explanation: The characters "b", "c", and "a" from order dictate the order for the characters in s. The character "d" in s does not appear in order, so its position is flexible.
+Following the order of appearance in order, "b", "c", and "a" from s should be arranged as "b", "c", "a". "d" can be placed at any position since it's not in order. The output "bcad" correctly follows this rule. Other arrangements like "bacd" or "bcda" would also be valid, as long as "b", "c", "a" maintain their order. */
+console.log(customSortString(order1, s1));
