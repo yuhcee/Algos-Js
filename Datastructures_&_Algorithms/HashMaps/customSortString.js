79e71f35d980222e5d092d1abe763c88352551bf
yuhcee <egbouchenna001@gmail.com>
2024-03-11 21:42:28 +0100

    :white_check_mark: (customSortString) add test case 1

--- Datastructures_&_Algorithms/HashMaps/customSortString.js
+++ Datastructures_&_Algorithms/HashMaps/customSortString.js
@@ -45,3 +45,9 @@ const customSortString = (order, s) => {
 
     return result;
 };
+const order = 'cba',
+    s = 'abcd';
+// Output:  "cbad"
+/* Explanation: "a", "b", "c" appear in order, so the order of "a", "b", "c" should be "c", "b", and "a".
+Since "d" does not appear in order, it can be at any position in the returned string. "dcba", "cdba", "cbda" are also valid outputs. */
+console.log(customSortString(order, s));
