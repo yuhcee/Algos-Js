01d2045aca18d58a07382c747d898533d1df0976
yuhcee <egbouchenna001@gmail.com>
2024-03-11 21:38:24 +0100

    :books: (customSortString) add task description

---
+++ Datastructures_&_Algorithms/HashMaps/customSortString.js
@@ -0,0 +1,18 @@
+/**
+ * **791. Custom Sort String**
+ *
+ * You are given two strings `order` and `s`. All the characters of order
+ * are **unique** and were sorted in some custom order previously.
+ *
+ * Permute the characters of `s` so that they match the order that `order`
+ * was sorted. More specifically, if a character `x` occurs before a
+ * character `y` in `order`, then `x` should occur before `y` in the
+ * permuted string.
+ *
+ * Return *any permutation of s that satisfies this property.*
+ *
+ * @param {string} order
+ * @param {string} s
+ * @return {string}
+ */
+const customSortString = (order, s) => {};
