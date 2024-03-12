a78252a49a0308d7219c24842926dfffac174500
yuhcee <egbouchenna001@gmail.com>
2024-03-12 23:03:34 +0100

    :books: (pivotInteger) add task description

---
+++ Datastructures_&_Algorithms/Math/pivotInteger.js
@@ -0,0 +1,16 @@
+/**
+ * **2485. Find the Pivot Integer**
+ *
+ * Given a positive integer `n`, find the **pivot integer** `x` such that:
+ *
+ * - The sum of all elements between `1` and `x` inclusively equals the sum
+ * of all elements between `x` and `n` inclusively.
+ *
+ * Return *the pivot integer `x`. If no such integer exists, return `-1`. It
+ * is guaranteed that there will be at most one pivot index for the given
+ * input.
+ *
+ * @param {number} n
+ * @return {number}
+ */
+const pivotInteger = (n) => {};
