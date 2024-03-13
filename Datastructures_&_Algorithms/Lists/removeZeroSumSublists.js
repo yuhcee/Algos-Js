87e89bc9786f5b42e405b9b545cd30a1a55df329
yuhcee <egbouchenna001@gmail.com>
2024-03-13 23:51:09 +0100

    :books: (removeZeroSumSublists) add task description

---
+++ Datastructures_&_Algorithms/Lists/removeZeroSumSublists.js
@@ -0,0 +1,23 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * **1171. Remove Zero Sum Consecutive Nodes from Linked List**
+ *
+ * Given the `head` of a linked list, we repeatedly delete consecutive
+ * sequences of nodes that sum to `0` until there are no such sequences.
+ *
+ * After doing so, return the head of the final linked list.  You may return
+ * any such answer.
+ *
+ * (Note that in the examples below, all sequences are serializations of
+ * `ListNode` objects.)
+ *
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+const removeZeroSumSublists = (head) => {};
