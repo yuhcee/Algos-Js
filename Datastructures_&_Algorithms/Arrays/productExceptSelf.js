6e0bbda5a024bd04a696f26ae03cc42ed57ceff8
yuhcee <egbouchenna001@gmail.com>
2024-03-15 00:03:05 +0100

    :sparkles: (productExceptSelf) add task solution

--- Datastructures_&_Algorithms/Arrays/productExceptSelf.js
+++ Datastructures_&_Algorithms/Arrays/productExceptSelf.js
@@ -21,4 +21,27 @@
  * @param {number[]} nums
  * @return {number[]}
  */
-const productExceptSelf = function (nums) {};
+const productExceptSelf = (nums) => {
+    const prefix = new Array(nums.length);
+    const suffix = new Array(nums.length);
+    const result = [];
+
+    // Compute prefix product
+    prefix[0] = 1;
+    for (let i = 1; i < nums.length; i++) {
+        prefix[i] = prefix[i - 1] * nums[i - 1];
+    }
+
+    // Compute suffix product
+    suffix[nums.length - 1] = 1;
+    for (let i = nums.length - 2; i >= 0; i--) {
+        suffix[i] = suffix[i + 1] * nums[i + 1];
+    }
+
+    // Compute result
+    for (let i = 0; i < nums.length; i++) {
+        result.push(prefix[i] * suffix[i]);
+    }
+
+    return result;
+};
