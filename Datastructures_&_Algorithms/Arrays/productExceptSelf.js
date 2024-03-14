432776613e39829a7d3531ba862071c76e099e25
yuhcee <egbouchenna001@gmail.com>
2024-03-15 00:01:41 +0100

    :books: (productExceptSelf) add task description

---
+++ Datastructures_&_Algorithms/Arrays/productExceptSelf.js
@@ -0,0 +1,17 @@
+/**
+ * **238. Product of Array Except Self**
+ *
+ * Given an integer array `nums`, return *an array answer such that `answer
+ * [i]` is equal to the product of all the elements of `nums` except `nums[i]
+ * `.*
+ *
+ * The product of any prefix or suffix of `nums` is guaranteed to fit in a
+ * **32-bit** integer.
+ *
+ * You must write an algorithm that runs in `O(n)` time and without using
+ * the division operation.
+ *
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+const productExceptSelf = function (nums) {};
