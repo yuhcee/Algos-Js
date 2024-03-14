e0b814c5c364035554b4c62e83fa7662224c183a
yuhcee <egbouchenna001@gmail.com>
2024-03-14 17:00:29 +0100

    :books: (numSubarraysWithSum) add task description

---
+++ Datastructures_&_Algorithms/HashMaps/numSubarraysWithSum.js
@@ -0,0 +1,13 @@
+/**
+ * **930. Binary Subarrays With Sum**
+ *
+ * Given a binary array `nums` and an integer `goal`, return *the number of
+ * non-empty subarrays with a sum `goal`*.
+ *
+ * A **subarray** is a contiguous part of the array.
+ *
+ * @param {number[]} nums
+ * @param {number} goal
+ * @return {number}
+ */
+const numSubarraysWithSum = (nums, goal) => {};
