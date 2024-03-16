82d09fecc209e8c5fe52b20cab1543f4a229d5b2
yuhcee <egbouchenna001@gmail.com>
2024-03-16 19:47:55 +0100

    :heavy_plus_sign:(findMaxLength) add constraints

--- Datastructures_&_Algorithms/HashMaps/findMaxLength.js
+++ Datastructures_&_Algorithms/HashMaps/findMaxLength.js
@@ -4,6 +4,11 @@
  * Given a binary array `nums`, return *the maximum length of a contiguous
  * subarray with an equal number of `0` and `1`*.
  *
+ * **Constraints:**
+ *
+ * - `1 <= nums.length <= 105`
+ * - `nums[i]` is either `0` or `1`.
+ *
  * @param {number[]} nums
  * @return {number}
  */
