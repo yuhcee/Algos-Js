db106065560fc3d491e9abcec5e089beefce9ba9
yuhcee <egbouchenna001@gmail.com>
2024-03-16 19:48:57 +0100

    :sparkles: (findMaxLength) add task solution

--- Datastructures_&_Algorithms/HashMaps/findMaxLength.js
+++ Datastructures_&_Algorithms/HashMaps/findMaxLength.js
@@ -12,4 +12,21 @@
  * @param {number[]} nums
  * @return {number}
  */
-const findMaxLength = function (nums) {};
+const findMaxLength = function (nums) {
+    const map = new Map();
+    let maxLen = 0;
+    let count = 0;
+
+    map.set(0, -1); // Initialize with count 0 at index -1 to handle the case when the equal number of 0s and 1s starts from index 0
+
+    for (let i = 0; i < nums.length; i++) {
+        count += nums[i] === 0 ? -1 : 1;
+        if (map.has(count)) {
+            maxLen = Math.max(maxLen, i - map.get(count));
+        } else {
+            map.set(count, i);
+        }
+    }
+
+    return maxLen;
+};
