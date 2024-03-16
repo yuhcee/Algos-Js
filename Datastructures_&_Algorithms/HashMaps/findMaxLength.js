4fcd86468c7b5bdc36bcc31b6dd9835424e6313f
yuhcee <egbouchenna001@gmail.com>
2024-03-16 19:46:53 +0100

    :books: (findMaxLength) add task description

---
+++ Datastructures_&_Algorithms/HashMaps/findMaxLength.js
@@ -0,0 +1,10 @@
+/**
+ * **525. Contiguous Array**
+ *
+ * Given a binary array `nums`, return *the maximum length of a contiguous
+ * subarray with an equal number of `0` and `1`*.
+ *
+ * @param {number[]} nums
+ * @return {number}
+ */
+const findMaxLength = function (nums) {};
