da384836cfd37d13220da208ccab3926c8a2b20f
yuhcee <egbouchenna001@gmail.com>
2024-03-16 19:50:29 +0100

    :white_check_mark: (findMaxLength) add test case 2

--- Datastructures_&_Algorithms/HashMaps/findMaxLength.js
+++ Datastructures_&_Algorithms/HashMaps/findMaxLength.js
@@ -35,3 +35,8 @@ const nums = [0, 1];
 // Output: 2
 // Explanation: [0, 1] is the longest contiguous subarray with an equal number of 0 and 1.
 console.log(findMaxLength(nums));
+
+const nums1 = [0, 1, 0];
+// Output: 2
+// Explanation: [0, 1] (or [1, 0]) is a longest contiguous subarray with equal number of 0 and 1.
+console.log(findMaxLength(nums1));
