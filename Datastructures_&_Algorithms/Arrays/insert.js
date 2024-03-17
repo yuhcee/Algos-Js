3d82401e908bd68a54cc3ff29bb18acd8a293af4
yuhcee <egbouchenna001@gmail.com>
2024-03-17 14:04:55 +0100

    :books: (insert) add task description

---
+++ Datastructures_&_Algorithms/Arrays/insert.js
@@ -0,0 +1,22 @@
+/**
+ * **57. Insert Interval**
+ * You are given an array of non-overlapping intervals `intervals` where
+ * `intervals[i] = [starti, endi]` represent the start and the end of the
+ * `ith` interval and `intervals` is sorted in ascending order by `starti`.
+ * You are also given an interval `newInterval = [start, end]` that
+ * represents the start and end of another interval.
+ *
+ * Insert `newInterval` into `intervals` such that `intervals` is still
+ * sorted in ascending order by `starti` and `intervals` still does not have
+ * any overlapping intervals (merge overlapping intervals if necessary).
+ *
+ * Return *`intervals` after the insertion.*
+ *
+ * **Note** that you don't need to modify `intervals` in-place. You can make
+ * a new array and return it.
+ *
+ * @param {number[][]} intervals
+ * @param {number[]} newInterval
+ * @return {number[][]}
+ */
+const insert = (intervals, newInterval) => {};
