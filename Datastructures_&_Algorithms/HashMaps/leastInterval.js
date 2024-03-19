5f54ef2daec7cf1a3bf6ad2779115fb9d3957153
yuhcee <egbouchenna001@gmail.com>
2024-03-19 14:31:26 +0100

    :books: (leastInterval) add task description

---
+++ Datastructures_&_Algorithms/HashMaps/leastInterval.js
@@ -0,0 +1,15 @@
+/**
+ * **621. Task Scheduler**
+ *
+ * You are given an array of CPU `tasks`, each represented by letters A to Z, and a cooling time,
+ * `n`. Each cycle or interval allows the completion of one task. Tasks can be completed in any
+ * order, but there's a constraint: **identical** tasks must be separated by at least `n` intervals
+ * due to cooling time.
+ *
+ * ​Return the *minimum number of intervals* required to complete all tasks.
+ *
+ * @param {character[]} tasks
+ * @param {number} n
+ * @return {number}
+ */
+const leastInterval = (tasks, n) => {};
