bc56fe45796fb17f22fa613bb344129e8e149b1a
yuhcee <egbouchenna001@gmail.com>
2024-03-20 06:47:24 +0100

    :books: (mergeInBetween) add task description

---
+++ Datastructures_&_Algorithms/Lists/mergeInBetween.js
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * **1669. Merge In Between Linked Lists**
+ *
+ * You are given two linked lists: `list1` and `list2` of sizes `n` and `m` respectively.
+ *
+ * Remove `list1`'s nodes from the `ath` node to the `bth` node, and put `list2` in their place.
+ *
+ * The blue edges and nodes in the following figure indicate the result:
+ *
+ * *Build the result list and return its head.*
+ *
+ * @param {ListNode} list1
+ * @param {number} a
+ * @param {number} b
+ * @param {ListNode} list2
+ * @return {ListNode}
+ */
+const mergeInBetween = (list1, a, b, list2) => {};
