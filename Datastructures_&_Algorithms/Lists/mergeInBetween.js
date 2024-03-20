d5d83082881592f502b2c169eea3c8f1f65aa3a6
yuhcee <egbouchenna001@gmail.com>
2024-03-20 06:57:13 +0100

    :white_check_mark: (mergeInBetween) add test case 1

--- Datastructures_&_Algorithms/Lists/mergeInBetween.js
+++ Datastructures_&_Algorithms/Lists/mergeInBetween.js
@@ -66,3 +66,11 @@ const mergeInBetween = (list1, a, b, list2) => {
 
     return list1;
 };
+
+const list1 = [10, 1, 13, 6, 9, 5],
+    a = 3,
+    b = 4,
+    list2 = [1000000, 1000001, 1000002];
+// Output: [10,1,13,1000000,1000001,1000002,5]
+// Explanation: We remove the nodes 3 and 4 and put the entire list2 in their place. The blue edges and nodes in the above figure indicate the result.
+console.log(mergeInBetween(list1, a, b, list2));
