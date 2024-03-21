eadff38d111c015ee7accab4424adc6239ca7efa
yuhcee <egbouchenna001@gmail.com>
2024-03-21 15:17:10 +0100

    :books: (reverseList) add task description

---
+++ Datastructures_&_Algorithms/Lists/reverseList.js
@@ -0,0 +1,17 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * **206. Reverse Linked List**
+ *
+ * Given the `head` of a singly linked list, reverse the list, and return
+ * *the reversed list*.
+ *
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+const reverseList = function (head) {};
