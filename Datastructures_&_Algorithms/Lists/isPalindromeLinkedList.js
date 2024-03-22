a46dacf0fff30180480df4209cd2df3d1d860136
yuhcee <egbouchenna001@gmail.com>
2024-03-22 14:48:31 +0100

    :books: (isPalindromeLinkedList) add task description

--- Datastructures_&_Algorithms/Lists/isPalindromeLinkedList.js
+++ Datastructures_&_Algorithms/Lists/isPalindromeLinkedList.js
@@ -8,7 +8,8 @@
 /**
  * **234. Palindrome Linked List**
  *
- * Given the `head` of a singly linked list, return *`true` if it is a palindrome*.
+ * Given the `head` of a singly linked list, return *`true` if it is a
+ * palindrome*.
  *
  * @param {ListNode} head
  * @return {boolean}
