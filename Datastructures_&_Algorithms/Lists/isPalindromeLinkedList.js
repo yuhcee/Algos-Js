f0548d8c39a76300f1dbe7cd067c22e45ef3bccf
yuhcee <egbouchenna001@gmail.com>
2024-03-22 14:49:18 +0100

    :heavy_plus_sign:(isPalindromeLinkedList) add constraints

--- Datastructures_&_Algorithms/Lists/isPalindromeLinkedList.js
+++ Datastructures_&_Algorithms/Lists/isPalindromeLinkedList.js
@@ -11,6 +11,11 @@
  * Given the `head` of a singly linked list, return *`true` if it is a
  * palindrome*.
  *
+ * **Constraints:**
+ *
+ * - The number of nodes in the list is in the range `[1, 105]`.
+ * - `0 <= Node.val <= 9`
+ *
  * @param {ListNode} head
  * @return {boolean}
  */
