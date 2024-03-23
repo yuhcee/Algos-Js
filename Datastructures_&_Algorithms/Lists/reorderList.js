34f0645760b4bcf522a9b07179f888a449fc9619
yuhcee <egbouchenna001@gmail.com>
2024-03-23 09:48:20 +0100

    :books: (reorderList) add task description

---
+++ Datastructures_&_Algorithms/Lists/reorderList.js
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * **143. Reorder List**
+ *
+ * You are given the head of a singly linked-list. The list can be
+ * represented as:
+ *
+ * `L0 → L1 → … → Ln - 1 → Ln`
+ *
+ * *Reorder the list to be on the following form:*
+ *
+ * `L0 → Ln → L1 → Ln - 1 → L2 → Ln - 2 → …`
+ *
+ * You may not modify the values in the list's nodes. Only nodes themselves
+ * may be changed.
+ *
+ * @param {ListNode} head
+ * @return {void} Do not return anything, modify head in-place instead.
+ */
+const reorderList = function (head) {};
