f43affc77d9922499ab5721ae8c94942a1bac041
yuhcee <egbouchenna001@gmail.com>
2024-03-23 09:50:00 +0100

    :sparkles: (reorderList) add task solution

--- Datastructures_&_Algorithms/Lists/reorderList.js
+++ Datastructures_&_Algorithms/Lists/reorderList.js
@@ -28,4 +28,39 @@
  * @param {ListNode} head
  * @return {void} Do not return anything, modify head in-place instead.
  */
-const reorderList = function (head) {};
+const reorderList = function (head) {
+    if (!head || !head.next) {
+        return;
+    }
+
+    // Step 1: Find the middle of the linked list
+    let slow = head;
+    let fast = head;
+    while (fast.next && fast.next.next) {
+        slow = slow.next;
+        fast = fast.next.next;
+    }
+
+    // Step 2: Reverse the second half of the linked list
+    let prev = null;
+    let current = slow.next;
+    while (current) {
+        let nextTemp = current.next;
+        current.next = prev;
+        prev = current;
+        current = nextTemp;
+    }
+    slow.next = null; // Break the link between the first and second halves
+
+    // Step 3: Merge the first half with the reversed second half
+    let first = head;
+    let second = prev;
+    while (first && second) {
+        let firstNext = first.next;
+        let secondNext = second.next;
+        first.next = second;
+        second.next = firstNext;
+        first = firstNext;
+        second = secondNext;
+    }
+};
