96878440a8a59711d7cd510ff794d7416cbf1da6
yuhcee <egbouchenna001@gmail.com>
2024-03-25 06:14:55 +0100

    :heavy_plus_sign:(findDuplicates) add constraints

--- Datastructures_&_Algorithms/Arrays/findDuplicates.js
+++ Datastructures_&_Algorithms/Arrays/findDuplicates.js
@@ -1,13 +1,20 @@
 /**
  * **442. Find All Duplicates in an Array**
- * 
- * Given an integer array `nums` of length `n` where all the integers of `nums` 
- * are in the range `[1, n]` and each integer appears **once** or **twice**, 
+ *
+ * Given an integer array `nums` of length `n` where all the integers of `nums`
+ * are in the range `[1, n]` and each integer appears **once** or **twice**,
  * return *an array of all the integers that appears **twice***.
- * 
- * You must write an algorithm that runs in `O(n)` time and uses only constant 
+ *
+ * You must write an algorithm that runs in `O(n)` time and uses only constant
  * extra space.
- * 
+ *
+ * **Constraints:**
+ *
+ * - `n == nums.length`
+ * - `1 <= n <= 105`
+ * - `1 <= nums[i] <= n`
+ * - Each element in `nums` appears **once** or **twice**.
+ *
  * @param {number[]} nums
  * @return {number[]}
  */
