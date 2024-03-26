6d9038bf5464a1c26cd1acb7abcbb78e8d6595eb
yuhcee <egbouchenna001@gmail.com>
2024-03-26 21:54:50 +0100

    :sparkles: (firstMissingPositive) add task solution

--- Datastructures_&_Algorithms/Arrays/firstMissingPositive.js
+++ Datastructures_&_Algorithms/Arrays/firstMissingPositive.js
@@ -15,4 +15,23 @@
  * @param {number[]} nums
  * @return {number}
  */
-const firstMissingPositive = function (nums) {};
+const firstMissingPositive = function (nums) {
+    // Perform cyclic sort
+    let i = 0;
+    while (i < nums.length) {
+        if (nums[i] > 0 && nums[i] <= nums.length && nums[i] !== nums[nums[i] - 1]) {
+            [nums[nums[i] - 1], nums[i]] = [nums[i], nums[nums[i] - 1]];
+        } else {
+            i++;
+        }
+    }
+
+    // Find the first missing positive integer
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] !== i + 1) {
+            return i + 1;
+        }
+    }
+
+    return nums.length + 1;
+};
