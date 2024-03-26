d25fddc34037a6e4efc00de51317c29ee6dde1c0
yuhcee <egbouchenna001@gmail.com>
2024-03-26 21:53:38 +0100

    :books: (firstMissingPositive) add task description

---
+++ Datastructures_&_Algorithms/Arrays/firstMissingPositive.js
@@ -0,0 +1,13 @@
+/**
+ * **41. First Missing Positive**
+ * 
+ * Given an unsorted integer array `nums`. Return the smallest positive integer 
+ * that is not present in `nums`.
+ * 
+ * You must implement an algorithm that runs in `O(n)` time and uses `O(1)` 
+ * auxiliary space.
+ * 
+ * @param {number[]} nums
+ * @return {number}
+ */
+const firstMissingPositive = function (nums) {};
