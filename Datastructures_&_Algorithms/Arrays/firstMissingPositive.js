e5bf16b79014ba4b0f73c94952d1b45f077425e0
yuhcee <egbouchenna001@gmail.com>
2024-03-26 21:54:14 +0100

    :heavy_plus_sign:(firstMissingPositive) add constraints

--- Datastructures_&_Algorithms/Arrays/firstMissingPositive.js
+++ Datastructures_&_Algorithms/Arrays/firstMissingPositive.js
@@ -1,12 +1,17 @@
 /**
  * **41. First Missing Positive**
- * 
- * Given an unsorted integer array `nums`. Return the smallest positive integer 
+ *
+ * Given an unsorted integer array `nums`. Return the smallest positive integer
  * that is not present in `nums`.
- * 
- * You must implement an algorithm that runs in `O(n)` time and uses `O(1)` 
+ *
+ * You must implement an algorithm that runs in `O(n)` time and uses `O(1)`
  * auxiliary space.
- * 
+ *
+ * **Constraints:**
+ *
+ * - `1 <= nums.length <= 105`
+ * - `-231 <= nums[i] <= 231 - 1`
+ *
  * @param {number[]} nums
  * @return {number}
  */
