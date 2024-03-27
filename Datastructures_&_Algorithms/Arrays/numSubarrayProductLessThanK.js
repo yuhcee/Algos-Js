aba5f609bf914db47c89702f2359445ff2c2e6e3
yuhcee <egbouchenna001@gmail.com>
2024-03-27 23:55:14 +0100

    :books: (numSubarrayProductLessThanK) add task description

---
+++ Datastructures_&_Algorithms/Arrays/numSubarrayProductLessThanK.js
@@ -0,0 +1,12 @@
+/**
+ * **713. Subarray Product Less Than K**
+ * 
+ * Given an array of integers `nums` and an integer `k`, return the number of 
+ * contiguous subarrays where the product of all the elements in the subarray is 
+ * strictly less than k.
+ * 
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+const numSubarrayProductLessThanK = (nums, k) => {};
