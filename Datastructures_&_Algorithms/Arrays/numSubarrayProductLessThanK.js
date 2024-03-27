d8b19f0c87c2812cd1afd2df22ad224977d24610
yuhcee <egbouchenna001@gmail.com>
2024-03-27 23:56:42 +0100

    :white_check_mark: (numSubarrayProductLessThanK) add test case 1

--- Datastructures_&_Algorithms/Arrays/numSubarrayProductLessThanK.js
+++ Datastructures_&_Algorithms/Arrays/numSubarrayProductLessThanK.js
@@ -33,3 +33,11 @@ const numSubarrayProductLessThanK = (nums, k) => {
 
     return result;
 };
+
+const nums = [10, 5, 2, 6],
+    k = 100;
+// Output: 8
+/* Explanation: The 8 subarrays that have product less than 100 are:
+[10], [5], [2], [6], [10, 5], [5, 2], [2, 6], [5, 2, 6]
+Note that [10, 5, 2] is not included as the product of 100 is not strictly less than k. */
+console.log(numSubarrayProductLessThanK(nums, k));
