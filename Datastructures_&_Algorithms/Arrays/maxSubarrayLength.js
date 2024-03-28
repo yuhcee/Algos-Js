22c4c5893c1907bf1b7dc01e9b5d4cc0c97506d4
yuhcee <egbouchenna001@gmail.com>
2024-03-28 16:23:12 +0100

    :books: (maxSubarrayLength) add task description

---
+++ Datastructures_&_Algorithms/Arrays/maxSubarrayLength.js
@@ -0,0 +1,20 @@
+/**
+ * **2958. Length of Longest Subarray With at Most K Frequency**
+ *
+ * You are given an integer array `nums` and an integer `k`.
+ *
+ * The **frequency** of an element `x` is the number of times it occurs in an
+ * array.
+ *
+ * An array is called **good** if the frequency of each element in this array is
+ * **less than or equal** to `k`.
+ *
+ * Return *the length of the **longest good** subarray of `nums`.*
+ *
+ * A **subarray** is a contiguous non-empty sequence of elements within an array.
+ *
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+const maxSubarrayLength = (nums, k) => {};
