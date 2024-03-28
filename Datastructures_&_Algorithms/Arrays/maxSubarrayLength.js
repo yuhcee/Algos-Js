61728097b6c02978023777f57fa434d6591c7bb9
yuhcee <egbouchenna001@gmail.com>
2024-03-28 16:25:49 +0100

    :white_check_mark: (maxSubarrayLength) add test case 1

--- Datastructures_&_Algorithms/Arrays/maxSubarrayLength.js
+++ Datastructures_&_Algorithms/Arrays/maxSubarrayLength.js
@@ -41,3 +41,10 @@ const maxSubarrayLength = (nums, k) => {
     }
     return ans;
 };
+
+const nums = [1, 2, 3, 1, 2, 3, 1, 2],
+    k = 2;
+// Output: 6
+/* Explanation: The longest possible good subarray is [1,2,3,1,2,3] since the values 1, 2, and 3 occur at most twice in this subarray. Note that the subarrays [2,3,1,2,3,1] and [3,1,2,3,1,2] are also good.
+It can be shown that there are no good subarrays with length more than 6. */
+console.log(maxSubarrayLength(nums, k));
