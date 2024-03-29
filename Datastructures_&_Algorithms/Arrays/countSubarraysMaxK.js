2b48fc11e9a577d43009dd89b819199c34842940
yuhcee <egbouchenna001@gmail.com>
2024-03-29 16:46:48 +0100

    :books: (countSubarraysMaxK) add task description

---
+++ Datastructures_&_Algorithms/Arrays/countSubarraysMaxK.js
@@ -0,0 +1,15 @@
+/**
+ * **2962. Count Subarrays Where Max Element Appears at Least K Times**
+ *
+ * You are given an integer array `nums` and a **positive** integer `k`.
+ *
+ * Return the number of subarrays where the **maximum** element of nums appears
+ * **at least** `k` times in that subarray.
+ *
+ * A **subarray** is a contiguous sequence of elements within an array.
+ *
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+const countSubarraysMaxK = (nums, k) => {};
