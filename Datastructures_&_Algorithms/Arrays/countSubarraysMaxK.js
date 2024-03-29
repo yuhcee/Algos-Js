75def21279fe4751c3a04dc78333b40d42e6de08
yuhcee <egbouchenna001@gmail.com>
2024-03-29 16:49:03 +0100

    :white_check_mark: (countSubarraysMaxK) add test case 1

--- Datastructures_&_Algorithms/Arrays/countSubarraysMaxK.js
+++ Datastructures_&_Algorithms/Arrays/countSubarraysMaxK.js
@@ -40,3 +40,9 @@ const countSubarraysMaxK = (nums, k) => {
 
     return count;
 };
+
+const nums = [1, 3, 2, 3, 3],
+    k = 2;
+// Output: 6
+// Explanation: The subarrays that contain the element 3 at least 2 times are: [1,3,2,3], [1,3,2,3,3], [3,2,3], [3,2,3,3], [2,3,3] and [3,3].
+console.log(countSubarraysMaxK(nums, k));
