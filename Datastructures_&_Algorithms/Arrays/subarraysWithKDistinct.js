ededadcb301b33b096578c9035dda9bdd67ff513
yuhcee <egbouchenna001@gmail.com>
2024-03-30 17:22:46 +0100

    :books: (subarraysWithKDistinct) add task description

---
+++ Datastructures_&_Algorithms/Arrays/subarraysWithKDistinct.js
@@ -0,0 +1,18 @@
+/**
+ * **992. Subarrays with K Different Integers**
+ *
+ * Given an integer array nums and an integer k, return the number of **good
+ * subarrays** of nums.***
+ *
+ * A **good array** is an array where the number of different integers in that
+ * array is exactly `k`.
+ *
+ * - For example, `[1,2,3,1,2]` has `3` different integers: `1`, `2`, and `3`.
+ *
+ * A **subarray** is a **contiguous** part of an array.
+ *
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+const subarraysWithKDistinct = (nums, k) => {};
