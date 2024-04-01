ed5a7db907e817a828873976020d12f16db9f6ad
yuhcee <egbouchenna001@gmail.com>
2024-04-01 23:41:49 +0100

    :books: (lengthOfLastWord) add task description

---
+++ Datastructures_&_Algorithms/Strings/lengthOfLastWord.js
@@ -0,0 +1,12 @@
+/**
+ * **58. Length of Last Word**
+ *
+ * Given a string `s` consisting of words and spaces, return the length of the
+ * **last** word in the string.*
+ *
+ * A **word** is a maximal substring consisting of non-space characters only.
+ *
+ * @param {string} s
+ * @return {number}
+ */
+const lengthOfLastWord = function (s) {};
