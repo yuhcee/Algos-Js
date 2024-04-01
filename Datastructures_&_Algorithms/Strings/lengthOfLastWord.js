9b1845a38b6f084cbb6dbc59d9da9ce5dc0c4942
yuhcee <egbouchenna001@gmail.com>
2024-04-01 23:42:45 +0100

    :heavy_plus_sign:(lengthOfLastWord) add constraints

--- Datastructures_&_Algorithms/Strings/lengthOfLastWord.js
+++ Datastructures_&_Algorithms/Strings/lengthOfLastWord.js
@@ -6,6 +6,12 @@
  *
  * A **word** is a maximal substring consisting of non-space characters only.
  *
+ * **Constraints:**
+ *
+ * - `1 <= s.length <= 104`
+ * - `s` consists of only English letters and spaces `' '`.
+ * - There will be at least one word in `s`.
+ *
  * @param {string} s
  * @return {number}
  */
