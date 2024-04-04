b13c8c119182a7d071d3d1ff78582b6a3a5f59d9
yuhcee <egbouchenna001@gmail.com>
2024-04-04 23:23:17 +0100

    :sparkles: (maxDepth) add task solution

--- Datastructures_&_Algorithms/Strings/maxDepth.js
+++ Datastructures_&_Algorithms/Strings/maxDepth.js
@@ -34,4 +34,18 @@
  * @param {string} s
  * @return {number}
  */
-const maxDepth = (s) => {};
+const maxDepth = (s) => {
+    let maxDepth = 0;
+    let currentDepth = 0;
+
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] === '(') {
+            currentDepth++;
+            maxDepth = Math.max(maxDepth, currentDepth);
+        } else if (s[i] === ')') {
+            currentDepth--;
+        }
+    }
+
+    return maxDepth;
+};
