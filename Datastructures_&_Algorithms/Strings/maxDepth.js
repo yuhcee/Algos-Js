e57e47221e58d0e40ca1daabc74854d220802332
yuhcee <egbouchenna001@gmail.com>
2024-04-04 23:22:56 +0100

    :heavy_plus_sign:(maxDepth) add constraints

--- Datastructures_&_Algorithms/Strings/maxDepth.js
+++ Datastructures_&_Algorithms/Strings/maxDepth.js
@@ -24,6 +24,13 @@
  * Given a **VPS** represented as string `s`, return *the **nesting depth** of
  * `s`*.
  *
+ * **Constraints:**
+ *
+ * - `1 <= s.length <= 100`
+ * - `s` consists of digits `0-9` and characters `'+'`, `'-'`, `'*'`, `'/'`, `'
+ * ('`, and `')'`.
+ * - It is guaranteed that parentheses expression `s` is a **VPS**.
+ *
  * @param {string} s
  * @return {number}
  */
