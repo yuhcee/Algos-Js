5c5d3bbb915b47388d22a53f48e35e2d0419ca08
yuhcee <egbouchenna001@gmail.com>
2024-04-08 18:46:14 +0100

    :heavy_plus_sign:(countStudents) add constraints

--- Datastructures_&_Algorithms/Stacks/countStudents.js
+++ Datastructures_&_Algorithms/Stacks/countStudents.js
@@ -23,6 +23,13 @@
  *
  * Return *the number of students that are unable to eat.*
  *
+ * **Constraints:**
+ * 
+ * - 1 <= students.length, sandwiches.length <= 100`
+ * - `students.length == sandwiches.length`
+ * - `sandwiches[i]` is `0` or `1`.
+ * - `students[i]` is `0` or `1`.
+ * 
  * @param {number[]} students
  * @param {number[]} sandwiches
  * @return {number}
