430134b4c261fc25ebc97759a35425e8068d5bb4
yuhcee <egbouchenna001@gmail.com>
2024-04-10 20:56:54 +0100

    :white_check_mark: (deckRevealedIncreasing) add test case 1

--- Datastructures_&_Algorithms/Arrays/deckRevealedIncreasing.js
+++ Datastructures_&_Algorithms/Arrays/deckRevealedIncreasing.js
@@ -28,7 +28,7 @@ const deckRevealedIncreasing = function (deck) {
 
     const revealed = [];
     revealed.unshift(deck[n - 1]);
-    
+
     for (let i = n - 2; i >= 0; i--) {
         revealed.unshift(revealed.pop());
         revealed.unshift(deck[i]);
@@ -36,3 +36,18 @@ const deckRevealedIncreasing = function (deck) {
 
     return revealed;
 };
+
+const deck = [17, 13, 11, 2, 3, 5, 7];
+// Output: [2,13,3,11,5,17,7]
+/* Explanation: 
+We get the deck in the order [17,13,11,2,3,5,7] (this order does not matter), and reorder it.
+After reordering, the deck starts as [2,13,3,11,5,17,7], where 2 is the top of the deck.
+We reveal 2, and move 13 to the bottom.  The deck is now [3,11,5,17,7,13].
+We reveal 3, and move 11 to the bottom.  The deck is now [5,17,7,13,11].
+We reveal 5, and move 17 to the bottom.  The deck is now [7,13,11,17].
+We reveal 7, and move 13 to the bottom.  The deck is now [11,17,13].
+We reveal 11, and move 17 to the bottom.  The deck is now [13,17].
+We reveal 13, and move 17 to the bottom.  The deck is now [17].
+We reveal 17.
+Since all the cards revealed are in increasing order, the answer is correct. */
+console.log(deckRevealedIncreasing(deck));
