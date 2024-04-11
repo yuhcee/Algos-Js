cc0c618479333bd79c076958290a30b40b564950
yuhcee <egbouchenna001@gmail.com>
2024-04-11 19:42:34 +0100

    :white_check_mark: (removeKdigits) add test case 1

--- Datastructures_&_Algorithms/Stacks/removeKdigits.js
+++ Datastructures_&_Algorithms/Stacks/removeKdigits.js
@@ -40,3 +40,9 @@ const removeKdigits = function (num, k) {
     // If result is empty, return "0"
     return result || '0';
 };
+
+const num = '1432219',
+    k = 3;
+// Output: "1219"
+// Explanation: Remove the three digits 4, 3, and 2 to form the new number 1219 which is the smallest.
+console.log(removeKdigits(num, k));
