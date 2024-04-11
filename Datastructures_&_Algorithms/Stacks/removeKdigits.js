f608593aa8513ce5477f553ed27c41a02dbeec04
yuhcee <egbouchenna001@gmail.com>
2024-04-11 19:40:18 +0100

    :heavy_plus_sign:(removeKdigits) add constraints

--- Datastructures_&_Algorithms/Stacks/removeKdigits.js
+++ Datastructures_&_Algorithms/Stacks/removeKdigits.js
@@ -1,9 +1,15 @@
 /**
  * **402. Remove K Digits**
  *
- * Given string num representing a non-negative integer `num`, and an integer 
- * `k`, return *the smallest possible integer after removing `k` digits from 
+ * Given string num representing a non-negative integer `num`, and an integer
+ * `k`, return *the smallest possible integer after removing `k` digits from
  * `num`.*
+ *
+ * **Constraints:**
+ *
+ * - `1 <= k <= num.length <= 105`
+ * - `num` consists of only digits.
+ * - `num` does not have any leading zeros except for the zero itself.
  *
  * @param {string} num
  * @param {number} k
