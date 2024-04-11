7ae533ecc05a9ecc5af273c67c2abbae4fc8cb20
yuhcee <egbouchenna001@gmail.com>
2024-04-11 19:39:34 +0100

    :books: (removeKdigits) add task description

---
+++ Datastructures_&_Algorithms/Stacks/removeKdigits.js
@@ -0,0 +1,12 @@
+/**
+ * **402. Remove K Digits**
+ *
+ * Given string num representing a non-negative integer `num`, and an integer 
+ * `k`, return *the smallest possible integer after removing `k` digits from 
+ * `num`.*
+ *
+ * @param {string} num
+ * @param {number} k
+ * @return {string}
+ */
+const removeKdigits = function (num, k) {};
