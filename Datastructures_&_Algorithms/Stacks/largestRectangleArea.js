31f8101d4460c6a343a3353158c862c5f9fe60c8
yuhcee <egbouchenna001@gmail.com>
2024-04-13 23:41:35 +0100

    :sparkles: (maximalRectangle) add task solution

--- Datastructures_&_Algorithms/Stacks/largestRectangleArea.js
+++ Datastructures_&_Algorithms/Stacks/largestRectangleArea.js
@@ -15,4 +15,47 @@
  * @return {number}
  *
  */
-const maximalRectangle = function (matrix) {};
+const maximalRectangle = function (matrix) {
+    if (!matrix || matrix.length === 0 || matrix[0].length === 0) {
+        return 0;
+    }
+
+    const m = matrix.length;
+    const n = matrix[0].length;
+    const heights = new Array(n).fill(0);
+    let maxArea = 0;
+
+    for (let i = 0; i < m; i++) {
+        // Update the heights array to represent the histogram of heights for the current row
+        for (let j = 0; j < n; j++) {
+            if (matrix[i][j] === '1') {
+                heights[j]++;
+            } else {
+                heights[j] = 0;
+            }
+        }
+
+        // Calculate the maximum area rectangle for the current histogram
+        maxArea = Math.max(maxArea, largestRectangleArea(heights));
+    }
+
+    return maxArea;
+};
+
+const largestRectangleArea = function (heights) {
+    const stack = [];
+    let maxArea = 0;
+    const n = heights.length;
+
+    for (let i = 0; i <= n; i++) {
+        const h = i === n ? 0 : heights[i];
+        while (stack.length !== 0 && h < heights[stack[stack.length - 1]]) {
+            const height = heights[stack.pop()];
+            const width = stack.length === 0 ? i : i - stack[stack.length - 1] - 1;
+            maxArea = Math.max(maxArea, height * width);
+        }
+        stack.push(i);
+    }
+
+    return maxArea;
+};
