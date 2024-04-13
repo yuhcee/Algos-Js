f135e74ffe49713a706ca65de010e28386d60c31
yuhcee <egbouchenna001@gmail.com>
2024-04-13 23:40:49 +0100

    :heavy_plus_sign:(maximalRectangle) add constraints

--- Datastructures_&_Algorithms/Stacks/largestRectangleArea.js
+++ Datastructures_&_Algorithms/Stacks/largestRectangleArea.js
@@ -1,10 +1,18 @@
 /**
  * **85. Maximal Rectangle**
- * 
- * Given a `rows x cols` binary `matrix` filled with `0`'s and `1`'s, find the 
+ *
+ * Given a `rows x cols` binary `matrix` filled with `0`'s and `1`'s, find the
  * largest rectangle containing only `1`'s and return *its area.*
- * 
+ *
+ * **Constraints:**
+ *
+ * - `rows == matrix.length`
+ * - `cols == matrix[i].length`
+ * - `1 <= row, cols <= 200`
+ * - `matrix[i][j] is '0' or '1'.`
+ *
  * @param {character[][]} matrix
  * @return {number}
+ *
  */
 const maximalRectangle = function (matrix) {};
