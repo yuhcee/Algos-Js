a015ffbeeec3f431b6bc871be7b7427c7c1f66a3
yuhcee <egbouchenna001@gmail.com>
2024-04-14 17:44:09 +0100

    :white_check_mark: (sumNumbers) add test case 1

--- Datastructures_&_Algorithms/Trees/BinaryTree/sumNumbers.js
+++ Datastructures_&_Algorithms/Trees/BinaryTree/sumNumbers.js
@@ -47,3 +47,11 @@ const sumNumbers = function (root) {
 
     return result;
 };
+
+const root = [1, 2, 3];
+// Output: 25
+/* Explanation:
+The root-to-leaf path 1->2 represents the number 12.
+The root-to-leaf path 1->3 represents the number 13.
+Therefore, sum = 12 + 13 = 25. */
+console.log(sumNumbers(root));
