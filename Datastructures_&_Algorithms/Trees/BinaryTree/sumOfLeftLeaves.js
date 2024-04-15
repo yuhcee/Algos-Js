77409b69c991800a38f13e3124475d81df33281a
yuhcee <egbouchenna001@gmail.com>
2024-04-15 19:06:21 +0100

    :books: (sumOfLeftLeaves) add task description

---
+++ Datastructures_&_Algorithms/Trees/BinaryTree/sumOfLeftLeaves.js
@@ -0,0 +1,20 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * **404. Sum of Left Leaves**
+ *
+ * Given the `root` of a binary tree, return *the sum of all left leaves.*
+ *
+ * A **leaf** is a node with no children. A **left leaf** is a leaf that is the
+ * left child of another node.
+ *
+ * @param {TreeNode} root
+ * @return {number}
+ */
+const sumOfLeftLeaves = function (root) {};
