09d41bd43afd5a2987e986e9b353c31312f35bd1
yuhcee <egbouchenna001@gmail.com>
2024-04-15 19:25:33 +0100

    :white_check_mark: (sumOfLeftLeaves) add test case 1

--- Datastructures_&_Algorithms/Trees/BinaryTree/sumOfLeftLeaves.js
+++ Datastructures_&_Algorithms/Trees/BinaryTree/sumOfLeftLeaves.js
@@ -40,3 +40,8 @@ const sumOfLeftLeaves = function (root) {
 
     return sum;
 };
+
+const root = [3, 9, 20, null, null, 15, 7];
+// Output: 24
+// Explanation: There are two left leaves in the binary tree, with values 9 and 15 respectively.
+console.log(sumOfLeftLeaves(root));
