c462476d164aab673a6436ec3014d7ca24a8b1c4
yuhcee <egbouchenna001@gmail.com>
2024-04-26 13:27:05 +0100

    :heavy_plus_sign:(minFallingPathSum) add constraints

--- Datastructures_&_Algorithms/Dynamic Programming/minFallingPathSum2.js
+++ Datastructures_&_Algorithms/Dynamic Programming/minFallingPathSum2.js
@@ -8,6 +8,12 @@
  * from each row of `grid` such that no two elements chosen in adjacent rows are
  * in the same column.
  *
+ * **Constraints:**
+ *
+ * - `n == grid.length == grid[i].length`
+ * - `1 <= n <= 200`
+ * - `-99 <= grid[i][j] <= 99`
+ *
  * @param {number[][]} grid
  * @return {number}
  */
