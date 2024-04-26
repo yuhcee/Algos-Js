c2d99dae3044fc52b05e567597e3250cde59b6e4
yuhcee <egbouchenna001@gmail.com>
2024-04-26 13:24:59 +0100

    :books: (minFallingPathSum) add task description

---
+++ Datastructures_&_Algorithms/Dynamic Programming/minFallingPathSum2.js
@@ -0,0 +1,14 @@
+/**
+ * **1289. Minimum Falling Path Sum II**
+ *
+ * Given an `n x n` integer matrix `grid`, return *the minimum sum of a
+ * **falling path with non-zero shifts.***
+ *
+ * A **falling path with non-zero shifts** is a choice of exactly one element
+ * from each row of `grid` such that no two elements chosen in adjacent rows are
+ * in the same column.
+ *
+ * @param {number[][]} grid
+ * @return {number}
+ */
+const minFallingPathSum = function (grid) {};
