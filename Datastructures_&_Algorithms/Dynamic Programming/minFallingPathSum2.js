04fa82170830aaed56876e91b602c1dfd66ba146
yuhcee <egbouchenna001@gmail.com>
2024-04-26 14:16:11 +0100

    :white_check_mark: (minFallingPathSum) add test case 1

--- Datastructures_&_Algorithms/Dynamic Programming/minFallingPathSum2.js
+++ Datastructures_&_Algorithms/Dynamic Programming/minFallingPathSum2.js
@@ -29,3 +29,17 @@ const minFallingPathSum = function (grid) {
 
     return Math.min(...dp[n - 1]);
 };
+
+const grid = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+];
+// Output: 13
+/* Explanation: 
+The possible falling paths are:
+[1,5,9], [1,5,7], [1,6,7], [1,6,8],
+[2,4,8], [2,4,9], [2,6,7], [2,6,8],
+[3,4,8], [3,4,9], [3,5,7], [3,5,9]
+The falling path with the smallest sum is [1,5,7], so the answer is 13 */
+console.log(minFallingPathSum(grid));
