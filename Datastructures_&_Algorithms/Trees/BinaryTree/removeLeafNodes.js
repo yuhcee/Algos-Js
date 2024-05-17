c6e689bb5f5bafcdaecd9089ef6b75808cf5f7dd
yuhcee <egbouchenna001@gmail.com>
2024-05-17 22:56:45 +0100

    :white_check_mark: (removeLeafNodes) add test case 2

--- Datastructures_&_Algorithms/Trees/BinaryTree/removeLeafNodes.js
+++ Datastructures_&_Algorithms/Trees/BinaryTree/removeLeafNodes.js
@@ -55,3 +55,8 @@ const root = [1, 2, 3, 2, null, 2, 4],
 /* Explanation: Leaf nodes in green with value (target = 2) are removed (Picture in left). 
 After removing, new nodes become leaf nodes with value (target = 2) (Picture in center). */
 console.log(removeLeafNodes(root, target));
+
+const root1 = [1, 3, 3, 3, 2],
+    target1 = 3;
+// Output: [1,3,null,null,2]
+console.log(removeLeafNodes(root1, target1));
