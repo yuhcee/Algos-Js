3fdc0aec9cfe765f04d41249c8fc4977a9d4e806
yuhcee <egbouchenna001@gmail.com>
2024-05-17 22:56:06 +0100

    :white_check_mark: (removeLeafNodes) add test case 1

--- Datastructures_&_Algorithms/Trees/BinaryTree/removeLeafNodes.js
+++ Datastructures_&_Algorithms/Trees/BinaryTree/removeLeafNodes.js
@@ -48,3 +48,10 @@ const removeLeafNodes = function (root, target) {
     // Start the recursive removal process from the root
     return removeLeaves(root);
 };
+
+const root = [1, 2, 3, 2, null, 2, 4],
+    target = 2;
+// Output: [1,null,3,null,4]
+/* Explanation: Leaf nodes in green with value (target = 2) are removed (Picture in left). 
+After removing, new nodes become leaf nodes with value (target = 2) (Picture in center). */
+console.log(removeLeafNodes(root, target));
