60533a3fa883c95285a0a9a1b23ae49031cb024b
yuhcee <egbouchenna001@gmail.com>
2024-05-17 22:53:14 +0100

    :books: (removeLeafNodes) add task description

---
+++ Datastructures_&_Algorithms/Trees/BinaryTree/removeLeafNodes.js
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+
+/**
+ * **1325. Delete Leaves With a Given Value**
+ *
+ * Given a binary tree `root` and an integer `target`, delete all the **leaf
+ * nodes** with value `target`.
+ *
+ * Note that once you delete a leaf node with value `target`, if its parent node
+ * becomes a leaf node and has the value `target`, it should also be deleted
+ * (you need to continue doing that until you cannot).
+ *
+ * @param {TreeNode} root
+ * @param {number} target
+ * @return {TreeNode}
+ */
+const removeLeafNodes = function (root, target) {};
