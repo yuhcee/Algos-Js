0b1ee873f156aaa6968771ab4a3abe2d716af037
yuhcee <egbouchenna001@gmail.com>
2024-05-18 23:51:25 +0100

    :books: (distributeCoins) add task description

---
+++ Datastructures_&_Algorithms/Trees/BinaryTree/distributeCoins.js
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+
+/**
+ * **979. Distribute Coins in Binary Tree**
+ *
+ * You are given the `root` of a binary tree with `n` nodes where each node in
+ * the tree has `node.val` coins. There are `n` coins in total throughout the
+ * whole tree.
+ *
+ * In one move, we may choose two adjacent nodes and move one coin from one node
+ * to another. A move may be from parent to child, or from child to parent.
+ *
+ * Return *the **minimum** number of moves required to make every node have
+ * **exactly** one coin*.
+ *
+ * @param {TreeNode} root
+ * @return {number}
+ */
+const distributeCoins = function (root) {};
