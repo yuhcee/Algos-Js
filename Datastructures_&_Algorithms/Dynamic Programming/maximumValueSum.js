2554549871821b59bec93c1b82496b9c5800268f
yuhcee <egbouchenna001@gmail.com>
2024-05-19 12:42:26 +0100

    :white_check_mark: (maximumValueSum) add test case 1

--- Datastructures_&_Algorithms/Dynamic Programming/maximumValueSum.js
+++ Datastructures_&_Algorithms/Dynamic Programming/maximumValueSum.js
@@ -56,3 +56,16 @@ const maximumValueSum = function (nums, k) {
     // Step 5: Return the final maximum sum
     return initialSum + additionalSum;
 };
+
+const nums = [1, 2, 1],
+    k = 3,
+    edges = [
+        [0, 1],
+        [0, 2],
+    ];
+// Output: 6
+/* Explanation: Alice can achieve the maximum sum of 6 using a single operation:
+- Choose the edge [0,2]. nums[0] and nums[2] become: 1 XOR 3 = 2, and the array nums becomes: [1,2,1] -> [2,2,2].
+The total sum of values is 2 + 2 + 2 = 6.
+It can be shown that 6 is the maximum achievable sum of values. */
+console.log(maximumValueSum(nums, k, edges));
