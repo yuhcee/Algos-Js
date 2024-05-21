7b3b56727e19e33d586878a5130ff8f927226c7b
yuhcee <egbouchenna001@gmail.com>
2024-05-21 19:26:46 +0100

    :books: (subsets) add task description

---
+++ Datastructures_&_Algorithms/Arrays/subsets.js
@@ -0,0 +1,13 @@
+/**
+ * **78. Subsets**
+ *
+ * Given an integer array `nums` of **unique** elements, return *all possible
+ * subsets (the power set)*.
+ *
+ * The solution set **must not** contain duplicate subsets. Return the solution
+ * in **any order**.
+ *
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+const subsets = function (nums) {};
