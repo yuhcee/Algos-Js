1ac4c13541ee0e5f4277cf51af402e0bfa538153
yuhcee <egbouchenna001@gmail.com>
2024-05-23 16:56:27 +0100

    :books: (beautifulSubsets) add task description

---
+++ Datastructures_&_Algorithms/Dynamic Programming/beautifulSubsets.js
@@ -0,0 +1,20 @@
+/**
+ * **2597. The Number of Beautiful Subsets**
+ *
+ * You are given an array `nums` of positive integers and a **positive** integer
+ * `k`.
+ *
+ * A subset of `nums` is **beautiful** if it does not contain two integers with
+ * an absolute difference equal to `k`.
+ *
+ * Return *the number of **non-empty beautiful** subsets of the array `nums`*.
+ *
+ * A **subset** of `nums` is an array that can be obtained by deleting some
+ * (possibly none) elements from `nums`. Two subsets are different if and only
+ * if the chosen indices to delete are different.
+ *
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+const beautifulSubsets = function (nums, k) {};
