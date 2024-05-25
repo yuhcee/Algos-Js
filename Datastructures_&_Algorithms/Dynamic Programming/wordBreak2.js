a68dc160aaf037fc019fa5541940f909f11de074
yuhcee <egbouchenna001@gmail.com>
2024-05-25 13:24:03 +0100

    :books: (wordBreak) add task description

---
+++ Datastructures_&_Algorithms/Dynamic Programming/wordBreak2.js
@@ -0,0 +1,15 @@
+/**
+ * **140. Word Break II**
+ *
+ * Given a string `s` and a dictionary of strings `wordDict`, add spaces in `s`
+ * to construct a sentence where each word is a valid dictionary word. Return
+ * all such possible sentences in **any order**.
+ *
+ * **Note** that the same word in the dictionary may be reused multiple times in
+ * the segmentation.
+ *
+ * @param {string} s
+ * @param {string[]} wordDict
+ * @return {string[]}
+ */
+const wordBreak = function (s, wordDict) {};
