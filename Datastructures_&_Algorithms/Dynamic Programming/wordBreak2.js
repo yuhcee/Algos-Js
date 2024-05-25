1060aa2f8532df08c480b15cf8c027ee2447b15c
yuhcee <egbouchenna001@gmail.com>
2024-05-25 13:34:15 +0100

    :sparkles: (wordBreak) add task solution

--- Datastructures_&_Algorithms/Dynamic Programming/wordBreak2.js
+++ Datastructures_&_Algorithms/Dynamic Programming/wordBreak2.js
@@ -9,16 +9,46 @@
  * the segmentation.
  *
  * **Constraints:**
- * 
+ *
  * - `1 <= s.length <= 20`
  * - `1 <= wordDict.length <= 1000`
  * - `1 <= wordDict[i].length <= 10`
  * - `s` and `wordDict[i]` consist of only lowercase English letters.
  * - All the strings of `wordDict` are **unique**.
  * - Input is generated in a way that the length of the answer doesn't exceed 105
- * 
+ *
  * @param {string} s
  * @param {string[]} wordDict
  * @return {string[]}
  */
-const wordBreak = function (s, wordDict) {};
+const wordBreak = function (s, wordDict) {
+    const wordSet = new Set(wordDict);
+    const memo = new Map();
+
+    const backtrack = (start) => {
+        if (memo.has(start)) {
+            return memo.get(start);
+        }
+
+        const result = [];
+
+        if (start === s.length) {
+            result.push('');
+        }
+
+        for (let end = start + 1; end <= s.length; end++) {
+            const word = s.slice(start, end);
+            if (wordSet.has(word)) {
+                const sublist = backtrack(end);
+                for (const sub of sublist) {
+                    result.push(word + (sub === '' ? '' : ' ') + sub);
+                }
+            }
+        }
+
+        memo.set(start, result);
+        return result;
+    };
+
+    return backtrack(0);
+};
