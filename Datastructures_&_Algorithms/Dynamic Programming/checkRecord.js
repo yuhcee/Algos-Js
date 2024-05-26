625934752ff29777ada75872c01b937f8f97c99b
yuhcee <egbouchenna001@gmail.com>
2024-05-26 23:52:17 +0100

    :sparkles: (checkRecord) add task solution

--- Datastructures_&_Algorithms/Dynamic Programming/checkRecord.js
+++ Datastructures_&_Algorithms/Dynamic Programming/checkRecord.js
@@ -22,4 +22,27 @@
  * @param {number} n
  * @return {number}
  */
-const checkRecord = function (n) {};
+const checkRecord = function (n) {
+    const MOD = 1000000007;
+
+    // Initialize the memoization array
+    let memo = new Array(n).fill(0).map(() => new Array(2).fill(0).map(() => new Array(3).fill(-1)));
+
+    const checkAllRecords = (curInd, countA, countL) => {
+        if (curInd === n) {
+            return 1;
+        }
+        if (memo[curInd][countA][countL] !== -1) {
+            return memo[curInd][countA][countL];
+        }
+        let withANext = countA === 0 ? checkAllRecords(curInd + 1, countA + 1, 0) : 0;
+        let withLNext = countL === 2 ? 0 : checkAllRecords(curInd + 1, countA, countL + 1);
+        let withPNext = checkAllRecords(curInd + 1, countA, 0);
+        let total = (((withANext + withLNext) % MOD) + withPNext) % MOD;
+
+        memo[curInd][countA][countL] = total;
+        return total;
+    };
+
+    return checkAllRecords(0, 0, 0);
+};
