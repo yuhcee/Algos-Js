8f89814275dc471288fed4a203d04688e930992c
yuhcee <egbouchenna001@gmail.com>
2024-05-26 23:51:01 +0100

    :books: (checkRecord) add task description

---
+++ Datastructures_&_Algorithms/Dynamic Programming/checkRecord.js
@@ -0,0 +1,21 @@
+/**
+ * **552. Student Attendance Record II**
+ *
+ * - `'A'`: Absent.
+ * - `'L'`: Late.
+ * - `'P'`: Present.
+ *
+ * Any student is eligible for an attendance award if they meet both of the
+ * following criteria:
+ *
+ * - The student was absent (`'A'`) for **strictly** fewer than 2 days **total**.
+ * - The student was **never** late (`'L'`) for 3 or more **consecutive** days.
+ *
+ * Given an integer `n`, return the **number** of possible attendance records of
+ * length `n` that make a student eligible for an attendance award. The answer
+ * may be very large, so return it **modulo** `109 + 7`.
+ *
+ * @param {number} n
+ * @return {number}
+ */
+const checkRecord = function (n) {};
