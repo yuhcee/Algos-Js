0bb4cdc786a8076cd7419abdf6e774496ffe8467
yuhcee <egbouchenna001@gmail.com>
2024-05-27 22:36:11 +0100

    :white_check_mark: (specialArray) add test case 2

--- Datastructures_&_Algorithms/Arrays/specialArray.js
+++ Datastructures_&_Algorithms/Arrays/specialArray.js
@@ -42,3 +42,12 @@ const nums = [3, 5];
 // Output: 2
 // Explanation: There are 2 values (3 and 5) that are greater than or equal to 2.
 console.log(specialArray(nums));
+
+const nums1 = [0, 0];
+// Output: -1
+/* Explanation: No numbers fit the criteria for x.
+If x = 0, there should be 0 numbers >= x, but there are 2.
+If x = 1, there should be 1 number >= x, but there are 0.
+If x = 2, there should be 2 numbers >= x, but there are 0.
+x cannot be greater since there are only 2 numbers in nums. */
+console.log(specialArray(nums1));
