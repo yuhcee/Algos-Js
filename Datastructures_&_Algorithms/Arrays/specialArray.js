6111ebbb877a4653aada3d680ab2721f41b7355b
yuhcee <egbouchenna001@gmail.com>
2024-05-27 22:32:30 +0100

    :books: (specialArray) add task description

---
+++ Datastructures_&_Algorithms/Arrays/specialArray.js
@@ -0,0 +1,16 @@
+/**
+ * **1608. Special Array With X Elements Greater Than or Equal X**
+ *
+ * You are given an array `nums` of non-negative integers. `nums` is considered
+ * **special** if there exists a number `x` such that there are **exactly** `x`
+ * numbers in `nums` that are **greater than or equal** to `x`.
+ *
+ * Notice that `x` **does** not have to be an element in `nums`.
+ *
+ * Return *`x` if the array is **special**, otherwise, return `-1`*. It can be
+ * proven that if `nums` is special, the value for `x` is **unique**.
+ *
+ * @param {number[]} nums
+ * @return {number}
+ */
+const specialArray = function (nums) {};
