c44384e3d722f561f8722ceefa346d14fb36a983
yuhcee <egbouchenna001@gmail.com>
2024-05-28 10:27:22 +0100

    :books: (equalSubstring) add task description

---
+++ Datastructures_&_Algorithms/Arrays/equalSubstring.js
@@ -0,0 +1,21 @@
+/**
+ * **1208. Get Equal Substrings Within Budget**
+ *
+ * You are given two strings `s` and `t` of the same length and an integer
+ * `maxCost`.
+ *
+ * You want to change `s` to `t`. Changing the `ith` character of `s` to `ith`
+ * character of t costs `|s[i] - t[i]|` (i.e., the absolute difference between
+ * the ASCII values of the characters).
+ *
+ * Return *the maximum length of a substring of `s` that can be changed to be
+ * the same as the corresponding substring of `t` with a cost less than or equal
+ * to `maxCost`. If there is no substring from `s` that can be changed to its
+ * corresponding substring from `t`, return `0`.
+ *
+ * @param {string} s
+ * @param {string} t
+ * @param {number} maxCost
+ * @return {number}
+ */
+const equalSubstring = function (s, t, maxCost) {};
