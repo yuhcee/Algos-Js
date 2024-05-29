f122d26362014862ebf9ad26995f1338a44bcdcc
yuhcee <egbouchenna001@gmail.com>
2024-05-29 21:06:13 +0100

    :sparkles: (numSteps) add task solution

--- Datastructures_&_Algorithms/Strings/numSteps.js
+++ Datastructures_&_Algorithms/Strings/numSteps.js
@@ -19,4 +19,27 @@
  * @param {string} s
  * @return {number}
  */
-const numSteps = function (s) {};
+const numSteps = function (s) {
+    let steps = 0;
+    let carry = 0;
+
+    // Start from the least significant bit (rightmost bit)
+    for (let i = s.length - 1; i > 0; i--) {
+        if (s[i] === '0') {
+            // If the bit is '0' and there is no carry, it's even, just divide by 2 (shift right)
+            // If there's a carry, it effectively means it's '1' after the carry addition, then we need to add 1 again
+            steps += 1 + carry;
+        } else {
+            // If the bit is '1', it's odd, we need to add 1 to make it even
+            // And there will be a carry to the next significant bit
+            steps += 2 - carry;
+            carry = 1; // Set carry since adding 1 to '1' results in '10' (carry over)
+        }
+    }
+
+    // If there's a carry left after processing all bits, it means we need one more step
+    // This handles the case where the most significant bit changes due to carry
+    steps += carry;
+
+    return steps;
+};
