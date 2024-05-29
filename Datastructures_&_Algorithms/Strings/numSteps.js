fd0998fae7b834612c34f11193b329bc1a587c3c
yuhcee <egbouchenna001@gmail.com>
2024-05-29 21:03:45 +0100

    :books: (numSteps) add task description

---
+++ Datastructures_&_Algorithms/Strings/numSteps.js
@@ -0,0 +1,16 @@
+/**
+ * **1404. Number of Steps to Reduce a Number in Binary Representation to One**
+ *
+ * Given the binary representation of an integer as a string `s`, return *the
+ * number of steps to reduce it to 1 under the following rules:*
+ *
+ * - If the current number is even, you have to divide it by `2`.
+ *
+ * - If the current number is odd, you have to add `1` to it.
+ *
+ * - It is guaranteed that you can always reach one for all test cases.
+ *
+ * @param {string} s
+ * @return {number}
+ */
+const numSteps = function (s) {};
