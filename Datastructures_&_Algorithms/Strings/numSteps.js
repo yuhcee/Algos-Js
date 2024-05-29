b5b73fc0396b2bf9ffcea346c85eb0c48c486da9
yuhcee <egbouchenna001@gmail.com>
2024-05-29 21:11:06 +0100

    :white_check_mark: (numSteps) add test case 1

--- Datastructures_&_Algorithms/Strings/numSteps.js
+++ Datastructures_&_Algorithms/Strings/numSteps.js
@@ -43,3 +43,15 @@ const numSteps = function (s) {
 
     return steps;
 };
+
+const s = '1101';
+// Output: 6
+/* Explanation: "1101" corressponds to number 13 in their decimal representation.
+Step 1) 13 is odd, add 1 and obtain 14. 
+Step 2) 14 is even, divide by 2 and obtain 7.
+Step 3) 7 is odd, add 1 and obtain 8.
+Step 4) 8 is even, divide by 2 and obtain 4.  
+Step 5) 4 is even, divide by 2 and obtain 2. 
+Step 6) 2 is even, divide by 2 and obtain 1. */
+console.log(numSteps(s));
+
