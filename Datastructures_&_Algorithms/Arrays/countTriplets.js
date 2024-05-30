7b26adf55db3c957e16912d45337313ba4eabf2b
yuhcee <egbouchenna001@gmail.com>
2024-05-30 08:57:50 +0100

    :sparkles: (countTriplets) add task solution

--- Datastructures_&_Algorithms/Arrays/countTriplets.js
+++ Datastructures_&_Algorithms/Arrays/countTriplets.js
@@ -23,4 +23,26 @@
  * @param {number[]} arr
  * @return {number}
  */
-const countTriplets = function (arr) {};
+const countTriplets = function (arr) {
+    const n = arr.length;
+    const prefix = new Array(n + 1).fill(0);
+
+    // Calculate the prefix XOR
+    for (let i = 0; i < n; i++) {
+        prefix[i + 1] = prefix[i] ^ arr[i];
+    }
+
+    let count = 0;
+
+    // Iterate over all pairs (i, k) and check the condition
+    for (let i = 0; i < n; i++) {
+        for (let k = i + 1; k < n; k++) {
+            if (prefix[i] == prefix[k + 1]) {
+                // If prefix[i] == prefix[k+1], all j between i and k are valid
+                count += k - i;
+            }
+        }
+    }
+
+    return count;
+};
