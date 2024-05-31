fbb4162741f9163c45963b8363077d593fbfd606
yuhcee <egbouchenna001@gmail.com>
2024-05-31 23:16:51 +0100

    :white_check_mark: (singleNumber) add test case 1

--- Datastructures_&_Algorithms/Arrays/singleNumber3.js
+++ Datastructures_&_Algorithms/Arrays/singleNumber3.js
@@ -43,3 +43,8 @@ const singleNumber = function (nums) {
     // num1 and num2 are the two unique numbers
     return [num1, num2];
 };
+
+const nums = [1, 2, 1, 3, 2, 5];
+// Output: [3,5]
+// Explanation:  [5, 3] is also a valid answer.
+console.log(singleNumber(nums));
