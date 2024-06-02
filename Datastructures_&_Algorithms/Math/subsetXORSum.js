6985192cd8e86f0b97e329c5e97fd031f98d31b4
yuhcee <egbouchenna001@gmail.com>
2024-06-02 21:37:12 +0100

    :white_check_mark: (subsetXORSum) add test case 2

--- Datastructures_&_Algorithms/Math/subsetXORSum.js
+++ Datastructures_&_Algorithms/Math/subsetXORSum.js
@@ -53,3 +53,19 @@ const nums = [1, 3];
 - [1,3] has an XOR total of 1 XOR 3 = 2.
 0 + 1 + 3 + 2 = 6 */
 console.log(subsetXORSum(nums));
+
+const nums1 = [5, 1, 6];
+// Output: 28
+/* Explanation: The 8 subsets of [5,1,6] are:
+- The empty subset has an XOR total of 0.
+- [5] has an XOR total of 5.
+- [1] has an XOR total of 1.
+- [6] has an XOR total of 6.
+- [5,1] has an XOR total of 5 XOR 1 = 4.
+- [5,6] has an XOR total of 5 XOR 6 = 3.
+- [1,6] has an XOR total of 1 XOR 6 = 7.
+- [5,1,6] has an XOR total of 5 XOR 1 XOR 6 = 2.
+0 + 5 + 1 + 6 + 4 + 3 + 7 + 2 = 28 */
+console.log(subsetXORSum(nums1));
+
+
