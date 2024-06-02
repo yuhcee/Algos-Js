15e7f8f8d1bd3afb19bd7d8a9b2ce6ae9dd24c1b
yuhcee <egbouchenna001@gmail.com>
2024-06-02 21:11:16 +0100

    :books: (scoreOfString) add task description

---
+++ Datastructures_&_Algorithms/Strings/scoreOfString.js
@@ -0,0 +1,12 @@
+/**
+ * **3110. Score of a String**
+ *
+ * You are given a string `s`. The score of a string is defined as the sum of
+ * the absolute difference between the **ASCII** values of adjacent characters
+ *
+ * Return *the **score** of `s`*.
+ *
+ * @param {string} s
+ * @return {number}
+ */
+const scoreOfString = function (s) {};
