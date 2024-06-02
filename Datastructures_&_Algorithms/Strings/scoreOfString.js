08a09cb5b8ab9788c8b2c921c3fa3756827028f1
yuhcee <egbouchenna001@gmail.com>
2024-06-02 21:17:11 +0100

    :sparkles: (scoreOfString) add task solution

--- Datastructures_&_Algorithms/Strings/scoreOfString.js
+++ Datastructures_&_Algorithms/Strings/scoreOfString.js
@@ -7,11 +7,17 @@
  * Return *the **score** of `s`*.
  *
  * **Constraints:**
- * 
+ *
  * - `2 <= s.length <= 100`
  * - `s` consists only of lowercase English letters.
- * 
+ *
  * @param {string} s
  * @return {number}
  */
-const scoreOfString = function (s) {};
+const scoreOfString = function (s) {
+    let score = 0;
+    for (let i = 0; i < s.length - 1; i++) {
+        score += Math.abs(s.charCodeAt(i) - s.charCodeAt(i + 1));
+    }
+    return score;
+};
