78e667783fb0539881fd538a115b9e3c2896ead1
yuhcee <egbouchenna001@gmail.com>
2024-06-03 07:36:44 +0100

    :books: (appendCharacters) add task description

---
+++ Datastructures_&_Algorithms/Arrays/appendCharacters.js
@@ -0,0 +1,18 @@
+/**
+ * **2486. Append Characters to String to Make Subsequence**
+ *
+ * You are given two strings `s` and `t` consisting of only lowercase English
+ * letters.
+ *
+ * Return *the minimum number of characters that need to be appended to the end
+ * of `s` so that `t` becomes a **subsequence** of `s`*.
+ *
+ * A **subsequence** is a string that can be derived from another string by
+ * deleting some or no characters without changing the order of the remaining
+ * characters.
+ *
+ * @param {string} s
+ * @param {string} t
+ * @return {number}
+ */
+const appendCharacters = function (s, t) {};
