396e1039e727f58505b7ca6113ba488e75025644
yuhcee <egbouchenna001@gmail.com>
2024-06-04 02:09:59 +0100

    :heavy_plus_sign:(longestPalindrome) add constraints

--- Datastructures_&_Algorithms/HashMaps/longestPalindrome1.js
+++ Datastructures_&_Algorithms/HashMaps/longestPalindrome1.js
@@ -7,6 +7,11 @@
  * Letters are **case sensitive**, for example, `"Aa"` is not considered a
  * palindrome.
  *
+ * **Constraints:**
+ *
+ * - `1 <= s.length <= 2000`
+ * - `s` consists of lowercase **and/or** uppercase English letters only.
+ *
  * @param {string} s
  * @return {number}
  */
