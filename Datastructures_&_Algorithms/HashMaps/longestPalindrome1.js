0150a73923f69dd4d2ac56aa41e7f90ee07c3b41
yuhcee <egbouchenna001@gmail.com>
2024-06-04 02:10:18 +0100

    :sparkles: (longestPalindrome) add task solution

--- Datastructures_&_Algorithms/HashMaps/longestPalindrome1.js
+++ Datastructures_&_Algorithms/HashMaps/longestPalindrome1.js
@@ -15,4 +15,31 @@
  * @param {string} s
  * @return {number}
  */
-const longestPalindrome = function (s) {};
+const longestPalindrome = function (s) {
+    const charCount = new Map();
+
+    // Count the frequency of each character
+    for (let char of s) {
+        charCount.set(char, (charCount.get(char) || 0) + 1);
+    }
+
+    let length = 0;
+    let hasOdd = false;
+
+    // Calculate the length of the longest palindrome
+    for (let count of charCount.values()) {
+        if (count % 2 === 0) {
+            length += count;
+        } else {
+            length += count - 1;
+            hasOdd = true;
+        }
+    }
+
+    // If there is any character with an odd count, add one to the length
+    if (hasOdd) {
+        length += 1;
+    }
+
+    return length;
+};
