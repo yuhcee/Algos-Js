a43e72c2e16ce30948f311faabe57a1969ded13e
yuhcee <egbouchenna001@gmail.com>
2024-06-04 02:11:20 +0100

    :white_check_mark: (longestPalindrome) add test case 2

--- Datastructures_&_Algorithms/HashMaps/longestPalindrome1.js
+++ Datastructures_&_Algorithms/HashMaps/longestPalindrome1.js
@@ -48,3 +48,8 @@ const s = 'abccccdd';
 // Output: 7
 // Explanation: One longest palindrome that can be built is "dccaccd", whose length is 7.
 console.log(longestPalindrome(s));
+
+const s1 = 'a';
+// Output: 1
+// Explanation: The longest palindrome that can be built is "a", whose length is 1.
+console.log(longestPalindrome(s1));
