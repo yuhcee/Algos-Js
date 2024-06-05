7ee6efb145e1c698d337bf4471e203c40110188c
yuhcee <egbouchenna001@gmail.com>
2024-06-05 20:54:19 +0100

    :books: (commonChars) add task description

---
+++ Datastructures_&_Algorithms/Arrays/commonChars.js
@@ -0,0 +1,11 @@
+/**
+ * **1002. Find Common Characters**
+ *
+ * Given a string array `words`, return *an array of all characters that show up
+ * in all strings within the `words` (including duplicates)*. You may return the
+ * answer in **any order**.
+ *
+ * @param {string[]} words
+ * @return {string[]}
+ */
+const commonChars = function (words) {};
