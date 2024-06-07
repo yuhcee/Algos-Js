3645d48cd392c17022924f80f0940543148058e6
yuhcee <egbouchenna001@gmail.com>
2024-06-07 22:29:09 +0100

    :heavy_plus_sign:(replaceWords) add constraints

--- Datastructures_&_Algorithms/Trees/Trie/replaceWords.js
+++ Datastructures_&_Algorithms/Trees/Trie/replaceWords.js
@@ -13,6 +13,19 @@
  *
  * Return *the `sentence` after the replacement*.
  *
+ * **Constraints:**
+ *
+ * - `1 <= dictionary.length <= 1000`
+ * - `1 <= dictionary[i].length <= 100`
+ * - `dictionary[i]` consists of only lower-case letters.
+ * - `1 <= sentence.length <= 106`
+ * - `sentence` consists of only lower-case letters and spaces.
+ * - The number of words in `sentence` is in the range `[1, 1000]`
+ * - The length of each word in `sentence` is in the range `[1, 1000]`
+ * - Every two consecutive words in `sentence` will be separated by exactly one
+ * space.
+ * - `sentence` does not have leading or trailing spaces.
+ *
  * @param {string[]} dictionary
  * @param {string} sentence
  * @return {string}
