17986d6d3d71f7a73185595a6b87221fd55c7c47
yuhcee <egbouchenna001@gmail.com>
2024-06-09 11:02:49 +0100

    :white_check_mark: (matrixScore) add test case 1

--- Datastructures_&_Algorithms/Greedy Algorithms/matrixScore.js
+++ Datastructures_&_Algorithms/Greedy Algorithms/matrixScore.js
@@ -69,3 +69,12 @@ function toggleColumn(grid, colIndex) {
         grid[i][colIndex] ^= 1; // Toggle 0 to 1 and 1 to 0
     }
 }
+
+const grid = [
+    [0, 0, 1, 1],
+    [1, 0, 1, 0],
+    [1, 1, 0, 0],
+];
+// Output: 39
+// Explanation: 0b1111 + 0b1001 + 0b1111 = 15 + 9 + 15 = 39
+console.log(matrixScore(grid));
