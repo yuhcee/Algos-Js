4346a2f89598c64552297b19d322e84faf0aa356
yuhcee <egbouchenna001@gmail.com>
2024-06-11 23:56:15 +0100

    :white_check_mark: (relativeSortArray) add test case 1

--- Datastructures_&_Algorithms/Arrays/relativeSortArray.js
+++ Datastructures_&_Algorithms/Arrays/relativeSortArray.js
@@ -45,3 +45,10 @@ const relativeSortArray = function (arr1, arr2) {
 
     return arr1;
 };
+
+const arr1 = [2, 3, 1, 3, 2, 4, 6, 7, 9, 2, 19],
+    arr2 = [2, 1, 4, 3, 9, 6];
+// Output: [2,2,2,1,4,3,3,9,6,7,19]
+console.log(relativeSortArray(arr1, arr2));
+
+
