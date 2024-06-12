e9e3bae50d4800143050df438a47c3d27c6475fd
yuhcee <egbouchenna001@gmail.com>
2024-06-12 20:14:28 +0100

    :white_check_mark: (sortColors) add test case 2

--- Datastructures_&_Algorithms/Arrays/sortColors.js
+++ Datastructures_&_Algorithms/Arrays/sortColors.js
@@ -44,3 +44,7 @@ const sortColors = function (nums) {
 const nums = [2, 0, 2, 1, 1, 0];
 // Output: [0,0,1,1,2,2]
 console.log(sortColors(nums));
+
+const nums1 = [2, 0, 1];
+// Output: [0,1,2]
+console.log(sortColors(nums1));
