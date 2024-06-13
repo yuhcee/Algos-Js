e7510853115282dc3492e0c7eac1625c5ad6de30
yuhcee <egbouchenna001@gmail.com>
2024-06-13 14:03:27 +0100

    :white_check_mark: (minMovesToSeat) add test case 1

--- Datastructures_&_Algorithms/Greedy Algorithms/minMovesToSeat.js
+++ Datastructures_&_Algorithms/Greedy Algorithms/minMovesToSeat.js
@@ -37,3 +37,13 @@ const minMovesToSeat = function (seats, students) {
 
     return totalMoves;
 };
+
+const seats = [3, 1, 5],
+    students = [2, 7, 4];
+// Output: 4
+/* Explanation: The students are moved as follows:
+- The first student is moved from from position 2 to position 1 using 1 move.
+- The second student is moved from from position 7 to position 5 using 2 moves.
+- The third student is moved from from position 4 to position 3 using 1 move.
+In total, 1 + 2 + 1 = 4 moves were used. */
+console.log(minMovesToSeat(seats, students));
