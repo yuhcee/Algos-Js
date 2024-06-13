e953dbce7adb33d7bf044f082841a31ba3075982
yuhcee <egbouchenna001@gmail.com>
2024-06-13 14:02:40 +0100

    :sparkles: (minMovesToSeat) add task solution

--- Datastructures_&_Algorithms/Greedy Algorithms/minMovesToSeat.js
+++ Datastructures_&_Algorithms/Greedy Algorithms/minMovesToSeat.js
@@ -25,4 +25,15 @@
  * @param {number[]} students
  * @return {number}
  */
-const minMovesToSeat = function (seats, students) {};
+const minMovesToSeat = function (seats, students) {
+    seats.sort((a, b) => a - b);
+    students.sort((a, b) => a - b);
+
+    let totalMoves = 0;
+
+    for (let i = 0; i < seats.length; i++) {
+        totalMoves = Math.abs(seats[i] - students[i]);
+    }
+
+    return totalMoves;
+};
