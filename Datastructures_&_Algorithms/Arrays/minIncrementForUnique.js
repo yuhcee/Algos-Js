2f76bb95a347cdb7e4682bae23a5e5839b42f149
yuhcee <egbouchenna001@gmail.com>
2024-06-14 23:45:12 +0100

    :books: (minIncrementForUnique) add task description

---
+++ Datastructures_&_Algorithms/Arrays/minIncrementForUnique.js
@@ -0,0 +1,15 @@
+/**
+ * **945. Minimum Increment to Make Array Unique**
+ *
+ * You are given an integer array `nums`. In one move, you can pick an
+ * index `i` where `0 <= i < nums.length` and increment `nums[i]` by `1`
+ *
+ * Return *the minimum number of moves to make every value in `nums`
+ * **unique***.
+ *
+ * The test cases are generated so that the answer fits in a 32-bit integer.
+ *
+ * @param {number[]} nums
+ * @return {number}
+ */
+const minIncrementForUnique = function (nums) {};
