037b17d85d65decb13501fc925cb4377fb2073fb
yuhcee <egbouchenna001@gmail.com>
2024-06-14 23:47:45 +0100

    :white_check_mark: (minIncrementForUnique) add test case 2

--- Datastructures_&_Algorithms/Arrays/minIncrementForUnique.js
+++ Datastructures_&_Algorithms/Arrays/minIncrementForUnique.js
@@ -41,3 +41,9 @@ const nums = [1, 2, 2];
 // Output: 1
 // Explanation: After 1 move, the array could be [1, 2, 3].
 console.log(minIncrementForUnique(nums));
+
+const nums1 = [3, 2, 1, 2, 1, 7];
+// Output: 6
+/* Explanation: After 6 moves, the array could be [3, 4, 1, 2, 5, 7].
+It can be shown with 5 or less moves that it is impossible for the array to have all unique values. */
+console.log(minIncrementForUnique(nums1));
