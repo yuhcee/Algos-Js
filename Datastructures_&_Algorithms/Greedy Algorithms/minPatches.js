1e1acb30c6728b229ce451d2ba9f87365ca0ed38
yuhcee <egbouchenna001@gmail.com>
2024-06-16 22:33:45 +0100

    :white_check_mark: (minPatches) add test case 1

--- Datastructures_&_Algorithms/Greedy Algorithms/minPatches.js
+++ Datastructures_&_Algorithms/Greedy Algorithms/minPatches.js
@@ -34,3 +34,13 @@ const minPatches = function (nums, n) {
 
     return patches;
 };
+
+const nums = [1, 3],
+    n = 6;
+// Output: 1
+/* Explanation:
+Combinations of nums are [1], [3], [1,3], which form possible sums of: 1, 3, 4.
+Now if we add/patch 2 to nums, the combinations are: [1], [2], [3], [1,3], [2,3], [1,2,3].
+Possible sums are 1, 2, 3, 4, 5, 6, which now covers the range [1, 6].
+So we only need 1 patch. */
+console.log(minPatches(nums, n));
