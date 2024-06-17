792f7ae99dabe0c0c8260a78d5869979fe2aeeba
yuhcee <egbouchenna001@gmail.com>
2024-06-17 21:42:20 +0100

    :books: (judgeSquareSum) add task description

---
+++ Datastructures_&_Algorithms/Arrays/judgeSquareSum.js
@@ -0,0 +1,10 @@
+/**
+ * **633. Sum of Square Numbers**
+ *
+ * Given a non-negative integer `c`, decide whether there're two integers `a`
+ * and `b` such that `a2 + b2 = c`.
+ *
+ * @param {number} c
+ * @return {boolean}
+ */
+const judgeSquareSum = function (c) {};
