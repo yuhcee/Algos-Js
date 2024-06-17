38ba7ac54f21a4dc6cf86f91c1ee73d8fb78665e
yuhcee <egbouchenna001@gmail.com>
2024-06-17 21:43:19 +0100

    :sparkles: (judgeSquareSum) add task solution

--- Datastructures_&_Algorithms/Arrays/judgeSquareSum.js
+++ Datastructures_&_Algorithms/Arrays/judgeSquareSum.js
@@ -11,4 +11,15 @@
  * @param {number} c
  * @return {boolean}
  */
-const judgeSquareSum = function (c) {};
+const judgeSquareSum = function (c) {
+    let left = 0,
+        right = Math.floor(Math.sqrt(c));
+
+    while (left <= right) {
+        let sum = left * left + right * right;
+        if (sum === c) return true;
+        else if (sum > c) right--;
+        else left++;
+    }
+    return false;
+};
