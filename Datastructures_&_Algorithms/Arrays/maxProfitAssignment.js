c077ba8472bad181a2a8da091f0be4357b5e68a9
yuhcee <egbouchenna001@gmail.com>
2024-06-18 23:10:56 +0100

    :books: (maxProfitAssignment) add task description

---
+++ Datastructures_&_Algorithms/Arrays/maxProfitAssignment.js
@@ -0,0 +1,27 @@
+/**
+ * **826. Most Profit Assigning Work**
+ *
+ * You have `n` jobs and `m` workers. You are given three arrays: `difficulty`,
+ * `profit`, and `worker` where:
+ *
+ * - `difficulty[i]` and `profit[i]` are the difficulty and the profit of the
+ * `ith` job, and
+ * - `worker[j]` is the ability of `jth` worker (i.e., the `jth` worker can only
+ * complete a job with difficulty at most `worker[j]`).
+ *
+ * Every worker can be assigned **at most one job**, but one job can be
+ * **completed multiple times**.
+ *
+ * - For example, if three workers attempt the same job that pays `$1`, then the
+ * total profit will be `$3`. If a worker cannot complete any job, their profit
+ * is `$0`.
+ *
+ * Return the maximum profit we can achieve after assigning the workers to the
+ * jobs.
+ *
+ * @param {number[]} difficulty
+ * @param {number[]} profit
+ * @param {number[]} worker
+ * @return {number}
+ */
+const maxProfitAssignment = function (difficulty, profit, worker) {};
