c17bb2a445048fa5d0a94e79d9fe1fa285db3753
yuhcee <egbouchenna001@gmail.com>
2024-06-18 23:12:30 +0100

    :white_check_mark: (maxProfitAssignment) add test case 1

--- Datastructures_&_Algorithms/Arrays/maxProfitAssignment.js
+++ Datastructures_&_Algorithms/Arrays/maxProfitAssignment.js
@@ -64,3 +64,10 @@ const maxProfitAssignment = function (difficulty, profit, worker) {
 
     return totalProfit;
 };
+
+const difficulty = [2, 4, 6, 8, 10],
+    profit = [10, 20, 30, 40, 50],
+    worker = [4, 5, 6, 7];
+// Output: 100
+// Explanation: Workers are assigned jobs of difficulty [4,4,6,6] and they get a profit of [20,20,30,30] separately.
+console.log(maxProfitAssignment(difficulty, profit, worker));
