c31fcc89087fc1728a739da64f281dc93cfad371
yuhcee <egbouchenna001@gmail.com>
2024-06-19 23:29:49 +0100

    :white_check_mark: (minDays) add test case 1

--- Datastructures_&_Algorithms/Arrays/minDays.js
+++ Datastructures_&_Algorithms/Arrays/minDays.js
@@ -62,3 +62,14 @@ const minDays = function (bloomDay, m, k) {
 
     return low;
 };
+
+const bloomDay = [1, 10, 3, 10, 2],
+    m = 3,
+    k = 1;
+// Output: 3
+/* Explanation: Let us see what happened in the first three days. x means flower bloomed and _ means flower did not bloom in the garden.
+We need 3 bouquets each should contain 1 flower.
+After day 1: [x, _, _, _, _]   // we can only make one bouquet.
+After day 2: [x, _, _, _, x]   // we can only make two bouquets.
+After day 3: [x, _, x, _, x]   // we can make 3 bouquets. The answer is 3. */
+console.log(minDays(bloomDay, m, k));
