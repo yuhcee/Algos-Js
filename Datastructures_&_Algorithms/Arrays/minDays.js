b8194da0153d43b13e1bef31d8f284d3f1cd697b
yuhcee <egbouchenna001@gmail.com>
2024-06-19 23:27:11 +0100

    :books: (minDays) add task description

---
+++ Datastructures_&_Algorithms/Arrays/minDays.js
@@ -0,0 +1,20 @@
+/**
+ * **1482. Minimum Number of Days to Make m Bouquets**
+ *
+ * You are given an integer array `bloomDay`, an integer `m` and an integer `k`.
+ *
+ * You want to make `m` bouquets. To make a bouquet, you need to use `k`
+ * **adjacent flowers** from the garden.
+ *
+ * The garden consists of `n` flowers, the `ith` flower will bloom in the
+ * `bloomDay[i]` and then can be used in **exactly one** bouquet.
+ *
+ * Return *the minimum number of days you need to wait to be able to make `m`
+ * bouquets from the garden*. If it is impossible to make m bouquets return `-1`.
+ *
+ * @param {number[]} bloomDay
+ * @param {number} m
+ * @param {number} k
+ * @return {number}
+ */
+const minDays = function (bloomDay, m, k) {};
