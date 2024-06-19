9e796f53625d0dfb39193c09cac05177721fdc1b
yuhcee <egbouchenna001@gmail.com>
2024-06-19 23:31:19 +0100

    :white_check_mark: (minDays) add test case 3

--- Datastructures_&_Algorithms/Arrays/minDays.js
+++ Datastructures_&_Algorithms/Arrays/minDays.js
@@ -80,3 +80,15 @@ const bloomDay1 = [1, 10, 3, 10, 2],
 // Output: -1
 /* Explanation: We need 3 bouquets each has 2 flowers, that means we need 6 flowers. We only have 5 flowers so it is impossible to get the needed bouquets and we return -1. */
 console.log(minDays(bloomDay1, m1, k1));
+
+const bloomDay2 = [7, 7, 7, 7, 12, 7, 7],
+    m2 = 2,
+    k2 = 3;
+// Output: 12
+/* Explanation: We need 2 bouquets each should have 3 flowers.
+Here is the garden after the 7 and 12 days:
+After day 7: [x, x, x, x, _, x, x]
+We can make one bouquet of the first three flowers that bloomed. We cannot make another bouquet from the last three flowers that bloomed because they are not adjacent.
+After day 12: [x, x, x, x, x, x, x]
+It is obvious that we can make two bouquets in different ways. */
+console.log(minDays(bloomDay2, m2, k2));
