85eef648e183b4494eaf69dbf6db57b8e42941d4
yuhcee <egbouchenna001@gmail.com>
2024-06-19 23:29:37 +0100

    :sparkles: (minDays) add task solution

--- Datastructures_&_Algorithms/Arrays/minDays.js
+++ Datastructures_&_Algorithms/Arrays/minDays.js
@@ -25,4 +25,40 @@
  * @param {number} k
  * @return {number}
  */
-const minDays = function (bloomDay, m, k) {};
+const minDays = function (bloomDay, m, k) {
+    const n = bloomDay.length;
+    if (m * k > n) return -1; // If the total required flowers exceed available flowers, return -1.
+
+    let low = 1;
+    let high = Math.max(...bloomDay);
+
+    const canMakeBouquets = (days) => {
+        let bouquets = 0;
+        let flowers = 0;
+
+        for (let i = 0; i < n; i++) {
+            if (bloomDay[i] <= days) {
+                flowers++;
+                if (flowers == k) {
+                    bouquets++;
+                    flowers = 0;
+                }
+            } else {
+                flowers = 0;
+            }
+        }
+
+        return bouquets >= m;
+    };
+
+    while (low < high) {
+        let mid = Math.floor((low + high) / 2);
+        if (canMakeBouquets(mid)) {
+            high = mid;
+        } else {
+            low = mid + 1;
+        }
+    }
+
+    return low;
+};
