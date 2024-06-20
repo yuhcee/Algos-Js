f6ec3b3358f2c713c0d2e5840a4b600bf124fa15
yuhcee <egbouchenna001@gmail.com>
2024-06-20 22:13:59 +0100

    :books: (maxDistance) add task description

---
+++ Datastructures_&_Algorithms/Arrays/maxDistance.js
@@ -0,0 +1,21 @@
+/**
+ * **1552. Magnetic Force Between Two Balls**
+ *
+ * In the universe Earth C-137, Rick discovered a special form of magnetic force
+ * between two balls if they are put in his new invented basket.
+ *
+ * Rick has `n` empty baskets, the `ith` basket is at `position[i]`, Morty has
+ * `m` balls and needs to distribute the balls into the baskets such that the
+ * **minimum magnetic force** between any two balls is **maximum**.
+ *
+ * Rick stated that magnetic force between two different balls at positions `x`
+ * and `y` is `|x - y|`.
+ *
+ * Given the integer array `position` and the integer m. Return the required
+ * force.
+ *
+ * @param {number[]} position
+ * @param {number} m
+ * @return {number}
+ */
+const maxDistance = function (position, m) {};
