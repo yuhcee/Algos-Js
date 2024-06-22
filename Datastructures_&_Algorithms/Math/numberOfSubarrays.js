29d0759abe5622453aa886eb5d081de8fb763562
yuhcee <egbouchenna001@gmail.com>
2024-06-22 13:51:02 +0100

    :sparkles: (numberOfSubarrays) add task solution

--- Datastructures_&_Algorithms/Math/numberOfSubarrays.js
+++ Datastructures_&_Algorithms/Math/numberOfSubarrays.js
@@ -16,4 +16,32 @@
  * @param {number} k
  * @return {number}
  */
-const numberOfSubarrays = function (nums, k) {};
+const numberOfSubarrays = function (nums, k) {
+    // Step 1: Transform the nums array to a binary array where 1 represents odd and 0 represents even
+    let binaryArray = nums.map((num) => num % 2);
+
+    // Step 2: Initialize variables
+    let prefixSum = 0;
+    let prefixSumCount = { 0: 1 }; // To handle the case when a subarray itself has exactly k odd numbers
+    let result = 0;
+
+    // Step 3: Traverse the binary array and count the number of nice subarrays
+    for (let num of binaryArray) {
+        prefixSum += num; // Update the current prefix sum
+
+        // If there is a prefix sum such that current_prefix_sum - k exists, it means
+        // there is a subarray ending at current index which has exactly k odd numbers
+        if (prefixSumCount[prefixSum - k] !== undefined) {
+            result += prefixSumCount[prefixSum - k];
+        }
+
+        // Update the prefix sum count
+        if (prefixSumCount[prefixSum] === undefined) {
+            prefixSumCount[prefixSum] = 1;
+        } else {
+            prefixSumCount[prefixSum] += 1;
+        }
+    }
+
+    return result;
+};
