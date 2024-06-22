55db3a8c5bbb4d705c0f7bfd0b6f731ce9c73edf
yuhcee <egbouchenna001@gmail.com>
2024-06-22 13:48:50 +0100

    :books: (numberOfSubarrays) add task description

---
+++ Datastructures_&_Algorithms/Math/numberOfSubarrays.js
@@ -0,0 +1,13 @@
+/**
+ * **1248. Count Number of Nice Subarrays**
+ *
+ * Given an array of integers `nums` and an integer `k`. A continuous subarray
+ * is called nice if there are `k` odd numbers on it.
+ *
+ * Return *the number of **nice** sub-arrays*.
+ *
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+const numberOfSubarrays = function (nums, k) {};
