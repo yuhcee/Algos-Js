b744b3baff8e4013eb51e423754fada09d70f5b6
yuhcee <egbouchenna001@gmail.com>
2024-06-23 21:04:19 +0100

    :sparkles: (longestSubarray) add task solution

--- Datastructures_&_Algorithms/Arrays/longestContinuousSubarray.js
+++ Datastructures_&_Algorithms/Arrays/longestContinuousSubarray.js
@@ -16,4 +16,38 @@
  * @param {number} limit
  * @return {number}
  */
-const longestSubarray = function (nums, limit) {};
+const longestSubarray = function (nums, limit) {
+    let left = 0,
+        right = 0;
+    let maxLength = 0;
+
+    let maxDeque = [];
+    let minDeque = [];
+
+    while (right < nums.length) {
+        while (maxDeque.length && nums[maxDeque[maxDeque.length - 1]] <= nums[right]) {
+            maxDeque.pop();
+        }
+        maxDeque.push(right);
+
+        while (minDeque.length && nums[minDeque[minDeque.length - 1]] >= nums[right]) {
+            minDeque.pop();
+        }
+        minDeque.push(right);
+
+        while (nums[maxDeque[0]] - nums[minDeque[0]] > limit) {
+            left++;
+            if (maxDeque[0] < left) {
+                maxDeque.shift();
+            }
+            if (minDeque[0] < left) {
+                minDeque.shift();
+            }
+        }
+
+        maxLength = Math.max(maxLength, right - left + 1);
+        right++;
+    }
+
+    return maxLength;
+};
