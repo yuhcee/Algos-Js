847c46ec9fe38b8310f6432a52bff336e7313d07
yuhcee <egbouchenna001@gmail.com>
2024-06-23 21:05:08 +0100

    :white_check_mark: (longestSubarray) add test case 1

--- Datastructures_&_Algorithms/Arrays/longestContinuousSubarray.js
+++ Datastructures_&_Algorithms/Arrays/longestContinuousSubarray.js
@@ -51,3 +51,20 @@ const longestSubarray = function (nums, limit) {
 
     return maxLength;
 };
+
+const nums = [8, 2, 4, 7],
+    limit = 4;
+// Output: 2
+/* Explanation: All subarrays are: 
+[8] with maximum absolute diff |8-8| = 0 <= 4.
+[8,2] with maximum absolute diff |8-2| = 6 > 4. 
+[8,2,4] with maximum absolute diff |8-2| = 6 > 4.
+[8,2,4,7] with maximum absolute diff |8-2| = 6 > 4.
+[2] with maximum absolute diff |2-2| = 0 <= 4.
+[2,4] with maximum absolute diff |2-4| = 2 <= 4.
+[2,4,7] with maximum absolute diff |2-7| = 5 > 4.
+[4] with maximum absolute diff |4-4| = 0 <= 4.
+[4,7] with maximum absolute diff |4-7| = 3 <= 4.
+[7] with maximum absolute diff |7-7| = 0 <= 4. 
+Therefore, the size of the longest subarray is 2. */
+console.log(longestSubarray(nums, limit));
