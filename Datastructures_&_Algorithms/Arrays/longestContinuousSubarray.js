44b0e5040dba4a1a5dc51f086c8b4f033f7c6015
yuhcee <egbouchenna001@gmail.com>
2024-06-23 21:03:56 +0100

    :heavy_plus_sign:(longestSubarray) add constraints

--- Datastructures_&_Algorithms/Arrays/longestContinuousSubarray.js
+++ Datastructures_&_Algorithms/Arrays/longestContinuousSubarray.js
@@ -2,9 +2,15 @@
  * **1438. Longest Continuous Subarray With Absolute Diff Less Than or Equal to
  * Limit**
  *
- * Given an array of integers `nums` and an integer `limit`, return the size of 
- * the longest **non-empty** subarray such that the absolute difference between 
+ * Given an array of integers `nums` and an integer `limit`, return the size of
+ * the longest **non-empty** subarray such that the absolute difference between
  * any two elements of this subarray is less than or equal to `limit`.
+ *
+ * **Constraints:**
+ *
+ * - `1 <= nums.length <= 105`
+ * - `1 <= nums[i] <= 109`
+ * - `0 <= limit <= 109`
  *
  * @param {number[]} nums
  * @param {number} limit
