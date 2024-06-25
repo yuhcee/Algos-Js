b37f0dbdce79d9a934c40de7e74ad5819e118dfc
yuhcee <egbouchenna001@gmail.com>
2024-06-25 21:40:23 +0100

    :white_check_mark: (bstToGst) add test case 1

--- Datastructures_&_Algorithms/Trees/BinaryTree/bstToGst.js
+++ Datastructures_&_Algorithms/Trees/BinaryTree/bstToGst.js
@@ -51,3 +51,7 @@ const bstToGst = function (root) {
     reverseInOrderTraversal(root);
     return root;
 };
+
+const root = [4, 1, 6, 0, 2, 5, 7, null, null, null, 3, null, null, null, 8];
+// Output: [30,36,21,36,35,26,15,null,null,null,33,null,null,null,8]
+console.log(bstToGst(root));
