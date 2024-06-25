b21547fc93d2f1188506da3028b98a5714ae81a8
yuhcee <egbouchenna001@gmail.com>
2024-06-25 21:37:13 +0100

    :books: (bstToGst) add task description

---
+++ Datastructures_&_Algorithms/Trees/BinaryTree/bstToGst.js
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * **1038. Binary Search Tree to Greater Sum Tree**
+ *
+ * Given the `root` of a Binary Search Tree (BST), convert it to a Greater Tree
+ * such that every key of the original BST is changed to the original key plus
+ * the sum of all keys greater than the original key in BST.
+ *
+ * As a reminder, a *binary search tree* is a tree that satisfies these
+ * constraints:
+ *
+ * - The left subtree of a node contains only nodes with keys **less than** the
+ * node's key.
+ * - The right subtree of a node contains only nodes with keys **greater than**
+ * the node's key.
+ * - Both the left and right subtrees must also be binary search trees.
+ *
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+const bstToGst = function (root) {};
