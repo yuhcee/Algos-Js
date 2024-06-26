c944f2f7e27901a84d2de9cb39c01d8207f02200
yuhcee <egbouchenna001@gmail.com>
2024-06-26 23:48:21 +0100

    :white_check_mark: (balanceBST) add test case 1

--- Datastructures_&_Algorithms/Trees/BinaryTree/balanceBST.js
+++ Datastructures_&_Algorithms/Trees/BinaryTree/balanceBST.js
@@ -47,3 +47,8 @@ const balanceBST = function (root) {
 
     return buildBalancedBST(0, values.length - 1);
 };
+
+const root = [1, null, 2, null, 3, null, 4, null, null];
+// Output: [2, 1, 3, null, null, null, 4];
+// Explanation: This is not the only correct answer, [3,1,4,null,2] is also correct.
+console.log(balanceBST(root));
