99254f756e6bb641a9290f1789dfd746df77b088
yuhcee <egbouchenna001@gmail.com>
2024-06-26 23:44:28 +0100

    :books: (balanceBST) add task description

---
+++ Datastructures_&_Algorithms/Trees/BinaryTree/balanceBST.js
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * **1382. Balance a Binary Search Tree**
+ *
+ * Given the `root` of a binary search tree, return *a **balanced** binary
+ * search tree with the same node values*. If there is more than one answer,
+ * return **any of them**.
+ *
+ * A binary search tree is **balanced** if the depth of the two subtrees of
+ * every node never differs by more than 1.
+ *
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+const balanceBST = function (root) {};
