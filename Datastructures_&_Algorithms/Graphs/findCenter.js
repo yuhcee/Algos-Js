1fe35c65a2aca88f69e78a29e999ece33199fad8
yuhcee <egbouchenna001@gmail.com>
2024-06-27 23:16:15 +0100

    :white_check_mark: (findCenter) add test case 2

--- Datastructures_&_Algorithms/Graphs/findCenter.js
+++ Datastructures_&_Algorithms/Graphs/findCenter.js
@@ -41,3 +41,12 @@ const edges = [
 // Output: 2
 // Explanation: As shown in the figure above, node 2 is connected to every other node, so 2 is the center.
 console.log(findCenter(edges));
+
+const edges1 = [
+    [1, 2],
+    [5, 1],
+    [1, 3],
+    [1, 4],
+];
+// Output: 1
+console.log(findCenter(edges1));
