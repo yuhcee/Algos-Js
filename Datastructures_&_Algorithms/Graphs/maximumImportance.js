1e500a80424d3f7afbcf3a16f9ba3e51a4d56f68
yuhcee <egbouchenna001@gmail.com>
2024-06-28 23:36:38 +0100

    :white_check_mark: (maximumImportance) add test case 1

--- Datastructures_&_Algorithms/Graphs/maximumImportance.js
+++ Datastructures_&_Algorithms/Graphs/maximumImportance.js
@@ -55,3 +55,24 @@ const maximumImportance = function (n, roads) {
 
     return totalImportance;
 };
+
+const n = 5,
+    roads = [
+        [0, 1],
+        [1, 2],
+        [2, 3],
+        [0, 2],
+        [1, 3],
+        [2, 4],
+    ];
+// Output: 43;
+/* Explanation: The figure above shows the country and the assigned values of [2,4,5,3,1].
+- The road (0,1) has an importance of 2 + 4 = 6.
+- The road (1,2) has an importance of 4 + 5 = 9.
+- The road (2,3) has an importance of 5 + 3 = 8.
+- The road (0,2) has an importance of 2 + 5 = 7.
+- The road (1,3) has an importance of 4 + 3 = 7.
+- The road (2,4) has an importance of 5 + 1 = 6.
+The total importance of all roads is 6 + 9 + 8 + 7 + 7 + 6 = 43.
+It can be shown that we cannot obtain a greater total importance than 43. */
+console.log(maximumImportance(n, roads));
