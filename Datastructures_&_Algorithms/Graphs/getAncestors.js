070dffe5fdb427eb4d11000e9cff84466a59a797
yuhcee <egbouchenna001@gmail.com>
2024-06-29 20:43:25 +0100

    :white_check_mark: (getAncestors) add test case 1

--- Datastructures_&_Algorithms/Graphs/getAncestors.js
+++ Datastructures_&_Algorithms/Graphs/getAncestors.js
@@ -66,3 +66,26 @@ const getAncestors = function (n, edges) {
     const result = ancestors.map((ancestorSet) => Array.from(ancestorSet).sort((a, b) => a - b));
     return result;
 };
+
+const n = 8,
+    edgeList = [
+        [0, 3],
+        [0, 4],
+        [1, 3],
+        [2, 4],
+        [2, 7],
+        [3, 5],
+        [3, 6],
+        [3, 7],
+        [4, 6],
+    ];
+// Output: [[],[],[],[0,1],[0,2],[0,1,3],[0,1,2,3,4],[0,1,2,3]]
+/* Explanation:
+The above diagram represents the input graph.
+- Nodes 0, 1, and 2 do not have any ancestors.
+- Node 3 has two ancestors 0 and 1.
+- Node 4 has two ancestors 0 and 2.
+- Node 5 has three ancestors 0, 1, and 3.
+- Node 6 has five ancestors 0, 1, 2, 3, and 4.
+- Node 7 has four ancestors 0, 1, 2, and 3. */
+console.log(getAncestors(n, edgeList));
