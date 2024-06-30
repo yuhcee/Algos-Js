3bd06649334c597f51ae094a570da6aa1090f48b
yuhcee <egbouchenna001@gmail.com>
2024-06-30 18:14:13 +0100

    :books: (maxNumEdgesToRemove) add task description

---
+++ Datastructures_&_Algorithms/Graphs/maxNumEdgesToRemove.js
@@ -0,0 +1,25 @@
+/**
+ * **1579. Remove Max Number of Edges to Keep Graph Fully Traversable**
+ *
+ * Alice and Bob have an undirected graph of `n` nodes and three types of
+ * edges:
+ *
+ * - Type 1: Can be traversed by Alice only.
+ * - Type 2: Can be traversed by Bob only.
+ * - Type 3: Can be traversed by both Alice and Bob.
+ *
+ * Given an array `edges` where `edges[i] = [typei, ui, vi]` represents a
+ * bidirectional edge of type `typei` between nodes `ui` and `vi`, find the
+ * maximum number of edges you can remove so that after removing the edges,
+ * the graph can still be fully traversed by both Alice and Bob. The graph
+ * is fully traversed by Alice and Bob if starting from any node, they can
+ * reach all other nodes.
+ *
+ * Return the maximum number of edges you can remove, or return `-1` if
+ * Alice and Bob cannot fully traverse the graph.
+ *
+ * @param {number} n
+ * @param {number[][]} edges
+ * @return {number}
+ */
+const maxNumEdgesToRemove = function (n, edges) {};
