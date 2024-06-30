13721cfedb8799f5938986646cb704a39c474af9
yuhcee <egbouchenna001@gmail.com>
2024-06-30 19:38:11 +0100

    :white_check_mark: (maxNumEdgesToRemove) add test case 1

--- Datastructures_&_Algorithms/Graphs/maxNumEdgesToRemove.js
+++ Datastructures_&_Algorithms/Graphs/maxNumEdgesToRemove.js
@@ -108,3 +108,16 @@ const maxNumEdgesToRemove = function (n, edges) {
 
     return edges.length - edgesUsed;
 };
+
+const n = 4,
+    edges = [
+        [3, 1, 2],
+        [3, 2, 3],
+        [1, 1, 3],
+        [1, 2, 4],
+        [1, 1, 2],
+        [2, 3, 4],
+    ];
+// Output: 2
+// Explanation: If we remove the 2 edges [1,1,2] and [1,1,3]. The graph will still be fully traversable by Alice and Bob. Removing any additional edge will not make it so. So the maximum number of edges we can remove is 2.
+console.log(maxNumEdgesToRemove(n, edges));
