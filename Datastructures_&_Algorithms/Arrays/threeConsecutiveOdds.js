3814c39d6c2ebb5d8a577fad72903f94f3f0b7e3
yuhcee <egbouchenna001@gmail.com>
2024-07-01 23:35:56 +0100

    :heavy_plus_sign:(threeConsecutiveOdds) add constraints

--- Datastructures_&_Algorithms/Arrays/threeConsecutiveOdds.js
+++ Datastructures_&_Algorithms/Arrays/threeConsecutiveOdds.js
@@ -4,6 +4,11 @@
  * Given an integer array `arr`, return `true` if there are three consecutive
  * odd numbers in the array. Otherwise, return `false`.
  *
+ * **Constraints:**
+ *
+ * - `1 <= arr.length <= 1000`
+ * - `1 <= arr[i] <= 1000`
+ *
  * @param {number[]} arr
  * @return {boolean}
  */
