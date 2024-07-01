8e66c3e146aecc3ccd9475fc6f8d5cfb89d31e02
yuhcee <egbouchenna001@gmail.com>
2024-07-01 23:34:57 +0100

    :books: (threeConsecutiveOdds) add task description

---
+++ Datastructures_&_Algorithms/Arrays/threeConsecutiveOdds.js
@@ -0,0 +1,10 @@
+/**
+ * **1550. Three Consecutive Odds**
+ *
+ * Given an integer array `arr`, return `true` if there are three consecutive
+ * odd numbers in the array. Otherwise, return `false`.
+ *
+ * @param {number[]} arr
+ * @return {boolean}
+ */
+const threeConsecutiveOdds = function (arr) {};
