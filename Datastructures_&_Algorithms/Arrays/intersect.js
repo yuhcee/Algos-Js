c09ad42c70bdebb80d5f10b0d6e49fa3026fe87b
yuhcee <egbouchenna001@gmail.com>
2024-07-02 23:54:41 +0100

    :heavy_plus_sign:(intersect) add constraints

--- Datastructures_&_Algorithms/Arrays/intersect.js
+++ Datastructures_&_Algorithms/Arrays/intersect.js
@@ -5,6 +5,11 @@
  * intersection*. Each element in the result must appear as many times as it
  * shows in both arrays and you may return the result in **any order**.
  *
+ * **Constraints:**
+ *
+ * - `1 <= nums1.length, nums2.length <= 1000`
+ * - `0 <= nums1[i], nums2[i] <= 1000`
+ *
  * @param {number[]} nums1
  * @param {number[]} nums2
  * @return {number[]}
