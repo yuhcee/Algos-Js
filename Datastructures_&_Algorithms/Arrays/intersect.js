6067f6207aacadeb23a58b6236ccc0f2e4bd7608
yuhcee <egbouchenna001@gmail.com>
2024-07-02 23:58:08 +0100

    :white_check_mark: (intersect) add test case 1

--- Datastructures_&_Algorithms/Arrays/intersect.js
+++ Datastructures_&_Algorithms/Arrays/intersect.js
@@ -34,3 +34,8 @@ const intersect = function (nums1, nums2) {
 
     return intersection;
 };
+
+const nums1 = [1, 2, 2, 1],
+    nums2 = [2, 2];
+// Output: [2,2]
+console.log(intersect(nums1, nums2));
