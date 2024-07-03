691ebb3a67d6bddd3df46020e8d5c80242da57cf
yuhcee <egbouchenna001@gmail.com>
2024-07-03 10:13:42 +0100

    :sparkles: (minDifference) add task solution

--- Datastructures_&_Algorithms/Greedy Algorithms/minDifference.js
+++ Datastructures_&_Algorithms/Greedy Algorithms/minDifference.js
@@ -17,4 +17,19 @@
  * @param {number[]} nums
  * @return {number}
  */
-const minDifference = function (nums) {};
+const minDifference = function (nums) {
+    if (nums.length <= 4) {
+        return 0; // If there are 4 or fewer elements, we can make all of them the same in at most 3 moves.
+    }
+
+    nums.sort((a, b) => a - b);
+
+    let n = nums.length;
+    // Possible scenarios
+    let scenario1 = nums[n - 1] - nums[3]; // Change the three smallest elements
+    let scenario2 = nums[n - 2] - nums[2]; // Change the two smallest elements and the largest element
+    let scenario3 = nums[n - 3] - nums[1]; // Change the smallest element and the two largest elements
+    let scenario4 = nums[n - 4] - nums[0]; // Change the three largest elements
+
+    return Math.min(scenario1, scenario2, scenario3, scenario4);
+};
