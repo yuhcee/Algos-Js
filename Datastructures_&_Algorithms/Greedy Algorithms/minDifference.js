5a0cd8b35d382919b9a0eedecce2a859ea581881
yuhcee <egbouchenna001@gmail.com>
2024-07-03 10:12:51 +0100

    :books: (minDifference) add task description

---
+++ Datastructures_&_Algorithms/Greedy Algorithms/minDifference.js
@@ -0,0 +1,15 @@
+/**
+ * **1509. Minimum Difference Between Largest and Smallest Value in Three Moves**
+ *
+ * You are given an integer array `nums`.
+ *
+ * In one move, you can choose one element of `nums` and change it to **any
+ * value**.
+ *
+ * Return *the minimum difference between the largest and smallest value of nums
+ * **after performing at most three moves***.
+ *
+ * @param {number[]} nums
+ * @return {number}
+ */
+const minDifference = function (nums) {};
