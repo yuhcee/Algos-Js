e06254a6c7df4439e93ec541b214d83ca2673427
yuhcee <egbouchenna001@gmail.com>
2024-07-04 10:15:30 +0100

    :sparkles: (mergeNodes) add task solution

--- Datastructures_&_Algorithms/Lists/mergeNodes.js
+++ Datastructures_&_Algorithms/Lists/mergeNodes.js
@@ -28,4 +28,29 @@
  * @param {ListNode} head
  * @return {ListNode}
  */
-const mergeNodes = function (head) {};
+const mergeNodes = function (head) {
+    // Create a dummy node to simplify edge cases
+    let dummy = new ListNode(0);
+    let current = dummy;
+
+    // Initialize sum accumulator
+    let sum = 0;
+
+    // Traverse the list starting from the next node of head (since head is 0)
+    head = head.next;
+    while (head !== null) {
+        if (head.val === 0) {
+            // When we encounter a zero, add a new node with the accumulated sum
+            current.next = new ListNode(sum);
+            current = current.next; // Move the pointer to the new node
+            sum = 0; // Reset the sum
+        } else {
+            // Accumulate the sum
+            sum += head.val;
+        }
+        head = head.next; // Move to the next node
+    }
+
+    // Return the next node of dummy, which is the head of the result list
+    return dummy.next;
+};
