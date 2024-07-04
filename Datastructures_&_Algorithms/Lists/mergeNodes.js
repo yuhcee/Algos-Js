8b32fcbccb591fab5ad0b1775eedf80484873c09
yuhcee <egbouchenna001@gmail.com>
2024-07-04 10:16:08 +0100

    :white_check_mark: (mergeNodes) add test case 1

--- Datastructures_&_Algorithms/Lists/mergeNodes.js
+++ Datastructures_&_Algorithms/Lists/mergeNodes.js
@@ -54,3 +54,11 @@ const mergeNodes = function (head) {
     // Return the next node of dummy, which is the head of the result list
     return dummy.next;
 };
+
+const head = [0, 3, 1, 0, 4, 5, 2, 0];
+// Output: [4,11]
+/* Explanation: 
+The above figure represents the given linked list. The modified list contains
+- The sum of the nodes marked in green: 3 + 1 = 4.
+- The sum of the nodes marked in red: 4 + 5 + 2 = 11. */
+console.log(mergeNodes(head));
