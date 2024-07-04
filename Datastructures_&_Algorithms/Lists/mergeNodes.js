8d7987456cf4abfe7982f207f6a43a2fb356d2f4
yuhcee <egbouchenna001@gmail.com>
2024-07-04 10:13:59 +0100

    :books: (mergeNodes) add task description

---
+++ Datastructures_&_Algorithms/Lists/mergeNodes.js
@@ -0,0 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * **2181. Merge Nodes in Between Zeros**
+ *
+ * You are given the `head` of a linked list, which contains a series of integers
+ * **separated** by `0`'s. The **beginning** and **end** of the linked list will
+ * have `Node.val == 0`.
+ *
+ * For every two consecutive `0`'s, **merge** all the nodes lying in between them
+ * into a single node whose value is the **sum** of all the merged nodes. The
+ * modified list should not contain any `0`'s.
+ *
+ * Return *the head of the modified linked list*.
+ *
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+const mergeNodes = function (head) {};
