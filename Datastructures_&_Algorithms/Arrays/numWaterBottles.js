d86cedcf7260b39f5fc6cf2d72d89d06a7ff3bc3
yuhcee <egbouchenna001@gmail.com>
2024-07-07 23:24:39 +0100

    :books: (numWaterBottles) add task description

---
+++ Datastructures_&_Algorithms/Arrays/numWaterBottles.js
@@ -0,0 +1,17 @@
+/**
+ * **1518. Water Bottles**
+ *
+ * There are `numBottles` water bottles that are initially full of water. You can
+ * exchange `numExchange` empty water bottles from the market with one full water
+ * bottle.
+ *
+ * The operation of drinking a full water bottle turns it into an empty bottle.
+ *
+ * Given the two integers `numBottles` and `numExchange`, return *the **maximum**
+ * number of water bottles you can drink*.
+ *
+ * @param {number} numBottles
+ * @param {number} numExchange
+ * @return {number}
+ */
+const numWaterBottles = function (numBottles, numExchange) {};
