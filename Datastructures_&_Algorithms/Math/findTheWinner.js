19c1dd28e5e1753e30ef6f426862d8c6873df457
yuhcee <egbouchenna001@gmail.com>
2024-07-08 23:01:36 +0100

    :white_check_mark: (findTheWinner) add test case 1

--- Datastructures_&_Algorithms/Math/findTheWinner.js
+++ Datastructures_&_Algorithms/Math/findTheWinner.js
@@ -43,3 +43,18 @@ const findTheWinner = function (n, k) {
 
     return winner + 1; // Convert to 1-indexed result
 };
+
+const n = 5,
+    k = 2;
+// Output: 3;
+/* Explanation: Here are the steps of the game:
+1) Start at friend 1.
+2) Count 2 friends clockwise, which are friends 1 and 2.
+3) Friend 2 leaves the circle. Next start is friend 3.
+4) Count 2 friends clockwise, which are friends 3 and 4.
+5) Friend 4 leaves the circle. Next start is friend 5.
+6) Count 2 friends clockwise, which are friends 5 and 1.
+7) Friend 1 leaves the circle. Next start is friend 3.
+8) Count 2 friends clockwise, which are friends 3 and 5.
+9) Friend 5 leaves the circle. Only friend 3 is left, so they are the winner. */
+console.log(findTheWinner(n, k));
