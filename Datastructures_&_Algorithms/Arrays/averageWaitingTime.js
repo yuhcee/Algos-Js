1d086bf8aa40ab7e6f20f658fe44d5e7b644256f
yuhcee <egbouchenna001@gmail.com>
2024-07-09 23:18:39 +0100

    :white_check_mark: (averageWaitingTime) add test case 1

--- Datastructures_&_Algorithms/Arrays/averageWaitingTime.js
+++ Datastructures_&_Algorithms/Arrays/averageWaitingTime.js
@@ -41,3 +41,16 @@ const averageWaitingTime = function (customers) {
 
     return totalWaitingTime / customers.length;
 };
+
+const customers = [
+    [1, 2],
+    [2, 5],
+    [4, 3],
+];
+// Output: 5.00000
+/* Explanation:
+1) The first customer arrives at time 1, the chef takes his order and starts preparing it immediately at time 1, and finishes at time 3, so the waiting time of the first customer is 3 - 1 = 2.
+2) The second customer arrives at time 2, the chef takes his order and starts preparing it at time 3, and finishes at time 8, so the waiting time of the second customer is 8 - 2 = 6.
+3) The third customer arrives at time 4, the chef takes his order and starts preparing it at time 8, and finishes at time 11, so the waiting time of the third customer is 11 - 4 = 7.
+So the average waiting time = (2 + 6 + 7) / 3 = 5. */
+console.log(averageWaitingTime(customers));
