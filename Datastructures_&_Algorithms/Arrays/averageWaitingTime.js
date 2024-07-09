07ebc79cf90ae61c3a564f67b9b9b6d267efdce2
yuhcee <egbouchenna001@gmail.com>
2024-07-09 23:10:19 +0100

    :sparkles: (averageWaitingTime) add task solution

--- Datastructures_&_Algorithms/Arrays/averageWaitingTime.js
+++ Datastructures_&_Algorithms/Arrays/averageWaitingTime.js
@@ -26,4 +26,18 @@
  * @param {number[][]} customers
  * @return {number}
  */
-const averageWaitingTime = function (customers) {};
+const averageWaitingTime = function (customers) {
+    let currentTime = 0;
+    let totalWaitingTime = 0;
+
+    for (let i = 0; i < customers.length; i++) {
+        const [arrival, time] = customers[i];
+        if (currentTime < arrival) {
+            currentTime = arrival;
+        }
+        currentTime += time;
+        totalWaitingTime += currentTime - arrival;
+    }
+
+    return totalWaitingTime / customers.length;
+};
