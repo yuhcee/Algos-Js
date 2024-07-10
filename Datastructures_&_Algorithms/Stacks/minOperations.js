e7cb5aa763fe11bc49dbac08e372ad30307591ce
yuhcee <egbouchenna001@gmail.com>
2024-07-10 23:52:21 +0100

    :sparkles: (minOperations) add task solution

--- Datastructures_&_Algorithms/Stacks/minOperations.js
+++ Datastructures_&_Algorithms/Stacks/minOperations.js
@@ -32,4 +32,18 @@
  * @param {string[]} logs
  * @return {number}
  */
-const minOperations = function (logs) {};
+const minOperations = function (logs) {
+    let depth = 0;
+
+    for (let log of logs) {
+        if (log === '../') {
+            if (depth > 0) {
+                depth--;
+            }
+        } else if (log !== './') {
+            depth++;
+        }
+    }
+
+    return depth;
+};
