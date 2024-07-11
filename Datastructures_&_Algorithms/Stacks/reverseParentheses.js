91efb0668984805c87ee921c3db1fec768f99ea9
yuhcee <egbouchenna001@gmail.com>
2024-07-11 23:37:24 +0100

    :books: (reverseParentheses) add task description

---
+++ Datastructures_&_Algorithms/Stacks/reverseParentheses.js
@@ -0,0 +1,15 @@
+/**
+ * **1190. Reverse Substrings Between Each Pair of Parentheses**
+ *
+ * You are given a string `s` that consists of lower case English letters and
+ * brackets.
+ *
+ * Reverse the strings in each pair of matching parentheses, starting from the
+ * innermost one.
+ *
+ * Your result should not contain any brackets.
+ *
+ * @param {string} s
+ * @return {string}
+ */
+const reverseParentheses = function (s) {};
