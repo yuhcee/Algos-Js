e66b82469b459c38e700b5244224a1d6397c2431
yuhcee <egbouchenna001@gmail.com>
2024-07-12 14:33:06 +0100

    :books: (maximumGain) add task description

---
+++ Datastructures_&_Algorithms/Stacks/maximumGain.js
@@ -0,0 +1,20 @@
+/**
+ * **1717. Maximum Score From Removing Substrings**
+ *
+ * You are given a string `s` and two integers `x` and `y`. You can perform two
+ * types of operations any number of times.
+ *
+ * - Remove substring `"ab"` and gain `x` points.
+ *  - For example, when removing `"ab"` from `"cabxbae"` it becomes `"cxbae"`.
+ * - Remove substring `"ba"` and gain `y` points.
+ *  - For example, when removing `"ba"` from `"cabxbae"` it becomes `"cabxe"`.
+ *
+ * Return *the maximum points you can gain after applying the above operations on
+ * `s`*.
+ *
+ * @param {string} s
+ * @param {number} x
+ * @param {number} y
+ * @return {number}
+ */
+const maximumGain = function (s, x, y) {};
