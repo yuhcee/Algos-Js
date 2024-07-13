40447dca0ffb7d01509cbea87f94c77cf800e20e
yuhcee <egbouchenna001@gmail.com>
2024-07-14 00:04:45 +0100

    :white_check_mark: (survivedRobotsHealths) add test case 1

--- Datastructures_&_Algorithms/Stacks/survivedRobotsHealths.js
+++ Datastructures_&_Algorithms/Stacks/survivedRobotsHealths.js
@@ -1,38 +1,50 @@
 /**
  * **2751. Robot Collisions**
- * 
- * There are n 1-indexed robots, each having a position on a line, health, and movement direction.
-
-You are given 0-indexed integer arrays positions, healths, and a string directions (directions[i] is either 'L' for left or 'R' for right). All integers in positions are unique.
-
-All robots start moving on the line simultaneously at the same speed in their given directions. If two robots ever share the same position while moving, they will collide.
-
-If two robots collide, the robot with lower health is removed from the line, and the health of the other robot decreases by one. The surviving robot continues in the same direction it was going. If both robots have the same health, they are both removed from the line.
-
-Your task is to determine the health of the robots that survive the collisions, in the same order that the robots were given, i.e. final heath of robot 1 (if survived), final health of robot 2 (if survived), and so on. If there are no survivors, return an empty array.
-
-Return an array containing the health of the remaining robots (in the order they were given in the input), after no further collisions can occur.
-
-Note: The positions may be unsorted.
-
-**Constraints:**
-
-1 <= positions.length == healths.length == directions.length == n <= 105
-1 <= positions[i], healths[i] <= 109
-directions[i] == 'L' or directions[i] == 'R'
-All values in positions are distinct
-
+ *
+ * There are `n` **1-indexed** robots, each having a position on a line, health,
+ * and movement direction.
+ *
+ * You are given **0-indexed** integer arrays `positions`, `healths`, and a
+ * string `directions` (`directions[i]` is either **'L'** for **left** or **'R'**
+ * for **right**). All integers in `positions` are **unique**.
+ *
+ * All robots start moving on the line **simultaneously** at the **same speed**
+ * in their given directions. If two robots ever share the same position while
+ * moving, they will collide.
+ *
+ * If two robots collide, the robot with **lower health** is **removed** from the
+ * line, and the health of the other robot **decreases by one**. The surviving
+ * robot continues in the **same** direction it was going. If both robots have
+ * the **same** health, they are both removed from the line.
+ *
+ * Your task is to determine the **health** of the robots that survive the
+ * collisions, in the same **order** that the robots were given, i.e. final heath
+ * of robot 1 (if survived), final health of robot 2 (if survived), and so on. If
+ * there are no survivors, return an empty array.
+ *
+ * Return an array containing the health of the remaining robots (in the order
+ * they were given in the input), after no further collisions can occur.
+ *
+ * **Note**: The positions may be unsorted.
+ *
+ * **Constraints:**
+ *
+ * - `1 <= positions.length == healths.length == directions.length == n <= 105`
+ * - `1 <= positions[i], healths[i] <= 109`
+ * - `directions[i] == 'L' or directions[i] == 'R'`
+ * - All values in `positions` are distinct
+ *
  * @param {number[]} positions
  * @param {number[]} healths
  * @param {string} directions
  * @return {number[]}
  */
-const survivedRobotsHealths = function(positions, healths, directions) {
+const survivedRobotsHealths = function (positions, healths, directions) {
     const robots = positions.map((pos, index) => ({
         pos,
         health: healths[index],
         dir: directions[index],
-        index
+        index,
     }));
 
     // Sort robots based on positions
@@ -70,5 +82,12 @@ const survivedRobotsHealths = function(positions, healths, directions) {
 
     result.sort((a, b) => a.index - b.index);
 
-    return result.map(robot => robot.health);
-}
+    return result.map((robot) => robot.health);
+};
+
+const positions = [5, 4, 3, 2, 1],
+    healths = [2, 17, 9, 15, 10],
+    directions = 'RRRRR';
+// Output: [2,17,9,15,10]
+/* Explanation: No collision occurs in this example, since all robots are moving in the same direction. So, the health of the robots in order from the first robot is returned, [2, 17, 9, 15, 10]. */
+console.log(survivedRobotsHealths(positions, healths, directions));
