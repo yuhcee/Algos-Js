770ee19c85ae679fa142457d153c91661af36f6c
yuhcee <egbouchenna001@gmail.com>
2024-07-13 23:58:41 +0100

    :books: (survivedRobotsHealths) add task description

---
+++ Datastructures_&_Algorithms/Stacks/survivedRobotsHealths.js
@@ -0,0 +1,7 @@
+/**
+ * @param {number[]} positions
+ * @param {number[]} healths
+ * @param {string} directions
+ * @return {number[]}
+ */
+const survivedRobotsHealths = function(positions, healths, directions) {}
