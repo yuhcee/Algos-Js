0bdaaa691e32ae1e02f8cb0abbe5d3b7b64e1d8c
Uchenna Egbo <egbouchenna001@gmail.com>
2024-07-15 23:01:09 +0100

    :white_check_mark: (createBinaryTree) add test 2

--- Datastructures_&_Algorithms/Trees/BinaryTree/createBinaryTree.js
+++ Datastructures_&_Algorithms/Trees/BinaryTree/createBinaryTree.js
@@ -62,3 +62,9 @@ const descriptions = [[20,15,1],[20,17,0],[50,20,1],[50,80,0],[80,19,1]]
 /* Explanation: The root node is the node with value 50 since it has no parent.
 The resulting binary tree is shown in the diagram. */
 console.log(createBinaryTree(descriptions));
+
+const descriptions1 = [[1,2,1],[2,3,0],[3,4,1]]
+// Output: [1,2,null,null,3,4]
+/* Explanation: The root node is the node with value 1 since it has no parent.
+The resulting binary tree is shown in the diagram. */
+console.log(createBinaryTree(descriptions1));
