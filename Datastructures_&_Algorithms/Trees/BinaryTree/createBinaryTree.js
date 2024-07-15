985914f2e80b8b8474d50865b82d15d676521f06
Uchenna Egbo <egbouchenna001@gmail.com>
2024-07-15 22:55:25 +0100

    :sparkles: (createBinaryTree) add task solution

--- Datastructures_&_Algorithms/Trees/BinaryTree/createBinaryTree.js
+++ Datastructures_&_Algorithms/Trees/BinaryTree/createBinaryTree.js
@@ -26,4 +26,33 @@ function TreeNode(val) {
  * @param {number[][]} descriptions
  * @return {TreeNode}
  */
-const createBinaryTree = function(descriptions) {}
+const createBinaryTree = function(descriptions) {
+    const nodes = new Map();
+    const children = new Set();
+
+    for (const [parent, child, isLeft] of descriptions) {
+        if (!nodes.has(parent)) {
+            nodes.set(parent, new TreeNode(parent));
+        }
+        if (!nodes.has(child)) {
+            nodes.set(child, new TreeNode(child));
+        }
+        
+        if (isLeft) {
+            nodes.get(parent).left = nodes.get(child);
+        } else {
+            nodes.get(parent).right = nodes.get(child);
+        }
+        
+        children.add(child);
+    }
+
+    // The root node is the one that is never a child
+    for (const [parent, ,] of descriptions) {
+        if (!children.has(parent)) {
+            return nodes.get(parent);
+        }
+    }
+
+    return null;
+}
