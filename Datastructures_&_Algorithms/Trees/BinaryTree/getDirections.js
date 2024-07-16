27cc708962846aa455786bf3b156d74d5c9d6eeb
yuhcee <egbouchenna001@gmail.com>
2024-07-16 01:39:35 +0100

    :books: (getDirections) add task description

---
+++ Datastructures_&_Algorithms/Trees/BinaryTree/getDirections.js
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * **2096. Step-By-Step Directions From a Binary Tree Node to Another**
+ *
+ * You are given the `root` of a **binary** tree with `n` nodes. Each node is
+ * uniquely assigned a value from `1` to `n`. You are also given an integer
+ * `startValue` representing the value of the start node `s`, and a different
+ * integer `destValue` representing the value of the destination node `t`.
+ *
+ * Find the **shortest** path starting from node s and ending at node t. Generate
+ * step-by-step directions of such path as a string consisting of only the
+ * **uppercase** letters 'L', 'R', and 'U'. Each letter indicates a specific
+ * direction:
+ *
+ * - `'L'` means to go from a node to its **left child** node.
+ * - `'R'` means to go from a node to its **right child** node.
+ * - `'U'` means to go from a node to its **parent** node.
+ *
+ * Return *the step-by-step directions of the **shortest path** from node `s` to \
+ * node `t`.*
+ *
+ * @param {TreeNode} root
+ * @param {number} startValue
+ * @param {number} destValue
+ * @return {string}
+ */
+const getDirections = function (root, startValue, destValue) {};
