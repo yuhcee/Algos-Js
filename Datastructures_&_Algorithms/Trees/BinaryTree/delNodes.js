1f9494b2d0f802a65f279cc1aed4a37623e952d1
yuhcee <egbouchenna001@gmail.com>
2024-07-17 23:52:24 +0100

    :books: (delNodes) add task description

---
+++ Datastructures_&_Algorithms/Trees/BinaryTree/delNodes.js
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * **1110. Delete Nodes And Return Forest**
+ *
+ * Given the `root` of a binary tree, each node in the tree has a distinct value.
+ *
+ * After deleting all nodes with a value in `to_delete`, we are left with a
+ * forest (a disjoint union of trees).
+ *
+ * Return the roots of the trees in the remaining forest. You may return the
+ * result in any order.
+ *
+ * @param {TreeNode} root
+ * @param {number[]} to_delete
+ * @return {TreeNode[]}
+ */
+const delNodes = function (root, to_delete) {};
