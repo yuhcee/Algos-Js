432377e247f626e3c3adc6c372dff2e37a5e01a0
yuhcee <egbouchenna001@gmail.com>
2024-07-18 22:57:05 +0100

    :books: (countPairs) add task description

---
+++ Datastructures_&_Algorithms/Trees/BinaryTree/countPairs.js
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * **1530. Number of Good Leaf Nodes Pairs**
+ *
+ * You are given the `root` of a binary tree and an integer `distance`. A
+ * pair of two different **leaf** nodes of a binary tree is said to be
+ * good if the length of **the shortest path** between them is less than
+ * or equal to `distance`.
+ *
+ * Return *the number of good leaf node pairs in the tree*.
+ *
+ * @param {TreeNode} root
+ * @param {number} distance
+ * @return {number}
+ */
+const countPairs = function (root, distance) {};
