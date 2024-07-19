67359090f9a8801e3c8d80db128be5b85c225e42
yuhcee <egbouchenna001@gmail.com>
2024-07-19 22:02:52 +0100

    :books: (luckyNumbers) add task description

---
+++ Datastructures_&_Algorithms/Arrays/luckyNumbers.js
@@ -0,0 +1,13 @@
+/**
+ * **1380. Lucky Numbers in a Matrix**
+ *
+ * Given an `m x n` matrix of **distinct** numbers, return *all **lucky
+ * numbers** in the matrix in **any** order*.
+ *
+ * A **lucky number** is an element of the matrix such that it is the
+ * minimum element in its row and maximum in its column.
+ *
+ * @param {number[][]} matrix
+ * @return {number[]}
+ */
+const luckyNumbers = function (matrix) {};
