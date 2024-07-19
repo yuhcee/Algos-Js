3c869e75df68b37802040728633f23cb46ffe7fc
yuhcee <egbouchenna001@gmail.com>
2024-07-19 22:03:55 +0100

    :heavy_plus_sign:(luckyNumbers) add constraints

--- Datastructures_&_Algorithms/Arrays/luckyNumbers.js
+++ Datastructures_&_Algorithms/Arrays/luckyNumbers.js
@@ -7,6 +7,14 @@
  * A **lucky number** is an element of the matrix such that it is the
  * minimum element in its row and maximum in its column.
  *
+ * **Constraints:**
+ * 
+ * - `m == mat.length`
+ * - `n == mat[i].length`
+ * - `1 <= n, m <= 50`
+ * - `1 <= matrix[i][j] <= 105.`
+ * - All elements in the matrix are distinct.
+ * 
  * @param {number[][]} matrix
  * @return {number[]}
  */
