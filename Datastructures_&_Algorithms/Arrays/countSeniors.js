31fb73821849ef2b6888faeae6aec9af04c9982c
yuhcee <egbouchenna001@gmail.com>
2024-08-02 00:01:51 +0100

    :white_check_mark: (countSeniors) add test case 2

--- Datastructures_&_Algorithms/Arrays/countSeniors.js
+++ Datastructures_&_Algorithms/Arrays/countSeniors.js
@@ -44,3 +44,8 @@ const details = ['7868190130M7522', '5303914400F9211', '9273338290F4010'];
 // Output: 2
 // Explanation: The passengers at indices 0, 1, and 2 have ages 75, 92, and 40. Thus, there are 2 people who are over 60 years old.
 console.log(countSeniors(details));
+
+const details1 = ['1313579440F2036', '2921522980M5644'];
+// Output: 0
+// Explanation: None of the passengers are older than 60.
+console.log(countSeniors(details1));
