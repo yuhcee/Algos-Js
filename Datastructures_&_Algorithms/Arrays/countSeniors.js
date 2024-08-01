f8390507d7808ef254569e742255e672ab79e295
yuhcee <egbouchenna001@gmail.com>
2024-08-01 23:55:30 +0100

    :white_check_mark: (countSeniors) add test case 1

--- Datastructures_&_Algorithms/Arrays/countSeniors.js
+++ Datastructures_&_Algorithms/Arrays/countSeniors.js
@@ -15,13 +15,13 @@
  * old***.
  *
  * **Constraints:**
- * 
+ *
  * - `1 <= details.length <= 100`
  * - `details[i].length == 15`
  * - `details[i]` consists of digits from `'0'` to `'9'`.
  * - `details[i][10]` is either `'M'` or `'F'` or `'O'`.
  * - The phone numbers and seat numbers of the passengers are distinct.
- * 
+ *
  * @param {string[]} details
  * @return {number}
  */
@@ -39,3 +39,8 @@ const countSeniors = function (details) {
 
     return count;
 };
+
+const details = ['7868190130M7522', '5303914400F9211', '9273338290F4010'];
+// Output: 2
+// Explanation: The passengers at indices 0, 1, and 2 have ages 75, 92, and 40. Thus, there are 2 people who are over 60 years old.
+console.log(countSeniors(details));
