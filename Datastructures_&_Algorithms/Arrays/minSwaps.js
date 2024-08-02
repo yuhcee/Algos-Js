27de7882ec9a0a776d96b5583ba642eb0405f46c
yuhcee <egbouchenna001@gmail.com>
2024-08-02 21:24:00 +0100

    :sparkles: (minSwaps) add task solution

--- Datastructures_&_Algorithms/Arrays/minSwaps.js
+++ Datastructures_&_Algorithms/Arrays/minSwaps.js
@@ -19,4 +19,30 @@
  * @param {number[]} nums
  * @return {number}
  */
-const minSwaps = function (nums) {};
+const minSwaps = function (nums) {
+    const totalOnes = nums.reduce((sum, num) => sum + num, 0);
+    const n = nums.length;
+
+    if (totalOnes === 0) return 0; // If there are no 1s, no swaps are needed
+
+    // Create a circular version of the array by concatenating it with itself
+    const extendedNums = nums.concat(nums);
+
+    let currentOnes = 0;
+    let maxOnesInWindow = 0;
+
+    // Initialize the first window
+    for (let i = 0; i < totalOnes; i++) {
+        currentOnes += extendedNums[i];
+    }
+    maxOnesInWindow = currentOnes;
+
+    // Slide the window over the extended array
+    for (let i = totalOnes; i < extendedNums.length; i++) {
+        currentOnes += extendedNums[i] - extendedNums[i - totalOnes];
+        maxOnesInWindow = Math.max(maxOnesInWindow, currentOnes);
+    }
+
+    // Minimum swaps needed to group all 1s together
+    return totalOnes - maxOnesInWindow;
+};
