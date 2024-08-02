5ae7aa733f93deeccf24580512d28a60351fe586
yuhcee <egbouchenna001@gmail.com>
2024-08-02 21:22:09 +0100

    :books: (minSwaps) add task description

---
+++ Datastructures_&_Algorithms/Arrays/minSwaps.js
@@ -0,0 +1,17 @@
+/**
+ * **2134. Minimum Swaps to Group All 1's Together II**
+ *
+ * A **swap** is defined as taking two **distinct** positions in an array
+ * and swapping the values in them.
+ *
+ * A **circular** array is defined as an array where we consider the
+ * **first** element and the **last** element to be **adjacent**.
+ *
+ * Given a **binary circular** array `nums`, return *the minimum number of
+ * swaps required to group all 1's present in the array together at **any
+ * location***.
+ *
+ * @param {number[]} nums
+ * @return {number}
+ */
+const minSwaps = function (nums) {};
