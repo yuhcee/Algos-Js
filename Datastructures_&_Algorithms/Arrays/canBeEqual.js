e0ac1dac40db6b03bca97b2bd1dbcd74daf029af
yuhcee <egbouchenna001@gmail.com>
2024-08-03 18:45:03 +0100

    :books: (canBeEqual) add task description

---
+++ Datastructures_&_Algorithms/Arrays/canBeEqual.js
@@ -0,0 +1,15 @@
+/**
+ * **1460. Make Two Arrays Equal by Reversing Subarrays**
+ *
+ * You are given two integer arrays of equal length `target` and `arr`. In
+ * one step, you can select any **non-empty subarray** of `arr` and
+ * reverse it. You are allowed to make any number of steps.
+ *
+ * Return *`true` if you can make `arr` equal to `target` or `false`
+ * otherwise*.
+ *
+ * @param {number[]} target
+ * @param {number[]} arr
+ * @return {boolean}
+ */
+const canBeEqual = function (target, arr) {};
