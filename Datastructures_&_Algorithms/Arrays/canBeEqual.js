509d32c9b6f98a4f07c812ebfdd35d6656ebe759
yuhcee <egbouchenna001@gmail.com>
2024-08-03 18:46:27 +0100

    :sparkles: (canBeEqual) add task solution

--- Datastructures_&_Algorithms/Arrays/canBeEqual.js
+++ Datastructures_&_Algorithms/Arrays/canBeEqual.js
@@ -19,4 +19,17 @@
  * @param {number[]} arr
  * @return {boolean}
  */
-const canBeEqual = function (target, arr) {};
+const canBeEqual = function (target, arr) {
+    // Sort both arrays
+    target.sort((a, b) => a - b);
+    arr.sort((a, b) => a - b);
+
+    // Compare the sorted arrays
+    for (let i = 0; i < target.length; i++) {
+        if (target[i] !== arr[i]) {
+            return false;
+        }
+    }
+
+    return true;
+};
