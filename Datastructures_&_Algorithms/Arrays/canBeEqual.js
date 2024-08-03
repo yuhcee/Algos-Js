8f56046335eabb624c978dd8be4645f5fd2a2769
yuhcee <egbouchenna001@gmail.com>
2024-08-03 18:47:33 +0100

    :white_check_mark: (canBeEqual) add test case 1

--- Datastructures_&_Algorithms/Arrays/canBeEqual.js
+++ Datastructures_&_Algorithms/Arrays/canBeEqual.js
@@ -33,3 +33,13 @@ const canBeEqual = function (target, arr) {
 
     return true;
 };
+
+const target = [1, 2, 3, 4],
+    arr = [2, 4, 1, 3];
+// Output: true
+/* Explanation: You can follow the next steps to convert arr to target:
+1- Reverse subarray [2,4,1], arr becomes [1,4,2,3]
+2- Reverse subarray [4,2], arr becomes [1,2,4,3]
+3- Reverse subarray [4,3], arr becomes [1,2,3,4]
+There are multiple ways to convert arr to target, this is not the only way to do so. */
+console.log(canBeEqual(target, arr));
