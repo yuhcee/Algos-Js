065752733f1f8dc8eae4d9bb90d55f947a8cb3a9
yuhcee <egbouchenna001@gmail.com>
2024-08-04 17:26:55 +0100

    :books: (rangeSum) add task description

---
+++ Datastructures_&_Algorithms/Arrays/rangeSum.js
@@ -0,0 +1,19 @@
+/**
+ * **1508. Range Sum of Sorted Subarray Sums**
+ *
+ * You are given the array `nums` consisting of `n` positive integers. You
+ * computed the sum of all non-empty continuous subarrays from the array
+ * and then sorted them in non-decreasing order, creating a new array of
+ * `n * (n + 1) / 2` numbers.
+ *
+ * Return *the sum of the numbers from index `left` to index `right`
+ * (**indexed from 1**), inclusive, in the new array*. Since the answer
+ * can be a huge number return it modulo `109 + 7`.
+ *
+ * @param {number[]} nums
+ * @param {number} n
+ * @param {number} left
+ * @param {number} right
+ * @return {number}
+ */
+const rangeSum = function (nums, n, left, right) {};
