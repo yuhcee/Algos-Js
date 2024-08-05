214b3e1a6dcfeba91ee1a7e5086095e52bd3e2c9
yuhcee <egbouchenna001@gmail.com>
2024-08-05 22:23:19 +0100

    :books: (kthDistinct) add task description

---
+++ Datastructures_&_Algorithms/Strings/kthDistinct.js
@@ -0,0 +1,18 @@
+/**
+ * **2053. Kth Distinct String in an Array**
+ *
+ * A **distinct string** is a string that is present only **once** in an
+ * array.
+ *
+ * Given an array of strings `arr`, and an integer `k`, return *the `kth`
+ * **distinct string** present in `arr`*. If there are **fewer** than `k`
+ * distinct strings, return *an **empty string** `""`*.
+ *
+ * Note that the strings are considered in the **order in which they
+ * appear** in the array.
+ *
+ * @param {string[]} arr
+ * @param {number} k
+ * @return {string}
+ */
+const kthDistinct = function (arr, k) {};
