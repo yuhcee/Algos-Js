65cea8e53ac1380096fd495c394ed414d0e5e83a
yuhcee <egbouchenna001@gmail.com>
2024-08-05 22:30:30 +0100

    :white_check_mark: (kthDistinct) add test case 1

--- Datastructures_&_Algorithms/Strings/kthDistinct.js
+++ Datastructures_&_Algorithms/Strings/kthDistinct.js
@@ -47,3 +47,13 @@ const kthDistinct = function (arr, k) {
         return '';
     }
 };
+
+const arr = ['d', 'b', 'c', 'b', 'c', 'a'],
+    k = 2;
+// Output: "a"
+/* Explanation:
+The only distinct strings in arr are "d" and "a".
+"d" appears 1st, so it is the 1st distinct string.
+"a" appears 2nd, so it is the 2nd distinct string.
+Since k == 2, "a" is returned.  */
+console.log(kthDistinct(arr, k));
