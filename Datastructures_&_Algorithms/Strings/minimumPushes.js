00b45aeca1943edc695c2eb4d5ada0b1f99c0d58
yuhcee <egbouchenna001@gmail.com>
2024-08-06 23:56:40 +0100

    :white_check_mark: (minimumPushes) add test case 3

--- Datastructures_&_Algorithms/Strings/minimumPushes.js
+++ Datastructures_&_Algorithms/Strings/minimumPushes.js
@@ -61,12 +61,29 @@ Total cost is 1 + 1 + 1 + 1 + 1 = 5.
 It can be shown that no other mapping can provide a lower cost. */
 console.log(minimumPushes(word));
 
-const word1 = 'xyzxyzxyzxyz'// Output: 12
+const word1 = 'xyzxyzxyzxyz'; // Output: 12
 /* Explanation: The remapped keypad given in the image provides the minimum cost.
 "x" -> one push on key 2
 "y" -> one push on key 3
 "z" -> one push on key 4
 Total cost is 1 * 4 + 1 * 4 + 1 * 4 = 12
 It can be shown that no other mapping can provide a lower cost.
-Note that the key 9 is not mapped to any letter: it is not necessary to map letters to every key, but to map all the letters */ .console
-    .log(minimumPushes(word1));
+Note that the key 9 is not mapped to any letter: it is not necessary to map letters to every key, but to map all the letters .*/
+
+console.log(minimumPushes(word1));
+
+const word2 = 'aabbccddeeffgghhiiiiii';
+// Output: 24
+/* Explanation: The remapped keypad given in the image provides the minimum cost.
+    "a" -> one push on key 2
+    "b" -> one push on key 3
+    "c" -> one push on key 4
+    "d" -> one push on key 5
+    "e" -> one push on key 6
+    "f" -> one push on key 7
+    "g" -> one push on key 8
+    "h" -> two pushes on key 9
+    "i" -> one push on key 9
+    Total cost is 1 * 2 + 1 * 2 + 1 * 2 + 1 * 2 + 1 * 2 + 1 * 2 + 1 * 2 + 2 * 2 + 6 * 1 = 24.
+    It can be shown that no other mapping can provide a lower cost. */
+    console.log(minimumPushes(word2));
