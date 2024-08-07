440e99aa3d189ba4d801e37c0dbfe0d252a28646
yuhcee <egbouchenna001@gmail.com>
2024-08-07 23:03:54 +0100

    :sparkles: (numberToWords) add task solution

--- Datastructures_&_Algorithms/Strings/numberToWords.js
+++ Datastructures_&_Algorithms/Strings/numberToWords.js
@@ -11,4 +11,51 @@
  * @param {number} num
  * @return {string}
  */
-const numberToWords = function (num) {};
+const numberToWords = function (num) {
+    if (num === 0) return 'Zero';
+
+    const belowTwenty = [
+        '',
+        'One',
+        'Two',
+        'Three',
+        'Four',
+        'Five',
+        'Six',
+        'Seven',
+        'Eight',
+        'Nine',
+        'Ten',
+        'Eleven',
+        'Twelve',
+        'Thirteen',
+        'Fourteen',
+        'Fifteen',
+        'Sixteen',
+        'Seventeen',
+        'Eighteen',
+        'Nineteen',
+    ];
+    const tens = ['', '', 'Twenty', 'Thirty', 'Forty', 'Fifty', 'Sixty', 'Seventy', 'Eighty', 'Ninety'];
+    const thousands = ['', 'Thousand', 'Million', 'Billion'];
+
+    const helper = (num) => {
+        if (num === 0) return '';
+        if (num < 20) return belowTwenty[num] + ' ';
+        if (num < 100) return tens[Math.floor(num / 10)] + ' ' + helper(num % 10);
+        return belowTwenty[Math.floor(num / 100)] + ' Hundred ' + helper(num % 100);
+    };
+
+    let result = '';
+    let i = 0;
+
+    while (num > 0) {
+        if (num % 1000 !== 0) {
+            result = helper(num % 1000) + thousands[i] + ' ' + result;
+        }
+        num = Math.floor(num / 1000);
+        i++;
+    }
+
+    return result.trim();
+};
