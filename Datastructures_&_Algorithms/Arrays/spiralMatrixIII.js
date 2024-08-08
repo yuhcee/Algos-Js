395dd53a635e92b4b1fc87609c40650ab3e7f9b8
yuhcee <egbouchenna001@gmail.com>
2024-08-08 22:51:29 +0100

    :white_check_mark: (spiralMatrixIII) add test case 2

--- Datastructures_&_Algorithms/Arrays/spiralMatrixIII.js
+++ Datastructures_&_Algorithms/Arrays/spiralMatrixIII.js
@@ -66,3 +66,10 @@ const rows = 1,
     cStart = 0;
 // Output: [[0,0],[0,1],[0,2],[0,3]]
 console.log(spiralMatrixIII(rows, cols, rStart, cStart));
+
+const rows1 = 5,
+    cols1 = 6,
+    rStart1 = 1,
+    cStart1 = 4;
+// Output: [[1,4],[1,5],[2,5],[2,4],[2,3],[1,3],[0,3],[0,4],[0,5],[3,5],[3,4],[3,3],[3,2],[2,2],[1,2],[0,2],[4,5],[4,4],[4,3],[4,2],[4,1],[3,1],[2,1],[1,1],[0,1],[4,0],[3,0],[2,0],[1,0],[0,0]]
+console.log(spiralMatrixIII(rows1, cols1, rStart1, cStart1));
