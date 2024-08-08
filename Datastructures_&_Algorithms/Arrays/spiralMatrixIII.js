ce1fd2046685165a2fe9cf779f22a456bd01d1c0
yuhcee <egbouchenna001@gmail.com>
2024-08-08 22:48:17 +0100

    :books: (spiralMatrixIII) add task description

---
+++ Datastructures_&_Algorithms/Arrays/spiralMatrixIII.js
@@ -0,0 +1,22 @@
+/**
+ * **885. Spiral Matrix III**
+ * 
+ * You start at the cell (`rStart, cStart`) of an `rows x cols` grid facing 
+ * east. The northwest corner is at the first row and column in the grid, 
+ * and the southeast corner is at the last row and column.
+ * 
+ * You will walk in a clockwise spiral shape to visit every position in this 
+ * grid. Whenever you move outside the grid's boundary, we continue our walk 
+ * outside the grid (but may return to the grid boundary later.). 
+ * Eventually, we reach all `rows * cols` spaces of the grid.
+ * 
+ * Return *an array of coordinates representing the positions of the grid in 
+ * the order you visited them*.
+ * 
+ * @param {number} rows
+ * @param {number} cols
+ * @param {number} rStart
+ * @param {number} cStart
+ * @return {number[][]}
+ */
+const spiralMatrixIII = function (rows, cols, rStart, cStart) {};
