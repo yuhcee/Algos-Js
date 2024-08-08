e142b78053c0cbaff5c25a358f0148903825112e
yuhcee <egbouchenna001@gmail.com>
2024-08-08 22:49:35 +0100

    :sparkles: (spiralMatrixIII) add task solution

--- Datastructures_&_Algorithms/Arrays/spiralMatrixIII.js
+++ Datastructures_&_Algorithms/Arrays/spiralMatrixIII.js
@@ -25,4 +25,37 @@
  * @param {number} cStart
  * @return {number[][]}
  */
-const spiralMatrixIII = function (rows, cols, rStart, cStart) {};
+const spiralMatrixIII = function (rows, cols, rStart, cStart) {
+    const result = [];
+    const directions = [
+        [0, 1],
+        [1, 0],
+        [0, -1],
+        [-1, 0],
+    ];
+    let directionIndex = 0; // start by moving right
+    let steps = 1; // steps to take in the current direction
+
+    let r = rStart;
+    let c = cStart;
+
+    result.push([r, c]); // start position
+
+    while (result.length < rows * cols) {
+        for (let i = 0; i < 2; i++) {
+            // increase step count every two directions
+            const [dr, dc] = directions[directionIndex];
+            for (let j = 0; j < steps; j++) {
+                r += dr;
+                c += dc;
+                if (r >= 0 && r < rows && c >= 0 && c < cols) {
+                    result.push([r, c]);
+                }
+            }
+            directionIndex = (directionIndex + 1) % 4; // turn to the next direction
+        }
+        steps++; // increase the number of steps after two turns
+    }
+
+    return result;
+};
