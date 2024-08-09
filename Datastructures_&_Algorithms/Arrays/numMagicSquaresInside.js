810baf9e0ef12bfb4779b850911e17e5aaa2e9ce
yuhcee <egbouchenna001@gmail.com>
2024-08-09 15:47:48 +0100

    :books: (numMagicSquaresInside) add task description

---
+++ Datastructures_&_Algorithms/Arrays/numMagicSquaresInside.js
@@ -0,0 +1,17 @@
+/**
+ * **840. Magic Squares In Grid**
+ *
+ * A `3 x 3` **magic square** is a `3 x 3` grid filled with distinct numbers
+ * from 1 **to** 9 such that each row, column, and both diagonals all have
+ * the same sum.
+ *
+ * Given a `row x col` `grid` of integers, how many `3 x 3` contiguous magic
+ * square subgrids are there?
+ *
+ * **Note**: while a magic square can only contain numbers from 1 to 9,
+ * `grid` may contain numbers up to 15.
+ *
+ * @param {number[][]} grid
+ * @return {number}
+ */
+const numMagicSquaresInside = function (grid) {};
