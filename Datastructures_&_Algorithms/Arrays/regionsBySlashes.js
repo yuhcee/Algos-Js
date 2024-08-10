3980d3c7a873ba223f7ea3366b60e8f98a35cd12
yuhcee <egbouchenna001@gmail.com>
2024-08-10 23:10:19 +0100

    :books: (regionsBySlashes) add task description

---
+++ Datastructures_&_Algorithms/Arrays/regionsBySlashes.js
@@ -0,0 +1,17 @@
+/**
+ * **959. Regions Cut By Slashes**
+ *
+ * An `n x n` grid is composed of `1 x 1` squares where each `1 x 1` square
+ * consists of a `'/'`, `'\'`, or blank space `' '`. These characters divide
+ * the square into contiguous regions.
+ *
+ * Given the grid `grid` represented as a string array, return the number of
+ * regions.
+ *
+ * Note that backslash characters are escaped, so a `'\'` is represented as
+ * `'\\'`.
+ *
+ * @param {string[]} grid
+ * @return {number}
+ */
+const regionsBySlashes = function (grid) {};
