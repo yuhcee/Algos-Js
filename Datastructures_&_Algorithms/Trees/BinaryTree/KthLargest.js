0b395f01b875ab0f5f499f8bb142b921c1399da1
yuhcee <egbouchenna001@gmail.com>
2024-08-12 23:48:46 +0100

    :books: (KthLargest) add task description

---
+++ Datastructures_&_Algorithms/Trees/BinaryTree/KthLargest.js
@@ -0,0 +1,19 @@
+/**
+ * **703. Kth Largest Element in a Stream**
+ *
+ * Design a class to find the `kth` largest element in a stream. Note that
+ * it is the `kth` largest element in the sorted order, not the `kth`
+ * distinct element.
+ *
+ * Implement `KthLargest` class:
+ *
+ * - `KthLargest(int k, int[] nums)` Initializes the object with the integer
+ * `k` and the stream of integers `nums`.
+ *
+ * - `int add(int val)` Appends the integer `val` to the stream and returns
+ * the element representing the `kth` largest element in the stream.
+ *
+ * @param {number} k
+ * @param {number[]} nums
+ */
+const KthLargest = function (k, nums) {};
