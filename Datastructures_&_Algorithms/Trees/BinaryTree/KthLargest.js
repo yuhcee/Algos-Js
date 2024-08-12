43e6e49a406e3130db68a3d8cf7e36aa3c409bc5
yuhcee <egbouchenna001@gmail.com>
2024-08-13 00:02:08 +0100

    :white_check_mark: (KthLargest) add test case 2

--- Datastructures_&_Algorithms/Trees/BinaryTree/KthLargest.js
+++ Datastructures_&_Algorithms/Trees/BinaryTree/KthLargest.js
@@ -14,14 +14,15 @@
  * the element representing the `kth` largest element in the stream.
  *
  * **Constraints:**
- * 
- * 1 <= k <= 104
-0 <= nums.length <= 104
--104 <= nums[i] <= 104
--104 <= val <= 104
-At most 104 calls will be made to add.
-It is guaranteed that there will be at least k elements in the array when you search for the kth element.
-
+ *
+ * - `1 <= k <= 104`
+ * - `0 <= nums.length <= 104`
+ * - `-104 <= nums[i] <= 104`
+ * - `-104 <= val <= 104`
+ * - At most `104` calls will be made to `add`.
+ * - It is guaranteed that there will be at least `k` elements in the array
+ * when you search for the `kth` element.
+ *
  * @param {number} k
  * @param {number[]} nums
  */
@@ -54,10 +55,9 @@ KthLargest.prototype.add = function (val) {
  * var param_1 = obj.add(val)
  */
 
-const ["KthLargest", "add", "add", "add", "add", "add"]
-[[3, [4, 5, 8, 2]], [3], [5], [10], [9], [4]]
-Output
-[null, 4, 5, 5, 8, 8]
+const data = ['KthLargest', 'add', 'add', 'add', 'add', 'add'][([3, [4, 5, 8, 2]], [3], [5], [10], [9], [4])];
+// Output
+// [null, 4, 5, 5, 8, 8]
 
 /* Explanation
 KthLargest kthLargest = new KthLargest(3, [4, 5, 8, 2]);
