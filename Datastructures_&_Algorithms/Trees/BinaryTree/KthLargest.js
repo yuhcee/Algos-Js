fe7b06b5787b782c2a3c89b91f637dff67e36b82
yuhcee <egbouchenna001@gmail.com>
2024-08-12 23:59:45 +0100

    :white_check_mark: (KthLargest) add test case 1

--- Datastructures_&_Algorithms/Trees/BinaryTree/KthLargest.js
+++ Datastructures_&_Algorithms/Trees/BinaryTree/KthLargest.js
@@ -53,3 +53,16 @@ KthLargest.prototype.add = function (val) {
  * var obj = new KthLargest(k, nums)
  * var param_1 = obj.add(val)
  */
+
+const ["KthLargest", "add", "add", "add", "add", "add"]
+[[3, [4, 5, 8, 2]], [3], [5], [10], [9], [4]]
+Output
+[null, 4, 5, 5, 8, 8]
+
+/* Explanation
+KthLargest kthLargest = new KthLargest(3, [4, 5, 8, 2]);
+kthLargest.add(3);   // return 4
+kthLargest.add(5);   // return 5
+kthLargest.add(10);  // return 5
+kthLargest.add(9);   // return 8
+kthLargest.add(4);   // return 8 */
