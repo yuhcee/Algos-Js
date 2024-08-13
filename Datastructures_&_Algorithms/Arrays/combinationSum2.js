f0f0a2e2c609c88cbeea714bbaf3316043fece90
yuhcee <egbouchenna001@gmail.com>
2024-08-13 22:36:51 +0100

    :white_check_mark: (combinationSum2) add test case 1

--- Datastructures_&_Algorithms/Arrays/combinationSum2.js
+++ Datastructures_&_Algorithms/Arrays/combinationSum2.js
@@ -42,3 +42,13 @@ const combinationSum2 = function (candidates, target) {
     backtrack(0, target, []);
     return results;
 };
+
+const candidates = [10, 1, 2, 7, 6, 1, 5],
+    target = 8;
+/* Output: [
+    [1, 1, 6],
+    [1, 2, 5],
+    [1, 7],
+    [2, 6],
+]; */
+console.log(combinationSum2(candidates, target));
