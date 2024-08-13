6f067dd7e6b5118d42337a477e6790b764616d88
yuhcee <egbouchenna001@gmail.com>
2024-08-13 22:35:00 +0100

    :books: (combinationSum2) add task description

---
+++ Datastructures_&_Algorithms/Arrays/combinationSum2.js
@@ -0,0 +1,16 @@
+/**
+ * **40. Combination Sum II**
+ *
+ * Given a collection of candidate numbers (`candidates`) and a target
+ * number (`target`), find all unique combinations in `candidates` where the
+ * candidate numbers sum to `target`.
+ *
+ * Each number in `candidates` may only be used **once** in the combination.
+ *
+ * **Note**: The solution set must not contain duplicate combinations.
+ *
+ * @param {number[]} candidates
+ * @param {number} target
+ * @return {number[][]}
+ */
+const combinationSum2 = function (candidates, target) {};
