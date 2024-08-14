f9299a140e0927374de0a62d3515419309a2c2ea
yuhcee <egbouchenna001@gmail.com>
2024-08-14 08:51:51 +0100

    :books: (smallestDistancePair) add task description

---
+++ Datastructures_&_Algorithms/Arrays/smallestDistancePair.js
@@ -0,0 +1,15 @@
+/**
+ * **719. Find K-th Smallest Pair Distance**
+ *
+ * The **distance of a pair** of integers `a` and `b` is defined as the
+ * absolute difference between `a` and `b`.
+ *
+ * Given an integer array `nums` and an integer `k`, return the `kth`
+ * smallest **distance among all the pairs** `nums[i]` and `nums[j]` where
+ * `0 <= i < j < nums.length`.
+ *
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+const smallestDistancePair = function (nums, k) {};
