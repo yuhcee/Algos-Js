73fddd64b1faf298bc7b81169ca133bafd764bf0
yuhcee <egbouchenna001@gmail.com>
2024-08-16 21:49:36 +0100

    :white_check_mark: (maxDistance) add test case 1

--- Datastructures_&_Algorithms/Arrays/maxDistance1.js
+++ Datastructures_&_Algorithms/Arrays/maxDistance1.js
@@ -39,3 +39,12 @@ const maxDistance = function (arrays) {
 
     return maxDistance;
 };
+
+const arrays = [
+    [1, 2, 3],
+    [4, 5],
+    [1, 2, 3],
+];
+// Output: 4
+// Explanation: One way to reach the maximum distance 4 is to pick 1 in the first or third array and pick 5 in the second array.
+console.log(maxDistance(arrays));
