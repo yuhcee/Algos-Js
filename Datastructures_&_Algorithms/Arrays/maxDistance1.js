f40add7800cc17e3df0037566b3f113f99d69515
yuhcee <egbouchenna001@gmail.com>
2024-08-16 21:37:34 +0100

    :books: (maxDistance) add task description

---
+++ Datastructures_&_Algorithms/Arrays/maxDistance1.js
@@ -0,0 +1,16 @@
+/**
+ * **624. Maximum Distance in Arrays**
+ *
+ * You are given `m` arrays, where each array is sorted in **ascending
+ * order**.
+ *
+ * You can pick up two integers from two different arrays (each array picks
+ * one) and calculate the distance. We define the distance between two
+ * integers `a` and `b` to be their absolute difference `|a - b|`.
+ *
+ * Return *the maximum distance*.
+ * 
+ * @param {number[][]} arrays
+ * @return {number}
+ */
+const maxDistance = function (arrays) {};
