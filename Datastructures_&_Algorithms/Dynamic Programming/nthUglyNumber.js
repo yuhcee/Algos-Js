349f172de092b29b5cef08a5923bc9364620803a
yuhcee <egbouchenna001@gmail.com>
2024-08-18 22:37:27 +0100

    :books: (nthUglyNumber) add task description

---
+++ Datastructures_&_Algorithms/Dynamic Programming/nthUglyNumber.js
@@ -0,0 +1,12 @@
+/**
+ * **264. Ugly Number II**
+ *
+ * An **ugly number** is a positive integer whose prime factors are limited
+ * to `2`, `3`, and `5`.
+ *
+ * Given an integer `n`, return the `nth` ugly number.
+ *
+ * @param {number} n
+ * @return {number}
+ */
+const nthUglyNumber = function (n) {};
