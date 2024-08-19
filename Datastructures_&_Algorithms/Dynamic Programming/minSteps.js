de3b3d1eb1076c44f52ea4868f310139fba118ed
yuhcee <egbouchenna001@gmail.com>
2024-08-19 23:19:11 +0100

    :white_check_mark: (minSteps) add test case 1

--- Datastructures_&_Algorithms/Dynamic Programming/minSteps.js
+++ Datastructures_&_Algorithms/Dynamic Programming/minSteps.js
@@ -32,3 +32,11 @@ const minSteps = function (n) {
 
     return steps;
 };
+
+const n = 3;
+// Output: 3
+/* Explanation: Initially, we have one character 'A'.
+In step 1, we use Copy All operation.
+In step 2, we use Paste operation to get 'AA'.
+In step 3, we use Paste operation to get 'AAA'. */
+console.log(minSteps(n));
