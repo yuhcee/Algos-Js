0a60184bc218653b98d231f1227bf47d960f9156
yuhcee <egbouchenna001@gmail.com>
2024-08-21 22:17:34 +0100

    :books: (strangePrinter) add task description

--- Datastructures_&_Algorithms/Dynamic Programming/strangePrinter.js
+++ Datastructures_&_Algorithms/Dynamic Programming/strangePrinter.js
@@ -3,11 +3,14 @@
  *
  * There is a strange printer with the following two special properties:
  *
- * - The printer can only print a sequence of **the same character** each time.
- * - At each turn, the printer can print new characters starting from and ending at any place and
+ * - The printer can only print a sequence of **the same character**
+ * each time.
+ * - At each turn, the printer can print new characters starting from
+ * and ending at any place and
  * will cover the original existing characters.
  *
- * Given a string `s`, return *the minimum number of turns the printer needed to print it*.
+ * Given a string `s`, return *the minimum number of turns the printer
+ * needed to print it*.
  *
  * **Constraints:**
  *
