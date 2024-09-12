fa3cd4f7c6248c6dd791be429951a63528f710dc
yuhcee <egbouchenna001@gmail.com>
2024-09-12 22:49:56 +0100

    :books: (countConsistentStrings) add task description

---
+++ Datastructures_&_Algorithms/Strings/countConsistentStrings.js
@@ -0,0 +1,15 @@
+/**
+ * **1684. Count the Number of Consistent Strings**
+ *
+ * You are given a string `allowed` consisting of **distinct**
+ * characters and an array of strings `words`. A string is
+ * **consistent** if all characters in the string appear in the string
+ * `allowed`.
+ *
+ * Return *the number of consistent strings in the array `words`*.
+ *
+ * @param {string} allowed
+ * @param {string[]} words
+ * @return {number}
+ */
+const countConsistentStrings = function (allowed, words) {};
