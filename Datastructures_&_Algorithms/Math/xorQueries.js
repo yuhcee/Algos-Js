85f9ec6dcee7d5b3143c356b3e9c44e4d5f8857d
yuhcee <egbouchenna001@gmail.com>
2024-09-13 21:35:40 +0100

    :books: (xorQueries) add task description

---
+++ Datastructures_&_Algorithms/Math/xorQueries.js
@@ -0,0 +1,18 @@
+/**
+ * **1310. XOR Queries of a Subarray**
+ *
+ * You are given an array `arr` of positive integers. You are also given
+ * the array `queries` where `queries[i] = [lefti, righti]`.
+ *
+ * For each query `i` compute the **XOR** of elements from `lefti` to
+ * `righti` (that is, `arr[lefti] XOR arr[lefti + 1] XOR ... XOR arr
+ * [righti]` ).
+ *
+ * Return an array `answer` where `answer[i]` is the answer to the `ith`
+ * query.
+ *
+ * @param {number[]} arr
+ * @param {number[][]} queries
+ * @return {number[]}
+ */
+const xorQueries = function (arr, queries) {};
