c994363abab0669720141636b8c77128d49b5d61
yuhcee <egbouchenna001@gmail.com>
2024-09-15 22:36:29 +0100

    :books: (findTheLongestSubstring) add task description

---
+++ Datastructures_&_Algorithms/HashMaps/findTheLongestSubstring.js
@@ -0,0 +1,11 @@
+/**
+ * **1371. Find the Longest Substring Containing Vowels in Even Counts**
+ *
+ * Given the string `s`, return the size of the longest substring
+ * containing each vowel an even number of times. That is, 'a', 'e', 'i',
+ * 'o', and 'u' must appear an even number of times.
+ *
+ * @param {string} s
+ * @return {number}
+ */
+const findTheLongestSubstring = function (s) {};
