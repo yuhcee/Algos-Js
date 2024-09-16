c995d31e9ab1923399764273719fa3400270cb51
yuhcee <egbouchenna001@gmail.com>
2024-09-16 19:23:34 +0100

    :books: (findMinDifference) add task description

---
+++ Datastructures_&_Algorithms/Math/findMinDifference.js
@@ -0,0 +1,11 @@
+/**
+ * **539. Minimum Time Difference**
+ *
+ * Given a list of 24-hour clock time points in **"HH:MM"** format, return
+ * *the minimum **minutes** difference between any two time-points in the
+ * list*.
+ *
+ * @param {string[]} timePoints
+ * @return {number}
+ */
+const findMinDifference = function (timePoints) {};
