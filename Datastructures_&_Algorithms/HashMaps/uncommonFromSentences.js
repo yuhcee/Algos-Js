0d82b4a6420575dcdd28331923bf5bfa53e9a811
yuhcee <egbouchenna001@gmail.com>
2024-09-17 11:48:23 +0100

    :books: (uncommonFromSentences) add task description

---
+++ Datastructures_&_Algorithms/HashMaps/uncommonFromSentences.js
@@ -0,0 +1,17 @@
+/**
+ * **884. Uncommon Words from Two Sentences**
+ *
+ * A **sentence** is a string of single-space separated words where each
+ * word consists only of lowercase letters.
+ *
+ * A word is **uncommon** if it appears exactly once in one of the
+ * sentences, and **does not appear** in the other sentence.
+ *
+ * Given two **sentences** `s1` and `s2`, return a list of all the
+ * **uncommon words**. You may return the answer in **any order**.
+ *
+ * @param {string} s1
+ * @param {string} s2
+ * @return {string[]}
+ */
+const uncommonFromSentences = function (s1, s2) {};
