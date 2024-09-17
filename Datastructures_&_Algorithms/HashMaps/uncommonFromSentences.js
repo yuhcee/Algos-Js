99bcfa105c884d9af1edd968888405b1d3e095b8
yuhcee <egbouchenna001@gmail.com>
2024-09-17 11:51:09 +0100

    :heavy_plus_sign:(uncommonFromSentences) add constraints

--- Datastructures_&_Algorithms/HashMaps/uncommonFromSentences.js
+++ Datastructures_&_Algorithms/HashMaps/uncommonFromSentences.js
@@ -10,6 +10,13 @@
  * Given two **sentences** `s1` and `s2`, return a list of all the
  * **uncommon words**. You may return the answer in **any order**.
  *
+ * **Constraints:**
+ *
+ * - `1 <= s1.length, s2.length <= 200`
+ * - `s1` and `s2` consist of lowercase English letters and spaces.
+ * - `s1` and `s2` do not have leading or trailing spaces.
+ * - All the words in `s1` and `s2` are separated by a single space.
+ *
  * @param {string} s1
  * @param {string} s2
  * @return {string[]}
