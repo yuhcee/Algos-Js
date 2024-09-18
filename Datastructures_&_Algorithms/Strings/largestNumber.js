0f6270a2ba131d38847e15365ed76552a06db6c1
yuhcee <egbouchenna001@gmail.com>
2024-09-18 08:58:09 +0100

    :books: (largestNumber) add task description

---
+++ Datastructures_&_Algorithms/Strings/largestNumber.js
@@ -0,0 +1,13 @@
+/**
+ * **179. Largest Number**
+ *
+ * Given a list of non-negative integers `nums`, arrange them such that
+ * they form the largest number and return it.
+ *
+ * Since the result may be very large, so you need to return a string
+ * instead of an integer.
+ *
+ * @param {number[]} nums
+ * @return {string}
+ */
+const largestNumber = function (nums) {};
