a97bc3e7c636fb6199e219dfe2b40fb5cd77d5df
yuhcee <egbouchenna001@gmail.com>
2024-09-19 19:39:27 +0100

    :sparkles: (diffWaysToCompute) add task solution

--- Datastructures_&_Algorithms/Recursion/diffWaysToCompute.js
+++ Datastructures_&_Algorithms/Recursion/diffWaysToCompute.js
@@ -23,4 +23,46 @@
  * @param {string} expression
  * @return {number[]}
  */
-const diffWaysToCompute = function (expression) {};
+const diffWaysToCompute = function (expression) {
+    const memo = {};
+
+    const compute = (exp) => {
+        if (memo[exp] !== undefined) {
+            return memo[exp];
+        }
+
+        let res = [];
+        // Iterate through the expression to find operators
+        for (let i = 0; i < exp.length; i++) {
+            const char = exp[i];
+            if (char === '+' || char === '-' || char === '*') {
+                // Split the expression at the current operator
+                const left = compute(exp.slice(0, i));
+                const right = compute(exp.slice(i + 1));
+
+                // Combine results from left and right sub-expressions
+                for (let l of left) {
+                    for (let r of right) {
+                        if (char === '+') {
+                            res.push(l + r);
+                        } else if (char === '-') {
+                            res.push(l - r);
+                        } else if (char === '*') {
+                            res.push(l * r);
+                        }
+                    }
+                }
+            }
+        }
+
+        // Base case: if there are no operators, the expression is a number
+        if (res.length === 0) {
+            res.push(parseInt(exp));
+        }
+
+        memo[exp] = res;
+        return res;
+    };
+
+    return compute(expression);
+};
