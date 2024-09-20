a7fcfcb2353aa2377f7c0ea1b43ffef6e956f663
yuhcee <egbouchenna001@gmail.com>
2024-09-20 23:36:50 +0100

    :books: (shortestPalindrome) add task description

---
+++ Datastructures_&_Algorithms/Strings/shortestPalindrome.js
@@ -0,0 +1,13 @@
+/**
+ * **214. Shortest Palindrome**
+ *
+ * You are given a string `s`. You can convert `s` to a palindrome
+ * by adding characters in front of it.
+ *
+ * Return *the shortest palindrome you can find by performing this
+ * transformation*.
+ *
+ * @param {string} s
+ * @return {string}
+ */
+const shortestPalindrome = function (s) {};
