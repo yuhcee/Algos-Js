861dd4ce5d77c1a3620f6fc14a3674eb0f1a11cb
yuhcee <egbouchenna001@gmail.com>
2024-09-21 23:36:46 +0100

    :books: (lexicalOrder) add task description

---
+++ Datastructures_&_Algorithms/Dynamic Programming/lexicalOrder.js
@@ -0,0 +1,13 @@
+/**
+ * **386. Lexicographical Numbers**
+ *
+ * Given an integer `n`, return all the numbers in the range `[1, n]`
+ * sorted in lexicographical order.
+ *
+ * You must write an algorithm that runs in `O(n)` time and uses `O(1)`
+ * extra space.
+ *
+ * @param {number} n
+ * @return {number[]}
+ */
+const lexicalOrder = function (n) {};
