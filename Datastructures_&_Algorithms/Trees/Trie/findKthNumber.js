ab92c922952c93abfdfb85d23a9667ee1b95b0fd
yuhcee <egbouchenna001@gmail.com>
2024-09-22 17:09:46 +0100

    :books: (findKthNumber) add task description

---
+++ Datastructures_&_Algorithms/Trees/Trie/findKthNumber.js
@@ -0,0 +1,11 @@
+/**
+ * **440. K-th Smallest in Lexicographical Order**
+ *
+ * Given two integers `n` and `k`, return *the `kth` lexicographically
+ * smallest integer in the range `[1, n]`*.
+ *
+ * @param {number} n
+ * @param {number} k
+ * @return {number}
+ */
+const findKthNumber = function (n, k) {};
