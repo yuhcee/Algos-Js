2e481cd15912cf6a6f396fb9ab7ef59c81aa7574
yuhcee <egbouchenna001@gmail.com>
2024-09-25 23:29:22 +0100

    :books: (sumPrefixScores) add task description

---
+++ Datastructures_&_Algorithms/Trees/Trie/sumPrefixScores.js
@@ -0,0 +1,21 @@
+/**
+ * **416. Sum of Prefix Scores of Strings**
+ *
+ * You are given an array `words` of size `n` consisting of **non-empty**
+ * strings.
+ *
+ * We define the **score** of a string `term` as the **number** of strings
+ * `words[i]` such that `term` is a **prefix** of `words[i]`.
+ *
+ * For example, if `words = ["a", "ab", "abc", "cab"]`, then the score of
+ * `"ab"` is `2`, since `"ab"` is a prefix of both `"ab"` and `"abc"`.
+ *
+ * Return *an array `answer` of size `n` where `answer[i]` is the **sum**
+ * of scores of every **non-empty** prefix of `words[i]`*.
+ *
+ * **Note** that a string is considered as a prefix of itself.
+ *
+ * @param {string[]} words
+ * @return {number[]}
+ */
+const sumPrefixScores = function (words) {};
