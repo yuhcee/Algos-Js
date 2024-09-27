cbf1c11cc34a92db7f2d01c5656aa089241cd0a6
yuhcee <egbouchenna001@gmail.com>
2024-09-27 23:12:58 +0100

    :heavy_plus_sign:(MyCalendarTwo) Trie class

--- Datastructures_&_Algorithms/Arrays/MyCalendarTwo.js
+++ Datastructures_&_Algorithms/Arrays/MyCalendarTwo.js
@@ -1,26 +1,32 @@
-/** 
+/**
  * **731. My Calendar II**
- * 
- * You are implementing a program to use as your calendar. We can add a 
+ *
+ * You are implementing a program to use as your calendar. We can add a
  * new event if adding the event will not cause a **triple booking**.
- * 
- * A **triple booking** happens when three events have some non-empty 
+ *
+ * A **triple booking** happens when three events have some non-empty
  * intersection (i.e., some moment is common to all the three events.).
- * 
- * The event can be represented as a pair of integers `start` and `end` 
- * that represents a booking on the half-open interval `[start, end]`, the 
+ *
+ * The event can be represented as a pair of integers `start` and `end`
+ * that represents a booking on the half-open interval `[start, end]`, the
  * range of real numbers `x` such that `start <= x < end`.
- * 
+ *
  * Implement the `MyCalendarTwo` class:
- * 
+ *
  * - `MyCalendarTwo()` Initializes the calendar object.
- * - `boolean book(int start, int end)` Returns `true` if the event can be 
- * added to the calendar successfully without causing a **triple 
- * booking**. Otherwise, return `false` and do not add the event to the 
+ * - `boolean book(int start, int end)` Returns `true` if the event can be
+ * added to the calendar successfully without causing a **triple
+ * booking**. Otherwise, return `false` and do not add the event to the
  * calendar.
- * 
- * @param {number} start 
+ *
+ * **Constraints:**
+ *
+ * - `0 <= start < end <= 109`
+ * - At most `1000` calls will be made to `book`.
+ *
+ * @param {number} start
  * @param {number} end
  * @return {boolean}
  */
-MyCalendarTwo.prototype.book = function(start, end) {}
+
+MyCalendarTwo.prototype.book = function (start, end) {};
