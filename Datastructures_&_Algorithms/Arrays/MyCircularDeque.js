9d9c4bc27d3df086e5c2aa00c3de4094c99f67b0
yuhcee <egbouchenna001@gmail.com>
2024-09-28 23:59:12 +0100

    :sparkles: (MyCircularDeque) add task solution

--- Datastructures_&_Algorithms/Arrays/MyCircularDeque.js
+++ Datastructures_&_Algorithms/Arrays/MyCircularDeque.js
@@ -40,3 +40,100 @@ const MyCircularDeque = function (k) {
     this.front = 0; // Pointer to the front of the deque
     this.rear = k - 1; // Pointer to the rear of the deque
 };
+
+/**
+ * @param {number} value
+ * @return {boolean}
+ */
+MyCircularDeque.prototype.insertFront = function (value) {
+    if (this.isFull()) return false;
+
+    // Move front pointer backward in circular fashion
+    this.front = (this.front - 1 + this.k) % this.k;
+    this.data[this.front] = value;
+    this.size++;
+    return true;
+};
+
+/**
+ * @param {number} value
+ * @return {boolean}
+ */
+MyCircularDeque.prototype.insertLast = function (value) {
+    if (this.isFull()) return false;
+
+    // Move rear pointer forward in circular fashion
+    this.rear = (this.rear + 1) % this.k;
+    this.data[this.rear] = value;
+    this.size++;
+    return true;
+};
+
+/**
+ * @return {boolean}
+ */
+MyCircularDeque.prototype.deleteFront = function () {
+    if (this.isEmpty()) return false;
+
+    // Move front pointer forward in circular fashion
+    this.front = (this.front + 1) % this.k;
+    this.size--;
+    return true;
+};
+
+/**
+ * @return {boolean}
+ */
+MyCircularDeque.prototype.deleteLast = function () {
+    if (this.isEmpty()) return false;
+
+    // Move rear pointer backward in circular fashion
+    this.rear = (this.rear - 1 + this.k) % this.k;
+    this.size--;
+    return true;
+};
+
+/**
+ * @return {number}
+ */
+MyCircularDeque.prototype.getFront = function () {
+    if (this.isEmpty()) return -1;
+
+    return this.data[this.front];
+};
+
+/**
+ * @return {number}
+ */
+MyCircularDeque.prototype.getRear = function () {
+    if (this.isEmpty()) return -1;
+
+    return this.data[this.rear];
+};
+
+/**
+ * @return {boolean}
+ */
+MyCircularDeque.prototype.isEmpty = function () {
+    return this.size === 0;
+};
+
+/**
+ * @return {boolean}
+ */
+MyCircularDeque.prototype.isFull = function () {
+    return this.size === this.k;
+};
+
+/**
+ * Your MyCircularDeque object will be instantiated and called as such:
+ * var obj = new MyCircularDeque(k)
+ * var param_1 = obj.insertFront(value)
+ * var param_2 = obj.insertLast(value)
+ * var param_3 = obj.deleteFront()
+ * var param_4 = obj.deleteLast()
+ * var param_5 = obj.getFront()
+ * var param_6 = obj.getRear()
+ * var param_7 = obj.isEmpty()
+ * var param_8 = obj.isFull()
+ */
