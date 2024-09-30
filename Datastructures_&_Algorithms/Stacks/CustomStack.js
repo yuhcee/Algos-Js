00ad413ea43eb37fa652a5cc5a9238f69093717c
yuhcee <egbouchenna001@gmail.com>
2024-09-30 22:44:45 +0100

    :books: (CustomStack) add task description

---
+++ Datastructures_&_Algorithms/Stacks/CustomStack.js
@@ -0,0 +1,20 @@
+/**
+ * **1381. Design a Stack With Increment Operation**
+ *
+ * Design a stack that supports increment operations on its elements.
+ *
+ * Implement the `CustomStack` class:
+ *
+ * - `CustomStack(int maxSize)` Initializes the object with `maxSize`
+ * which is the maximum number of elements in the stack.
+ * - `void push(int x)` Adds x to the top of the stack if the stack has
+ * not reached the `maxSize`.
+ * - `int pop()` Pops and returns the top of the stack or `-1` if the stack
+ * is empty.
+ * - `void inc(int k, int val)` Increments the bottom `k` elements of the
+ * stack by `val`. If there are less than `k` elements in the stack, increment
+ * all the elements in the stack.
+ *
+ * @param {number} maxSize
+ */
+const CustomStack = function (maxSize) {};
