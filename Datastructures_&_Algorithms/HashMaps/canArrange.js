2169cfd824f1c87776bec65d066e68c77b008e85
yuhcee <egbouchenna001@gmail.com>
2024-10-01 23:50:49 +0100

    :white_check_mark: (canArrange) add test case 1

--- Datastructures_&_Algorithms/HashMaps/canArrange.js
+++ Datastructures_&_Algorithms/HashMaps/canArrange.js
@@ -42,3 +42,9 @@ const canArrange = function (arr, k) {
 
     return true;
 };
+
+const arr = [1, 2, 3, 4, 5, 10, 6, 7, 8, 9],
+    k = 5;
+// Output: true
+// Explanation: Pairs are (1,9),(2,8),(3,7),(4,6) and (5,10).
+console.log(canArrange(arr, k));
