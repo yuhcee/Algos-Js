53c76b057f4e85ee06c2e60dd9f8b28f96987ae5
yuhcee <egbouchenna001@gmail.com>
2024-10-02 23:28:57 +0100

    :white_check_mark: (arrayRankTransform) add test case 1

--- Datastructures_&_Algorithms/HashMaps/arrayRankTransform.js
+++ Datastructures_&_Algorithms/HashMaps/arrayRankTransform.js
@@ -31,3 +31,8 @@ const arrayRankTransform = function (arr) {
 
     return arr.map((value) => rankMap.get(value));
 };
+
+const arr = [40, 10, 20, 30];
+// Output: [4,1,2,3]
+// Explanation: 40 is the largest element. 10 is the smallest. 20 is the second smallest. 30 is the third smallest.
+console.log(arrayRankTransform(arr));
