b182fe0d28978b94ec7a463da155cb767e059fbf
yuhcee <egbouchenna001@gmail.com>
2024-10-02 22:50:43 +0100

    :books: (arrayRankTransform) add task description

---
+++ Datastructures_&_Algorithms/HashMaps/arrayRankTransform.js
@@ -0,0 +1,16 @@
+/**
+ * **1331. Rank Transform of an Array**
+ *
+ * Given an array of integers `arr`, replace each element with its rank.
+ *
+ * The rank represents how large the element is. The rank has the following rules:
+ *
+ * - Rank is an integer starting from 1.
+ * - The larger the element, the larger the rank. If two elements are equal, their rank must be
+ * the same.
+ * - Rank should be as small as possible.
+ *
+ * @param {number[]} arr
+ * @return {number[]}
+ */
+const arrayRankTransform = function (arr) {};
