ce76de56764a8be1686460a34c77aa431e4eb51d
yuhcee <egbouchenna001@gmail.com>
2024-10-03 23:48:10 +0100

    :white_check_mark: (minSubarray) add test case 2

--- Datastructures_&_Algorithms/HashMaps/minSubarray.js
+++ Datastructures_&_Algorithms/HashMaps/minSubarray.js
@@ -57,3 +57,10 @@ const nums = [3, 1, 4, 2],
 // Output: 1
 // Explanation: The sum of the elements in nums is 10, which is not divisible by 6. We can remove the subarray [4], and the sum of the remaining elements is 6, which is divisible by 6.
 console.log(minSubarray(nums, p));
+
+const nums1 = [6, 3, 5, 2],
+    p1 = 9;
+// Output: 2
+// Explanation: We cannot remove a single element to get a sum divisible by 9. The best way is to remove the subarray [5,2], leaving us with [6,3] with sum 9.
+console.log(minSubarray(nums1, p1));
+
