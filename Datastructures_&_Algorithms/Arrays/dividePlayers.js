7e510c80a2e6230f8b464bf230b46dc6ccf4d163
yuhcee <egbouchenna001@gmail.com>
2024-10-04 23:50:55 +0100

    :heavy_plus_sign:(dividePlayers) add constraints

--- Datastructures_&_Algorithms/Arrays/dividePlayers.js
+++ Datastructures_&_Algorithms/Arrays/dividePlayers.js
@@ -1,18 +1,24 @@
 /**
  * **2491. Divide Players Into Teams of Equal Skill**
- * 
- * You are given a positive integer array `skill` of **even** length `n` 
- * where `skill[i]` denotes the skill of the `ith` player. Divide the 
- * players into `n / 2` teams of size `2` such that the total skill of 
+ *
+ * You are given a positive integer array `skill` of **even** length `n`
+ * where `skill[i]` denotes the skill of the `ith` player. Divide the
+ * players into `n / 2` teams of size `2` such that the total skill of
  * each team is **equal**.
- * 
- * The **chemistry** of a team is equal to the **product** of the skills 
+ *
+ * The **chemistry** of a team is equal to the **product** of the skills
  * of the players on that team.
- * 
- * Return *the sum of the **chemistry** of all the teams, or return `-1` if 
- * here is no way to divide the players into teams such that the total skill 
+ *
+ * Return *the sum of the **chemistry** of all the teams, or return `-1` if
+ * here is no way to divide the players into teams such that the total skill
  * of each team is equal*.
- * 
+ *
+ * **Constraints:**
+ *
+ * - `2 <= skill.length <= 105`
+ * - `skill.length is even.`
+ * - `1 <= skill[i] <= 1000`
+ *
  * @param {number[]} skill
  * @return {number}
  */
