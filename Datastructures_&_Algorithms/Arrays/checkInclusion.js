552025a4dd9625546c350d7373cdf57354e53daf
yuhcee <egbouchenna001@gmail.com>
2024-10-05 20:27:22 +0100

    :books: (checkInclusion) add task description

--- Datastructures_&_Algorithms/Arrays/checkInclusion.js
+++ Datastructures_&_Algorithms/Arrays/checkInclusion.js
@@ -1,10 +1,12 @@
 /**
  * **567. Permutation in String**
  *
- * Given two strings `s1` and `s2`, return *`true` if `s2` contains a permutation of s1, or `false`
+ * Given two strings `s1` and `s2`, return *`true` if `s2` contains a 
+ * permutation of s1, or `false`
  * otherwise*.
  *
- * In other words, return `true` if one of `s1`'s permutations is the substring of `s2`.
+ * In other words, return `true` if one of `s1`'s permutations is the 
+ * substring of `s2`.
  *
  * **Constraints:**
  *
