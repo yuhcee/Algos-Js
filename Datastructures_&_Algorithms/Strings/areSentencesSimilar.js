6203db6b3c046bde3358dc8388a39e56b2604924
yuhcee <egbouchenna001@gmail.com>
2024-10-06 21:16:03 +0100

    :sparkles: (areSentencesSimilar) add task solution

--- Datastructures_&_Algorithms/Strings/areSentencesSimilar.js
+++ Datastructures_&_Algorithms/Strings/areSentencesSimilar.js
@@ -38,4 +38,23 @@
  * @param {string} sentence2
  * @return {boolean}
  */
-const areSentencesSimilar = function (sentence1, sentence2) {};
+const areSentencesSimilar = function (sentence1, sentence2) {
+    // Split sentences into arrays of words
+    let words1 = sentence1.split(' ');
+    let words2 = sentence2.split(' ');
+
+    let i = 0,
+        j = 0;
+    // Match prefixes
+    while (i < words1.length && i < words2.length && words1[i] === words2[i]) {
+        i++;
+    }
+
+    // Match suffixes
+    while (j < words1.length - i && j < words2.length - i && words1[words1.length - 1 - j] === words2[words2.length - 1 - j]) {
+        j++;
+    }
+
+    // If all words from one sentence are matched, return true
+    return i + j === Math.min(words1.length, words2.length);
+};
