7bd928a3511803a6e8c0db2ae3806a33a3a843ee
yuhcee <egbouchenna001@gmail.com>
2024-10-07 22:37:38 +0100

    :white_check_mark: (minLength) add test 1

--- Datastructures_&_Algorithms/Strings/minLength.js
+++ Datastructures_&_Algorithms/Strings/minLength.js
@@ -38,3 +38,13 @@ const minLength = function (s) {
     // The remaining length of the stack is the minimum length of the string
     return stack.length;
 };
+
+const s = 'ABFCACDB';
+// Output: 2
+/* Explanation: We can do the following operations:
+- Remove the substring "ABFCACDB", so s = "FCACDB".
+- Remove the substring "FCACDB", so s = "FCAB".
+- Remove the substring "FCAB", so s = "FC".
+So the resulting length of the string is 2.
+It can be shown that it is the minimum length that we can obtain. */
+console.log(minLength(s));
