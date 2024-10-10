60817073bd84a7866a117f7f04d37612e6d00377
yuhcee <egbouchenna001@gmail.com>
2024-10-10 23:50:33 +0100

    :heavy_plus_sign: (maxWidthRamp) add  constraints

--- Datastructures_&_Algorithms/Stacks/maxWidthRamp.js
+++ Datastructures_&_Algorithms/Stacks/maxWidthRamp.js
@@ -7,6 +7,11 @@
  * Given an integer array `nums`, return *the maximum width of a **ramp** in
  * `nums`. If there is no **ramp** in `nums`, return `0`*.
  *
+ * **Constraints:**
+ *
+ * - `2 <= nums.length <= 5 * 104`
+ * - `0 <= nums[i] <= 5 * 104`
+ *
  * @param {number[]} nums
  * @return {number}
  */
