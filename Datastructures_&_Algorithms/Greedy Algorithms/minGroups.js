b3827a329fc23fa11fddeecf6c8708d2689c24bd
yuhcee <egbouchenna001@gmail.com>
2024-10-12 22:36:02 +0100

    :books: (minGroups) add task description

---
+++ Datastructures_&_Algorithms/Greedy Algorithms/minGroups.js
@@ -0,0 +1,19 @@
+/**
+ * **2406. Divide Intervals Into Minimum Number of Groups**
+ *
+ * You are given a 2D integer array `intervals` where `intervals[i] = [lefti,
+ * righti]` represents the **inclusive** interval `[lefti, righti]`.
+ *
+ * You have to divide the intervals into one or more **groups** such that each
+ * interval is in **exactly** one group, and no two intervals that are in the
+ * same group **intersect** each other.
+ *
+ * Return *the **minimum** number of groups you need to make*.
+ *
+ * Two intervals **intersect** if there is at least one common number between
+ * them. For example, the intervals `[1, 5]` and `[5, 8]` intersect.
+ *
+ * @param {number[][]} intervals
+ * @return {number}
+ */
+const minGroups = function (intervals) {};
