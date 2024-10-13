780e3c1df6ecd145b36bf47f78745aeb2ec2e9e1
yuhcee <egbouchenna001@gmail.com>
2024-10-13 23:11:56 +0100

    :white_check_mark: (smallestRange) add test 1

--- Datastructures_&_Algorithms/Greedy Algorithms/smallestRange.js
+++ Datastructures_&_Algorithms/Greedy Algorithms/smallestRange.js
@@ -51,3 +51,15 @@ const smallestRange = function (nums) {
 
     return [rangeStart, rangeEnd];
 };
+
+const nums = [
+    [4, 10, 15, 24, 26],
+    [0, 9, 12, 20],
+    [5, 18, 22, 30],
+];
+// Output: [20,24]
+/* Explanation: 
+List 1: [4, 10, 15, 24,26], 24 is in range [20,24].
+List 2: [0, 9, 12, 20], 20 is in range [20,24].
+List 3: [5, 18, 22, 30], 22 is in range [20,24]. */
+console.log(smallestRange(nums));
