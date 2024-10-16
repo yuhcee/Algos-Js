b645a1c6761a0dccfe7bd6988c1598cad434c7d9
yuhcee <egbouchenna001@gmail.com>
2024-10-16 23:31:33 +0100

    :sparkles: (longestDiverseString) add task solution

--- Datastructures_&_Algorithms/Greedy Algorithms/longestDiverseString.js
+++ Datastructures_&_Algorithms/Greedy Algorithms/longestDiverseString.js
@@ -27,4 +27,45 @@
  * @param {number} c
  * @return {string}
  */
-const longestDiverseString = function (a, b, c) {};
+const longestDiverseString = function (a, b, c) {
+    let result = [];
+
+    // Create an array of pairs [count, character]
+    let maxHeap = [];
+    if (a > 0) maxHeap.push([a, 'a']);
+    if (b > 0) maxHeap.push([b, 'b']);
+    if (c > 0) maxHeap.push([c, 'c']);
+
+    // Use a max-heap like structure by sorting the array
+    maxHeap.sort((x, y) => y[0] - x[0]);
+
+    while (maxHeap.length > 0) {
+        // Pick the character with the highest remaining count
+        maxHeap.sort((x, y) => y[0] - x[0]); // Sort heap by count descending
+        let [count1, char1] = maxHeap[0];
+
+        // Check if we can add this character (without making three consecutive)
+        if (result.length >= 2 && result[result.length - 1] === char1 && result[result.length - 2] === char1) {
+            // If adding char1 would violate the rule, pick the second most frequent character
+            if (maxHeap.length < 2) break; // If there's no second option, we can't add more
+
+            let [count2, char2] = maxHeap[1];
+            result.push(char2);
+            maxHeap[1][0]--;
+
+            if (maxHeap[1][0] === 0) {
+                maxHeap.splice(1, 1); // Remove if exhausted
+            }
+        } else {
+            // Otherwise, we can safely add char1
+            result.push(char1);
+            maxHeap[0][0]--;
+
+            if (maxHeap[0][0] === 0) {
+                maxHeap.shift(); // Remove if exhausted
+            }
+        }
+    }
+
+    return result.join('');
+};
