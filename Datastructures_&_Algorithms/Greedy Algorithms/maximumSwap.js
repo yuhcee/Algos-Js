7f5889e3b5b198747c0ce4c9caa47c5176f70985
yuhcee <egbouchenna001@gmail.com>
2024-10-17 23:02:24 +0100

    :books: (maximumSwap) add task description

---
+++ Datastructures_&_Algorithms/Greedy Algorithms/maximumSwap.js
@@ -0,0 +1,12 @@
+/**
+ * **670. Maximum Swap**
+ *
+ * You are given an integer `num`. You can swap two digits at most once to
+ * get the maximum valued number.
+ *
+ * Return **the maximum valued number you can get**.
+ *
+ * @param {number} num
+ * @return {number}
+ */
+const maximumSwap = function (num) {};
