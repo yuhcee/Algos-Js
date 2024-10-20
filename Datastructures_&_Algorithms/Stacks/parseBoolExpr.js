6a40ddb4e6a06228cfa7ecf02fef8ed1986e0ff1
yuhcee <egbouchenna001@gmail.com>
2024-10-20 19:39:34 +0100

    :books: (parseBoolExpr) add task description

---
+++ Datastructures_&_Algorithms/Stacks/parseBoolExpr.js
@@ -0,0 +1,27 @@
+/**
+ * **1106. Parsing A Boolean Expression**
+ *
+ * A **boolean expression** is an expression that evaluates to either
+ * `true` or `false`. It can be in one of the following shapes:
+ *
+ * - `'t'` that evaluates to `true`.
+ * - `'f'` that evaluates to `false`.
+ * - `'!(subExpr)'` that evaluates to the **logical NOT** of the inner
+ * expression `subExpr`.
+ * - `'&(subExpr1, subExpr2, ..., subExprn)'` that evaluates to the
+ * **logical AND** of the inner expressions `subExpr1, subExpr2, ...,
+ * subExprn` where `n >= 1`.
+ * - `'|(subExpr1, subExpr2, ..., subExprn)'` that evaluates to the
+ * **logical OR** of the inner expressions `subExpr1, subExpr2, ...,
+ * subExprn` where `n >= 1`.
+ *
+ * Given a string `expression` that represents a **boolean expression**,
+ * return *the evaluation of that expression*.
+ *
+ * It is **guaranteed** that the given expression is valid and follows the
+ * given rules.
+ *
+ * @param {string} expression
+ * @return {boolean}
+ */
+const parseBoolExpr = function (expression) {};
