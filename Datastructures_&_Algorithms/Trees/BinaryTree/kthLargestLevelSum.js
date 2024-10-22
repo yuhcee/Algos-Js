e5d6e53db9bf370f33fa36bc171293ee877a06ad
yuhcee <egbouchenna001@gmail.com>
2024-10-22 19:32:40 +0100

    :books: (kthLargestLevelSum) add task description

---
+++ Datastructures_&_Algorithms/Trees/BinaryTree/kthLargestLevelSum.js
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * **2583. Kth Largest Sum in a Binary Tree**
+ *
+ * You are given the `root` of a binary tree and a positive integer
+ * `k`.
+ *
+ * The **level sum** in the tree is the **sum** of the values of the
+ * nodes that are on the same level.
+ *
+ * Return *the `kth` **largest** level sum in the tree (not
+ * necessarily distinct)*. If there are fewer than `k` levels in the
+ * tree, return `-1`.
+ *
+ * **Note** that two nodes are on the same level if they have the same
+ * distance from the root.
+ *
+ * @param {TreeNode} root
+ * @param {number} k
+ * @return {number}
+ */
+const kthLargestLevelSum = function (root, k) {};
