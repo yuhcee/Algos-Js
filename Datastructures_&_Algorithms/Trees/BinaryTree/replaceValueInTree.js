db6731a1828bf525a10f35b89cc170c7870ce224
yuhcee <egbouchenna001@gmail.com>
2024-10-23 07:42:41 +0100

    :books: (replaceValueInTree) add task description

---
+++ Datastructures_&_Algorithms/Trees/BinaryTree/replaceValueInTree.js
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+
+/**
+ * **2641. Cousins in Binary Tree II**
+ *
+ * Given the `root` of a binary tree, replace the value of each node in
+ * the tree with the **sum of all its cousins' values**.
+ *
+ * Two nodes of a binary tree are **cousins** if they have the same depth
+ * with different parents.
+ *
+ * Return *the `root` of the modified tree*.
+ *
+ * **Note** that the depth of a node is the number of edges in the path
+ * from the root node to it.
+ *
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+const replaceValueInTree = function (root) {};
