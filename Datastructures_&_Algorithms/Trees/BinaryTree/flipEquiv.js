15f59e017785a15e6f8706173bdb6e22dd094515
yuhcee <egbouchenna001@gmail.com>
2024-10-24 15:49:35 +0100

    :books: (flipEquiv) add task description

---
+++ Datastructures_&_Algorithms/Trees/BinaryTree/flipEquiv.js
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+
+/**
+ * **951. Flip Equivalent Binary Trees**
+ *
+ * For a binary tree **T**, we can define a **flip operation** as follows:
+ * choose any node, and swap the left and right child subtrees.
+ *
+ * A binary tree **X** is flip equivalent to a binary tree **Y** if and
+ * only if we can make **X** equal to **Y** after some number of flip
+ * operations.
+ *
+ * Given the roots of two binary trees `root1` and `root2`, return `true`
+ * if the two trees are flip equivalent or `false` otherwise.
+ *
+ * @param {TreeNode} root1
+ * @param {TreeNode} root2
+ * @return {boolean}
+ */
+const flipEquiv = function (root1, root2) {};
