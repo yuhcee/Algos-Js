f5cf265a5f839ea297a0264081586d56d4e7b5ea
yuhcee <egbouchenna001@gmail.com>
2024-10-27 21:12:33 +0100

    :books: (countSquares) add task description

---
+++ Datastructures_&_Algorithms/Dynamic Programming/countSquares.js
@@ -0,0 +1,10 @@
+/**
+ * **1277. Count Square Submatrices with All Ones**
+ *
+ * Given a `m * n` matrix of ones and zeros, return how many **square**
+ * submatrices have all ones.
+ *
+ * @param {number[][]} matrix
+ * @return {number}
+ */
+const countSquares = function (matrix) {};
