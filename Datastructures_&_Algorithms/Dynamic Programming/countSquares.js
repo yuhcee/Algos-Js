699fe7a5beac8d63b0697afff160e3381ac34a0d
yuhcee <egbouchenna001@gmail.com>
2024-10-27 21:13:52 +0100

    :sparkles: (countSquares) add task solution

--- Datastructures_&_Algorithms/Dynamic Programming/countSquares.js
+++ Datastructures_&_Algorithms/Dynamic Programming/countSquares.js
@@ -13,4 +13,24 @@
  * @param {number[][]} matrix
  * @return {number}
  */
-const countSquares = function (matrix) {};
+const countSquares = function (matrix) {
+    const m = matrix.length;
+    const n = matrix[0].length;
+    let dp = Array.from({ length: m }, () => Array(n).fill(0));
+    let totalSquares = 0;
+
+    for (let i = 0; i < m; i++) {
+        for (let j = 0; j < n; j++) {
+            if (matrix[i][j] === 1) {
+                if (i === 0 || j === 0) {
+                    dp[i][j] = 1; // First row or column, only 1x1 squares are possible.
+                } else {
+                    dp[i][j] = 1 + Math.min(dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1]);
+                }
+                totalSquares += dp[i][j];
+            }
+        }
+    }
+
+    return totalSquares;
+};
