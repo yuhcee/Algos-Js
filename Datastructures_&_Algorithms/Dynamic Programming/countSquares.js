d505da27c0c2e0bae38c7c3dea4d4a348df9548e
yuhcee <egbouchenna001@gmail.com>
2024-10-27 21:13:21 +0100

    :heavy_plus_sign: (countSquares) add  constraints

--- Datastructures_&_Algorithms/Dynamic Programming/countSquares.js
+++ Datastructures_&_Algorithms/Dynamic Programming/countSquares.js
@@ -4,6 +4,12 @@
  * Given a `m * n` matrix of ones and zeros, return how many **square**
  * submatrices have all ones.
  *
+ * **Constraints:**
+ *
+ * - `1 <= arr.length <= 300`
+ * - `1 <= arr[0].length <= 300`
+ * - `0 <= arr[i][j] <= 1`
+ *
  * @param {number[][]} matrix
  * @return {number}
  */
