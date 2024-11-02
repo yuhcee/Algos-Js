fbfb69f0c078023a48ae4fa8ed04bdd63d2bb33e
yuhcee <egbouchenna001@gmail.com>
2024-11-02 21:53:34 +0100

    :white_check_mark: (isCircularSentence) add test 1

--- Datastructures_&_Algorithms/Strings/isCircularSentence.js
+++ Datastructures_&_Algorithms/Strings/isCircularSentence.js
@@ -50,3 +50,13 @@ const isCircularSentence = function (sentence) {
 
     return true;
 };
+
+const sentence = 'leetcode exercises sound delightful';
+// Output: true
+/* Explanation: The words in sentence are ["leetcode", "exercises", "sound", "delightful"].
+- leetcode's last character is equal to exercises's first character.
+- exercises's last character is equal to sound's first character.
+- sound's last character is equal to delightful's first character.
+- delightful's last character is equal to leetcode's first character.
+The sentence is circular. */
+console.log(isCircularSentence(sentence));
