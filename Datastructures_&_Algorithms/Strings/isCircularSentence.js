f0a57c50024fe6abcf37c96e65a82fb83b706c43
yuhcee <egbouchenna001@gmail.com>
2024-11-02 21:52:47 +0100

    :sparkles: (isCircularSentence) add task solution

--- Datastructures_&_Algorithms/Strings/isCircularSentence.js
+++ Datastructures_&_Algorithms/Strings/isCircularSentence.js
@@ -36,4 +36,17 @@
  * @param {string} sentence
  * @return {boolean}
  */
-const isCircularSentence = function (sentence) {};
+const isCircularSentence = function (sentence) {
+    const words = sentence.split(' ');
+
+    for (let i = 0; i < words.length; i++) {
+        const currentWord = words[i];
+        const nextWord = words[(i + 1) % words.length];
+
+        if (currentWord.at(-1) !== nextWord[0]) {
+            return false;
+        }
+    }
+
+    return true;
+};
