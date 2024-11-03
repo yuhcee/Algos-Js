bb87a98e41a866d004f209092a250984effe3538
yuhcee <egbouchenna001@gmail.com>
2024-11-03 15:26:08 +0100

    :books: (rotateString) add task description

---
+++ Datastructures_&_Algorithms/Strings/rotateString.js
@@ -0,0 +1,17 @@
+/**
+ * **796. Rotate String**
+ *
+ * Given two strings `s` and `goal`, return *`true` if and only if `s` can
+ * become `goal` after some number of **shifts** on `s`*.
+ *
+ * A **shift** on s consists of moving the leftmost character of `s` to
+ * the rightmost position.
+ *
+ * For example, if `s = "abcde"`, then it will be `"bcdea"` after one
+ * shift.
+ *
+ * @param {string} s
+ * @param {string} goal
+ * @return {boolean}
+ */
+const rotateString = function (s, goal) {};
