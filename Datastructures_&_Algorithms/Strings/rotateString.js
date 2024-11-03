937d4e61b9d8884cea770fe1267a09a853d8e00f
yuhcee <egbouchenna001@gmail.com>
2024-11-03 15:27:42 +0100

    :sparkles: (rotateString) add task solution

--- Datastructures_&_Algorithms/Strings/rotateString.js
+++ Datastructures_&_Algorithms/Strings/rotateString.js
@@ -19,4 +19,8 @@
  * @param {string} goal
  * @return {boolean}
  */
-const rotateString = function (s, goal) {};
+const rotateString = function (s, goal) {
+    if (s.length !== goal.length) return false;
+
+    return (s + s).includes(goal);
+};
