9ffb7b7653dfeb170b52b806af9059a1979d16d5
yuhcee <egbouchenna001@gmail.com>
2024-11-04 23:31:06 +0100

    :white_check_mark: (compressedString) add test 2

--- Datastructures_&_Algorithms/Strings/compressedString.js
+++ Datastructures_&_Algorithms/Strings/compressedString.js
@@ -40,6 +40,15 @@ const compressedString = function (word) {
 const word = 'abcde';
 // Output: "1a1b1c1d1e"
 /* Explanation:
-Initially, comp = "". Apply the operation 5 times, choosing "a", "b", "c", "d", and "e" as the prefix in each operation.
-For each prefix, append "1" followed by the character to comp. */
+Initially, `comp` = "". Apply the operation 5 times, choosing `"a"`, `"b"`, `"c"`, `"d"`, and `"e"` as the prefix in each operation.
+For each prefix, append `"1"` followed by the character to `comp`. */
 console.log(compressedString(word));
+
+const word1 = 'aaaaaaaaaaaaaabb';
+// Output: "9a5a2b"
+/* Explanation:
+Initially, `comp` = "". Apply the operation 3 times, choosing `"aaaaaaaaa"`, `"aaaaa"`, and `"bb"` as the prefix in each operation.
+For prefix `"aaaaaaaaa"`, append `"9"` followed by `"a"` to comp.
+For prefix `"aaaaa"`, append `"5"` followed by `"a"` to comp.
+For prefix `"bb"`, append `"2"` followed by `"b"` to comp. */
+console.log(compressedString(word1));
