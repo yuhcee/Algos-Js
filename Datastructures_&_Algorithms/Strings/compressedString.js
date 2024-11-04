29f23dd127ee80c199c855182c781a3799e5c469
yuhcee <egbouchenna001@gmail.com>
2024-11-04 23:28:56 +0100

    :white_check_mark: (compressedString) add test 1

--- Datastructures_&_Algorithms/Strings/compressedString.js
+++ Datastructures_&_Algorithms/Strings/compressedString.js
@@ -36,3 +36,10 @@ const compressedString = function (word) {
 
     return comp;
 };
+
+const word = 'abcde';
+// Output: "1a1b1c1d1e"
+/* Explanation:
+Initially, comp = "". Apply the operation 5 times, choosing "a", "b", "c", "d", and "e" as the prefix in each operation.
+For each prefix, append "1" followed by the character to comp. */
+console.log(compressedString(word));
