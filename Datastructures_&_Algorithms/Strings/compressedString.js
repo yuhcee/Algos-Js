0ed564755a86fd0a9a4d516f20e0786db641e75e
yuhcee <egbouchenna001@gmail.com>
2024-11-04 23:06:45 +0100

    :books: (compressedString) add task description

---
+++ Datastructures_&_Algorithms/Strings/compressedString.js
@@ -0,0 +1,17 @@
+/**
+ * **3163. String Compression III**
+ *
+ * Given a string `word`, compress it using the following algorithm:
+ *
+ * - Begin with an empty string `comp`. While `` is not empty, use the
+ * following operation:
+ *      - Remove a maximum length prefix of `` made of a single character
+ * `c` repeating at **most** 9 times.
+ *      - Append the length of the prefix followed by c to `comp`.
+ *
+ * Return the string `comp`.
+ *
+ * @param {string} word
+ * @return {string}
+ */
+const compressedString = function (word) {};
