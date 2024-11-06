92cf006265498e728d0ab5e2b1ea2cafa069c01c
yuhcee <egbouchenna001@gmail.com>
2024-11-06 15:12:22 +0100

    :books: (canSortArray) add task description

---
+++ Datastructures_&_Algorithms/Arrays/canSortArray.js
@@ -0,0 +1,19 @@
+/**
+ * **3011. Find if Array Can Be Sorted**
+ *
+ * You are given a **0-indexed** array of
+ * **positive** integers `nums`.
+ *
+ * In one **operation**, you can swap any two
+ * **adjacent** elements if they have the **same**
+ * number of set bits. You are allowed to do this
+ * operation **any** number of times (**including
+ * zero**).
+ *
+ * Return *`true` if you can sort the array, else
+ * return `false`*.
+ *
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+const canSortArray = function (nums) {};
