0125d8e55c65ccfd9d52a43fc324875892b8dfa9
yuhcee <egbouchenna001@gmail.com>
2024-11-08 22:12:28 +0100

    :books: (getMaximumXor) add task description

---
+++ Datastructures_&_Algorithms/Math/getMaximumXor.js
@@ -0,0 +1,18 @@
+/**
+ * **1829. Maximum XOR for Each Query**
+ *
+ * You are given a **sorted** array nums of `n` non-negative integers and an integer `maximumBit`. You want
+ * to perform the following query `n` **times**:
+ *
+ * 1. Find a non-negative integer `k < 2maximumBit` such that `nums[0] XOR nums[1] XOR ... XOR nums[nums.
+ * length-1]` XOR k is **maximized**. `k` is the answer to the `ith` query.
+ *
+ * 2. Remove the **last** element from the current array nums.
+ *
+ * Return an array `answer`, where `answer[i]` is the answer to the `ith` query.
+ *
+ * @param {number[]} nums
+ * @param {number} maximumBit
+ * @return {number[]}
+ */
+const getMaximumXor = function (nums, maximumBit) {};
