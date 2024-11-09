4686e386195689baa3df9b866f2db5af4818f224
yuhcee <egbouchenna001@gmail.com>
2024-11-09 20:06:35 +0100

    :sparkles: (minEnd) add task solution

--- Datastructures_&_Algorithms/Math/minEnd.js
+++ Datastructures_&_Algorithms/Math/minEnd.js
@@ -15,4 +15,20 @@
  * @param {number} x
  * @return {number}
  */
-const minEnd = function (n, x) {};
+const minEnd = function (n, x) {
+    let result = BigInt(x);
+    let remaining = BigInt(n - 1);
+    let position = 1n;
+
+    while (remaining > 0n) {
+        if ((BigInt(x) & position) === 0n) {
+            if (remaining & 1n) {
+                result |= position;
+            }
+            remaining >>= 1n;
+        }
+        position <<= 1n;
+    }
+
+    return Number(result);
+};
