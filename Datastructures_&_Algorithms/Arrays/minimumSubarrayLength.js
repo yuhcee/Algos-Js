5f1137941c3f148a9cdda68bd1fc6bb8ed6beb36
yuhcee <egbouchenna001@gmail.com>
2024-11-10 22:46:51 +0100

    :heavy_plus_sign: (minimumSubarrayLength) add  constraints

--- Datastructures_&_Algorithms/Arrays/minimumSubarrayLength.js
+++ Datastructures_&_Algorithms/Arrays/minimumSubarrayLength.js
@@ -1,15 +1,21 @@
 /**
  * **3097. Shortest Subarray With OR at Least K II**
- * 
+ *
  * You are given an array `nums` of **non-negative** integers and an integer `k`.
- * 
+ *
  * An array is called **special** if the bitwise `OR` of all of its elements is **at least** `k`.
- * 
- * Return *the length of the **shortest special non-empty subarray** of `nums`, or return `-1` if no special 
+ *
+ * Return *the length of the **shortest special non-empty subarray** of `nums`, or return `-1` if no special
  * subarray exists.*
- * 
+ *
+ * **Constraints:**
+ *
+ * - `1 <= nums.length <= 2 * 105`
+ * - `0 <= nums[i] <= 109`
+ * - `0 <= k <= 109`
+ *
  * @param {number[]} nums
  * @param {number} k
  * @return {number}
  */
-const minimumSubarrayLength = (nums, k) => {}
+const minimumSubarrayLength = (nums, k) => {};
