079ab26dbc81655f5961a3f12d863f25dec69909
yuhcee <egbouchenna001@gmail.com>
2024-11-10 22:46:07 +0100

    :books: (minimumSubarrayLength) add task description

---
+++ Datastructures_&_Algorithms/Arrays/minimumSubarrayLength.js
@@ -0,0 +1,15 @@
+/**
+ * **3097. Shortest Subarray With OR at Least K II**
+ * 
+ * You are given an array `nums` of **non-negative** integers and an integer `k`.
+ * 
+ * An array is called **special** if the bitwise `OR` of all of its elements is **at least** `k`.
+ * 
+ * Return *the length of the **shortest special non-empty subarray** of `nums`, or return `-1` if no special 
+ * subarray exists.*
+ * 
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+const minimumSubarrayLength = (nums, k) => {}
