29ffde75a880cad7bb5263b750c7cc58b0d1e5e4
yuhcee <egbouchenna001@gmail.com>
2024-11-11 08:26:49 +0100

    :books: (primeSubOperation) add task description

---
+++ Datastructures_&_Algorithms/Math/primeSubOperation.js
@@ -0,0 +1,20 @@
+/**
+ * **2601. Prime Subtraction Operation**
+ *
+ * You are given a **0-indexed** integer array `nums` of length `n`.
+ *
+ * You can perform the following operation as many times as you want:
+ *
+ * - Pick an index `i` that you haven’t picked before, and pick a prime `p` **strictly less than** `nums[i]
+ * `, then subtract `p` from `nums[i]`.
+ *
+ * Return *`true` if you can make nums a strictly increasing array using the above operation and false
+ * otherwise*.
+ *
+ * A **strictly increasing array** is an array whose each element is strictly greater than its preceding
+ * element.
+ *
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+const primeSubOperation = function (nums) {};
