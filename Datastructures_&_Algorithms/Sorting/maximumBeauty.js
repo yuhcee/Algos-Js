faf97d8c3768caf247d0e6e6840b691a69d06704
yuhcee <egbouchenna001@gmail.com>
2024-11-12 21:30:09 +0100

    :books: (maximumBeauty) add task description

---
+++ Datastructures_&_Algorithms/Sorting/maximumBeauty.js
@@ -0,0 +1,18 @@
+/**
+ * **2070. Most Beautiful Item for Each Query**
+ *
+ * You are given a 2D integer array `items` where `items[i] = [pricei, beautyi]` denotes the **price** and
+ * **beauty** of an item respectively.
+ *
+ * You are also given a **0-indexed** integer array `queries`. For each `queries[j]`, you want to determine
+ * the **maximum beauty** of an item whose price is less than or equal to `queries[j]`. If no such item
+ * exists, then the answer to this query is `0`.
+ *
+ * Return *an `array` answer of the same length as `queries` where `answer[j]` is the answer to the `jth`
+ * query*.
+ *
+ * @param {number[][]} items
+ * @param {number[]} queries
+ * @return {number[]}
+ */
+const maximumBeauty = function (items, queries) {};
