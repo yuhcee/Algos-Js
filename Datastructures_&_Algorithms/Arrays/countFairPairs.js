6fbd2b780fa7ffcfa6870c4d754d9a810ea0f897
yuhcee <egbouchenna001@gmail.com>
2024-11-13 20:31:32 +0100

    :heavy_plus_sign: (countFairPairs) add  constraints

--- Datastructures_&_Algorithms/Arrays/countFairPairs.js
+++ Datastructures_&_Algorithms/Arrays/countFairPairs.js
@@ -4,6 +4,13 @@
  * Given a **0-indexed** integer array `nums` of size `n` and two integers `lower` and `upper`, return *the
  * number of fair pairs*.
  *
+ * **Constraints:**
+ *
+ * - `1 <= nums.length <= 105`
+ * - `nums.length == n`
+ * - `-109 <= nums[i] <= 109`
+ * - `-109 <= lower <= upper <= 109`
+ *
  * @param {number[]} nums
  * @param {number} lower
  * @param {number} upper
