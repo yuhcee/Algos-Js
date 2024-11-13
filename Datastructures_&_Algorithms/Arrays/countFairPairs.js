8ae33ca194632a048a55e3e01ecd43cd72d55435
yuhcee <egbouchenna001@gmail.com>
2024-11-13 20:32:39 +0100

    :white_check_mark: (countFairPairs) add test 1

--- Datastructures_&_Algorithms/Arrays/countFairPairs.js
+++ Datastructures_&_Algorithms/Arrays/countFairPairs.js
@@ -49,3 +49,10 @@ const countFairPairs = function (nums, lower, upper) {
 
     return count;
 };
+
+const nums = [0, 1, 7, 4, 4, 5],
+    lower = 3,
+    upper = 6;
+// Output: 6
+// Explanation: There are 6 fair pairs: (0,3), (0,4), (0,5), (1,3), (1,4), and (1,5).
+console.log(countFairPairs(nums, lower, upper));
