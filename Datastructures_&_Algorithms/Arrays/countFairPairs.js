15c2d15d565955b4db4d88196a1717995b6f16f7
yuhcee <egbouchenna001@gmail.com>
2024-11-13 20:30:35 +0100

    :books: (countFairPairs) add task description

---
+++ Datastructures_&_Algorithms/Arrays/countFairPairs.js
@@ -0,0 +1,12 @@
+/**
+ * **2563. Count the Number of Fair Pairs**
+ *
+ * Given a **0-indexed** integer array `nums` of size `n` and two integers `lower` and `upper`, return *the
+ * number of fair pairs*.
+ *
+ * @param {number[]} nums
+ * @param {number} lower
+ * @param {number} upper
+ * @return {number}
+ */
+const countFairPairs = function (nums, lower, upper) {};
