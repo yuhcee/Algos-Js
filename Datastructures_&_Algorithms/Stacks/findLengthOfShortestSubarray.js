df3895b9334a33ec950aa132e7a62761c692bcbe
yuhcee <egbouchenna001@gmail.com>
2024-11-16 00:12:01 +0100

    :books: (findLengthOfShortestSubarray) add task description

---
+++ Datastructures_&_Algorithms/Stacks/findLengthOfShortestSubarray.js
@@ -0,0 +1,14 @@
+/**
+ * **1574. Shortest Subarray to be Removed to Make Array Sorted**
+ *
+ * Given an integer array `arr`, remove a subarray (can be empty) from `arr` such that the remaining
+ * elements in arr are **non-decreasing**.
+ *
+ * Return *the length of the shortest subarray to remove*.
+ *
+ * A **subarray** is a contiguous subsequence of the array.
+ *
+ * @param {number[]} arr
+ * @return {number}
+ */
+const findLengthOfShortestSubarray = function (arr) {};
