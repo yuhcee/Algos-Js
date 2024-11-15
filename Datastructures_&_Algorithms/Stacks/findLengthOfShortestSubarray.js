4efaf457e7c502d490d2e0dafa2e7e00bd2752b1
yuhcee <egbouchenna001@gmail.com>
2024-11-16 00:14:51 +0100

    :white_check_mark: (findLengthOfShortestSubarray) add test 2

--- Datastructures_&_Algorithms/Stacks/findLengthOfShortestSubarray.js
+++ Datastructures_&_Algorithms/Stacks/findLengthOfShortestSubarray.js
@@ -59,3 +59,8 @@ const arr = [1, 2, 3, 10, 4, 2, 3, 5];
 /* Explanation: The shortest subarray we can remove is [10,4,2] of length 3. The remaining elements after that will be [1,2,3,3,5] which are sorted.
 Another correct solution is to remove the subarray [3,10,4]. */
 console.log(findLengthOfShortestSubarray(arr));
+
+const arr1 = [5, 4, 3, 2, 1];
+// Output: 4
+/* Explanation: Since the array is strictly decreasing, we can only keep a single element. Therefore we need to remove a subarray of length 4, either [5,4,3,2] or [4,3,2,1]. */
+console.log(findLengthOfShortestSubarray(arr1));
