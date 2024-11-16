e6a7ef8a92887e04394af7bdc0ff16a7d3a8977c
yuhcee <egbouchenna001@gmail.com>
2024-11-16 23:33:52 +0100

    :books: (resultsArray) add task description

---
+++ Datastructures_&_Algorithms/Arrays/resultsArray.js
@@ -0,0 +1,20 @@
+/**
+ * **3254. Find the Power of K-Size Subarrays I**
+ *
+ * You are given an array of integers `nums` of length `n` and a *positive* integer `k`.
+ *
+ * The **power** of an array is defined as:
+ *
+ * - Its **maximum** element if all of its elements are **consecutive** and **sorted** in **ascending**
+ * order.
+ * - -1 otherwise.
+ * You need to find the power of all subarrays of `nums` of size `k`.
+ *
+ * Return an integer array `results` of size `n - k + 1`, where `results[i]` is the power of `nums[i..(i + k
+ * - 1)]`.
+ *
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+const resultsArray = function (nums, k) {};
