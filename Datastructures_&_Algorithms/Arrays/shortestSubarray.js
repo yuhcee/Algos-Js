0612f417dc5baa321e3c7f0254663498f7cc7521
yuhcee <egbouchenna001@gmail.com>
2024-11-17 17:52:04 +0100

    :books: (shortestSubarray) add task description

---
+++ Datastructures_&_Algorithms/Arrays/shortestSubarray.js
@@ -0,0 +1,13 @@
+/**
+ * **862. Shortest Subarray with Sum at Least K**
+ * 
+ * Given an integer array `nums` and an integer `k`, return *the length of the shortest non-empty 
+ * **subarray** of `nums` with a sum of at least `k`*. If there is no such **subarray**, return `-1`.
+ * 
+ * A **subarray** is a **contiguous** part of an array.
+ * 
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+const shortestSubarray = function (nums, k) {};
