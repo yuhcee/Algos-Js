2f7318d21ffa1ec50e65b81a9fde82b13083fe36
yuhcee <egbouchenna001@gmail.com>
2024-11-17 17:52:53 +0100

    :heavy_plus_sign: (shortestSubarray) add  constraints

--- Datastructures_&_Algorithms/Arrays/shortestSubarray.js
+++ Datastructures_&_Algorithms/Arrays/shortestSubarray.js
@@ -1,11 +1,17 @@
 /**
  * **862. Shortest Subarray with Sum at Least K**
- * 
- * Given an integer array `nums` and an integer `k`, return *the length of the shortest non-empty 
+ *
+ * Given an integer array `nums` and an integer `k`, return *the length of the shortest non-empty
  * **subarray** of `nums` with a sum of at least `k`*. If there is no such **subarray**, return `-1`.
- * 
+ *
  * A **subarray** is a **contiguous** part of an array.
- * 
+ *
+ * **Constraints:**
+ *
+ * - `1 <= nums.length <= 105`
+ * - `-105 <= nums[i] <= 105`
+ * - `1 <= k <= 109`
+ *
  * @param {number[]} nums
  * @param {number} k
  * @return {number}
