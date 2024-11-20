e073614445e79c1850ff6a03befa275441989a28
yuhcee <egbouchenna001@gmail.com>
2024-11-20 21:03:40 +0100

    :white_check_mark: (takeCharacters) add test 1

--- Datastructures_&_Algorithms/HashMaps/takeCharacters.js
+++ Datastructures_&_Algorithms/HashMaps/takeCharacters.js
@@ -53,3 +53,13 @@ const takeCharacters = function (s, k) {
     // Minimum minutes = total length - max valid subarray size
     return n - maxSubarraySize;
 };
+
+const s = 'aabaaaacaabc',
+    k = 2;
+// Output: 8
+/* Explanation: 
+Take three characters from the left of s. You now have two 'a' characters, and one 'b' character.
+Take five characters from the right of s. You now have four 'a' characters, two 'b' characters, and two 'c' characters.
+A total of 3 + 5 = 8 minutes is needed.
+It can be proven that 8 is the minimum number of minutes needed. */
+console.log(takeCharacters(s, k));
