2eadccd3122b8c29829ca759d902ae5bd3d5ef36
yuhcee <egbouchenna001@gmail.com>
2024-11-21 23:32:54 +0100

    :books: (countUnguarded) add task description

---
+++ Datastructures_&_Algorithms/Arrays/countUnguarded.js
@@ -0,0 +1,22 @@
+/**
+ * **2257. Count Unguarded Cells in the Grid**
+ *
+ * You are given two integers `m` and `n` representing a **0-indexed** `m x n`
+ * grid. You are also given two 2D integer arrays `guards` and `walls` where
+ * `guards[i] = [rowi, coli]` and `walls[j] = [rowj, colj]` represent the
+ * positions of the `ith` guard and `jth` wall respectively.
+ *
+ * A guard can see **every** cell in the four cardinal directions (north, east,
+ * south, or west) starting from their position unless **obstructed** by a wall
+ * or another guard. A cell is **guarded** if there is **at least one** guard
+ * that can see it.
+ *
+ * Return *the number of unoccupied cells that are **not guarded***.
+ *
+ * @param {number} m
+ * @param {number} n
+ * @param {number[][]} guards
+ * @param {number[][]} walls
+ * @return {number}
+ */
+const countUnguarded = function (m, n, guards, walls) {};
