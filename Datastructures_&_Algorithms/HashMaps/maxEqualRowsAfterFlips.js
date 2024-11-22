57fe8fe9db44d8db03c73279516fe672897f0020
yuhcee <egbouchenna001@gmail.com>
2024-11-22 22:52:45 +0100

    :books: (maxEqualRowsAfterFlips) add task description

---
+++ Datastructures_&_Algorithms/HashMaps/maxEqualRowsAfterFlips.js
@@ -0,0 +1,16 @@
+/**
+ * **1072. Flip Columns For Maximum Number of Equal Rows**
+ *
+ * You are given an `m x n` binary matrix `matrix`.
+ *
+ * You can choose any number of columns in the matrix and flip every cell in
+ * that column (i.e., Change the value of the cell from `0` to `1` or vice
+ * versa).
+ *
+ * Return *the maximum number of rows that have all values equal after some
+ * number of flips.*
+ *
+ * @param {number[][]} matrix
+ * @return {number}
+ */
+const maxEqualRowsAfterFlips = function (matrix) {};
