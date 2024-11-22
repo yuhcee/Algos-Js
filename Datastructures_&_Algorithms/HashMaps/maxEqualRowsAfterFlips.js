510b61295be32dbfb6110f2812a3895698f8f2a0
yuhcee <egbouchenna001@gmail.com>
2024-11-22 22:53:56 +0100

    :heavy_plus_sign: (maxEqualRowsAfterFlips) add  constraints

--- Datastructures_&_Algorithms/HashMaps/maxEqualRowsAfterFlips.js
+++ Datastructures_&_Algorithms/HashMaps/maxEqualRowsAfterFlips.js
@@ -10,6 +10,13 @@
  * Return *the maximum number of rows that have all values equal after some
  * number of flips.*
  *
+ * **Constraints:**
+ *
+ * - `m == matrix.length`
+ * - `n == matrix[i].length`
+ * - `1 <= m, n <= 300`
+ * - `matrix[i][j]` is either `0` or `1`.
+ *
  * @param {number[][]} matrix
  * @return {number}
  */
