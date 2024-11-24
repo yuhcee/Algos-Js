699dc5babbbfa9dbee95ffcd72011669e859bfce
yuhcee <egbouchenna001@gmail.com>
2024-11-24 14:37:19 +0100

    :white_check_mark: (maxMatrixSum) add test 2

--- Datastructures_&_Algorithms/Greedy Algorithms/maxMatrixSum.js
+++ Datastructures_&_Algorithms/Greedy Algorithms/maxMatrixSum.js
@@ -56,3 +56,13 @@ const matrix = [
 - Multiply the 2 elements in the first row by -1.
 - Multiply the 2 elements in the first column by -1. */
 console.log(maxMatrixSum(matrix));
+
+const matrix1 = [
+    [1, 2, 3],
+    [-1, -2, -3],
+    [1, 2, 3],
+];
+// Output: 16
+/* Explanation: We can follow the following step to reach sum equals 16:
+- Multiply the 2 last elements in the second row by -1. */
+console.log(maxMatrixSum(matrix1));
