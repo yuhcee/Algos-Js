7f1a91f50ba719fd27be88e49293c51d072f7809
yuhcee <egbouchenna001@gmail.com>
2024-11-24 14:34:54 +0100

    :books: (maxMatrixSum) add task description

---
+++ Datastructures_&_Algorithms/Greedy Algorithms/maxMatrixSum.js
@@ -0,0 +1,20 @@
+/**
+ * **1975. Maximum Matrix Sum**
+ *
+ * You are given an `n x n` integer `matrix`. You can do the following
+ * operation any number of times:
+ *
+ * - Choose any two **adjacent** elements of matrix and **multiply** each of
+ * them by `-1`.
+ *
+ * Two elements are considered **adjacent** if and only if they share a
+ * **border**.
+ *
+ * Your goal is to **maximize** the summation of the matrix's elements. Return
+ * *the **maximum** sum of the matrix's elements using the operation mentioned
+ * above*.
+ *
+ * @param {number[][]} matrix
+ * @return {number}
+ */
+const maxMatrixSum = function (matrix) {};
