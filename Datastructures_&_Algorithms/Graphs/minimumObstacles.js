880c836e0f2c79084fa4fb702545091bfc9c06e3
yuhcee <egbouchenna001@gmail.com>
2024-11-28 22:35:51 +0100

    :heavy_plus_sign: (minimumObstacles) add  constraints

--- Datastructures_&_Algorithms/Graphs/minimumObstacles.js
+++ Datastructures_&_Algorithms/Graphs/minimumObstacles.js
@@ -12,6 +12,15 @@
  * Return the **minimum** number of **obstacles** to **remove** so you can move
  * from the upper left corner (0, 0) to the lower right corner (m - 1, n - 1).
  *
+ * **Constraints:**
+ * 
+ * - `m == grid.length`
+ * - `n == grid[i].length`
+ * - `1 <= m, n <= 105`
+ * - `2 <= m * n <= 105`
+ * - `grid[i][j]` is either `0` or `1`.
+ * - `grid[0][0] == grid[m - 1][n - 1] == 0`
+ * 
  * @param {number[][]} grid
  * @return {number}
  */
