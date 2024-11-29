9d0b604bd22907e5907ab7aa6a446bc49e8c8e0d
yuhcee <egbouchenna001@gmail.com>
2024-11-29 22:34:59 +0100

    :books: (minimumTime) add task description

---
+++ Datastructures_&_Algorithms/Graphs/minimumTimeCell.js
@@ -0,0 +1,21 @@
+/**
+ * **2577. Minimum Time to Visit a Cell In a Grid**
+ *
+ * You are given a `m x n` matrix `grid` consisting of **non-negative**
+ * integers where `grid[row][col]` represents the minimum time required to be
+ * able to visit the cell `(row, col)`, which means you can visit the cell `
+ * (row, col)` only when the time you visit it is greater than or equal to `grid
+ * [row][col]`.
+ *
+ * You are standing in the **top-left** cell of the matrix in the `0th` second,
+ * and you must move to **any** adjacent cell in the four directions: up, down,
+ * left, and right. Each move you make takes 1 second.
+ *
+ * Return *the **minimum** time required in which you can visit the
+ * bottom-right cell of the matrix*. If you cannot visit the bottom-right cell,
+ * then return -1.
+ *
+ * @param {number[][]} grid
+ * @return {number}
+ */
+const minimumTime = function (grid) {};
