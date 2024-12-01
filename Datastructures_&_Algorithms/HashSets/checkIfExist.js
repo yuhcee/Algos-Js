e93ee38865d7ca6b75035560a46b51c9a9248627
yuhcee <egbouchenna001@gmail.com>
2024-12-01 05:18:51 +0100

    :white_check_mark: (checkIfExist) add test 1

--- Datastructures_&_Algorithms/HashSets/checkIfExist.js
+++ Datastructures_&_Algorithms/HashSets/checkIfExist.js
@@ -28,3 +28,8 @@ const checkIfExist = function (arr) {
 
     return false;
 };
+
+const arr = [10, 2, 5, 3];
+// Output: true
+// Explanation: For i = 0 and j = 2, arr[i] == 10 == 2 * 5 == 2 * arr[j]
+console.log(checkIfExist(arr));
