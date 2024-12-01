593b8a1d98d74652f90ebac28f56a551f1d373bd
yuhcee <egbouchenna001@gmail.com>
2024-12-01 05:18:13 +0100

    :sparkles: (checkIfExist) add task solution

--- Datastructures_&_Algorithms/HashSets/checkIfExist.js
+++ Datastructures_&_Algorithms/HashSets/checkIfExist.js
@@ -16,4 +16,15 @@
  * @param {number[]} arr
  * @return {boolean}
  */
-const checkIfExist = function (arr) {};
+const checkIfExist = function (arr) {
+    const seen = new Set();
+
+    for (const num of arr) {
+        if (seen.has(num * 2) || (num % 2 === 0 && seen.has(num / 2))) {
+            return true;
+        }
+        seen.add(num);
+    }
+
+    return false;
+};
