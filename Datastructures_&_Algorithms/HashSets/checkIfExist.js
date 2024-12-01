74f96268f3859b2d32a75f1548dc65f22b5f3f6e
yuhcee <egbouchenna001@gmail.com>
2024-12-01 05:17:51 +0100

    :heavy_plus_sign: (checkIfExist) add  constraints

--- Datastructures_&_Algorithms/HashSets/checkIfExist.js
+++ Datastructures_&_Algorithms/HashSets/checkIfExist.js
@@ -1,12 +1,17 @@
 /**
  * **1346. Check If N and Its Double Exist**
  *
- * Given an array `arr` of integers, check if there exist two indices `i` and 
+ * Given an array `arr` of integers, check if there exist two indices `i` and
  * `j` such that :
  *
  * - `i != j`
  * - `0 <= i, j < arr.length`
  * - `arr[i] == 2 * arr[j]`
+ *
+ * **Constraints:**
+ *
+ * - `2 <= arr.length <= 500`
+ * - `-103 <= arr[i] <= 103`
  *
  * @param {number[]} arr
  * @return {boolean}
