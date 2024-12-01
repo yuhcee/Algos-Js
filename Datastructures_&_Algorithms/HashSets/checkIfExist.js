6ed74d1f838266ee0cf109980ca5aecc1dd1e5ae
yuhcee <egbouchenna001@gmail.com>
2024-12-01 05:17:15 +0100

    :books: (checkIfExist) add task description

---
+++ Datastructures_&_Algorithms/HashSets/checkIfExist.js
@@ -0,0 +1,14 @@
+/**
+ * **1346. Check If N and Its Double Exist**
+ *
+ * Given an array `arr` of integers, check if there exist two indices `i` and 
+ * `j` such that :
+ *
+ * - `i != j`
+ * - `0 <= i, j < arr.length`
+ * - `arr[i] == 2 * arr[j]`
+ *
+ * @param {number[]} arr
+ * @return {boolean}
+ */
+const checkIfExist = function (arr) {};
