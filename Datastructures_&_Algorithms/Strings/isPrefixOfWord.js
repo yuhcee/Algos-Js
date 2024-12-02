321e177e2d325d197a294a5006ba3df0c363ed84
yuhcee <egbouchenna001@gmail.com>
2024-12-02 05:56:45 +0100

    :books: (isPrefixOfWord) add task description

---
+++ Datastructures_&_Algorithms/Strings/isPrefixOfWord.js
@@ -0,0 +1,19 @@
+/**
+ * **1455. Check If a Word Occurs As a Prefix of Any Word in a Sentence**
+ *
+ * Given a `sentence` that consists of some words separated by a **single
+ * space**, and a `searchWord`, check if `searchWord` is a prefix of any word
+ * in `sentence`.
+ *
+ * Return *the index of the word in `sentence` (**1-indexed**) where
+ * `searchWord`is a prefix of this word*. If `searchWord` is a prefix of more
+ * than one word, return the index of the first word (**minimum index**). If
+ * there is no such word return `-1`.
+ *
+ * **prefix** of a string `s` is any leading contiguous substring of `s`.
+ *
+ * @param {string} sentence
+ * @param {string} searchWord
+ * @return {number}
+ */
+const isPrefixOfWord = function (sentence, searchWord) {};
