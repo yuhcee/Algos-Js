52d1cee168054706a900f0db1e7af649f624eccf
yuhcee <egbouchenna001@gmail.com>
2024-12-03 23:17:59 +0100

    :books: (addSpaces) add task description

---
+++ Datastructures_&_Algorithms/Strings/addSpaces.js
@@ -0,0 +1,19 @@
+/**
+ * **2109. Adding Spaces to a String**
+ *
+ * You are given a **0-indexed** string `s` and a **0-indexed** integer array
+ * `spaces` that describes the indices in the original string where spaces will
+ * be added. Each space should be inserted **before** the character at the
+ * given index.
+ *
+ * For example, given `s = "EnjoyYourCoffee"` and `spaces = [5, 9]`, we place
+ * spaces before `'Y'` and `'C'`, which are at indices `5` and `9`
+ * respectively. Thus, we obtain `"Enjoy Your Coffee"`.
+ *
+ * Return *the modified string **after** the spaces have been added.*
+ *
+ * @param {string} s
+ * @param {number[]} spaces
+ * @return {string}
+ */
+const addSpaces = function (s, spaces) {};
