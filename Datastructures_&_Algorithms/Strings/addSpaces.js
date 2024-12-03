28f837e9339864e25d8d295285a0bb1774f191a7
yuhcee <egbouchenna001@gmail.com>
2024-12-03 23:20:10 +0100

    :white_check_mark: (addSpaces) add test 1

--- Datastructures_&_Algorithms/Strings/addSpaces.js
+++ Datastructures_&_Algorithms/Strings/addSpaces.js
@@ -40,3 +40,12 @@ const addSpaces = function (s, spaces) {
 
     return result.join(''); // Combine the result array into a single string
 };
+
+const s = 'LeetcodeHelpsMeLearn',
+    spaces = [8, 13, 15];
+// Output: "Leetcode Helps Me Learn"
+/* Explanation: 
+The indices 8, 13, and 15 correspond to the underlined characters in "LeetcodeHelpsMeLearn".
+We then place spaces before those characters.
+ */
+console.log(addSpaces(s, spaces));
