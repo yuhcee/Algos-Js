aa7d9dc9411315870418d379c5f4d4deb099e1b5
yuhcee <egbouchenna001@gmail.com>
2024-12-04 23:40:37 +0100

    :white_check_mark: (canMakeSubsequence) add test 3

--- Datastructures_&_Algorithms/Strings/canMakeSubsequence.js
+++ Datastructures_&_Algorithms/Strings/canMakeSubsequence.js
@@ -49,11 +49,18 @@ Hence, str1 becomes "abd" and str2 is now a subsequence. Therefore, true is retu
  */
 console.log(canMakeSubsequence(str1, str2));
 
-const str1 = 'zc',
-    str2 = 'ad';
+const str11 = 'zc',
+    str21 = 'ad';
 // Output: true
 /* Explanation: Select indices 0 and 1 in str1. 
 Increment str1[0] to become 'a'. 
 Increment str1[1] to become 'd'. 
 Hence, str1 becomes "ad" and str2 is now a subsequence. Therefore, true is returned. */
 console.log(canMakeSubsequence(str11, str21));
+
+const str12 = 'ab',
+    str22 = 'd';
+// Output: false
+/* Explanation: In this example, it can be shown that it is impossible to make str2 a subsequence of str1 using the operation at most once. 
+Therefore, false is returned. */
+console.log(canMakeSubsequence(str12, str22));
