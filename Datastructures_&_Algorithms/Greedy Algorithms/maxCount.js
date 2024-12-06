af2c5f9e65b634211cc7743e7ab8b4b27f62c402
yuhcee <egbouchenna001@gmail.com>
2024-12-06 12:46:18 +0100

    :white_check_mark: (maxCount) add test 1

--- Datastructures_&_Algorithms/Greedy Algorithms/maxCount.js
+++ Datastructures_&_Algorithms/Greedy Algorithms/maxCount.js
@@ -51,3 +51,11 @@ const maxCount = function (banned, n, maxSum) {
 
     return count;
 };
+
+const banned = [1, 6, 5],
+    n = 5,
+    maxSum = 6;
+// Output: 2
+/* Explanation: You can choose the integers 2 and 4.
+2 and 4 are from the range [1, 5], both did not appear in banned, and their sum is 6, which did not exceed maxSum. */
+console.log(maxCount(banned, n, maxSum));
