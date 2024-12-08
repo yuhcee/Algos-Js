2219a55dfa6617a076899a584d0a0a9d7cd35d03
yuhcee <egbouchenna001@gmail.com>
2024-12-08 23:03:11 +0100

    :books: (maxTwoEvents) add task description

---
+++ Datastructures_&_Algorithms/Dynamic Programming/maxTwoEvents.js
@@ -0,0 +1,20 @@
+/**
+ * **2054. Two Best Non-Overlapping Events**
+ *
+ * You are given a **0-indexed** 2D integer array of `events` where `events[i]
+ * = [startTimei, endTimei, valuei]`. The `ith` event starts at `startTimei`
+ * and ends at `endTimei`, and if you attend this event, you will receive a
+ * value of `valuei`. You can choose **at most two non-overlapping** events to
+ * attend such that the sum of their values is **maximized**.
+ *
+ * Return *this **maximum** sum.*
+ *
+ * Note that the start time and end time is **inclusive**: that is, you cannot
+ * attend two events where one of them starts and the other ends at the same
+ * time. More specifically, if you attend an event with end time `t`, the next
+ * event must start at or after `t + 1`.
+ *
+ * @param {number[][]} events
+ * @return {number}
+ */
+const maxTwoEvents = function (events) {};
