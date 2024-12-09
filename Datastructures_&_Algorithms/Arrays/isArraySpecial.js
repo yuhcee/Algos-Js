a4c90e342ee0e16107328ba555e7fbac8bc22266
yuhcee <egbouchenna001@gmail.com>
2024-12-09 05:38:52 +0100

    :books: (isArraySpecial) add task description

---
+++ Datastructures_&_Algorithms/Arrays/isArraySpecial.js
@@ -0,0 +1,18 @@
+/**
+ * **3152. Special Array II**
+ *
+ * An array is considered **special** if every pair of its adjacent elements
+ * contains two numbers with different parity.
+ *
+ * You are given an array of integer `nums` and a 2D integer matrix `queries`,
+ * where for `queries[i] = [fromi, toi]` your task is to check that
+ * subarray `nums[fromi..toi]` is **special** or not.
+ *
+ * Return an array of booleans `answer` such that `answer[i]` is `true` if `nums
+ * [fromi..toi]` is special.
+ *
+ * @param {number[]} nums
+ * @param {number[][]} queries
+ * @return {boolean[]}
+ */
+const isArraySpecial = function (nums, queries) {};
