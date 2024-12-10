d412c98e51902508efb6f8ab26d74045da3e56c4
yuhcee <egbouchenna001@gmail.com>
2024-12-10 19:59:21 +0100

    :books: (maximumLength) add task description

---
+++ Datastructures_&_Algorithms/Strings/maximumLength.js
@@ -0,0 +1,19 @@
+/**
+ * **2981. Find Longest Special Substring That Occurs Thrice I**
+ *
+ * You are given a string `s` that consists of lowercase English letters.
+ *
+ * A string is called **special** if it is made up of only a single character.
+ * For example, the string "abc" is not special, whereas the strings `"ddd"`,
+ * `"zz"`, and `"f"` are special.
+ *
+ * Return *the length of the **longest special substring** of `s` which occurs
+ * **at least thrice**, or `-1` if no special substring occurs at least thrice.*
+ *
+ * A **substring** is a contiguous **non-empty** sequence of characters within
+ * a string.
+ *
+ * @param {string} s
+ * @return {number}
+ */
+const maximumLength = function (s) {};
