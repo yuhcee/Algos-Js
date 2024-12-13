4d1bdce8eadd079a339dfb8c2096b4fafedab529
yuhcee <egbouchenna001@gmail.com>
2024-12-13 20:38:04 +0100

    :sparkles: (continuousSubarrays) add task solution

--- Datastructures_&_Algorithms/Arrays/continuousSubarrays.js
+++ Datastructures_&_Algorithms/Arrays/continuousSubarrays.js
@@ -20,4 +20,37 @@
  * @param {number[]} nums
  * @return {number}
  */
-const continuousSubarrays = function (nums) {};
+const continuousSubarrays = function (nums) {
+    let n = nums.length;
+    let start = 0;
+    let count = 0;
+
+    let minDeque = []; // Monotonic increasing deque
+    let maxDeque = []; // Monotonic decreasing deque
+
+    for (let end = 0; end < n; end++) {
+        // Update max deque
+        while (maxDeque.length && nums[maxDeque[maxDeque.length - 1]] <= nums[end]) {
+            maxDeque.pop();
+        }
+        maxDeque.push(end);
+
+        // Update min deque
+        while (minDeque.length && nums[minDeque[minDeque.length - 1]] >= nums[end]) {
+            minDeque.pop();
+        }
+        minDeque.push(end);
+
+        // Shrink the window if the condition is violated
+        while (nums[maxDeque[0]] - nums[minDeque[0]] > 2) {
+            if (maxDeque[0] === start) maxDeque.shift();
+            if (minDeque[0] === start) minDeque.shift();
+            start++;
+        }
+
+        // Add the number of valid subarrays ending at `end`
+        count += end - start + 1;
+    }
+
+    return count;
+};
