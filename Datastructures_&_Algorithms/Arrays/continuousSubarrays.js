655e5fa45bc74b5dc42e2fac4e6586548d9fe89b
yuhcee <egbouchenna001@gmail.com>
2024-12-13 20:37:18 +0100

    :books: (continuousSubarrays) add task description

---
+++ Datastructures_&_Algorithms/Arrays/continuousSubarrays.js
@@ -0,0 +1,18 @@
+/**
+ * **2762. Continuous Subarrays**
+ *
+ * You are given a **0-indexed** integer array `nums`. A subarray of `nums` is
+ * called **continuous** if:
+ *
+ * - Let `i`, `i + 1`, ..., `j` be the indices in the subarray. Then, for each
+ * pair of indices `i <= i1`, `i2 <= j`, `0 <= |nums[i1] - nums[i2]| <= 2`.
+ *
+ * Return *the total number of **continuous** subarrays*.
+ *
+ * A subarray is a contiguous **non-empty** sequence of elements within an
+ * array.
+ *
+ * @param {number[]} nums
+ * @return {number}
+ */
+const continuousSubarrays = function (nums) {};
