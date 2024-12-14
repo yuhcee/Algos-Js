7658cf5acb41029376d7492355ff21fd62ef6097
yuhcee <egbouchenna001@gmail.com>
2024-12-14 20:30:41 +0100

    :white_check_mark: (findScore) add test 2

--- Datastructures_&_Algorithms/HashSets/findScore.js
+++ Datastructures_&_Algorithms/HashSets/findScore.js
@@ -56,3 +56,12 @@ const nums = [2, 1, 3, 4, 5, 2];
 - 4 is the only remaining unmarked element, so we mark it: [2,1,3,4,5,2].
 Our score is 1 + 2 + 4 = 7. */
 console.log(findScore(nums));
+
+const nums1 = [2, 3, 5, 1, 3, 2];
+// Output: 5
+/* Explanation: We mark the elements as follows:
+- 1 is the smallest unmarked element, so we mark it and its two adjacent elements: [2,3,5,1,3,2].
+- 2 is the smallest unmarked element, since there are two of them, we choose the left-most one, so we mark the one at index 0 and its right adjacent element: [2,3,5,1,3,2].
+- 2 is the only remaining unmarked element, so we mark it: [2,3,5,1,3,2].
+Our score is 1 + 2 + 2 = 5. */
+console.log(findScore(nums1));
