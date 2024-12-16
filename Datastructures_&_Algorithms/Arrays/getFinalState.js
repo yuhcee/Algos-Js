8380076489d07b01ab9016078b5872e605b7937a
yuhcee <egbouchenna001@gmail.com>
2024-12-16 22:35:17 +0100

    :books: (getFinalState) add task description

---
+++ Datastructures_&_Algorithms/Arrays/getFinalState.js
@@ -0,0 +1,22 @@
+/**
+ * **3264. Final Array State After K Multiplication Operations I**
+ *
+ * You are given an integer array `nums`, an integer `k`, and an integer
+ * `multiplier`.
+ *
+ * You need to perform `k` operations on `nums`. In each operation:
+ *
+ * - Find the minimum value `x` in `nums`. If there are multiple
+ * occurrences of the minimum value, select the one that appears first.
+ *
+ * - Replace the selected minimum value `x` with `x * multiplier`.
+ *
+ * Return *an integer array denoting the final state of `nums` after
+ * performing all `k` operations*.
+ *
+ * @param {number[]} nums
+ * @param {number} k
+ * @param {number} multiplier
+ * @return {number[]}
+ */
+const getFinalState = function (nums, k, multiplier) {};
