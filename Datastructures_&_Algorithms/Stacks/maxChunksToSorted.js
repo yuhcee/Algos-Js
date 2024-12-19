bbb6e74a5107a6384e62d855a7dfa93363c54d73
yuhcee <egbouchenna001@gmail.com>
2024-12-19 16:39:12 +0100

    :heavy_plus_sign: (maxChunksToSorted) add  constraints

--- Datastructures_&_Algorithms/Stacks/maxChunksToSorted.js
+++ Datastructures_&_Algorithms/Stacks/maxChunksToSorted.js
@@ -9,6 +9,13 @@
  * result should equal the sorted array.
  *
  * Return *the largest number of chunks we can make to sort the array.*
+ * **Constraints:**
+ *
+ * - `n == arr.length`
+ * - `1 <= n <= 10`
+ * - `0 <= arr[i] < n`
+ * - All the elements of arr are unique.
+ *
  * @param {number[]} arr
  * @return {number}
  */
