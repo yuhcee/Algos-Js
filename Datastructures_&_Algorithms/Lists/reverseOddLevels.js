1cc6c43bc32db66655ccd70f2623c787cf8c2c2b
yuhcee <egbouchenna001@gmail.com>
2024-12-20 17:51:30 +0100

    :white_check_mark: (reverseOddLevels) add test 1

--- Datastructures_&_Algorithms/Lists/reverseOddLevels.js
+++ Datastructures_&_Algorithms/Lists/reverseOddLevels.js
@@ -34,3 +34,11 @@
  * @return {TreeNode}
  */
 const reverseOddLevels = function (root) {};
+
+const root = [2, 3, 5, 8, 13, 21, 34];
+// Output: [2,5,3,8,13,21,34]
+/* Explanation: 
+The tree has only one odd level.
+The nodes at level 1 are 3, 5 respectively, which are reversed and become 5, 3.
+ */
+console.log(reverseOddLevels(root));
