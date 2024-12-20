9905b7a939af1db03b451d60b6400e603151c6cb
yuhcee <egbouchenna001@gmail.com>
2024-12-20 17:48:52 +0100

    :books: (reverseOddLevels) add task description

---
+++ Datastructures_&_Algorithms/Lists/reverseOddLevels.js
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val);
+ *     this.left = (left===undefined ? null : left);
+ *     this.right = (right===undefined ? null : right);
+ * }
+ */
+
+/**
+ * **2415. Reverse Odd Levels of Binary Tree**
+ *
+ * Given the `root` of a **perfect** binary tree, reverse the node values at
+ * each **odd** level of the tree.
+ *
+ * - For example, suppose the node values at level 3 are `[2,1,3,4,7,11,29,18]
+ * `, then it should become `[18,29,11,7,4,3,1,2]`.
+ *
+ * Return *the root of the reversed tree*.
+ *
+ * A binary tree is **perfect** if all parent nodes have two children and all
+ * leaves are on the same level.
+ *
+ * The **level** of a node is the number of edges along the path between it and
+ * the root node.
+ *
+ *
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+const reverseOddLevels = function (root) {};
