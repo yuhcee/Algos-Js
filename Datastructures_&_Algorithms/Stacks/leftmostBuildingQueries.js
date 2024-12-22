9bb36e4e3de4bede78ce9e45a6d6f6320f7b4352
yuhcee <egbouchenna001@gmail.com>
2024-12-22 23:54:29 +0100

    :heavy_plus_sign: (leftmostBuildingQueries) add  constraints

--- Datastructures_&_Algorithms/Stacks/leftmostBuildingQueries.js
+++ Datastructures_&_Algorithms/Stacks/leftmostBuildingQueries.js
@@ -1,4 +1,19 @@
 /**
+ * **2940. Find Building Where Alice and Bob Can Meet**
+ *
+ * You are given a **0-indexed** array `heights` of positive integers, where
+ * `heights[i]` represents the height of the `ith` building.
+ *
+ * If a person is in building `i`, they can move to any other building `j` if
+ * and only if `i < j` and `heights[i] < heights[j]`.
+ *
+ * You are also given another array `queries` where `queries[i] = [ai, bi]`. On
+ * the `ith` query, Alice is in building `ai` while Bob is in building `bi`.
+ *
+ * Return *an array `ans` where `ans[i]` is **the index of the leftmost
+ * building** where Alice and Bob can meet on the `ith` query.* If Alice and Bob
+ * cannot move to a common building on query `i`, set `ans[i]` to `-1`.
+ *
  * @param {number[]} heights
  * @param {number[][]} queries
  * @return {number[]}
