da6a39a032f62ef4b009868aeca3247591fa5c77
yuhcee <egbouchenna001@gmail.com>
2024-12-23 22:39:21 +0100

    :books: (minimumOperations) add task description

---
+++ Datastructures_&_Algorithms/Trees/BinaryTree/minimumOperations.js
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+
+/**
+ * **2471. Minimum Number of Operations to Sort a Binary Tree by Level**
+ *
+ * You are given the `root` of a binary tree with **unique values**.
+ *
+ * In one operation, you can choose any two nodes **at the same level** and
+ * swap their values.
+ *
+ * Return *the minimum number of operations needed to make the values at each
+ * level sorted in a **strictly increasing order***.
+ *
+ * The **level** of a node is the number of edges along the path between it and
+ * the root node.
+ *
+ * @param {TreeNode} root
+ * @return {number}
+ */
+const minimumOperations = function (root) {};
