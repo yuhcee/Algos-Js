0b098a3d646ed58007a9552bd7bb1ba95d4f6f6e
yuhcee <egbouchenna001@gmail.com>
2024-12-28 08:43:47 +0100

    :books: (maxSumOfThreeSubarrays) add task description

---
+++ Datastructures_&_Algorithms/Dynamic Programming/maxSumOfThreeSubarrays.js
@@ -0,0 +1,15 @@
+/**
+ * **689. Maximum Sum of 3 Non-Overlapping Subarrays**
+ *
+ * Given an integer array `nums` and an integer `k`, find three non-overlapping
+ * subarrays of length k with maximum sum and return them.
+ *
+ * Return the result as a list of indices representing the starting position of
+ * each interval (**0-indexed**). If there are multiple answers, return the
+ * lexicographically smallest one.
+ *
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+const maxSumOfThreeSubarrays = function (nums, k) {};
