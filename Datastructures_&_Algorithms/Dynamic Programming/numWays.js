0121a8b726998c3ff81dd9e484a3754f12ea71a7
yuhcee <egbouchenna001@gmail.com>
2024-12-29 13:14:14 +0100

    :white_check_mark: (numWays) add test 1

--- Datastructures_&_Algorithms/Dynamic Programming/numWays.js
+++ Datastructures_&_Algorithms/Dynamic Programming/numWays.js
@@ -1,28 +1,28 @@
 /**
  * **1639. Number of Ways to Form a Target String Given a Dictionary**
  *
- * You are given a list of strings of the **same length** `words` and a string 
+ * You are given a list of strings of the **same length** `words` and a string
  * `target`.
  *
- * Your task is to form `target` using the given `words` under the following 
+ * Your task is to form `target` using the given `words` under the following
  * rules:
  *
  * - `target` should be formed from left to right.
- * - To form the `ith`character (**0-indexed**) of `target`, you can choose the 
+ * - To form the `ith`character (**0-indexed**) of `target`, you can choose the
  * `kth` character
  * of the `jth` string in `words` if `target[i] = words[j][k]`.
- * - Once you use the `kth` character of the `jth` string of `words`, you can 
+ * - Once you use the `kth` character of the `jth` string of `words`, you can
  * no longer use the
- * `xth` character of any string in `words` where `x <= k`. In other words, all 
+ * `xth` character of any string in `words` where `x <= k`. In other words, all
  * characters to
  * the left of or at index `k` become unusuable for every string.
  * - Repeat the process until you form the string `target`.
  *
- * **Notice** that you can use **multiple characters** from the **same string** 
+ * **Notice** that you can use **multiple characters** from the **same string**
  * in words
  * provided the conditions above are met.
  *
- * Return *the number of ways to form `target` from `words`*. Since the answer 
+ * Return *the number of ways to form `target` from `words`*. Since the answer
  * may be too
  * large, return it **modulo** 109 + 7.
  *
