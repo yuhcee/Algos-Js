ab5d1a8ad74950d79df2b53e1ebf27296059b547
yuhcee <egbouchenna001@gmail.com>
2024-12-30 10:26:55 +0100

    :white_check_mark: (countGoodStrings) add test 1

--- Datastructures_&_Algorithms/Dynamic Programming/countGoodStrings.js
+++ Datastructures_&_Algorithms/Dynamic Programming/countGoodStrings.js
@@ -1,8 +1,8 @@
 /**
  * **2466. Count Ways To Build Good Strings**
  *
- * Given the integers `zero`, `one`, `low`, and `high`, we can construct a 
- * string by starting with an empty string, and then at each step perform 
+ * Given the integers `zero`, `one`, `low`, and `high`, we can construct a
+ * string by starting with an empty string, and then at each step perform
  * either of the following:
  *
  * - Append the character `'0'` zero times.
@@ -13,8 +13,8 @@
  * A **good** string is a string constructed by the above process having a
  * **length** between `low` and `high` (**inclusive**).
  *
- * Return *the number of **different** good strings that can be constructed 
- * satisfying these properties*. Since the answer can be large, return it 
+ * Return *the number of **different** good strings that can be constructed
+ * satisfying these properties*. Since the answer can be large, return it
  * **modulo** `109 + 7`.
  *
  * **Constraints:**
