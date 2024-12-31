2bcdde76d96b3eda38159cb2845ea045bcc2069a
yuhcee <egbouchenna001@gmail.com>
2024-12-31 18:52:12 +0100

    :sparkles: (mincostTickets) add task solution

--- Datastructures_&_Algorithms/Dynamic Programming/mincostTickets.js
+++ Datastructures_&_Algorithms/Dynamic Programming/mincostTickets.js
@@ -1,8 +1,8 @@
 /**
  * **983. Minimum Cost For Tickets**
  *
- * You have planned some train traveling one year in advance. The days of the 
- * year in which you will travel are given as an integer array `days`. Each day 
+ * You have planned some train traveling one year in advance. The days of the
+ * year in which you will travel are given as an integer array `days`. Each day
  * is an integer from `1` to `365`.
  *
  * Train tickets are sold in **three different ways**:
@@ -13,10 +13,10 @@
  *
  * The passes allow that many days of consecutive travel.
  *
- * - For example, if we get a **7-day** pass on day `2`, then we can travel for 
+ * - For example, if we get a **7-day** pass on day `2`, then we can travel for
  * `7` days: `2`, `3`, `4`, `5`, `6`, `7`, and `8`.
  *
- * Return *the minimum number of dollars you need to travel every day in the 
+ * Return *the minimum number of dollars you need to travel every day in the
  * given list of days*.
  *
  * **Constraints:**
@@ -35,6 +35,7 @@
 const mincostTickets = function (days, costs) {
     const dp = new Array(366).fill(0);
     let j = 0;
+
     for (let i = 1; i < 366; i++) {
         if (i === days[j]) {
             dp[i] = Math.min(dp[Math.max(0, i - 1)] + costs[0], dp[Math.max(0, i - 7)] + costs[1], dp[Math.max(0, i - 30)] + costs[2]);
