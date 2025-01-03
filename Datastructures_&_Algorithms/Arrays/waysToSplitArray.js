a6b677271a7a1b17c4aff78c09f6291092a7a9ab
yuhcee <egbouchenna001@gmail.com>
2025-01-03 23:25:32 +0100

    :white_check_mark: (waysToSplitArray) add test case 2

--- Datastructures_&_Algorithms/Arrays/waysToSplitArray.js
+++ Datastructures_&_Algorithms/Arrays/waysToSplitArray.js
@@ -47,3 +47,11 @@ There are three ways of splitting nums into two non-empty parts:
 - Split nums at index 2. Then, the first part is [10,4,-8], and its sum is 6. The second part is [7], and its sum is 7. Since 6 < 7, i = 2 is not a valid split.
 Thus, the number of valid splits in nums is 2. */
 console.log(waysToSplitArray(nums));
+
+const nums1 = [2, 3, 1, 0];
+// Output: 2
+/* Explanation: 
+There are two valid splits in nums:
+- Split nums at index 1. Then, the first part is [2,3], and its sum is 5. The second part is [1,0], and its sum is 1. Since 5 >= 1, i = 1 is a valid split. 
+- Split nums at index 2. Then, the first part is [2,3,1], and its sum is 6. The second part is [0], and its sum is 0. Since 6 >= 0, i = 2 is a valid split. */
+console.log(waysToSplitArray(nums1));
