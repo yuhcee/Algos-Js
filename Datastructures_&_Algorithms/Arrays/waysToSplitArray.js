63f53b0bef9f7ba3f23642614327927fba32d8c6
yuhcee <egbouchenna001@gmail.com>
2025-01-03 22:49:29 +0100

    :sparkles: (waysToSplitArray) add task solution

--- Datastructures_&_Algorithms/Arrays/waysToSplitArray.js
+++ Datastructures_&_Algorithms/Arrays/waysToSplitArray.js
@@ -20,4 +20,20 @@
  * @param {number[]} nums
  * @return {number}
  */
-const waysToSplitArray = function (nums) {};
+const waysToSplitArray = function (nums) {
+    const n = nums.length;
+    let totalSum = nums.reduce((sum, num) => sum + num, 0);
+    let leftSum = 0;
+    let validSplits = 0;
+
+    for (let i = 0; i < n - 1; i++) {
+        leftSum += nums[i];
+        let rightSum = totalSum - leftSum;
+
+        if (leftSum >= rightSum) {
+            validSplits++;
+        }
+    }
+
+    return validSplits;
+};
