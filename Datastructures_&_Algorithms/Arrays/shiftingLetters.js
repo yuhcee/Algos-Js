369028c243f18a5eabcd7a723bd4358f3d81ba24
yuhcee <egbouchenna001@gmail.com>
2025-01-05 14:46:44 +0100

    :books: (shiftingLetters) add task description

---
+++ Datastructures_&_Algorithms/Arrays/shiftingLetters.js
@@ -0,0 +1,22 @@
+/**
+ * **2381. Shifting Letters II**
+ *
+ * You are given a string `s` of lowercase English letters and a 2D integer
+ * array `shifts` where `shifts[i] = [starti, endi, directioni]`. For every
+ * `i`, **shift** the characters in `s` from the index `starti` to the index
+ * `endi` (**inclusive**) forward if `directioni = 1`, or shift the characters
+ * backward if `directioni = 0`.
+ *
+ * Shifting a character **forward** means replacing it with the **next** letter
+ * in the alphabet (wrapping around so that `'z'` becomes `'a'`). Similarly,
+ * shifting a character **backward** means replacing it with the **previous**
+ * letter in the alphabet (wrapping around so that `'a'` becomes `'z'`).
+ *
+ * Return *the final string after all such shifts to s are applied*.
+ *
+ *
+ * @param {string} s
+ * @param {number[][]} shifts
+ * @return {string}
+ */
+const shiftingLetters = function (s, shifts) {};
