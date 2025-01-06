4604a1e2b7d14c9b09be1abc1b7d8d16d07e782e
yuhcee <egbouchenna001@gmail.com>
2025-01-06 22:55:56 +0100

    :sparkles: (minOperations) add task solution

--- Datastructures_&_Algorithms/Strings/minOperations1.js
+++ Datastructures_&_Algorithms/Strings/minOperations1.js
@@ -26,7 +26,28 @@
  * @return {number[]}
  */
 const minOperations = function (boxes) {
-    
+    const n = boxes.length;
+    const answer = Array(n).fill(0);
+
+    // Left-to-right pass
+    let operations = 0; // Cumulative operations from the left
+    let balls = 0;      // Number of balls seen so far on the left
+    for (let i = 0; i < n; i++) {
+        answer[i] += operations;
+        balls += boxes[i] === '1' ? 1 : 0;
+        operations += balls; // Add balls to operations for the next index
+    }
+
+    // Right-to-left pass
+    operations = 0; // Reset cumulative operations for the right
+    balls = 0;      // Reset number of balls seen so far on the right
+    for (let i = n - 1; i >= 0; i--) {
+        answer[i] += operations;
+        balls += boxes[i] === '1' ? 1 : 0;
+        operations += balls; // Add balls to operations for the next index
+    }
+
+    return answer;
 };
 
 const boxes = '110';
