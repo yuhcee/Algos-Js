42c805270d2eeab82d3a2fd54e8bf1299a7d9700
yuhcee <egbouchenna001@gmail.com>
2025-01-07 04:24:55 +0100

    :books: (stringMatching) add task description

---
+++ Datastructures_&_Algorithms/Strings/stringMatching.js
@@ -0,0 +1,13 @@
+/**
+ * **1408. String Matching in an Array**
+ *
+ * Given an array of string `words`, return all strings in `words` that is
+ * a **substring** of another word. You can return the answer in **any
+ * order**.
+ *
+ * A **substring** is a contiguous sequence of characters within a string
+ *
+ * @param {string[]} words
+ * @return {string[]}
+ */
+const stringMatching = function (words) {};
