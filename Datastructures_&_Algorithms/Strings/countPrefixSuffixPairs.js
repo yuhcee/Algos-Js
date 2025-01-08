e45ad67d1a2f7e7d246024aec5cd503e43560ec5
yuhcee <egbouchenna001@gmail.com>
2025-01-08 04:43:01 +0100

    :white_check_mark: (countPrefixSuffixPairs) add test case 2

--- Datastructures_&_Algorithms/Strings/countPrefixSuffixPairs.js
+++ Datastructures_&_Algorithms/Strings/countPrefixSuffixPairs.js
@@ -37,3 +37,11 @@ i = 0 and j = 3 because isPrefixAndSuffix("a", "aa") is true.
 i = 1 and j = 2 because isPrefixAndSuffix("aba", "ababa") is true.
 Therefore, the answer is 4. */
 console.log(countPrefixSuffixPairs(words));
+
+const words1 = ['pa', 'papa', 'ma', 'mama'];
+// Output: 2
+/* Explanation: In this example, the counted index pairs are:
+i = 0 and j = 1 because isPrefixAndSuffix("pa", "papa") is true.
+i = 2 and j = 3 because isPrefixAndSuffix("ma", "mama") is true.
+Therefore, the answer is 2. */
+console.log(countPrefixSuffixPairs(words1));
