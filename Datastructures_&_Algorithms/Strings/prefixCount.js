e587a69911f3f87fca218bd2cea42a7eef96b1ce
yuhcee <egbouchenna001@gmail.com>
2025-01-09 23:57:03 +0100

    :heavy_plus_sign: (prefixCount) add  constraints

--- Datastructures_&_Algorithms/Strings/prefixCount.js
+++ Datastructures_&_Algorithms/Strings/prefixCount.js
@@ -1,13 +1,19 @@
 /**
  * **2185. Counting Words With a Given Prefix**
- * 
+ *
  * You are given an array of strings `words` and a string `pref`.
- * 
- * Return *the number of strings in `words` that contain `pref` as a 
+ *
+ * Return *the number of strings in `words` that contain `pref` as a
  * **prefix**.*
- * 
+ *
  * A **prefix** of a string s is any leading contiguous substring of s.
- * 
+ *
+ * **Constraints:**
+ *
+ * - `1 <= words.length <= 100`
+ * - `1 <= words[i].length, pref.length <= 100`
+ * - `words[i]` and `pref` consist of lowercase English letters.
+ *
  * @param {string[]} words
  * @param {string} pref
  * @return {number}
