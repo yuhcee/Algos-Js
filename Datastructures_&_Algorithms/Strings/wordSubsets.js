a9f02264bde92d84ec84d7bc704df53d51ddc6b2
yuhcee <egbouchenna001@gmail.com>
2025-01-10 23:00:18 +0100

    :books: (wordSubsets) add task description

---
+++ Datastructures_&_Algorithms/Strings/wordSubsets.js
@@ -0,0 +1,22 @@
+/**
+ * **916. Word Subsets**
+ * 
+ * You are given two string arrays `words1` and `words2`.
+ * 
+ * A string `b` is a **subset** of string `a` if every letter in `b` occurs 
+ * in `a` including multiplicity.
+ * 
+ * - For example, `"wrr"` is a subset of `"warrior"` but is not a subset of 
+ * `"world"`.
+ * 
+ * A string `a` from `words1` is **universal** if for every string `b` in 
+ * `words2`, `b` is a subset of `a`.
+ * 
+ * Return an array of all the **universal** strings in `words1`. You may 
+ * return the answer in **any order**.
+ * 
+ * @param {string[]} words1
+ * @param {string[]} words2
+ * @return {string[]}
+ */
+const wordSubsets = function (words1, words2) {};
