e245f82370c0445dc144bff39fee7d94af8e66c1
yuhcee <egbouchenna001@gmail.com>
2025-01-11 22:52:28 +0100

    :books: (canConstruct) add task description

---
+++ Datastructures_&_Algorithms/Strings/canConstruct.js
@@ -0,0 +1,12 @@
+/**
+ * **1400. Construct K Palindrome Strings**
+ *
+ * Given a string `s` and an integer `k`, return *`true` if you can use all
+ * the characters in `s` to construct `k` palindrome strings or `false`
+ * otherwise.*
+ *
+ * @param {string} s
+ * @param {number} k
+ * @return {boolean}
+ */
+const canConstruct = function (s, k) {};
