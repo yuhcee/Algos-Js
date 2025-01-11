32c2100e459b773b07289404be1e33c92e47d388
yuhcee <egbouchenna001@gmail.com>
2025-01-11 22:53:12 +0100

    :heavy_plus_sign: (canConstruct) add  constraints

--- Datastructures_&_Algorithms/Strings/canConstruct.js
+++ Datastructures_&_Algorithms/Strings/canConstruct.js
@@ -5,6 +5,12 @@
  * the characters in `s` to construct `k` palindrome strings or `false`
  * otherwise.*
  *
+ * **Constraints:**
+ *
+ * - `1 <= s.length <= 105`
+ * - `s` consists of lowercase English letters.
+ * - `1 <= k <= 105`
+ *
  * @param {string} s
  * @param {number} k
  * @return {boolean}
