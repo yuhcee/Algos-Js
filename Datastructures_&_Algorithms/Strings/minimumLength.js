675ea3adf372c8e416c7823999d22044b218c35a
yuhcee <egbouchenna001@gmail.com>
2025-01-13 04:42:32 +0100

    :books: (minimumLength) add task description

---
+++ Datastructures_&_Algorithms/Strings/minimumLength.js
@@ -0,0 +1,21 @@
+/**
+ * **3223. Minimum Length of String After Operations**
+ *
+ * You are given a string `s`.
+ *
+ * You can perform the following process on `s` **any** number of times:
+ *
+ * - Choose an index `i` in the string such that there is at least one
+ * character to the left of index `i` that is equal to `s[i]`, and at least
+ * one character to the right that is also equal to `s[i]`.
+ * - Delete the **closest** character to the **left** of index `i` that is
+ * equal to `s[i]`.
+ * - Delete the **closest** character to the **right** of index `i` that is
+ * equal to `s[i]`.
+ *
+ * Return the **minimum** length of the final string s that you can achieve.
+ *
+ * @param {string} s
+ * @return {number}
+ */
+const minimumLength = function (s) {};
