016f2724af67d98ab008b27c1b113d33bbfce026
yuhcee <egbouchenna001@gmail.com>
2025-01-15 22:41:27 +0100

    :books: (minimizeXor) add task description

---
+++ Datastructures_&_Algorithms/Greedy Algorithms/minimizeXor.js
@@ -0,0 +1,22 @@
+/**
+ * **2429. Minimize XOR**
+ *
+ * Given two positive integers `num1` and `num2`, find the positive integer
+ * `x` such that:
+ *
+ * - `x` has the same number of set bits as `num2`, and
+ * - The value `x XOR num1` is **minimal**.
+ *
+ * Note that `XOR` is the bitwise XOR operation.
+ *
+ * Return *the integer `x`*. The test cases are generated such that `x` is
+ * **uniquely determined**.
+ *
+ * The number of **set bits** of an integer is the number of `1`'s in its
+ * binary representation.
+ *
+ * @param {number} num1
+ * @param {number} num2
+ * @return {number}
+ */
+const minimizeXor = function (num1, num2) {};
