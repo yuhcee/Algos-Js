5c58597a02f0f3103903e79731e701dd63bc7aea
yuhcee <egbouchenna001@gmail.com>
2025-01-16 07:13:49 +0100

    :books: (xorAllNums) add task description

---
+++ Datastructures_&_Algorithms/Math/xorAllNums.js
@@ -0,0 +1,16 @@
+/**
+ * **2425. Bitwise XOR of All Pairings**
+ *
+ * You are given two **0-indexed** arrays, `nums1` and `nums2`, consisting
+ * of non-negative integers. There exists another array, `nums3`, which
+ * contains the bitwise XOR of **all pairings** of integers between `nums1`
+ * and `nums2` (every integer in `nums1` is paired with every integer in
+ * `nums2` **exactly once**).
+ *
+ * Return the **bitwise XOR** of all integers in nums3.
+ *
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ */
+const xorAllNums = function (nums1, nums2) {};
