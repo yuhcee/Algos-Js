8a85212c1ba62e8bd154b846526a258c7d35dc8f
yuhcee <egbouchenna001@gmail.com>
2025-01-19 15:04:53 +0100

    :books: (trapRainWater) add task description

---
+++ Datastructures_&_Algorithms/Graphs/trapRainWater.js
@@ -0,0 +1,11 @@
+/**
+ * **407. Trapping Rain Water II**
+ *
+ * Given an `m x n` integer matrix `heightMap` representing the height of
+ * each unit cell in a 2D elevation map, return *the volume of water it can
+ * trap after raining.*
+ *
+ * @param {number[][]} heightMap
+ * @return {number}
+ */
+const trapRainWater = function (heightMap) {};
