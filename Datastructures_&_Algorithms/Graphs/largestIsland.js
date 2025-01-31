b4f8cc2224a1cf930a8f7e9eaf1023becde1aca4
yuhcee <egbouchenna001@gmail.com>
2025-01-31 16:33:28 +0100

    :books: (largestIsland) add task description

---
+++ Datastructures_&_Algorithms/Graphs/largestIsland.js
@@ -0,0 +1,15 @@
+/**
+ * **827. Making A Large Island**
+ * 
+ * You are given an `n x n` binary matrix `grid`. You are allowed to change 
+ * at most one `0` to be `1`.
+ * 
+ * Return *the size of the largest **island** in `grid` after applying this 
+ * operation*.
+ * 
+ * An **island** is a 4-directionally connected group of `1`s.
+ * 
+ * @param {number[][]} grid
+ * @return {number}
+ */
+const largestIsland = function(grid) {}
