b3e4811811c3e32762fd11f8e577f3403d7545bd
yuhcee <egbouchenna001@gmail.com>
2025-02-01 14:22:51 +0100

    :sparkles: (isArraySpecial) add function solution

--- Datastructures_&_Algorithms/Arrays/isArraySpecial1.js
+++ Datastructures_&_Algorithms/Arrays/isArraySpecial1.js
@@ -15,4 +15,14 @@
  * @param {number[]} nums
  * @return {boolean}
  */
-const isArraySpecial = function (nums) {};
+const isArraySpecial = function (nums) {
+    if (nums.length === 1) return true;
+
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] % 2 === nums[i + 1] % 2) {
+            return false;
+        }
+    }
+
+    return true;
+};
