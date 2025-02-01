87299d9ab7a9807d488275d843e92c04db7506d4
yuhcee <egbouchenna001@gmail.com>
2025-02-01 14:25:52 +0100

    :white_check_mark: (isArraySpecial) add test case 2

--- Datastructures_&_Algorithms/Arrays/isArraySpecial1.js
+++ Datastructures_&_Algorithms/Arrays/isArraySpecial1.js
@@ -31,3 +31,8 @@ const nums = [1];
 Explanation:
 There is only one element. So the answer is true. */
 console.log(isArraySpecial(nums));
+
+const nums1 = [2, 1, 4];
+/* Output: true
+Explanation:
+There is only two pairs: (2,1) and (1,4), and both of them contain numbers with different parity. So the answer is true. */
