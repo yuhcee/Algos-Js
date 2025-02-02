0beb421ee9520a3c65ddd6ce99b37fd79b37cbfb
yuhcee <egbouchenna001@gmail.com>
2025-02-02 13:05:44 +0100

    :books: (check) add task description

---
+++ Datastructures_&_Algorithms/Arrays/check.js
@@ -0,0 +1,17 @@
+/**
+ * **1752. Check if Array Is Sorted and Rotated**
+ * 
+ * Given an array `nums`, return *`true` if the array was originally sorted 
+ * in non-decreasing order, then rotated some number of positions 
+ * (including zero).* Otherwise, return `false`.
+ * 
+ * There may be **duplicates** in the original array.
+ * 
+ * **Note**: An array A rotated by `x` positions results in an array `B` of 
+ * the same length such that `A[i] == B[(i+x) % A.length]`, where `%` is 
+ * the modulo operation.
+ * 
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+const check = function (nums) {};
