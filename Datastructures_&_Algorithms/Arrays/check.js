44e72edd59c412be06931b5a3d6e20e5587631a2
yuhcee <egbouchenna001@gmail.com>
2025-02-02 13:06:47 +0100

    :sparkles: (check) add function solution

--- Datastructures_&_Algorithms/Arrays/check.js
+++ Datastructures_&_Algorithms/Arrays/check.js
@@ -19,4 +19,15 @@
  * @param {number[]} nums
  * @return {boolean}
  */
-const check = function (nums) {};
+const check = function (nums) {
+    let count = 0;
+    let n = nums.length;
+
+    for (let i = 0; i < n; i++) {
+        if (nums[i] > nums[(i + 1) % n]) {
+            count++;
+        }
+    }
+
+    return count <= 1;
+};
