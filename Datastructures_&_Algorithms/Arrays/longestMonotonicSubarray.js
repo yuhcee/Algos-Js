6fa175e259a4769359b8c7eedd07ad25bf5002f7
yuhcee <egbouchenna001@gmail.com>
2025-02-03 06:37:26 +0100

    :sparkles: (longestMonotonicSubarray) add function solution

--- Datastructures_&_Algorithms/Arrays/longestMonotonicSubarray.js
+++ Datastructures_&_Algorithms/Arrays/longestMonotonicSubarray.js
@@ -13,4 +13,25 @@
  * @param {number[]} nums
  * @return {number}
  */
-const longestMonotonicSubarray = function (nums) {};
+const longestMonotonicSubarray = function (nums) {
+    let maxLen = 1;
+    let incLen = 1,
+        decLen = 1;
+
+    for (let i = 1; i < nums.length; i++) {
+        if (nums[i] > nums[i - 1]) {
+            incLen++;
+            decLen = 1; // Reset decreasing count
+        } else if (nums[i] < nums[i - 1]) {
+            decLen++;
+            incLen = 1; // Reset increasing count
+        } else {
+            incLen = 1;
+            decLen = 1;
+        }
+
+        maxLen = Math.max(maxLen, incLen, decLen);
+    }
+
+    return maxLen;
+};
