af1203fdb0aa281d9cc6e257716f2ef36d6eeadf
yuhcee <egbouchenna001@gmail.com>
2025-02-03 06:37:01 +0100

    :heavy_plus_sign: (longestMonotonicSubarray) add  constraints

--- Datastructures_&_Algorithms/Arrays/longestMonotonicSubarray.js
+++ Datastructures_&_Algorithms/Arrays/longestMonotonicSubarray.js
@@ -5,6 +5,11 @@
  * **longest** subarray of `nums` which is either **strictly increasing**
  * or **strictly decreasing**.
  *
+ * **Constraints:**
+ *
+ * - `1 <= nums.length <= 50`
+ * - `1 <= nums[i] <= 50`
+ *
  * @param {number[]} nums
  * @return {number}
  */
