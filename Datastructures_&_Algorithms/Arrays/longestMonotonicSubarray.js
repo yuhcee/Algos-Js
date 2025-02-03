621601c9e53a796c2efdff1749f2f744b12965da
yuhcee <egbouchenna001@gmail.com>
2025-02-03 06:39:12 +0100

    :white_check_mark: (longestMonotonicSubarray) add test case 2

--- Datastructures_&_Algorithms/Arrays/longestMonotonicSubarray.js
+++ Datastructures_&_Algorithms/Arrays/longestMonotonicSubarray.js
@@ -42,3 +42,11 @@ The strictly increasing subarrays of nums are [1], [2], [3], [3], [4], and [1,4]
 The strictly decreasing subarrays of nums are [1], [2], [3], [3], [4], [3,2], and [4,3].
 Hence, we return 2. */
 console.log(longestMonotonicSubarray(nums));
+
+const nums1 = [3, 3, 3, 3];
+/* Output: 1
+Explanation:
+The strictly increasing subarrays of nums are [3], [3], [3], and [3].
+The strictly decreasing subarrays of nums are [3], [3], [3], and [3].
+Hence, we return 1. */
+console.log(longestMonotonicSubarray(nums1));
