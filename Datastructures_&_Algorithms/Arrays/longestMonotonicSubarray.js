2945a413f8609585ab9f0229e5ab7ee0c0cf8143
yuhcee <egbouchenna001@gmail.com>
2025-02-03 06:36:21 +0100

    :books: (longestMonotonicSubarray) add task description

---
+++ Datastructures_&_Algorithms/Arrays/longestMonotonicSubarray.js
@@ -0,0 +1,11 @@
+/**
+ * **3105. Longest Strictly Increasing or Strictly Decreasing Subarray**
+ *
+ * You are given an array of integers `nums`. Return *the length of the
+ * **longest** subarray of `nums` which is either **strictly increasing**
+ * or **strictly decreasing**.
+ *
+ * @param {number[]} nums
+ * @return {number}
+ */
+const longestMonotonicSubarray = function (nums) {};
