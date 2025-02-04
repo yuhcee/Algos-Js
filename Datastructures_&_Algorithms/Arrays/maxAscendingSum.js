44e319c17aaf521eef3ce80e205107788c1a0565
yuhcee <egbouchenna001@gmail.com>
2025-02-04 15:29:50 +0100

    :books: (maxAscendingSum) add task description

---
+++ Datastructures_&_Algorithms/Arrays/maxAscendingSum.js
@@ -0,0 +1,16 @@
+/**
+ * **1800. Maximum Ascending Subarray Sum**
+ *
+ * Given an array of positive integers `nums`, return the maximum possible
+ * sum of an **ascending** subarray in `nums`.
+ *
+ * A subarray is defined as a contiguous sequence of numbers in an array.
+ *
+ * A subarray `[numsl, numsl+1, ..., numsr-1, numsr]` is **ascending** if
+ * for all i where l <= i < r, numsi  < numsi+1. Note that a subarray of
+ * size 1 is **ascending**.
+ *
+ * @param {number[]} nums
+ * @return {number}
+ */
+const maxAscendingSum = function (nums) {};
