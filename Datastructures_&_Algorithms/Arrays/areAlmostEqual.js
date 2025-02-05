57184e8eb30cbfbc2d5ef7030c4f91190373eed8
yuhcee <egbouchenna001@gmail.com>
2025-02-05 23:39:56 +0100

    :books: (areAlmostEqual) add task description

---
+++ Datastructures_&_Algorithms/Arrays/areAlmostEqual.js
@@ -0,0 +1,16 @@
+/**
+ * **1790. Check if One String Swap Can Make Strings Equal**
+ *
+ * You are given two strings `s1` and `s2` of equal length. A **string
+ * swap** is an operation where you choose two indices in a string (not
+ * necessarily different) and swap the characters at these indices.
+ *
+ * Return `true` if it is possible to make both strings equal by performing
+ * **at most one string swap** on **exactly one** of the strings.
+ * Otherwise, return `false`.
+ *
+ * @param {string} s1
+ * @param {string} s2
+ * @return {boolean}
+ */
+const areAlmostEqual = function (s1, s2) {};
