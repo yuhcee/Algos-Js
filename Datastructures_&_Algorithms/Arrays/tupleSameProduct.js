2ad05f3af9440ffbb171e4b741985f41f251a709
yuhcee <egbouchenna001@gmail.com>
2025-02-06 22:03:49 +0100

    :heavy_plus_sign: (tupleSameProduct) add  constraints

--- Datastructures_&_Algorithms/Arrays/tupleSameProduct.js
+++ Datastructures_&_Algorithms/Arrays/tupleSameProduct.js
@@ -5,6 +5,12 @@
  * number of tuples `(a, b, c, d)` such that `a * b = c * d` where `a`,
  * `b`, `c`, and `d` are elements of `nums`, and `a != b != c != d`.*
  *
+ * **Constraints:**
+ *
+ * - `1 <= nums.length <= 1000`
+ * - `1 <= nums[i] <= 104`
+ * - All elements in `nums` are distinct.
+ *
  * @param {number[]} nums
  * @return {number}
  */
