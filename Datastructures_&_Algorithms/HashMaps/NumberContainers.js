faef290106f75da68dfca5bbc9e3b9c4dc4c6cdc
yuhcee <egbouchenna001@gmail.com>
2025-02-08 13:47:57 +0100

    :sparkles: (NumberContainers) add change function

--- Datastructures_&_Algorithms/HashMaps/NumberContainers.js
+++ Datastructures_&_Algorithms/HashMaps/NumberContainers.js
@@ -25,3 +25,38 @@ const NumberContainers = function () {
     this.indexToNumber = new Map(); // Maps index to number
     this.numberToIndices = new Map(); // Maps number to a sorted list of indices
 };
+
+/**
+ * @param {number} index
+ * @param {number} number
+ * @return {void}
+ */
+NumberContainers.prototype.change = function (index, number) {
+    // If the index already has a number, remove it from the old number's indices
+    if (this.indexToNumber.has(index)) {
+        const oldNumber = this.indexToNumber.get(index);
+        const indices = this.numberToIndices.get(oldNumber);
+        const indexPos = indices.indexOf(index);
+        if (indexPos !== -1) {
+            indices.splice(indexPos, 1);
+        }
+        if (indices.length === 0) {
+            this.numberToIndices.delete(oldNumber);
+        }
+    }
+
+    // Update the index to the new number
+    this.indexToNumber.set(index, number);
+
+    // Add the index to the new number's indices
+    if (!this.numberToIndices.has(number)) {
+        this.numberToIndices.set(number, []);
+    }
+    const newIndices = this.numberToIndices.get(number);
+    // Insert the index in a sorted manner
+    let insertPos = 0;
+    while (insertPos < newIndices.length && newIndices[insertPos] < index) {
+        insertPos++;
+    }
+    newIndices.splice(insertPos, 0, index);
+};
