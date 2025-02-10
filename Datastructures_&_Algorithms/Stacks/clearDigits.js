a3ee52e625ae46aaf32e27aac2172d147c22f143
yuhcee <egbouchenna001@gmail.com>
2025-02-10 07:43:59 +0100

    :sparkles: (clearDigits) add function solution

--- Datastructures_&_Algorithms/Stacks/clearDigits.js
+++ Datastructures_&_Algorithms/Stacks/clearDigits.js
@@ -18,4 +18,21 @@
  * @param {string} s
  * @return {string}
  */
-const clearDigits = function (s) {};
+const clearDigits = function (s) {
+    const stack = [];
+    for (const char of s) {
+        if (/\d/.test(char)) {
+            // If the character is a digit, pop the closest non-digit character to its left
+            if (stack.length > 0) {
+                stack.pop();
+            }
+        } else {
+            // If the character is not a digit, push it onto the stack
+            stack.push(char);
+        }
+    }
+    // Join the stack to form the resulting string
+    return stack.join('');
+};
+
+
