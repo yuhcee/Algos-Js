105f01eb06c5345de2113a4bcdc9da0388579e7d
yuhcee <egbouchenna001@gmail.com>
2025-02-10 07:39:56 +0100

    :books: (clearDigits) add task description

---
+++ Datastructures_&_Algorithms/Stacks/clearDigits.js
@@ -0,0 +1,15 @@
+/**
+ * **3174. Clear Digits**
+ *
+ * You are given a string `s`.
+ *
+ * Your task is to remove **all** digits by doing this operation repeatedly:
+ *
+ * - Delete the first digit and the **closest non-digit** character to its
+ * *left*.
+ * Return *the resulting string after removing all digits*.
+ *
+ * @param {string} s
+ * @return {string}
+ */
+const clearDigits = function (s) {};
