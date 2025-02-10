4e2e5e3b4362b0b9af7f274566c21d51c66fe22d
yuhcee <egbouchenna001@gmail.com>
2025-02-10 07:45:03 +0100

    :white_check_mark: (clearDigits) add test case 2

--- Datastructures_&_Algorithms/Stacks/clearDigits.js
+++ Datastructures_&_Algorithms/Stacks/clearDigits.js
@@ -39,3 +39,10 @@ const s = 'abc';
 // Output: "abc"
 // Explanation:There is no digit in the string.
 console.log(clearDigits(s));
+
+const s1 = 'cb34';
+// Output: ""
+/* Explanation:
+First, we apply the operation on s[2], and s becomes "c4".
+Then we apply the operation on s[1], and s becomes "". */
+console.log(clearDigits(s1));
