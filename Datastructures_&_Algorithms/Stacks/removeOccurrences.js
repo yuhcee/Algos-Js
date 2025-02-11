66b630c7fe2de595b5fe1223580154cbb748a61a
yuhcee <egbouchenna001@gmail.com>
2025-02-11 15:52:25 +0100

    :books: (removeOccurrences) add task description

---
+++ Datastructures_&_Algorithms/Stacks/removeOccurrences.js
@@ -0,0 +1,18 @@
+/**
+ * **1910. Remove All Occurrences of a Substring**
+ *
+ * Given two strings `s` and `part`, perform the following operation on `s`
+ * until **all** occurrences of the substring `part` are removed:
+ *
+ * - Find the **leftmost** occurrence of the substring `part` and
+ * **remove** it from `s`.
+ *
+ * Return *`s` after removing all occurrences of `part`.*
+ *
+ * A **substring** is a contiguous sequence of characters in a string.
+ *
+ * @param {string} s
+ * @param {string} part
+ * @return {string}
+ */
+const removeOccurrences = function (s, part) {};
