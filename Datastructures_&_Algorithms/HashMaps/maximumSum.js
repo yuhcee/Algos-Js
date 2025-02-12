2189ad6a21e8694b7fb55bf5d3f7d63439c7839c
yuhcee <egbouchenna001@gmail.com>
2025-02-12 07:47:45 +0100

    :sparkles: (maximumSum) add function solution

--- Datastructures_&_Algorithms/HashMaps/maximumSum.js
+++ Datastructures_&_Algorithms/HashMaps/maximumSum.js
@@ -17,4 +17,34 @@
  * @param {number[]} nums
  * @return {number}
  */
-const maximumSum = function (nums) {};
+const maximumSum = function (nums) {
+    const sumOfDigits = (num) => {
+        let sum = 0;
+        while (num > 0) {
+            sum += num % 10;
+            num = Math.floor(num / 10);
+        }
+        return sum;
+    };
+
+    const digitSumMap = new Map();
+
+    for (const num of nums) {
+        const sum = sumOfDigits(num);
+        if (!digitSumMap.has(sum)) {
+            digitSumMap.set(sum, []);
+        }
+        digitSumMap.get(sum).push(num);
+    }
+
+    for (const [sum, numbers] of digitSumMap.entries()) {
+        if (numbers.length >= 2) {
+            numbers.sort((a, b) => b - a);
+            const currentSum = numbers[0] + numbers[1];
+            if (currentSum > maxSum) {
+                maxSum = currentSum;
+            }
+        }
+    }
+    return maxSum;
+};
