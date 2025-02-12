c95822c549d4858ca4d57086a3d740a75418cb52
yuhcee <egbouchenna001@gmail.com>
2025-02-12 07:38:59 +0100

    :books: (maximumSum) add task description

---
+++ Datastructures_&_Algorithms/HashMaps/maximumSum.js
@@ -0,0 +1,15 @@
+/**
+ * **2342. Max Sum of a Pair With Equal Sum of Digits**
+ *
+ * You are given a **0-indexed** array `nums` consisting of **positive**
+ * integers. You can choose two indices `i` and `j`, such that `i != j`,
+ * and the sum of digits of the number `nums[i]` is equal to that of `nums
+ * [j]`.
+ *
+ * Return the **maximum** value of `nums[i] + nums[j]` that you can obtain
+ * over all possible indices `i` and `j` that satisfy the conditions.*
+ *
+ * @param {number[]} nums
+ * @return {number}
+ */
+const maximumSum = function (nums) {};
