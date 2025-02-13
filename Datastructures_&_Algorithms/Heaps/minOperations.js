93462563c76483444d8a459b9da17725aceb14f2
yuhcee <egbouchenna001@gmail.com>
2025-02-13 22:48:42 +0100

    :books: (minOperations) add task description

---
+++ Datastructures_&_Algorithms/Heaps/minOperations.js
@@ -0,0 +1,22 @@
+/**
+ * **3066. Minimum Operations to Exceed Threshold Value II**
+ *
+ * You are given a **0-indexed** integer array `nums`, and an integer `k`.
+ *
+ * You are allowed to perform some operations on `nums`, where in a single
+ * operation, you can:
+ *
+ * - Select the two smallest integers `x` and `y` from `nums`.
+ * - Remove `x` and `y` from `nums`.
+ * - Insert `(min(x, y) * 2 + max(x, y))` at any position in the array.
+ * **Note** that you can only apply the described operation if `nums`
+ * contains at least two elements.
+ *
+ * Return the **minimum** number of operations needed so that all elements
+ * of the array are greater than or equal to `k`.
+ *
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+const minOperations = function (nums, k) {};
