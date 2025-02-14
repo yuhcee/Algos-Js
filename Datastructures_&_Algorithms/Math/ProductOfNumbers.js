8ad07f45acd4081955c27d6ef466a638c810b893
yuhcee <egbouchenna001@gmail.com>
2025-02-14 15:31:41 +0100

    :books: (ProductOfNumbers) add task description

---
+++ Datastructures_&_Algorithms/Math/ProductOfNumbers.js
@@ -0,0 +1,37 @@
+var ProductOfNumbers = function () {};
+
+/**
+ * **1352. Product of the Last K Numbers**
+ *
+ * Design an algorithm that accepts a stream of integers and retrieves the
+ * product of the last `k` integers of the stream.
+ *
+ * Implement the `ProductOfNumbers` class:
+ *
+ * - `ProductOfNumbers()` Initializes the object with an empty stream.
+ * - `void add(int num)` Appends the integer `num` to the stream.
+ * - `int getProduct(int k)` Returns the product of the last `k` numbers in
+ * the current list. You can assume that always the current list has at
+ * least `k` numbers.
+ *
+ * The test cases are generated so that, at any time, the product of any
+ * contiguous sequence of numbers will fit into a single 32-bit integer
+ * without overflowing.
+ *
+ * @param {number} num
+ * @return {void}
+ */
+ProductOfNumbers.prototype.add = function (num) {};
+
+/**
+ * @param {number} k
+ * @return {number}
+ */
+ProductOfNumbers.prototype.getProduct = function (k) {};
+
+/**
+ * Your ProductOfNumbers object will be instantiated and called as such:
+ * var obj = new ProductOfNumbers()
+ * obj.add(num)
+ * var param_2 = obj.getProduct(k)
+ */
