0b0b0cbad48c7525de6af276a98f2d785aa91030
yuhcee <egbouchenna001@gmail.com>
2025-02-15 15:42:15 +0100

    :books: (punishmentNumber) add task description

---
+++ Datastructures_&_Algorithms/Math/punishmentNumber.js
@@ -0,0 +1,18 @@
+/**
+ * **2698. Find the Punishment Number of an Integer**
+ * 
+ * Given a positive integer `n`, return the **punishment number** 
+ * of `n`.
+ *
+ * The **punishment number** of n is defined as the sum of the 
+ * squares of all integers `i` such that:
+ *
+ * - `1 <= i <= n`
+ * - The decimal representation of `i * i` can be partitioned into 
+ * contiguous substrings such that the sum of the integer values 
+ * of these substrings equals `i`.
+ *
+ * @param {number} n
+ * @return {number}
+ */
+const punishmentNumber = function (n) {};
