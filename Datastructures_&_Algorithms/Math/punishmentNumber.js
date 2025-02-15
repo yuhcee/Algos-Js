d72939dd16dae73b7f5af51c7a0aee1b22632957
yuhcee <egbouchenna001@gmail.com>
2025-02-15 15:43:00 +0100

    :heavy_plus_sign: (punishmentNumber) add  constraints

--- Datastructures_&_Algorithms/Math/punishmentNumber.js
+++ Datastructures_&_Algorithms/Math/punishmentNumber.js
@@ -1,16 +1,20 @@
 /**
  * **2698. Find the Punishment Number of an Integer**
- * 
- * Given a positive integer `n`, return the **punishment number** 
+ *
+ * Given a positive integer `n`, return the **punishment number**
  * of `n`.
  *
- * The **punishment number** of n is defined as the sum of the 
+ * The **punishment number** of n is defined as the sum of the
  * squares of all integers `i` such that:
  *
  * - `1 <= i <= n`
- * - The decimal representation of `i * i` can be partitioned into 
- * contiguous substrings such that the sum of the integer values 
+ * - The decimal representation of `i * i` can be partitioned into
+ * contiguous substrings such that the sum of the integer values
  * of these substrings equals `i`.
+ *
+ * **Constraints:**
+ *
+ * - `1 <= n <= 1000`
  *
  * @param {number} n
  * @return {number}
