21d3ce64db7a41a20a8a8d08deb74fce7fe4c0cb
yuhcee <egbouchenna001@gmail.com>
2025-02-15 15:46:25 +0100

    :white_check_mark: (punishmentNumber) add test case 2

--- Datastructures_&_Algorithms/Math/punishmentNumber.js
+++ Datastructures_&_Algorithms/Math/punishmentNumber.js
@@ -50,3 +50,13 @@ const n = 10;
 - 10 since 10 * 10 = 100 and 100 can be partitioned into 10 and 0 with a sum equal to 10 + 0 == 10.
 Hence, the punishment number of 10 is 1 + 81 + 100 = 182 */
 console.log(punishmentNumber(n));
+
+const n1 = 37;
+// Output: 1478
+/* Explanation: There are exactly 4 integers i in the range [1, 37] that satisfy the conditions in the statement:
+- 1 since 1 * 1 = 1. 
+- 9 since 9 * 9 = 81 and 81 can be partitioned into 8 + 1. 
+- 10 since 10 * 10 = 100 and 100 can be partitioned into 10 + 0. 
+- 36 since 36 * 36 = 1296 and 1296 can be partitioned into 1 + 29 + 6.
+Hence, the punishment number of 37 is 1 + 81 + 100 + 1296 = 1478 */
+console.log(punishmentNumber(n1));
