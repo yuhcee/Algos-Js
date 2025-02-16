55804e25f6eb9a5a809cbdbb5517bf7c7f601d01
yuhcee <egbouchenna001@gmail.com>
2025-02-16 21:45:13 +0100

    :white_check_mark: (constructDistancedSequence) add test case 1

--- Datastructures_&_Algorithms/Arrays/constructDistancedSequence.js
+++ Datastructures_&_Algorithms/Arrays/constructDistancedSequence.js
@@ -70,3 +70,8 @@ const constructDistancedSequence = function (n) {
     backtrack(0);
     return result;
 };
+
+const n = 3;
+// Output: [3,1,2,3,2]
+// Explanation: [2,3,2,1,3] is also a valid sequence, but [3,1,2,3,2] is the lexicographically largest valid sequence.
+console.log(constructDistancedSequence(n));
