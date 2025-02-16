bf3c74ba27c99d459887ae784c53ba795b2d4efc
yuhcee <egbouchenna001@gmail.com>
2025-02-16 21:41:32 +0100

    :sparkles: (constructDistancedSequence) add task solution

--- Datastructures_&_Algorithms/Arrays/constructDistancedSequence.js
+++ Datastructures_&_Algorithms/Arrays/constructDistancedSequence.js
@@ -29,4 +29,44 @@
  * @param {number} n
  * @return {number[]}
  */
-const constructDistancedSequence = function (n) {};
+const constructDistancedSequence = function (n) {
+    const length = 2 * n - 1;
+    const result = new Array(length).fill(0);
+    const used = new Array(n + 1).fill(false);
+
+    const backtrack = (index) => {
+        if (index === length) {
+            return true;
+        }
+        if (result[index] !== 0) {
+            return backtrack(index + 1);
+        }
+        for (let num = n; num >= 1; num--) {
+            if (used[num]) continue;
+            if (num === 1) {
+                result[index] = 1;
+                used[1] = true;
+                if (backtrack(index + 1)) {
+                    return true;
+                }
+                result[index] = 0;
+                used[1] = false;
+            } else {
+                if (index + num >= length || result[index + num] !== 0) continue;
+                result[index] = num;
+                result[index + num] = num;
+                used[num] = true;
+                if (backtrack(index + 1)) {
+                    return true;
+                }
+                result[index] = 0;
+                result[index + num] = 0;
+                used[num] = false;
+            }
+        }
+        return false;
+    };
+
+    backtrack(0);
+    return result;
+};
