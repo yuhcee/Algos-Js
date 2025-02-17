36276ebef06eff1a27ff77f76be61e62d558f1da
yuhcee <egbouchenna001@gmail.com>
2025-02-17 21:23:16 +0100

    :books: (numTilePossibilities) add task description

---
+++ Datastructures_&_Algorithms/HashMaps/numTilePossibilities.js
@@ -0,0 +1,13 @@
+/**
+ * **1079. Letter Tile Possibilities**
+ *
+ * You have `n`  `tiles`, where each tile has one letter `tiles[i]`
+ * printed on it.
+ *
+ * Return *the number of possible non-empty sequences of letters* you
+ * can make using the letters printed on those `tiles`.
+ *
+ * @param {string} tiles
+ * @return {number}
+ */
+const numTilePossibilities = function (tiles) {};
