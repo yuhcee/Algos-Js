5a34720dbd498c46d73d3e054d0d1e71c15d7833
yuhcee <egbouchenna001@gmail.com>
2025-02-20 23:00:56 +0100

    :white_check_mark: (sortJumbled) add test case 1

--- Datastructures_&_Algorithms/Sorting/sortJumbled.js
+++ Datastructures_&_Algorithms/Sorting/sortJumbled.js
@@ -59,3 +59,17 @@ const sortJumbled = function (mapping, nums) {
     // Extract the original numbers in the sorted order
     return numsWithMapped.map((item) => item.original);
 };
+
+const mapping = [8, 9, 4, 0, 2, 1, 3, 5, 7, 6],
+    nums = [991, 338, 38];
+// Output: [338,38,991]
+/* Explanation: 
+Map the number 991 as follows:
+1. mapping[9] = 6, so all occurrences of the digit 9 will become 6.
+2. mapping[1] = 9, so all occurrences of the digit 1 will become 9.
+Therefore, the mapped value of 991 is 669.
+338 maps to 007, or 7 after removing the leading zeros.
+38 maps to 07, which is also 7 after removing leading zeros.
+Since 338 and 38 share the same mapped value, they should remain in the same relative order, so 338 comes before 38.
+Thus, the sorted array is [338,38,991]. */
+console.log(sortJumbled(mapping, nums));
