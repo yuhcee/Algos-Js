cb76c2a2ca769639eb92384cbb571950721a5e8e
yuhcee <egbouchenna001@gmail.com>
2025-02-22 22:41:59 +0100

    :books: (recoverFromPreorder) add task description

---
+++ Datastructures_&_Algorithms/Trees/BinaryTree/recoverFromPreorder.js
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * **1028. Recover a Tree From Preorder Traversal**
+ *
+ * We run a preorder depth-first search (DFS) on the `root` of a binary
+ * tree.
+ *
+ * At each node in this traversal, we output `D` dashes (where `D` is the
+ * depth of this node), then we output the value of this node.  If the
+ * depth of a node is `D`, the depth of its immediate child is `D + 1`.
+ * The depth of the `root` node is `0`.
+ *
+ * If a node has only one child, that child is guaranteed to be **the left
+ * child**.
+ *
+ * Given the output `traversal` of this traversal, recover the tree and
+ * return *its `root`*.
+ *
+ * @param {string} traversal
+ * @return {TreeNode}
+ */
+const recoverFromPreorder = function (traversal) {};
