5e7ca11cb882e40c159dacb8d26a9e7b315e9c40
yuhcee <egbouchenna001@gmail.com>
2025-02-23 14:29:48 +0100

    :books: (constructFromPrePost) add task description

---
+++ Datastructures_&_Algorithms/Trees/BinaryTree/constructFromPrePost.js
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * **889. Construct Binary Tree from Preorder and Postorder Traversal**
+ * 
+ * Given two integer arrays, `preorder` and `postorder` where `preorder` is 
+ * the preorder traversal of a binary tree of **distinct** values and 
+ * `postorder` is the postorder traversal of the same tree, reconstruct and 
+ * return the binary tree.
+ *
+ * If there exist multiple answers, you can **return any** of them.
+ *
+ * @param {number[]} preorder
+ * @param {number[]} postorder
+ * @return {TreeNode}
+ */
+const constructFromPrePost = function (preorder, postorder) {};
