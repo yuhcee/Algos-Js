921fa8841ed360dbc30ad59ff6e11c0285011f8e
yuhcee <egbouchenna001@gmail.com>
2025-02-25 11:24:22 +0100

    :books: (numOfSubarrays) add task description

---
+++ Datastructures_&_Algorithms/Arrays/numOfSubarrays.js
@@ -0,0 +1,12 @@
+/**
+ * **1524. Number of Sub-arrays With Odd Sum**
+ *
+ * Given an array of integers `arr`, return *the number of subarrays with
+ * an **odd** sum*.
+ *
+ * Since the answer can be very large, return it modulo `10^9 + 7`.
+ *
+ * @param {number[]} arr
+ * @return {number}
+ */
+const numOfSubarrays = function (arr) {};
