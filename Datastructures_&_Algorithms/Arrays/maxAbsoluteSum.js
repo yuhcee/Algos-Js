8d5e44d1c374a7288c9166988efd624ffc287aaf
yuhcee <egbouchenna001@gmail.com>
2025-02-26 23:21:04 +0100

    :white_check_mark: (maxAbsoluteSum) add test case 2

--- Datastructures_&_Algorithms/Arrays/maxAbsoluteSum.js
+++ Datastructures_&_Algorithms/Arrays/maxAbsoluteSum.js
@@ -44,3 +44,8 @@ const nums = [1, -3, 2, 3, -4];
 // Output: 5
 // Explanation: The subarray [2,3] has absolute sum = abs(2+3) = abs(5) = 5.
 console.log(maxAbsoluteSum(nums));
+
+const nums1 = [2, -5, 1, -4, 3, -2];
+// Output: 8
+// Explanation: The subarray [-5, 1, -4] has absolute sum = abs(-5 + 1 - 4) = abs(-8) = 8.
+console.log(maxAbsoluteSum(nums1));
