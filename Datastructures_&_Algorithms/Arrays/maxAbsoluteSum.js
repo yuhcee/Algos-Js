0951f4cf9146437ed4c8f074ab1f59e45d476afc
yuhcee <egbouchenna001@gmail.com>
2025-02-26 23:18:39 +0100

    :books: (maxAbsoluteSum) add task description

---
+++ Datastructures_&_Algorithms/Arrays/maxAbsoluteSum.js
@@ -0,0 +1,19 @@
+/**
+ * **1749. Maximum Absolute Sum of Any Subarray**
+ *
+ * You are given an integer array `nums`. The **absolute sum** of a
+ * subarray `[numsl, numsl+1, ..., numsr-1, numsr]` is `abs(numsl + numsl+1
+ * + ... + numsr-1 + numsr)`.
+ *
+ * Return *the **maximum** absolute sum of any **(possibly empty)**
+ * subarray of nums.*
+ *
+ * Note that `abs(x)` is defined as follows:
+ *
+ * If `x` is a negative integer, then `abs(x) = -x`.
+ * If `x` is a non-negative integer, then `abs(x) = x`.
+ *
+ * @param {number[]} nums
+ * @return {number}
+ */
+const maxAbsoluteSum = function (nums) {};
