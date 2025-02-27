daacdea0e3207aaf7c6d9ba7689debcb26aa5da0
yuhcee <egbouchenna001@gmail.com>
2025-02-27 14:32:43 +0100

    :white_check_mark: (lenLongestFibSubseq) add test case 2

--- Datastructures_&_Algorithms/Dynamic Programming/lenLongestFibSubseq.js
+++ Datastructures_&_Algorithms/Dynamic Programming/lenLongestFibSubseq.js
@@ -51,3 +51,8 @@ const arr = [1, 2, 3, 4, 5, 6, 7, 8];
 // Output: 5
 // Explanation: The longest subsequence that is fibonacci-like: [1,2,3,5,8].
 console.log(lenLongestFibSubseq(arr));
+
+const arr1 = [1, 3, 7, 11, 12, 14, 18];
+// Output: 3
+// Explanation: The longest subsequence that is fibonacci-like: [1,11,12], [3,11,14] or [7,11,18].
+console.log(lenLongestFibSubseq(arr1));
