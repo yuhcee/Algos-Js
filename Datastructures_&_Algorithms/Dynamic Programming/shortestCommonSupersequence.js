e365accc7b4346b211036c3adf01a4b5ed7384af
yuhcee <egbouchenna001@gmail.com>
2025-02-28 14:18:07 +0100

    :sparkles: (shortestCommonSupersequence) add task solution

--- Datastructures_&_Algorithms/Dynamic Programming/shortestCommonSupersequence.js
+++ Datastructures_&_Algorithms/Dynamic Programming/shortestCommonSupersequence.js
@@ -17,4 +17,53 @@
  * @param {string} str2
  * @return {string}
  */
-const shortestCommonSupersequence = function (str1, str2) {};
+const shortestCommonSupersequence = function (str1, str2) {
+    let m = str1.length,
+        n = str2.length;
+
+    // Step 1: Compute LCS using DP
+    let dp = Array(m + 1)
+        .fill(null)
+        .map(() => Array(n + 1).fill(0));
+
+    for (let i = 1; i <= m; i++) {
+        for (let j = 1; j <= n; j++) {
+            if (str1[i - 1] === str2[j - 1]) {
+                dp[i][j] = 1 + dp[i - 1][j - 1];
+            } else {
+                dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+    }
+
+    // Step 2: Construct SCS from LCS table
+    let i = m,
+        j = n;
+    let result = [];
+
+    while (i > 0 && j > 0) {
+        if (str1[i - 1] === str2[j - 1]) {
+            result.push(str1[i - 1]);
+            i--;
+            j--;
+        } else if (dp[i - 1][j] > dp[i][j - 1]) {
+            result.push(str1[i - 1]);
+            i--;
+        } else {
+            result.push(str2[j - 1]);
+            j--;
+        }
+    }
+
+    // Add remaining characters
+    while (i > 0) {
+        result.push(str1[i - 1]);
+        i--;
+    }
+    while (j > 0) {
+        result.push(str2[j - 1]);
+        j--;
+    }
+
+    return result.reverse().join('');
+};
