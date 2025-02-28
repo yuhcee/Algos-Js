8627e73a138ea6ec65bd0dede21a07786316564d
yuhcee <egbouchenna001@gmail.com>
2025-02-28 14:16:56 +0100

    :books: (shortestCommonSupersequence) add task description

---
+++ Datastructures_&_Algorithms/Dynamic Programming/shortestCommonSupersequence.js
@@ -0,0 +1,15 @@
+/**
+ * **1092. Shortest Common Supersequenc**
+ *
+ * Given two strings `str1` and `str2`, return *the shortest string that
+ * has both `str1` and `str2` as **subsequences***. If there are multiple
+ * valid strings, return **any** of them.
+ *
+ * A string `s` is a **subsequence** of string `t` if deleting some number
+ * of characters from `t` (possibly `0`) results in the string `s`.
+ *
+ * @param {string} str1
+ * @param {string} str2
+ * @return {string}
+ */
+const shortestCommonSupersequence = function (str1, str2) {};
