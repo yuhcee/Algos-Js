76e01b16e5211705f500b38c31cf98850343298d
yuhcee <egbouchenna001@gmail.com>
2025-02-28 14:17:44 +0100

    :heavy_plus_sign: (shortestCommonSupersequence) add  constraints

--- Datastructures_&_Algorithms/Dynamic Programming/shortestCommonSupersequence.js
+++ Datastructures_&_Algorithms/Dynamic Programming/shortestCommonSupersequence.js
@@ -8,6 +8,11 @@
  * A string `s` is a **subsequence** of string `t` if deleting some number
  * of characters from `t` (possibly `0`) results in the string `s`.
  *
+ * **Constraints:**
+ *
+ * - `1 <= str1.length, str2.length <= 1000`
+ * - `str1` and `str2` consist of lowercase English letters.
+ *
  * @param {string} str1
  * @param {string} str2
  * @return {string}
