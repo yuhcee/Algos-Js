77927beb28bc22a1ad6dc3dde98b8d4f108b828b
yuhcee <egbouchenna001@gmail.com>
2025-03-01 11:31:24 +0100

    :books: (applyOperations) add task description

---
+++ Datastructures_&_Algorithms/Arrays/applyOperations.js
@@ -0,0 +1,23 @@
+/**
+ * ** 2460. Apply Operations to an Array**
+ *
+ * You are given a **0-indexed** array `nums` of size `n` consisting of **non-negative** integers.
+ *
+ * You need to apply n - 1 operations to this array where, in the ith operation (**0-indexed**), you
+ * will apply the following on the ith element of nums:
+ *
+ * - If `nums[i] == nums[i + 1]`, then multiply `nums[i]` by `2` and set `nums[i + 1]` to `0`.
+ * Otherwise, you skip this operation.
+ *
+ * After performing **all** the operations, **shift** all the `0`'s to the **end** of the array.
+ *
+ * - For example, the array `[1,0,2,0,0,1]` after shifting all its `0`'s to the end, is `
+ * `.
+ * Return *the resulting array.*
+ *
+ * **Note** that the operations are applied **sequentially**, not all at once.
+ *
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+const applyOperations = function (nums) {};
