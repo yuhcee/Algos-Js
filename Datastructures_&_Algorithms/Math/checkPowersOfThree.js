47be9c308fb1909e61ee33ee5401ecf98a7ec75b
yuhcee <egbouchenna001@gmail.com>
2025-03-04 23:08:53 +0100

    :heavy_plus_sign: (checkPowersOfThree) add  constraints

--- Datastructures_&_Algorithms/Math/checkPowersOfThree.js
+++ Datastructures_&_Algorithms/Math/checkPowersOfThree.js
@@ -1,11 +1,15 @@
 /**
  * **1780. Check if Number is a Sum of Powers of Three**
- * 
- * Given an integer `n`, return `true` if it is possible to represent `n` as the sum of distinct 
+ *
+ * Given an integer `n`, return `true` if it is possible to represent `n` as the sum of distinct
  * powers of three. Otherwise, return `false`.
- * 
+ *
  * An integer `y` is a power of three if there exists an integer `x` such that `y == 3x`.
- * 
+ *
+ * **Constraints:**
+ *
+ * - `1 <= n <= 107`
+ *
  * @param {number} n
  * @return {boolean}
  */
