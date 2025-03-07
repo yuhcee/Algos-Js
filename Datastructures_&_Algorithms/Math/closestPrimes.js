a7db997890a8be03561c2e764f65ec04b21924a0
yuhcee <egbouchenna001@gmail.com>
2025-03-07 10:30:55 +0100

    :books: (closestPrimes) add task description

---
+++ Datastructures_&_Algorithms/Math/closestPrimes.js
@@ -0,0 +1,20 @@
+/**
+ * **2523. Closest Prime Numbers in Range**
+ *
+ * Given two positive integers `left` and `right`, find the two integers `num1` and
+ * `num2` such that:
+ *
+ * - `left <= num1 < num2 <= right` .
+ * - Both `num1` and `num2` are prime numbers.
+ * - `num2 - num1` is the **minimum** amongst all other pairs satisfying the above
+ * conditions.
+ *
+ * Return the positive integer array `ans = [num1, num2]`. If there are multiple pairs
+ * satisfying these conditions, return the one with the **smallest** num1 value. If no
+ * such numbers exist, return `[-1, -1]`.
+ *
+ * @param {number} left
+ * @param {number} right
+ * @return {number[]}
+ */
+const closestPrimes = function (left, right) {};
