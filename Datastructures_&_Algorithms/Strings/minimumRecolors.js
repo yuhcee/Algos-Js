19632515ff1823d2e5b6d28b84ccecc4d9d61a39
yuhcee <egbouchenna001@gmail.com>
2025-03-08 14:57:36 +0100

    :white_check_mark: (minimumRecolors) add test case 1

--- Datastructures_&_Algorithms/Strings/minimumRecolors.js
+++ Datastructures_&_Algorithms/Strings/minimumRecolors.js
@@ -44,3 +44,13 @@ const minimumRecolors = function (blocks, k) {
 
     return minOperations;
 };
+
+const blocks = 'WBBWWBBWBW',
+    k = 7;
+// Output: 3
+/* Explanation:
+One way to achieve 7 consecutive black blocks is to recolor the 0th, 3rd, and 4th blocks
+so that blocks = "BBBBBBBWBW". 
+It can be shown that there is no way to achieve 7 consecutive black blocks in less than 3 operations.
+Therefore, we return 3. */
+console.log(minimumRecolors(blocks, k));
