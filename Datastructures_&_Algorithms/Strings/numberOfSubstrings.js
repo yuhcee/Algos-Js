2101a0a7034d4b63cdca0cfe7216b38e0520cfde
yuhcee <egbouchenna001@gmail.com>
2025-03-11 21:56:12 +0100

    :books: (numberOfSubstrings) add task description

---
+++ Datastructures_&_Algorithms/Strings/numberOfSubstrings.js
@@ -0,0 +1,11 @@
+/**
+ * **1358. Number of Substrings Containing All Three Characters**
+ *
+ * Given a string `s` consisting only of characters a, b and c.
+ *
+ * Return the number of substrings containing **at least** one occurrence of all these characters a, b and c.
+ *
+ * @param {string} s
+ * @return {number}
+ */
+const numberOfSubstrings = function (s) {};
