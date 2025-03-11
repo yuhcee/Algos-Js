73b8f972da6595d644cb7cf3f87eb6f6c42b5b01
yuhcee <egbouchenna001@gmail.com>
2025-03-12 00:46:41 +0100

    :sparkles: (maximumCount) add task solution

--- Datastructures_&_Algorithms/Arrays/maximumCount.js
+++ Datastructures_&_Algorithms/Arrays/maximumCount.js
@@ -18,4 +18,12 @@
  * @param {number[]} nums
  * @return {number}
  */
-const maximumCount = function (nums) {};
+const maximumCount = function (nums) {
+    let negativeCount = 0,
+        positiveCount = 0;
+    for (let num of nums) {
+        if (num < 0) negativeCount++;
+        else if (num > 0) positiveCount++;
+    }
+    return Math.max(negativeCount, positiveCount);
+};
