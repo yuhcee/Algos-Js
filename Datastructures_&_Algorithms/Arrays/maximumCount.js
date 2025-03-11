2e5a66d0ca1d5144809a6850118a68010bda50a6
yuhcee <egbouchenna001@gmail.com>
2025-03-12 00:48:30 +0100

    :white_check_mark: (maximumCount) add test case 2

--- Datastructures_&_Algorithms/Arrays/maximumCount.js
+++ Datastructures_&_Algorithms/Arrays/maximumCount.js
@@ -33,3 +33,10 @@ const nums = [-2, -1, -1, 1, 2, 3];
 // Explanation: There are 3 positive integers and 3 negative integers. The maximum count among them is 3.
 // Example 2:
 console.log(maximumCount(nums));
+
+const nums1 = [-3, -2, -1, 0, 0, 1, 2];
+// Output: 3
+// Explanation: There are 2 positive integers and 3 negative integers. The maximum count among them is 3.
+// Example 3:
+console.log(maximumCount(nums1));
+
