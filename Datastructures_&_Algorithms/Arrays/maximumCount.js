8be627425994caf6b4e4ad381b088c93dea97541
yuhcee <egbouchenna001@gmail.com>
2025-03-12 00:45:22 +0100

    :books: (maximumCount) add task description

---
+++ Datastructures_&_Algorithms/Arrays/maximumCount.js
@@ -0,0 +1,15 @@
+/**
+ * **2529. Maximum Count of Positive Integer and Negative Integer**
+ *
+ * Given an array `nums` sorted in **non-decreasing** order, return the maximum between the number of positive
+ * integers and the number of negative integers.
+ *
+ * - In other words, if the number of positive integers in `nums` is `pos` and the number of negative integers is
+ * `neg`, then return the maximum of `pos` and `neg`.
+ *
+ * **Note** that `0` is neither positive nor negative.
+ *
+ * @param {number[]} nums
+ * @return {number}
+ */
+const maximumCount = function (nums) {};
