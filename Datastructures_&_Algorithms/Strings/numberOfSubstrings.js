a21484b8bec3c112a104da496fd8c5b06a9e5ed2
yuhcee <egbouchenna001@gmail.com>
2025-03-11 21:57:20 +0100

    :sparkles: (numberOfSubstrings) add task solution

--- Datastructures_&_Algorithms/Strings/numberOfSubstrings.js
+++ Datastructures_&_Algorithms/Strings/numberOfSubstrings.js
@@ -13,4 +13,30 @@
  * @param {string} s
  * @return {number}
  */
-const numberOfSubstrings = function (s) {};
+const numberOfSubstrings = function (s) {
+    // Initialize count of valid substrings
+    let count = 0;
+
+    // Array to keep track of character counts (a, b, c)
+    let charCount = [0, 0, 0];
+
+    // Left pointer of sliding window
+    let left = 0;
+
+    // Iterate through string with right pointer
+    for (let right = 0; right < s.length; right++) {
+        // Increment count for current character
+        charCount[s[right].charCodeAt(0) - 'a'.charCodeAt(0)]++;
+
+        // While window has at least one of each character
+        while (charCount[0] > 0 && charCount[1] > 0 && charCount[2] > 0) {
+            // All substrings from left to end of string are valid
+            count += s.length - right;
+            // Shrink window by moving left pointer
+            charCount[s[left].charCodeAt(0) - 'a'.charCodeAt(0)]--;
+            left++;
+        }
+    }
+
+    return count;
+};
