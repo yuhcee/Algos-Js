5faa20760f4c9796d1774762451fe6e204ade83d
yuhcee <egbouchenna001@gmail.com>
2025-03-13 23:28:38 +0100

    :white_check_mark: (minZeroArray) add test case 1

--- Datastructures_&_Algorithms/Arrays/minZeroArray.js
+++ Datastructures_&_Algorithms/Arrays/minZeroArray.js
@@ -75,3 +75,21 @@ const minZeroArray = function (nums, queries) {
 
     return ans;
 };
+
+const nums = [2, 0, 2],
+    queries = [
+        [0, 2, 1],
+        [0, 2, 1],
+        [1, 1, 3],
+    ];
+
+// Output: 2
+/* Explanation:
+For i = 0 (l = 0, r = 2, val = 1):
+Decrement values at indices [0, 1, 2] by [1, 0, 1] respectively.
+The array will become [1, 0, 1].
+For i = 1 (l = 0, r = 2, val = 1):
+Decrement values at indices [0, 1, 2] by [1, 0, 1] respectively.
+The array will become [0, 0, 0], which is a Zero Array. Therefore, the minimum value of k is 2.
+ */
+console.log(minZeroArray(nums, queries));
