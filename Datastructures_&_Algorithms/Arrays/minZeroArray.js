2367fd3291e9ba92c9e43180f7f355ab03f5266b
yuhcee <egbouchenna001@gmail.com>
2025-03-13 23:26:15 +0100

    :books: (minZeroArray) add task description

---
+++ Datastructures_&_Algorithms/Arrays/minZeroArray.js
@@ -0,0 +1,20 @@
+/**
+ * **3356. Zero Array Transformation II**
+ *
+ * You are given an integer array `nums` of length `n` and a 2D array `queries` where `queries[i] = [li, ri, vali]`.
+ *
+ * Each `queries[i]` represents the following action on `nums`:
+ *
+ * - Decrement the value at each index in the range `[li, ri]` in `nums` by at **mos** vali.
+ * - The amount by which each value is decremented can be chosen **independently** for each index.
+ *
+ * A **Zero Array** is an array with all its elements equal to 0.
+ *
+ * Return the **minimum** possible **non-negative** value of `k`, such that after processing the first `k` queries
+ * in **sequence**, nums becomes a **Zero Array**. If no such `k` exists, return -1.
+ *
+ * @param {number[]} nums
+ * @param {number[][]} queries
+ * @return {number}
+ */
+const minZeroArray = function (nums, queries) {};
