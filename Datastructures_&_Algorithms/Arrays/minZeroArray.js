5d5ec7f8fc7ee117ea10390cefb216344cb16d35
yuhcee <egbouchenna001@gmail.com>
2025-03-13 23:29:20 +0100

    :white_check_mark: (minZeroArray) add test case 2

--- Datastructures_&_Algorithms/Arrays/minZeroArray.js
+++ Datastructures_&_Algorithms/Arrays/minZeroArray.js
@@ -93,3 +93,19 @@ Decrement values at indices [0, 1, 2] by [1, 0, 1] respectively.
 The array will become [0, 0, 0], which is a Zero Array. Therefore, the minimum value of k is 2.
  */
 console.log(minZeroArray(nums, queries));
+
+const nums1 = [4, 3, 2, 1],
+    queries1 = [
+        [1, 3, 2],
+        [0, 2, 1],
+    ];
+// Output: -1
+/* Explanation:
+For i = 0 (l = 1, r = 3, val = 2):
+Decrement values at indices [1, 2, 3] by [2, 2, 1] respectively.
+The array will become [4, 1, 0, 0].
+For i = 1 (l = 0, r = 2, val = 1):
+Decrement values at indices [0, 1, 2] by [1, 1, 0] respectively.
+The array will become [3, 0, 0, 0], which is not a Zero Array.
+ */
+console.log(minZeroArray(nums1, queries1));
