8635afbb0a9549951e798b3e1395a8d11286cbb6
yuhcee <egbouchenna001@gmail.com>
2025-03-14 23:32:51 +0100

    :white_check_mark: (maximumCandies) add test case 2

--- Datastructures_&_Algorithms/Arrays/maximumCandies.js
+++ Datastructures_&_Algorithms/Arrays/maximumCandies.js
@@ -51,3 +51,9 @@ const candies = [5, 8, 6],
 // Output: 5
 /* Explanation: We can divide candies[1] into 2 piles of size 5 and 3, and candies[2] into 2 piles of size 5 and 1. We now have five piles of candies of sizes 5, 5, 3, 5, and 1. We can allocate the 3 piles of size 5 to 3 children. It can be proven that each child cannot receive more than 5 candies. */
 console.log(maximumCandies(candies, k));
+
+const candies1 = [2, 5],
+    k1 = 11;
+// Output: 0
+// Explanation: There are 11 children but only 7 candies in total, so it is impossible to ensure each child receives at least one candy. Thus, each child gets no candy and the answer is 0.
+console.log(maximumCandies(candies1, k1));
