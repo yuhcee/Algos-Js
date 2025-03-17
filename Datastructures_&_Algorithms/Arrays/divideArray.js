41615eceb3dc6eedcfb7ae4f8c3bfec248b88a9d
yuhcee <egbouchenna001@gmail.com>
2025-03-17 23:15:02 +0100

    :books: (divideArray) add task description

---
+++ Datastructures_&_Algorithms/Arrays/divideArray.js
@@ -0,0 +1,16 @@
+/**
+ * **2206. Divide Array Into Equal Pairs**
+ *
+ * You are given an integer array `nums` consisting of `2 * n` integers.
+ *
+ * You need to divide `nums` into `n` pairs such that:
+ *
+ * - Each element belongs to **exactly one** pair.
+ * - The elements present in a pair are **equal**.
+ *
+ * Return *`true` if nums can be divided into `n` pairs, otherwise return `false`*.
+ *
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+const divideArray = function (nums) {};
