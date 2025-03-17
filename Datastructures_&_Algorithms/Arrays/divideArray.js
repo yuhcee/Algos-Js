916a0ed277080618504757d5d34a5867cd6e79b2
yuhcee <egbouchenna001@gmail.com>
2025-03-17 23:16:45 +0100

    :white_check_mark: (divideArray) add test case 1

--- Datastructures_&_Algorithms/Arrays/divideArray.js
+++ Datastructures_&_Algorithms/Arrays/divideArray.js
@@ -36,3 +36,10 @@ const divideArray = function (nums) {
 
     return true;
 };
+
+const nums = [3, 2, 3, 2, 2, 2];
+// Output: true
+/* Explanation: 
+There are 6 elements in nums, so they should be divided into 6 / 2 = 3 pairs.
+If nums is divided into the pairs (2, 2), (3, 3), and (2, 2), it will satisfy all the conditions. */
+console.log(divideArray(nums));
