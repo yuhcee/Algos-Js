4b751d3c55c8c433fe1dc80d8bd4efcd92ea65a4
yuhcee <egbouchenna001@gmail.com>
2025-03-17 23:17:33 +0100

    :white_check_mark: (divideArray) add test case 2

--- Datastructures_&_Algorithms/Arrays/divideArray.js
+++ Datastructures_&_Algorithms/Arrays/divideArray.js
@@ -43,3 +43,9 @@ const nums = [3, 2, 3, 2, 2, 2];
 There are 6 elements in nums, so they should be divided into 6 / 2 = 3 pairs.
 If nums is divided into the pairs (2, 2), (3, 3), and (2, 2), it will satisfy all the conditions. */
 console.log(divideArray(nums));
+
+const nums1 = [1, 2, 3, 4];
+// Output: false
+/* Explanation: 
+There is no way to divide nums into 4 / 2 = 2 pairs such that the pairs satisfy every condition. */
+console.log(divideArray(nums1));
