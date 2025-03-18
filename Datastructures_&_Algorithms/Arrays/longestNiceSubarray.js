f7e05bc490fcc5a15308acacaabc1af8852a6954
yuhcee <egbouchenna001@gmail.com>
2025-03-18 22:51:01 +0100

    :books: (longestNiceSubarray) add task description

---
+++ Datastructures_&_Algorithms/Arrays/longestNiceSubarray.js
@@ -0,0 +1,18 @@
+/**
+ * **2401. Longest Nice Subarray**
+ *
+ * You are given an array `nums` consisting of **positive** integers.
+ *
+ * We call a subarray of `nums` nice if the bitwise **AND** of every pair of elements that are in **different** positions
+ * in the subarray is equal to `0`.
+ *
+ * Return *the length of the **longest** nice subarray*.
+ *
+ * A **subarray** is a **contiguous** part of an array.
+ *
+ * **Note** that subarrays of length 1 are always considered nice.
+ *
+ * @param {number[]} nums
+ * @return {number}
+ */
+const longestNiceSubarray = function (nums) {};
