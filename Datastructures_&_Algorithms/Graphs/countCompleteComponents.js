9b3eb7a194b275ebfa5627a4253224904f0f6ab5
yuhcee <egbouchenna001@gmail.com>
2025-03-22 13:08:12 +0100

    :books: (countCompleteComponents) add task description

---
+++ Datastructures_&_Algorithms/Graphs/countCompleteComponents.js
@@ -0,0 +1,19 @@
+/**
+ * **2685. Count the Number of Complete Components**
+ *
+ * You are given an integer `n`. There is an **undirected** graph with `n` vertices, numbered from `0` to `n - 1`. You
+ * are given a 2D integer array `edges` where `edges[i] = [ai, bi]` denotes that there exists an **undirected** edge
+ * connecting vertices `ai` and `bi`.
+ *
+ * Return the number of **complete connected components** of the graph.
+ *
+ * A **connected component** is a subgraph of a graph in which there exists a path between any two vertices, and no
+ * vertex of the subgraph shares an edge with a vertex outside of the subgraph.
+ *
+ * A connected component is said to be **complete** if there exists an edge between every pair of its vertices.
+ *
+ * @param {number} n
+ * @param {number[][]} edges
+ * @return {number}
+ */
+const countCompleteComponents = function (n, edges) {};
