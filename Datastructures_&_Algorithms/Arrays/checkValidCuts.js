bd09374a111613375fe63217e27cd846dc9c156b
yuhcee <egbouchenna001@gmail.com>
2025-03-25 23:22:09 +0100

    :white_check_mark: (checkValidCuts) add test case 2

--- Datastructures_&_Algorithms/Arrays/checkValidCuts.js
+++ Datastructures_&_Algorithms/Arrays/checkValidCuts.js
@@ -60,3 +60,15 @@ const n = 5,
 // Output: true
 // Explanation: The grid is shown in the diagram. We can make horizontal cuts at y = 2 and y = 4. Hence, output is true.
 console.log(checkValidCuts(n, rectangles));
+
+const n1 = 4,
+    rectangles1 = [
+        [0, 0, 1, 1],
+        [2, 0, 3, 4],
+        [0, 2, 2, 3],
+        [3, 0, 4, 3],
+    ];
+// Output: true
+/* Explanation:
+We can make vertical cuts at x = 2 and x = 3. Hence, output is true. */
+console.log(checkValidCuts(n1, rectangles1));
