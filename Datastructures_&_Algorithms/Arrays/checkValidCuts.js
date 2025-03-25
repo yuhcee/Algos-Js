383e2d8ebadf5223ae526776a626e28bcdc3a790
yuhcee <egbouchenna001@gmail.com>
2025-03-25 23:19:55 +0100

    :sparkles: (checkValidCuts) add task solution

--- Datastructures_&_Algorithms/Arrays/checkValidCuts.js
+++ Datastructures_&_Algorithms/Arrays/checkValidCuts.js
@@ -29,4 +29,23 @@
  * @param {number[][]} rectangles
  * @return {boolean}
  */
-const checkValidCuts = function (n, rectangles) {};
+const checkValidCuts = function (n, rectangles) {
+    return canCut(rectangles, 0) || canCut(rectangles, 1);
+};
+
+function canCut(rectangles, axis) {
+    rectangles.sort((a, b) => a[axis] - b[axis]);
+    let cuts = 0,
+        previousEnd = -1;
+
+    for (let rect of rectangles) {
+        let start = rect[axis],
+            end = rect[axis + 2];
+
+        if (start >= previousEnd) cuts++;
+        previousEnd = Math.max(previousEnd, end);
+        if (cuts >= 3) return true;
+    }
+
+    return false;
+}
