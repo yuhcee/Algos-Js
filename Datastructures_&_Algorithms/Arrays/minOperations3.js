46271fe85730e2d94656019d3c77102cffe59314
yuhcee <egbouchenna001@gmail.com>
2025-03-26 22:20:17 +0100

    :books: (minOperations) add task description

---
+++ Datastructures_&_Algorithms/Arrays/minOperations3.js
@@ -0,0 +1,15 @@
+/**
+ * **2033. Minimum Operations to Make a Uni-Value Grid**
+ *
+ * You are given a 2D integer `grid` of size `m x n` and an integer `x`. In one operation, you can add `x` to or subtract
+ * `x` from any element in the `grid`.
+ *
+ * A **uni-value grid** is a grid where all the elements of it are equal.
+ *
+ * Return the **minimum** number of operations to make the grid **uni-value**. If it is not possible, return `-1`.
+ *
+ * @param {number[][]} grid
+ * @param {number} x
+ * @return {number}
+ */
+const minOperations = function (grid, x) {};
