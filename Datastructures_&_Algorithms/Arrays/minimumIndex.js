edd9ccda9be0e2b80580945ebd6180ff26a45a3b
yuhcee <egbouchenna001@gmail.com>
2025-03-27 11:38:20 +0100

    :white_check_mark: (minimumIndex) add test case 2

--- Datastructures_&_Algorithms/Arrays/minimumIndex.js
+++ Datastructures_&_Algorithms/Arrays/minimumIndex.js
@@ -73,3 +73,12 @@ In array [2], element 2 is dominant since it occurs once in the array and 1 * 2 
 Both [1,2,2] and [2] have the same dominant element as nums, so this is a valid split. 
 It can be shown that index 2 is the minimum index of a valid split. */
 console.log(minimumIndex(nums));
+
+const nums1 = [2, 1, 3, 1, 1, 1, 7, 1, 2, 1];
+// Output: 4
+/* Explanation: We can split the array at index 4 to obtain arrays [2,1,3,1,1] and [1,7,1,2,1].
+In array [2,1,3,1,1], element 1 is dominant since it occurs thrice in the array and 3 * 2 > 5.
+In array [1,7,1,2,1], element 1 is dominant since it occurs thrice in the array and 3 * 2 > 5.
+Both [2,1,3,1,1] and [1,7,1,2,1] have the same dominant element as nums, so this is a valid split.
+It can be shown that index 4 is the minimum index of a valid split. */
+console.log(minimumIndex(nums1));
