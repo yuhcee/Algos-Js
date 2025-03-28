8274b071796a560429a379ce3765744dce4e3149
yuhcee <egbouchenna001@gmail.com>
2025-03-29 00:21:12 +0100

    :books: (putMarbles) add task description

---
+++ Datastructures_&_Algorithms/Greedy Algorithms/putMarbles.js
@@ -0,0 +1,24 @@
+/**
+ * **2551. Put Marbles in Bags**
+ *
+ * You have `k` bags. You are given a **0-indexed** integer array weights where weights[i] is the
+ * weight of the ith marble. You are also given the integer `k`.
+ *
+ * Divide the marbles into the `k` bags according to the following rules:
+ *
+ * - No bag is empty.
+ * - If the `ith` marble and `jth` marble are in a bag, then all marbles with an index between the
+ * `ith` and `jth` indices should also be in that same bag.
+ * - If a bag consists of all the marbles with an index from `i` to `j` inclusively, then the cost
+ * of the bag is `weights[i] + weights[j]`.
+ *
+ * The **score** after distributing the marbles is the sum of the costs of all the `k` bags.
+ *
+ * Return the **difference** between the **maximum** and **minimum** scores among marble
+ * distributions.
+ *
+ * @param {number[]} weights
+ * @param {number} k
+ * @return {number}
+ */
+const putMarbles = function (weights, k) {};
