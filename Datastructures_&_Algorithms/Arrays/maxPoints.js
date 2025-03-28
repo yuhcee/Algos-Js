d75f6119107f7a89c6367a160d993f35f7816063
yuhcee <egbouchenna001@gmail.com>
2025-03-28 22:09:07 +0100

    :books: (maxPoints) add task description

---
+++ Datastructures_&_Algorithms/Arrays/maxPoints.js
@@ -0,0 +1,24 @@
+/**
+ * **2503. Maximum Number of Points From Grid Queries**
+ * 
+ * You are given an `m x n` integer matrix `grid` and an array `queries` of size `k`.
+ * 
+ * Find an array `answer` of size `k` such that for each integer `queries[i]` you start in the top left cell of the 
+ * matrix and repeat the following process:
+ * 
+ * - If `queries[i]` is **strictly** greater than the value of the current cell that you are in, then you get one point 
+ * if it is your first time visiting this cell, and you can move to any **adjacent** cell in all `4` directions: up, 
+ * down, left, and right.
+ * 
+ * - Otherwise, you do not get any points, and you end this process.
+ * 
+ * After the process, `answer[i]` is the **maximum** number of points you can get. **Note** that for each query you are 
+ * allowed to visit the same cell **multiple** times.
+ * 
+ * Return the resulting array answer.
+ * 
+ * @param {number[][]} grid
+ * @param {number[]} queries
+ * @return {number[]}
+ */
+const maxPoints = function(grid, queries) {{}
