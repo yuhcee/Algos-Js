b08b67cdea4e096d537b8dee989e32c10038020c
yuhcee <egbouchenna001@gmail.com>
2025-03-30 23:50:42 +0100

    :sparkles: (partitionLabels) add task solution

--- Datastructures_&_Algorithms/Greedy Algorithms/partitionLabels.js
+++ Datastructures_&_Algorithms/Greedy Algorithms/partitionLabels.js
@@ -20,4 +20,28 @@
  * @param {string} s
  * @return {number[]}
  */
-const partitionLabels = function (s) {};
+const partitionLabels = function (s) {
+    // Step 1: Record the last occurrence of each character
+    const last = new Array(26).fill(-1);
+    for (let i = 0; i < s.length; i++) {
+        last[s.charCodeAt(i) - 'a'.charCodeAt(0)] = i;
+    }
+
+    // Step 2: Greedy partitioning
+    const result = [];
+    let start = 0;
+    let end = 0;
+
+    for (let i = 0; i < s.length; i++) {
+        // Update end to the farthest last occurrence of any character seen so far
+        end = Math.max(end, last[s.charCodeAt(i) - 'a'.charCodeAt(0)]);
+
+        // If we’ve reached the end of the current part
+        if (i === end) {
+            result.push(end - start + 1);
+            start = i + 1;
+        }
+    }
+
+    return result;
+};
