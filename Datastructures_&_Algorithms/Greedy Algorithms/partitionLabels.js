6c42bbb955307f8bb313cf55c8b72bab9b137958
yuhcee <egbouchenna001@gmail.com>
2025-03-30 23:49:32 +0100

    :books: (partitionLabels) add task description

---
+++ Datastructures_&_Algorithms/Greedy Algorithms/partitionLabels.js
@@ -0,0 +1,18 @@
+/**
+ * **763. Partition Labels**
+ *
+ *
+ * You are given a string `s`. We want to partition the string into as many parts as possible so
+ * that each letter appears in at most one part. For example, the string `"ababcc"` can be
+ * partitioned into `["abab", "cc"]`, but partitions such as `["aba", "bcc"]` or `["ab", "ab", "cc"]
+ * ` are invalid.
+ *
+ * Note that the partition is done so that after concatenating all the parts in order, the resultant
+ * string should be `s`.
+ *
+ * Return a list of integers representing the size of these parts.
+ *
+ * @param {string} s
+ * @return {number[]}
+ */
+const partitionLabels = function (s) {};
