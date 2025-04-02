290d1168ea0100bdc45048014ee350d5695ff522
yuhcee <egbouchenna001@gmail.com>
2025-04-02 23:09:26 +0100

    :books: (maximumTripletValue) add task description

---
+++ Datastructures_&_Algorithms/Arrays/maximumTripletValue.js
@@ -0,0 +1,13 @@
+/**
+ * **2873. Maximum Value of an Ordered Triplet I**
+ *
+ * You are given a **0-indexed** integer array nums.
+ *
+ * Return ***the maximum value over all triplets of indices** `(i, j, k)` such that `i < j < k`*. If all such triplets have a negative value, return 0.
+ *
+ * The **value of a triplet of indices** `(i, j, k)` is equal to `(nums[i] - nums[j]) * nums[k]`.
+ *
+ * @param {number[]} nums
+ * @return {number}
+ */
+const maximumTripletValue = function (nums) {};
