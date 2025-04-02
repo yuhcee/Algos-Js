9cbdca8909da9c4d395e2acfb2414f01a6c46577
yuhcee <egbouchenna001@gmail.com>
2025-04-02 23:13:46 +0100

    :white_check_mark: (maximumTripletValue) add test case 3

--- Datastructures_&_Algorithms/Arrays/maximumTripletValue.js
+++ Datastructures_&_Algorithms/Arrays/maximumTripletValue.js
@@ -44,3 +44,8 @@ const nums1 = [1, 10, 3, 4, 19];
 /* Explanation: The value of the triplet (1, 2, 4) is (nums[1] - nums[2]) * nums[4] = 133.
 It can be shown that there are no ordered triplets of indices with a value greater than 133. */
 console.log(maximumTripletValue(nums1)); // Output: 133
+
+const nums2 = [1, 2, 3];
+// Output: 0
+// Explanation: The only ordered triplet of indices (0, 1, 2) has a negative value of (nums[0] - nums[1]) * nums[2] = -3. Hence, the answer would be 0.
+console.log(maximumTripletValue(nums2)); // Output: 0
