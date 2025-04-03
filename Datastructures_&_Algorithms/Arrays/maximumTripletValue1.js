74386d1043abe97606d71b3c24972733d8c0d09b
yuhcee <egbouchenna001@gmail.com>
2025-04-03 23:19:28 +0100

    :heavy_plus_sign: (maximumTripletValue) add  constraints

--- Datastructures_&_Algorithms/Arrays/maximumTripletValue1.js
+++ Datastructures_&_Algorithms/Arrays/maximumTripletValue1.js
@@ -8,6 +8,11 @@
  *
  * The **value of a triplet of indices** `(i, j, k)` is equal to `(nums[i] - nums[j]) * nums[k]`.
  *
+ * **Constraints:**
+ *
+ * - `3 <= nums.length <= 105`
+ * - `1 <= nums[i] <= 106`
+ *
  * @param {number[]} nums
  * @return {number}
  */
